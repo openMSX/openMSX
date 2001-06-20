9a76c47f82959113bdcdc0e5478a5cb6f475af5d
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-06-20 19:23:16 +0000

    Joost: msxconfig code first touch of libxml++, compiles and links
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@382 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/msxconfig.cc
+++ src/msxconfig.cc
@@ -1,3 +1,30 @@
 // $Id$
 
 #include "msxconfig.hh"
+
+// you need libxml++ 0.13 and libxml2 > 2.0.0
+#include <xml++.h>
+
+MSXConfig *volatile MSXConfig::oneInstance;
+
+MSXConfig *MSXConfig::instance()
+{
+	if (oneInstance == 0)
+		oneInstance = new MSXConfig;
+	return oneInstance;
+}
+
+MSXConfig::MSXConfig():tree(0)
+{
+}
+
+MSXConfig::~MSXConfig()
+{
+	if (tree != 0)
+		delete tree;
+}
+
+int MSXConfig::loadFile(const string &filename)
+{
+	tree = new XMLTree(filename);
+}
--- src/msxconfig.hh
+++ src/msxconfig.hh
@@ -3,6 +3,32 @@
 #ifndef __MSXCONFIG_HH__
 #define __MSXCONFIG_HH__
 
+#include <string>
+
 // sample xml msxconfig file is available in msxconfig.xml in this dir
+
+// predefines
+class XMLTree;
+
+class MSXConfig
+{
+public:
+	// this class is a singleton class
+	// usage: MSXConfig::instance()->method(args);
+
+	static MSXConfig *instance();
+
+	int loadFile(const string &filename);
+	//void saveFile(const string &filename = 0);
+
+private:
+	MSXConfig(); // private constructor -> can only construct self
+	~MSXConfig();
+	MSXConfig(const MSXConfig &foo) {} // block usage
+	MSXConfig &operator=(const MSXConfig &foo) {} // block usage
+
+	static MSXConfig *volatile oneInstance;
+	XMLTree *tree;
+};
 
 #endif
