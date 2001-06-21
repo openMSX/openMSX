83771c3c196ddb0da36610340cb2d1a2a0dc5580
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-06-21 14:35:38 +0000

    Joost: more msxconfig stuff.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@389 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/msxconfig.hh
+++ src/msxconfig.hh
@@ -4,11 +4,28 @@
 #define __MSXCONFIG_HH__
 
 #include <string>
+#include <list>
 
 // sample xml msxconfig file is available in msxconfig.xml in this dir
 
 // predefines
 class XMLTree;
+class XMLNode;
+
+class MSXConfigDevice
+{
+public:
+	MSXConfigDevice(XMLNode *deviceNode);
+	const string &getType();
+	const string &getId();
+	bool  isSlotted();
+	int   getPage();
+	int   getPS();
+	int   getSS();
+	const string &getParameter(const string &name);
+private:
+	MSXConfigDevice() {};
+};
 
 class MSXConfig
 {
@@ -20,6 +37,10 @@ public:
 
 	bool loadFile(const string &filename);
 	//void saveFile(const string &filename = 0);
+
+	list<MSXConfigDevice*> deviceList;
+	list<MSXConfigDevice*> getDeviceByType(const string &type);
+	MSXConfigDevice *getDeviceById(const string &type);
 
 private:
 	MSXConfig(); // private constructor -> can only construct self
