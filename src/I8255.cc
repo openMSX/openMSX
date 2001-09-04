24eea73539b76fc27fa72fd23ffd2e349058cd60
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-09-04 16:15:54 +0000

    wouter: added two missing files
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@441 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/I8255.cc
@@ -0,0 +1,208 @@
+// $Id$
+
+#include "I8255.hh"
+#include <assert.h>
+
+
+I8255::I8255(I8255Interface &interf) : interface(interf)
+{
+	reset();
+}
+
+I8255::~I8255()
+{
+}
+
+void I8255::reset()
+{
+	latchPortA = 255;	// TODO check this behavior
+	latchPortB = 255;
+	latchPortC = 255;
+	writeControlPort(SET_MODE | DIRECTION_A | DIRECTION_B | 
+	                            DIRECTION_C0 | DIRECTION_C1); // all input
+}
+
+byte I8255::readPortA() {
+	switch (control & MODE_A) {
+	case MODEA_0:
+		if (control & DIRECTION_A) {
+			//input
+			return interface.readA();	// input not latched
+		} else {
+			//output
+			return latchPortA;		// output is latched
+		}
+	case MODEA_1:		//TODO but not relevant for MSX
+	case MODEA_2: case MODEA_2_:
+	default:
+		assert (false);
+	}
+}
+
+byte I8255::readPortB() {
+	switch (control & MODE_B) {
+	case MODEB_0:
+		if (control & DIRECTION_B) {
+			//input
+			return interface.readB();	// input not latched
+		} else {
+			//output
+			return latchPortB;		// output is latched
+		}
+	case MODEB_1:		// TODO but not relevant for MSX
+	default:
+		assert (false);
+	}
+}
+
+byte I8255::readPortC() {
+	byte tmp = readC1() & readC0();
+	switch (control & MODE_A) {
+	case MODEA_0:
+		// do nothing
+		break;
+	case MODEA_1:		// TODO but not relevant for MSX
+	case MODEA_2: case MODEA_2_:
+		assert (false);
+	}
+	switch (control & MODE_B) {
+	case MODEB_0:
+		// do nothing
+		break;
+	case MODEB_1:		// TODO but not relevant for MSX
+		assert (false);
+	}
+	return tmp;
+}
+
+byte I8255::readC1() {
+	if (control & DIRECTION_C1) {
+		//input
+		return interface.readC1() << 4;	// input not latched
+	} else {
+		//output
+		return latchPortC & 0xf0;	// output is latched
+	}
+}
+
+byte I8255::readC0() {
+	if (control & DIRECTION_C0) {
+		//input
+		return interface.readC0();		// input not latched
+	} else {
+		//output
+		return latchPortC & 0x0f;		// output is latched
+	}
+}
+
+byte I8255::readControlPort() {
+	return control;
+}
+
+void I8255::writePortA(byte value) {
+	switch (control & MODE_A) {
+	case MODEA_0:
+		// do nothing
+		break;
+	case MODEA_1:		// TODO but not relevant for MSX
+	case MODEA_2: case MODEA_2_:
+		assert (false);
+	}
+	outputPortA(value);
+}
+
+void I8255::writePortB(byte value) {
+	switch (control & MODE_B) {
+	case MODEB_0:
+		// do nothing
+		break;
+	case MODEB_1:		// TODO but not relevant for MSX
+		assert (false);
+	}
+	outputPortB(value);
+}
+
+void I8255::writePortC(byte value) {
+	switch (control & MODE_A) {
+	case MODEA_0:
+		// do nothing
+		break;
+	case MODEA_1:		// TODO but not relevant for MSX
+	case MODEA_2: case MODEA_2_:
+		assert (false);
+	}
+	switch (control & MODE_B) {
+	case MODEB_0:
+		// do nothing
+		break;
+	case MODEB_1:		// TODO but not relevant for MSX
+		assert (false);
+	}
+	outputPortC(value);
+}
+
+void I8255::outputPortA(byte value) {
+	latchPortA = value;
+	if (!(control & DIRECTION_A)) {
+		//output
+		interface.writeA(value);
+	}
+}
+	
+void I8255::outputPortB(byte value) {
+	latchPortB = value;
+	if (!(control & DIRECTION_B)) {
+		//output
+		interface.writeB(value);
+	}
+}
+	
+void I8255::outputPortC(byte value) {
+	latchPortC = value;
+	if (!(control & DIRECTION_C1)) {
+		//output
+		interface.writeC1(latchPortC >> 4);
+	}
+	if (!(control & DIRECTION_C0)) {
+		//output
+		interface.writeC0(latchPortC & 15);
+	}
+}
+
+void I8255::writeControlPort(byte value) {
+	if (value & SET_MODE) {
+		// set new control mode
+		control = value;
+		// TODO check this behavior, see tech docs
+		outputPortA(latchPortA);
+		outputPortB(latchPortB);
+		outputPortC(latchPortC);
+	} else {
+		// (re)set bit of port C
+		byte bitmask = 1 << ((value & BIT_NR) >> 1);
+		if (value & SET_RESET) {
+			// set
+			latchPortC |= bitmask;
+		} else {
+			// reset
+			latchPortC &= ~bitmask;
+		}
+		outputPortC(latchPortC);
+		switch (control & MODE_A) {
+		case MODEA_0:
+			// do nothing
+			break;
+		case MODEA_1:		// TODO but not relevant for MSX
+		case MODEA_2: case MODEA_2_:
+			assert (false);
+		}
+		switch (control & MODE_B) {
+		case MODEB_0:
+			// do nothing
+			break;
+		case MODEB_1:		// TODO but not relevant for MSX
+			assert (false);
+		}
+	}
+}
+	
---
+++ src/I8255.hh
@@ -0,0 +1,76 @@
+
+// This class implements the Intel 8255 chip
+//
+// * Only the 8255 is emulated, no surrounding hardware. 
+//   Use the class I8255Interface to do that.
+// * Only mode 0 (basic input/output) is implemented
+// * For techncal details on 8255 see
+//    http://.../8255.pdf    TODO: fix url
+
+#ifndef __I8255_HH__
+#define __I8255_HH__
+
+#include "MSXDevice.hh"		// for typedef byte, TODO split in seperate file
+
+
+class I8255Interface
+{
+	public:
+		virtual byte readA()=0;
+		virtual byte readB()=0;
+		virtual byte readC0()=0;
+		virtual byte readC1()=0;
+		virtual void writeA(byte value)=0;
+		virtual void writeB(byte value)=0;
+		virtual void writeC0(byte value)=0;
+		virtual void writeC1(byte value)=0;
+};
+
+class I8255
+{
+	public:
+		I8255(I8255Interface &interf); 
+		~I8255(); 
+		
+		void reset();
+	
+		byte readPortA();
+		byte readPortB();
+		byte readPortC();
+		byte readControlPort();
+		void writePortA(byte value);
+		void writePortB(byte value);
+		void writePortC(byte value);
+		void writeControlPort(byte value);
+		
+	private:
+		static const int MODE_A   = 0x60;
+		static const int MODEA_0  = 0x00;
+		static const int MODEA_1  = 0x20;
+		static const int MODEA_2  = 0x40;
+		static const int MODEA_2_ = 0x60;
+		static const int MODE_B  = 0x04;
+		static const int MODEB_0 = 0x00;
+		static const int MODEB_1 = 0x04;
+		static const int DIRECTION_A  = 0x10;
+		static const int DIRECTION_B  = 0x02;
+		static const int DIRECTION_C0 = 0x01;
+		static const int DIRECTION_C1 = 0x08;
+		static const int SET_MODE  = 0x80;
+		static const int BIT_NR    = 0x0e;
+		static const int SET_RESET = 0x01;
+		
+		byte readC0();
+		byte readC1();
+		void outputPortA(byte value);
+		void outputPortB(byte value);
+		void outputPortC(byte value);
+
+		int control;
+		int latchPortA;
+		int latchPortB;
+		int latchPortC;
+
+		I8255Interface &interface;
+};
+#endif
