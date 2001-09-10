29b5221cc80c0aab8e01ff8608f34e885e9443eb
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-09-10 08:37:06 +0000

    wouter: removed double initialization
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@452 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/openmsx.cc
+++ src/openmsx.cc
@@ -29,32 +29,27 @@
 
 //David Heremans
 
-int
-main (int argc, char **argv)
+int main (int argc, char **argv)
 {
 	char *configfile="msxconfig.xml";
 
 	if (argc<2) {
-		cout << "Using " << configfile << " as default configuration file." << endl;
+		PRT_INFO ("Using " << configfile << " as default configuration file.");
 	} else {
-	configfile=argv[1];
+		configfile=argv[1];
 	}
 	// this is mainly for me (Joost), for testing the xml parsing
-	try
-	{
+	try {
 		MSXConfig::instance()->loadFile(configfile);
-    	
-        list<MSXConfig::Device*>::const_iterator i=MSXConfig::instance()->deviceList.begin();
-    	for (; i != MSXConfig::instance()->deviceList.end(); i++)
-    	{
-    		(*i)->dump();
-    	}
-    }
-	catch (MSXException e)
-	{
+	
+		list<MSXConfig::Device*>::const_iterator i=MSXConfig::instance()->deviceList.begin();
+		for (; i != MSXConfig::instance()->deviceList.end(); i++) {
+			(*i)->dump();
+		}
+	} catch (MSXException e) {
 		cerr << e.desc << endl;
 		exit(1);
-    }
+	}
 	// End of Joosts test routine 
 	// Here comes the Real Stuff(tm) :-)
 	// (Actualy David's test stuff)
@@ -64,16 +59,12 @@ main (int argc, char **argv)
 	//MSXMotherBoard* MSX;
 	MSXMotherBoard* moederbord;
 	MSXDevice* device;
-	MSXDevice* geheugen;
-	MSXRom16KB *rom1,*rom2;
 	//MSXDevice* processor;
 	MSXZ80* processor;
 
 	//Werkt SDL ?
-	if (SDL_Init(SDL_INIT_VIDEO)<0)
-	{
-	  printf("Couldn't init SDL: %s\n", SDL_GetError());
-	  exit(1);
+	if (SDL_Init(SDL_INIT_VIDEO)<0) {
+		PRT_ERROR("Couldn't init SDL: " << SDL_GetError());
 	}
 	atexit(SDL_Quit);
 
@@ -81,15 +72,14 @@ main (int argc, char **argv)
 	moederbord=MSXMotherBoard::instance();
 	// en dan nu al de devices die volgens de xml nodig zijn
 	list<MSXConfig::Device*>::const_iterator j=MSXConfig::instance()->deviceList.begin();
-	for (; j != MSXConfig::instance()->deviceList.end(); j++)
-	{
+	for (; j != MSXConfig::instance()->deviceList.end(); j++) {
 		(*j)->dump();
 		//TODO 
 		//device=deviceFactory::create( (*j)->getType() );
 		device=deviceFactory::create( (*j) );
 		if (device != 0){
 			moederbord->addDevice(device);
-		cout << "---------------------------\nfactory:"<< (*j)->getType() << "\n\n";
+		PRT_DEBUG ("---------------------------\nfactory:" << (*j)->getType());
 		}
 	}
 	// dan maar de CPU
@@ -97,34 +87,10 @@ main (int argc, char **argv)
 	processor->Motherb=moederbord;
 	moederbord->addDevice(processor);
 	moederbord->setActiveCPU(processor);
-	// dan geheugen
-//	geheugen=new MSXSimple64KB();
-//	moederbord->addDevice(geheugen);
-//	moederbord->visibleDevices[2]=geheugen;
-//	moederbord->visibleDevices[3]=geheugen;
-	// dan rom 1
-//	rom1=new MSXRom16KB();
-//	strcpy(rom1->romfile,"BIOS.ROM");
-//	moederbord->addDevice(rom1);
-//	moederbord->visibleDevices[0]=rom1;
-	// dan rom 2
-//	rom2=new MSXRom16KB();
-//	strcpy(rom2->romfile,"BASIC.ROM");
-//	moederbord->addDevice(rom2);
-//	moederbord->visibleDevices[1]=rom2;
-	//moederbord->registerSlottedDevice(geheugen,0,0,0);
-	//moederbord->registerSlottedDevice(geheugen,0,0,1);
-//	moederbord->registerSlottedDevice(geheugen,0,0,2);
-//	moederbord->registerSlottedDevice(geheugen,0,0,3);
 	
 	cout << "Initing MSX\n\n";
 	moederbord->InitMSX();
-
-	cout << "Initing MSX\n\n";
-	moederbord->InitMSX();
 	
-	//Emutime dummy(DUMMY_FREQ, 100000);
-	//moederbord->scheduler.insertStamp(dummy , *geheugen); //om toch een SP te hebben :-)
 	cout << "starting MSX\n\n";
 	moederbord->StartMSX();
 
