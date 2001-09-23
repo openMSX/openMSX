c17b92159cb2d33f3accd9c4526e3e03a4e4f788
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-09-23 16:39:29 +0000

    Joost: added m4 dir, added autogen.sh script
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@504 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ autogen.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Generate the Makefiles and configure files
+if ( aclocal --version ) </dev/null > /dev/null 2>&1; then
+	echo "Building macros."
+	aclocal -I m4
+else
+	echo "aclocal not found -- aborting"
+	exit
+fi
+
+if ( autoheader --version ) </dev/null > /dev/null 2>&1; then
+	echo "Building config header template"
+	autoheader
+else
+	echo "autoheader not found -- aborting"
+	exit
+fi
+
+if ( autoconf --version ) </dev/null > /dev/null 2>&1; then
+	echo "Building configure"
+	autoconf
+	echo 'run "./configure ; make"'
+else
+	echo "autoconf not found -- aborting"
+	exit
+fi
+
+if ( automake --version ) </dev/null > /dev/null 2>&1; then
+	echo "Building Makefiles"
+	automake
+else
+	echo "automake not found -- aborting"
+	exit
+fi
+
+
+
