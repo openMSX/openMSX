8be2217c42307e6a6949321c34b2f62f7ed7cc18
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-09-24 19:37:36 +0000

    wouter: implemented mouse support. Cannot be used yet, same reason as for joystick
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@518 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/Mouse.cc
@@ -0,0 +1,99 @@
+
+#include "Mouse.hh"
+
+
+Mouse::Mouse()
+{
+	status = JOY_BUTTONA | JOY_BUTTONB;
+	faze = FAZE_YLOW;
+	xrel = yrel = curxrel = curyrel = 0;
+	EventDistributor::instance()->registerListener(SDL_MOUSEMOTION,     this);
+	EventDistributor::instance()->registerListener(SDL_MOUSEBUTTONDOWN, this);
+	EventDistributor::instance()->registerListener(SDL_MOUSEBUTTONUP,   this);
+}
+
+Mouse::~Mouse()
+{
+}
+
+//JoystickDevice
+byte Mouse::read()
+{
+	switch (faze) {
+	case FAZE_XHIGH:
+		return ((xrel>>4)&0x0f)|status;
+	case FAZE_XLOW:
+		return  (xrel    &0x0f)|status;
+	case FAZE_YHIGH:
+		return ((yrel>>4)&0x0f)|status;
+	case FAZE_YLOW:
+		return  (yrel    &0x0f)|status;
+	default:
+		assert(false);
+	}
+}
+
+void Mouse::write(byte value)
+{
+	switch (faze) {
+	case FAZE_XHIGH:
+		if ((value&STROBE)==0) faze=FAZE_XLOW;
+		break;
+	case FAZE_XLOW:
+		if ((value&STROBE)!=0) faze=FAZE_YHIGH;
+		break;
+	case FAZE_YHIGH:
+		if ((value&STROBE)==0) faze=FAZE_YLOW;
+		break;
+	case FAZE_YLOW:
+		if ((value&STROBE)!=0) {
+			faze=FAZE_XHIGH;
+			xrel = curxrel; yrel = curyrel;
+			curxrel = 0; curyrel = 0;
+		}
+		break;
+	}
+}
+
+//EventListener
+void Mouse::signalEvent(SDL_Event &event)
+{
+	switch (event.type) {
+	case SDL_MOUSEMOTION:
+		curxrel += event.motion.xrel;
+		curyrel += event.motion.yrel;
+		if (curxrel> 127) curxrel= 127;
+		if (curxrel<-128) curxrel=-128;
+		if (curyrel> 127) curyrel= 127;
+		if (curyrel<-128) curyrel=-128;
+		break;
+	case SDL_MOUSEBUTTONDOWN:
+		switch (event.button.button) {
+		case SDL_BUTTON_LEFT:
+			status &= ~JOY_BUTTONA;
+			break;
+		case SDL_BUTTON_RIGHT:
+			status &= ~JOY_BUTTONB;
+			break;
+		default:
+			// ignore other buttons
+			break;
+		}
+		break;
+	case SDL_MOUSEBUTTONUP:
+		switch (event.button.button) {
+		case SDL_BUTTON_LEFT:
+			status |= JOY_BUTTONA;
+			break;
+		case SDL_BUTTON_RIGHT:
+			status |= JOY_BUTTONB;
+			break;
+		default:
+			// ignore other buttons
+			break;
+		}
+		break;
+	default:
+		assert(false);
+	}
+}
---
+++ src/Mouse.hh
@@ -0,0 +1,37 @@
+
+#ifndef __MOUSE_HH__
+#define __MOUSE_HH__
+
+#include "JoystickDevice.hh"
+#include "EventDistributor.hh"
+#include <SDL/SDL.h>
+
+
+class Mouse : public JoystickDevice, EventListener
+{
+	public:
+		Mouse();
+		virtual ~Mouse();
+		
+		//JoystickDevice
+		byte read();
+		void write(byte value);
+	
+		//EventListener
+		void signalEvent(SDL_Event &event);
+
+	private:
+		static const int JOY_BUTTONA = 0x10;
+		static const int JOY_BUTTONB = 0x20;
+		static const int FAZE_XHIGH = 0;
+		static const int FAZE_XLOW  = 1;
+		static const int FAZE_YHIGH = 2;
+		static const int FAZE_YLOW  = 3;
+		static const int STROBE = 0x04;	//TODO check this, probably wrong
+
+		byte status;
+		int faze;
+		int xrel, yrel;
+		int curxrel, curyrel;
+};
+#endif
