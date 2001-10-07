2731d8212fd49419df2c665d989f49d632a18918
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-10-07 22:28:20 +0000

    wouter: extended Mixer to support (pseudo)stereo
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@552 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/Mixer.cc
+++ src/Mixer.cc
@@ -4,9 +4,10 @@
 #include "Mixer.hh"
 
 SDL_AudioSpec Mixer::audioSpec;
-int Mixer::nbDevices;
-std::vector<SoundDevice*> Mixer::devices;
-std::vector<short*> Mixer::buffers;
+int Mixer::nbAllDevices;
+int Mixer::nbDevices[NB_MODES];
+std::vector<SoundDevice*> Mixer::devices[NB_MODES];
+std::vector<short*> Mixer::buffers[NB_MODES];
 
 
 Mixer::Mixer()
@@ -14,16 +15,17 @@ Mixer::Mixer()
 	PRT_DEBUG("Creating a Mixer object");
 	
 	SDL_AudioSpec desired;
-	// TODO make these values configurable
-	desired.freq     = 22050;
-	desired.format   = AUDIO_S16LSB;
-	desired.channels = 1;
-	desired.samples  = 8192;
-	desired.callback = audioCallback;
-	desired.userdata = NULL;
+	desired.freq     = 22050;	// TODO make configurable
+	desired.samples  = 8192;	// TODO make configurable
+	desired.channels = 2;
+	desired.format   = AUDIO_S16LSB;	// check MSB <-> LSB
+	desired.callback = audioCallback;	// must be a static method
+	desired.userdata = NULL;		// not used
 	if (SDL_OpenAudio(&desired, &audioSpec) < 0 )
 		PRT_ERROR("Couldn't open audio : " << SDL_GetError());
-	nbDevices = 0;
+	for (int i=0; i<NB_MODES; i++) 
+		nbDevices[i] = 0;
+	nbAllDevices = 0;
 }
 
 Mixer::~Mixer()
@@ -41,14 +43,17 @@ Mixer* Mixer::instance(void)
 Mixer *Mixer::oneInstance = NULL;
 
 
-void Mixer::registerSound(SoundDevice *device)
+void Mixer::registerSound(SoundDevice *device, ChannelMode mode=MONO)
 {
 	PRT_DEBUG("Registering sound device");
 	SDL_LockAudio();
-	short *buf = new short[audioSpec.size/2];
-	buffers.push_back(buf);
-	devices.push_back(device);
-	if (++nbDevices == 1) SDL_PauseAudio(0);	// unpause when first device registers
+	int bufSize = audioSpec.size / sizeof(short);
+	if (mode!=STEREO) bufSize /= 2;
+	short *buf = new short[bufSize];
+	buffers[mode].push_back(buf);
+	devices[mode].push_back(device);
+	nbDevices[mode]++;
+	if (++nbAllDevices == 1) SDL_PauseAudio(0);	// unpause when first device registers
 	SDL_UnlockAudio();
 }
 
@@ -57,18 +62,34 @@ void Mixer::registerSound(SoundDevice *device)
 
 void Mixer::audioCallback(void *userdata, Uint8 *strm, int len)
 {
-	int length = len/2;
+	PRT_DEBUG("Audio callback");
+	int length = (len / sizeof(short)) / 2;	// STEREO -> MONO  => /2
 	short *stream = (short*)strm;
-	for (int i=0; i<nbDevices; i++) {
-		devices[i]->updateBuffer(buffers[i], length);
+	for (int mode=0; mode<NB_MODES; mode++) {
+		for (int i=0; i<nbDevices[mode]; i++) {
+			devices[mode][i]->updateBuffer(buffers[mode][i], length);
+		}
 	}
 	for (int j=0; j<length; j++) {
-		int temp = 0;
-		for (int i=0; i<nbDevices; i++)
-			temp += buffers[i][j];
-		if      (temp > 32767)  temp =  32767;
-		else if (temp < -32768) temp = -32768;
-		(*stream++) = temp;
+		int both = 0;
+		for (int i=0; i<nbDevices[MONO]; i++)
+			both  += buffers[MONO][i][j];
+		int left = both;
+		for (int i=0; i<nbDevices[MONO_LEFT]; i++)
+			left  += buffers[MONO_LEFT][i][j];
+		int right = both;
+		for (int i=0; i<nbDevices[MONO_RIGHT]; i++)
+			right += buffers[MONO_RIGHT][i][j];
+		for (int i=0; i<nbDevices[STEREO]; i++) {
+			left  += buffers[STEREO][i][2*j];
+			right += buffers[STEREO][i][2*j+1];
+		}
+		if      (left  > 32767)  left  =  32767;
+		else if (left  < -32768) left  = -32768;
+		(*stream++) = (short)left;
+		if      (right > 32767)  right =  32767;
+		else if (right < -32768) right = -32768;
+		(*stream++) = (short)right;
 	}
 }
 
--- src/Mixer.hh
+++ src/Mixer.hh
@@ -11,10 +11,13 @@
 class Mixer
 {
 	public:
+		enum ChannelMode { MONO, MONO_LEFT, MONO_RIGHT, STEREO };
+		static const int NB_MODES = STEREO+1;
+
 		~Mixer();
 		static Mixer *instance();
 		
-		void registerSound(SoundDevice *device);
+		void registerSound(SoundDevice *device, ChannelMode mode=MONO);
 		//void unregisterSound(SoundDevice *device);
 	private:
 		Mixer();
@@ -23,9 +26,10 @@ class Mixer
 		static void audioCallback(void *userdata, Uint8 *stream, int len);
 
 		static SDL_AudioSpec audioSpec;
-		static int nbDevices;
-		static std::vector<SoundDevice*> devices;
-		static std::vector<short*> buffers;
+		static int nbAllDevices;
+		static int nbDevices[NB_MODES];
+		static std::vector<SoundDevice*> devices[NB_MODES];
+		static std::vector<short*> buffers[NB_MODES];
 };
 
 #endif //__MIXER_HH__
