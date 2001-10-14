f83279eeb56b526421f5e45e482348c1c3505ada
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-10-14 21:51:27 +0000

    wouter: work in progress         Started implementation of Intel 8254 timer chip 	this chip is used in Sunrise RS232, MSX RS232, MSX MIDI, ...
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@573 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/I8254.cc
@@ -0,0 +1,322 @@
+// $Id: 
+
+#include <assert.h>
+#include "I8254.hh"
+
+
+I8254::I8254(int freq[NR_COUNTERS], I8254Interface *interf)
+{
+	interface = interf;
+	for (int i=0; i<NR_COUNTERS; i++) {
+		counter[i] = new Counter(freq[i], this, i);
+	}
+}
+
+I8254::~I8254()
+{
+}
+
+void I8254::reset()
+{
+	for (int i=0; i<NR_COUNTERS; i++) {
+		counter[i]->reset();
+	}
+}
+
+byte I8254::readIO(byte port, const Emutime &time)
+{
+	assert(port<NR_COUNTERS+1);
+	if (port<NR_COUNTERS) {
+		// read from counters
+		return counter[port]->readIO(time);
+	} else {
+		// read from control word
+		// illegal
+		return 255;	//TODO check value
+	}
+}
+
+void I8254::writeIO(byte port, byte value, const Emutime &time)
+{
+	assert(port<NR_COUNTERS+1);
+	if (port<NR_COUNTERS) {
+		// write to one of the counters
+		counter[port]->writeIO(value, time);
+	} else {
+		// write to control register
+		if ((value&READ_BACK)!=READ_BACK) {
+			// set control word of a counter
+			counter[value>>6]->writeControlWord(value&0x3f, time);
+		} else {
+			// Read-Back-Command
+			if (value&RB_CNTR0) readBackHelper(value, 0, time);
+			if (value&RB_CNTR1) readBackHelper(value, 1, time);
+			if (value&RB_CNTR2) readBackHelper(value, 2, time);
+		}
+	}
+}
+void I8254::readBackHelper(byte value, byte cntr, const Emutime &time) {
+	if (!(value&RB_STATUS)) counter[cntr]->latchStatus (time);
+	if (!(value&RB_COUNT))  counter[cntr]->latchCounter(time);
+}
+
+void I8254::setGateStatus(byte cntr, bool status, const Emutime &time)
+{
+	assert(timer<NR_COUNTERS);
+	counter[cntr]->setGateStatus(status, time);
+}
+
+bool I8254::getOutput(byte cntr, const Emutime &time)
+{
+	assert(timer<NR_COUNTERS);
+	return counter[cntr]->getOutput(time);
+}
+
+void I8254::outputCallback(int id, bool status)
+{
+	     if (id==0) interface->output0(status);
+	else if (id==1) interface->output1(status);
+	else if (id==2) interface->output2(status);
+	assert(false);
+}
+
+
+/////
+
+
+I8254::Counter::Counter(int freq, I8254 *prnt, int _id) : currentTime(freq)
+{
+	parent = prnt;
+	id = _id;
+	reset();
+}
+
+void I8254::Counter::reset()
+{
+	ltchCtrl = ltchCntr = false;
+	readOrder = writeOrder = LOW;
+	control = 0x30;	// Write BOTH / mode 0 / binary mode
+}
+
+byte I8254::Counter::readIO(const Emutime &time)
+{
+	if (ltchCtrl) {
+		ltchCtrl = false;
+		return latchedControl;
+	}
+	advance(time);
+	word readData = ltchCntr ? latchedCounter : counter;
+	switch (control & WRT_FRMT) {
+	case WF_LATCH:
+		assert(false);
+	case WF_LOW:
+		ltchCntr = false;
+		return readData&0x00ff;
+	case WF_HIGH:
+		ltchCntr = false;
+		return readData>>8;
+	case WF_BOTH:
+		if (readOrder==LOW) {
+			readOrder = HIGH;
+			return readData&0x00ff;
+		} else {
+			readOrder = LOW;
+			ltchCntr = false;
+			return readData>>8;
+		}
+	default:
+		assert(false);
+		return 0;	// avoid warning
+	}
+}
+
+void I8254::Counter::writeIO(byte value, const Emutime &time)
+{
+	advance(time);
+	switch (control & WRT_FRMT) {
+	case WF_LATCH:
+		assert(false);
+	case WF_LOW:
+		writeLoad((counterLoad&0xff00)|value);
+		break;
+	case WF_HIGH:
+		writeLoad((counterLoad&0x00ff)|(value<<8));
+		break;
+	case WF_BOTH:
+		if (writeOrder==LOW) {
+			writeOrder = HIGH;
+			writeLatch = value;
+			if ((control & CNTR_MODE) == CNTR_M0)
+				counting = false;
+		} else {
+			writeOrder = LOW;
+			counting = true;
+			writeLoad((value<<8)|writeLatch);
+		}
+		break;
+	default:
+		assert(false);
+	}
+}
+void I8254::Counter::writeLoad(word value) 
+{
+	counterLoad = value;
+	active = true;
+	byte mode = control & CNTR_MODE
+	if ((mode==CNTR_M0)||(mode==CNTR_M4))
+		counter = counterLoad;
+	if (mode==CNTR_M0)
+		changeOutput(false);
+	//TODO mode2 & inactive -> load
+}
+
+bool I8254::Counter::getOutput(const Emutime &time)
+{
+	advance(time);
+	return output;
+}
+
+void I8254::Counter::writeControlWord(byte value, const Emutime &time)
+{
+	advance(time);
+	if ((value&WRT_FRMT)==0) {
+		latchCounter(time);
+		return;
+	}
+	control = value;
+	writeOrder = LOW;
+	//TODO
+	counting = true;
+	active = false;
+	switch (control & CNTR_MODE) {
+	case CNTR_M0:
+		changeOutput(false);
+		break;
+	case CNTR_M1:
+		changeOutput(true);
+		break;
+	case CNTR_M2: case CNTR_M2_:
+		changeOutput(true);
+		break;
+	case CNTR_M3: case CNTR_M3_:
+		break;
+	case CNTR_M4:
+		break;
+	case CNTR_M5:
+		break;
+	default:
+		assert(false);
+	}
+}
+
+void I8254::Counter::latchStatus(const Emutime &time)
+{
+	advance(time);
+	if (!ltchCtrl) {
+		ltchCtrl = true;
+		byte out = getOutput(time) ? 0x80 : 0;
+		latchedControl = out|control; // TODO bit 6 null-count
+	}
+}
+
+void I8254::Counter::latchCounter(const Emutime &time)
+{
+	advance(time);
+	if (!ltchCntr) {
+		ltchCntr = true;
+		readOrder = LOW;
+		latchedCounter = counter;
+	}
+}
+
+void I8254::Counter::setGateStatus(bool newStatus, const Emutime &time)
+{
+	advance(time);
+	gate = status;
+	//TODO
+	if (gate != newStatus) {
+		gate = newStatus;
+		switch (control & CNTR_MODE) {
+		case CNTR_M0:
+			// nothing needs to be done
+			break;
+		case CNTR_M1:
+			if (gate && active)
+				counter = counterLoad;
+				changeOutput(false);
+			break;
+		case CNTR_M2: case CNTR_M2_:
+			if (gate) {
+				counter = counterLoad;
+			} else {
+				changeOutput(true);
+			}
+			break;
+		case CNTR_M3: case CNTR_M3_:
+			break;
+		case CNTR_M4:
+			break;
+		case CNTR_M5:
+			break;
+		default:
+			assert(false);
+		}
+	}
+}
+
+void I8254::Counter::advance(const Emutime &time)
+{
+	uint64 ticks = currentTime.getTicksTill(time);
+	currentTime = time;
+	//TODO
+	switch (control & CNTR_MODE) {
+	case CNTR_M0:
+		if (gate && counting) {
+			counter -= ticks;
+			if (counter<0) {
+				counter&=0xffff;
+				if (active) {
+					changeOutput(false);
+					active = false;
+				}
+			}
+		}
+		break;
+	case CNTR_M1:
+		counter -= ticks;
+		if (counter<0)
+			counter&=0xffff
+			changeOutput(true);
+		break;
+	case CNTR_M2: case CNTR_M2_:
+		if (gate) {
+			counter -= ticks;
+			if (active) {
+				if (counter == 1)
+					changeOutput(false);
+				if (counter <= 0) {
+					counter = counterLoad;	//TODO check reload when inactive
+					changeOutput(true);
+				}
+			}
+		}
+		break;
+	case CNTR_M3: case CNTR_M3_:
+		break;
+	case CNTR_M4:
+		break;
+	case CNTR_M5:
+		break;
+	default:
+		assert(false);
+	}
+}
+
+void I8254::Counter::changeOutput(bool newOutput)
+{
+	if (newOutput != output) {
+		output = newOutput;
+		parent->outputCallback(id, output);
+	}
+}
+	
---
+++ src/I8254.hh
@@ -0,0 +1,97 @@
+// $Id: 
+
+// This class implements the Intel 8254 chip (and 8253)
+//
+// * Only the 8254 is emulated, no surrounding hardware. 
+//   Use the class I8254Interface to do that.
+// * For techncal details on 8254 see
+//    http://w3.qahwah.net/joost/openMSX/8254.pdf
+
+#ifndef __I8254_HH__
+#define __I8254_HH__
+
+#include "openmsx.hh"
+#include "emutime.hh"
+
+
+class I8254Interface
+{
+	public:
+		virtual void output0(bool status) {}
+		virtual void output1(bool status) {}
+		virtual void output2(bool status) {}
+};
+
+class I8254
+{
+	class Counter {
+		public:
+			Counter(int freq, I8254 *parent, int id);
+			void reset();
+			byte readIO(const Emutime &time);
+			void writeIO(byte value, const Emutime &time);
+			void setGateStatus(bool status, const Emutime &time);
+			bool getOutput(const Emutime &time);
+			void writeControlWord(byte value, const Emutime &time);
+			void latchStatus(const Emutime &time);
+			void latchCounter(const Emutime &time);
+		private:
+			enum ByteOrder {LOW, HIGH};
+			static const byte WRT_FRMT = 0x30;
+			static const byte WF_LATCH = 0x00;
+			static const byte WF_LOW   = 0x10;
+			static const byte WF_HIGH  = 0x20;
+			static const byte WF_BOTH  = 0x30;
+			static const byte CNTR_MODE = 0x0e;
+			static const byte CNTR_M0   = 0x00;
+			static const byte CNTR_M1   = 0x02;
+			static const byte CNTR_M2   = 0x04;
+			static const byte CNTR_M3   = 0x06;
+			static const byte CNTR_M4   = 0x08;
+			static const byte CNTR_M5   = 0x0a;
+			static const byte CNTR_M2_  = 0x0c;
+			static const byte CNTR_M3_  = 0x0e;
+	
+			void advance(const Emutime &time);
+	
+			Emutime currentTime;
+			I8254 *parent;
+			int id;
+			int counter, latchedCounter;
+			byte control, latchedControl;
+			bool ltchCtrl, ltchCntr;
+			ByteOrder readOrder, writeOrder;
+			byte writeLatch;
+			bool output, gate;
+			bool active;
+	};
+	
+	public:
+		static const int NR_COUNTERS = 3;
+	
+		I8254(int freq[NR_COUNTERS], I8254Interface *interf); 
+		~I8254(); 
+		
+		void reset();
+
+		byte readIO(byte port, const Emutime &time);
+		void writeIO(byte port, byte value, const Emutime &time);
+		void setGateStatus(byte counter, bool status, const Emutime &time);
+		bool getOutput(byte counter, const Emutime &time);
+
+		void outputCallback(int id, bool status);
+
+	private:
+		static const byte READ_BACK = 0xc0;
+		static const byte RB_CNTR0  = 0x02;
+		static const byte RB_CNTR1  = 0x04;
+		static const byte RB_CNTR2  = 0x08;
+		static const byte RB_STATUS = 0x10;
+		static const byte RB_COUNT  = 0x20;
+		
+		void readBackHelper(byte value, byte cntr, const Emutime &time);
+	
+		I8254Interface *interface;
+		Counter* counter[NR_COUNTERS];
+};
+#endif
