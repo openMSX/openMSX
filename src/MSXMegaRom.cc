834e3a86403314b17ba4eea173fa4eaad5152378
Common openMSX developers <openmsx-devel@lists.sourceforge.net>
2001-10-15 13:43:48 +0000

    David: * Got some of the megarom parts corrected (not all)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@575 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/MSXMegaRom.cc
+++ src/MSXMegaRom.cc
@@ -170,7 +170,7 @@ void MSXMegaRom::writeMem(word address, byte value, Emutime &time)
     case 0: //   --==>> Generic 8kB cartridges <<==--
       if ((address < 0x4000) || (address >0xBFFF)) return;
       if ((address & 0xe000) == 0x8000 ) { // 0x8000..0x9FFF is used as SCC switch
-	enabledSCC=(value==0x3F)?true:false;
+	enabledSCC=((value&0x3F)==0x3F)?true:false;
       }
       /* change internal mapper*/
       value&=mapperMask;
@@ -187,10 +187,21 @@ void MSXMegaRom::writeMem(word address, byte value, Emutime &time)
       internalMapper[regio]=value;
       break;
     case 2: //   --==**>> KONAMI5 8kB cartridges <<**==--
-      if ((address < 0x5000) || (address >0xB000) || ((address & 0x1FFF)!=0x1000)) return;
+      /* this type is used by Konami cartridges that do have an SCC and some others
+      * example of catrridges: Nemesis 2, Nemesis 3, King's Valley 2, Space Manbow
+      * Solid Snake, Quarth, Ashguine 1, Animal, Arkanoid 2, ...
+      */
+      
+      /* The address to change banks:
+       bank 1: 0x5000 - 0x57FF (0x5000 used)
+       bank 2: 0x7000 - 0x77FF (0x7000 used)
+       bank 3: 0x9000 - 0x97FF (0x9000 used)
+       bank 4: 0xB000 - 0xB7FF (0xB000 used)
+      */
+      if ((address < 0x5000) || (address >0xB000) || ((address & 0x1800)!=0x1000)) return;
       // Enable/disable SCC
-      if (address == 0x9000 ) { // 0x9000 is used as SCC switch
-	enabledSCC=(value==0x3F)?true:false;
+      if ((address & 0xF800) == 0x9000 )  { // 0x9000-0x97ff is used as SCC switch
+	enabledSCC=((value&0x3F)==0x3F)?true:false;
       }
       /* change internal mapper*/
       regio=(address-0x5000)>>13;
@@ -199,7 +210,13 @@ void MSXMegaRom::writeMem(word address, byte value, Emutime &time)
       internalMapper[regio]=value;
       break;
     case 3: //   --==**>> KONAMI4 8kB cartridges <<**==--
-      /* page at 4000 is fixed */
+      /* this type is used by Konami cartridges that do not have an SCC and some others
+      * example of catrridges: Nemesis, Penguin Adventure, Usas, Metal Gear, Shalom, 
+      * The Maze of Galious, Aleste 1, 1942,Heaven, Mystery World, ...
+      */
+      
+      /* page at 4000 is fixed, other banks are switched
+         by writting at 0x6000,0x8000 and 0xA000 */
       if ((address < 0x6000) || (address >0xa000) || (address & 0x1FFF)) return;
       regio=(address>>13)-2;
       value&=mapperMask;
@@ -207,7 +224,16 @@ void MSXMegaRom::writeMem(word address, byte value, Emutime &time)
       internalMapper[regio]=value;
       break;
     case 4: //   --==**>> ASCII 8kB cartridges <<**==--
-      // TODO: As I understand corectly from fmsx this mapper changes if if not currently selected ??
+      /* this type is used in many japanese-only cartridges.
+      * example of cartridges: Valis(Fantasm Soldier), Dragon Slayer, Outrun, Ashguine 2, ...
+      */
+      
+      /* The address to change banks:
+       bank 1: 0x6000 - 0x67FF (0x5000 used)
+       bank 2: 0x6800 - 0x6FFF (0x7000 used)
+       bank 3: 0x7000 - 0x77FF (0x9000 used)
+       bank 4: 0x7800 - 0x7FFF (0xB000 used)
+      */
       if ((address < 0x6000) || (address >0x7fff)) return;
       regio=(address>>11)&3;
       value&=mapperMask;
@@ -215,12 +241,23 @@ void MSXMegaRom::writeMem(word address, byte value, Emutime &time)
       internalMapper[regio]=value;
       break;
     case 5: //   --==**>> ASCII 16kB cartridges <<**==--
-      // TODO: As I understand corectly from fmsx this mapper changes if if not currently selected ??
-      if ((address < 0x6000) || (address >0x7fff)) return;
+      /* this type is used in a few cartridges.
+      * example of cartridges: Xevious, Fantasy Zone 2, Return of Ishitar, Androgynus, ...
+      *
+      * Gallforce is a special case after a reset the second 16kb has to start with 
+      * the first 16kb after a reset
+      *
+      */
+      
+      /* The address to change banks:
+       first  16kb: 0x6000 - 0x67FF (0x6000 used)
+       second 16kb: 0x7000 - 0x77FF (0x7000 and 0x77FF used)
+      */
+      if ((address < 0x6000) || (address >0x77ff) || (address&0x800)) return;
       regio=(address>>11)&2;
       value&=(value<<1)&mapperMask;
       internalMemoryBank[regio]=memoryBank+(value<<13);
-      internalMemoryBank[regio+1]=internalMemoryBank[regio]+0x2000; //TODO check if fMSX is right?
+      internalMemoryBank[regio+1]=internalMemoryBank[regio]+0x2000;
       internalMapper[regio]=value;
       break;
     case 6: //   --==**>> GameMaster2+SRAM cartridge <<**==--
