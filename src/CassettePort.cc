62fc6bad7888157a6dfb453c589dad0c85678bd8
Wouter Vermaelen <m9710797@users.sourceforge.net>
2001-12-19 09:24:40 +0000

    forgot to add these files
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@768 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/CassettePort.cc
@@ -0,0 +1,133 @@
+// $Id$
+
+#include "CassettePort.hh"
+#include "DummyCassetteDevice.hh"
+#include "MSXCPU.hh"
+
+
+// CassettePortFactory //
+
+CassettePortInterface *CassettePortFactory::instance()
+{
+	if (oneInstance == NULL) {
+		try {
+			MSXConfig::instance()->getConfigById("CassettePort");
+			// there is a CassettePort in config
+			oneInstance = new CassettePort();
+		} catch (MSXConfig::Exception e) {
+			// there is no CassettePort in config
+			oneInstance = new DummyCassettePort();
+		}
+		oneInstance = new DummyCassettePort();
+	}
+	return oneInstance;
+}
+CassettePortInterface *CassettePortFactory::oneInstance = NULL;
+
+
+
+// CassettePortInterface //
+
+void CassettePortInterface::plug(CassetteDevice *dev)
+{
+	if (device != dev) {
+		flushOutput(MSXCPU::instance()->getCurrentTime());
+		device = dev;
+	}
+}
+
+void CassettePortInterface::unplug()
+{
+	plug(DummyCassetteDevice::instance());
+}
+
+
+// DummyCassettePort //
+
+DummyCassettePort::DummyCassettePort()
+{
+	device = DummyCassetteDevice::instance();	//unplug();
+}
+void DummyCassettePort::setMotor(bool status, const EmuTime &time)
+{
+	// do nothing
+}
+void DummyCassettePort::cassetteOut(bool output, const EmuTime &time)
+{
+	// do nothing
+}
+bool DummyCassettePort::cassetteIn(const EmuTime &time)
+{
+	return true;	// TODO check on Turbo-R
+}
+void DummyCassettePort::flushOutput(const EmuTime &time)
+{
+	// do nothing
+}
+
+
+// CassettePort //
+
+CassettePort::CassettePort()
+{
+	device = DummyCassetteDevice::instance();	//unplug();
+	buffer = new short[BUFSIZE];
+}
+
+CassettePort::~CassettePort()
+{
+	flushOutput(MSXCPU::instance()->getCurrentTime());
+	delete[] buffer;
+}
+
+void CassettePort::setMotor(bool status, const EmuTime &time)
+{
+	device->setMotor(status, time);
+	//TODO make 'click' sound
+}
+
+void CassettePort::cassetteOut(bool output, const EmuTime &time)
+{
+	// this implements a VERY rough filter
+	//   on a transition the output is 0
+	//   everywhere else it is max or min
+	// this is probably good enough
+	flushOutput(time);
+	nextSample = 0;
+	lastOutput = output;
+}
+
+bool CassettePort::cassetteIn(const EmuTime &time)
+{
+	// All analog filtering is ignored for now
+	//   only important component is DC-removal
+	//   we just assume sample has no DC component
+	short sample = device->readSample(time);	// read 1 sample  
+	return (sample > 0); // comparator
+}
+
+void CassettePort::flushOutput(const EmuTime &time)
+{
+	int sampleRate = device->getWriteSampleRate();	// can be changed since prev flush
+	if (sampleRate == 0) {
+		// 99% of the time
+		PRT_DEBUG("Cas: must not generate wave");
+		prevTime = time;
+		return;
+	}
+	int samples = (int)(prevTime.getDuration(time) * sampleRate);
+	prevTime = time;
+	PRT_DEBUG("Cas: generate " << samples << " samples");
+	
+	// dumb implementation, good enough for now
+	device->writeWave(&nextSample, 1);
+	samples--;
+	nextSample = lastOutput ? 32767 : -32768;
+	int numSamples = (samples > BUFSIZE) ? BUFSIZE : samples;
+	for (int i=1; i<numSamples; i++) buffer[i] = nextSample;
+	while (samples) {
+		device->writeWave(buffer, numSamples);
+		samples -= numSamples;
+		numSamples = (samples > BUFSIZE) ? BUFSIZE : samples;
+	}
+}
