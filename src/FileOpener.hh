4b68be3684f25d92f0b03bca9a6d15edf7758158
David Heremans <turbor@users.sourceforge.net>
2002-01-10 11:13:34 +0000

    Forgot to commit :-)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@873 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/FileOpener.hh
@@ -0,0 +1,65 @@
+// $Id$
+//
+// FileOpener mixin
+
+#ifndef __FILEOPENER_HH__
+#define __FILEOPENER_HH__
+
+#include "config.h"
+#include "openmsx.hh"
+#include "msxconfig.hh"
+#include <fstream>
+
+#ifdef HAVE_FSTREAM_TEMPL
+#define IFILETYPE std::ifstream<byte>
+#else
+#define IFILETYPE std::ifstream
+#endif
+
+#ifdef HAVE_FSTREAM_TEMPL
+#define OFILETYPE std::ofstream<byte>
+#else
+#define OFILETYPE std::ofstream
+#endif
+
+#ifdef HAVE_FSTREAM_TEMPL
+#define IOFILETYPE std::fstream<byte>
+#else
+#define IOFILETYPE std::fstream
+#endif
+
+class FileOpener
+{
+	public:
+		/**
+		 * try to find a readable file in the current rompath with matching filename
+		 * returns the filename with path as string
+		 */
+		static std::string  findFileName(std::string filename);
+		/**
+		 * Open a file for reading only.
+		 */
+		static IFILETYPE* openFileRO(std::string filename);
+		/**
+		 * Open a file for reading and writing.
+		 * if not writeable then fail
+		 */
+		static IOFILETYPE* openFileMustRW(std::string filename);
+		/**
+		 * Open a file for reading and writing.
+		 * if not writeable then open readonly
+		 */
+		static IOFILETYPE* openFilePreferRW(std::string filename);
+
+		/** Following are for creating/reusing files **/
+		/** if not writeable then fail **/
+		static IOFILETYPE* openFileAppend(std::string filename);
+		static IOFILETYPE* openFileTruncate(std::string filename);
+	private:
+		/**
+		 * Objects of this class can not be constructed 
+		 */
+		FileOpener() {}
+};
+
+#endif // __FILEOPENER_HH__
