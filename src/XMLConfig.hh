04525c5d8adc6b3a3acb193598e4d842a96074af
David Heremans <turbor@users.sourceforge.net>
2002-01-31 15:04:38 +0000

    Now Joost has made the stringstream interface to CMLconfig, I started on creating an easier to use commandline format. This will wrap commandline specified disks,roms and tapes in a correct xml config and configure them this way, instead of having to create an entire xml part for each seperate disk/cartridge/tape.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@915 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/XMLConfig.hh
+++ src/XMLConfig.hh
@@ -1,4 +1,189 @@
 // $Id$
+// $Id$
+
+#ifndef __MSXCONFIG_HH__
+#define __MSXCONFIG_HH__
+
+// for uint64
+#include "EmuTime.hh"
+
+// TODO XXX rework msxexception
+#include "MSXException.hh"
+
+#include <string>
+#include <list>
+#include <sstream>
+
+namespace MSXConfig
+{
+
+class Exception: public MSXException
+{
+public:
+	Exception(const std::string &descs):MSXException(descs) {}
+	Exception(const std::ostringstream &stream):MSXException(stream.str()) {}
+};
+
+class Config
+{
+public:
+
+	// a parameter is the same for all backends:
+	class Parameter
+	{
+	public:
+		Parameter(const std::string &name, const std::string &value, const std::string &clasz);
+		~Parameter();
+	private:
+		Parameter(); // block usage
+		Parameter(const Parameter &); // block usage
+		Parameter &operator=(const Parameter &); // block usage
+
+	public:
+		const bool getAsBool() const;
+		const int getAsInt() const ;
+		const uint64 getAsUint64() const;
+		// please let me know what types are needed exactly
+		
+		const std::string name;
+		const std::string value;
+		const std::string clasz;
+
+		static bool stringToBool(const std::string &str);
+		static int stringToInt(const std::string &str);
+		static uint64 stringToUint64(const std::string &str);
+	};
+
+	Config();
+	virtual ~Config();
+
+	virtual bool isDevice();
+	virtual const std::string &getType()=0;
+	virtual const std::string &getId()=0;
+	virtual const std::string &getDesc()=0;
+	virtual const std::string &getRem()=0;
+
+	virtual bool hasParameter(const std::string &name)=0;
+	virtual const std::string &getParameter(const std::string &name)=0;
+	virtual const bool getParameterAsBool(const std::string &name)=0;
+	virtual const int getParameterAsInt(const std::string &name)=0;
+	virtual const uint64 getParameterAsUint64(const std::string &name)=0;
+
+	/**
+	 * This returns a freshly allocated list with freshly allocated
+	 * Parameter objects. The caller has to clean this all up with
+	 * getParametersWithClassClean
+	 */
+	virtual std::list<Parameter*>* getParametersWithClass(const std::string &clasz)=0;
+	/**
+	 * cleanup function for getParametersWithClass
+	 */
+	static void getParametersWithClassClean(std::list<Parameter*>* list);
+
+	virtual void dump();
+
+};
+
+class Device: virtual public Config
+{
+public:
+
+	// a slotted is the same for all backends:
+	class Slotted
+	{
+	public:
+		Slotted(int PS, int SS=-1, int Page=-1);
+		~Slotted();
+	private:
+		Slotted(); // block usage
+		Slotted(const Slotted &); // block usage
+		Slotted &operator=(const Slotted &); // block usage
+		int ps;
+		int ss;
+		int page;
+	public:
+		bool hasSS();
+		bool hasPage();
+		int getPS();
+		int getSS();
+		int getPage();
+
+		void dump();
+	};
+
+	Device();
+	virtual ~Device();
+
+	virtual bool isDevice();
+	bool  isSlotted();
+	int   getPage(); // of first slotted [backward compat]
+	int   getPS(); // of first slotted [backward compat]
+	int   getSS(); // of first slotted [backward compat]
+
+	std::list <Slotted*> slotted;
+
+	virtual void dump();
+	
+private:
+	Device(const Device &foo); // block usage
+	Device &operator=(const Device &foo); // block usage
+};
+
+class Backend
+{
+public:
+	/**
+	 * load a config file's content, and add it
+	 * to the config data [can be called multiple
+	 * times]
+	 */
+	virtual void loadFile(const std::string &filename)=0;
+	virtual void loadStream(const std::ostringstream &stream)=0;
+
+	/**
+	 * save current config to file
+	 */
+	virtual void saveFile()=0;
+	virtual void saveFile(const std::string &filename)=0;
+
+	/**
+	 * get a config or device by id
+	 */
+	virtual Config* getConfigById(const std::string &id)=0;
+	virtual Device* getDeviceById(const std::string &id)=0;
+
+	/**
+	 * get a device
+	 */
+	virtual void initDeviceIterator()=0;
+	virtual Device* getNextDevice()=0;
+
+	/**
+	 * backend factory
+	 * create a backend, by name
+	 * currently only one backend: "xml"
+	 */
+	static Backend* createBackend(const std::string &name);
+
+	/**
+	 * returns the one backend, for backwards compat
+	 */
+	static Backend* instance();
+
+	virtual ~Backend();
+
+protected:
+	Backend();
+	// only I create instances of me
+
+private:
+	static Backend* _instance;
+
+};
+
+}; // end namespace MSXConfig
+
+#endif
 
 #ifndef __XMLCONFIG_HH__
 #define __XMLCONFIG_HH__
--- src/openmsx.cc
+++ src/openmsx.cc
@@ -9,6 +9,7 @@
 #include "config.h"
 #include "MSXConfig.hh"
 #include <string>
+#include <sstream>
 #include <SDL/SDL.h>
 #include "Thread.hh"
 #include "MSXMotherBoard.hh"
@@ -39,6 +40,33 @@ void initializeSDL()
 	SDL_WM_SetIcon(iconSurf, NULL);
 }
 
+int checkFileType(char* parameter,char &diskLetter,byte &cartridge){
+  int fileType=0;
+  printf("parameter: %s\n",parameter);
+  if ( 0 == strcmp(parameter,"-config")) { fileType=1; };
+  if ( 0 == strcmp(parameter,"-disk"))   { fileType=2; };
+  if ( 0 == strcmp(parameter,"-diska"))  { fileType=2; diskLetter='A'; };
+  if ( 0 == strcmp(parameter,"-diskb"))  { fileType=2; diskLetter='B'; };
+  if ( 0 == strcmp(parameter,"-tape"))   { fileType=3; };
+  if ( 0 == strcmp(parameter,"-cart"))   { fileType=4;  };
+  if ( 0 == strcmp(parameter,"-carta"))  { fileType=4; cartridge=0; };
+  if ( 0 == strcmp(parameter,"-cartb"))  { fileType=4; cartridge=1; };
+  return fileType;
+}
+
+int checkFileExt(char* filename){
+  int fileType=0;
+  char* Extension=filename + strlen(filename) -4;
+  printf("Extension : %s\n",Extension);
+  // check the extension caseinsensitive
+  if (0 == strcmp(Extension,".xml")) fileType=1;
+  if (0 == strcmp(Extension,".dsk")) fileType=2;
+  if (0 == strcmp(Extension,".di1")) fileType=2;
+  if (0 == strcmp(Extension,".di2")) fileType=2;
+  if (0 == strcmp(Extension,".cas")) fileType=3;
+  if (0 == strcmp(Extension,".rom")) fileType=4;
+  return fileType;
+}
 
 int main (int argc, char **argv)
 {
@@ -47,42 +75,105 @@ int main (int argc, char **argv)
 	MSXConfig::Backend* config = MSXConfig::Backend::createBackend("xml");
 	
 	try {
-		if (argc<2) {
-			PRT_INFO ("Using msxconfig.xml as default configuration file");
-			config->loadFile(std::string("msxconfig.xml"));
-		} else {
-			for (int i=1; i<argc; i++) {
-				config->loadFile(std::string(argv[i]));
-			}
+	    int i;
+	    int fileType=0;
+	    int nrXMLfiles=0;
+	    char driveLetter='A';
+	    byte cartridgeNr=0;
+
+	    for (i=1; i<argc; i++) {
+	      if ( *(argv[i]) == '-' ){
+		printf("need to parse general option\n");
+		fileType=checkFileType(argv[i],driveLetter,cartridgeNr);
+		//TODO if fileType has changed then the next MUST be a filename
+	      } else {
+		// no '-' as first character so it is a filename :-)
+		printf("need to parse filename option: %s\n",argv[i]);
+		if (fileType == 0) 
+		  fileType=checkFileExt(argv[i]);
+		switch (fileType){
+		  case 1: //xml files
+		    config->loadFile(std::string(argv[i]));
+		    nrXMLfiles++;
+		    break;
+		  case 2: //dsk,di2,di1 files
+		    break;
+		  case 3: //cas files
+		    {
+		      std::ostringstream s;
+		      s << "<msxconfig>";
+		      s << " <config id=\"tapepatch\">";
+		      s << "  <type>tape</type>";
+		      s << "  <parameter name=\"filename\">";
+		      s << std::string(argv[i]) << "</parameter>";
+		      s << " </config>";
+		      s << "</msxconfig>";
+		      PRT_INFO(s.str());
+		      config->loadStream(s);
+		    }
+		    break;
+		  case 4: //rom files
+		    {
+		      std::ostringstream s;
+		      s << "<msxconfig>";
+		      s << " <device id=\"GameCartridge"<< (int)cartridgeNr <<"\">";
+		      s << "  <type>GameCartridge</type><slotted><ps>";
+		      s << (int)(1+cartridgeNr) <<"</ps><ss>0</ss><page>1</page></slotted>";
+		      s << "<slotted><ps>";
+		      s << (int)(1+cartridgeNr) <<"</ps><ss>0</ss><page>2</page></slotted>";
+		      s << "<parameter name=\"filename\">";
+		      s << std::string(argv[i]) << "</parameter>";
+		      s << "<parameter name=\"filesize\">0</parameter>";
+		      s << "<parameter name=\"volume\">13000</parameter>";
+		      s << "<parameter name=\"automappertype\">true</parameter>";
+		      s << "<parameter name=\"mappertype\">2</parameter>";
+		      s << " </device>";
+		      s << "</msxconfig>";
+		      PRT_INFO(s.str());
+		      config->loadStream(s);
+		      cartridgeNr++;
+		    }
+		    break;
+		  default:
+		    PRT_INFO("Couldn't make sense of argument\n");
 		}
+		fileType=0;
+	      };
+	    }
+	  
+	  if (nrXMLfiles==0){
+	    PRT_INFO ("Using msxconfig.xml as default configuration file");
+	    config->loadFile(std::string("msxconfig.xml"));
+	  } 
 
-		initializeSDL();
-	
-		EmuTime zero;
-		config->initDeviceIterator();
-		MSXConfig::Device* d;
-		while ((d=config->getNextDevice()) != 0) {
-			//std::cout << "<device>" << std::endl;
-			//d->dump();
-			//std::cout << "</device>" << std::endl << std::endl;
-			MSXDevice *device = deviceFactory::create(d, zero);
-			MSXMotherBoard::instance()->addDevice(device);
-			PRT_DEBUG ("Instantiated: " << d->getType());
-		}
+	  initializeSDL();
 
-		// Start a new thread for event handling
-		Thread thread(EventDistributor::instance());
-		thread.start();
+	  EmuTime zero;
+	  config->initDeviceIterator();
+	  MSXConfig::Device* d;
+	  while ((d=config->getNextDevice()) != 0) {
+	    //std::cout << "<device>" << std::endl;
+	    //d->dump();
+	    //std::cout << "</device>" << std::endl << std::endl;
+	    MSXDevice *device = deviceFactory::create(d, zero);
+	    MSXMotherBoard::instance()->addDevice(device);
+	    PRT_DEBUG ("Instantiated: " << d->getType());
+	  }
 
-		PRT_DEBUG ("starting MSX");
-		MSXMotherBoard::instance()->StartMSX();
-		
-		// When we return we clean everything up
-		thread.stop();
-		MSXMotherBoard::instance()->DestroyMSX();
+	  // Start a new thread for event handling
+	  Thread thread(EventDistributor::instance());
+	  thread.start();
+
+	  PRT_DEBUG ("starting MSX");
+	  MSXMotherBoard::instance()->StartMSX();
+
+	  // When we return we clean everything up
+	  thread.stop();
+	  MSXMotherBoard::instance()->DestroyMSX();
 	} 
 	catch (MSXException& e) {
 		PRT_ERROR("Uncatched exception: " << e.desc);
 	}
 	exit (0);
 }
+
