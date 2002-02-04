beaef938c6e5e7d9eb8dbe8c692584bab70c5a0f
Maarten ter Huurne <maarten@treewalker.org>
2002-02-04 00:11:36 +0000

    Transformed comments to Doxygen style.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@928 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/ConsoleSource/CON_console.hh
+++ src/ConsoleSource/CON_console.hh
@@ -9,29 +9,89 @@
 #define CON_CHAR_BORDER      4
 
 
-/* This is a struct for each consoles data */
+/** This is a struct for each consoles data.
+  * TODO: Transform this into a class.
+  */
 typedef struct console_information_td
 {
-	char **ConsoleLines;		/* List of all the past lines */
-	char **CommandLines;		/* List of all the past commands */
-	int TotalConsoleLines;		/* Total number of lines in the console */
-	int ConsoleScrollBack;		/* How much the users scrolled back in the console */
-	int TotalCommands;		/* Number of commands in the Back Commands */
-	int FontNumber;			/* This is the number of the font for the console */
-	int Line_Buffer;		/* The number of lines in the console */
-	int BackX, BackY;		/* Background images x and y coords */
-	SDL_Surface *ConsoleSurface;	/* Surface of the console */
-	SDL_Surface *OutputScreen;	/* This is the screen to draw the console to */
-	SDL_Surface *BackgroundImage;	/* Background image for the console */
-	SDL_Surface *InputBackground;	/* Dirty rectangle to draw over behind the users background */
-	int DispX, DispY;		/* The top left x and y coords of the console on the display screen */
-	unsigned char ConsoleAlpha;	/* The consoles alpha level */
-	int StringLocation;		/* Current character location in the current string */
-	int CommandScrollBack;		/* How much the users scrolled back in the command lines */
+	/** List of all the past lines.
+	  */
+	char **ConsoleLines;
+
+	/** List of all the past commands.
+	  * TODO: Call this "history" or "commandHistory".
+	  */
+	char **CommandLines;
+
+	/** Total number of lines in the console.
+	  */
+	int TotalConsoleLines;
+
+	/** How much the users scrolled back in the console.
+	  */
+	int ConsoleScrollBack;
+
+	/** Number of commands in the Back Commands.
+	  */
+	int TotalCommands;
+
+	/** This is the number of the font for the console.
+	  */
+	int FontNumber;
+
+	/** The number of lines in the console.
+	  */
+	int Line_Buffer;
+
+	/** Background images x coordinate.
+	  */
+	int BackX;
+
+	/** Background images y coordinate.
+	  */
+	int BackY;
+
+	/** Surface of the console.
+	  */
+	SDL_Surface *ConsoleSurface;
+
+	/** This is the screen to draw the console to.
+	  */
+	SDL_Surface *OutputScreen;
+
+	/** Background image for the console.
+	  */
+	SDL_Surface *BackgroundImage;
+
+	/** Dirty rectangle to draw over behind the users background.
+	  */
+	SDL_Surface *InputBackground;
+
+	/** The top-left x coordinate of the console on the display screen.
+	  */
+	int DispX;
+
+	/** The top-left y coordinate of the console on the display screen.
+	  */
+	int DispY;
+
+	/** The consoles alpha level.
+	  */
+	unsigned char ConsoleAlpha;
+
+	/** Current character location in the current string.
+	  */
+	int StringLocation;
+
+	/** How much the users scrolled back in the command lines.
+	  */
+	int CommandScrollBack;
+
 } ConsoleInformation;
 
 
-
+// TODO: Documentation of these functions is currently in CON_console.cc;
+//       it should be moved here.
 void	CON_Events(SDL_Event *event);
 void	CON_DrawConsole(ConsoleInformation *console);
 ConsoleInformation	*CON_Init(const char *FontName, SDL_Surface *DisplayScreen, int lines, SDL_Rect rect);
