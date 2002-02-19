87a4c77e95bf8ce3e3aac8416e3a121f310b1d4d
Joost Yervante Damad <joost@damad.be>
2002-02-19 21:11:12 +0000

    Joost: WIP
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@989 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/atoll.c
+++ src/atoll.c
@@ -1,150 +1,213 @@
-/*
-  This code is based on strtoul.c which has the following copyright.
-  It is used to convert a string into a signed long long.
+/* Copyright (C) 2000 MySQL AB & MySQL Finland AB & TCX DataKonsult AB
 
-  long long _strtoll_r (struct _reent *rptr, const char *s, 
-                        char **ptr, int base);
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   MA 02111-1307, USA */
+
+/*
+  strtol,strtoul,strtoll,strtoull
+  convert string to long, unsigned long, long long or unsigned long long.
+  strtoxx(char *src,char **ptr,int base)
+  converts the string pointed to by src to an long of appropriate long and
+  returnes it. It skips leading spaces and tabs (but not newlines, formfeeds,
+  backspaces), then it accepts an optional sign and a sequence of digits
+  in the specified radix.
+  If the value of ptr is not (char **)NULL, a pointer to the character
+  terminating the scan is returned in the location pointed to by ptr.
+  Trailing spaces will NOT be skipped.
+
+  If an error is detected, the result will be LONG_MIN, 0 or LONG_MAX,
+  (or LONGLONG..)  and errno will be set to
+	EDOM	if there are no digits
+	ERANGE	if the result would overflow.
+  the ptr will be set to src.
+  This file is based on the strtol from the the GNU C Library.
+  it can be compiled with the UNSIGNED and/or LONGLONG flag set
 */
 
-/*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
-/* #include <_ansi.h> */
-#include <limits.h>
-#include <ctype.h>
+/*  #include <global.h> */
+#include <limits.h> /* for LONG_LONG_MIN etc */
+#include <ctype.h> /* for isspace() etc, toupper() */
+#include <sys/types.h> /* uint */
 #include <errno.h>
-#include <stdlib.h>
-/* #include <reent.h> */
 
-#define LLONG_MAX 9223372036854775807LL
-#define LLONG_MIN      (-LLONG_MAX - 1LL)
-#define LONG_LONG_MIN LLONG_MIN
-#define LONG_LONG_MAX LLONG_MAX
-/*
- * Convert a string to a long long integer.
- *
- * Ignores `locale' stuff.  Assumes that the upper and lower case
- * alphabets and digits are each contiguous.
- */
-long long _strtoll_r(
-        const char *nptr,
-        char **endptr,
-        int base)
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+
+#define UTYPE_MAX (~(unsigned long long) 0)
+#define TYPE_MIN LONG_LONG_MIN
+#define TYPE_MAX LONG_LONG_MAX
+#define longtype long long
+#define ulongtype unsigned long long
+
+#ifndef HAVE_STRTOLL /* this conditional encloses the rest of the file */
+
+#ifdef UNSIGNED
+#	define function ulongtype strtoull
+#else
+#	define function longtype strtoll
+#endif
+
+
+/* Convert NPTR to an `unsigned long int' or `long int' in base BASE.
+   If BASE is 0 the base is determined by the presence of a leading
+   zero, indicating octal or a leading "0x" or "0X", indicating hexadecimal.
+   If BASE is < 2 or > 36, it is reset to 10.
+   If ENDPTR is not NULL, a pointer to the character after the last
+   one converted is stored in *ENDPTR.	*/
+
+
+function (const char *nptr,char **endptr,int base)
 {
-        register const char *s = nptr;
-        register unsigned long long acc;
-        register int c;
-        register unsigned long long cutoff;
-        register int neg = 0, any, cutlim;
+  int negative;
+  register ulongtype cutoff;
+  register unsigned int cutlim;
+  register ulongtype i;
+  register const char *s;
+  register unsigned char c;
+  const char *save;
+  int overflow;
 
-        /*
-         * Skip white space and pick up leading +/- sign if any.
-         * If base is 0, allow 0x for hex and 0 for octal, else
-         * assume decimal; if base is already 16, allow 0x.
-         */
-        do {
-                c = *s++;
-        } while (isspace(c));
-        if (c == '-') {
-                neg = 1;
-                c = *s++;
-        } else if (c == '+')
-                c = *s++;
-        if ((base == 0 || base == 16) &&
-            c == '0' && (*s == 'x' || *s == 'X')) {
-                c = s[1];
-                s += 2;
-                base = 16;
-        }
-        if (base == 0)
-                base = c == '0' ? 8 : 10;
+  if (base < 0 || base == 1 || base > 36)
+    base = 10;
 
-        /*
-         * Compute the cutoff value between legal numbers and illegal
-         * numbers.  That is the largest legal value, divided by the
-         * base.  An input number that is greater than this value, if
-         * followed by a legal input character, is too big.  One that
-         * is equal to this value may be valid or not; the limit
-         * between valid and invalid numbers is then based on the last
-         * digit.  For instance, if the range for longs is
-         * [-2147483648..2147483647] and the input base is 10,
-         * cutoff will be set to 214748364 and cutlim to either
-         * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
-         * a value > 214748364, or equal but the next digit is > 7 (or 8),
-         * the number is too big, and we will return a range error.
-         *
-         * Set any if any `digits' consumed; make it negative to indicate
-         * overflow.
-         */
-        cutoff = neg ? -(unsigned long long)LONG_LONG_MIN : LONG_LONG_MAX;
-        cutlim = cutoff % (unsigned long long)base;
-        cutoff /= (unsigned long long)base;
-        for (acc = 0, any = 0;; c = *s++) {
-                if (isdigit(c))
-                        c -= '0';
-                else if (isalpha(c))
-                        c -= isupper(c) ? 'A' - 10 : 'a' - 10;
-                else
-                        break;
-                if (c >= base)
-                        break;
-               if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
-                        any = -1;
-                else {
-                        any = 1;
-                        acc *= base;
-                        acc += c;
-                }
-        }
-        if (any < 0) {
-                acc = neg ? LONG_LONG_MIN : LONG_LONG_MAX;
-                /* JYD rptr->_errno = ERANGE; */
-        } else if (neg)
-                acc = -acc;
-        if (endptr != 0)
-                *endptr = (char *) (any ? s - 1 : nptr);
-        return (acc);
+  s = nptr;
+
+  /* Skip white space.	*/
+  while (isspace (*s))
+    ++s;
+  if (*s == '\0')
+  {
+    goto noconv;
+  }
+
+  /* Check for a sign.	*/
+  if (*s == '-')
+  {
+    negative = 1;
+    ++s;
+  }
+  else if (*s == '+')
+  {
+    negative = 0;
+    ++s;
+  }
+  else
+    negative = 0;
+
+  if (base == 16 && s[0] == '0' && toupper (s[1]) == 'X')
+    s += 2;
+
+  /* If BASE is zero, figure it out ourselves.	*/
+  if (base == 0)
+  {
+    if (*s == '0')
+    {
+      if (toupper (s[1]) == 'X')
+      {
+	s += 2;
+	base = 16;
+      }
+      else
+	base = 8;
+    }
+    else
+      base = 10;
+  }
+
+  /* Save the pointer so we can check later if anything happened.  */
+  save = s;
+
+  cutoff = UTYPE_MAX / (unsigned long int) base;
+  cutlim = (uint) (UTYPE_MAX % (unsigned long int) base);
+
+  overflow = 0;
+  i = 0;
+  for (c = *s; c != '\0'; c = *++s)
+  {
+    if (isdigit (c))
+      c -= '0';
+    else if (isalpha (c))
+      c = toupper (c) - 'A' + 10;
+    else
+      break;
+    if (c >= base)
+      break;
+    /* Check for overflow.  */
+    if (i > cutoff || (i == cutoff && c > cutlim))
+      overflow = 1;
+    else
+    {
+      i *= (ulongtype) base;
+      i += c;
+    }
+  }
+
+  /* Check if anything actually happened.  */
+  if (s == save)
+    goto noconv;
+
+  /* Store in ENDPTR the address of one character
+     past the last character we converted.  */
+  if (endptr != NULL)
+    *endptr = (char *) s;
+
+#ifndef UNSIGNED
+  /* Check for a value that is within the range of
+     `unsigned long int', but outside the range of `long int'.	*/
+  if (negative)
+  {
+    if (i  > (ulongtype) TYPE_MIN)
+      overflow = 1;
+  }
+  else if (i > (ulongtype) TYPE_MAX)
+    overflow = 1;
+#endif
+
+  if (overflow)
+  {
+    errno=ERANGE;
+#ifdef UNSIGNED
+    return UTYPE_MAX;
+#else
+    return negative ? TYPE_MIN : TYPE_MAX;
+#endif
+  }
+
+  /* Return the result of the appropriate sign.  */
+  return (negative ? -((longtype) i) : i);
+
+noconv:
+  /* There was no number to convert.  */
+  errno=EDOM;
+  if (endptr != NULL)
+    *endptr = (char *) nptr;
+  return 0L;
 }
 
+#endif /* !HAVE_STRTOLL */
 
-long long strtoll(const char *s, char **ptr, int base)
-{
-        return _strtoll_r (s, ptr, base);
+#ifndef HAVE_ATOLL
+longtype atoll(const char *str) {
+  return strtoll(str, (char **)NULL, 10);
 }
-
-
-long long atoll (const char *s)
-{
-  return strtoll (s, NULL, 10);
-}
-
+#endif
