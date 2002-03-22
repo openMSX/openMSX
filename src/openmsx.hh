106352f76718aa94c1e9cffe7a956ce073f6684e
Maarten ter Huurne <maarten@treewalker.org>
2002-03-22 01:25:23 +0000

    64 bit integer support that works under both GCC and VC++.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1094 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/openmsx.hh
+++ src/openmsx.hh
@@ -3,12 +3,32 @@
 #ifndef __OPENMSX_HH__
 #define __OPENMSX_HH__
 
-typedef unsigned char nibble;	//  4 bit
-typedef unsigned char byte;	//  8 bit
-typedef unsigned short word;	// 16 bit 
-typedef unsigned long long uint64; // this is not portable to 64bit platforms? -> TODO check
-typedef char  signed_byte;	//  8 bit signed
-typedef short signed_word;	// 16 bit signed
+/** 4 bit integer */
+typedef unsigned char nibble;
+
+/** 8 bit signed integer */
+typedef char signed_byte;
+/** 8 bit unsigned integer */
+typedef unsigned char byte;
+
+/** 16 bit signed integer */
+typedef short signed_word;
+/** 16 bit unsigned integer */
+typedef unsigned short word;
+
+#ifdef __WIN32__
+/** 64 bit signed integer */
+typedef __int64 int64;
+/** 64 bit unsigned integer */
+typedef unsigned __int64 uint64;
+#else
+// this is not portable to 64bit platforms? -> TODO check
+/** 64 bit signed integer */
+typedef long long int64;
+// this is not portable to 64bit platforms? -> TODO check
+/** 64 bit unsigned integer */
+typedef unsigned long long uint64;
+#endif
 
 #ifdef DEBUG
 #define PRT_DEBUG(mes) {std::cout << mes << "\n"; }
