7b645b6e223e3c7d50de64665d8f2d160234a772
Wouter Vermaelen <m9710797@users.sourceforge.net>
2002-03-22 09:19:10 +0000

    faster without profiling
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1099 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- config-i686.sh
+++ config-i686.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CXXFLAGS="-O3 -pg -mcpu=pentiumpro -march=pentiumpro -ffast-math -DNDEBUG -funroll-loops"
+CXXFLAGS="-O3 -mcpu=pentiumpro -march=pentiumpro -ffast-math -DNDEBUG -funroll-loops"
 
 echo Configuring for compile with: ${CXXFLAGS}
 rm -f config.cache
