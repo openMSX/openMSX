c07299fa4876d810542fe5f00d6512912d3bd7f5
Joost Yervante Damad <joost@damad.be>
2002-05-09 19:07:10 +0000

    *** empty log message ***
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1227 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/HttpFile.cc
+++ src/HttpFile.cc
@@ -3,6 +3,21 @@
 #include "HttpFile.hh"
 #include "FileManager.hh"
 
+#include <sstream>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <cerrno>
+extern int errno;
+#include <sys/types.h>
+#include <sys/socket.h>
+
+// please note that HttpFile.cc as it currently
+// is implemented contains stuff not portable to
+// windoze, for ideas how to fix it, look for example
+// in nanohttp.c of libxml2 [http://xmlsoft.org]
+// it should not be hard
+
 HttpFile::HttpFile(const std::string& uri, byte mode)
 :File(mode)
 {
@@ -16,6 +31,24 @@ bool HttpFile::head()
 {
 	assert(false);
 	return false;
+	int s;
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		std::ostringstream s;
+		s << "HttpFile::head: socket:" << strerror(errno);
+		throw FileManagerException(s.str());
+	}
+	int status;
+	if ((status = fcntl(s, F_GETFL, 0)) != -1) {
+		status |= O_NONBLOCK;
+		status = fcntl(s, F_SETFL, status);
+	}
+	else
+	{
+		std::ostringstream s;
+		s << "HttpFile::head: fcntl:" << strerror(errno);
+		throw FileManagerException(s.str());
+	}
 }
 
 void HttpFile::fetch()
