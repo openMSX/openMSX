41302123664ff897567e3d748e6a009b544003a0
Maarten ter Huurne <maarten@treewalker.org>
2002-09-08 19:04:09 +0000

    Updated C-BIOS to version 0.07.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1389 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- Contrib/cbios/src/cmp.bat
+++ Contrib/cbios/src/cmp.bat
@@ -1,5 +1,5 @@
-as80 -z -x3 msxdmy
+as80 -z -x3 msxb_sub
 as80 -z -x3 msxb_sh
 as80 -lmsx.syn -z -x3 msxb
 copy /b msxb.bin+msxb_sh.bin msx.rom
-copy msxdmy.bin msxdmy.rom
+copy msxb_sub.bin msxb_sub.rom
