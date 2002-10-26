913be17567a71d6403f82efea6a120aafabace72
Wouter Vermaelen <m9710797@users.sourceforge.net>
2002-10-26 11:44:31 +0000

    forgot to add files
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1459 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/CartridgeSlotManager.cc
@@ -0,0 +1,80 @@
+// $Id$
+
+#include "CartridgeSlotManager.hh"
+#include "MSXConfig.hh"
+
+
+const int RESERVED = 16;
+const int EXISTS   = 32;
+
+
+CartridgeSlotManager::CartridgeSlotManager()
+{
+	for (int slot = 0; slot < 16; slot++) {
+		slots[slot] = 0;
+	}
+}
+
+CartridgeSlotManager* CartridgeSlotManager::instance()
+{
+	static CartridgeSlotManager oneInstance;
+	return &oneInstance;
+}
+
+
+void CartridgeSlotManager::reserveSlot(int slot)
+{
+	assert((0 <= slot) && (slot < 16));
+	slots[slot] |= RESERVED;
+}
+
+void CartridgeSlotManager::readConfig()
+{
+	try {
+		MSXConfig::Config *config = MSXConfig::Backend::instance()->
+					       getConfigById("ExternalSlots");
+		std::string slotName("slota");
+		for (int slot = 0; slot < 16; slot++) {
+			slotName[4] = 'a' + slot;
+			if (config->hasParameter(slotName)) {
+				std::string slotValue = config->getParameter(slotName);
+				int ps = slotValue[0] - '0';
+				int ss = slotValue[2] - '0';
+				if ((ps < 0) || (ps > 3) || (ss < 0) || (ss > 3) ||
+				    (slotValue[1] != '-') || (slotValue.length() != 3)) {
+					PRT_ERROR("Syntax error in ExternalSlots config");
+				}
+				slots[slot] |= (ss << 2) | ps | EXISTS;
+			}
+		}
+	} catch (MSXException &e) {
+		// do nothing
+	}
+}
+
+void CartridgeSlotManager::getSlot(int slot, int &ps, int &ss)
+{
+	assert((0 <= slot) && (slot < 16));
+	assert(slots[slot] & RESERVED);
+
+	if (slots[slot] & EXISTS) {
+		ps = slots[slot] & 3;
+		ss = (slots[slot] >> 2) & 3;
+	} else {
+		PRT_ERROR("Slot"<< (char)('a' + slot) << " not defined");
+	}
+}
+
+void CartridgeSlotManager::getSlot(int &ps, int &ss)
+{
+	for (int slot = 0; slot < 16; slot++) {
+		if ((slots[slot] & EXISTS) && !(slots[slot] & RESERVED)) {
+			slots[slot] |= RESERVED;
+			ps = slots[slot] & 3;
+			ss = (slots[slot] >> 2) & 3;
+			return;
+		}
+	}
+	PRT_ERROR("Not enough free cartridge slots");
+}
+
---
+++ src/CartridgeSlotManager.hh
@@ -0,0 +1,25 @@
+// $Id$
+
+#ifndef __CARTRIDGESLOTMANAGER_HH__
+#define __CARTRIDGESLOTMANAGER_HH__
+
+
+class CartridgeSlotManager
+{
+	public:
+		static CartridgeSlotManager* instance();
+
+		void reserveSlot(int slot);
+		void readConfig();
+		void getSlot(int slot, int &ps, int &ss);
+		void getSlot(int &ps, int &ss);
+
+	private:
+		CartridgeSlotManager();
+
+		int slots[16];
+};
+
+
+#endif
+
