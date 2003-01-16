8ce51a7d93d23e950f7f5309b0c7cd759f94e2fa
Wouter Vermaelen <m9710797@users.sourceforge.net>
2003-01-16 20:04:54 +0000

    fixed MSXF4Device
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1596 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/MSXF4Device.cc
+++ src/MSXF4Device.cc
@@ -17,15 +17,19 @@ MSXF4Device::~MSXF4Device()
 
 void MSXF4Device::reset(const EmuTime &time)
 {
-	status = 0;
+	status = inverted ? 0xFF : 0x00;
 }
 
 byte MSXF4Device::readIO(byte port, const EmuTime &time)
 {
-	return inverted ? status ^ 0xFF : status;
+	return status;
 }
 
 void MSXF4Device::writeIO(byte port, byte value, const EmuTime &time)
 {
-	status = (status & 0x20) | (value & 0xa0);
+	if (inverted) {
+		status = value | 0x7F;
+	} else {
+		status = (status & 0x20) | (value & 0xa0);
+	}
 }
