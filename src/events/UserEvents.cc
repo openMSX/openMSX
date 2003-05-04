63d27135878d0180516bdaa705f969e6593a1959
Maarten ter Huurne <maarten@treewalker.org>
2003-05-04 14:59:59 +0000

    Reikan's workaround for SDL_PushEvent incompatibility between SDL versions.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@1915 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/events/UserEvents.cc
+++ src/events/UserEvents.cc
@@ -9,16 +9,17 @@ void UserEvents::push(
 	UserEvents::UserEventID id, void *data1, void *data2 )
 {
 	// Push user event into SDL event queue.
-	// SDL_PushEvent operates on a copy of the event, so we can use a temp.
+	// SDL_PeepEvents operates on a copy of the event, so we can use a temp.
 	SDL_Event event;
 	event.type = SDL_USEREVENT;
 	event.user.code = id;
 	event.user.data1 = data1;
 	event.user.data2 = data2;
-	while (true) {
-		int pushed = SDL_PushEvent(&event);
-		if (pushed == 1) break;
-		SDL_Delay(100);
+	// We cannot use SDL_PushEvent, because it is incompatible:
+	// SDL 1.2.5 returns 0 on OK, -1 on error.
+	// SDL 1.2.4 returns 1 on OK, 0 on queue full and -1 on other errors.
+	while (SDL_PeepEvents(&event, 1, SDL_ADDEVENT, 0) < 1) {
+		SDL_Delay(100); // retry in 100 ms
 	}
 }
 
