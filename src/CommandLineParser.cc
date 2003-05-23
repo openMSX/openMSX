5cfac39f3437a2c515e2f9501f0680f04f4a6b59
Wouter Vermaelen <m9710797@users.sourceforge.net>
2003-05-23 22:39:39 +0000

    fixed indentation
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2019 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/CommandLineParser.cc
+++ src/CommandLineParser.cc
@@ -41,10 +41,10 @@ CommandLineParser::CommandLineParser()
 {
 	haveConfig = false;
 
-	registerOption("-config",	&configFile);
-	registerFileType(".xml",	&configFile);
-	registerOption("-machine",	&machineOption);
-	registerOption("-setting",	&settingOption);
+	registerOption("-config",  &configFile);
+	registerFileType(".xml",   &configFile);
+	registerOption("-machine", &machineOption);
+	registerOption("-setting", &settingOption);
 }
 
 void CommandLineParser::registerOption(const std::string &str,
@@ -56,10 +56,9 @@ void CommandLineParser::registerOption(const std::string &str,
 void CommandLineParser::registerFileType(const std::string &str,
 		CLIFileType* cliFileType)
 {
-	if (str[0] == '.'){ // a real extention
+	if (str[0] == '.') { // a real extention
 		fileTypeMap[str.substr(1)] = cliFileType;
-	}
-	else{ // or a file class
+	} else{ // or a file class
 		fileClassMap[str] = cliFileType;
 	}
 }
@@ -67,33 +66,33 @@ void CommandLineParser::registerFileType(const std::string &str,
 void CommandLineParser::postRegisterFileTypes()
 {
 	std::list<Config::Parameter*> * extensions;
-	std::map<std::string, CLIFileType*,caseltstr>::const_iterator i;
-	try{
-		Config * config = MSXConfig::instance()->getConfigById("FileTypes");
-		for (i=fileClassMap.begin();i!=fileClassMap.end();i++){
+	std::map<std::string, CLIFileType*, caseltstr>::const_iterator i;
+	try {
+		Config *config = MSXConfig::instance()->getConfigById("FileTypes");
+		for (i = fileClassMap.begin(); i != fileClassMap.end(); i++) {
 			extensions = config->getParametersWithClass((*i).first);
 			std::list<Config::Parameter*>::const_iterator j;
-			for (j=extensions->begin();j!=extensions->end();j++){
-				fileTypeMap[(*j)->value]=(*i).second;
+			for (j = extensions->begin(); j != extensions->end(); j++) {
+				fileTypeMap[(*j)->value] = (*i).second;
 			}
 		}
-	}catch (MSXException &e) {
+	} catch (MSXException &e) {
 		std::map<std::string,std::string> fileExtMap;
 		std::map<std::string,std::string>::const_iterator j;
-		fileExtMap["rom"]="romimages";
-		fileExtMap["dsk"]="diskimages";
-		fileExtMap["di1"]="diskimages";
-		fileExtMap["di2"]="diskimages";
-		fileExtMap["xsa"]="diskimages";
-		fileExtMap["cas"]="cassetteimages";
-		fileExtMap["wav"]="cassettesounds";
-		for (j=fileExtMap.begin();j!=fileExtMap.end();j++){	
-			i=fileClassMap.find((*j).second);
-			if (i!=fileClassMap.end()){
-				fileTypeMap[(*j).first]=(*i).second;
+		fileExtMap["rom"] = "romimages";
+		fileExtMap["dsk"] = "diskimages";
+		fileExtMap["di1"] = "diskimages";
+		fileExtMap["di2"] = "diskimages";
+		fileExtMap["xsa"] = "diskimages";
+		fileExtMap["cas"] = "cassetteimages";
+		fileExtMap["wav"] = "cassettesounds";
+		for (j = fileExtMap.begin(); j != fileExtMap.end(); j++) {
+			i = fileClassMap.find((*j).second);
+			if (i != fileClassMap.end()) {
+				fileTypeMap[(*j).first] = (*i).second;
 			}
-		}	
-	}		
+		}
+	}
 }
 
 bool CommandLineParser::parseOption(const std::string &arg,std::list<std::string> &cmdLine)
@@ -104,11 +103,12 @@ bool CommandLineParser::parseOption(const std::string &arg,std::list<std::string
 		// parse option
 		it1->second->parseOption(arg, cmdLine);
 		return true; // processed
-	}	
+	}
 	return false; // unknown
 }
 
-bool CommandLineParser::parseFileName(const std::string &arg,std::list<std::string> &cmdLine)
+bool CommandLineParser::parseFileName(const std::string &arg,
+                                      std::list<std::string> &cmdLine)
 {
 	int begin = arg.find_last_of('.');
 	if (begin != -1) {
@@ -151,22 +151,22 @@ void CommandLineParser::parse(int argc, char **argv)
 	while (!cmdLine.empty()) {
 		std::string arg = cmdLine.front();
 		cmdLine.pop_front();
-				
+		
 		// first try options
-		if (!parseOption(arg,cmdLine)){
+		if (!parseOption(arg, cmdLine)) {
 			// next try the registered filetypes (xml)
-			if (!parseFileName(arg,cmdLine)){
+			if (!parseFileName(arg, cmdLine)) {
 				fileCmdLine.push_back(arg); // no option or known file
-			}	
-		}	
-	}	
+			}
+		}
+	}
 	
 	// load default settings file in case the user didn't specify one
 	MSXConfig *config = MSXConfig::instance();
-	
 	try {
 		if (!settingOption.parsed) {
-			config->loadFile(new SystemFileContext(), "share/settings.xml");
+			config->loadFile(new SystemFileContext(),
+			                 "share/settings.xml");
 		}
 	} catch (MSXException &e) {
 		// settings.xml not found
@@ -186,22 +186,22 @@ void CommandLineParser::parse(int argc, char **argv)
 		} catch (MSXException &e) {
 			// no DefaultMachine section
 		}
-		try{		
-		std::cout << "Selected: " << MACHINE_PATH + machine << std::endl;
-		config->loadFile(new SystemFileContext(),
-		        MACHINE_PATH + machine + "/hardwareconfig.xml");
+		try {
+			PRT_INFO("Selected: " << MACHINE_PATH + machine);
+			config->loadFile(new SystemFileContext(),
+				MACHINE_PATH + machine + "/hardwareconfig.xml");
 		} catch (FileException &e) {
-		bool found=false;
-		std::list<std::string>::const_iterator it;
-		for (it = fileCmdLine.begin();it !=	fileCmdLine.end();it ++){
-			if (*it == "-h"){
-				found = true;	
+			bool found = false;
+			std::list<std::string>::const_iterator it;
+			for (it = fileCmdLine.begin(); it != fileCmdLine.end(); it++) {
+				if (*it == "-h") {
+					found = true;
+				}
+			}
+			if (!found) {
+				PRT_INFO("Warning: No machine file found!");
 			}
 		}
-		if (!found){
-			PRT_INFO("Warning: No machine file found!");	
-		}
-		}		
 	}
 
 	postRegisterFileTypes();
@@ -212,13 +212,13 @@ void CommandLineParser::parse(int argc, char **argv)
 		std::string arg = fileCmdLine.front();
 		fileCmdLine.pop_front();
 		// first, try the options
-		if (!parseOption(arg,cmdLine)){
+		if (!parseOption(arg, cmdLine)) {
 			// next try the registered filetypes (xml)
-			if (!parseFileName(arg,cmdLine)){
+			if (!parseFileName(arg, cmdLine)) {
 				// what is this?
 				PRT_ERROR("Wrong parameter: " << arg);
-			}	
-		}	
+			}
+		}
 	}
 	
 	// read existing cartridge slots from config
@@ -248,114 +248,125 @@ void CommandLineParser::HelpOption::parseOption(const std::string &option,
 	PRT_INFO("  this is the list of supported options:");
 	
 	std::set<std::string> printSet;
-	std::map<CLIOption*,std::set<std::string>*> tempOptionMap;
-	std::map<CLIOption*,std::set<std::string>*>::iterator itTempOptionMap;
+	std::map<CLIOption*, std::set<std::string>*> tempOptionMap;
+	std::map<CLIOption*, std::set<std::string>*>::iterator itTempOptionMap;
 	std::set<std::string>::iterator itSet;
 	std::map<std::string, CLIOption*>::const_iterator itOption;
-	for (itOption = parser->optionMap.begin(); itOption != parser->optionMap.end(); itOption++) {
+	for (itOption = parser->optionMap.begin();
+	     itOption != parser->optionMap.end();
+	     itOption++) {
 		itTempOptionMap = tempOptionMap.find(itOption->second);
-		if (itTempOptionMap == tempOptionMap.end()){
-		tempOptionMap[itOption->second]=new std::set<std::string>;
+		if (itTempOptionMap == tempOptionMap.end()) {
+			tempOptionMap[itOption->second] = new std::set<std::string>;
 		}
 		itTempOptionMap = tempOptionMap.find(itOption->second);
-		if (itTempOptionMap != tempOptionMap.end()){	
+		if (itTempOptionMap != tempOptionMap.end()) {
 			itTempOptionMap->second->insert(itOption->first);
-		}			
+		}
 	}
-	std::string tempString;	
-	for (itTempOptionMap=tempOptionMap.begin();itTempOptionMap != tempOptionMap.end();itTempOptionMap++){
-		tempString = formatSet(itTempOptionMap->second,15) + " " 
-		+ formatHelptext(itTempOptionMap->first->optionHelp() ,50,20);
-		printSet.insert (tempString);
+	std::string tempString;
+	for (itTempOptionMap = tempOptionMap.begin();
+	     itTempOptionMap != tempOptionMap.end();
+	     itTempOptionMap++) {
+		tempString = formatSet(itTempOptionMap->second,15) + " " +
+			formatHelptext(itTempOptionMap->first->optionHelp(), 50, 20);
+		printSet.insert(tempString);
 		delete itTempOptionMap->second;
 	}
-	for (itSet = printSet.begin(); itSet!=printSet.end();itSet++){
-	PRT_INFO(*itSet);
-	}	
-	printSet.clear();	
-		
+	for (itSet = printSet.begin(); itSet != printSet.end(); itSet++) {
+		PRT_INFO(*itSet);
+	}
+	printSet.clear();
+	
 	PRT_INFO("");
 	PRT_INFO("  this is the list of supported file types:");
 
-	std::map<CLIFileType*,std::set<std::string>*> tempExtMap;
-	std::map<CLIFileType*,std::set<std::string>*>::iterator itTempExtMap;
+	std::map<CLIFileType*, std::set<std::string>*> tempExtMap;
+	std::map<CLIFileType*, std::set<std::string>*>::iterator itTempExtMap;
 	std::map<std::string, CLIFileType*>::const_iterator itFileType;
-	for (itFileType = parser->fileTypeMap.begin(); itFileType != parser->fileTypeMap.end(); itFileType++) {
+	for (itFileType = parser->fileTypeMap.begin();
+	     itFileType != parser->fileTypeMap.end();
+	     itFileType++) {
 		itTempExtMap = tempExtMap.find(itFileType->second);
-		if (itTempExtMap == tempExtMap.end()){
-		tempExtMap[itFileType->second]=new std::set<std::string>;
+		if (itTempExtMap == tempExtMap.end()) {
+			tempExtMap[itFileType->second] = new std::set<std::string>;
 		}
 		itTempExtMap = tempExtMap.find(itFileType->second);
-		if (itTempExtMap != tempExtMap.end()){	
+		if (itTempExtMap != tempExtMap.end()) {
 			itTempExtMap->second->insert(itFileType->first);
-		}			
+		}
 	}
-	for (itTempExtMap=tempExtMap.begin();itTempExtMap != tempExtMap.end();itTempExtMap++){
-		tempString = formatSet(itTempExtMap->second,15) + " " 
-		+ formatHelptext(itTempExtMap->first->fileTypeHelp(),50,20);
-		printSet.insert (tempString);
+	for (itTempExtMap = tempExtMap.begin();
+	     itTempExtMap != tempExtMap.end();
+	     itTempExtMap++) {
+		tempString = formatSet(itTempExtMap->second, 15) + " " +
+			formatHelptext(itTempExtMap->first->fileTypeHelp(), 50, 20);
+		printSet.insert(tempString);
 		delete itTempExtMap->second; 
 	}
-	for (itSet = printSet.begin(); itSet!=printSet.end();itSet++){
-	PRT_INFO(*itSet);
+	for (itSet = printSet.begin(); itSet != printSet.end(); itSet++) {
+		PRT_INFO(*itSet);
 	}
 	exit(0);
 }
+
 const std::string& CommandLineParser::HelpOption::optionHelp() const
 {
 	static const std::string text("Shows this text");
 	return text;
 }
 
-std::string CommandLineParser::HelpOption::formatSet(std::set<std::string> * inputSet,unsigned columns){
-		std::string fillString (60,' ');
-		std::set<std::string>::iterator it4;
-		std::string outString="";
-		unsigned totalLength=0; // ignore the starting spaces for now
-		for (it4 = inputSet->begin(); it4!=inputSet->end();it4++){
-			std::string temp = *it4;
-					
-			if (totalLength == 0){	// first element ?
-				outString += "    " + temp;
-				totalLength = temp.length();	
-			}
-			else{
-				outString += ", ";	
-				if ((totalLength + temp.length()) > columns){
-					outString += "\n    " + temp;
-					totalLength = temp.length();
-				}	
-				else{
-					outString += temp;
-					totalLength += 2 + temp.length();
-				}		
+std::string CommandLineParser::HelpOption::formatSet(std::set<std::string> *inputSet,
+                                                     unsigned columns)
+{
+	std::string fillString(60, ' ');
+	std::set<std::string>::iterator it4;
+	std::string outString;
+	unsigned totalLength = 0; // ignore the starting spaces for now
+	for (it4 = inputSet->begin(); it4 != inputSet->end(); it4++) {
+		std::string temp = *it4;
+		if (totalLength == 0) {
+			// first element ?
+			outString += "    " + temp;
+			totalLength = temp.length();
+		} else {
+			outString += ", ";
+			if ((totalLength + temp.length()) > columns) {
+				outString += "\n    " + temp;
+				totalLength = temp.length();
+			} else {
+				outString += temp;
+				totalLength += 2 + temp.length();
 			}
 		}
-		if (totalLength <= columns){
-			outString += fillString.substr(60-(columns-totalLength));
-		}
+	}
+	if (totalLength <= columns) {
+		outString += fillString.substr(60 - (columns - totalLength));
+	}
 	return outString;
 }
 
-std::string CommandLineParser::HelpOption::formatHelptext(std::string helpText,unsigned maxlength, unsigned indent){
-	std::string outText="";
+std::string CommandLineParser::HelpOption::formatHelptext(std::string helpText,
+                   unsigned maxlength, unsigned indent)
+{
+	std::string outText;
 	unsigned pos;
-	std::string fillString (60,' ');
-	unsigned index=0;
-	while (helpText.substr(index).length() > maxlength){
-		pos=helpText.substr(index).rfind(' ',index+maxlength);
-		if (!pos){
-		pos=helpText.substr(index+maxlength).find(' ');
+	std::string fillString(60, ' ');
+	unsigned index = 0;
+	while (helpText.substr(index).length() > maxlength) {
+		pos = helpText.substr(index).rfind(' ', index+maxlength);
+		if (!pos) {
+			pos = helpText.substr(index + maxlength).find(' ');
 		}
-		if (pos){
-			outText += helpText.substr(index,index+pos) + "\n" + fillString.substr(60-indent);
-			index=pos+1;
+		if (pos) {
+			outText += helpText.substr(index, index + pos) + 
+			           "\n" + fillString.substr(60 - indent);
+			index = pos + 1;
 		}
-	}	
+	}
 	outText += helpText.substr(index);
-		
 	return outText;
-}	
+}
 
 // Config file type
 void CommandLineParser::ConfigFile::parseOption(const std::string &option,
