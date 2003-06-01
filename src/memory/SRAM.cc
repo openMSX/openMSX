cadf217a1d655648afd7c41652d31e5ea8b5d298
Wouter Vermaelen <m9710797@users.sourceforge.net>
2003-06-01 11:33:44 +0000

    Removed "loadsram" and "savesram" settings
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2073 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/SRAM.cc
+++ src/memory/SRAM.cc
@@ -13,52 +13,46 @@ SRAM::SRAM(int size_, Config *config_, const char *header_)
 	sram = new byte[size];
 	memset(sram, 0xFF, size);
 	
-	if (!config->hasParameter("loadsram") ||
-	     config->getParameterAsBool("loadsram")) {
-		const std::string &filename = config->getParameter("sramname");
-		PRT_DEBUG("SRAM: read " << filename);
-		try {
-			bool headerOk = true;
-			File file(config->getContext()->resolveSave(filename),
-			          LOAD_PERSISTENT);
-			if (header) {
-				int length = strlen(header);
-				byte* temp = new byte[length];
-				file.read(temp, length);
-				if (strncmp((char*)temp, header, length) != 0) {
-					headerOk = false;
-				}
-				delete[] temp;
+	const std::string &filename = config->getParameter("sramname");
+	PRT_DEBUG("SRAM: read " << filename);
+	try {
+		bool headerOk = true;
+		File file(config->getContext()->resolveSave(filename),
+			  LOAD_PERSISTENT);
+		if (header) {
+			int length = strlen(header);
+			byte* temp = new byte[length];
+			file.read(temp, length);
+			if (strncmp((char*)temp, header, length) != 0) {
+				headerOk = false;
 			}
-			if (headerOk) {
-				file.read(sram, size);
-			} else {
-				PRT_INFO("Warning no correct SRAM file: " <<
-				         filename);
-			}
-		} catch (FileException &e) {
-			PRT_INFO("Couldn't load SRAM " << filename);
+			delete[] temp;
 		}
+		if (headerOk) {
+			file.read(sram, size);
+		} else {
+			PRT_INFO("Warning no correct SRAM file: " <<
+				 filename);
+		}
+	} catch (FileException &e) {
+		PRT_DEBUG("Couldn't load SRAM " << filename);
 	}
 }
 
 SRAM::~SRAM()
 {
-	if (!config->hasParameter("savesram") ||
-	     config->getParameterAsBool("savesram")) {
-		const std::string &filename = config->getParameter("sramname");
-		PRT_DEBUG("SRAM: save " << filename);
-		try {
-			File file(config->getContext()->resolveSave(filename),
-			          SAVE_PERSISTENT);
-			if (header) {
-				int length = strlen(header);
-				file.write((const byte*)header, length);
-			}
-			file.write(sram, size);
-		} catch (FileException &e) {
-			PRT_INFO("Couldn't save SRAM " << filename);
+	const std::string &filename = config->getParameter("sramname");
+	PRT_DEBUG("SRAM: save " << filename);
+	try {
+		File file(config->getContext()->resolveSave(filename),
+			  SAVE_PERSISTENT);
+		if (header) {
+			int length = strlen(header);
+			file.write((const byte*)header, length);
 		}
+		file.write(sram, size);
+	} catch (FileException &e) {
+		PRT_INFO("Couldn't save SRAM " << filename);
 	}
 	delete[] sram;
 }
