5fe10f4d183467593f246d79c7b5a0ca69f88781
Maarten ter Huurne <maarten@treewalker.org>
2003-07-06 17:16:04 +0000

    Adapted to work from if source dir != build dir.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2218 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- config-i686.sh
+++ config-i686.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+srcdir=`dirname $0`
 
 CXXFLAGS="-O3 -mcpu=pentiumpro -march=pentiumpro -ffast-math -funroll-loops -I/usr/include/libxml2/libxml"
 
 echo Configuring for compile with: ${CXXFLAGS}
 rm -f config.cache
-CXXFLAGS=${CXXFLAGS} ./configure --without-pic $*
+CXXFLAGS=${CXXFLAGS} ${srcdir}/configure --without-pic $*
