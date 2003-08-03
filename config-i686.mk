489ead7024c36bae56145298a46de217acc300e2
Maarten ter Huurne <maarten@treewalker.org>
2003-08-03 21:30:11 +0000

    Disable asserts.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2304 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- config-i686.mk
+++ config-i686.mk
@@ -4,7 +4,8 @@
 # Optimised for Pentium 2 and higher.
 
 # Optimisation flags.
-CXXFLAGS+=-O3 -mcpu=pentiumpro -march=pentiumpro -ffast-math -funroll-loops
+CXXFLAGS+=-O3 -DNDEBUG \
+	-mcpu=pentiumpro -march=pentiumpro -ffast-math -funroll-loops
 
 # Strip executable?
 OPENMSX_STRIP:=true
