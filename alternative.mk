5e80882ec2171cd30a0f1ec0768221182ff53b9c
Maarten ter Huurne <maarten@treewalker.org>
2003-08-03 18:35:49 +0000

    Added dependency file generation. And some other improvements.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2300 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- alternative.mk
+++ alternative.mk
@@ -9,18 +9,28 @@
 # "Recursive Make Considered Harmful".
 # http://www.tip.net.au/~millerp/rmch/recu-make-cons-harm.html
 
-.PHONY: all
-
 # Used-adjustable values:
 
-CXXFLAGS=-O3 -mcpu=pentiumpro -march=pentiumpro -ffast-math -funroll-loops
+# Generic compilation flags.
+CXXFLAGS:=-pipe
+# Stricter warning and error reporting.
+CXXFLAGS+=-Wall
+# Optimisation flags.
+CXXFLAGS+=-O3 -mcpu=pentiumpro -march=pentiumpro -ffast-math -funroll-loops
 
-LIBS_EXTERNAL:=stdc++ gcc_s xml2 SDL SDL_image GL
+LIBS_EXTERNAL:=stdc++ xml2 SDL SDL_image GL
 INCLUDE_EXTERNAL:=/usr/include/libxml2
 
 BUILD_PATH:=derived
 
 # No user-servicable parts below:
+
+# Logical targets which require dependency files.
+DEPEND_TARGETS:=all
+# Logical targets which do not require dependency files.
+NODEPEND_TARGETS:=clean
+# Mark all logical targets as such.
+.PHONY: $(DEPEND_TARGETS) $(NODEPEND_TARGETS)
 
 SOURCES_PATH:=src
 # TODO: Use node.mk system for building sources list.
@@ -30,6 +40,9 @@ SOURCES_FULL:=$(filter-out $(SOURCES_PATH)/debugger/Views.cc,$(SOURCES_FULL))
 SOURCES_FULL:=$(filter-out $(SOURCES_PATH)/thread/testCondVar.cc,$(SOURCES_FULL))
 SOURCES_FULL:=$(filter-out $(SOURCES_PATH)/libxmlx/xmlxdump.cc,$(SOURCES_FULL))
 SOURCES:=$(SOURCES_FULL:$(SOURCES_PATH)/%.cc=%)
+
+DEPEND_PATH:=$(BUILD_PATH)/dep
+DEPEND_FULL:=$(addsuffix .d,$(addprefix $(DEPEND_PATH)/,$(SOURCES)))
 
 LIBS_FLAGS:=$(addprefix -l,$(LIBS_EXTERNAL))
 
@@ -42,19 +55,32 @@ OBJECTS_FULL:=$(addsuffix .o,$(addprefix $(OBJECTS_PATH)/,$(SOURCES)))
 BINARY_PATH:=$(BUILD_PATH)/bin
 BINARY_FULL:=$(BINARY_PATH)/openmsx
 
-# TODO: Generate dependency files.
-
+# Default target; make sure this is always the first target in this Makefile.
 all: $(BINARY_FULL)
 
+# Include dependency files.
+ifeq ($(filter $(NODEPEND_TARGETS),$(MAKECMDGOALS)),)
+-include $(DEPEND_FULL)
+endif
+
+# Clean up entire build tree.
 clean:
 	@echo "Cleaning up..."
 	@rm -rf $(BUILD_PATH)
 
-$(OBJECTS_PATH)/%.o: $(SOURCES_PATH)/%.cc
+# Compile and generate dependency files in one go.
+DEPEND_SUBST=$(patsubst $(SOURCES_PATH)/%.cc,$(DEPEND_PATH)/%.d,$<)
+$(OBJECTS_FULL): $(OBJECTS_PATH)/%.o: $(SOURCES_PATH)/%.cc $(DEPEND_PATH)/%.d
 	@echo "Compiling $(patsubst $(SOURCES_PATH)/%,%,$<)..."
 	@mkdir -p $(@D)
-	@gcc -o $@ $(CXXFLAGS) $(INCLUDE_FLAGS) -c $<
+	@mkdir -p $(patsubst $(OBJECTS_PATH)%,$(DEPEND_PATH)%,$(@D))
+	@gcc \
+		-MMD -MT $(DEPEND_SUBST) -MF $(DEPEND_SUBST) \
+		-o $@ $(CXXFLAGS) $(INCLUDE_FLAGS) -c $<
+	@touch $@ # Force .o file to be newer than .d file.
+$(DEPEND_FULL):
 
+# Link executable.
 $(BINARY_FULL): $(OBJECTS_FULL)
 	@echo "Linking $(patsubst $(BINARY_PATH)/%,%,$@)..."
 	@mkdir -p $(@D)
