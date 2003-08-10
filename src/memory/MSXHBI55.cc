ce211aef6ce12e047e0838119d3393c48f0ebaf8
Maarten ter Huurne <maarten@treewalker.org>
2003-08-10 17:52:17 +0000

    Avoid Doxygen confusion. And moved documentation to implementation file.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2348 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/MSXHBI55.cc
+++ src/memory/MSXHBI55.cc
@@ -1,5 +1,33 @@
 // $Id$
 
+/*
+ * Submitted By: Albert Beevendorp (bifimsx)
+ *
+ * Some Sony MSX machines have built-in firmware
+ * containing an address 'book', memo and scheduler and a
+ * few of these can store these data on a data cartridge.
+ *
+ * In Basic it's possible to use it with the "CAT:" device.
+ *
+ * I'm not sure if there are more types, though the HBI-55
+ * is a 4KB SRAM containing cartridge accessed by I/O
+ * ports connected to a 8255 chip:
+ *
+ * B0 = LSB address
+ * B1 = MSB address (D7-D6 = 01 write, 11 read)
+ * B2 = Data port
+ * B3 = Access control
+ *
+ * Sample basic program:
+ *
+ * 10  FOR I=0 TO 4095: OUT &HB3,128: OUT &HB0,I MOD 256:
+ *     OUT &HB1,64 OR I\256: OUT &HB2,I MOD 256: NEXT I
+ * 20  FOR I=0 TO 4095:OUT &HB3,128-9*(I<>0): OUT &HB0,I MOD 256:
+ *     OUT &HB1,192 OR I\256: IF I MOD 256=INP(&HB2) THEN NEXT
+ *     ELSE PRINT "Error comparing byte:";I: END
+ * 30  PRINT "Done!"
+ */
+
 #include <cassert>
 #include "MSXHBI55.hh"
 #include "MSXConfig.hh"
