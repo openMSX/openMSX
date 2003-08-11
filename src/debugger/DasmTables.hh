900fb966656aab988ccd058265cd1df6d06c7772
Maarten ter Huurne <maarten@treewalker.org>
2003-08-11 06:31:32 +0000

    Added missing double-include prevention.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2353 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/debugger/DasmTables.hh
+++ src/debugger/DasmTables.hh
@@ -1,5 +1,8 @@
 // $Id$
 
+#ifndef __DASMTABLES_HH__
+#define __DASMTABLES_HH__
+
 namespace openmsx {
 
 extern const char *mnemonic_xx_cb[256];
@@ -9,3 +12,5 @@ extern const char *mnemonic_xx[256];
 extern const char *mnemonic_main[256];
 
 } // namespace openmsx
+
+#endif // __DASMTABLES_HH__
