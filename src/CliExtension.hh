bd07b4f7acb9f7132e7805a770d12d7ede97b141
Reikan <reikan@users.sourceforge.net>
2003-08-17 15:20:05 +0000

    initial step to support Win32.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2385 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/CliExtension.hh
+++ src/CliExtension.hh
@@ -21,7 +21,17 @@ class CliExtension : public CLIOption
 	private:
 		void createExtensions(const string &path);
 		
+#ifdef	FS_CASEINSENSE
+		// on win32, MacOSX, filesystem itself is case-insensitive...
+		struct caseltstr {
+			bool operator()(const string s1, const string s2) const {
+				return strcasecmp(s1.c_str(), s2.c_str()) < 0;
+			}
+		};
+		map<string, string, caseltstr> extensions;
+#else
 		map<string, string> extensions;
+#endif
 };
 
 } // namespace openmsx
