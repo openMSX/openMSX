307adb1cebce1656cc0af985c48ed6b94d6defb0
Maarten ter Huurne <maarten@treewalker.org>
2003-08-21 17:19:07 +0000

    Based on Reikan's openMSX.snap.1.928.UOW32.diff, but unverified.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2396 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ flavour-uow32.mk
@@ -0,0 +1,15 @@
+# $Id$
+#
+# Configuration for UOW32 flavour:
+# Optimised for Pentium 3 but runnable at i386 and higher.
+
+# Optimisation flags.
+CXXFLAGS+= \
+	-mcpu=pentium3 -O \
+	-fno-force-mem -fno-force-addr \
+	-fstrength-reduce -fexpensive-optimizations -fschedule-insns2 \
+	-fomit-frame-pointer \
+	-DNDEBUG
+
+# Strip executable?
+OPENMSX_STRIP:=true
---
+++ platform-mingw-x86.mk
@@ -0,0 +1,34 @@
+# $Id$
+#
+# Configuration for MinGW on x86 machines.
+
+# Does platform support symlinks?
+USE_SYMLINK:=true
+
+# Default build flavour.
+OPENMSX_FLAVOUR?=uow32
+
+# Default compiler.
+OPENMSX_CXX?=g++ -B/mingw
+
+# File name extension of executables.
+EXEEXT:=.exe
+
+# Libraries that do not have a lib-config script.
+LIBS_PLAIN:=stdc++ SDL_image z
+# Libraries that have a lib-config script.
+LIBS_CONFIG:=xml2 sdl
+
+# Compiler flags.
+CXXFLAGS+= \
+	-mthreads -mconsole -mms-bitfields \
+	-D__GTHREAD_HIDE_WIN32API \
+	-DNO_X11 -DNO_MMAP -DNO_SOCKET -DNO_BZERO -DNO_LINUX_RTC \
+	-DFS_CASEINSENSE \
+	-DUOW32
+# TODO: Not sure this is needed anymore if "g++ -B" is used.
+#	-I/mingw/include -I/mingw/include/w32api
+
+# Linker flags.
+LINK_FLAGS:=-L/mingw/lib -L/mingw/lib/w32api $(LINK_FLAGS)
+
