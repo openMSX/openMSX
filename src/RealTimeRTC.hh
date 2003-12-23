cbc0ca68f502fc10d7ac68ce45395ebc38f001d8
Maarten ter Huurne <maarten@treewalker.org>
2003-12-23 03:59:00 +0000

    Cleaned up code layout.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2849 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/RealTimeRTC.hh
+++ src/RealTimeRTC.hh
@@ -1,44 +1,45 @@
 // $Id$
 
-#ifndef __MSXREALTIMERTC_HH__
-#define __MSXREALTIMERTC_HH__
+#ifndef __REALTIMERTC_HH__
+#define __REALTIMERTC_HH__
 
 #include "config.h"
 #ifdef HAVE_LINUX_RTC_H
 
 #include "RealTime.hh"
 
+
 namespace openmsx {
 
 class RealTimeRTC : public RealTime
 {
-	public:
-		static RealTimeRTC* create();
-		virtual ~RealTimeRTC(); 
+public:
+	static RealTimeRTC* create();
+	virtual ~RealTimeRTC();
 
-	protected:
-		virtual float doSync(const EmuTime &time);  
-		virtual void resync();
-		
-	private:
-		// Must be a power of two, <= 8192.
-		static const int RTC_HERTZ = 1024;
-		
-		RealTimeRTC();
-		bool init();
-		int readRTC();
-		int nonBlockReadRTC();
-		void reset(const EmuTime &time);
-		
-		int rtcFd;
-		bool resyncFlag;
-		int overslept;
-		int prevOverslept;
-		EmuTimeFreq<RTC_HERTZ> emuRef;
+protected:
+	virtual float doSync(const EmuTime& time);
+	virtual void resync();
+
+private:
+	// Must be a power of two, <= 8192.
+	static const int RTC_HERTZ = 1024;
+
+	RealTimeRTC();
+	bool init();
+	int readRTC();
+	int nonBlockReadRTC();
+	void reset(const EmuTime& time);
+
+	int rtcFd;
+	bool resyncFlag;
+	int overslept;
+	int prevOverslept;
+	EmuTimeFreq<RTC_HERTZ> emuRef;
 };
 
 } // namespace openmsx
 
 #endif // HAVE_LINUX_RTC_H
 
-#endif // __MSXREALTIMERTC_HH__
+#endif // __REALTIMERTC_HH__
