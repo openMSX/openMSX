f94765946efbb2178e4ce4d29e372061a5bcd811
Maarten ter Huurne <maarten@treewalker.org>
2003-12-30 16:18:51 +0000

    ICC should not have libstdc++ in its library list, or openMSX will crash on startup.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@2868 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- platform-x86-linux-icc.mk
+++ platform-x86-linux-icc.mk
@@ -15,7 +15,7 @@ OPENMSX_CXX?=icc
 EXEEXT:=
 
 # Libraries that do not have a lib-config script.
-LIBS_PLAIN:=stdc++ SDL_image GL z
+LIBS_PLAIN:=SDL_image GL z
 # Libraries that have a lib-config script.
 LIBS_CONFIG:=xml2 sdl
 
