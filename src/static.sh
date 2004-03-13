fd33e67c605925338240b89f64a01255407bda57
Wouter Vermaelen <m9710797@users.sourceforge.net>
2004-03-13 18:34:36 +0000

    removed ancient static.sh file
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3055 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# make a completely static binary
-
-c++ -Wall -DDEBUG -g -pipe -g -O2 -o s_openmsx \
- `ls *.o` \
- -Xlinker -static \
- -L/usr/lib libxmlx/.libs/libxmlx.a -lxml2 -lz -L/lib -lSDL -lm -L/usr/X11R6/lib -lX11 -lXext -lXxf86vm -lXxf86dga -lXv -ldl -lpthread
