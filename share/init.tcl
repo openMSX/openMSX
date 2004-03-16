107c46d9ee0745a952569e5c6a814c78ddae4e25
Maarten ter Huurne <maarten@treewalker.org>
2004-03-16 20:15:51 +0000

    Removed "bind_helper" because it is no longer needed now that the "bind" command redefines an existing key instead of keeping a list of commands per key.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3066 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/init.tcl
+++ share/init.tcl
@@ -1,17 +1,13 @@
+# $Id$
+
 # Default key bindings
-proc bind_helper {key command} {
-	# only bind when not already bound
-	if {[bind $key] == ""} {
-		bind $key $command
-	}
-}
-bind_helper print "screenshot"
-bind_helper pause "toggle pause"
-bind_helper ctrl+pause "quit"
-bind_helper F9 "toggle throttle"
-bind_helper F10 "toggle console"
-bind_helper F11 "toggle mute"
-bind_helper F12 "toggle fullscreen"
+bind print "screenshot"
+bind pause "toggle pause"
+bind ctrl+pause "quit"
+bind F9 "toggle throttle"
+bind F10 "toggle console"
+bind F11 "toggle mute"
+bind F12 "toggle fullscreen"
 
 # Backwards compatibility commands
 proc decr { var { num 1 } } {
@@ -27,13 +23,13 @@ proc alias { cmd body } {
 # Resolve data files. First try user directory, if the file doesn't exist
 # there try the system direcectory
 proc data_file { file } {
-        global env
-        set user_file $env(OPENMSX_USER_DATA)/$file
-        if [file exists $user_file] { return $user_file }
-        return $env(OPENMSX_SYSTEM_DATA)/$file
+	global env
+	set user_file $env(OPENMSX_USER_DATA)/$file
+	if [file exists $user_file] { return $user_file }
+	return $env(OPENMSX_SYSTEM_DATA)/$file
 }
 
-# source additional scripts
+# Source additional scripts
 source [data_file scripts/multi_screenshot.tcl]
 
 # Disable build-in firmware
