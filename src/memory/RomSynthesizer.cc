c922a51f09c0f174fd7068016b56df2509c79001
Wouter Vermaelen <m9710797@users.sourceforge.net>
2004-04-03 07:47:45 +0000

    implemented mirroring
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3091 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/RomSynthesizer.cc
+++ src/memory/RomSynthesizer.cc
@@ -1,5 +1,23 @@
 // $Id$
 
+/* On Sat, 3 Apr 2004, Manuel Pazos wrote:
+ *
+ * As you know, the cartridge has an 8bit D/A, accessed through
+ * memory-mapped at address #4000 by the program. OpenMSX also uses that
+ * address to access it. But examining the cartridge board I found that the
+ * address is decoded by a LS138 in this way:
+ *
+ *    /WR = L
+ *    A15 = L
+ *    A4 = L
+ *    /MERQ = L
+ *    /SLT = L
+ *    A14 = H
+ *
+ * So any value equal to %01xxxxxxxxx0xxxx should work (i.e.: #4000, #4020,
+ * #7C00, etc.)
+*/
+
 #include "RomSynthesizer.hh"
 #include "Config.hh"
 #include "DACSound8U.hh"
@@ -34,14 +52,14 @@ void RomSynthesizer::reset(const EmuTime& time)
 
 void RomSynthesizer::writeMem(word address, byte value, const EmuTime& time)
 {
-	if (address == 0x4000) {
+	if ((address & 0xC010) == 0x4000) {
 		dac->writeDAC(value, time);
 	}
 }
 
 byte* RomSynthesizer::getWriteCacheLine(word address) const
 {
-	if (address == (0x4000 & CPU::CACHE_LINE_SIZE)) {
+	if ((address & 0xC010 & CPU::CACHE_LINE_HIGH) == 0x4000) {
 		return NULL;
 	} else {
 		return unmappedWrite;
