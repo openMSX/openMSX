1dab9363bcadc537f3a98a9465491e881f8fd9b2
Maarten ter Huurne <maarten@treewalker.org>
2004-04-06 22:38:15 +0000

    No longer useful now that auto* build is removed.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3105 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- config-i686.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-srcdir=`dirname $0`
-
-CXXFLAGS="-O3 -mcpu=pentiumpro -march=pentiumpro -ffast-math -funroll-loops -I/usr/include/libxml2/libxml"
-
-echo Configuring for compile with: ${CXXFLAGS}
-rm -f config.cache
-CXXFLAGS=${CXXFLAGS} ${srcdir}/configure --without-pic $*
