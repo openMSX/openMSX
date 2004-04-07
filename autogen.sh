379004586bdceaef7ff2d6845ec42f3f289f4f45
Maarten ter Huurne <maarten@treewalker.org>
2004-04-07 23:44:26 +0000

    Put all output in "derived" dir. Also no longer a need to call automake.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3119 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-
 # $Id$
 
-# Generate configure script
+INDIR=${PWD}/build
+OUTDIR=${PWD}/derived/autotools
+
+mkdir -p ${OUTDIR}
+
 if ( aclocal --version ) </dev/null > /dev/null 2>&1; then
 	echo "Building macros"
-	aclocal -I m4 || exit
+	cd ${INDIR} ; aclocal --output=${OUTDIR}/aclocal.m4 -I m4 || exit
 else
 	echo "aclocal not found -- aborting"
 	exit
@@ -13,7 +16,7 @@ fi
 
 if ( autoheader --version ) </dev/null > /dev/null 2>&1; then
 	echo "Building config header template"
-	autoheader || exit
+	cd ${OUTDIR} ; autoheader ${INDIR}/configure.ac || exit
 else
 	echo "autoheader not found -- aborting"
 	exit
@@ -21,19 +24,10 @@ fi
 
 if ( autoconf --version ) </dev/null > /dev/null 2>&1; then
 	echo "Building configure"
-	autoconf || exit
+	cd ${OUTDIR} ; autoconf --output=configure ${INDIR}/configure.ac || exit
 else
 	echo "autoconf not found -- aborting"
 	exit
 fi
 
-if ( automake --version ) </dev/null > /dev/null 2>&1; then
-	echo "Building Makefiles"
-	automake --foreign -a || exit
-else
-	echo "automake not found -- aborting"
-	exit
-fi
-
-rm -f config.cache
 echo 'Next step: run "(g)make"'
