d78905cd57d9cbd6fc14231a5bc92cc1f35dc9d3
Maarten ter Huurne <maarten@treewalker.org>
2004-04-12 09:49:07 +0000

    Refactored to C++ style.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3149 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/sha1.cc
+++ src/sha1.cc
@@ -1,31 +1,91 @@
 // $Id$
 
+/*
+Based on:
+100% free public domain implementation of the SHA-1 algorithm
+by Dominik Reichl <Dominik.Reichl@tiscali.de>
+
+Refactored in C++ style as part of openMSX
+by Maarten ter Huurne and Wouter Vermaelen.
+
+=== Test Vectors (from FIPS PUB 180-1) ===
+
+"abc"
+A9993E36 4706816A BA3E2571 7850C26C 9CD0D89D
+
+"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+84983E44 1C3BD26E BAAE4AA1 F95129E5 E54670F1
+
+A million repetitions of "a"
+34AA973C D4C4DAA4 F61EEB2B DBAD2731 6534016F
+*/
+
+#include "sha1.hh"
+#include "config.h"
 #include <cstdio>
 #include <memory.h>
-#include "config.h"
-#include "sha1.hh"
 
 namespace openmsx {
 
-// Rotate x bits to the left
-#define ROL32(value, bits) (((value)<<(bits))|((value)>>(32-(bits))))
+// Rotate x bits to the left.
+inline static uint32 rol32(uint32 value, int bits) {
+	return (value << bits) | (value >> (32 - bits));
+}
 
-#ifdef WORDS_BIGENDIAN
-	#define SHABLK0(i) (block->l[i])
-#else
-	#define SHABLK0(i) (block->l[i] = (ROL32(block->l[i],24) & 0xFF00FF00) \
-		| (ROL32(block->l[i],8) & 0x00FF00FF))
-#endif
+class WorkspaceBlock {
+private:
+	uint32 data[16];
 
-#define SHABLK(i) (block->l[i&15] = ROL32(block->l[(i+13)&15] ^ block->l[(i+8)&15] \
-	^ block->l[(i+2)&15] ^ block->l[i&15],1))
+	uint32 next0(const int i) {
+		if (OPENMSX_BIGENDIAN) {
+			return data[i];
+		} else {
+			return data[i] =
+				  (rol32(data[i], 24) & 0xFF00FF00)
+				| (rol32(data[i],  8) & 0x00FF00FF);
+		}
+	}
+	uint32 next(const int i) {
+		return data[i & 15] = rol32(
+			data[(i + 13) & 15] ^ data[(i + 8) & 15] ^
+			data[(i +  2) & 15] ^ data[ i      & 15]
+			, 1);
+	}
 
-// SHA-1 rounds
-#define R0(v,w,x,y,z,i) { z+=((w&(x^y))^y)+SHABLK0(i)+0x5A827999+ROL32(v,5); w=ROL32(w,30); }
-#define R1(v,w,x,y,z,i) { z+=((w&(x^y))^y)+SHABLK(i)+0x5A827999+ROL32(v,5); w=ROL32(w,30); }
-#define R2(v,w,x,y,z,i) { z+=(w^x^y)+SHABLK(i)+0x6ED9EBA1+ROL32(v,5); w=ROL32(w,30); }
-#define R3(v,w,x,y,z,i) { z+=(((w|x)&y)|(w&x))+SHABLK(i)+0x8F1BBCDC+ROL32(v,5); w=ROL32(w,30); }
-#define R4(v,w,x,y,z,i) { z+=(w^x^y)+SHABLK(i)+0xCA62C1D6+ROL32(v,5); w=ROL32(w,30); }
+public:
+	WorkspaceBlock(const byte buffer[64]);
+
+	// SHA-1 rounds
+	void r0(const uint32 v, uint32 &w, const uint32 x, const uint32 y,
+			uint32 &z, const int i) {
+		z += ((w & (x ^ y)) ^ y) + next0(i) + 0x5A827999 + rol32(v, 5);
+		w = rol32(w, 30);
+	}
+	void r1(const uint32 v, uint32 &w, const uint32 x, const uint32 y,
+			uint32 &z, const int i) {
+		z += ((w & (x ^ y)) ^ y) + next(i) + 0x5A827999 + rol32(v, 5);
+		w = rol32(w, 30);
+	}
+	void r2(const uint32 v, uint32 &w, const uint32 x, const uint32 y,
+			uint32 &z, const int i) {
+		z += (w ^ x ^ y) + next(i) + 0x6ED9EBA1 + rol32(v, 5);
+		w = rol32(w, 30);
+	}
+	void r3(const uint32 v, uint32 &w, const uint32 x, const uint32 y,
+			uint32 &z, const int i) {
+		z += (((w | x) & y) | (w & x)) + next(i) + 0x8F1BBCDC + rol32(v, 5);
+		w = rol32(w, 30);
+	}
+	void r4(const uint32 v, uint32 &w, const uint32 x, const uint32 y,
+			uint32 &z, const int i) {
+		z += (w ^ x ^ y) + next(i) + 0xCA62C1D6 + rol32(v, 5);
+		w = rol32(w, 30);
+	}
+};
+
+WorkspaceBlock::WorkspaceBlock(const byte buffer[64]) {
+	memcpy(data, buffer, 64);
+}
 
 SHA1::SHA1()
 {
@@ -45,70 +105,75 @@ void SHA1::reset()
 	m_state[3] = 0x10325476;
 	m_state[4] = 0xC3D2E1F0;
 
-	m_count[0] = 0;
-	m_count[1] = 0;
+	m_count = 0;
 }
 
-void SHA1::transform(unsigned long state[5], const unsigned char buffer[64])
+void SHA1::transform(const byte buffer[64])
 {
-	SHA1_WORKSPACE_BLOCK* block;
-	static unsigned char workspace[64];
-	block = (SHA1_WORKSPACE_BLOCK*)workspace;
-	memcpy(block, buffer, 64);
+	WorkspaceBlock block(buffer);
 
-	// Copy state[] to working vars
-	unsigned long a = state[0];
-	unsigned long b = state[1];
-	unsigned long c = state[2];
-	unsigned long d = state[3];
-	unsigned long e = state[4];
+	// Copy m_state[] to working vars.
+	uint32 a = m_state[0];
+	uint32 b = m_state[1];
+	uint32 c = m_state[2];
+	uint32 d = m_state[3];
+	uint32 e = m_state[4];
 
 	// 4 rounds of 20 operations each. Loop unrolled.
-	R0(a,b,c,d,e, 0); R0(e,a,b,c,d, 1); R0(d,e,a,b,c, 2); R0(c,d,e,a,b, 3);
-	R0(b,c,d,e,a, 4); R0(a,b,c,d,e, 5); R0(e,a,b,c,d, 6); R0(d,e,a,b,c, 7);
-	R0(c,d,e,a,b, 8); R0(b,c,d,e,a, 9); R0(a,b,c,d,e,10); R0(e,a,b,c,d,11);
-	R0(d,e,a,b,c,12); R0(c,d,e,a,b,13); R0(b,c,d,e,a,14); R0(a,b,c,d,e,15);
-	R1(e,a,b,c,d,16); R1(d,e,a,b,c,17); R1(c,d,e,a,b,18); R1(b,c,d,e,a,19);
-	R2(a,b,c,d,e,20); R2(e,a,b,c,d,21); R2(d,e,a,b,c,22); R2(c,d,e,a,b,23);
-	R2(b,c,d,e,a,24); R2(a,b,c,d,e,25); R2(e,a,b,c,d,26); R2(d,e,a,b,c,27);
-	R2(c,d,e,a,b,28); R2(b,c,d,e,a,29); R2(a,b,c,d,e,30); R2(e,a,b,c,d,31);
-	R2(d,e,a,b,c,32); R2(c,d,e,a,b,33); R2(b,c,d,e,a,34); R2(a,b,c,d,e,35);
-	R2(e,a,b,c,d,36); R2(d,e,a,b,c,37); R2(c,d,e,a,b,38); R2(b,c,d,e,a,39);
-	R3(a,b,c,d,e,40); R3(e,a,b,c,d,41); R3(d,e,a,b,c,42); R3(c,d,e,a,b,43);
-	R3(b,c,d,e,a,44); R3(a,b,c,d,e,45); R3(e,a,b,c,d,46); R3(d,e,a,b,c,47);
-	R3(c,d,e,a,b,48); R3(b,c,d,e,a,49); R3(a,b,c,d,e,50); R3(e,a,b,c,d,51);
-	R3(d,e,a,b,c,52); R3(c,d,e,a,b,53); R3(b,c,d,e,a,54); R3(a,b,c,d,e,55);
-	R3(e,a,b,c,d,56); R3(d,e,a,b,c,57); R3(c,d,e,a,b,58); R3(b,c,d,e,a,59);
-	R4(a,b,c,d,e,60); R4(e,a,b,c,d,61); R4(d,e,a,b,c,62); R4(c,d,e,a,b,63);
-	R4(b,c,d,e,a,64); R4(a,b,c,d,e,65); R4(e,a,b,c,d,66); R4(d,e,a,b,c,67);
-	R4(c,d,e,a,b,68); R4(b,c,d,e,a,69); R4(a,b,c,d,e,70); R4(e,a,b,c,d,71);
-	R4(d,e,a,b,c,72); R4(c,d,e,a,b,73); R4(b,c,d,e,a,74); R4(a,b,c,d,e,75);
-	R4(e,a,b,c,d,76); R4(d,e,a,b,c,77); R4(c,d,e,a,b,78); R4(b,c,d,e,a,79);
+	block.r0(a,b,c,d,e, 0); block.r0(e,a,b,c,d, 1); block.r0(d,e,a,b,c, 2);
+	block.r0(c,d,e,a,b, 3); block.r0(b,c,d,e,a, 4);
+	block.r0(a,b,c,d,e, 5); block.r0(e,a,b,c,d, 6); block.r0(d,e,a,b,c, 7);
+	block.r0(c,d,e,a,b, 8); block.r0(b,c,d,e,a, 9);
+	block.r0(a,b,c,d,e,10); block.r0(e,a,b,c,d,11); block.r0(d,e,a,b,c,12);
+	block.r0(c,d,e,a,b,13); block.r0(b,c,d,e,a,14);
+	block.r0(a,b,c,d,e,15); block.r1(e,a,b,c,d,16); block.r1(d,e,a,b,c,17);
+	block.r1(c,d,e,a,b,18); block.r1(b,c,d,e,a,19);
+	block.r2(a,b,c,d,e,20); block.r2(e,a,b,c,d,21); block.r2(d,e,a,b,c,22);
+	block.r2(c,d,e,a,b,23); block.r2(b,c,d,e,a,24);
+	block.r2(a,b,c,d,e,25); block.r2(e,a,b,c,d,26); block.r2(d,e,a,b,c,27);
+	block.r2(c,d,e,a,b,28); block.r2(b,c,d,e,a,29);
+	block.r2(a,b,c,d,e,30); block.r2(e,a,b,c,d,31); block.r2(d,e,a,b,c,32);
+	block.r2(c,d,e,a,b,33); block.r2(b,c,d,e,a,34);
+	block.r2(a,b,c,d,e,35); block.r2(e,a,b,c,d,36); block.r2(d,e,a,b,c,37);
+	block.r2(c,d,e,a,b,38); block.r2(b,c,d,e,a,39);
+	block.r3(a,b,c,d,e,40); block.r3(e,a,b,c,d,41); block.r3(d,e,a,b,c,42);
+	block.r3(c,d,e,a,b,43); block.r3(b,c,d,e,a,44);
+	block.r3(a,b,c,d,e,45); block.r3(e,a,b,c,d,46); block.r3(d,e,a,b,c,47);
+	block.r3(c,d,e,a,b,48); block.r3(b,c,d,e,a,49);
+	block.r3(a,b,c,d,e,50); block.r3(e,a,b,c,d,51); block.r3(d,e,a,b,c,52);
+	block.r3(c,d,e,a,b,53); block.r3(b,c,d,e,a,54);
+	block.r3(a,b,c,d,e,55); block.r3(e,a,b,c,d,56); block.r3(d,e,a,b,c,57);
+	block.r3(c,d,e,a,b,58); block.r3(b,c,d,e,a,59);
+	block.r4(a,b,c,d,e,60); block.r4(e,a,b,c,d,61); block.r4(d,e,a,b,c,62);
+	block.r4(c,d,e,a,b,63); block.r4(b,c,d,e,a,64);
+	block.r4(a,b,c,d,e,65); block.r4(e,a,b,c,d,66); block.r4(d,e,a,b,c,67);
+	block.r4(c,d,e,a,b,68); block.r4(b,c,d,e,a,69);
+	block.r4(a,b,c,d,e,70); block.r4(e,a,b,c,d,71); block.r4(d,e,a,b,c,72);
+	block.r4(c,d,e,a,b,73); block.r4(b,c,d,e,a,74);
+	block.r4(a,b,c,d,e,75); block.r4(e,a,b,c,d,76); block.r4(d,e,a,b,c,77);
+	block.r4(c,d,e,a,b,78); block.r4(b,c,d,e,a,79);
 
-	// Add the working vars back into state[]
-	state[0] += a;
-	state[1] += b;
-	state[2] += c;
-	state[3] += d;
-	state[4] += e;
+	// Add the working vars back into m_state[].
+	m_state[0] += a;
+	m_state[1] += b;
+	m_state[2] += c;
+	m_state[3] += d;
+	m_state[4] += e;
 }
 
 // Use this function to hash in binary data and strings
-void SHA1::update(const unsigned char* data, unsigned int len)
+void SHA1::update(const byte* data, unsigned int len)
 {
-	unsigned long j = (m_count[0] >> 3) & 63;
+	uint32 j = (m_count >> 3) & 63;
 
-	if ((m_count[0] += len << 3) < (len << 3)) {
-		m_count[1]++;
-	}
-	m_count[1] += (len >> 29);
+	m_count += len << 3;
 
-	unsigned long i;
+	uint32 i;
 	if ((j + len) > 63) {
 		memcpy(&m_buffer[j], data, (i = 64 - j));
-		transform(m_state, m_buffer);
-		for (; i+63 < len; i += 64) {
-			transform(m_state, &data[i]);
+		transform(m_buffer);
+		for (; i + 63 < len; i += 64) {
+			transform(&data[i]);
 		}
 		j = 0;
 	} else {
@@ -119,26 +184,23 @@ void SHA1::update(const unsigned char* data, unsigned int len)
 
 void SHA1::finalize()
 {
-	unsigned char finalcount[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-
+	byte finalcount[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	for (int i = 0; i < 8; i++) {
-		finalcount[i] = (unsigned char)((m_count[(i >= 4 ? 0 : 1)]
-			>> ((3 - (i & 3)) * 8)) & 255); // Endian independent
-	}
-	update((const unsigned char*)"\200", 1);
-
-	while ((m_count[0] & 504) != 448) {
-		update((const unsigned char *)"\0", 1);
+		finalcount[i] = static_cast<byte>(m_count >> ((7 - i) * 8));
 	}
 
-	update(finalcount, 8); // Cause a SHA1Transform()
+	update((const byte*)"\200", 1);
+	while ((m_count & 504) != 448) {
+		update((const byte*)"\0", 1);
+	}
+	update(finalcount, 8); // cause a transform()
 
 	for (int i = 0; i < 20; i++) {
-		m_digest[i] = (unsigned char)((m_state[i >> 2] >> ((3 - (i & 3)) * 8) ) & 255);
+		m_digest[i] = static_cast<byte>(
+			m_state[i >> 2] >> ((3 - (i & 3)) * 8) );
 	}
 }
 
-// Get the final hash as a pre-formatted string
 string SHA1::hex_digest()
 {
 	char s[41];
--- src/sha1.hh
+++ src/sha1.hh
@@ -1,24 +1,9 @@
 // $Id$
 
-/**
- * 100% free public domain implementation of the SHA-1
- * algorithm by Dominik Reichl <Dominik.Reichl@tiscali.de>
- * 
- * === Test Vectors (from FIPS PUB 180-1) ===
- *
- * "abc"
- * A9993E36 4706816A BA3E2571 7850C26C 9CD0D89D
- *
- * "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
- * 84983E44 1C3BD26E BAAE4AA1 F95129E5 E54670F1
- *
- * A million repetitions of "a"
- * 34AA973C D4C4DAA4 F61EEB2B DBAD2731 6534016F
- */
-
 #ifndef __SHA1_HH__
 #define __SHA1_HH__
 
+#include "openmsx.hh"
 #include <string>
 
 using std::string;
@@ -27,34 +12,36 @@ namespace openmsx {
 
 class SHA1
 {
-	public:
-		// Constructor and Destructor
-		SHA1();
-		~SHA1();
+public:
+	SHA1();
+	~SHA1();
 
-		void reset();
+	/** Reset this SHA1 object to compute a new hash.
+	  */
+	void reset();
 
-		// Update the hash value
-		void update(const unsigned char* data, unsigned int len);
+	/** Update the hash value.
+	  */
+	void update(const byte* data, unsigned int len);
 
-		// Finalize hash and report
-		void finalize();
-		string hex_digest();
+	/** Finalize the hash computation.
+	  */
+	void finalize();
 
-	private:
-		// Private SHA-1 transformation
-		void transform(unsigned long state[5],
-		               const unsigned char buffer[64]);
-		
-		unsigned long m_state[5];
-		unsigned long m_count[2];
-		unsigned char m_buffer[64];
-		unsigned char m_digest[20];
+	/** Get the final hash as a pre-formatted string.
+	  */
+	string hex_digest();
 
-		typedef union {
-			unsigned char c[64];
-			unsigned long l[16];
-		} SHA1_WORKSPACE_BLOCK;
+private:
+	/** SHA-1 transformation.
+	  */
+	void transform(const byte buffer[64]);
+	
+	uint32 m_state[5];
+	uint64 m_count;
+	byte m_buffer[64];
+	byte m_digest[20];
+
 };
 
 } // namespace openmsx
