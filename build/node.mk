b1c0e89bcd49d98297a3970ad8f6570af6234a85
Maarten ter Huurne <maarten@treewalker.org>
2004-04-18 23:29:04 +0000

    Dropped autoconf.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3185 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -4,8 +4,8 @@ include build/node-start.mk
 
 DIST:= \
 	config.guess detectsys.sh tcl-search.sh \
-	configure.ac \
 	main.mk node-end.mk node-start.mk \
+	info2code.mk probe.mk \
 	flavour-*.mk platform-*.mk
 
 include build/node-end.mk
