73235b2946c7231dd3561132c11cb670f3496bcf
Maarten ter Huurne <maarten@treewalker.org>
2004-04-24 03:22:04 +0000

    Added "components.mk". Also added "probe-results.mk" (forgot to do this two days ago).
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3205 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -5,7 +5,7 @@ include build/node-start.mk
 DIST:= \
 	config.guess detectsys.sh tcl-search.sh \
 	main.mk node-end.mk node-start.mk \
-	info2code.mk probe.mk \
+	info2code.mk components.mk probe.mk probe-results.mk \
 	flavour-*.mk platform-*.mk
 
 include build/node-end.mk
