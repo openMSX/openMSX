e159d2d7d75495f68f55978a1354c088302f52ed
Maarten ter Huurne <maarten@treewalker.org>
2004-04-28 23:07:26 +0000

    Use "platform-x86-freebsd4.mk" as a base and only override what is different.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3228 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-x86-freebsd5.mk
+++ build/platform-x86-freebsd5.mk
@@ -2,30 +2,8 @@
 #
 # Configuration for FreeBSD 5.x on x86 machines.
 
-# Does platform support symlinks?
-USE_SYMLINK:=true
-
-# Default build flavour.
-OPENMSX_FLAVOUR?=i686
-
 # Default compiler.
 OPENMSX_CXX?=g++
 
-# File name extension of executables.
-EXEEXT:=
+include build/platform-x86-freebsd4.mk
 
-LINK_FLAGS+=-L/usr/X11R6/lib
-
-
-# Probe Overrides
-# ===============
-
-SDL_CFLAGS:=`sdl11-config --cflags 2>> $(LOG)`
-
-SDL_LDFLAGS:=`sdl11-config --libs 2>> $(LOG)`
-SDL_RESULT:=`sdl11-config --version`
-
-PNG_CFLAGS:=
-
-PNG_LDFLAGS:=-lpng
-PNG_RESULT:=yes
