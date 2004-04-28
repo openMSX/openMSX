b076f8945e4db9f2bff844cd06ff2bd1ba563078
Maarten ter Huurne <maarten@treewalker.org>
2004-04-28 23:06:15 +0000

    Use "platform-x86-linux.mk" as a base and only override what is different.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3227 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-x86-linux-icc.mk
+++ build/platform-x86-linux-icc.mk
@@ -2,15 +2,7 @@
 #
 # Configuration for Linux on x86 machines.
 
-# Does platform support symlinks?
-USE_SYMLINK:=true
-
-# Default build flavour.
-OPENMSX_FLAVOUR?=i686
-
 # Default compiler.
 OPENMSX_CXX?=icc
 
-# File name extension of executables.
-EXEEXT:=
-
+include build/platform-x86-linux.mk
