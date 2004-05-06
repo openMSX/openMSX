dfe99e965c05e01d0c8c0e9229a26bc7c744d371
Maarten ter Huurne <maarten@treewalker.org>
2004-05-06 00:17:24 +0000

    Put customisable stuff into separate Makefile.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3266 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/custom.mk
@@ -0,0 +1,10 @@
+# $Id$
+#
+# Customize openMSX to your system/preferences.
+
+# Directory to install to.
+# openMSX is always installed into a single self-contained directory.
+# But you can change that directory to for example /usr/local/openMSX
+# or /usr/games/openMSX if you like.
+INSTALL_BASE:=/opt/openMSX
+
