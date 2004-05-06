47e7812420180978cda266a9b21421c374c95d75
Maarten ter Huurne <maarten@treewalker.org>
2004-05-06 00:17:02 +0000

    Put version info into separate Makefile.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3265 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/version.mk
@@ -0,0 +1,12 @@
+# $Id$
+#
+# Version info.
+
+# Name used for packaging.
+PACKAGE_NAME:=openmsx
+
+# Version number.
+PACKAGE_VERSION:=0.3.4
+
+# Is this a release version ("true") or development version ("false").
+RELEASE_FLAG:=false
