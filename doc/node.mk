cca80bac3bbe0893fb7b01af0e91705fee3e3110
Maarten ter Huurne <maarten@treewalker.org>
2004-05-23 21:22:25 +0000

    Forgot to package the after-bussum-FAQ. Also made sure such a mistake cannot happen again.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3323 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- doc/node.mk
+++ doc/node.mk
@@ -4,20 +4,18 @@ include build/node-start.mk
 
 SUBDIRS:=manual
 
-DIST:= \
-	release-notes.txt release-history.txt \
-	commands.txt debugdevice.txt \
-	developersFAQ.txt testcases.txt \
-	exampleconfigs.xml \
-	vram-addressing.txt \
-	openmsx.tex MSX-cassette.dia \
-	msxinfo-article.html schema1.png schema2.png screenshot.png
-
-include build/node-end.mk
-
 # Not all of the docs are useful for end users, either because they are about
 # the implementation, or because they are too rough/unfinished.
 # Below is the list of docs that should be included in installation.
 INSTALL_DOCS:= \
 	release-notes.txt release-history.txt \
 	after-bussum-FAQ.txt commands.txt exampleconfigs.xml
+
+DIST:=$(INSTALL_DOCS) \
+	debugdevice.txt \
+	developersFAQ.txt testcases.txt \
+	vram-addressing.txt \
+	openmsx.tex MSX-cassette.dia \
+	msxinfo-article.html schema1.png schema2.png screenshot.png
+
+include build/node-end.mk
