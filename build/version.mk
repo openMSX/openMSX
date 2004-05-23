59d52cdf5d2ac84140aa0021e56d12fc5e8125e0
Maarten ter Huurne <maarten@treewalker.org>
2004-05-23 20:50:44 +0000

    Updated for 0.4.0 release.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3321 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.mk
+++ build/version.mk
@@ -6,7 +6,7 @@
 PACKAGE_NAME:=openmsx
 
 # Version number.
-PACKAGE_VERSION:=0.3.4
+PACKAGE_VERSION:=0.4.0
 
 # Is this a release version ("true") or development version ("false").
-RELEASE_FLAG:=false
+RELEASE_FLAG:=true
