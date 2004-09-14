25663569c8f302a9a29f363485f62b2c8ba3cd65
Maarten ter Huurne <maarten@treewalker.org>
2004-09-14 01:45:20 +0000

    Specify the right GL include and link flags for Apple's GL implementation.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3611 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-darwin.mk
+++ build/platform-darwin.mk
@@ -17,3 +17,9 @@ EXEEXT:=
 MMAP_PREHEADER:=<sys/types.h>
 SYS_MMAN_PREHEADER:=<sys/types.h>
 SYS_SOCKET_PREHEADER:=<sys/types.h>
+# TODO:
+# GL_HEADER:=<OpenGL/gl.h> iso GL_CFLAGS is cleaner,
+# but we have to modify the build before we can use it.
+GL_CFLAGS:=-I/System/Library/Frameworks/OpenGL.framework/Headers
+GL_LDFLAGS:=-framework OpenGL -lGL \
+	-L/System/Library/Frameworks/OpenGL.framework/Libraries
