c4f936ffd73729ec54e9392b36220c2c3b18a5ae
Maarten ter Huurne <maarten@treewalker.org>
2004-09-15 23:23:04 +0000

    Added new type of event listener (DETACHED).
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3617 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/events/EventDistributor.cc
+++ src/events/EventDistributor.cc
@@ -39,17 +39,33 @@ EventDistributor& EventDistributor::instance()
 	return oneInstance;
 }
 
+EventDistributor::ListenerMap& EventDistributor::getListeners(
+	ListenerType listenerType )
+{
+	switch(listenerType) {
+	case NATIVE:
+		return nativeListeners;
+	case DETACHED:
+		return detachedListeners;
+	case EMU:
+		return emuListeners;
+	default:
+		assert(false);
+		return detachedListeners; // any ListenerMap will do
+	}
+}
+
 void EventDistributor::registerEventListener(
 	EventType type, EventListener& listener, ListenerType listenerType)
 {
-	ListenerMap &map = (listenerType == EMU) ? emuListeners : nativeListeners;
+	ListenerMap& map = getListeners(listenerType);
 	map.insert(ListenerMap::value_type(type, &listener));
 }
 
 void EventDistributor::unregisterEventListener(
 	EventType type, EventListener& listener, ListenerType listenerType)
 {
-	ListenerMap &map = (listenerType == EMU) ? emuListeners : nativeListeners;
+	ListenerMap& map = getListeners(listenerType);
 	pair<ListenerMap::iterator, ListenerMap::iterator> bounds =
 		map.equal_range(type);
 	for (ListenerMap::iterator it = bounds.first;
@@ -64,6 +80,10 @@ void EventDistributor::unregisterEventListener(
 void EventDistributor::distributeEvent(Event* event)
 {
 	assert(event);
+	// TODO: Use semaphores to guard access to the maps.
+	//       This is needed because this method can be called from any thread.
+
+	// Deliver event to NATIVE listeners.
 	bool cont = true;
 	pair<ListenerMap::iterator, ListenerMap::iterator> bounds =
 		nativeListeners.equal_range(event->getType());
@@ -72,11 +92,29 @@ void EventDistributor::distributeEvent(Event* event)
 		cont &= it->second->signalEvent(*event);
 	}
 	if (!cont) {
+		// Block event from other listeners.
 		delete event;
-	} else {
-		toBeScheduledEvents.push_back(
-			EventTime(event, Timer::getTime()));
+		return;
 	}
+
+	// Deliver event to DETACHED listeners.
+	// TODO: Is there an easier way to search a map?
+	// TODO: Is it guaranteed that the Nth entry in scheduledEvents always
+	//       corresponds to the Nth call to executeUntil?
+	pair<ListenerMap::iterator, ListenerMap::iterator> bounds2 =
+		detachedListeners.equal_range(event->getType());
+	if (bounds2.first != bounds2.second) {
+		scheduledEvents.push_front(event);
+		scheduler.setAsyncPoint(this, DETACHED);
+		// TODO: We cannot deliver the event to an EMU listener as well,
+		//       because the object will be deleted after the DETACHED
+		//       listener gets it.
+		//       If this is acceptable behaviour, document it.
+		return;
+	}
+
+	// Deliver event to EMU listeners.
+	toBeScheduledEvents.push_back(EventTime(event, Timer::getTime()));
 }
 
 void EventDistributor::sync(const EmuTime& emuTime)
@@ -99,7 +137,7 @@ void EventDistributor::sync(const EmuTime& emuTime)
 			//PRT_DEBUG("input delay too short");
 			schedTime = emuTime;
 		}
-		scheduler.setSyncPoint(schedTime, this);
+		scheduler.setSyncPoint(schedTime, this, EMU);
 	}
 	toBeScheduledEvents.clear();
 	
@@ -107,13 +145,13 @@ void EventDistributor::sync(const EmuTime& emuTime)
 	prevEmu = emuTime;
 }
 
-void EventDistributor::executeUntil(const EmuTime& /*time*/, int /*userData*/)
+void EventDistributor::executeUntil(const EmuTime& /*time*/, int userData)
 {
+	ListenerMap& listeners = getListeners(static_cast<ListenerType>(userData));
 	Event* event = scheduledEvents.front();
 	scheduledEvents.pop_front();
-
 	pair<ListenerMap::iterator, ListenerMap::iterator> bounds =
-		emuListeners.equal_range(event->getType());
+		listeners.equal_range(event->getType());
 	for (ListenerMap::iterator it = bounds.first;
 	     it != bounds.second; ++it) {
 		it->second->signalEvent(*event);
--- src/events/EventDistributor.hh
+++ src/events/EventDistributor.hh
@@ -26,21 +26,41 @@ class EventDistributor : private Schedulable
 public:
 	static EventDistributor& instance();
 
+	/** Ways in which an event can be delivered to a listener.
+	  */
+	enum ListenerType {
+		/** Deliver the event immediately, in the same thread that posted it.
+		  * A native event handler can block the event from being processed
+		  * by other listener types.
+		  * Low overhead, but watch out for threading issues and do not
+		  * unregister listeners in the callback.
+		  */
+		NATIVE,
+		/** Deliver the event immediately, in a separate thread.
+		  * A bit of overhead, but safe.
+		  */
+		DETACHED,
+		/** Deliver the event at the EmuTime equivalent of the current real
+		  * time, in a separate thread.
+		  * Typically used by MSX input devices.
+		  */
+		EMU,
+	};
+
 	/**
-	 * Use this method to (un)register a given class to receive certain
-	 * events.
-	 * @param type The type of the events you want to receive
-	 * @param listener Object that will be notified when the events arrives
-	 * @param listenerType Is the event ment for the MSX or just to control
-	 *        openMSX (console for example). Native events are delivered
-	 *        immediatly, emu events can get queued. Native event listeners
-	 *        can block events for emu event listeners.
-	 * The delivery of the event is done by the 'main-emulation-thread',
-	 * so there is no need for extra synchronization.
+	 * Registers a given object to receive certain events.
+	 * @param type The type of the events you want to receive.
+	 * @param listener Listener that will be notified when an event arrives.
+	 * @param listenerType The way this event should be delivered.
 	 */
-	enum ListenerType { EMU, NATIVE };
 	void   registerEventListener(EventType type, EventListener& listener,
 	                             ListenerType listenerType = EMU);
+	/**
+	 * Unregisters a previously registered event listener.
+	 * @param type The type of the events the listener should no longer receive.
+	 * @param listener Listener to unregister.
+	 * @param listenerType Delivery method the listener was registered with.
+	 */
 	void unregisterEventListener(EventType type, EventListener& listener,
 	                             ListenerType listenerType = EMU);
 
@@ -57,7 +77,9 @@ private:
 
 	typedef multimap<EventType, EventListener*> ListenerMap;
 	ListenerMap nativeListeners;
+	ListenerMap detachedListeners;
 	ListenerMap emuListeners;
+	ListenerMap& getListeners(ListenerType listenerType);
 
 	struct EventTime {
 		EventTime(Event* event_, unsigned long long time_)
