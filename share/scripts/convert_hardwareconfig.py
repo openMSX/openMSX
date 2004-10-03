1728d4a9c1bd5691b2400026d9060940529278cc
Maarten ter Huurne <maarten@treewalker.org>
2004-10-03 02:51:49 +0000

    Skip configs that are already in the new format (checks system ID in doctype declaration). Rename <config> and <device> tags to the config/device type. And a few other conversions added.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3704 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/convert_hardwareconfig.py
+++ share/scripts/convert_hardwareconfig.py
@@ -51,7 +51,15 @@ ioAddresses = {
 	'MegaRam': [ (0x8E, 1, 'IO') ],
 	'HBI55': [ (0xB0, 4, 'IO') ],
 	'DebugDevice': [ (0x2E, 2, 'O') ], # can be any (free) port
-	
+
+	# FDCs:
+	'MB8877A': [],
+	'TC8566AF': [],
+	'WD2793': [],
+	# TODO: Microsol FDC uses IO ports (0xd0 - 0xd4  5(!) ports), but I doubt
+	#       this FDC really works in openMSX.
+	'Microsol': [],
+
 	# Devices without I/O addresses:
 	'ROM': [],
 	'RAM': [],
@@ -60,9 +68,6 @@ ioAddresses = {
 	'SunriseIDE': [],
 	'PAC': [],
 	'SCC+': [],
-	# TODO: Microsol FDC uses IO ports (0xd0 - 0xd4  5(!) ports), but I doubt
-	#       this FDC really works in openMSX.
-	'FDC': [],
 	
 	# Switched devices (share ports 0x40-0x4F):
 	'Matsushita': [],
@@ -82,9 +87,28 @@ def getParameter(node, name):
 			return getText(child)
 	return None
 
+def renameElement(node, name):
+	newNode = node.ownerDocument.createElement(name)
+	for index in range(node.attributes.length):
+		attribute = node.attributes.item(index)
+		newNode.setAttribute(attribute.name, attribute.nodeValue)
+	for child in list(node.childNodes):
+		node.removeChild(child)
+		newNode.appendChild(child)
+	node.parentNode.replaceChild(newNode, node)
+	node.unlink()
+	return newNode
+
 def convertDoc(dom):
 	assert dom.nodeType == dom.DOCUMENT_NODE
-	
+	if dom.doctype.systemId == 'msxconfig2.dtd':
+		print '  already in new format, skipping'
+		return
+	elif dom.doctype.systemId != 'msxconfig.dtd':
+		print '  unknown format, skipping'
+		return
+	dom.doctype.systemId = 'msxconfig2.dtd'
+
 	def isCVSId(node):
 		if node.nodeType != dom.COMMENT_NODE: return False
 		if not node.nodeValue.startswith(' $Id'): return False
@@ -118,15 +142,15 @@ def convertDoc(dom):
 	convertRoot(dom.childNodes[2])
 
 def convertRoot(node):
-	for child in node.childNodes:
+	for child in list(node.childNodes):
 		if child.nodeType == node.ELEMENT_NODE:
 			if child.nodeName == 'config':
 				convertConfig(child)
 			elif child.nodeName == 'device':
 				convertDevice(child)
-			else:
-				print 'cannot handle element at top level:', child.nodeName
-				assert False
+			elif child.nodeName != 'info':
+				print '  NOTE: ignoring new-style element at top level:', \
+					child.nodeName
 		elif child.nodeType == node.TEXT_NODE:
 			pass
 		elif child.nodeType == node.COMMENT_NODE:
@@ -136,11 +160,14 @@ def convertRoot(node):
 			assert False
 
 def convertConfig(node):
-	print '  config:', node.attributes['id'].nodeValue
-	for child in node.childNodes:
+	doc = node.ownerDocument
+	configId = node.attributes['id'].nodeValue
+	print '  config:', configId
+
+	for child in list(node.childNodes):
 		if child.nodeType == node.ELEMENT_NODE:
 			if child.nodeName == 'parameter':
-				convertParameter(child)
+				child = convertParameter(child)
 		elif child.nodeType == node.TEXT_NODE:
 			pass
 		elif child.nodeType == node.COMMENT_NODE:
@@ -149,11 +176,30 @@ def convertConfig(node):
 			print 'cannot handle node inside config:', child
 			assert False
 
+	# Type is now stored in tag name instead of "id" attribute.
+	node.removeAttribute('id')
+	node = renameElement(node, configId)
+
+	if configId == 'MapperType':
+		# Store type in body text instead of "type" tag.
+		newNode = doc.createElement(configId)
+		newNode.appendChild(doc.createTextNode(getParameter(node, 'type')))
+		node.parentNode.replaceChild(newNode, node)
+		node.unlink()
+	elif configId not in [
+		'CassettePort', 'ExternalSlots', 'MotherBoard', 'RenShaTurbo'
+		]:
+		print '    NOTE: conversion of config type "%s" may be wrong' \
+			% configId
+		# TODO: CassettePort: if only whitespace text nodes, then remove them
+		# TODO: MotherBoard: if slot is expanded but empty, it will disappear
+		# TODO: myHD: conversion of IDE HD configuration is not implemented
+
 def convertDevice(node):
 	print '  device:', node.attributes['id'].nodeValue
 	deviceType = getParameter(node, 'type')
 	assert deviceType is not None
-	deviceType = {
+	newDeviceType = {
 		'FM-PAC': 'FMPAC',
 		'Rom': 'ROM',
 		'PanasonicRam': 'PanasonicRAM',
@@ -163,8 +209,11 @@ def convertDevice(node):
 		'MSX-AUDIO MIDI': 'Music Module MIDI',
 		'SCCPlusCart': 'SCC+',
 		'MSX-Midi': 'MSX-MIDI',
-		}.get(deviceType, deviceType)
-	
+		}.get(deviceType)
+	if newDeviceType is not None:
+		print '    rename type "' + deviceType + '" to "' + newDeviceType + '"'
+		deviceType = newDeviceType
+
 	if deviceType == 'CPU':
 		print '    removing CPU device'
 		node.parentNode.removeChild(node)
@@ -172,25 +221,26 @@ def convertDevice(node):
 		return
 	
 	# Convert parameters.
-	for child in node.childNodes:
+	for child in list(node.childNodes):
 		if child.nodeType == node.ELEMENT_NODE:
-			if child.nodeName == 'parameter':
-				convertParameter(child)
-			elif child.nodeName == 'type':
-				oldType = getText(child)
-				if oldType != deviceType:
-					print '    convert "' + oldType + '" ' \
-						'to "' + deviceType + '"'
-					child.childNodes[0].nodeValue = deviceType
+			if child.nodeName == 'type':
+				node.removeChild(child)
+				child.unlink()
 			else:
+				if child.nodeName == 'parameter':
+					child = convertParameter(child)
 				newName = getNewParameterName(deviceType, child.nodeName)
+				if deviceType == 'FDC' and newName == 'fdc_type':
+					deviceType = getText(child)
+					newName = None
 				if newName is None:
 					print '    remove parameter "' + child.nodeName + '"'
 					node.removeChild(child)
+					child.unlink()
 				elif newName != child.nodeName:
 					print '    rename parameter "' + child.nodeName + '" ' \
 						'to "' + newName + '"'
-					child.tagName = newName
+					child = renameElement(child, newName)
 		elif child.nodeType == node.TEXT_NODE:
 			pass
 		elif child.nodeType == node.COMMENT_NODE:
@@ -198,7 +248,10 @@ def convertDevice(node):
 		else:
 			print 'cannot handle node inside device:', child
 			assert False
-	
+
+	# Note: must happen here because fdc_type can rename node.
+	node = renameElement(node, deviceType)
+
 	# Insert I/O addresses.
 	if deviceType == 'VDP':
 		deviceType = {
@@ -241,46 +294,26 @@ def getNewParameterName(deviceType, name):
 
 def convertParameter(node):
 	name = node.attributes['name'].nodeValue
-	if len(node.attributes) == 1:
-		print '    convert parameter:', name
+	node.removeAttribute('name')
+	if len(node.attributes) == 0:
 		parent = node.parentNode
 		deviceType = getParameter(parent, 'type')
-		name = getNewParameterName(deviceType, name)
-		if name is None:
-			parent.removeChild(node)
-		else:
-			# Extract info from old-style node.
-			assert len(node.childNodes) == 1
-			text = node.childNodes[0]
-			node.removeChild(text)
-			# Store info in new-style node.
-			elem = node.ownerDocument.createElement(name)
-			elem.appendChild(text)
-			# Replace old node by new node.
-			parent.replaceChild(elem, node)
-			node.unlink()
+		print '    convert parameter:', name
+		assert len(node.childNodes) == 1
+		return renameElement(node, name)
 	else:
-		assert len(node.attributes) == 2
+		assert len(node.attributes) == 1
 		clazz = node.attributes['class'].nodeValue
+		node.removeAttribute('class')
 		assert clazz == 'subslotted', clazz
 		print '    convert parameter:', clazz, name
 		assert len(node.childNodes) == 1
 		value = node.childNodes[0].nodeValue
 		assert value in [ 'true', 'false' ], value
-		# Store info in new-style node.
-		elem = node.ownerDocument.createElement('slot')
-		elem.setAttribute('num', name)
-		elem.setAttribute('expanded', value)
-		# Replace old node by new node.
-		parent = node.parentNode
-		parent.replaceChild(elem, node)
-		node.unlink()
-	#	print 'param:', node.attributes['name'].nodeValue
-	#	# Fails when value is integer (Python 2.3).
-	#	#for attrib in node.attributes:
-	#	for index in range(node.attributes.length):
-	#		attrib = node.attributes.item(index)
-	#		print attrib.name, '=', attrib.nodeValue
+		node = renameElement(node, 'slot')
+		node.setAttribute('num', name)
+		node.setAttribute('expanded', value)
+		return node
 
 if len(sys.argv) != 3:
 	print 'Usage: convert_hardwareconfig.py <indir> <outdir>'
