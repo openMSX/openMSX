1b975ad454ae63b6c3c841c453018376d6642cb8
Wouter Vermaelen <m9710797@users.sourceforge.net>
2004-12-09 20:04:22 +0000

    forgot to add file
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3908 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/settings/Setting.cc
@@ -0,0 +1,39 @@
+// $Id$
+
+#include "Setting.hh"
+#include "SettingListener.hh"
+#include "CliCommOutput.hh"
+
+namespace openmsx {
+
+Setting::Setting(const string& name_, const string& description_)
+	: name(name_), description(description_)
+{
+}
+
+Setting::~Setting()
+{
+}
+
+void Setting::notify() const
+{
+	for (vector<SettingListener*>::const_iterator it = listeners.begin();
+	     it != listeners.end(); ++it) {
+		(*it)->update(this);
+	}
+	CliCommOutput::instance().update(CliCommOutput::SETTING, getName(),
+	                                 getValueString());
+}
+
+void Setting::addListener(SettingListener* listener)
+{
+	listeners.push_back(listener);
+}
+
+void Setting::removeListener(SettingListener* listener)
+{
+	assert(count(listeners.begin(), listeners.end(), listener) == 1);
+	listeners.erase(find(listeners.begin(), listeners.end(), listener));
+}
+
+} // namespace openmsx
