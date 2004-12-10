2abbc2ba6bddd75128189f4445e0430eb08590dd
Wouter Vermaelen <m9710797@users.sourceforge.net>
2004-12-10 21:51:37 +0000

    allow to configure LED icon settings
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3912 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/IconLayer.cc
+++ src/video/IconLayer.cc
@@ -6,6 +6,7 @@
 #include "FileException.hh"
 #include "Timer.hh"
 #include "SDLImage.hh"
+#include "IntegerSetting.hh"
 #include "components.hh"
 #ifdef COMPONENT_GL
 #include "GLImage.hh"
@@ -25,9 +26,6 @@ static bool init = false;
 static bool ledStatus[LedEvent::NUM_LEDS];
 static unsigned long long ledTime[LedEvent::NUM_LEDS];
 
-static const unsigned long long fadeTime = 5000000;
-static const unsigned long long fadeDuration = 5000000;
-
 template <class IMAGE>
 IconLayer<IMAGE>::IconLayer(SDL_Surface* screen)
 	// Just assume partial coverage and let paint() sort it out.
@@ -43,19 +41,41 @@ IconLayer<IMAGE>::IconLayer(SDL_Surface* screen)
 		}
 		
 	}
-	SystemFileContext context;
-	try {
-		iconOn.reset(new IMAGE(screen, context.resolve("skins/led.png")));
-	} catch (MSXException& e) {
-		// nothing
-	}
-	try {
-		iconOff.reset(new IMAGE(screen, context.resolve("skins/led-off.png")));
-	} catch (MSXException& e) {
-		// nothing
-	}
+
+	createSettings(LedEvent::POWER, "power");
+	createSettings(LedEvent::CAPS,  "caps");
+	createSettings(LedEvent::KANA,  "kana");
+	createSettings(LedEvent::PAUSE, "pause");
+	createSettings(LedEvent::TURBO, "turbo");
+	createSettings(LedEvent::FDD,   "fdd");
+
+	fadeTimeSetting.reset(new IntegerSetting("icon.fade-delay",
+		"Time (in ms) after which the icons start to fade (0 means no fading)",
+		5000, 0, 1000000));
+	fadeDurationSetting.reset(new IntegerSetting("icon.fade-duration",
+		"Time (in ms) it takes for the icons the fade from completely opaque "
+		"to completely transparent", 5000, 0, 1000000));
+	
 	EventDistributor::instance().registerEventListener(LED_EVENT, *this);
 }
+
+template <class IMAGE>
+void IconLayer<IMAGE>::createSettings(LedEvent::Led led, const string& name)
+{
+	string icon_name = "icon." + name;
+	ledInfo[led].xcoord.reset(new IntegerSetting(icon_name + ".xcoord",
+		"X-coordinate for LED icon", ((int)led) * 50, 0, 640));
+	ledInfo[led].ycoord.reset(new IntegerSetting(icon_name + ".ycoord",
+		"Y-coordinate for LED icon", 0, 0, 480));
+	ledInfo[led].active.reset(new FilenameSetting(icon_name + ".active",
+		"Image for active LED icon", "skins/led.png"));
+	ledInfo[led].nonActive.reset(new FilenameSetting(icon_name + ".non-active",
+		"Image for active LED icon", "skins/led-off.png"));
+
+	ledInfo[led].active->setChecker(this);
+	ledInfo[led].nonActive->setChecker(this);
+}
+
 
 template <class IMAGE>
 IconLayer<IMAGE>::~IconLayer()
@@ -66,20 +86,32 @@ IconLayer<IMAGE>::~IconLayer()
 template <class IMAGE>
 void IconLayer<IMAGE>::paint()
 {
+	unsigned long long fadeTime     = 1000 * fadeTimeSetting->getValue();
+	unsigned long long fadeDuration = 1000 * fadeDurationSetting->getValue();
+
 	for (int i = 0; i < LedEvent::NUM_LEDS; ++i) {
 		unsigned long long now = Timer::getTime();
 		unsigned long long diff = now - ledTime[i];
 		byte alpha;
-		if (diff > (fadeTime + fadeDuration)) {
+		if (fadeTime == 0) {
+			// no fading, draw completely opaque
+			alpha = 255;
+		} else if (diff > (fadeTime + fadeDuration)) {
+			// completely faded out, don't even bother to draw
 			continue;
 		} else if (diff < fadeTime) {
+			// no fading yet
 			alpha = 255;
 		} else {
+			// fading out
 			alpha = 255 - (255 * (diff - fadeTime) / fadeDuration);
 		}
-		IMAGE* icon = ledStatus[i] ? iconOn.get() : iconOff.get();
+		IMAGE* icon = ledStatus[i] ? ledInfo[i].iconOn.get()
+		                           : ledInfo[i].iconOff.get();
 		if (icon) {
-			icon->draw(50 * i, 0, alpha);
+			int x = ledInfo[i].xcoord->getValue();
+			int y = ledInfo[i].ycoord->getValue();
+			icon->draw(x, y, alpha);
 		}
 	}
 }
@@ -103,6 +135,25 @@ bool IconLayer<IMAGE>::signalEvent(const Event& event)
 		ledTime[led] = Timer::getTime();
 	}
 	return true;
+}
+
+template <class IMAGE>
+void IconLayer<IMAGE>::check(SettingImpl<FilenameSetting::Policy>& setting,
+                             string& value)
+{
+	string filename = static_cast<FilenameSetting&>(setting).
+		getFileContext().resolve(value);
+
+	for (int i = 0; i < LedEvent::NUM_LEDS; ++i) {
+		if (&setting == ledInfo[i].active.get()) {
+			ledInfo[i].iconOn.reset(new IMAGE(outputScreen, filename));
+			break;
+		}
+		if (&setting == ledInfo[i].nonActive.get()) {
+			ledInfo[i].iconOff.reset(new IMAGE(outputScreen, filename));
+			break;
+		}
+	}
 }
 
 } // namespace openmsx
--- src/video/IconLayer.hh
+++ src/video/IconLayer.hh
@@ -6,14 +6,18 @@
 #include "Display.hh"
 #include "EventListener.hh"
 #include "LedEvent.hh"
+#include "FilenameSetting.hh"
 #include <memory>
 
 class SDL_Surface;
 
 namespace openmsx {
 
+class IntegerSetting;
+
 template <class IMAGE>
-class IconLayer : public Layer, private EventListener
+class IconLayer : public Layer, private EventListener,
+                  private SettingChecker<FilenameSetting::Policy>
 {
 public:
 	IconLayer(SDL_Surface* screen);
@@ -21,15 +25,31 @@ public:
 
 	// Layer interface:
 	virtual void paint();
-	virtual const string& getName();
+	virtual const std::string& getName();
 
 private:
+	void createSettings(LedEvent::Led led, const std::string& name);
+
 	// EventListener interface:
 	virtual bool signalEvent(const Event& event);
 
+	// SettingChecker
+	virtual void check(SettingImpl<FilenameSetting::Policy>& setting,
+	                   std::string& value);
+
 	SDL_Surface* outputScreen;
-	std::auto_ptr<IMAGE> iconOn;
-	std::auto_ptr<IMAGE> iconOff;
+	std::auto_ptr<IntegerSetting> fadeTimeSetting;
+	std::auto_ptr<IntegerSetting> fadeDurationSetting;
+
+	struct LedInfo {
+		std::auto_ptr<IntegerSetting> xcoord;
+		std::auto_ptr<IntegerSetting> ycoord;
+		std::auto_ptr<FilenameSetting> active;
+		std::auto_ptr<FilenameSetting> nonActive;
+		std::auto_ptr<IMAGE> iconOn;
+		std::auto_ptr<IMAGE> iconOff;
+	};
+	LedInfo ledInfo[LedEvent::NUM_LEDS];
 };
 
 class SDLImage;
