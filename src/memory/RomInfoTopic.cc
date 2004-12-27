386cdb36b6c5cb4871ad6d4008072ca3f797952a
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2004-12-27 12:12:16 +0000

    Added descriptions to mapper types
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@3951 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/RomInfoTopic.cc
+++ src/memory/RomInfoTopic.cc
@@ -20,55 +20,51 @@ static Description description;
 
 RomInfoTopic::RomInfoTopic()
 {
-	description[GENERIC_8KB] = "desc";
-	/*GENERIC_16KB
-	KONAMI5
-	KONAMI4
-	ASCII_8KB
-	ASCII_16KB
-	R_TYPE
-	CROSS_BLAIM
-	MSX_AUDIO
-	HARRY_FOX
-	HALNOTE
-	KOREAN80IN1
-	KOREAN90IN1
-	KOREAN126IN1
-	HOLY_QURAN
-	PLAIN
-	FSA1FM1
-	FSA1FM2
-	DRAM
+	description[GENERIC_8KB] = "Generic 8kB";
+	description[GENERIC_16KB] = "Generic 16kB";
+	description[KONAMI5] = "Konami with SCC";
+	description[KONAMI4] = "Konami MegaROM";
+	description[ASCII_8KB] = "ASCII 8kB";
+	description[ASCII_16KB] = "ASCII 16kB";
+	description[R_TYPE] = "R-Type";
+	description[CROSS_BLAIM] = "Cross Blaim";
+	description[MSX_AUDIO] = "Panasonic MSX-AUDIO";
+	description[HARRY_FOX] = "Harry Fox";
+	description[HALNOTE] = "Halnote";
+	description[KOREAN80IN1] = "Zemmix 80 in 1";
+	description[KOREAN90IN1] = "Zemmix 90 in 1";
+	description[KOREAN126IN1] = "Zemmix 126 in 1";
+	description[HOLY_QURAN] = "Holy Qu'ran";
+	description[PLAIN] = "Plain (any size)";
+	description[FSA1FM1] = "Panasonic FS-A1FM internal mapper 1";
+	description[FSA1FM2] = "Panasonic FS-A1FM internal mapper 2";
+	description[DRAM] = "MSXturboR DRAM";
 
-	PAGE0
-	PAGE1
-	PAGE01
-	PAGE2
-	PAGE02
-	PAGE12
-	PAGE012
-	PAGE3
-	PAGE03
-	PAGE13
-	PAGE013
-	PAGE23
-	PAGE023
-	PAGE123
-	PAGE0123
+	// plain variants
+	description[PAGE0] = "Plain 16kB page 0";
+	description[PAGE1] = "Plain 16kB page 1";
+	description[PAGE01] = "Plain 32kB page 0-1";
+	description[PAGE2] = "Plain 16kB page 2 (BASIC)";
+	description[PAGE12] = "Plain 32kB page 1-2";
+	description[PAGE012] = "Plain 48kB page 0-2";
+	description[PAGE3] = "Plain 16kB page 3";
+	description[PAGE23] = "Plain 32kB page 2-3";
+	description[PAGE123] = "Plain 48kB page 1-3";
+	description[PAGE0123] = "Plain 64kB";
 	
-	HAS_SRAM
-	ASCII8_8
-	HYDLIDE2
-	GAME_MASTER2
-	PANASONIC
-	NATIONAL
-	KOEI_8
-	KOEI_32
-	WIZARDRY
-
-	HAS_DAC
-	MAJUTSUSHI
-	SYNTHESIZER*/
+	// with SRAM
+	description[ASCII8_8] = "ASCII 8kB with 8kB SRAM";
+	description[HYDLIDE2] = "ASCII 16kB with 2kB SRAM";
+	description[GAME_MASTER2] = "Konami's Game Master 2";
+	description[PANASONIC] = "Panasonic internal mapper";
+	description[NATIONAL] = "National internal mapper";
+	description[KOEI_8] = "Koei with 8kB SRAM";
+	description[KOEI_32] = "Koei with 32kB SRAM";
+	description[WIZARDRY] = "Wizardry";
+	
+	// with DAC
+	description[MAJUTSUSHI] = "Hai no Majutsushi";
+	description[SYNTHESIZER] = "Konami's Synthesizer";
 }
 
 void RomInfoTopic::execute(const vector<CommandArgument>& tokens,
