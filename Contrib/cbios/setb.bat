b2018cd268171a3687293b83d99775f630697cf6
Maarten ter Huurne <maarten@treewalker.org>
2005-02-12 04:12:53 +0000

    C-BIOS 0.20 has a different structure; removing all old files to make space.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4100 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- Contrib/cbios/setb.bat
+++
@@ -1,2 +0,0 @@
-copy /b cb.bin+cb_sh.bin cbios.rom
-copy cb_sub.bin cbios_sub.rom
--- Contrib/cbios/src/cmp.bat
+++
@@ -1,5 +0,0 @@
-as80 -z -x3 cb_sub
-as80 -z -x3 cb_sh
-as80 -lcb.syn -z -x3 cb_main
-copy /b cb_main.bin+cb_sh.bin cbios.rom
-copy cb_sub.bin cbios_sub.rom
