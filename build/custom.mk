9492da65455159fa60dd75a2db6cd748f520f6de
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2005-02-13 11:53:15 +0000

    Oops, accidentally committed this file
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4107 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/custom.mk
+++ build/custom.mk
@@ -6,7 +6,7 @@
 # openMSX is always installed into a single self-contained directory.
 # But you can change that directory to for example /usr/local/openMSX
 # or /usr/games/openMSX if you like.
-INSTALL_BASE:=/tmp
+INSTALL_BASE:=/opt/openMSX
 
 # Add ChangeLog version number to executable file name? This applies only to
 # development versions, not to release versions (see version.mk).
