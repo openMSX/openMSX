219059f6d253794ce42c8b3cce513d8e2c3cfe83
Patrick van Arkel <vampier@users.sourceforge.net>
2005-04-29 05:20:26 +0000

    Window timmer support
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4319 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/thread/Timer.cc
+++ src/thread/Timer.cc
@@ -11,7 +11,9 @@
 #ifdef HAVE_USLEEP
 #include <unistd.h>
 #endif
-
+#ifdef WIN32
+#include <windows.h>
+#endif
 
 // TODO add us precision timer routines for win32
 
@@ -19,7 +21,25 @@ namespace openmsx {
 
 unsigned long long Timer::getTime()
 {
-#ifdef HAVE_GETTIMEOFDAY
+#if defined (WIN32)
+    static LONGLONG hfFrequency = 0;
+    LARGE_INTEGER li;
+
+    if (!hfFrequency) {
+        if (QueryPerformanceFrequency(&li)) {
+            hfFrequency = li.QuadPart;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    QueryPerformanceCounter(&li);
+
+    // Assumes that the timer never wraps. The mask is just to
+    // ensure that the multiplication doesn't wrap.
+    return (li.QuadPart & ((long long)-1 >> 20)) * 1000000 / hfFrequency;
+#elif defined (HAVE_GETTIMEOFDAY)
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	return (unsigned long long)tv.tv_sec * 1000000 +
@@ -29,9 +49,32 @@ unsigned long long Timer::getTime()
 #endif
 }
 
+#ifdef WIN32
+static void CALLBACK timerCallback(unsigned int,
+                                   unsigned int,
+                                   unsigned long eventHandle,
+                                   unsigned long,
+                                   unsigned long)
+{
+    SetEvent((HANDLE)eventHandle);
+}
+#endif
+
 void Timer::sleep(unsigned long long us)
 {
-#ifdef HAVE_USLEEP
+#if defined (WIN32)
+    us /= 1000;
+
+    if (us > 0) {
+        static HANDLE timerEvent = NULL;
+        if (timerEvent == NULL) {
+            timerEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+        }
+        UINT id = timeSetEvent(us, 1, timerCallback, (DWORD)timerEvent, TIME_ONESHOT);
+        WaitForSingleObject(timerEvent, INFINITE);
+        timeKillEvent(id);
+    }
+#elif defined (HAVE_USLEEP)
 	usleep(us);
 #else
 	SDL_Delay(us / 1000);
@@ -39,4 +82,3 @@ void Timer::sleep(unsigned long long us)
 }
 
 } // namespace openmsx
-
