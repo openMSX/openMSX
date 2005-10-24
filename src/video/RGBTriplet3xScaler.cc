88298f30e2e5c74570db5a01d021ad5302976899
Wouter Vermaelen <m9710797@users.sourceforge.net>
2005-10-24 18:02:57 +0000

    improved Manuel's RGB scaler (still WIP)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4771 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/RGBTriplet3xScaler.cc
+++ src/video/RGBTriplet3xScaler.cc
@@ -15,33 +15,55 @@ RGBTriplet3xScaler<Pixel>::RGBTriplet3xScaler(SDL_PixelFormat* format)
 {
 }
 
+static void calcSpil(unsigned x, unsigned& r, unsigned& s)
+{
+	r = 3 * x;
+	s = 0;
+	if (r > 255) {
+		s = (r - 255) / 2;
+		r = 255;
+	}
+}
+
 template <class Pixel>
 void RGBTriplet3xScaler<Pixel>::scale256(
 	FrameSource& src, unsigned srcStartY, unsigned srcEndY,
-	SDL_Surface* dst, unsigned dstStartY, unsigned dstEndY
-) {
+	SDL_Surface* dst, unsigned dstStartY, unsigned dstEndY)
+{
 	for (unsigned y = dstStartY; y < dstEndY; y += 3, ++srcStartY) {
 		const Pixel* srcLine = src.getLinePtr(srcStartY, (Pixel*)0);
 		Pixel* dstLine0 = Scaler<Pixel>::linePtr(dst, y + 0);
-		for (int p = 0; p < 320; ++p) {
-			Pixel pix = srcLine[p];
-			dstLine0[3 * p + 0] = (pix & 0x00FF0000) | ((pix >> 1) & 0x007F7F);
-			dstLine0[3 * p + 1] = (pix & 0x0000FF00) | ((pix >> 1) & 0x7F007F);
-			dstLine0[3 * p + 2] = (pix & 0x000000FF) | ((pix >> 1) & 0x7F7F00);
+
+		unsigned r, g, b, rs, gs, bs;
+		calcSpil((srcLine[0] >> 16) & 0xFF, r, rs);
+		calcSpil((srcLine[0] >>  8) & 0xFF, g, gs);
+		calcSpil((srcLine[0] >>  0) & 0xFF, b, bs);
+		dstLine0[0] = (r  << 16) + (gs << 8);
+		dstLine0[1] = (rs << 16) + (g  << 8) + (bs << 0);
+		calcSpil((srcLine[1] >> 16) & 0xFF, r, rs);
+		dstLine0[2] = (rs << 16) + (gs << 8) + (b  << 0);
+		for (int i = 1; i < 319; ++i) {
+			calcSpil((srcLine[i + 0] >>  8) & 0xFF, g, gs);
+			dstLine0[3 * i + 0] = (r  << 16) + (gs << 8) + (bs << 0);
+			calcSpil((srcLine[i + 0] >>  0) & 0xFF, b, bs);
+			dstLine0[3 * i + 1] = (rs << 16) + (g  << 8) + (bs << 0);
+			calcSpil((srcLine[i + 1] >> 16) & 0xFF, r, rs);
+			dstLine0[3 * i + 2] = (rs << 16) + (gs << 8) + (b  << 0);
 		}
-		//scale_1on3(srcLine, dstLine0, 320);
+		calcSpil((srcLine[319] >>  8) & 0xFF, g, gs);
+		dstLine0[957] = (r  << 16) + (gs << 8) + (bs << 0);
+		calcSpil((srcLine[319] >>  0) & 0xFF, b, bs);
+		dstLine0[958] = (rs << 16) + (g  << 8) + (bs << 0);
+		dstLine0[959] =              (gs << 8) + (b  << 0);
+		
 		Pixel* dstLine1 = Scaler<Pixel>::linePtr(dst, y + 1);
 		copyLine(dstLine0, dstLine1, 960);
 		if (y + 2 == dstEndY) break;
 		Pixel* dstLine2 = Scaler<Pixel>::linePtr(dst, y + 2);
 		// emulate a scanline for the 3rd row
-		for (int p = 0; p < 320; ++p) {
-			Pixel pix = (srcLine[p]>>1 & 0x007F7F7F);
-			dstLine2[3 * p + 0] = pix;
-			dstLine2[3 * p + 1] = pix;
-			dstLine2[3 * p + 2] = pix;
+		for (int i = 0; i < 960; ++i) {
+			dstLine2[i] = (dstLine0[i] >> 1) & 0x007F7F7F;
 		}
-//		copyLine(dstLine0, dstLine2, 960);
 	}
 }
 
