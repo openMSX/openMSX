5ebd65556154472ee24f66f6818b7deb367e7013
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2005-10-24 20:15:18 +0000

    Implemented blank() for RGBTriplet3x scaler
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4772 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/RGBTriplet3xScaler.cc
+++ src/video/RGBTriplet3xScaler.cc
@@ -60,9 +60,57 @@ void RGBTriplet3xScaler<Pixel>::scale256(
 		copyLine(dstLine0, dstLine1, 960);
 		if (y + 2 == dstEndY) break;
 		Pixel* dstLine2 = Scaler<Pixel>::linePtr(dst, y + 2);
-		// emulate a scanline for the 3rd row
+		// emulate a 75% scanline for the 3rd row
 		for (int i = 0; i < 960; ++i) {
-			dstLine2[i] = (dstLine0[i] >> 1) & 0x007F7F7F;
+			dstLine2[i] = ((dstLine0[i] >> 1) & 0x7F7F7F) + ((dstLine0[i] >>2) & 0x3F3F3F);
+		}
+	}
+}
+
+
+// the following is mostly copied from scale256 (with some minor optimizations
+// TODO: see if we can avoid code duplication
+template <class Pixel>
+void Scaler<Pixel>::scaleBlank(Pixel color, SDL_Surface* dst,
+                               unsigned startY, unsigned endY)
+{
+	for (unsigned y = startY; y < endY; y += 3) {
+		Pixel* dstLine0 = Scaler<Pixel>::linePtr(dst, y + 0);
+
+		unsigned r, g, b, rs, gs, bs;
+		calcSpil((color >> 16) & 0xFF, r, rs);
+		calcSpil((color >>  8) & 0xFF, g, gs);
+		calcSpil((color >>  0) & 0xFF, b, bs);
+		dstLine0[0] = (r  << 16) + (gs << 8);
+		dstLine0[1] = (rs << 16) + (g  << 8) + (bs << 0);
+		calcSpil((color >> 16) & 0xFF, r, rs);
+		dstLine0[2] = (rs << 16) + (gs << 8) + (b  << 0);
+		
+		// calculate the first non-edge pixel
+		calcSpil((color >>  8) & 0xFF, g, gs);
+		dstLine0[3 * 1 + 0] = (r  << 16) + (gs << 8) + (bs << 0);
+		calcSpil((color >>  0) & 0xFF, b, bs);
+		dstLine0[3 * 1 + 1] = (rs << 16) + (g  << 8) + (bs << 0);
+		calcSpil((color >> 16) & 0xFF, r, rs);
+		dstLine0[3 * 1 + 2] = (rs << 16) + (gs << 8) + (b  << 0);
+		// copy this for the whole line
+		for (int i = 2; i < 319; ++i) {
+			dstLine0[3 * i + 0] = dstLine0[3 * 1 + 0];
+			dstLine0[3 * i + 1] = dstLine0[3 * 1 + 1];
+			dstLine0[3 * i + 2] = dstLine0[3 * 1 + 2];
+		}
+		calcSpil((color >>  8) & 0xFF, g, gs);
+		dstLine0[957] = (r  << 16) + (gs << 8) + (bs << 0);
+		calcSpil((color >>  0) & 0xFF, b, bs);
+		dstLine0[958] = (rs << 16) + (g  << 8) + (bs << 0);
+		dstLine0[959] =              (gs << 8) + (b  << 0);
+		
+		Pixel* dstLine1 = Scaler<Pixel>::linePtr(dst, y + 1);
+		copyLine(dstLine0, dstLine1, 960);
+		Pixel* dstLine2 = Scaler<Pixel>::linePtr(dst, y + 2);
+		// emulate a 75% scanline for the 3rd row
+		for (int i = 0; i < 960; ++i) {
+			dstLine2[i] = ((dstLine0[i] >> 1) & 0x7F7F7F) + ((dstLine0[i] >>2) & 0x3F3F3F);
 		}
 	}
 }
