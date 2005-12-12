8233d714cfbb6580ffe066ef850f607e7dd2d39a
Wouter Vermaelen <m9710797@users.sourceforge.net>
2005-12-12 10:27:50 +0000

    removed openmsx-control.cc from dist and instead added openmsx-control-stdio.cc and openmsx-control-socket.cc
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4939 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- Contrib/node.mk
+++ Contrib/node.mk
@@ -5,7 +5,8 @@ include build/node-start.mk
 DIST:= \
 	README \
 	README.cbios cbios \
-	README.openmsx-control openmsx-control.cc
+	README.openmsx-control \
+	openmsx-control-stdio.cc openmsx-control-socket.cc
 
 include build/node-end.mk
 
