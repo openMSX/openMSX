7f450627dae3f7584d1672dc0e22eab2410d20b4
Wouter Vermaelen <m9710797@users.sourceforge.net>
2005-12-20 19:57:18 +0000

    prepare openmsx-0.6.0-rc1
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@4969 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.mk
+++ build/version.mk
@@ -6,7 +6,7 @@
 PACKAGE_NAME:=openmsx
 
 # Version number.
-PACKAGE_VERSION:=0.5.2
+PACKAGE_VERSION:=0.6.0-rc1
 
 # Is this a release version ("true") or development version ("false").
-RELEASE_FLAG:=false
+RELEASE_FLAG:=true
