9d524abcbdd7af375ebe0b239ed6cd09c2d84db5
Maarten ter Huurne <maarten@treewalker.org>
2006-01-12 19:53:34 +0000

    Set version to 0.6.0-rc3.
    git-svn-id: file:///openmsx-svn/code/openmsx/branches/branch_0_6_0@5036 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.mk
+++ build/version.mk
@@ -6,7 +6,7 @@
 PACKAGE_NAME:=openmsx
 
 # Version number.
-PACKAGE_VERSION:=0.6.0-rc2
+PACKAGE_VERSION:=0.6.0-rc3
 
 # Is this a release version ("true") or development version ("false").
 RELEASE_FLAG:=true
