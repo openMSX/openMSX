c19733f37c7417ee8c171a2ec4495da78f33d706
Maarten ter Huurne <maarten@treewalker.org>
2006-01-21 22:38:20 +0000

    Merged fix from 0.6.0 branch.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5065 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -3,7 +3,7 @@
 include build/node-start.mk
 
 DIST:= \
-	config.guess detectsys.sh tcl-search.sh \
+	config.guess detectsys.sh tcl-search.sh install-recursive.sh \
 	main.mk node-end.mk node-start.mk \
 	info2code.mk components.mk probe.mk probe-results.mk \
 	version.mk custom.mk \
