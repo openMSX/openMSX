e9d1eed26f8e85ff7a59ff5e2a7a6cc52876e690
Maarten ter Huurne <maarten@treewalker.org>
2006-01-29 23:53:11 +0000

    Documented flag for prebinding, but kept it disabled because it does not work (at least on my machine).
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5107 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-darwin-app.mk
+++ build/platform-darwin-app.mk
@@ -20,6 +20,11 @@
 
 include build/platform-darwin.mk
 
+# Enable prebinding for better startup performance.
+# On OS X 10.3 the system frameworks are using overlapping addresses, so
+# prebinding fails. Let's try again later.
+#LINK_FLAGS+=-prebind
+
 # The app folder will set a hi-res icon, so the openMSX process should not
 # replace this with its own low-res icon.
 SET_WINDOW_ICON:=false
