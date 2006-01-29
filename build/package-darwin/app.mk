9381d105127fc2e4549d2188fdace915cf139cba
Maarten ter Huurne <maarten@treewalker.org>
2006-01-29 23:54:28 +0000

    Use "bindist" where "app" was incorrectly used before. Added creation of disk image.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5110 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/package-darwin/app.mk
+++ build/package-darwin/app.mk
@@ -3,6 +3,7 @@
 # Create an application directory for Darwin.
 
 BINDIST_DIR:=$(BUILD_PATH)/bindist
+BINDIST_IMAGE:=$(BUILD_PATH)/$(PACKAGE_FULL).dmg
 
 APP_SUPPORT_DIR:=build/package-darwin
 APP_DIR:=$(BINDIST_DIR)/openMSX.app
@@ -14,32 +15,38 @@ APP_ICON:=$(APP_RES)/openmsx-logo.icns
 # Override install locations.
 INSTALL_BINARY_DIR:=$(APP_EXE_DIR)
 INSTALL_SHARE_DIR:=$(APP_DIR)/share
-INSTALL_DOC_DIR:=$(BINDIST_DIR)/doc
+INSTALL_DOC_DIR:=$(BINDIST_DIR)/Documentation
 # C-BIOS should be included.
 INSTALL_CONTRIB:=true
 # Do not display header and post-install instructions.
 INSTALL_VERBOSE:=false
 
-.PHONY: app appinit
+.PHONY: bindist bindistclean
 
-app: install $(APP_PLIST) $(APP_ICON)
+bindist: install $(APP_PLIST) $(APP_ICON)
+	@echo "Creating disk image:"
+	@hdiutil create -srcfolder $(BINDIST_DIR) \
+		-volname openMSX \
+		-imagekey zlib-level=9 \
+		-ov $(BINDIST_IMAGE)
+	@hdiutil internet-enable -yes $(BINDIST_IMAGE)
 
 # Force removal of old app before installing to new app.
-install: appinit
+install: bindistclean
 
-appinit: $(BINARY_FULL)
-	@echo "Removing any old application..."
-	@rm -rf $(APP_DIR)
-	@echo "Packaging application:"
+bindistclean: $(BINARY_FULL)
+	@echo "Removing any old binary package..."
+	@rm -rf $(BINDIST_DIR)
+	@echo "Creating binary package:"
 
-$(APP_PLIST): $(APP_DIR)/Contents/%: $(APP_SUPPORT_DIR)/% appinit
+$(APP_PLIST): $(APP_DIR)/Contents/%: $(APP_SUPPORT_DIR)/% bindistclean
 	@echo "  Writing meta-info..."
 	@mkdir -p $(@D)
 	@sed -e 's/%ICON%/$(notdir $(APP_ICON))/' \
 		-e 's/%VERSION%/$(PACKAGE_VERSION)/' < $< > $@
 	@echo "APPLoMSX" > $(@D)/PkgInfo
 
-$(APP_ICON): $(APP_RES)/%: $(APP_SUPPORT_DIR)/% appinit
+$(APP_ICON): $(APP_RES)/%: $(APP_SUPPORT_DIR)/% bindistclean
 	@echo "  Copying resources..."
 	@mkdir -p $(@D)
 	@cp $< $@
