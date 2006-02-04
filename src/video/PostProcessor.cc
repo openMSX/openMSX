e47edbd3ebeed8b51fbb271936ccf2163f8d78a1
Maarten ter Huurne <maarten@treewalker.org>
2006-02-04 22:52:25 +0000

    At the end of scaleImage or just after scaleImage... it's basically the same thing. Thanks to Wouter for spotting this.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5143 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/PostProcessor.cc
+++ src/video/PostProcessor.cc
@@ -158,14 +158,12 @@ void PostProcessor<Pixel>::paint()
 		currScaler->scaleImage(
 			*frame, srcStartY, srcEndY, lineWidth, // source
 			screen, dstStartY, dstEndY); // dest
+		frame->freeLineBuffers();
 
 		// next region
 		srcStartY = srcEndY;
 		dstStartY = dstEndY;
 	}
-	// TODO: Actually, this could be done at the end of scaleImage, but it
-	//       would require changing many scalers.
-	frame->freeLineBuffers();
 
 	// TODO: This statement is the only reason PostProcessor uses "screen"
 	//       as a VisibleSurface instead of as an OutputSurface.
