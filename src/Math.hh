e5ffd88151852655833dac664848a80336a3f1b1
Maarten ter Huurne <maarten@treewalker.org>
2006-03-25 06:22:32 +0000

    Moved gcd implementation to Math module.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5277 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/Math.hh
+++ src/Math.hh
@@ -25,6 +25,39 @@ inline int clip(double r, double factor)
 	return std::min(std::max(a, LO), HI);
 }
 
+/** Calculate greatest common divider of two strictly positive integers.
+  * Classical implementation is like this:
+  *    while (unsigned t = b % a) { b = a; a = t; }
+  *    return a;
+  * The following implementation avoids the costly modulo operation. It
+  * is about 40% faster on my machine.
+  *
+  * require: a != 0  &&  b != 0
+  */
+inline unsigned gcd(unsigned a, unsigned b)
+{
+	unsigned k = 0;
+	while (((a & 1) == 0) && ((b & 1) == 0)) {
+		a >>= 1; b >>= 1; ++k;
+	}
+
+	// either a or b (or both) is odd
+	while ((a & 1) == 0) a >>= 1;
+	while ((b & 1) == 0) b >>= 1;
+
+	// both a and b odd
+	while (a != b) {
+		if (a >= b) {
+			a -= b;
+			do { a >>= 1; } while ((a & 1) == 0);
+		} else {
+			b -= a;
+			do { b >>= 1; } while ((b & 1) == 0);
+		}
+	}
+	return b << k;
+}
+
 } // namespace Math
 
 } // namespace openmsx
--- src/video/GLPostProcessor.cc
+++ src/video/GLPostProcessor.cc
@@ -44,39 +44,6 @@ GLPostProcessor::~GLPostProcessor()
 	renderSettings.getNoise().detach(*this);
 }
 
-/** Calculate greatest common divider of two strictly positive integers.
-  * Classical implementation is like this:
-  *    while (unsigned t = b % a) { b = a; a = t; }
-  *    return a;
-  * The following implementation avoids the costly modulo operation. It
-  * is about 40% faster on my machine.
-  *
-  * require: a != 0  &&  b != 0
-  */
-static inline unsigned gcd(unsigned a, unsigned b)
-{
-	unsigned k = 0;
-	while (((a & 1) == 0) && ((b & 1) == 0)) {
-		a >>= 1; b >>= 1; ++k;
-	}
-
-	// either a or b (or both) is odd
-	while ((a & 1) == 0) a >>= 1;
-	while ((b & 1) == 0) b >>= 1;
-
-	// both a and b odd
-	while (a != b) {
-		if (a >= b) {
-			a -= b;
-			do { a >>= 1; } while ((a & 1) == 0);
-		} else {
-			b -= a;
-			do { b >>= 1; } while ((b & 1) == 0);
-		}
-	}
-	return b << k;
-}
-
 void GLPostProcessor::paint()
 {
 	if (!paintFrame) {
@@ -98,7 +65,7 @@ void GLPostProcessor::paint()
 	const unsigned srcHeight = paintFrame->getHeight();
 	const unsigned dstHeight = screen.getHeight();
 
-	unsigned g = gcd(srcHeight, dstHeight);
+	unsigned g = Math::gcd(srcHeight, dstHeight);
 	unsigned srcStep = srcHeight / g;
 	unsigned dstStep = dstHeight / g;
 
