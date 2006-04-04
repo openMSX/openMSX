726792968010a14eb50a4d2527ad5ccdb38dd9eb
Maarten ter Huurne <maarten@treewalker.org>
2006-04-04 21:30:36 +0000

    Removed superfluous setting of texture blend parameters.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5297 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/GLUtil.cc
+++ src/video/GLUtil.cc
@@ -76,8 +76,6 @@ void Texture::drawRect(
 void ColourTexture::setImage(GLsizei width, GLsizei height, GLuint* data)
 {
 	bind();
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexImage2D(
 		GL_TEXTURE_2D,    // target
 		0,                // level
