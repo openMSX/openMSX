d806faff68acfbf5df9cbd6a780746ad64f39975
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2006-04-05 20:03:30 +0000

    Some renames and added amber monitors
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5304 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/monitor.tcl
+++ share/scripts/monitor.tcl
@@ -10,15 +10,21 @@ proc monitor_green {} {
 proc monitor_blue {} {
 	set ::color_matrix { { 0 0 0 } { 0 0 0 } { 0 0 1 } }
 }
-proc monitor_monochrome {} {
+proc monitor_monochrome_amber {} {
+	set ::color_matrix { { .257 .504 .098 } { .193 .378 .073 } { 0 0 0 } }
+}
+proc monitor_monochrome_amber_bright {} {
+	set ::color_matrix { { .333 .333 .333 } { .249 .249 .249 } { 0 0 0 } }
+}
+proc monitor_monochrome_green {} {
 	set ::color_matrix { { .129 .252 .049 } { .257 .504 .098 } { .129 .252 .049 } }
 }
-proc monitor_monochrome2 {} {
+proc monitor_monochrome_green_bright {} {
 	set ::color_matrix { { .167 .167 .167 } { .333 .333 .333 } { .167 .167 .167 } }
 }
-proc monitor_bw {} {
+proc monitor_monochrome_white {} {
 	set ::color_matrix { { .257 .504 .098 } { .257 .504 .098 } { .257 .504 .098 } }
 }
-proc monitor_bw2 {} {
+proc monitor_monochrome_white_bright {} {
 	set ::color_matrix { { .333 .333 .333 } { .333 .333 .333 } { .333 .333 .333 } }
 }
