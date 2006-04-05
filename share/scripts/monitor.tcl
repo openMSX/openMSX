9cd5eb11e574e583c5ebffa0ab538fb429c20fc2
Wouter Vermaelen <m9710797@users.sourceforge.net>
2006-04-05 19:10:37 +0000

    a few more monitor types (need better names?)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5303 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/monitor.tcl
+++ share/scripts/monitor.tcl
@@ -13,4 +13,12 @@ proc monitor_blue {} {
 proc monitor_monochrome {} {
 	set ::color_matrix { { .129 .252 .049 } { .257 .504 .098 } { .129 .252 .049 } }
 }
-
+proc monitor_monochrome2 {} {
+	set ::color_matrix { { .167 .167 .167 } { .333 .333 .333 } { .167 .167 .167 } }
+}
+proc monitor_bw {} {
+	set ::color_matrix { { .257 .504 .098 } { .257 .504 .098 } { .257 .504 .098 } }
+}
+proc monitor_bw2 {} {
+	set ::color_matrix { { .333 .333 .333 } { .333 .333 .333 } { .333 .333 .333 } }
+}
