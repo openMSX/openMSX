901e177782fa829a8d0aa1550e269ae34227eb93
Wouter Vermaelen <m9710797@users.sourceforge.net>
2006-05-20 09:52:09 +0000

    added -D_GLIBCXX_DEBUG_PEDANTIC flag for debug builds
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5375 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/flavour-debug.mk
+++ build/flavour-debug.mk
@@ -4,7 +4,8 @@
 # Build with all debugging info, no optimisations.
 
 # Debug flags.
-CXXFLAGS+=-O0 -g -DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCPP_CONCEPT_CHECKS
+CXXFLAGS+=-O0 -g -DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC \
+          -D_GLIBCPP_CONCEPT_CHECKS
 
 # Strip executable?
 OPENMSX_STRIP:=false
