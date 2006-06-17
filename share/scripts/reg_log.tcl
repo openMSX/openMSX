50e3757b883ad8d19a346495c7a3042f8f8350df
Wouter Vermaelen <m9710797@users.sourceforge.net>
2006-06-17 11:04:13 +0000

    added generic register logger
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5462 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ share/scripts/reg_log.tcl
@@ -0,0 +1,92 @@
+
+set_help_text reg_log \
+{TODO
+}
+
+set_tabcompletion_proc reg_log tab_reg_log
+proc tab_reg_log { args } {
+	switch [llength $args] {
+		2 { return "record play stop" }
+		3 { return [debug list] }
+	}
+}
+
+proc reg_log { subcommand debuggable {filename ""} } {
+	if {$filename == ""} { set filename ${debuggable}.log }
+	switch $subcommand {
+		"record" { return [__reg_log_record $debuggable $filename] }
+		"play"   { return [__reg_log_play $debuggable $filename] }
+		"stop"   { return [__reg_log_stop $debuggable] }
+		default  { error "bad option \"$subcommand\": must be start stop" }
+	}
+}
+
+proc __reg_log_check { debuggable } {
+	if {[lsearch [debug list] $debuggable] == -1} {
+		error "No such debuggable: $debuggable"
+	}
+}
+
+proc __reg_log_record { debuggable filename } {
+	global __reg_log_file __reg_log_count
+	__reg_log_check $debuggable
+	__reg_log_stop $debuggable
+	set __reg_log_file($debuggable) [open $filename {WRONLY TRUNC CREAT}]
+	set __reg_log_count($debuggable) 0
+	__do_reg_record $debuggable
+	return ""
+}
+
+proc __reg_log_play { debuggable filename } {
+	global __reg_log_data
+	__reg_log_check $debuggable
+	__reg_log_stop $debuggable
+	set file [open $filename RDONLY]
+	set __reg_log_data($debuggable) [split [read $file] \n]
+	close $file
+	__do_reg_play $debuggable
+	return ""
+}
+
+proc __reg_log_stop { debuggable } {
+	global __reg_log_file __reg_log_count __reg_log_data
+	if [info exists __reg_log_file($debuggable)] {
+		close $__reg_log_file($debuggable)
+		unset __reg_log_file($debuggable)
+		unset __reg_log_count($debuggable)
+	}
+	if [info exists __reg_log_data($debuggable)] {
+		unset __reg_log_data($debuggable)
+	}
+	return ""
+}
+
+proc __do_reg_record { debuggable } {
+	global __reg_log_file __reg_log_count
+	if ![info exists __reg_log_file($debuggable)] return
+	puts -nonewline $__reg_log_file($debuggable) "$__reg_log_count($debuggable) : "
+	incr __reg_log_count($debuggable)
+	set size [debug size $debuggable]
+	for {set i 0} {$i < $size} {incr i} {
+		puts -nonewline $__reg_log_file($debuggable) "[debug read $debuggable $i] "
+	}
+	puts $__reg_log_file($debuggable) ""  ;#newline
+	after frame [list __do_reg_record $debuggable]
+}
+
+proc __do_reg_play { debuggable } {
+	global __reg_log_data
+	if ![info exists __reg_log_data($debuggable)] return
+
+	set reg 0
+	set line [lindex $__reg_log_data($debuggable) 0]
+	foreach val [lrange $line 2 end] {
+		debug write $debuggable $reg $val
+		incr reg
+	}
+
+	set __reg_log_data($debuggable) [lrange $__reg_log_data($debuggable) 1 end]
+	if {[llength $__reg_log_data($debuggable)] > 0} {
+		after frame [list __do_reg_play $debuggable]
+	}
+}
