1e03d440a51da6c0f8e53814e5902f4574a5ef39
Maarten ter Huurne <maarten@treewalker.org>
2006-07-23 18:50:57 +0000

    Use "opt" flavour on Intel Macs instead of "i686", since the system headers use SSE registers.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5527 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/cpu-x86.mk
+++ build/cpu-x86.mk
@@ -6,7 +6,12 @@
 BIG_ENDIAN:=false
 
 # Default build flavour.
+ifeq ($(filter darwin%,$(OPENMSX_TARGET_OS)),)
 # To run openMSX with decent speed, at least a Pentium 2 class machine
 # is needed, so let's optimise for that.
 OPENMSX_FLAVOUR?=i686
-
+else
+# The system headers of OS X use SSE features, which are not available on
+# i686, so we only use the generic optimisation flags instead.
+OPENMSX_FLAVOUR?=opt
+endif
