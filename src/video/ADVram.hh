7da626bff5ae7a95aaeedc02fe8f0e91591f4b43
Arnold Metselaar <arnoldmnl@users.sourceforge.net>
2006-07-29 11:27:18 +0000

    Add doxygen comments.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5541 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/ADVram.hh
+++ src/video/ADVram.hh
@@ -12,7 +12,7 @@ class VDP;
 class VDPVRAM;
 
 /** Implementation of direct cpu access to VRAM.  ADVram (Accesso
-  * Direito \`a Vram is a rare hardware modification that allows the
+  * Direito &agrave; Vram is a rare hardware modification that allows the
   * CPU to access the video ram in the same way as ordinary ram.
   */
 class ADVram : public MSXDevice
@@ -20,10 +20,32 @@ class ADVram : public MSXDevice
 public:
 	ADVram(MSXMotherBoard& motherBoard, const XMLElement& config,
 	       const EmuTime& time);
+
+	/** This method is called on reset.  Reset the mapper register and
+	 * the planar bit, if the device is configured with an enable bit
+	 * then that bit is reset as well.  
+	 */
 	virtual void reset(const EmuTime& time);
+
+	/** Read a byte from an IO port, change mode bits.  The planar bit
+	 * and possibly the enable bit are set according to address lines
+	 * that are normally ignored for IO reads.  Returns 255.  
+	 */
 	virtual byte readIO(word port, const EmuTime& time);
+
+	/** Write a byte to a given IO port, set mapper register.  */
 	virtual void writeIO(word port, byte value, const EmuTime& time);
+
+	/** Read a byte from a location in the video ram at a certain
+	 * time.  If the device is enabled then the value returned comes
+	 * from the video ram, otherwise it returns 255.  
+	 */
 	virtual byte readMem(word address, const EmuTime& time);
+
+	/** Write a given byte at a certain time to a given location in
+	 * the video ram.  If the device is enabled then the write is
+	 * redirected to the video ram, if it is not, nothing happens.  
+	 */
 	virtual void writeMem(word address, byte value, const EmuTime& time);
 
 private:
@@ -32,10 +54,17 @@ private:
 
 	VDP* vdp;
 	VDPVRAM* vram;
+    /** Bit mask applied to logical addresses, reflects vram size.  */
 	unsigned mask;
+	/** Base address of selected page in vram.  */
 	unsigned baseAddr;
+	/** True if ADVram device is enabled.  */
 	bool enabled;
+	/** True if ADVram device can be switched on or off by performing
+		IO-reads.  */
 	bool hasEnable;
+	/** True if address bits are shuffled as is appropriate for screen
+		7 and higher.  */
 	bool planar;
 };
 
