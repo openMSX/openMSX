c5f03cf5412974592a629ef9a5f1646c29730314
Joost Yervante Damad <joost@damad.be>
2006-07-31 20:24:15 +0000

    small cleanup
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5558 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/cpu-ppc64.mk
+++ build/cpu-ppc64.mk
@@ -1,6 +1,6 @@
 # $Id$
 #
-# Configuration for PowerPC CPUs.
+# Configuration for PowerPC CPUs in 64bit mode
 
 # Big or little endian?
 BIG_ENDIAN:=true
--- build/flavour-ppc64.mk
+++ build/flavour-ppc64.mk
@@ -4,10 +4,7 @@
 
 # Optimisation flags.
 CXXFLAGS+=-O3 -DNDEBUG -mpowerpc-gfxopt
-#CXXFLAGS+=-mcpu=750
-#CXXFLAGS+=-mcpu=G3
 
 # Strip executable?
-# TODO: Stripping would be good, but linker doesn't understand "--strip-all".
-OPENMSX_STRIP:=false
+OPENMSX_STRIP:=true
 
