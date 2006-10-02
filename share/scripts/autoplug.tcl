85fb16bfe546fb4ae9674eedac6579468370e45d
Wouter Vermaelen <m9710797@users.sourceforge.net>
2006-10-02 19:59:52 +0000

    also auto plug the cassetteplayer after a machine switch
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5737 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/autoplug.tcl
+++ share/scripts/autoplug.tcl
@@ -1,6 +1,10 @@
 # if this machine has a cassetteport, then automaticaly plug
 # in the cassetteplayer
 
-if { [lsearch [openmsx_info connector] "cassetteport"] != -1 } {
-	plug cassetteport cassetteplayer
+proc do_autoplug {} {
+	if { [lsearch [openmsx_info connector] "cassetteport"] != -1 } {
+		plug cassetteport cassetteplayer
+	}
+	after machine_switch do_autoplug
 }
+do_autoplug
