4b536233d4da8a9bd19fd8bb02445ee7305ca597
Wouter Vermaelen <m9710797@users.sourceforge.net>
2006-11-15 21:08:03 +0000

    optimized hq edge calculation
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@5883 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/HQCommon.hh
+++ src/video/HQCommon.hh
@@ -71,20 +71,22 @@ static inline bool edge(unsigned c1, unsigned c2)
 	unsigned r1 = c1 >> 16;
 	unsigned g1 = (c1 >> 8) & 0xFF;
 	unsigned b1 = c1 & 0xFF;
-	unsigned y1 = r1 + g1 + b1;
 
 	unsigned r2 = c2 >> 16;
 	unsigned g2 = (c2 >> 8) & 0xFF;
 	unsigned b2 = c2 & 0xFF;
-	unsigned y2 = r2 + g2 + b2;
 
-	int dy = y1 - y2;
+	int dr = r1 - r2;
+	int dg = g1 - g2;
+	int db = b1 - b2;
+
+	int dy = dr + dg + db;
 	if (dy < -0xC0 || dy > 0xC0) return true;
 
-	int du = r1 - r2 + b2 - b1;
+	int du = dr - db;
 	if (du < -0x1C || du > 0x1C) return true;
 
-	int dv = 3 * (g1 - g2) - dy;
+	int dv = 3 * dg - dy;
 	if (dv < -0x30 || dv > 0x30) return true;
 
 	return false;
