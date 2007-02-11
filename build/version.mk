3a72add6e58e7b75d837211ae59e5094268dfc81
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-02-11 21:47:29 +0000

    set version=0.6.2-pre1, release_flag=true
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6070 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.mk
+++ build/version.mk
@@ -6,7 +6,7 @@
 PACKAGE_NAME:=openmsx
 
 # Version number.
-PACKAGE_VERSION:=0.6.1
+PACKAGE_VERSION:=0.6.2-pre1
 
 # Is this a release version ("true") or development version ("false").
-RELEASE_FLAG:=false
+RELEASE_FLAG:=true
