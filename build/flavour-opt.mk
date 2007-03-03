ba07af3a5fb9ae5d36800949dc7d313bdd9c16d0
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2007-03-03 18:43:15 +0000

    Don't use -fomit-frame-pointer on Win32, because it makes the binary crash for some reason
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6140 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/flavour-opt.mk
+++ build/flavour-opt.mk
@@ -5,7 +5,13 @@
 
 # Optimisation flags.
 CXXFLAGS+=-O3 -DNDEBUG \
-	-ffast-math -funroll-loops -fomit-frame-pointer
+	-ffast-math -funroll-loops 
+
+# openMSX crashes when the win32 version is built with -fomit-frame-pointer
+# TODO: investigate and recheck when new compiler is used.
+ifneq ($(OPENMSX_TARGET_OS),mingw32)
+CXXFLAGS+=-fomit-frame-pointer
+endif
 
 # Strip executable?
 OPENMSX_STRIP:=true
