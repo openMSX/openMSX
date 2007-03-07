93ae198c91519ef54d42123abbbd9e1a87aeba14
Maarten ter Huurne <maarten@treewalker.org>
2007-03-07 23:26:07 +0000

    Redesigned entire script. Added download feature, modeled after BouKiCHi's SUNNY script.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6146 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/3rdparty.mk
+++ build/3rdparty.mk
@@ -6,16 +6,26 @@
 # It enables only the features needed by openMSX: for example from SDL_image
 # we only need PNG handling cability.
 
-SOURCES_DIR:=3rdparty
 STATIC_DIR:=derived/3rdparty
+TARBALLS_DIR:=$(STATIC_DIR)/download
+SOURCE_DIR:=$(STATIC_DIR)/src
 SUPPORTDIR:=build/package-darwin
+TIMESTAMP_DIR:=$(STATIC_DIR)/timestamps
+BUILD_DIR:=$(STATIC_DIR)/build
+INSTALL_DIR:=$(STATIC_DIR)/install
+
+# Download locations for package sources.
+DOWNLOAD_PNG:=ftp://ftp.simplesystems.org/pub/libpng/png/src
+DOWNLOAD_SDL:=http://www.libsdl.org/release
+DOWNLOAD_SDL_IMAGE:=http://www.libsdl.org/projects/SDL_image/release
+DOWNLOAD_GLEW:=http://downloads.sourceforge.net/glew
 
 # These were the most recent versions at the moment of writing this Makefile.
 # You can use other versions if you like; adjust the names accordingly.
 PACKAGE_PNG:=libpng-1.2.16
 PACKAGE_SDL:=SDL-1.2.11
 PACKAGE_SDL_IMAGE:=SDL_image-1.2.5
-PACKAGE_GLEW:=glew-1.3.5
+PACKAGE_GLEW:=glew-1.3.6
 
 # Unfortunately not all packages stick to naming conventions such as putting
 # the sources in a dir that includes the version number.
@@ -23,42 +33,68 @@ PACKAGES_STD:=PNG SDL SDL_IMAGE
 PACKAGES_NONSTD:=GLEW
 PACKAGES:=$(PACKAGES_STD) $(PACKAGES_NONSTD)
 
-BUILD_TARGETS:=$(addprefix build-,$(PACKAGES))
-INSTALL_TARGETS:=$(addprefix install-,$(PACKAGES))
+# Source tar file names for non-standard packages.
+TARBALL_GLEW:=$(PACKAGE_GLEW)-src.tgz
+# Source tar file names for standard packages.
+TARBALL_PNG:=$(PACKAGE_PNG).tar.gz
+TARBALL_SDL:=$(PACKAGE_SDL).tar.gz
+TARBALL_SDL_IMAGE:=$(PACKAGE_SDL_IMAGE).tar.gz
+# All source tar file names.
+TARBALLS:=$(foreach PACKAGE,$(PACKAGES),$(TARBALL_$(PACKAGE)))
 
-INSTALL_PARAMS_GLEW:=GLEW_DEST=$(PWD)/$(STATIC_DIR)/install
+BUILD_TARGETS:=$(foreach PACKAGE,$(PACKAGES),$(TIMESTAMP_DIR)/build-$(PACKAGE_$(PACKAGE)))
+INSTALL_TARGETS:=$(foreach PACKAGE,$(PACKAGES),$(TIMESTAMP_DIR)/install-$(PACKAGE_$(PACKAGE)))
+
+INSTALL_PARAMS_GLEW:=GLEW_DEST=$(PWD)/$(INSTALL_DIR)
+
+# Function which, given a variable name prefix and the variable's value,
+# returns the name of the package.
+findpackage=$(strip $(foreach PACKAGE,$(PACKAGES),$(if $(filter $(2),$($(1)_$(PACKAGE))),$(PACKAGE),)))
 
 # We want a small distribution set, so optimize for size.
 # We got pretty good results with -Os in benchmarks, so it is not likely to
 # harm performance.
 export CFLAGS:=-Os
 
-.PHONY: all $(BUILD_TARGETS) $(INSTALL_TARGETS)
+.PHONY: all clean download
 
 all: $(INSTALL_TARGETS)
 
+clean:
+	rm -rf $(SOURCE_DIR)
+	rm -rf $(BUILD_DIR)
+	rm -rf $(INSTALL_DIR)
+
 # Install.
-$(INSTALL_TARGETS): install-%: build-%
-	make -C $(STATIC_DIR)/$< install $(INSTALL_PARAMS_$(subst install-,,$@))
+$(INSTALL_TARGETS): $(TIMESTAMP_DIR)/install-%: $(TIMESTAMP_DIR)/build-%
+	make -C $(BUILD_DIR)/$* install $(INSTALL_PARAMS_$(call findpackage,PACKAGE,$*))
+	mkdir -p $(@D)
+	touch $@
 
 # Build.
-$(BUILD_TARGETS): build-%: $(STATIC_DIR)/build-%/Makefile
-	make -C $(STATIC_DIR)/$@
+$(BUILD_TARGETS): $(TIMESTAMP_DIR)/build-%: $(BUILD_DIR)/%/Makefile
+	make -C $(<D)
+	mkdir -p $(@D)
+	touch $@
 
 # Configure SDL.
-$(STATIC_DIR)/build-SDL/Makefile: \
-  $(STATIC_DIR)/$(PACKAGE_SDL)
+$(BUILD_DIR)/$(PACKAGE_SDL)/Makefile: \
+  $(SOURCE_DIR)/$(PACKAGE_SDL)
 	mkdir -p $(@D)
 	cd $(@D) && $(PWD)/$</configure \
 		--disable-debug \
 		--disable-cdrom \
-		--prefix=$(PWD)/$(STATIC_DIR)/install
+		--prefix=$(PWD)/$(INSTALL_DIR)
 # While openMSX does not use "cpuinfo", "endian" and "file" modules, other
 # modules do and if we disable them, SDL will not link.
 
 # Configure SDL_image.
-$(STATIC_DIR)/build-SDL_IMAGE/Makefile: \
-  $(STATIC_DIR)/$(PACKAGE_SDL_IMAGE) install-SDL install-PNG
+PNG_CONFIG_SCRIPT:=$(INSTALL_DIR)/bin/libpng12-config
+SDL_CONFIG_SCRIPT:=$(INSTALL_DIR)/bin/sdl-config
+$(PNG_CONFIG_SCRIPT): $(TIMESTAMP_DIR)/install-$(PACKAGE_PNG)
+$(SDL_CONFIG_SCRIPT): $(TIMESTAMP_DIR)/install-$(PACKAGE_SDL)
+$(BUILD_DIR)/$(PACKAGE_SDL_IMAGE)/Makefile: \
+  $(SOURCE_DIR)/$(PACKAGE_SDL_IMAGE) $(PNG_CONFIG_SCRIPT) $(SDL_CONFIG_SCRIPT)
 	mkdir -p $(@D)
 	cd $(@D) && $(PWD)/$</configure \
 		--disable-bmp \
@@ -72,34 +108,45 @@ $(STATIC_DIR)/build-SDL_IMAGE/Makefile: \
 		--disable-tif \
 		--disable-xcf \
 		--disable-xpm \
-		--prefix=$(PWD)/$(STATIC_DIR)/install \
-		CFLAGS="$(shell $(PWD)/$(STATIC_DIR)/install/bin/libpng12-config --cflags)"
+		--prefix=$(PWD)/$(INSTALL_DIR) \
+		CFLAGS="$(shell $(PWD)/$(INSTALL_DIR)/bin/libpng12-config --cflags)"
 
 # Configure libpng.
-$(STATIC_DIR)/build-PNG/Makefile: \
-  $(STATIC_DIR)/$(PACKAGE_PNG)
+$(BUILD_DIR)/$(PACKAGE_PNG)/Makefile: \
+  $(SOURCE_DIR)/$(PACKAGE_PNG)
 	mkdir -p $(@D)
 	cd $(@D) && $(PWD)/$</configure \
-		--prefix=$(PWD)/$(STATIC_DIR)/install
+		--prefix=$(PWD)/$(INSTALL_DIR)
 
 # Don't configure GLEW.
 # GLEW does not support building outside of the source tree, so just copy
 # everything over (it's a small package).
-$(STATIC_DIR)/build-GLEW/Makefile: \
-  $(STATIC_DIR)/glew
+$(BUILD_DIR)/$(PACKAGE_GLEW)/Makefile: \
+  $(SOURCE_DIR)/$(PACKAGE_GLEW)
+	mkdir -p $(dir $(@D))
+	rm -rf $(@D)
 	cp -r $< $(@D)
 
-# Extract standardized packages.
-$(foreach PACKAGE,$(PACKAGES_STD),$(STATIC_DIR)/$(PACKAGE_$(PACKAGE))): \
-  $(STATIC_DIR)/%: $(SOURCES_DIR)/%.tar.gz
-	mkdir -p $(STATIC_DIR)
-	tar -zxf $< -C $(STATIC_DIR)
+# Extract packages.
+$(foreach PACKAGE,$(PACKAGES_STD),$(SOURCE_DIR)/$(PACKAGE_$(PACKAGE))): \
+  $(SOURCE_DIR)/%: $(TARBALLS_DIR)/%.tar.gz
+	mkdir -p $(@D)
+	tar -zxf $< -C $(@D)
 	test ! -e $(SUPPORTDIR)/$(<F:%.tar.gz=%.diff) || patch -p1 -N -u -d $@ < $(SUPPORTDIR)/$(<F:%.tar.gz=%.diff)
 	touch $@
 
 # Extract GLEW.
-$(STATIC_DIR)/glew: $(SOURCES_DIR)/$(PACKAGE_GLEW)-src.tgz
-	mkdir -p $(STATIC_DIR)
-	tar -zxf $< -C $(STATIC_DIR)
+$(SOURCE_DIR)/$(PACKAGE_GLEW): $(TARBALLS_DIR)/$(TARBALL_GLEW)
+	rm -rf $@
+	mkdir -p $(@D)
+	tar -zxf $< -C $(@D)
+	mv $(@D)/glew $@
 	test ! -e $(SUPPORTDIR)/$(<F:%-src.tgz=%.diff) || patch -p1 -N -u -d $@ < $(SUPPORTDIR)/$(<F:%-src.tgz=%.diff)
 	touch $@
+
+# Download source packages.
+TARBALLS:=$(foreach PACKAGE,$(PACKAGES),$(TARBALLS_DIR)/$(TARBALL_$(PACKAGE)))
+download: $(TARBALLS)
+$(TARBALLS):
+	mkdir -p $(@D)
+	curl --fail --location -o $@ $(DOWNLOAD_$(call findpackage,TARBALL,$(@F)))/$(@F)
