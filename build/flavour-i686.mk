bec287c57cbecc0f020f6cfa7b4c041704ada8a9
Maarten ter Huurne <maarten@treewalker.org>
2007-03-25 16:55:43 +0000

    Optimise for Pentium 3, but stay compatible with Pentium 2.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6217 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/flavour-i686.mk
+++ build/flavour-i686.mk
@@ -1,10 +1,10 @@
 # $Id$
 #
 # Configuration for "i686" flavour:
-# Optimised for Pentium 2 and higher.
+# Optimised for Pentium 3, but compatible with Pentium 2 and higher.
 
 # Start with generic optimisation flags.
 include build/flavour-opt.mk
 
 # Add x86 specific flags.
-CXXFLAGS+=-march=i686
+CXXFLAGS+=-march=i686 -mtune=pentium3
