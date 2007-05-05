b9a4355d142d01ed60ef4e3c9688e89bb672824a
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-05-05 15:09:29 +0000

    fixed modified registers
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6440 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/Scanline.cc
+++ src/video/Scanline.cc
@@ -154,7 +154,8 @@ void Scanline<Pixel>::draw(const Pixel* src1, const Pixel* src2,
 			, "r" (factor << 8)  // 3
 			, "r" (-4 * width)   // 4
 			#ifdef __MMX__
-			: "mm0", "mm1", "mm2", "mm3", "mm4", "mm5", "mm6", "mm7"
+			: "xmm0", "xmm1", "xmm2", "xmm3",
+			  "xmm4", "xmm5", "xmm6", "xmm7"
 			#endif
 		);
 		return;
