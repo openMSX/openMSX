14d0095a83284171dfd77ea056877677b27885b4
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-06-02 14:12:04 +0000

    implemented debug reads for PasswordCart / tiny cleanups
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6579 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/PasswordCart.cc
+++ src/PasswordCart.cc
@@ -1,4 +1,4 @@
-//
+// $Id$
 
 /*
  * Password Cartridge
@@ -10,45 +10,45 @@
  */
 
 #include "PasswordCart.hh"
-#include "MSXCPUInterface.hh"
-#include "MSXMotherBoard.hh"
 #include "XMLElement.hh"
+#include <algorithm>
 
 namespace openmsx {
 
-PasswordCart::PasswordCart(
-	MSXMotherBoard& motherBoard, const XMLElement& config,
-	const EmuTime& time)
+PasswordCart::PasswordCart(MSXMotherBoard& motherBoard,
+                           const XMLElement& config, const EmuTime& time)
 	: MSXDevice(motherBoard, config, time)
 {
 	password = config.getChildDataAsInt("password", 0);
 	reset(time);
 }
 
-void PasswordCart::reset(const EmuTime& time)
+void PasswordCart::reset(const EmuTime& /*time*/)
 {
 	pointer = 3;
 }
 
-void PasswordCart::writeIO(word port, byte value, const EmuTime& /*time*/)
+void PasswordCart::writeIO(word /*port*/, byte value, const EmuTime& /*time*/)
 {
 	pointer = (value == 0) ? 0 : 3;
 }
 
-byte PasswordCart::readIO(word port, const EmuTime& /*time*/)
+byte PasswordCart::readIO(word port, const EmuTime& time)
+{
+	byte result = peekIO(port, time);
+	pointer = std::min(3, pointer + 1);
+	return result;
+}
+
+byte PasswordCart::peekIO(word /*port*/, const EmuTime& /*time*/) const
 {
 	switch (pointer) {
 	case 0:
-		++pointer;
 		return 0xaa;
 	case 1:
-		++pointer;
 		return password >> 8;
 	case 2:
-		++pointer;
 		return password & 0xff;
-	case 3:
-		return 0xff;
 	default:
 		return 0xff;
 	}
--- src/PasswordCart.hh
+++ src/PasswordCart.hh
@@ -1,4 +1,4 @@
-//
+// $Id$
 
 #ifndef PASSWORDCART_HH
 #define PASSWORDCART_HH
@@ -11,15 +11,16 @@ class PasswordCart : public MSXDevice
 {
 public:
 	PasswordCart(MSXMotherBoard& motherBoard, const XMLElement& config,
-		     const EmuTime& time);
+	             const EmuTime& time);
 
 	virtual void reset(const EmuTime& time);
 	virtual void writeIO(word port, byte value, const EmuTime& time);
 	virtual byte readIO(word port, const EmuTime& time);
+	virtual byte peekIO(word port, const EmuTime& time) const;
 
 private:
-	byte pointer;
 	word password;
+	byte pointer;
 };
 
 } // namespace
