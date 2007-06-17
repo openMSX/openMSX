5311ce0cd07952d34111deefa4393cfdce740ef7
Maarten ter Huurne <maarten@treewalker.org>
2007-06-17 13:19:31 +0000

    Add the zlib header dir as -I to the libpng CPPFLAGS.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6640 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/3rdparty.mk
+++ build/3rdparty.mk
@@ -132,15 +132,16 @@ $(BUILD_DIR)/$(PACKAGE_SDL_IMAGE)/Makefile: \
 		LDFLAGS="$(shell $(PWD)/$(INSTALL_DIR)/bin/libpng12-config --static --ldflags)"
 
 # Configure libpng.
-# The CPPFLAGS definition is a workaround for a bug in the configure script of
-# libpng 1.2.18 (reported as bug 1738534).
+# The "-I$(PWD)/$<" part of the CPPFLAGS definition is a workaround for a bug
+# in the configure script of libpng 1.2.18 (reported as bug 1738534).
 $(BUILD_DIR)/$(PACKAGE_PNG)/Makefile: \
   $(SOURCE_DIR)/$(PACKAGE_PNG) \
   $(foreach PACKAGE,$(filter-out $(SYSTEM_LIBS),ZLIB),$(TIMESTAMP_DIR)/install-$(PACKAGE_$(PACKAGE)))
 	mkdir -p $(@D)
 	cd $(@D) && $(PWD)/$</configure \
 		--prefix=$(PWD)/$(INSTALL_DIR) \
-		CFLAGS="$(_CFLAGS)" CPPFLAGS=-I$(PWD)/$< \
+		CFLAGS="$(_CFLAGS)" \
+		CPPFLAGS="-I$(PWD)/$(INSTALL_DIR)/include -I$(PWD)/$<" \
 		LDFLAGS=-L$(PWD)/$(INSTALL_DIR)/lib
 
 # Configure zlib.
