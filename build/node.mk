5d143abb6bd43be13d9a962bd9067fb51e386501
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-08-14 18:19:25 +0000

    include entry.mk in packaging
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6836 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -7,7 +7,7 @@ SUBDIRS:= \
 
 DIST:= \
 	config.guess detectsys.sh tcl-search.sh install-recursive.sh \
-	main.mk node-end.mk node-start.mk \
+	main.mk node-end.mk node-start.mk entry.mk \
 	info2code.mk components.mk probe.mk probe-results.mk \
 	version.mk custom.mk 3rdparty.mk \
 	flavour-*.mk cpu-*.mk platform-*.mk
