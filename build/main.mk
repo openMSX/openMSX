f524c01417503395b85f331a45e0717598105ce8
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-08-21 19:50:20 +0000

    don't compile with -Wold-style-cast by default: glew-1.4.0 has redefined some macros to contain casts. There is no workaround for this. It's also possible future versions of other libs do something similar
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6846 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/main.mk
+++ build/main.mk
@@ -331,7 +331,7 @@ ifneq ($(filter %g++,$(CXX))$(filter g++%,$(CXX)),)
   # Generic compilation flags.
   COMPILE_FLAGS+=-pipe
   # Stricter warning and error reporting.
-  COMPILE_FLAGS+=-Wall -Wold-style-cast
+  COMPILE_FLAGS+=-Wall
   # Empty definition of used headers, so header removal doesn't break things.
   DEPEND_FLAGS+=-MP
   # Plain C compiler, for the 3rd party libs.
