9f10378eed4c1a1c507be28530344ddbf9b127ba
Maarten ter Huurne <maarten@treewalker.org>
2007-08-30 00:34:27 +0000

    Pass OPENMSX_TARGET_CPU to probe.mk; platform-darwin.mk will use this to select the right SDK version.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@6874 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/main.mk
+++ build/main.mk
@@ -453,7 +453,9 @@ endif
 # TODO: It would be cleaner to include probe.mk and probe-results.mk,
 #       instead of executing them in a sub-make.
 $(PROBE_MAKE): $(PROBE_SCRIPT) $(MAKE_PATH)/custom.mk $(MAKE_PATH)/tcl-search.sh
-	@OUTDIR=$(@D) OPENMSX_TARGET_OS=$(OPENMSX_TARGET_OS) \
+	@OUTDIR=$(@D) \
+		OPENMSX_TARGET_OS=$(OPENMSX_TARGET_OS) \
+		OPENMSX_TARGET_CPU=$(OPENMSX_TARGET_CPU) \
 		COMPILE="$(CXX) $(TARGET_FLAGS)" \
 		STATIC_INSTALL_DIR=$(STATIC_INSTALL_DIR) \
 		$(MAKE) --no-print-directory -f $<
