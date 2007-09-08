007e57c9bec971e3060834cd31c8ba174f266195
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-09-08 21:21:29 +0000

    Removed some outdated bits.
    git-svn-id: file:///openmsx-svn/code/openmsx/branches/branch_0_6_3@6912 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-linux.mk
+++ build/platform-linux.mk
@@ -1,6 +1,6 @@
 # $Id$
 #
-# Configuration for Linux on x86 machines.
+# Configuration for Linux machines.
 
 # Does platform support symlinks?
 USE_SYMLINK:=true
@@ -10,8 +10,3 @@ OPENMSX_CXX?=g++
 
 # File name extension of executables.
 EXEEXT:=
-
-# TODO: Temp to force g++ 3.4-pre to pick up the right libs.
-ifeq ($(OPENMSX_FLAVOUR),gcc34)
-LINK_FLAGS:=-L/opt/gcc-cvs/lib $(LINK_FLAGS)
-endif
