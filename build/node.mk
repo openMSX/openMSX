3843158ae4ca51ffd9e3f0c83fe8d1d227ed7f55
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-09-14 12:16:11 +0000

    Include new file "bindist.mk" in source distribution.
    git-svn-id: file:///openmsx-svn/code/openmsx/branches/branch_0_6_3@6996 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -9,7 +9,7 @@ DIST:= \
 	config.guess detectsys.sh tcl-search.sh install-recursive.sh \
 	main.mk node-end.mk node-start.mk entry.mk \
 	info2code.mk components.mk probe.mk probe-results.mk \
-	version.mk custom.mk 3rdparty.mk \
+	version.mk custom.mk 3rdparty.mk bindist.mk \
 	flavour-*.mk cpu-*.mk platform-*.mk
 
 include build/node-end.mk
