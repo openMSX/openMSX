e220c7d204741625814d033271dab14f6bf1718f
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-09-17 12:53:27 +0000

    Link GLEW statically.
    git-svn-id: file:///openmsx-svn/code/openmsx/branches/branch_0_6_3@7029 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-mingw32.mk
+++ build/platform-mingw32.mk
@@ -30,8 +30,9 @@ LINK_FLAGS:=-L/mingw/lib -L/mingw/lib/w32api -lwsock32 -lwinmm -ldsound \
 
 GL_LDFLAGS:=-lopengl32
 
+GLEW_CFLAGS_3RD_STA+=-DGLEW_STATIC
 GLEW_LDFLAGS_SYS_DYN:=-lglew32
-GLEW_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libglew32.dll.a
+GLEW_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libglew32.a $(GL_LDFLAGS)
 
 SDL_LDFLAGS_SYS_DYN:=`sdl-config --libs | sed -e 's/-mwindows/-mconsole/' 2>> $(LOG)`
 SDL_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libSDL.a /mingw/lib/libmingw32.a $(3RDPARTY_INSTALL_DIR)/lib/libSDLmain.a -lwinmm -lgdi32
