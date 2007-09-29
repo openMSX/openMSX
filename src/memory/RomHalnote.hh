756fd622b1aeaa54335f47daab7ea89183d9caef
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-09-29 09:35:45 +0000

    updated Halnote mapper (based on implementation in blueMSX)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7098 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/RomHalnote.hh
+++ src/memory/RomHalnote.hh
@@ -7,15 +7,26 @@
 
 namespace openmsx {
 
+class SRAM;
+
 class RomHalnote : public Rom8kBBlocks
 {
 public:
 	RomHalnote(MSXMotherBoard& motherBoard, const XMLElement& config,
 	           const EmuTime& time, std::auto_ptr<Rom> rom);
+	virtual ~RomHalnote();
 
 	virtual void reset(const EmuTime& time);
+	virtual byte readMem(word address, const EmuTime& time);
+	virtual const byte* getReadCacheLine(word address) const;
 	virtual void writeMem(word address, byte value, const EmuTime& time);
 	virtual byte* getWriteCacheLine(word address) const;
+
+private:
+	const std::auto_ptr<SRAM> sram;
+	byte subBanks[2];
+	bool sramEnabled;
+	bool subMapperEnabled;
 };
 
 } // namespace openmsx
