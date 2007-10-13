01b2f6aba411119f7e95b5564c5464b464641838
Patrick van Arkel <vampier@users.sourceforge.net>
2007-10-13 18:42:54 +0000

    screen 0/1 detection implemented
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7137 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/catapult.tcl
+++ share/scripts/catapult.tcl
@@ -1,18 +1,36 @@
 # Catapult Helper File (for use with openMSX Catapult)
 
+# Catapult Helper File (for use with openMSX Catapult)
+
 proc getScreen {} {
-set line ""
-set screen ""
-set base 0
-	for {set y 0} {$y < 23} {incr y } {
-		set base [expr {$y * [peek 0xf3b0]}]
-		for {set x 0} {$x < [peek 0xf3b0]} {incr x } {
-			append line  [format %c [debug read VRAM [expr {$x + $base}]]]
-		}
+	#screen detection
+
+	if {[peek 0xfcaf]>1} {return [concat "Screen Mode " [peek 0xfcaf] " Not Supported"]}
+
+	if {[peek 0xfcaf]==0} 	{
+				#screen 0
+					if {[debug read VDP\ regs 0] & 0x04} {set chars 80} else {set chars 40}
+					set base 0
+				} else {
+				#screen 1
+					set base 6144
+					set chars 32
+
+				}
+
+	set line ""
+	set screen ""
+
+	#scrape screen and build string
+		for {set y 0} {$y < 23} {incr y } {
+			set row [expr {$y * $chars}]
+			for {set x 0} {$x < $chars} {incr x } {
+				append line  [format %c [debug read VRAM [expr {$base + $x + $row}]]]
+			}
 		append screen [string trim $line]
 		append screen "\n"
 		set line ""
-	}
-return  [string trim $screen]
-}
+		}
 
+	return  [string trim $screen]
+}
