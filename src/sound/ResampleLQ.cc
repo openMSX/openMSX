a9f7c1d5cd42808c493d1bc0a585e8d3636c71f9
Wouter Vermaelen <m9710797@users.sourceforge.net>
2007-12-05 21:26:52 +0000

    simplified ResampleLQ
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7359 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/sound/ResampleLQ.cc
+++ src/sound/ResampleLQ.cc
@@ -2,80 +2,57 @@
 
 #include "ResampleLQ.hh"
 #include "Resample.hh"
-#include <algorithm>
 #include <cassert>
 #include <cstring>
 
 namespace openmsx {
 
+static const int BUFSIZE = 16384;
+static int buffer[BUFSIZE];
+
 template <unsigned CHANNELS>
 ResampleLQ<CHANNELS>::ResampleLQ(Resample& input_, double ratio)
-	: input(input_), pos(0), step(ratio), bufStart(0), bufEnd(0)
+	: input(input_), pos(0), step(ratio)
 {
-}
-
-template <unsigned CHANNELS>
-void ResampleLQ<CHANNELS>::fillBuffer(unsigned pos, unsigned num)
-{
-	if (!input.generateInput(&buffer[pos * CHANNELS], num)) {
-		memset(&buffer[pos * CHANNELS], 0, num * CHANNELS * sizeof(int));
-	}
-}
-
-template <unsigned CHANNELS>
-void ResampleLQ<CHANNELS>::prepareData(unsigned missing)
-{
-	unsigned free = BUF_LEN - bufEnd;
-	if (missing < free) {
-		// new data fits without wrapping
-		fillBuffer(bufEnd, missing); // 3 extra, ok
-		bufEnd += missing;
-		assert(bufEnd < BUF_LEN);
-	} else {
-		// partly fill at end, partly at begin
-		fillBuffer(bufEnd, free); // 3 extra, ok
-		fillBuffer(0, missing - free); // 3 extra, ok
-		bufEnd = missing - free;
-		assert(bufEnd < bufStart);
+	assert(step >= Pos(1)); // can only do downsampling
+	for (unsigned j = 0; j < CHANNELS; ++j) {
+		lastInput[j] = 0;
 	}
 }
 
 template <unsigned CHANNELS>
 bool ResampleLQ<CHANNELS>::generateOutput(int* dataOut, unsigned num)
 {
-	bool anyNonZero = false;
-	for (unsigned i = 0; i < num; ++i) {
-		// interpolate between these two positions
-		int p0 = pos.toInt();
-		int p1 = p0 + 1;
-
-		// need to reload buffer?
-		int available = (bufEnd - bufStart) & BUF_MASK;
-		if (available <= p1) {
-			int request = std::min<int>((step * (num - i)).toInt() + 2 + 1,
-			                            BUF_LEN - 1);
-			int missing = request - available;
-			assert(missing > 0);
-			prepareData(missing);
+	Pos end = pos + step * num;
+	int numInput = end.toInt();
+	assert((numInput + 1) <= BUFSIZE);
+	if (!input.generateInput(&buffer[1 * CHANNELS], numInput)) {
+		int last = 0;
+		for (unsigned j = 0; j < CHANNELS; ++j) {
+			last |= lastInput[j];
 		}
-
-		// actually interpolate
-		int i0 = (p0 + bufStart) & BUF_MASK;
-		int i1 = (p1 + bufStart) & BUF_MASK;
+		if (last == 0) {
+			return false;
+		}
+		memset(&buffer[CHANNELS], 0, numInput * CHANNELS * sizeof(int));
+	}
+	for (unsigned j = 0; j < CHANNELS; ++j) {
+		buffer[j] = lastInput[j];
+		lastInput[j] = buffer[numInput * CHANNELS + j];
+	}
+	for (unsigned i = 0; i < num; ++i) {
+		int p = pos.toInt();
 		Pos fract = pos.fract();
 		for (unsigned j = 0; j < CHANNELS; ++j) {
-			int s0 = buffer[i0 * CHANNELS + j];
-			int s1 = buffer[i1 * CHANNELS + j];
+			int s0 = buffer[(p + 0) * CHANNELS + j];
+			int s1 = buffer[(p + 1) * CHANNELS + j];
 			int out = s0 + (fract * (s1 - s0)).toInt();
 			dataOut[i * CHANNELS + j] = out;
-			anyNonZero |= out;
 		}
-
-		// figure out the next index
-		bufStart = (bufStart + p0) & BUF_MASK;
-		pos = fract + step;
+		pos += step;
 	}
-	return anyNonZero;
+	pos = pos.fract();
+	return true;
 }
 
 // Force template instantiation.
--- src/sound/ResampleLQ.hh
+++ src/sound/ResampleLQ.hh
@@ -15,23 +15,14 @@ class ResampleLQ : public ResampleAlgo
 {
 public:
 	ResampleLQ(Resample& input, double ratio);
-
 	virtual bool generateOutput(int* dataOut, unsigned num);
 
 private:
+	Resample& input;
 	typedef FixedPoint<16> Pos;
-	static const unsigned BUF_LEN = 16384;
-	static const unsigned BUF_MASK = BUF_LEN - 1;
-
-	void prepareData(unsigned missing);
-	void fillBuffer(unsigned pos, unsigned num);
-
-        Resample& input;
 	Pos pos;
-	Pos step;
-	unsigned bufStart;
-	unsigned bufEnd;
-	int buffer[BUF_LEN * CHANNELS + 3];
+	const Pos step;
+	int lastInput[CHANNELS];
 };
 
 } // namespace openmsx
