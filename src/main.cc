bc582f601870e3f480098033517a0f892f605f9b
Maarten ter Huurne <maarten@treewalker.org>
2007-12-05 23:57:55 +0000

    Only use SDL_putenv on SDL 1.2.10 and higher.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7361 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/main.cc
+++ src/main.cc
@@ -40,9 +40,13 @@ static void initializeSDL()
 		throw FatalError(string("Couldn't init SDL: ") + SDL_GetError());
 	}
 
+// In SDL 1.2.9 and before SDL_putenv has different semantics and is not
+// guaranteed to exist on all platforms.
+#if SDL_VERSION_ATLEAST(1, 2, 10)
 	// On Mac OS X, send key combos like Cmd+H and Cmd+M to Cocoa, so it can
 	// perform the corresponding actions.
 	SDL_putenv(const_cast<char*>("SDL_ENABLEAPPEVENTS=1"));
+#endif
 }
 
 static void unexpectedExceptionHandler()
