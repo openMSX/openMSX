1dc9620479aa162a821e3283d56bb3e3e11b1376
Arnold Metselaar <arnoldmnl@users.sourceforge.net>
2007-12-09 20:56:51 +0000

    Another bugfix in diskmanipulator timestamps
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7400 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/fdc/MSXtar.cc
+++ src/fdc/MSXtar.cc
@@ -827,7 +827,7 @@ void MSXtar::changeTime(string resultFile, MSXDirEntry& direntry)
 	struct tm mtim;
 	struct utimbuf utim;
 	mtim.tm_sec   =  (t & 0x001f) << 1;
-	mtim.tm_min   =  (t & 0x03e0) >> 5;
+	mtim.tm_min   =  (t & 0x07e0) >> 5;
 	mtim.tm_hour  =  (t & 0xf800) >> 11;
 	mtim.tm_mday  =  (d & 0x001f);
 	mtim.tm_mon   = ((d & 0x01e0) >> 5) - 1;
