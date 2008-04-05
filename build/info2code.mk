565ea8a7360c1c382a6be356d653d61331a1d048
Maarten ter Huurne <maarten@treewalker.org>
2008-04-05 16:30:02 +0000

    DATADIR should be the location of the "share" dir at run time, which might be different from INSTALL_SHARE_DIR, since that is overridden to a build dir when building binary packages.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7734 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/info2code.mk
+++ build/info2code.mk
@@ -45,7 +45,7 @@ $(CONFIG_HEADER): $(MAKE_PATH)/info2code.mk $(MAKE_PATH)/custom.mk
 	@echo "static const bool OPENMSX_BIGENDIAN = $(BIG_ENDIAN);" >> $@
 	@echo "static const bool OPENMSX_UNALIGNED_MEMORY_ACCESS = $(UNALIGNED_MEMORY_ACCESS);" >> $@
 	@echo "static const bool OPENMSX_SET_WINDOW_ICON = $(SET_WINDOW_ICON);" >> $@
-	@echo "static const std::string DATADIR = \"$(INSTALL_SHARE_DIR)\";" >> $@
+	@echo "static const std::string DATADIR = \"$(INSTALL_BASE)/share\";" >> $@
 	@echo "static const std::string BUILD_FLAVOUR = \"$(OPENMSX_FLAVOUR)\";" >> $@
 	@echo "static const std::string BUILD_COMPONENTS = \"$(sort $(COMPONENTS_TRUE))\";" >> $@
 	@echo "" >> $@
