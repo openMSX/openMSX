28a09546ba4bcc3bb3476bcf8068690b1b7b7f8e
David Heremans <turbor@users.sourceforge.net>
2008-04-07 18:26:23 +0000

    a new skin with frame
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7741 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ share/skins/fancy/skin.tcl
@@ -0,0 +1,10 @@
+set xbase 0
+set ybase 32
+set xwidth 25
+set yheight 30
+set xspacing 32
+set yspacing 32
+set scale 4
+set ::__ledtime 0
+#osd destroy osd_frame
+#osd create rectangle osd_frame -image $::env(OPENMSX_USER_DATA)/skins/fancy/frame.png -x 0 -y 0 -z -1 -scale 0.75
