bd0280eeb844a87b7995ec0c419a6d2e4ea9427d
Wouter Vermaelen <m9710797@users.sourceforge.net>
2008-05-24 19:34:29 +0000

    moved static page-breaks in RDMEM/WRMEM to static const table
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7849 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/cpu/R800.hh
+++ src/cpu/R800.hh
@@ -73,7 +73,8 @@ protected:
 	{
 		int newPage = address >> 8;
 		if (PRE_PB) {
-			add(1); // TODO move to static cost table
+			// there is a statically predictable page break at this
+			// point -> 'add(1)' moved to static cost table
 		} else {
 			if (unlikely(newPage != lastPage) ||
 			    unlikely(extraMemoryDelay[address >> 14])) {
@@ -135,24 +136,22 @@ protected:
 	}
 
 	static const int I  = 8; // cycles for an I/O operation
-	static const int PW = 1; // cycles for a write operation (pre)   // TODO remove
-	static const int W  = 2; // cycles for a write operation (total) // TODO remove
 	static const int P  = 1; // cycles for a (statically known) page-break
 
 	static const unsigned
-		CC_LD_A_SS   = 2,     CC_LD_A_SS_1  = 1,
-		CC_LD_A_NN   = 4,     CC_LD_A_NN_1  = 1, CC_LD_A_NN_2  = 3,
+		CC_LD_A_SS   = 1+P+1, CC_LD_A_SS_1  = 1+P,
+		CC_LD_A_NN   = 3+P+1, CC_LD_A_NN_1  = 1, CC_LD_A_NN_2  = 3+P,
 		CC_LD_A_I    = 2,
 		CC_LD_R_R    = 1,
 		CC_LD_R_N    = 2,     CC_LD_R_N_1   = 1,
-		CC_LD_R_HL   = 2,     CC_LD_R_HL_1  = 1,
-		CC_LD_R_XIX  = 4,     CC_LD_R_XIX_1 = 1, CC_LD_R_XIX_2 = 3, // +1
-		CC_LD_HL_R   = 1+W,   CC_LD_HL_R_1  = 1+PW,
-		CC_LD_HL_N   = 2+W,   CC_LD_HL_N_1  = 1, CC_LD_HL_N_2  = 2+PW,
-		CC_LD_SS_A   = 1+W,   CC_LD_SS_A_1  = 1+PW,
-		CC_LD_NN_A   = 3+W,   CC_LD_NN_A_1  = 1, CC_LD_NN_A_2  = 3+PW,
-		CC_LD_XIX_R  = 3+W,   CC_LD_XIX_R_1 = 1, CC_LD_XIX_R_2 = 3+PW, // +1
-		CC_LD_XIX_N  = 3+W,   CC_LD_XIX_N_1 = 1, CC_LD_XIX_N_2 = 3+PW, // +1
+		CC_LD_R_HL   = 1+P+1, CC_LD_R_HL_1  = 1+P,
+		CC_LD_R_XIX  = 3+P+1, CC_LD_R_XIX_1 = 1, CC_LD_R_XIX_2 = 3+P, // +1
+		CC_LD_HL_R   = 1+P+1, CC_LD_HL_R_1  = 1+P,
+		CC_LD_HL_N   = 2+P+1, CC_LD_HL_N_1  = 1, CC_LD_HL_N_2  = 2+P,
+		CC_LD_SS_A   = 1+P+1, CC_LD_SS_A_1  = 1+P,
+		CC_LD_NN_A   = 3+P+1, CC_LD_NN_A_1  = 1, CC_LD_NN_A_2  = 3+P,
+		CC_LD_XIX_R  = 3+P+1, CC_LD_XIX_R_1 = 1, CC_LD_XIX_R_2 = 3+P, // +1
+		CC_LD_XIX_N  = 3+P+1, CC_LD_XIX_N_1 = 1, CC_LD_XIX_N_2 = 3+P, // +1
 		CC_LD_HL_XX  = 3+P+2, CC_LD_HL_XX_1 = 1, CC_LD_HL_XX_2 = 3+P,
 		CC_LD_SP_HL  = 1,
 		CC_LD_SS_NN  = 3,     CC_LD_SS_NN_1 = 1,
@@ -160,27 +159,27 @@ protected:
 
 		CC_CP_R      = 1,
 		CC_CP_N      = 2,     CC_CP_N_1     = 1,
-		CC_CP_XHL    = 2,     CC_CP_XHL_1   = 1,
-		CC_CP_XIX    = 4,     CC_CP_XIX_1   = 1, CC_CP_XIX_2   = 3, // +1
+		CC_CP_XHL    = 1+P+1, CC_CP_XHL_1   = 1+P,
+		CC_CP_XIX    = 3+P+1, CC_CP_XIX_1   = 1, CC_CP_XIX_2   = 3+P, // +1
 		CC_INC_R     = 1,
-		CC_INC_XHL   = 3+W,   CC_INC_XHL_1  = 1, CC_INC_XHL_2  = 3+PW,
-		CC_INC_XIX   = 5+W,   CC_INC_XIX_1  = 1, EE_INC_XIX    = 2, // +1
+		CC_INC_XHL   = 1+P+2+P+1, CC_INC_XHL_1 = 1, CC_INC_XHL_2 = 1+P+2+P,
+		CC_INC_XIX   = 3+P+2+P+1, CC_INC_XIX_1 = 1, EE_INC_XIX   = 2, // +1
 		CC_INC_SS    = 1,
 		CC_ADD_HL_SS = 1,
 		CC_ADC_HL_SS = 2,
 
-		CC_LDI       = 3+W,   CC_LDI_1      = 2, CC_LDI_2      = 3+PW,
-		CC_LDIR      = 3+W+1,
-		CC_CPI       = 4,     CC_CPI_1      = 2,
-		CC_CPIR      = 5,
+		CC_LDI       = 2+P+1+P+1, CC_LDI_1  = 2+P, CC_LDI_2    = 2+P+1+P,
+		CC_LDIR      = 2+P+1+P+2,
+		CC_CPI       = 2+P+2, CC_CPI_1      = 2+P,
+		CC_CPIR      = 2+P+3,
 
 		CC_PUSH      = 2+P+2, CC_PUSH_1     = 2+P,
 		CC_POP       = 1+P+2, CC_POP_1      = 1+P,
-		CC_CALL      = 4+W+W, CC_CALL_1     = 1, EE_CALL       = 2,
-		CC_CALL_A    = 4+W+W, CC_CALL_B     = 3,
-		CC_RST       = 2+W+W,
-		CC_RET_A     = 3,     CC_RET_B      = 1, EE_RET_C      = 0,
-		CC_RETN      = 5,     EE_RETN       = 2,
+		CC_CALL      = 3+P+2, CC_CALL_1     = 1, EE_CALL       = 1,
+		CC_CALL_A    = 3+P+2, CC_CALL_B     = 3,
+		CC_RST       = 2+P+2, // TODO check
+		CC_RET_A     = 1+P+2, CC_RET_B      = 1, EE_RET_C      = 0,
+		CC_RETN      = 2+P+2, EE_RETN       = 1, // TODO check
 		CC_JP        = 4,     CC_JP_1       = 1,
 		CC_JP_HL     = 2,
 		CC_JR_A      = 3,     CC_JR_B       = 2, CC_JR_1       = 1,
@@ -189,22 +188,22 @@ protected:
 		CC_EX_SP_HL  = 1+P+4, CC_EX_SP_HL_1 = 1+P, CC_EX_SP_HL_2 = 1+P+2,
 
 		CC_BIT_R     = 2,
-		CC_BIT_XHL   = 3,     CC_BIT_XHL_1  = 2,
-		CC_BIT_XIX   = 4,     CC_BIT_XIX_1  = 3,                    // +1
+		CC_BIT_XHL   = 2+P+1, CC_BIT_XHL_1  = 2+P,
+		CC_BIT_XIX   = 3+P+1, CC_BIT_XIX_1  = 3+P, // +1
 		CC_SET_R     = 2,
-		CC_SET_XHL   = 4+W,   CC_SET_XHL_1  = 2, CC_SET_XHL_2  = 4+PW,
-		CC_SET_XIX   = 5+W,   EE_SET_XIX    = 1,                    // +1
+		CC_SET_XHL   = 2+P+2+P+1, CC_SET_XHL_1 = 2+P, CC_SET_XHL_2 = 2+P+2+P,
+		CC_SET_XIX   = 3+P+2+P+1, EE_SET_XIX   = 1, // +1
 
 		CC_RLA       = 1,
-		CC_RLD       = 4+W,   CC_RLD_1      = 2, CC_RLD_2      = 4+PW,
+		CC_RLD       = 2+P+2+P+1, CC_RLD_1  = 2+P, CC_RLD_2    = 2+P+2+P,
 
 		CC_IN_A_N    = 2+I,   CC_IN_A_N_1   = 1, CC_IN_A_N_2   = 2,
 		CC_IN_R_C    = 2+I,   CC_IN_R_C_1   = 2,
-		CC_INI       = 2+I+W, CC_INI_1      = 2, CC_INI_2      = 2+I+PW,
-		CC_INIR      = 2+I+W+1,
+		CC_INI       = 2+I+P+1, CC_INI_1    = 2, CC_INI_2      = 2+I+P,
+		CC_INIR      = 2+I+P+2,
 		CC_OUT_N_A   = 2+I,   CC_OUT_N_A_1  = 1, CC_OUT_N_A_2  = 2,
 		CC_OUT_C_R   = 2+I,   CC_OUT_C_R_1  = 2,
-		CC_OUTI      = 3+I,   CC_OUTI_1     = 2, CC_OUTI_2     = 3,
+		CC_OUTI      = 2+P+1+I, CC_OUTI_1   = 2+P, CC_OUTI_2   = 2+P+1,
 		CC_OTIR      = 3+I+1,
 
 		CC_EX        = 1,
@@ -222,9 +221,9 @@ protected:
 		CC_MULUB     = 14,
 		CC_MULUW     = 36,
 
-		CC_NMI       = 1+W+W,   EE_NMI_1      = -1,                    // TODO check
-		CC_IRQ0      = 1+W+W,   EE_IRQ0_1     = -1,                    // TODO check
-		CC_IRQ1      = 1+W+W,   EE_IRQ1_1     = -1,                    // TODO check
+		CC_NMI       = 1+P+2,   EE_NMI_1      = -1,                    // TODO check
+		CC_IRQ0      = 1+P+2,   EE_IRQ0_1     = -1,                    // TODO check
+		CC_IRQ1      = 1+P+2,   EE_IRQ1_1     = -1,                    // TODO check
 		CC_IRQ2      = 1+P+2+P+2, EE_IRQ2_1   = -1, CC_IRQ2_2 = 1+P+2+P, // TODO check
 
 		CC_MAIN      = 0,
