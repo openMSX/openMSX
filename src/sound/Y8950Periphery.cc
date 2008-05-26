fb6ae3a07207bf4282270f2ff2255138624ee213
Wouter Vermaelen <m9710797@users.sourceforge.net>
2008-05-26 18:40:05 +0000

    forgot to add file
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7865 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ src/sound/Y8950Periphery.cc
@@ -0,0 +1,34 @@
+// $Id$
+
+#include "Y8950Periphery.hh"
+#include "MSXDevice.hh"
+
+namespace openmsx {
+
+Y8950Periphery::~Y8950Periphery()
+{
+}
+
+void Y8950Periphery::reset()
+{
+	// nothing
+}
+
+byte Y8950Periphery::readMem(word address, const EmuTime& time)
+{
+	return 0xFF;
+}
+void Y8950Periphery::writeMem(word address, byte value, const EmuTime& time)
+{
+	// nothing
+}
+const byte* Y8950Periphery::getReadCacheLine(word start) const
+{
+	return MSXDevice::unmappedRead;
+}
+byte* Y8950Periphery::getWriteCacheLine(word start) const
+{
+	return MSXDevice::unmappedWrite;
+}
+
+} // namespace openmsx
