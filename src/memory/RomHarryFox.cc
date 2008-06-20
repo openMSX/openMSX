938bd70b76e0016d44670530f047e975e5e735fd
Wouter Vermaelen <m9710797@users.sourceforge.net>
2008-06-20 21:30:34 +0000

    improved HarryFox mapper type implementation (thanks to enen)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7914 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/RomHarryFox.cc
+++ src/memory/RomHarryFox.cc
@@ -1,7 +1,13 @@
 // $Id$
 
+/** Thanks to enen for testing this on a real cartridge:
+ *
+ * Writing to 0x6xxx, 0 or 1, will switch bank 0 or 2 into 0x4000-0x7fff
+ * Writing to 0x7xxx, 0 or 1, will switch bank 1 or 3 into 0x8000-0xbfff
+ * 0x0000-0x3fff and 0xc000-0xffff are unmapped (read as 0xff)
+ */
+
 #include "RomHarryFox.hh"
-#include "CacheLine.hh"
 #include "Rom.hh"
 
 namespace openmsx {
@@ -23,18 +29,16 @@ void RomHarryFox::reset(const EmuTime& /*time*/)
 
 void RomHarryFox::writeMem(word address, byte value, const EmuTime& /*time*/)
 {
-	if (address == 0x6000) {
-		setRom(1, 2 * (value & 1));
-	}
-	if (address == 0x7000) {
+	if        ((0x6000 <= address) && (address < 0x7000)) {
+		setRom(1, 2 * (value & 1) + 0);
+	} else if ((0x7000 <= address) && (address < 0x8000)) {
 		setRom(2, 2 * (value & 1) + 1);
 	}
 }
 
 byte* RomHarryFox::getWriteCacheLine(word address) const
 {
-	if ((address == (0x6000 & CacheLine::HIGH)) ||
-	    (address == (0x7000 & CacheLine::HIGH))) {
+	if ((0x6000 <= address) && (address < 0x8000)) {
 		return NULL;
 	} else {
 		return unmappedWrite;
