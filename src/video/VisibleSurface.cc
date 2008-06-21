a50c69f9db0de04495afa3ce59720363e8bab568
Wouter Vermaelen <m9710797@users.sourceforge.net>
2008-06-21 07:53:47 +0000

    fixed(?) compilation error on win32
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@7915 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/VisibleSurface.cc
+++ src/video/VisibleSurface.cc
@@ -98,7 +98,7 @@ VisibleSurface::~VisibleSurface()
 {
 #ifdef _WIN32
 	// Find our current location.
-	SDL_Surface* surface = getSDLSurface();
+	SDL_Surface* surface = getSDLDisplaySurface();
 	if (surface && ((surface->flags & SDL_FULLSCREEN) == 0)) {
 		HWND handle = GetActiveWindow();
 		RECT windowRect;
