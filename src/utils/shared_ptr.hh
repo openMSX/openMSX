c68ba36372fdf9acf134f8d1569d69fc200b8593
Wouter Vermaelen <m9710797@users.sourceforge.net>
2008-09-03 16:18:20 +0000

    refactored shared_ptr
    prevent problem of delete on incomplete type (see shared_ptr.hh for details)
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@8197 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/shared_ptr.hh
+++ src/utils/shared_ptr.hh
@@ -3,6 +3,20 @@
 #ifndef SHARED_PTR_HH
 #define SHARED_PTR_HH
 
+/** checked_delete utility copied from boost::checked_delete
+  *   http://www.boost.org/doc/libs/1_36_0/libs/utility/checked_delete.html
+  * Is like a normal delete, but given a compilation error (as opposed to a
+  * warning in gcc) when the to be deleted type is an incomplete type.
+  * Deleting an incomplete type with a non-trivial destructor is undefined
+  * behaviour in c++.
+  */
+template<typename T> void checked_delete(T* t)
+{
+	typedef char type_must_be_complete[sizeof(T) ? 1 : -1];
+	(void)sizeof(type_must_be_complete);
+	delete t;
+}
+
 /**
  * Based on boost::shared_ptr
  *   http://www.boost.org/libs/smart_ptr/shared_ptr.htm
@@ -13,30 +27,50 @@
 #include <algorithm>
 #include <cassert>
 
+// The code below looks unnecessarily complex, but it's structured like this
+// to move the delete (checked_delete) operation out of the shared_ptr class.
+// This allows to instantiate shared_ptr with an incomplete type. Only the
+// constructor that creates a shared_ptr from a raw pointer needs a complete
+// type (but it anyway already needs it to be able to do 'new T').
+
+struct shared_ptr_impl_base
+{
+	shared_ptr_impl_base() : count(1) {}
+	virtual ~shared_ptr_impl_base() { assert(count == 0); }
+	unsigned count;
+};
+
+template<typename T> struct shared_ptr_impl : shared_ptr_impl_base
+{
+	shared_ptr_impl(T* t_) : t(t_) {}
+	virtual ~shared_ptr_impl() { checked_delete(t); }
+private:
+	T* t;
+};
+
 template<typename T> class shared_ptr
 {
 public:
 	explicit shared_ptr(T* p = 0)
-		: ptr(p), count(new unsigned(1)) {}
+		: ptr(p), impl(new shared_ptr_impl<T>(p)) {}
 
 	shared_ptr(const shared_ptr<T>& other)
-		: ptr(other.ptr), count(other.count)
+		: ptr(other.ptr), impl(other.impl)
 	{
-		++(*count);
+		++(impl->count);
 	}
 
 	~shared_ptr()
 	{
-		if (--(*count) == 0) {
-			delete ptr;
-			delete count;
+		if (--(impl->count) == 0) {
+			delete impl;
 		}
 	}
 
 	void swap(shared_ptr<T>& other)
 	{
 		std::swap(ptr, other.ptr);
-		std::swap(count, other.count);
+		std::swap(impl, other.impl);
 	}
 
 	shared_ptr& operator=(shared_ptr other) // note: pass by value
@@ -50,7 +84,7 @@ public:
 
 private:
 	T* ptr;
-	unsigned* count;
+	shared_ptr_impl_base* impl;
 };
 
 template<typename T, typename U>
