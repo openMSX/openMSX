ae999b4c8160f796a07663bd79a5ff3287917b55
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2008-11-30 20:48:14 +0000

    Do not use the new inline stuff on gcc 3 with DEBUG enabled (fixes compilation of DEBUG flavour on e.g. Windows with MinGW's default gcc 3.4.5).
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@8376 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/inline.hh
+++ src/utils/inline.hh
@@ -3,7 +3,10 @@
 #ifndef INLINE_HH
 #define INLINE_HH
 
-#if __GNUC__ > 2
+// This inline trick doesn't work on gcc 3.x when using debug flavour
+// so we only enable it in the other cases.
+
+#if __GNUC__ > 3 || (!defined(DEBUG) && __GNUC__ > 2)
 #define ALWAYS_INLINE inline __attribute__((always_inline))
 #define NEVER_INLINE __attribute__((noinline))
 #else
