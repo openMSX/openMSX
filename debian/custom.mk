a228d14a5cc0f88a8dad789e81ea1e96cf330abf
Joost Yervante Damad <joost@damad.be>
2009-01-05 19:37:31 +0000

    preparation for 0.7.0 debian package
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@8435 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ debian/custom.mk
@@ -0,0 +1,8 @@
+INSTALL_BASE:=/usr
+INSTALL_SHARE_DIR:=/usr/share/openmsx
+VERSION_EXEC:=false
+SYMLINK_FOR_BINARY:=false
+INSTALL_CONTRIB:=false
+INSTALL_DOC_DIR:=/usr/share/doc/openmsx-data
+INSTALL_BINARY_DIR:=/usr/bin
+OPENMSX_INSTALL:=/usr
