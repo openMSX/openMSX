7ca62c77d5818fb0691550496302ad1d9068dff3
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-01-12 17:29:02 +0000

    oops, these files belong in src/utils
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@8471 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/win32-dirent.cc
+++ src/utils/win32-dirent.cc
@@ -1,5 +1,5 @@
 /* Copyright (C) 2001, 2006 Free Software Foundation, Inc.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
