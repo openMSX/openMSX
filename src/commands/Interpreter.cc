924ff58fdaf0dd70d8c268001724d3ba8c9a6329
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-02-02 17:48:54 +0000

    Revert "Tcl manual recommends to use new-style channel types"
    This broke compilation on OSX because it still uses libtcl version 8.4.7.
    This patch was purely a cleanup. So even though 8.4.7 is very old, there's
    no real reason to force to upgrade to a newer version.
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@8550 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/commands/Interpreter.cc
+++ src/commands/Interpreter.cc
@@ -45,7 +45,7 @@ static int dummyGetHandle(ClientData /*instanceData*/, int /*direction*/,
 }
 Tcl_ChannelType Interpreter::channelType = {
 	const_cast<char*>("openMSX console"),// Type name
-	TCL_CHANNEL_VERSION_4,   // Version
+	NULL,			 // Always non-blocking
 	dummyClose,		 // Close proc
 	dummyInput,		 // Input proc
 	Interpreter::outputProc, // Output proc
@@ -59,7 +59,6 @@ Tcl_ChannelType Interpreter::channelType = {
 	NULL,			 // Tcl_DriverFlushProc
 	NULL,			 // Tcl_DriverHandlerProc
 	NULL,			 // Tcl_DriverWideSeekProc
-	NULL,			 // Tcl_DriverThreadActionProc
 };
 
 void Interpreter::init(const char* programName)
