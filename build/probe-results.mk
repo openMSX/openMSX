05ca9b1ab834ea6b63e8acbb9a98f5ba62e16ab9
Maarten ter Huurne <maarten@treewalker.org>
2009-02-10 01:14:14 +0000

    Replaced "probe-results" Makefile by Python script.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@8576 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/probe-results.mk
+++ build/probe-results.mk
@@ -15,89 +15,11 @@ endif
 
 include $(PROBE_MAKE)
 include $(MAKE_PATH)/components.mk
-include $(MAKE_PATH)/custom.mk
 
-# Usage: $(call FOUND,LIB_NAME)
-FOUND=$(if $(HAVE_$(1)_LIB),$(HAVE_$(1)_LIB),$(if $(filter $(1),$(DISABLED_LIBS)),disabled,no))
+PYTHON?=python
 
-# Usage: $(call YESNO,true|false)
-YESNO=$(subst true,yes,$(subst false,no,$(1)))
+.PHONY: all
 
-# Usage: $(call HEADER,LIB1 LIB2 ...)
-HEADER=$(if $(strip $(foreach LIB,$(1),$(HAVE_$(LIB)_H))),yes,$(if $(filter $(1)_H,$(DISABLED_HEADERS)),disabled,no))
-
-.PHONY: all no-compiler components components-print \
-	found-all found-enough found-insufficient
-
-ifeq ($(COMPILER),true)
-all: components
-else
-all: no-compiler
-endif
-
-ifeq ($(COMPONENTS_ALL),true)
-components: found-all
-else
-  ifeq ($(COMPONENT_CORE),true)
-components: found-enough
-  else
-components: found-insufficient
-  endif
-endif
-
-no-compiler:
-	@echo ""
-	@echo "No working C++ compiler was found."
-	@echo "Please install a C++ compiler, such as GCC's g++."
-	@echo "If you have a C++ compiler installed and openMSX did not detect it, please set the environment variable OPENMSX_CXX to the name of your C++ compiler."
-	@echo "After you have corrected the situation, rerun \"configure\"."
-	@echo ""
-
-components-print:
-	@echo ""
-	@echo "Found libraries:"
-	@echo "  libpng:          $(call FOUND,PNG)"
-	@echo "  libxml2:         $(call FOUND,XML)"
-	@echo "  OpenGL:          $(call FOUND,GL)"
-	@echo "  GLEW:            $(call FOUND,GLEW)"
-	@echo "  Jack:            $(call FOUND,JACK)"
-	@echo "  SDL:             $(call FOUND,SDL)"
-	@echo "  SDL_image:       $(call FOUND,SDL_IMAGE)"
-	@echo "  SDL_ttf:         $(call FOUND,SDL_TTF)"
-	@echo "  Tcl:             $(call FOUND,TCL)"
-	@echo "  zlib:            $(call FOUND,ZLIB)"
-	@echo ""
-	@echo "Found headers:"
-	@echo "  libpng:          $(call HEADER,PNG)"
-	@echo "  libxml2:         $(call HEADER,XML)"
-	@echo "  OpenGL:          $(call HEADER,GL GL_GL)"
-	@echo "  GLEW:            $(call HEADER,GLEW GL_GLEW)"
-	@echo "  Jack:            $(call HEADER,JACK)"
-	@echo "  SDL:             $(call HEADER,SDL)"
-	@echo "  SDL_image:       $(call HEADER,SDL_IMAGE)"
-	@echo "  SDL_ttf:         $(call HEADER,SDL_TTF)"
-	@echo "  Tcl:             $(call HEADER,TCL)"
-	@echo "  zlib:            $(call HEADER,ZLIB)"
-	@echo ""
-	@echo "Components overview:"
-	@echo "  Emulation core:  $(call YESNO,$(COMPONENT_CORE))"
-	@echo "  CassetteJack:    $(call YESNO,$(COMPONENT_JACK))"
-	@echo "  SDLGL renderer:  $(call YESNO,$(COMPONENT_GL))"
-	@echo ""
-	@echo "Customisable options:"
-	@echo "  Install to:      $(INSTALL_BASE)"
-	@echo "  (you can edit these in build/custom.mk)"
-	@echo ""
-
-found-all: components-print
-	@echo "All required and optional components can be built."
-	@echo ""
-
-found-enough: components-print
-	@echo "If you are satisfied with the probe results, run \"$(MAKE)\" to start the build."
-	@echo "Otherwise, install some libraries and headers and rerun \"configure\"."
-	@echo ""
-
-found-insufficient: components-print
-	@echo "Please install missing libraries and headers and rerun \"configure\"."
-	@echo ""
+all:
+	@$(PYTHON) build/probe-results.py \
+		$(PROBE_MAKE) $(COMPONENT_CORE) $(COMPONENT_JACK) $(COMPONENT_GL)
---
+++ build/probe-results.py
@@ -0,0 +1,121 @@
+# $Id$
+#
+# Display probe results, so user can decide whether to start the build,
+# or to change system configuration and rerun "configure".
+
+from makeutils import extractMakeVariables, parseBool
+
+import sys
+
+libraries = (
+	('libpng', 'PNG'),
+	('libxml2', 'XML'),
+	('OpenGL', 'GL'),
+	('GLEW', 'GLEW'),
+	('Jack', 'JACK'),
+	('SDL', 'SDL'),
+	('SDL_image', 'SDL_IMAGE'),
+	('SDL_ttf', 'SDL_TTF'),
+	('Tcl', 'TCL'),
+	('zlib', 'ZLIB'),
+	)
+
+headers = (
+	('libpng', 'PNG'),
+	('libxml2', 'XML'),
+	('OpenGL', ('GL', 'GL_GL')),
+	('GLEW', ('GLEW', 'GL_GLEW')),
+	('Jack', 'JACK'),
+	('SDL', 'SDL'),
+	('SDL_image', 'SDL_IMAGE'),
+	('SDL_ttf', 'SDL_TTF'),
+	('Tcl', 'TCL'),
+	('zlib', 'ZLIB'),
+	)
+
+components = (
+	('Emulation core', 'CORE'),
+	('CassetteJack', 'JACK'),
+	('SDLGL renderer', 'GL'),
+	)
+
+def iterProbeResults(probeMakePath, componentStatus):
+	probeVars = extractMakeVariables(probeMakePath)
+	customVars = extractMakeVariables('build/custom.mk')
+	maxLen = max(
+		len(niceName)
+		for niceName, _ in libraries + headers + components
+		)
+
+	yield ''
+	if not parseBool(probeVars['COMPILER']):
+		yield 'No working C++ compiler was found.'
+		yield "Please install a C++ compiler, such as GCC's g++."
+		yield 'If you have a C++ compiler installed and openMSX did not detect it, please set the environment variable OPENMSX_CXX to the name of your C++ compiler.'
+		yield 'After you have corrected the situation, rerun "configure".'
+		yield ''
+	else:
+		yield 'Found libraries:'
+		disabledLibs = probeVars['DISABLED_LIBS'].split()
+		formatStr = '  %-' + str(maxLen + 3) + 's %s'
+		for niceName, varName in libraries:
+			found = probeVars['HAVE_%s_LIB' % varName] or (
+				'disabled' if varName in disabledLibs else 'no'
+				)
+			yield formatStr % (niceName + ':', found)
+		yield ''
+
+		yield 'Found headers:'
+		disabledHeaders = probeVars['DISABLED_HEADERS'].split()
+		for niceName, varNames in headers:
+			if not hasattr(varNames, '__iter__'):
+				varNames = (varNames, )
+			for varName in varNames:
+				if probeVars['HAVE_%s_H' % varName]:
+					found = 'yes'
+					break
+			else:
+				if any(varName in disabledHeaders for varName in varNames):
+					found = 'disabled'
+				else:
+					found = 'no'
+			yield formatStr % (niceName + ':', found)
+		yield ''
+
+		yield 'Components overview:'
+		for niceName, varName in components:
+			buildable = componentStatus[varName]
+			yield formatStr % (niceName + ':', 'yes' if buildable else 'no')
+		yield ''
+
+		yield 'Customisable options:'
+		yield formatStr % ('Install to', customVars['INSTALL_BASE'])
+		yield '  (you can edit these in build/custom.mk)'
+		yield ''
+
+		if all(componentStatus.itervalues()):
+			yield 'All required and optional components can be built.'
+		elif componentStatus['CORE']:
+			yield 'If you are satisfied with the probe results, ' \
+				'run "make" to start the build.'
+			yield 'Otherwise, install some libraries and headers ' \
+				'and rerun "configure".'
+		else:
+			yield 'Please install missing libraries and headers ' \
+				'and rerun "configure".'
+		yield ''
+
+if len(sys.argv) == 2 + len(components):
+	probeMakePath = sys.argv[1]
+	componentStatus = dict(
+		( varName, parseBool(value) )
+		for (niceName_, varName), value in zip(components, sys.argv[2 : ])
+		)
+	for line in iterProbeResults(probeMakePath, componentStatus):
+		print line
+else:
+	print >>sys.stderr, (
+		'Usage: python probe-results.py '
+		'PROBE_MAKE COMPONENT_CORE COMPONENT_JACK COMPONENT_GL'
+		)
+	sys.exit(2)
