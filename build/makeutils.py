434ef617ff6dbdb1dec2d402deeb415539141859
Maarten ter Huurne <maarten@treewalker.org>
2009-02-22 12:12:40 +0000

    Added support for parsing continued lines (trailing backslash). Added support for values containing spaces.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9216 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/makeutils.py
+++ build/makeutils.py
@@ -2,32 +2,61 @@
 
 import re
 
+def filterLines(lines, regex):
+	'''Filters each line of the given line iterator using the given regular
+	expression string. For each match, a tuple containing the text matching
+	each capture group from the regular expression is yielded.
+	'''
+	matcher = re.compile(regex)
+	for line in lines:
+		if line.endswith('\n'):
+			line = line[ : -1]
+		match = matcher.match(line)
+		if match:
+			yield match.groups()
+
 def filterFile(filePath, regex):
 	'''Filters each line of the given text file using the given regular
 	expression string. For each match, a tuple containing the text matching
 	each capture group from the regular expression is yielded.
 	'''
-	matcher = re.compile(regex)
 	inp = open(filePath, 'r')
 	try:
-		for line in inp:
-			if line.endswith('\n'):
-				line = line[ : -1]
-			match = matcher.match(line)
-			if match:
-				yield match.groups()
+		for groups in filterLines(inp, regex):
+			yield groups
 	finally:
 		inp.close()
+
+def joinContinuedLines(lines):
+	'''Iterates through the given lines, replacing lines that are continued
+	using a trailing backslash with a single line.
+	'''
+	buf = ''
+	for line in lines:
+		if line.endswith('\\\n'):
+			buf += line[ : -2]
+		elif line.endswith('\\'):
+			buf += line[ : -1]
+		else:
+			yield buf + line
+			buf = ''
+	if buf:
+		raise ValueError('Continuation on last line')
 
 def extractMakeVariables(filePath):
 	'''Extract all variable definitions from the given Makefile.
 	Returns a dictionary that maps each variable name to its value.
 	'''
 	makeVars = {}
-	for name, value in filterFile(
-		filePath, r'[ ]*([A-Za-z0-9_]+)[ ]*:=[ ]*([^ ]*)[ ]*'
-		):
-		makeVars[name] = value
+	inp = open(filePath, 'r')
+	try:
+		for name, value in filterLines(
+			joinContinuedLines(inp),
+			r'[ ]*([A-Za-z0-9_]+)[ ]*:=(.*)'
+			):
+			makeVars[name] = value.strip()
+	finally:
+		inp.close()
 	return makeVars
 
 def parseBool(valueStr):
