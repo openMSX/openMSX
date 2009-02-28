f07bfa0b0ddabb2477bf4dc932f76adef0b1380d
Max Feingold <mfeingol@users.sourceforge.net>
2009-02-28 20:08:43 +0000

    Add id support to recent .py files, clean up genconfig.py style
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9290 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/msvc/genconfig.py
+++ build/msvc/genconfig.py
@@ -1,5 +1,7 @@
-import os.path
+# Generates configuration headers for VC++ builds
+
 import sys
+import os.path
 import outpututils
 import buildinfo2code
 import components2code
@@ -7,63 +9,63 @@ import probe_header
 import win_resource
 import version2code
 
-if len(sys.argv) != 4:
-	print >> sys.stderr, "Usage: python genconfig.py platform configuration outputPath"
-	sys.exit(2)
-else:
-	# One of { Win32, x64 }
-	platform = sys.argv[1]
-	
-	# One of { Debug, Developer, Release }
-	configuration = sys.argv[2]
-	
-	# The location in which to generate config files
-	outputPath = sys.argv[3]
-	
-	buildPath = "build"
-	msvcPath = os.path.join(buildPath, "msvc")
-	probeMakePath = os.path.join(msvcPath, "probed_defs.mk")
+#
+# platform: one of { Win32, x64 }
+# configuration: one of { Debug, Developer, Release }
+# outputPath: the location in which to generate config files
+#
+def genConfig(platform, configuration, outputPath):
+
+	buildPath = 'build'
+	msvcPath = os.path.join(buildPath, 'msvc')
+	probeMakePath = os.path.join(msvcPath, 'probed_defs.mk')
 	
 	#
 	# build-info.hh
 	#
-	buildInfoHeader = os.path.join(outputPath, "build-info.hh")
-	targetPlatform = "mingw32"
-	if platform == "Win32":
-		targetCPU = "x86"
-	elif platform == "x64":
-		targetCPU = "x86_64"
+	buildInfoHeader = os.path.join(outputPath, 'build-info.hh')
+	targetPlatform = 'mingw32'
+	if platform == 'Win32':
+		targetCPU = 'x86'
+	elif platform == 'x64':
+		targetCPU = 'x86_64'
 	else:
-		raise ValueError("Invalid platform: " + platform)
+		raise ValueError('Invalid platform: ' + platform)
 	flavour = configuration
-	installShareDir = "/opt/openMSX/share" #not used on Windows, so whatever
+	installShareDir = '/opt/openMSX/share' #not used on Windows, so whatever
 	generator = buildinfo2code.iterBuildInfoHeader(targetPlatform, targetCPU, flavour, installShareDir)
 	outpututils.rewriteIfChanged(buildInfoHeader, generator)
 	
 	#
 	# components.hh
 	#
-	componentsHeader = os.path.join(outputPath, "components.hh")
+	componentsHeader = os.path.join(outputPath, 'components.hh')
 	generator = components2code.iterComponentsHeader(probeMakePath)
 	outpututils.rewriteIfChanged(componentsHeader, generator)
 
 	#
 	# probed_defs.hh
 	#
-	probedDefsHeader = os.path.join(outputPath, "probed_defs.hh")
+	probedDefsHeader = os.path.join(outputPath, 'probed_defs.hh')
 	generator = probe_header.iterProbeHeader(probeMakePath)
 	outpututils.rewriteIfChanged(probedDefsHeader, generator)
 
 	#
 	# resource-info.hh
 	#
-	resourceInfoHeader = os.path.join(outputPath, "resource-info.h")
+	resourceInfoHeader = os.path.join(outputPath, 'resource-info.h')
 	generator = win_resource.iterResourceHeader()
 	outpututils.rewriteIfChanged(resourceInfoHeader, generator)
 
 	#
 	# version.ii
 	#
-	versionHeader = os.path.join(outputPath, "version.ii")
+	versionHeader = os.path.join(outputPath, 'version.ii')
 	generator = version2code.iterVersionInclude()
 	outpututils.rewriteIfChanged(versionHeader, generator)
+
+if len(sys.argv) == 4:
+	genConfig(sys.argv[1], sys.argv[2], sys.argv[3])
+else:
+	print >> sys.stderr, 'Usage: python genconfig.py platform configuration outputPath'
+	sys.exit(2)
