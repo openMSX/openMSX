488b9a9e6724b81993861965f2f84b20b1a8a4ea
Maarten ter Huurne <maarten@treewalker.org>
2009-02-28 18:04:35 +0000

    Made a class for every component.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9285 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/components.py
+++ build/components.py
@@ -7,20 +7,44 @@
 #       we would compute the link flags order from that.
 coreLibs = ( 'SDL_IMAGE', 'SDL_TTF', 'SDL', 'PNG', 'TCL', 'XML', 'ZLIB' )
 
+class Component(object):
+	makeName = None
+
+	@classmethod
+	def canBuild(cls, probeVars):
+		raise NotImplementedError
+
+class EmulationCore(Component):
+	makeName = 'CORE'
+
+	@classmethod
+	def canBuild(cls, probeVars):
+		return all(
+			probeVars['HAVE_%s_LIB' % lib] and probeVars['HAVE_%s_H' % lib]
+			for lib in coreLibs
+			)
+
+class GLRenderer(Component):
+	makeName = 'GL'
+
+	@classmethod
+	def canBuild(cls, probeVars):
+		return all((
+			probeVars['HAVE_GL_LIB'],
+			probeVars['HAVE_GL_H'] or probeVars['HAVE_GL_GL_H'],
+			probeVars['HAVE_GLEW_LIB'],
+			probeVars['HAVE_GLEW_H'] or probeVars['HAVE_GL_GLEW_H'],
+			))
+
+class CassetteJack(Component):
+	makeName = 'JACK'
+
+	@classmethod
+	def canBuild(cls, probeVars):
+		return bool(probeVars['HAVE_JACK_LIB'] and probeVars['HAVE_JACK_H'])
+
 def checkComponents(probeVars):
-	buildCore = all(
-		probeVars['HAVE_%s_LIB' % lib] and probeVars['HAVE_%s_H' % lib]
-		for lib in coreLibs
+	return dict(
+		(component.makeName, component.canBuild(probeVars))
+		for component in ( EmulationCore, GLRenderer, CassetteJack )
 		)
-	buildGL = all((
-		probeVars['HAVE_GL_LIB'],
-		probeVars['HAVE_GL_H'] or probeVars['HAVE_GL_GL_H'],
-		probeVars['HAVE_GLEW_LIB'],
-		probeVars['HAVE_GLEW_H'] or probeVars['HAVE_GL_GLEW_H'],
-		))
-	buildJack = bool(probeVars['HAVE_JACK_LIB'] and probeVars['HAVE_JACK_H'])
-	return {
-		'CORE': buildCore,
-		'GL': buildGL,
-		'JACK': buildJack,
-		}
