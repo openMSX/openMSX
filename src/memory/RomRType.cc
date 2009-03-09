af4db6c5feb30e1c6bebb8b3b480570375c5ca52
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-03-09 20:57:09 +0000

    fixed bank switch range, thanks n_n
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9361 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/RomRType.cc
+++ src/memory/RomRType.cc
@@ -3,11 +3,12 @@
 // R-Type cartridges
 //
 // The address to change banks:
-//  first  16kb: fixed at 0x0f or 0x17
-//  second 16kb: 0x7000 - 0x7FFF (0x7000 and 0x7800 used)
+//  first  16kb: fixed at 0x0f or 0x17 (both have the same content)
+//  second 16kb: 0x4000 - 0x7FFF (0x7000 and 0x7800 used)
 //               bit 4 selects ROM chip,
 //                if low  bit 3-0 select page
 //                   high     2-0
+// Thanks to n_n for investigating this on a real cartridge.
 
 #include "RomRType.hh"
 #include "Rom.hh"
@@ -32,7 +33,7 @@ void RomRType::reset(EmuTime::param /*time*/)
 
 void RomRType::writeMem(word address, byte value, EmuTime::param /*time*/)
 {
-	if ((0x7000 <= address) && (address < 0x8000)) {
+	if ((0x4000 <= address) && (address < 0x8000)) {
 		value &= (value & 0x10) ? 0x17 : 0x1F;
 		setRom(2, value);
 	}
@@ -40,7 +41,7 @@ void RomRType::writeMem(word address, byte value, EmuTime::param /*time*/)
 
 byte* RomRType::getWriteCacheLine(word address) const
 {
-	if ((0x7000 <= address) && (address < 0x8000)) {
+	if ((0x4000 <= address) && (address < 0x8000)) {
 		return NULL;
 	} else {
 		return unmappedWrite;
