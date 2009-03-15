721f039dd46e02d78f01370579c70c0304c8ce76
Maarten ter Huurne <maarten@treewalker.org>
2009-03-15 16:11:43 +0000

    Renamed Package class to DownloadablePackage, so we can support non-downloadable packages as well. Added OpenGL, which is a non-downloadable packages. Also suppressed pylint warning about class names.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9374 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/packages.py
+++ build/packages.py
@@ -5,14 +5,18 @@ from urlparse import urljoin
 class Package(object):
 	'''Abstract base class for packages.
 	'''
-	downloadURL = None
 	name = None
-	version = None
-	dependsOn = ()
 
 	@classmethod
 	def getMakeName(cls):
 		return cls.name.upper()
+
+class DownloadablePackage(Package):
+	'''Abstract base class for packages that can be downloaded.
+	'''
+	downloadURL = None
+	version = None
+	dependsOn = ()
 
 	@classmethod
 	def getTarballName(cls):
@@ -22,7 +26,11 @@ class Package(object):
 	def getURL(cls):
 		return urljoin(cls.downloadURL + '/', cls.getTarballName())
 
-class DirectX(Package):
+# The class names reflect package names, which do not necessarily follow our
+# naming convention.
+# pylint: disable-msg=C0103
+
+class DirectX(DownloadablePackage):
 	downloadURL = 'http://alleg.sourceforge.net/files'
 	name = 'dx'
 	version = '70'
@@ -31,22 +39,23 @@ class DirectX(Package):
 	def getTarballName(cls):
 		return '%s%s_mgw.tar.gz' % (cls.name, cls.version)
 
-class FreeType(Package):
+class FreeType(DownloadablePackage):
 	downloadURL = 'http://nongnu.askapache.com/freetype'
 	name = 'freetype'
 	version = '2.3.7'
 	dependsOn = ('zlib', )
 
-class GLEW(Package):
+class GLEW(DownloadablePackage):
 	downloadURL = 'http://downloads.sourceforge.net/glew'
 	name = 'glew'
 	version = '1.5.1'
+	dependsOn = ('gl', )
 
 	@classmethod
 	def getTarballName(cls):
 		return '%s-%s-src.tgz' % (cls.name, cls.version)
 
-class LibPNG(Package):
+class LibPNG(DownloadablePackage):
 	downloadURL = 'http://downloads.sourceforge.net/libpng'
 	name = 'libpng'
 	version = '1.2.34'
@@ -56,7 +65,7 @@ class LibPNG(Package):
 	def getMakeName(cls):
 		return 'PNG'
 
-class LibXML2(Package):
+class LibXML2(DownloadablePackage):
 	downloadURL = 'http://xmlsoft.org/sources'
 	name = 'libxml2'
 	version = '2.7.2'
@@ -66,24 +75,27 @@ class LibXML2(Package):
 	def getMakeName(cls):
 		return 'XML'
 
-class SDL(Package):
+class OpenGL(Package):
+	name = 'gl'
+
+class SDL(DownloadablePackage):
 	downloadURL = 'http://www.libsdl.org/release'
 	name = 'SDL'
 	version = '1.2.13'
 
-class SDL_image(Package):
+class SDL_image(DownloadablePackage):
 	downloadURL = 'http://www.libsdl.org/projects/SDL_image/release'
 	name = 'SDL_image'
 	version = '1.2.7'
 	dependsOn = ('SDL', 'libpng')
 
-class SDL_ttf(Package):
+class SDL_ttf(DownloadablePackage):
 	downloadURL = 'http://www.libsdl.org/projects/SDL_ttf/release'
 	name = 'SDL_ttf'
 	version = '2.0.9'
 	dependsOn = ('SDL', 'freetype')
 
-class TCL(Package):
+class TCL(DownloadablePackage):
 	downloadURL = 'http://downloads.sourceforge.net/tcl'
 	name = 'tcl'
 	version = '8.5.6'
@@ -92,7 +104,7 @@ class TCL(Package):
 	def getTarballName(cls):
 		return '%s%s-src.tar.gz' % (cls.name, cls.version)
 
-class ZLib(Package):
+class ZLib(DownloadablePackage):
 	downloadURL = 'http://downloads.sourceforge.net/libpng'
 	name = 'zlib'
 	version = '1.2.3'
@@ -101,7 +113,7 @@ class ZLib(Package):
 def _discoverPackages(localObjects):
 	for obj in localObjects:
 		if isinstance(obj, type) and issubclass(obj, Package):
-			if not obj is Package:
+			if not (obj is Package or obj is DownloadablePackage):
 				yield obj.name, obj
 _packagesByName = dict(_discoverPackages(locals().itervalues()))
 
