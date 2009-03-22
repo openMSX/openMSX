9d40c0141bae8c144d1a4dc2c756c70dab35c379
Maarten ter Huurne <maarten@treewalker.org>
2009-03-22 13:51:43 +0000

    Moved common functions to new "compilers" module.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9410 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/probe_run_compiler.py
+++ build/probe_run_compiler.py
@@ -1,65 +1,9 @@
 # $Id$
 # Perform a sanity check on the compiler.
 
-from os import environ, remove
-from os.path import isfile
-from subprocess import PIPE, STDOUT, Popen
+from compilers import tryCompile
+
 import sys
-
-def writeFile(path, lines):
-	out = open(path, 'w')
-	try:
-		for line in lines:
-			print >> out, line
-	finally:
-		out.close()
-
-def tryCompile(
-	log, compileEnv, compileCommand, compileFlags, sourcePath, lines
-	):
-	'''Write the program defined by "lines" to a text file specified by "path"
-	and try to compile it.
-	Returns True iff compilation succeeded.
-	'''
-	mergedEnv = dict(environ)
-	mergedEnv.update(compileEnv)
-
-	assert sourcePath.endswith('.cc')
-	objectPath = sourcePath[ : -3] + '.o'
-	writeFile(sourcePath, lines)
-
-	try:
-		try:
-			proc = Popen(
-				[ compileCommand ] + compileFlags +
-					[ '-c', sourcePath, '-o', objectPath ],
-				bufsize = -1,
-				env = mergedEnv,
-				stdin = None,
-				stdout = PIPE,
-				stderr = STDOUT,
-				)
-		except OSError, ex:
-			print >> log, 'failed to execute compiler: %s' % ex
-			return False
-		stdoutdata, stderrdata = proc.communicate()
-		if stdoutdata:
-			log.write(stdoutdata)
-			if not stdoutdata.endswith('\n'): # pylint: disable-msg=E1103
-				log.write('\n')
-		assert stderrdata is None, stderrdata
-		if proc.returncode == 0:
-			return True
-		else:
-			print >> log, 'return code from compile command: %d' % (
-				proc.returncode
-				)
-			return False
-		return proc.returncode == 0
-	finally:
-		remove(sourcePath)
-		if isfile(objectPath):
-			remove(objectPath)
 
 def checkCompiler(log, compileEnv, compileCommand, compileFlags, outDir):
 	'''Checks whether compiler can compile anything at all.
