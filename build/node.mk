cc628741cea6d657c8c75927aa3def6a838befed
Maarten ter Huurne <maarten@treewalker.org>
2009-04-05 22:24:38 +0000

    Put the compile/link flag definitions in the new "probe_defs.mk" Makefile, separate from the existing "probe.mk" Makefile which is now only responsible for executing the probe.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9482 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -8,7 +8,7 @@ SUBDIRS:= \
 DIST:= \
 	tcl-search.sh \
 	main.mk node-end.mk node-start.mk entry.mk \
-	probe.mk \
+	probe.mk probe_defs.mk \
 	custom.mk 3rdparty.mk bindist.mk \
 	flavour-*.mk cpu-*.mk platform-*.mk \
 	*.py
