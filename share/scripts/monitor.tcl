3c2c708d0467c5438bf6b5a49f7913bdd74b2211
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2009-04-06 21:18:33 +0000

    Convert monitor script to use Tcl name spaces
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9495 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/monitor.tcl
+++ share/scripts/monitor.tcl
@@ -1,3 +1,7 @@
+namespace eval monitor {
+
+variable monitors
+
 set_help_text monitor_type \
 {Select a monitor color profile.
 
@@ -7,38 +11,47 @@ Usage:
   monitor_type <type>   Select new type
 }
 
-set_tabcompletion_proc monitor_type __tab_monitor_type
-proc __tab_monitor_type { args } {
-	set result [array names ::__monitor]
+set_tabcompletion_proc monitor_type [namespace code tab_monitor_type]
+proc tab_monitor_type { args } {
+	variable monitors
+	set result [array names monitors]
 	lappend result "-list"
 }
 
-set __monitor(normal) {{ 1 0 0 } { 0 1 0 } { 0 0 1 }}
-set __monitor(red)    {{ 1 0 0 } { 0 0 0 } { 0 0 0 }}
-set __monitor(green)  {{ 0 0 0 } { 0 1 0 } { 0 0 0 }}
-set __monitor(blue)   {{ 0 0 0 } { 0 0 0 } { 0 0 1 }}
-set __monitor(monochrome_amber) {{ .257 .504 .098 } { .193 .378 .073 } { 0 0 0 }}
-set __monitor(monochrome_amber_bright) {{ .333 .333 .333 } { .249 .249 .249 } { 0 0 0 }}
-set __monitor(monochrome_green) {{ .129 .252 .049 } { .257 .504 .098 } { .129 .252 .049 }}
-set __monitor(monochrome_green_bright) {{ .167 .167 .167 } { .333 .333 .333 } { .167 .167 .167 }}
-set __monitor(monochrome_white) {{ .257 .504 .098 } { .257 .504 .098 } { .257 .504 .098 }}
-set __monitor(monochrome_white_bright) {{ .333 .333 .333 } { .333 .333 .333 } { .333 .333 .333 }}
+set monitors(normal) {{ 1 0 0 } { 0 1 0 } { 0 0 1 }}
+set monitors(red)    {{ 1 0 0 } { 0 0 0 } { 0 0 0 }}
+set monitors(green)  {{ 0 0 0 } { 0 1 0 } { 0 0 0 }}
+set monitors(blue)   {{ 0 0 0 } { 0 0 0 } { 0 0 1 }}
+set monitors(monochrome_amber) {{ .257 .504 .098 } { .193 .378 .073 } { 0 0 0 }}
+set monitors(monochrome_amber_bright) {{ .333 .333 .333 } { .249 .249 .249 } { 0 0 0 }}
+set monitors(monochrome_green) {{ .129 .252 .049 } { .257 .504 .098 } { .129 .252 .049 }}
+set monitors(monochrome_green_bright) {{ .167 .167 .167 } { .333 .333 .333 } { .167 .167 .167 }}
+set monitors(monochrome_white) {{ .257 .504 .098 } { .257 .504 .098 } { .257 .504 .098 }}
+set monitors(monochrome_white_bright) {{ .333 .333 .333 } { .333 .333 .333 } { .333 .333 .333 }}
 
 proc monitor_type { {monitor ""} } {
+	variable monitors
+	
 	if [string equal $monitor ""] {
-		foreach type [array names ::__monitor] {
-			if [string equal $::__monitor($type) $::color_matrix] {
+		foreach type [array names monitors] {
+			if [string equal $monitors($type) $::color_matrix] {
 				return $type
 			}
 		}
 		return "Custom monitor type: $::color_matrix"
 	} elseif [string equal $monitor "-list"] {
-		return [array names ::__monitor]
+		return [array names monitors]
 	} else {
-		if [info exists ::__monitor($monitor)] {
-			set ::color_matrix $::__monitor($monitor)
+		if [info exists monitors($monitor)] {
+			set ::color_matrix $monitors($monitor)
 		} else {
 			error "No such monitor type: $monitor"
 		}
 	}
 }
+
+namespace export monitor_type
+
+} ;# namespace monitor
+
+namespace import monitor::*
