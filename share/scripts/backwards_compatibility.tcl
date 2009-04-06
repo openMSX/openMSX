af511d1d24702141aeb583e355a3ab488b3ad0a4
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2009-04-06 12:25:25 +0000

    Document deprecated bwcompat commands as such
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9485 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/backwards_compatibility.tcl
+++ share/scripts/backwards_compatibility.tcl
@@ -2,15 +2,19 @@
 # now 'exit'. TCL normally only has a 'exit' command. Also most shells have
 # exit but no quit.
 proc quit { } {
+	puts stderr "This command ('quit') has been deprecated (and may be removed in a future release), please use 'exit' instead!"
 	exit
 }
 
 proc decr { var { num 1 } } {
+	puts stderr "This command ('decr $var $num') has been deprecated (and may be removed in a future release), please use 'incr $var -$num' instead!"
 	uplevel incr $var [expr -$num]
 }
 proc restoredefault { var } {
+	puts stderr "This command ('restoredefault $var') has been deprecated (and may be removed in a future release), please use 'unset $var' instead!"
 	uplevel unset $var
 }
 proc alias { cmd body } {
+	puts stderr "This command ('alias $cmd $body') has been deprecated (and may be removed in a future release), please define a proc instead: proc $cmd {} $body"
 	proc $cmd {} $body
 }
