b0085baab95e85895a932f589a7d40e015dd6afe
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2009-04-06 12:36:42 +0000

    Change indentation to tabs, which is more like other scripts
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9486 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/cheat.tcl
+++ share/scripts/cheat.tcl
@@ -33,107 +33,106 @@ set __findcheat_max  15
 
 # Restart cheat finder. Should not be used directly by the user
 proc __findcheat_start {} {
-  global findcheat_mem
-  set mem [debug read_block memory 0 0x10000]
-  binary scan $mem c* values
-  set addr 0
-  foreach val $values {
-    set findcheat_mem($addr) $val
-    incr addr
-  }
+	global findcheat_mem
+	set mem [debug read_block memory 0 0x10000]
+	binary scan $mem c* values
+	set addr 0
+	foreach val $values {
+		set findcheat_mem($addr) $val
+		incr addr
+	}
 }
 
 # Helper function to do the actual search.
 # Should not be used directly by the user.
 # Returns a list of triplets (addr, old, new)
 proc __findcheat_helper { expression } {
-  global findcheat_mem
-  set result [list]
-  foreach {addr old} [array get findcheat_mem] {
-    set new [debug read memory $addr]
-    if [expr $expression] {
-      set findcheat_mem($addr) $new
-      lappend result [list $addr $old $new]
-    } else {
-      unset findcheat_mem($addr)
-    }
-  }
-  return $result
+	global findcheat_mem
+	set result [list]
+	foreach {addr old} [array get findcheat_mem] {
+		set new [debug read memory $addr]
+		if [expr $expression] {
+			set findcheat_mem($addr) $new
+			lappend result [list $addr $old $new]
+		} else {
+			unset findcheat_mem($addr)
+		}
+	}
+	return $result
 }
 
 # main routine
 proc findcheat { args } {
-  # create findcheat_mem array
-  global findcheat_mem
-  if ![array exists findcheat_mem] __findcheat_start
+	# create findcheat_mem array
+	global findcheat_mem
+	if ![array exists findcheat_mem] __findcheat_start
 
-  # get the maximum results to display
-  global __findcheat_max
+	# get the maximum results to display
+	global __findcheat_max
 
-  # build translation array for convenience expressions
-  global findcheat_translate
-  if ![array exists findcheat_translate] {
-    # TODO add more here
-    set findcheat_translate()         "true"
+	# build translation array for convenience expressions
+	global findcheat_translate
+	if ![array exists findcheat_translate] {
+		# TODO add more here
+		set findcheat_translate()         "true"
 
-    set findcheat_translate(bigger)   "new > old"
-    set findcheat_translate(smaller)  "new < old"
+		set findcheat_translate(bigger)   "new > old"
+		set findcheat_translate(smaller)  "new < old"
 
-    set findcheat_translate(more)     "new > old"
-    set findcheat_translate(less)     "new < old"
+		set findcheat_translate(more)     "new > old"
+		set findcheat_translate(less)     "new < old"
 
-    set findcheat_translate(notequal) "new != old"
-    set findcheat_translate(equal)    "new == old"
+		set findcheat_translate(notequal) "new != old"
+		set findcheat_translate(equal)    "new == old"
 
-    set findcheat_translate(loe)      "new <= old"
-    set findcheat_translate(moe)      "new >= old"
-  }
+		set findcheat_translate(loe)      "new <= old"
+		set findcheat_translate(moe)      "new >= old"
+	}
 
-  # parse options
-  while (1) {
-    switch -- [lindex $args 0] {
-      "-max" {
-        set __findcheat_max  [lindex $args 1]
-        set args [lrange $args 2 end]
-      }
-      "-start" {
-        __findcheat_start
-        set args [lrange $args 1 end]
-      }
-      "default" break
-    }
-  }
+	# parse options
+	while (1) {
+		switch -- [lindex $args 0] {
+		  "-max" {
+			  set __findcheat_max  [lindex $args 1]
+			  set args [lrange $args 2 end]
+		}
+		"-start" {
+			__findcheat_start
+			set args [lrange $args 1 end]
+		}
+		"default" break
+		}
+	}
 
-  # all remaining arguments form the expression
-  set expression [join $args]
+	# all remaining arguments form the expression
+	set expression [join $args]
 
-  if [info exists findcheat_translate($expression)] {
-    # translate a convenience expression into a real expression
-    set expression $findcheat_translate($expression)
-  } elseif [string is integer $expression] {
-    # search for a specific value
-    set expression "new == $expression"
-  }
+	if [info exists findcheat_translate($expression)] {
+		# translate a convenience expression into a real expression
+		set expression $findcheat_translate($expression)
+	} elseif [string is integer $expression] {
+		# search for a specific value
+		set expression "new == $expression"
+	}
 
-  # prefix 'old', 'new' and 'addr' with '$'
-  set expression [string map {old $old new $new addr $addr} $expression]
+	# prefix 'old', 'new' and 'addr' with '$'
+	set expression [string map {old $old new $new addr $addr} $expression]
 
-  # search memory
-  set result [__findcheat_helper $expression]
+	# search memory
+	set result [__findcheat_helper $expression]
 
-  # display the result
-  set num [llength $result]
-  set output ""
-  if {$num == 0} {
-    return "No results left"
-  } elseif {$num <= $__findcheat_max} {
-    set sorted [lsort -integer -index 0 $result]
-    foreach {addr old new} [join $sorted] {
-      append output "[format 0x%04X $addr] : $old -> $new \n"
-    }
-    return $output
-  } else {
-    return "$num results found -> Maximum result to display set to $__findcheat_max "
-  }
+	# display the result
+	set num [llength $result]
+	set output ""
+	if {$num == 0} {
+		return "No results left"
+	} elseif {$num <= $__findcheat_max} {
+		set sorted [lsort -integer -index 0 $result]
+		foreach {addr old new} [join $sorted] {
+			append output "[format 0x%04X $addr] : $old -> $new \n"
+		}
+		return $output
+	} else {
+		return "$num results found -> Maximum result to display set to $__findcheat_max "
+	}
 }
-
