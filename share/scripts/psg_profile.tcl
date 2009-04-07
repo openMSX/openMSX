502909e752880ddf1f68a44fcbb009b918912c50
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2009-04-07 14:41:58 +0000

    Convert psg_profile script to use Tcl name spaces
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9500 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/psg_profile.tcl
+++ share/scripts/psg_profile.tcl
@@ -1,3 +1,5 @@
+namespace eval psg_profile {
+
 set_help_text psg_profile \
 {Select a PSG sound profile.
 
@@ -7,21 +9,26 @@ Usage:
   psg_profile <profile>  Select a new profile
 }
 
-set_tabcompletion_proc psg_profile __tab_psg_profile
-proc __tab_psg_profile { args } {
-	set result [array names ::__psg_profile]
+variable psg_profiles
+variable psg_settings
+
+set_tabcompletion_proc psg_profile [namespace code tab_psg_profile]
+proc tab_psg_profile { args } {
+	variable psg_profiles
+	set result [array names psg_profiles]
 	lappend result "-list"
 }
 
-set __psg_settings {::PSG_vibrato_percent ::PSG_vibrato_frequency
+set psg_settings {::PSG_vibrato_percent ::PSG_vibrato_frequency
                     ::PSG_detune_percent  ::PSG_detune_frequency}
-set __psg_profile(normal)         { 0.0  -  0.0  -  }
-set __psg_profile(vibrato)        { 1.0 5.0 0.0  -  }
-set __psg_profile(detune)         { 0.0  -  0.5 5.0 }
-set __psg_profile(detune_vibrato) { 1.0 5.0 0.5 5.0 }
+set psg_profiles(normal)         { 0.0  -  0.0  -  }
+set psg_profiles(vibrato)        { 1.0 5.0 0.0  -  }
+set psg_profiles(detune)         { 0.0  -  0.5 5.0 }
+set psg_profiles(detune_vibrato) { 1.0 5.0 0.5 5.0 }
 
-proc __equal_psg_profile { values } {
-	foreach setting $::__psg_settings value $values {
+proc equal_psg_profile { values } {
+	variable psg_settings
+	foreach setting $psg_settings value $values {
 		if {$value != "-"} {
 			if {[set $setting] != $value} {
 				return false
@@ -31,16 +38,18 @@ proc __equal_psg_profile { values } {
 	return true
 }
 
-proc __get_psg_profile { } {
+proc get_psg_profile { } {
+	variable psg_settings
 	set result [list]
-	foreach setting $::__psg_settings {
+	foreach setting $psg_settings {
 		lappend result [set $setting]
 	}
 	return $result
 }
 
-proc __set_psg_profile { values } {
-	foreach setting $::__psg_settings value $values {
+proc set_psg_profile { values } {
+	variable psg_settings
+	foreach setting $psg_settings value $values {
 		if {$value != "-"} {
 			set $setting $value
 		}
@@ -48,20 +57,28 @@ proc __set_psg_profile { values } {
 }
 
 proc psg_profile { {profile ""} } {
+	variable psg_settings
+	variable psg_profiles
 	if [string equal $profile ""] {
-		foreach profile [array names ::__psg_profile] {
-			if [__equal_psg_profile $::__psg_profile($profile)] {
+		foreach profile [array names psg_profiles] {
+			if [equal_psg_profile $psg_profiles($profile)] {
 				return $profile
 			}
 		}
-		return "Custom profile: [__get_psg_profile]"
+		return "Custom profile: [get_psg_profile]"
 	} elseif [string equal $profile "-list"] {
-		return [array names ::__psg_profile]
+		return [array names psg_profiles]
 	} else {
-		if [info exists ::__psg_profile($profile)] {
-			__set_psg_profile $::__psg_profile($profile)
+		if [info exists psg_profiles($profile)] {
+			set_psg_profile $psg_profiles($profile)
 		} else {
 			error "No such profile: $profile"
 		}
 	}
 }
+
+namespace export psg_profile
+
+} ;# namespace psg_profile
+
+namespace import psg_profile::*
