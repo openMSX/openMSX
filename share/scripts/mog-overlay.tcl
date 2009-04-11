c9d2f98ced746beb6b7d7402973d07fef8803d79
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-04-11 20:32:08 +0000

    more cleanups and compensate for 'horizontal stretch' and 'set adjust'
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9523 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/mog-overlay.tcl
+++ share/scripts/mog-overlay.tcl
@@ -14,7 +14,7 @@ variable ep
 variable spell [list "" "YOMAR" "ELOHIM" "HAHAKLA" "BARECHET" "HEOTYMEO" \
                      "LEPHA" "NAWABRA" "ASCHER" "XYWOLEH" "HAMALECH"]
 # Demon Max Power
-variable demon [list 24 40 64 112 72 56 64 80 80 248]
+variable demon_power [list -1 -1 24 40 64 112 72 56 64 80 80 248]
 # Item names
 variable items [list "" "Arrow" "Ceramic Arrow" "Rolling Fire" "Fire" "Mine" \
                      "Magnifying Glass" "Holy Water" "Cape" "Magic Rod" \
@@ -39,46 +39,48 @@ proc init {} {
 	variable demon_cache
 	variable ep
 
-	osd create rectangle mog -scaled true
+	osd create rectangle mog -scaled true -alpha 0
 
 	for {set i 0} {$i < $num_enemies} {incr i} {
-		__create_power_bar mog.powerbar$i 0 999 16 4 0xff0000ff 0x00000080 0xffffffff
+		create_power_bar mog.powerbar$i 14 2 0xff0000ff 0x00000080 0xffffffff
 	}
 
-	__create_power_bar mog.item 0 999 18 18 0x00000000 0xff770020 0xffffffff
+	create_power_bar mog.item 16 16 0x00000000 0xff770020 0xffffffff
 	set item_cache 0
 
-	osd create rectangle mog.tomb -x 0 -y 999 -w 16 -h 16 -rgba 0x00000080
+	osd create rectangle mog.tomb -rely 999 -w 16 -h 16 -rgba 0x00000080
 	osd create text mog.tomb.text -x 1 -y 1 -size 5 -rgb 0xffffff
 	set tomb_cache 0
 
-	osd create rectangle mog.ladder -x 0 -y 999 -w 16 -h 1 -rgba 0xffffff80
+	osd create rectangle mog.ladder -rely 999 -relw 16 -rgba 0xffffff80
 	set ladder_cache 0
 
-	osd create rectangle mog.wall -x 0 -y 999 -w 8 -h 32 -rgba 0x00ffff80
+	osd create rectangle mog.wall -rely 999 -relw 8 -relh 24 -rgba 0x00ffff80
 	set wall_cache 0
 
-	osd create rectangle mog.wall2 -x 0 -y 999 -w 32 -h 24 -rgba 0x00000080
+	osd create rectangle mog.wall2 -rely 999 -relw 32 -relh 24 -rgba 0x00000080
 	osd create text mog.wall2.text -x 1 -y 1 -size 5 -rgb 0xffffff
 
 	for {set i 0} {$i < $num_rocks} {incr i} {
-		osd create rectangle mog.rock$i  -x 0 -y 999 -w 24 -h 4 -rgba 0x0000ff80
+		osd create rectangle mog.rock$i  -rely 999 -w 24 -h 4 -rgba 0x0000ff80
 		osd create text mog.rock$i.text -x 0 -y 0 -size 4 -rgb 0xffffff
 	}
 
-	__create_power_bar mog.demon 0 999 49 5 0x2bdd2bff 0x00000080 0xffffffff
+	create_power_bar mog.demon 48 4 0x2bdd2bff 0x00000080 0xffffffff
 	set demon_cache 0
 
-	#set field
-#	for {set y 0} {$y<20} {incr y} {
-#		for {set x 0} {$x<32} {incr x} {
-#			osd create rectangle chr[expr $x+($y*32)] -x [expr ($x*8)+32] -y [expr ($y*8)+56] -h 8 -w 8 -rgba 0xffffff00
+#	#set field
+#	for {set y 4} {$y < 24} {incr y} {
+#		for {set x 0} {$x < 32} {incr x} {
+#			osd create rectangle chr_${x}_${y} \
+#			                    -relx [expr $x * 8] \
+#			                    -rely [expr $y * 8] \
+#			                    -relh 8 -relw  8 -rgba 0xffffff00
 #		}
 #	}
-	
+
 	# Enemy Power
 	for {set i 0} {$i < 256} {incr i} {set ep($i) 1}
-
 }
 
 # Update the overlays
@@ -94,11 +96,24 @@ proc update_overlay {} {
 	variable wall_cache
 	variable demon_cache
 	variable spell
-	variable demon
+	variable demon_power
 	variable items
 	variable ep
 
 	if {!$mog_overlay_active} return
+
+	# compensate for horizontal-stretch and set-adjust
+	set hstretch [expr {$::renderer != "SDL"} ? $::horizontal_stretch : 320]
+	set xsize   [expr 320.0 / $hstretch]
+	set xoffset [expr ($hstretch - 256) / 2 * $xsize]
+	set ysize 1
+	set yoffset [expr (240 - 192) / 2 * $ysize]
+	set adjreg [vdpreg 18]
+	set hadj [expr (($adjreg & 15) ^ 7) - 7]
+	set vadj [expr (($adjreg >> 4) ^ 7) - 7]
+	set xoffset [expr $xoffset + $xsize * $hadj]
+	set yoffset [expr $yoffset + $ysize * $vadj]
+	osd configure mog -x $xoffset -y $yoffset -w $xsize -h $ysize
 
 	for {set i 0; set addr 0xe800} {$i < $num_enemies} {incr i; incr addr 0x20} {
 		set enemy_type [peek $addr]
@@ -107,8 +122,8 @@ proc update_overlay {} {
 			# Record Highest Power for an enemy
 			if {$ep($enemy_type) < $enemy_hp} {set ep($enemy_type) $enemy_hp}
 
-			set pos_x [expr 40 + [peek [expr $addr + 7]]]
-			set pos_y [expr 18 + [peek [expr $addr + 5]]]
+			set pos_x [expr  8 + [peek [expr $addr + 7]]]
+			set pos_y [expr -6 + [peek [expr $addr + 5]]]
 			set power [expr 1.00 * $enemy_hp / $ep($enemy_type)]
 			set text [format "(%d): #%02X %d %d" $i $enemy_type $enemy_hp $ep($enemy_type)]
 			update_power_bar mog.powerbar$i $pos_x $pos_y $power $text
@@ -121,15 +136,12 @@ proc update_overlay {} {
 	if {$new_item != $item_cache} {
 		set item_cache $new_item
 		if {$item_cache} {
-			set height [expr ($item_cache < 7) ? 9 : 17]
-			osd configure mog.item.left -h $height
-			osd configure mog.item.right -h $height
-			osd configure mog.item.bottom -y $height
+			set height [expr ($item_cache < 7) ? 8 : 16]
+			osd configure mog.item -relh $height
 			osd configure mog.item.text -text [lindex $items $item_cache]]
-			osd configure mog.item -x [expr 31 + [peek 0xe743]] \
-			                       -y [expr 23 + [peek 0xe742]]
+			osd configure mog.item -relx [peek 0xe743] -rely [peek 0xe742]
 		} else {
-			osd configure mog.item -y 999
+			osd configure mog.item -rely 999
 		}
 	}
 
@@ -138,10 +150,9 @@ proc update_overlay {} {
 		set tomb_cache $new_tomb
 		if {($tomb_cache > 0) && ($tomb_cache <= 10)} {
 			osd configure mog.tomb.text -text "[lindex $spell $tomb_cache]"
-			osd configure mog.tomb -x [expr 32 + [peek 0xe67a]] \
-			                       -y [expr 24 + [peek 0xe679]]
+			osd configure mog.tomb -relx [peek 0xe67a] -rely [peek 0xe679]
 		} else {
-			osd configure mog.tomb -y 999
+			osd configure mog.tomb -rely 999
 		}
 	}
 
@@ -150,11 +161,10 @@ proc update_overlay {} {
 	if {$new_ladder != $ladder_cache} {
 		set ladder_cache $new_ladder
 		if {$ladder_cache != 0} {
-			osd configure mog.ladder -x [expr 32 + [peek 0xec02]] \
-			                         -y [expr 24 + [peek 0xec01]] \
-			                         -h [expr 8 * [peek 0xec03]]
+			osd configure mog.ladder -relx [peek 0xec02] -rely [peek 0xec01] \
+			                         -relh [expr 8 * [peek 0xec03]]
 		} else {
-			osd configure mog.ladder -y 999
+			osd configure mog.ladder -rely 999
 		}
 	}
 
@@ -162,53 +172,53 @@ proc update_overlay {} {
 	if {$new_wall != $wall_cache} {
 		set wall_cache $new_wall
 		if {$wall_cache != 0} {
-			osd configure mog.wall -x [expr 32 + [peek 0xeaa3]] \
-			                       -y [expr 24 + [peek 0xeaa2]]
+			osd configure mog.wall -relx [peek 0xeaa3] -rely [peek 0xeaa2]
 		} else {
-			osd configure mog.wall -y 999
+			osd configure mog.wall -rely 999
 		}
 	}
 
 	if {[peek 0xea20] != 0} {
 		set text [format "-: -- (%02X)" [peek 0xea23]]
-		osd configure mog.wall2 -x [expr 32 + [peek 0xea22]] \
-		                        -y [expr 24 + [peek 0xea21]]
+		osd configure mog.wall2 -relx [peek 0xea22] -rely [peek 0xea21]
 		osd configure mog.wall2.text -text $text
 	} else {
-		osd configure mog.wall2 -y 999
+		osd configure mog.wall2 -rely 999
 	}
 
 	for {set i 0; set addr 0xec30} {$i < $num_rocks} {incr i; incr addr 8} {
 		if {[peek $addr] > 0} {
-			set pos_x [expr 40 + [peek [expr $addr + 3]]]
-			set pos_y [expr 18 + [peek [expr $addr + 2]]]
+			set pos_x [expr  8 + [peek [expr $addr + 3]]]
+			set pos_y [expr -6 + [peek [expr $addr + 2]]]
 			set rock_hp [peek [expr $addr + 6]]
 			set text [format "$i: -- (%02X)" $rock_hp]
-			osd configure mog.rock$i -x $pos_x -y $pos_y
+			osd configure mog.rock$i -relx $pos_x -rely $pos_y
 			osd configure mog.rock$i.text -text $text
 		} else {
-			osd configure mog.rock$i -y 999
+			osd configure mog.rock$i -rely 999
 		}
 	}
 
 	if {([peek 0xe0d2] != 0) && ([peek 0xe710] != 0)} {
-		osd configure mog.demon -x 143 -y 49
+		osd configure mog.demon -relx 112 -rely 26
 		print_mog_text 1 1 "Now Fighting [lindex $spell [expr [peek 0xe041] - 1]]"
 		set new_demon [peek 0xe710]
 		if {$new_demon != $demon_cache} {
 			set demon_cache new_demon
-			set demon_max [lindex $demon [expr [peek 0xe041] - 2]]
+			set demon_max [lindex $demon_power [peek 0xe041]]
 			if {$demon_max > 0} {
-				osd configure mog.demon.bar -w [expr (48 * (($demon_cache * ([peek 0xe076] + 1)) / $demon_max.0))]
+				set power [expr $demon_cache * ([peek 0xe076] + 1) / $demon_max.0]
+				osd configure mog.demon.bar -relw $power
 			}
 		}
 	} else {
-		osd configure mog.demon -y 999
+		osd configure mog.demon -rely 999
 	}
 
-#	for {set y 0} {$y<20} {incr y} {
-#		for {set x 0} {$x<32} {incr x} {
-#			if {[peek [expr (0xed80+$x)+($y*32)]]==108} {osd configure chr[expr $x+($y*32)] -alpha 128} else {osd configure chr[expr $x+($y*32)] -alpha 0}
+#	for {set y 4} {$y < 24} {incr y} {
+#		for {set x 0} {$x < 32} {incr x} {
+#			set alpha [expr ([peek [expr 0xed00 + $x + $y * 32]] == 108) ? 128 : 0]
+#			osd configure chr_${x}_${y} -alpha $alpha
 #		}
 #	}
 
@@ -216,59 +226,55 @@ proc update_overlay {} {
 }
 
 proc print_mog_text {x y text} {
-
 	set text [string tolower $text]
-
 	for {set i 0} {$i < [string length $text]} {incr i} {
 		scan [string range $text $i $i]] "%c" ascii
-		set ascii [expr $ascii - 86]
-		if {$ascii >= -38 && $ascii <= -29} {set ascii [expr $ascii + 39]}
+		incr ascii -86
+		if {$ascii >= -38 && $ascii <= -29} {incr ascii 39}
 		if {$ascii < 0} {set ascii 0}
-		poke [expr (0xed80 + $x) + ($y * 32) + $i] $ascii
+		poke [expr 0xed80 + $x + $y * 32 + $i] $ascii
 	}
 }
 
 proc toggle_mog_overlay {} {
 	variable mog_overlay_active
-	variable num_enemies
-
+	set mog_overlay_active [expr !$mog_overlay_active]
 	if {$mog_overlay_active} {
-		set mog_overlay_active false
-		osd destroy mog
-	} else {
-		set mog_overlay_active true
 		init
 		update_overlay
+	} else {
+		osd destroy mog
 	}
 }
 
 namespace export toggle_mog_overlay
 
-};# namespace mog_overlay
 
-namespace import mog_overlay::*
+# these procs are generic and are supposed to be moved to a generic OSD
+# library or something similar.
 
-
-# these procs are generic and are supposed to be moved to a generic OSD library or something similar.
-
-proc __create_power_bar {name x y w h barcolor background edgecolor} {
-	osd create rectangle $name        -x $x -y $y -w $w -h $h -rgba $background
-	osd create rectangle $name.top    -x 0 -y 0 -w $w -h 1 -rgba $edgecolor
-	osd create rectangle $name.left   -x 0 -y 0 -w 1 -h $h -rgba $edgecolor
-	osd create rectangle $name.right  -x $w -y 0 -w 1 -h $h -rgba $edgecolor
-	osd create rectangle $name.bottom -x 0 -y $h -w [expr $w + 1] -h 1 -rgba $edgecolor
-	osd create rectangle $name.bar    -x 1 -y 1 -w [expr $w - 1] -h [expr $h - 1] -rgba $barcolor
+proc create_power_bar {name w h barcolor background edgecolor} {
+	osd create rectangle $name        -rely 999 -relw $w -relh $h -rgba $background
+	osd create rectangle $name.top    -x 0 -y   -1 -relw 1 -h 1 -rgba $edgecolor
+	osd create rectangle $name.bottom -x 0 -rely 1 -relw 1 -h 1 -rgba $edgecolor
+	osd create rectangle $name.left   -x   -1 -y 0 -w 1 -relh 1 -rgba $edgecolor
+	osd create rectangle $name.right  -relx 1 -y 0 -w 1 -relh 1 -rgba $edgecolor
+	osd create rectangle $name.bar    -relw 1 -relh 1 -rgba $barcolor
 	osd create text      $name.text   -x 0 -y -6 -size 4 -rgba $edgecolor
 }
 
 proc update_power_bar {name x y power text} {
 	if {$power > 1.0} {set power 1.0}
 	if {$power < 0.0} {set power 0.0}
-	osd configure $name -x $x -y $y 
-	osd configure $name.bar -w [expr ([osd info $name -w] - 1) * $power]
+	osd configure $name -relx $x -rely $y
+	osd configure $name.bar -relw $power
 	osd configure $name.text -text "$text"
 }
 
 proc hide_power_bar {name} {
-	osd configure $name -y 999
+	osd configure $name -rely 999
 }
+
+};# namespace mog_overlay
+
+namespace import mog_overlay::*
