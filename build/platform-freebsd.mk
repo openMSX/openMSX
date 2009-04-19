9ebb10830b9395d772bf1edf41957ccfaf93b58a
Maarten ter Huurne <maarten@treewalker.org>
2009-04-19 15:26:30 +0000

    X11BASE is no longer needed. PKGBASE is needed, but is guaranteed to exist on every FreeBSD system.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9600 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-freebsd.mk
+++ build/platform-freebsd.mk
@@ -11,11 +11,6 @@ OPENMSX_CXX?=g++
 # File name extension of executables.
 EXEEXT:=
 
-# X11 location
-#  Expect default of ports/packages.
-#  Define X11BASE when different.
-X11BASE?=/usr/X11R6
-
 # ports/packages location
 #  openMSX depends on various libraries.
 #  Here we expect user installed libraries via ports/packages.
@@ -23,10 +18,6 @@ X11BASE?=/usr/X11R6
 #  Define PKGBASE when different.
 PKGBASE?=/usr/local
 
-COMPILE_FLAGS+=-D_REENTRANT -D_THREAD_SAFE \
-	`if [ -d $(X11BASE)/include ]; then echo '-I$(X11BASE)/include'; fi` \
-	`if [ -d $(PKGBASE)/include ]; then echo '-I$(PKGBASE)/include'; fi`
+COMPILE_FLAGS+=-D_REENTRANT -D_THREAD_SAFE -I$(PKGBASE)/include
 
-LINK_FLAGS+=-pthread \
-	`if [ -d $(X11BASE)/lib ]; then echo '-L$(X11BASE)/lib'; fi` \
-	`if [ -d $(PKGBASE)/lib ]; then echo '-L$(PKGBASE)/lib'; fi`
+LINK_FLAGS+=-pthread -L$(PKGBASE)/lib
