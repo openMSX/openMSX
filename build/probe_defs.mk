e8ba2db1cdfcfca7bf58fe42bbb23b6065020767
Maarten ter Huurne <maarten@treewalker.org>
2009-04-21 22:08:41 +0000

    Put header and lib info from each package together. This make sense now that there are no more system header probes.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9616 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/probe_defs.mk
+++ build/probe_defs.mk
@@ -21,12 +21,8 @@
 # _3RD_STA: link statically against libs from non-system dir
 #           this is how we build our redistributable binaries
 
-
-# Headers
-# =======
-
-ALL_HEADERS:= \
-	GL GL_GL GLEW GL_GLEW JACK PNG SDL SDL_IMAGE SDL_TTF TCL XML ZLIB
+ALL_LIBS:=GL GLEW JACK PNG SDL SDL_IMAGE SDL_TTF TCL XML ZLIB
+ALL_HEADERS:=$(ALL_LIBS) GL_GL GL_GLEW
 
 # Location of GL headers is not standardised; if one of these matches,
 # we consider the GL headers found.
@@ -36,85 +32,63 @@ GL_CFLAGS:=
 # Use GL_CFLAGS for GL_GL as well, if someone overrides it.
 # In any case, only GL_CFLAGS will be used by the actual build.
 GL_GL_CFLAGS=$(GL_CFLAGS)
+GL_LDFLAGS:=-lGL
+GL_RESULT:=yes
 
 # The comment for the GL headers applies to GLEW as well.
 GLEW_HEADER:=<glew.h>
 GL_GLEW_HEADER:=<GL/glew.h>
 GLEW_CFLAGS_SYS_DYN:=
 GLEW_CFLAGS_3RD_STA:=-I$(3RDPARTY_INSTALL_DIR)/include
+GLEW_LDFLAGS_SYS_DYN:=-lGLEW
+GLEW_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libGLEW.a
+GLEW_RESULT:=yes
 
 JACK_HEADER:=<jack/jack.h>
 JACK_CFLAGS_SYS_DYN:=
 JACK_CFLAGS_3RD_STA:=-I$(3RDPARTY_INSTALL_DIR)/include
+JACK_LDFLAGS_SYS_DYN:=-ljack
+JACK_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libjack.a
+JACK_RESULT:=yes
 
 PNG_HEADER:=<png.h>
 PNG_CFLAGS_SYS_DYN:=`libpng-config --cflags`
 # Note: The additional -I is to pick up the zlib headers when zlib is not
 #       installed systemwide.
 PNG_CFLAGS_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/libpng-config --cflags` -I$(3RDPARTY_INSTALL_DIR)/include
-
-SDL_HEADER:=<SDL.h>
-SDL_CFLAGS_SYS_DYN:=`sdl-config --cflags`
-SDL_CFLAGS_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/sdl-config --cflags`
-
-SDL_IMAGE_HEADER:=<SDL_image.h>
-# Note: "=" instead of ":=", so overriden value of SDL_CFLAGS will be used.
-SDL_IMAGE_CFLAGS_SYS_DYN=$(SDL_CFLAGS_SYS_DYN)
-SDL_IMAGE_CFLAGS_3RD_STA=$(SDL_CFLAGS_3RD_STA)
-
-SDL_TTF_HEADER:=<SDL_ttf.h>
-# Note: "=" instead of ":=", so overriden value of SDL_CFLAGS will be used.
-SDL_TTF_CFLAGS_SYS_DYN=$(SDL_CFLAGS_SYS_DYN)
-SDL_TTF_CFLAGS_3RD_STA=$(SDL_CFLAGS_3RD_STA)
-
-TCL_HEADER:=<tcl.h>
-TCL_CFLAGS_SYS_DYN:=`build/tcl-search.sh --cflags`
-TCL_CFLAGS_3RD_STA:=`TCL_CONFIG_DIR=$(3RDPARTY_INSTALL_DIR)/lib build/tcl-search.sh --cflags`
-
-XML_HEADER:=<libxml/parser.h>
-XML_CFLAGS_SYS_DYN:=`xml2-config --cflags`
-XML_CFLAGS_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/xml2-config --cflags` -DLIBXML_STATIC
-
-ZLIB_HEADER:=<zlib.h>
-ZLIB_CFLAGS_SYS_DYN:=
-ZLIB_CFLAGS_3RD_STA:=-I$(3RDPARTY_INSTALL_DIR)/include
-
-
-# Libraries
-# =========
-
-ALL_LIBS:=GL GLEW JACK PNG SDL SDL_IMAGE SDL_TTF TCL XML ZLIB
-
-GL_LDFLAGS:=-lGL
-GL_RESULT:=yes
-
-GLEW_LDFLAGS_SYS_DYN:=-lGLEW
-GLEW_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libGLEW.a
-GLEW_RESULT:=yes
-
-JACK_LDFLAGS_SYS_DYN:=-ljack
-JACK_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libjack.a
-JACK_RESULT:=yes
-
 PNG_LDFLAGS_SYS_DYN:=`libpng-config --ldflags`
 PNG_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libpng12.a
 PNG_RESULT_SYS_DYN:=`libpng-config --version`
 PNG_RESULT_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/libpng-config --version`
 
+SDL_HEADER:=<SDL.h>
+SDL_CFLAGS_SYS_DYN:=`sdl-config --cflags`
+SDL_CFLAGS_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/sdl-config --cflags`
 SDL_LDFLAGS_SYS_DYN:=`sdl-config --libs`
 SDL_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libSDL.a
 SDL_RESULT_SYS_DYN:=`sdl-config --version`
 SDL_RESULT_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/sdl-config --version`
 
+SDL_IMAGE_HEADER:=<SDL_image.h>
+# Note: "=" instead of ":=", so overriden value of SDL_CFLAGS will be used.
+SDL_IMAGE_CFLAGS_SYS_DYN=$(SDL_CFLAGS_SYS_DYN)
+SDL_IMAGE_CFLAGS_3RD_STA=$(SDL_CFLAGS_3RD_STA)
 # Note: "=" instead of ":=", so overriden value of SDL_LDFLAGS will be used.
 SDL_IMAGE_LDFLAGS_SYS_DYN=-lSDL_image $(SDL_LDFLAGS_SYS_DYN)
 SDL_IMAGE_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libSDL_image.a
 SDL_IMAGE_RESULT:=yes
 
+SDL_TTF_HEADER:=<SDL_ttf.h>
+# Note: "=" instead of ":=", so overriden value of SDL_CFLAGS will be used.
+SDL_TTF_CFLAGS_SYS_DYN=$(SDL_CFLAGS_SYS_DYN)
+SDL_TTF_CFLAGS_3RD_STA=$(SDL_CFLAGS_3RD_STA)
 SDL_TTF_LDFLAGS_SYS_DYN=-lSDL_ttf $(SDL_LDFLAGS_SYS_DYN)
 SDL_TTF_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libSDL_ttf.a $(3RDPARTY_INSTALL_DIR)/lib/libfreetype.a
 SDL_TTF_RESULT:=yes
 
+TCL_HEADER:=<tcl.h>
+TCL_CFLAGS_SYS_DYN:=`build/tcl-search.sh --cflags`
+TCL_CFLAGS_3RD_STA:=`TCL_CONFIG_DIR=$(3RDPARTY_INSTALL_DIR)/lib build/tcl-search.sh --cflags`
 TCL_LDFLAGS_SYS_DYN:=`build/tcl-search.sh --ldflags`
 # Note: Tcl can be compiled with a static or a dynamic library, not both.
 #       So whether this returns static or dynamic link flags depends on how
@@ -123,11 +97,17 @@ TCL_LDFLAGS_3RD_STA:=`TCL_CONFIG_DIR=$(3RDPARTY_INSTALL_DIR)/lib build/tcl-searc
 TCL_RESULT_SYS_DYN:=`build/tcl-search.sh --version`
 TCL_RESULT_3RD_STA:=`TCL_CONFIG_DIR=$(3RDPARTY_INSTALL_DIR)/lib build/tcl-search.sh --version`
 
+XML_HEADER:=<libxml/parser.h>
+XML_CFLAGS_SYS_DYN:=`xml2-config --cflags`
+XML_CFLAGS_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/xml2-config --cflags` -DLIBXML_STATIC
 XML_LDFLAGS_SYS_DYN:=`xml2-config --libs`
 XML_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libxml2.a
 XML_RESULT_SYS_DYN:=`xml2-config --version`
 XML_RESULT_3RD_STA:=`$(3RDPARTY_INSTALL_DIR)/bin/xml2-config --version`
 
+ZLIB_HEADER:=<zlib.h>
+ZLIB_CFLAGS_SYS_DYN:=
+ZLIB_CFLAGS_3RD_STA:=-I$(3RDPARTY_INSTALL_DIR)/include
 ZLIB_LDFLAGS_SYS_DYN:=-lz
 ZLIB_LDFLAGS_3RD_STA:=$(3RDPARTY_INSTALL_DIR)/lib/libz.a
 ZLIB_RESULT:=yes
