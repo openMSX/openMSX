03b4b6aff1a3a7050e175d724c43a399af36e670
Maarten ter Huurne <maarten@treewalker.org>
2009-04-27 23:51:53 +0000

    Removed files that were deleted from SVN earlier today.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9648 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -8,9 +8,8 @@ SUBDIRS:= \
 DIST:= \
 	tcl-search.sh \
 	main.mk node-end.mk node-start.mk entry.mk \
-	probe_defs.mk \
 	custom.mk 3rdparty.mk \
-	flavour-*.mk platform-*.mk probe-*.mk \
+	flavour-*.mk platform-*.mk \
 	*.py
 
 include build/node-end.mk
