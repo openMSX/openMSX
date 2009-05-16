eff11fd7efb2c4fdfed5c0f2e5b0fdccc5d5a23a
Patrick van Arkel <vampier@users.sourceforge.net>
2009-05-16 18:28:08 +0000

    Implemented the switch command to make things shorter. TODO: name spaces
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9814 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/osd_widgets.tcl
+++ share/scripts/osd_widgets.tcl
@@ -4,54 +4,38 @@
 proc osd_box {name args} {
 #todo: a lot
 
-array set properties $args
+	array set properties $args
 
-#set parents widget
-osd create rectangle $name -alpha 0
+	#set parents widget
+	osd create rectangle $name -alpha 0
 
-set background 0xff0000ff
-set border 1
+	set background 0xffffffff
+	set border 1
 
-#set properties for the parent widget
-foreach {var val} [array get properties] {
-    puts "The value of $var is $val"
+	#set properties for the parent widget
+	foreach {var val} [array get properties] {
+		
+	#how to handle?
+	#	-alpha
+	#	-rgb
 
-	#Standard OSD properties
-    if {$var=="-alpha"} {osd configure $name -alpha $val} ;#how to handle this?
-    if {$var=="-fadePeriod"} {osd configure $name -fadePeriod $val}
-    if {$var=="-fadeTarget"} {osd configure $name -fadeTarget $val}
+		switch $var {
+			-rgba
+				{set background $val}
+			-border
+				{set border $val} 
+			-background
+				{osd configure $name -rgba $val}
+			default
+				{osd configure $name $var $val;puts "The value of $var is $val"}
+		}
+	}
 
-    if {$var=="-rgb"} {osd configure $name -rgb $val}	;#how to handle this?
-    if {$var=="-scale"} {osd configure $name -scale $val}
-    if {$var=="-type"} {osd configure $name -type $val}
-
-    if {$var=="-h"} {osd configure $name -h $val}
-    if {$var=="-x"} {osd configure $name -x $val}
-    if {$var=="-z"} {osd configure $name -z $val}
-    if {$var=="-w"} {osd configure $name -w $val}
-    if {$var=="-y"} {osd configure $name -y $val}
-    if {$var=="-clip"} {osd configure $name -clip $val} ;#what does this do?
-
-    if {$var=="-image"} {osd configure $name -image $val}
-    if {$var=="-relh"} {osd configure $name -relh $val}
-    if {$var=="-relx"} {osd configure $name -relx $val}
-    if {$var=="-relw"} {osd configure $name -relw $val}
-    if {$var=="-rely"} {osd configure $name -rely $val}
-    if {$var=="-scaled"} {osd configure $name -scaled $val} ;#what does this do?
-    
-    #custom parameters
-    if {$var=="-rgba"} {set background $val}
-    if {$var=="-border"} {set border $val}   
-    if {$var=="-background"} {osd configure $name -rgba $val}
-    
-#eval osd create rectangle $name $args -alpha 0
-}
-
-#Set child widget properties
-	osd create rectangle $name.child1 -relx 0.0 -rely 0.0 -relw 1.0 -h $border -rgba $background
-	osd create rectangle $name.child2 -relx 0.0 -rely 1.0 -relw 1.0 -h -$border -rgba $background
-	osd create rectangle $name.child3 -relx 0.0 -rely 0.0 -relh 1.0 -w $border -rgba $background
-	osd create rectangle $name.child4 -relx 1.0 -rely -0.0 -relh 1.0 -w -$border -rgba $background
+	#Set child widget properties
+		osd create rectangle $name.child1 -relx 0.0 -rely 0.0 -relw 1.0 -h $border -rgba $background
+		osd create rectangle $name.child2 -relx 0.0 -rely 1.0 -relw 1.0 -h -$border -rgba $background
+		osd create rectangle $name.child3 -relx 0.0 -rely 0.0 -relh 1.0 -w $border -rgba $background
+		osd create rectangle $name.child4 -relx 1.0 -rely -0.0 -relh 1.0 -w -$border -rgba $background
 }
 
 proc create_power_bar {name w h barcolor background edgecolor} {
