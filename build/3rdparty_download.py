4d7d03d737ceabbf84ee423703c3aafb14c9a8df
Maarten ter Huurne <maarten@treewalker.org>
2009-05-17 21:08:38 +0000

    Automatically compute which packages to download.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9816 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/3rdparty_download.py
+++ build/3rdparty_download.py
@@ -1,24 +1,20 @@
 # $Id$
 
+from components import requiredLibrariesFor
+from configurations import getConfiguration
 from download import downloadURL
-from packages import (
-	DirectX, FreeType, GLEW, LibPNG, LibXML2, SDL, SDL_image, SDL_ttf, TCL, ZLib
-	)
+from libraries import librariesByName
+from packages import getPackage
 
 from os.path import isdir, isfile, join as joinpath
 import sys
 
-#TODO:
-#	Specify downloads for:
-#	- MINGW (DX7)
-#	- VC++
-#	- OTHER
-#
-# required components -> required packages -> minus system packages
-#   -> 3rdparty packages
+# TODO: Make DirectX headers for MinGW a package and make the DirectX sound
+#       driver a component.
 
-def downloadPackages(packages):
-	for package in packages:
+def downloadPackages(tarballsDir, makeNames):
+	for makeName in sorted(makeNames):
+		package = getPackage(makeName)
 		if isfile(joinpath(tarballsDir, package.getTarballName())):
 			print '%s version %s - already downloaded' % (
 				package.niceName, package.version
@@ -26,39 +22,34 @@ def downloadPackages(packages):
 		else:
 			downloadURL(package.getURL(), tarballsDir)
 
+def main(platform, tarballsDir):
+	if not isdir(tarballsDir):
+		print >> sys.stderr, \
+			'Output directory "%s" does not exist' % tarballsDir
+		sys.exit(2)
+
+	configuration = getConfiguration('3RD_STA')
+
+	# Compute the set of all directly and indirectly required libraries.
+	transLibs = set()
+	for makeName in requiredLibrariesFor(configuration.iterDesiredComponents()):
+		library = librariesByName[makeName]
+		transLibs.add(makeName)
+		for depMakeName in library.dependsOn:
+			transLibs.add(depMakeName)
+	# Filter out system libraries.
+	thirdPartyLibs = set(
+		makeName
+		for makeName in transLibs
+		if not librariesByName[makeName].isSystemLibrary(platform)
+		)
+
+	downloadPackages(tarballsDir, thirdPartyLibs)
+
 if __name__ == '__main__':
 	if len(sys.argv) == 3:
-		platform = sys.argv[1] # One of { Win32, x64, mingw }
-		tarballsDir = sys.argv[2]
-
-		#Make Package selection
-		if platform == 'Win32':
-			download_packages = (
-				GLEW, ZLib, LibPNG, TCL, SDL, SDL_image, SDL_ttf, FreeType,
-				LibXML2
-				)
-		elif platform == 'x64':
-			download_packages = (
-				GLEW, ZLib, LibPNG, TCL, SDL, SDL_image, SDL_ttf, FreeType,
-				LibXML2
-				)
-		elif platform == 'mingw':
-			download_packages = (
-				GLEW, ZLib, LibPNG, TCL, SDL, SDL_image, SDL_ttf, FreeType,
-				LibXML2, DirectX
-				)
-			#add platform etc
-		else:
-			print >> sys.stderr, 'Unknown platform "%s"' % platform
-			sys.exit(2)
-
-		if not isdir(tarballsDir):
-			print >> sys.stderr, \
-				'Output directory "%s" does not exist' % tarballsDir
-			sys.exit(2)
-
-		downloadPackages(download_packages)
+		main(*sys.argv[1 : ])
 	else:
 		print >> sys.stderr, \
-			'Usage: python 3rdparty_download.py platform TARBALLS_DIR'
+			'Usage: python 3rdparty_download.py TARGET_OS TARBALLS_DIR'
 		sys.exit(2)
