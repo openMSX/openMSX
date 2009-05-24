eeadccd379c5ce94a2ec50e4918f3430ff3143e1
Maarten ter Huurne <maarten@treewalker.org>
2009-05-24 15:04:22 +0000

    Renamed backtick() to captureStdout() and moved it to new "executils" module. Also moved shjoin() there.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9881 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/executils.py
@@ -0,0 +1,66 @@
+# $Id$
+
+from msysutils import msysActive
+
+from os import environ
+from shlex import split as shsplit
+from subprocess import PIPE, Popen
+
+def captureStdout(log, commandLine):
+	'''Run a command and capture what it writes to stdout.
+	If the command fails or writes something to stderr, that is logged.
+	Returns the captured string, or None if the command failed.
+	'''
+	# TODO: This is a modified copy-paste from compilers._Command.
+	commandParts = shsplit(commandLine)
+	env = dict(environ)
+	while commandParts:
+		if '=' in commandParts[0]:
+			name, value = commandParts[0].split('=', 1)
+			del commandParts[0]
+			env[name] = value
+		else:
+			break
+	else:
+		raise ValueError(
+			'No command specified in "%s"' % commandLine
+			)
+
+	if msysActive():
+		commandParts = [ environ['SHELL'], '-c', shjoin(commandParts) ]
+
+	try:
+		proc = Popen(
+			commandParts, bufsize = -1, env = env,
+			stdin = None, stdout = PIPE, stderr = PIPE,
+			)
+	except OSError, ex:
+		print >> log, 'Failed to execute "%s": %s' % (commandLine, ex)
+		return None
+	stdoutdata, stderrdata = proc.communicate()
+	if stderrdata:
+		severity = 'warning' if proc.returncode == 0 else 'error'
+		log.write('%s executing "%s"\n' % (severity.capitalize(), commandLine))
+		# pylint 0.18.0 somehow thinks stdoutdata is a list, not a string.
+		# pylint: disable-msg=E1103
+		stderrdata = stderrdata.replace('\r', '')
+		log.write(stderrdata)
+		if not stderrdata.endswith('\n'):
+			log.write('\n')
+	if proc.returncode == 0:
+		return stdoutdata
+	else:
+		print >> log, 'Execution failed with exit code %d' % proc.returncode
+		return None
+
+def shjoin(parts):
+	'''Joins the given sequence into a single string with space as a separator.
+	Characters that have a special meaning for the shell are escaped.
+	This is the counterpart of shlex.split().
+	'''
+	def escape(part):
+		return ''.join(
+			'\\' + ch if ch in '\\ \'"$()[]' else ch
+			for ch in part
+			)
+	return ' '.join(escape(part) for part in parts)
--- build/probe.py
+++ build/probe.py
@@ -6,6 +6,7 @@
 from compilers import CompileCommand, LinkCommand
 from components import iterComponents, requiredLibrariesFor
 from configurations import getConfiguration
+from executils import captureStdout, shjoin
 from libraries import librariesByName
 from makeutils import extractMakeVariables, parseBool
 from outpututils import rewriteIfChanged
@@ -13,11 +14,9 @@ from packages import getPackage
 from systemfuncs import systemFunctions
 from systemfuncs2code import iterSystemFuncsHeader
 
-from msysutils import msysActive
 from os import environ, makedirs, remove
 from os.path import isdir, isfile, pathsep
 from shlex import split as shsplit
-from subprocess import PIPE, Popen
 import sys
 
 def resolve(log, expr):
@@ -80,53 +79,6 @@ def checkFunc(log, compileCommand, outDir, checkName, funcName, headers):
 		log, compileCommand, outDir + '/' + checkName + '.cc', takeFuncAddr()
 		)
 
-def backtick(log, commandLine):
-	'''Run a command and capture what it writes to stdout.
-	If the command fails or writes something to stderr, that is logged.
-	Returns the captured string, or None if the command failed.
-	'''
-	# TODO: This is a modified copy-paste from compilers._Command.
-	commandParts = shsplit(commandLine)
-	env = dict(environ)
-	while commandParts:
-		if '=' in commandParts[0]:
-			name, value = commandParts[0].split('=', 1)
-			del commandParts[0]
-			env[name] = value
-		else:
-			break
-	else:
-		raise ValueError(
-			'No command specified in "%s"' % commandLine
-			)
-
-	if msysActive():
-		commandParts = [ environ['SHELL'], '-c', shjoin(commandParts) ]
-
-	try:
-		proc = Popen(
-			commandParts, bufsize = -1, env = env,
-			stdin = None, stdout = PIPE, stderr = PIPE,
-			)
-	except OSError, ex:
-		print >> log, 'Failed to execute "%s": %s' % (commandLine, ex)
-		return None
-	stdoutdata, stderrdata = proc.communicate()
-	if stderrdata:
-		severity = 'warning' if proc.returncode == 0 else 'error'
-		log.write('%s executing "%s"\n' % (severity.capitalize(), commandLine))
-		# pylint 0.18.0 somehow thinks stdoutdata is a list, not a string.
-		# pylint: disable-msg=E1103
-		stderrdata = stderrdata.replace('\r', '')
-		log.write(stderrdata)
-		if not stderrdata.endswith('\n'):
-			log.write('\n')
-	if proc.returncode == 0:
-		return stdoutdata
-	else:
-		print >> log, 'Execution failed with exit code %d' % proc.returncode
-		return None
-
 def evaluateBackticks(log, expression):
 	parts = []
 	index = 0
@@ -140,20 +92,12 @@ def evaluateBackticks(log, expression):
 			raise ValueError('Unmatched backtick: %s' % expression)
 		parts.append(expression[index : start])
 		command = expression[start + 1 : end].strip()
-		result = backtick(log, command)
+		result = captureStdout(log, command)
 		if result is None:
 			raise IOError('Backtick evaluation failed; see log')
 		parts.append(result)
 		index = end + 1
 	return ''.join(parts)
-
-def shjoin(parts):
-	def escape(part):
-		return ''.join(
-			'\\' + ch if ch in '\\ \'"$()[]' else ch
-			for ch in part
-			)
-	return ' '.join(escape(part) for part in parts)
 
 def normalizeWhitespace(expression):
 	return shjoin(shsplit(expression))
