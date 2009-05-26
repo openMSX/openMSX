5dfd2716c40063276ba949354c4808b7ec85e1af
Maarten ter Huurne <maarten@treewalker.org>
2009-05-26 01:12:32 +0000

    Support BSD ports using "distroRoot" (also used for supporting our home-grown 3rdparty system and for MacPorts and Fink) instead of appending compile and link flags directly.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9899 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-freebsd.mk
+++ build/platform-freebsd.mk
@@ -8,13 +8,6 @@ USE_SYMLINK:=true
 # File name extension of executables.
 EXEEXT:=
 
-# ports/packages location
-#  openMSX depends on various libraries.
-#  Here we expect user installed libraries via ports/packages.
-#  Expect default prefix of ports/packages.
-#  Define PKGBASE when different.
-PKGBASE?=/usr/local
+COMPILE_FLAGS+=-D_REENTRANT -D_THREAD_SAFE
 
-COMPILE_FLAGS+=-D_REENTRANT -D_THREAD_SAFE -I$(PKGBASE)/include
-
-LINK_FLAGS+=-pthread -L$(PKGBASE)/lib
+LINK_FLAGS+=-pthread
