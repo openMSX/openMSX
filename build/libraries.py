c937a69350307422abaf4f9dbb6c5ec124317334
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-06-01 18:55:01 +0000

    Changed quotes to actually do what the previous commit promised.
    git-svn-id: file:///openmsx-svn/code/openmsx/branches/branch_0_7_1@9967 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/libraries.py
+++ build/libraries.py
@@ -399,7 +399,7 @@ class TCL(Library):
 			text = captureStdout(
 				log,
 				shjoin([
-					'sh', '-c', '. %s && eval \'echo "%s"\'' % (tclConfig, expr)
+					'sh', '-c', '. %s && eval "echo \\"%s\\""' % (tclConfig, expr)
 					])
 				)
 		finally:
