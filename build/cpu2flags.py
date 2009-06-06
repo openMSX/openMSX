c734d82a823f8837967d96cf7b23553c323fdb04
Maarten ter Huurne <maarten@treewalker.org>
2009-06-06 18:50:26 +0000

    Pass CPU flags to GCC: "-m32" on x86 and "-m64" on x86-64. This ensures that openMSX and GCC have the same idea about whether we are compiling 32 or 64 bit mode. Before this change 32-bit GCC would compile without warnings or errors when ASM_X86_64 was defined, but the resulting binary was subtly incorrect.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@9989 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/cpu2flags.py
@@ -0,0 +1,20 @@
+# $Id$
+
+from cpu import getCPU
+
+import sys
+
+def getCPUFlags(cpuName):
+	cpu = getCPU(cpuName)
+	return ' '.join(cpu.gccFlags)
+
+if __name__ == '__main__':
+	if len(sys.argv) == 2:
+		cpuName = sys.argv[1]
+		try:
+			print getCPUFlags(cpuName)
+		except KeyError:
+			print >> sys.stderr, 'Unknown CPU "%s"' % cpuName
+	else:
+		print >> sys.stderr, 'Usage: python cpu2flags.py OPENMSX_TARGET_CPU'
+		sys.exit(2)
