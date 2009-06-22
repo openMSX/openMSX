82465458a45468ae531680a0499a1b09cbb1390e
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2009-06-22 21:02:31 +0000

    Added script (with keybindings) with which you can cycle through the running machines
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10042 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ share/scripts/cycle_machine.tcl
@@ -0,0 +1,32 @@
+namespace eval cycle_machine {
+
+set help_cycle_machine \
+{Cycle through the currently running machines.
+'cycle_back_machine' does the same as 'cycle_machine', but it goes in the
+opposite direction.
+}
+
+set_help_text cycle_machine $help_cycle_machine
+set_help_text cycle_back_machine $help_cycle_machine
+
+proc cycle_machine { {step 1} } {
+	set cycle_list [list_machines]
+	set cur [lsearch -exact $cycle_list [activate_machine]]
+	set new [expr ($cur + $step) % [llength $cycle_list]]
+	activate_machine [lindex $cycle_list $new]
+}
+
+proc cycle_back_machine {} {
+        cycle_machine -1
+}
+
+# keybindings
+bind CTRL+PAGEUP cycle_machine
+bind CTRL+PAGEDOWN cycle_back_machine
+
+namespace export cycle_machine
+namespace export cycle_back_machine
+
+} ;# namespace cycle_machine
+
+namespace import cycle_machine::*
