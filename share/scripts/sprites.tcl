c892ee929db76e427a62041e38273f5d9db31d30
Patrick van Arkel <vampier@users.sourceforge.net>
2009-06-27 08:21:49 +0000

    Updated sprites.tcl to make it more usable (space refreshed the current sprite)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10057 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/sprites.tcl
+++ share/scripts/sprites.tcl
@@ -3,23 +3,24 @@
 
 namespace eval test_osd_menu {
 
-#todo: more sprite info / better layout?
+#todo: more sprite info / better layout (sprite mode/ hex values/ any other info)
 
 variable title_pos 0
 variable max_sprites 0
 
 proc sprite_menu {} {
-	set pause on
-	
-	bind_default "keyb left"    -repeat { test_osd_menu::sprite_menu_action 1 }
-	bind_default "keyb right"   -repeat { test_osd_menu::sprite_menu_action 2 }
-	bind_default "keyb space"   		{ test_osd_menu::execute_action	    }
-	bind_default "keyb escape"   		{ test_osd_menu::kill_menu		    }
 
-	osd create rectangle	sprite_menu -x 100 -y 100 -w 255 -h 192 -rgba 0x00000080
-	osd create rectangle	sprite_menu.title -x 0 -y 15 -w 255 -h 32 -rgba 0x008000ff -clip on
- 	osd create text		sprite_menu.title.text -x 24 -text "" -size 24 -rgba 0xffffffff
-	osd create text 	sprite_menu.index -x 184 -y 15 -text "" -size 10 -rgba 0xffffffff
+	bind_default "keyb LEFT"    -repeat { test_osd_menu::sprite_menu_action 1 }
+	bind_default "keyb RIGHT"   -repeat { test_osd_menu::sprite_menu_action 2 }
+	bind_default "keyb ESCAPE"  -repeat	{ test_osd_menu::sprite_menu_hide	}
+	bind_default "keyb SPACE"   -repeat	{ test_osd_menu::sprite_menu_action 0}
+
+	osd create rectangle sprite_menu -x 100 -y 100 -w 255 -h 192 -rgba 0x00000080
+	osd create rectangle sprite_menu.title -x 0 -y 15 -w 255 -h 32 -rgba 0x008000ff -clip on
+ 	osd create text	sprite_menu.title.text -x 24 -text "" -size 24 -rgba 0xffffffff
+	osd create text sprite_menu.index -x 184 -y 15 -text "" -size 10 -rgba 0xffffffff
+	osd create text sprite_menu.refresh -x 130 -y 24 -size 10 -text "\[Space\] Refresh Sprite" -rgba 0xffffffff
+	osd create text sprite_menu.escape -x 130 -y 35 -size 10 -text "\[Escape\] to Exit Viewer" -rgba 0xffffffff
 	
 	sprite_menu_action 0
 }
@@ -42,27 +43,20 @@ proc sprite_menu_action {action} {
 	#fade/ease
 	osd configure sprite_menu.title.text -text "Sprite $title_pos" -alpha 0
 	osd configure sprite_menu.index	-text "In mem: $max_sprites"
-	pop_menu sprite_menu.title.text 0 $action
+	ease_text sprite_menu.title.text 0 $action
 
 }
 
-proc execute_action {} {
-	variable title_pos
-	puts "running $title_pos"
-}
-
-proc kill_menu {} {
+proc sprite_menu_hide {} {
 	osd destroy sprite_menu
-	unbind_default "keyb left"
-	unbind_default "keyb right"
-	unbind_default "keyb space"
-	unbind_default "keyb escape"
-	set pause off
+	unbind_default "keyb LEFT"
+	unbind_default "keyb RIGHT"
+	unbind_default "keyb ESCAPE"
+	unbind_default "keyb SPACE"
 }
 
-proc pop_menu {osd_object {frame_render 0} {action 0}} {
+proc ease_text {osd_object {frame_render 0} {action 0}} {
 
-		
 	#Ease in length
 	set x 16
 
@@ -82,11 +76,11 @@ proc pop_menu {osd_object {frame_render 0} {action 0}} {
 	} else {
 	osd configure $osd_object -x [expr ($x-$frame_render)]	
 	}
-	
+
 	incr frame_render 1
 
 	#call same function
-	after frame [namespace code [list pop_menu $osd_object $frame_render $action]]
+	after frame [namespace code [list ease_text $osd_object $frame_render $action]]
 }
 
 proc show_sprite {sprite} {
@@ -138,7 +132,7 @@ proc draw_matrix {matrixname x y blocksize matrixsize matrixgap} {
 	}
 
 
-	return "" #$matrixname
+	return ""
 }
 
 namespace export sprite_menu
