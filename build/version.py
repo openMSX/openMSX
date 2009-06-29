cb800f2dab11324cc395be8aa7e9205bd4b29579
Maarten ter Huurne <maarten@treewalker.org>
2009-06-29 21:00:14 +0000

    We will be post-0.7.2 soon.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10089 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.py
+++ build/version.py
@@ -11,7 +11,7 @@ from os.path import isdir
 packageName = 'openmsx'
 
 # Version number.
-packageVersionNumber = '0.7.1'
+packageVersionNumber = '0.7.2'
 packageVersionSuffix = ''
 packageVersion = packageVersionNumber + packageVersionSuffix
 
