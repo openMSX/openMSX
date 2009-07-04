openMSX/openMSX

commit e76ca389c1295024408638f15751e90e0ef35c36

Allocate Display explicitly instead of on-demand.
git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10132 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/Reactor.cc
@@ -206,6 +206,7 @@ Reactor::Reactor()
 	, globalCliComm(new GlobalCliComm(*globalCommandController, *eventDistributor))
 	, inputEventGenerator(new InputEventGenerator(
 		*globalCommandController, *eventDistributor))
+	, display(new Display(*this))
 	, mixer(new Mixer(*globalCommandController))
 	, diskManipulator(new DiskManipulator(*globalCommandController))
 	, filePool(new FilePool(globalCommandController->getSettingsConfig()))
@@ -231,6 +232,11 @@ Reactor::Reactor()
 	, paused(false)
 	, running(true)
 {
+	// TODO: Currently it is not possible to move this call into the constructor
+	//       of Display because the call to createVideoSystem() indirectly calls
+	//       Reactor.getDisplay().
+	display->createVideoSystem();
+
 	createMachineSetting();
 
 	pauseSetting.attach(*this);
@@ -276,10 +282,6 @@ InputEventGenerator& Reactor::getInputEventGenerator()
 
 Display& Reactor::getDisplay()
 {
-	if (!display.get()) {
-		display.reset(new Display(*this));
-		display->createVideoSystem();
-	}
 	return *display;
 }
 
@@ -292,7 +294,7 @@ CommandConsole& Reactor::getCommandConsole()
 {
 	if (!commandConsole.get()) {
 		commandConsole.reset(new CommandConsole(
-			*globalCommandController, *eventDistributor, getDisplay()));
+			*globalCommandController, *eventDistributor, *display));
 	}
 	return *commandConsole;
 }

commit 7097b8747773fc6975f4346a461199c4cdb85d7f

Fixed regression introduced in r10132: saved renderer settings would be ignored on startup. This happens if Display is constructed before the "settings.xml" file is parsed. This fix is not very elegant, but at least it makes the initialization order clearer than it was before.
git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10156 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/Reactor.cc
@@ -206,7 +206,6 @@ Reactor::Reactor()
 	, globalCliComm(new GlobalCliComm(*globalCommandController, *eventDistributor))
 	, inputEventGenerator(new InputEventGenerator(
 		*globalCommandController, *eventDistributor))
-	, display(new Display(*this))
 	, mixer(new Mixer(*globalCommandController))
 	, diskManipulator(new DiskManipulator(*globalCommandController))
 	, filePool(new FilePool(globalCommandController->getSettingsConfig()))
@@ -232,11 +231,6 @@ Reactor::Reactor()
 	, paused(false)
 	, running(true)
 {
-	// TODO: Currently it is not possible to move this call into the constructor
-	//       of Display because the call to createVideoSystem() indirectly calls
-	//       Reactor.getDisplay().
-	display->createVideoSystem();
-
 	createMachineSetting();
 
 	pauseSetting.attach(*this);
@@ -282,6 +276,7 @@ InputEventGenerator& Reactor::getInputEventGenerator()
 
 Display& Reactor::getDisplay()
 {
+	assert(display.get());
 	return *display;
 }
 
@@ -386,6 +381,14 @@ Reactor::Board Reactor::getMachine(const string& machineID) const
 
 void Reactor::switchMachine(const string& machine)
 {
+	if (!display.get()) {
+		display.reset(new Display(*this));
+		// TODO: Currently it is not possible to move this call into the
+		//       constructor of Display because the call to createVideoSystem()
+		//       indirectly calls Reactor.getDisplay().
+		display->createVideoSystem();
+	}
+
 	// create+load new machine
 	// switch to new machine
 	// delete old active machine