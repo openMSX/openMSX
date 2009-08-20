f867db0871c4d0eff0ef2f9e61171dc8d9b21f10
Maarten ter Huurne <maarten@treewalker.org>
2009-08-20 17:44:59 +0000

    Do not write a log file.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10407 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/package-dingux/openmsx-start.sh
+++ build/package-dingux/openmsx-start.sh
@@ -4,4 +4,5 @@
 echo -n 1 > /proc/sys/vm/overcommit_memory
 export HOME=/usr/local/home
 export OPENMSX_SYSTEM_DATA=/usr/local/share/openmsx
-exec /usr/local/bin/openmsx &> ~/openmsx-log.txt
+exec /usr/local/bin/openmsx
+#exec /usr/local/bin/openmsx &> ~/openmsx-log.txt
