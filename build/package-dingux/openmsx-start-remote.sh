af80351747ba5bff2d0ae5573254bc9d3b0ff13e
Maarten ter Huurne <maarten@treewalker.org>
2009-08-20 17:20:06 +0000

    First phase script is only needed when starting from telnet.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10405 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/package-dingux/openmsx-start-remote.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Script for starting openMSX from a remote connection, such as telnet.
+exec openvt -c2 -sw /usr/local/bin/openmsx-start2.sh
--- build/package-dingux/openmsx-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Startup script for openMSX, first phase.
-# Run this to start openMSX; running the executable directly does not work.
-exec openvt -c2 -sw /usr/local/bin/openmsx-start2.sh
