50d2adcb46a1485c9b072161acfc38fb66911f28
Maarten ter Huurne <maarten@treewalker.org>
2009-08-20 17:00:01 +0000

    Moved all envvars and other settings from before to after "openvt".
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10404 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/package-dingux/openmsx-start2.sh
+++ build/package-dingux/openmsx-start2.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Startup script for openMSX, second phase.
 # To start openMSX, run the first phase script (openmsx-start.sh) instead.
+echo -n 1 > /proc/sys/vm/overcommit_memory
+export HOME=/usr/local/home
+export OPENMSX_SYSTEM_DATA=/usr/local/share/openmsx
 exec /usr/local/bin/openmsx &> ~/openmsx-log.txt
