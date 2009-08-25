d61eab5450d492baf5326e37bdc5aa436c768b02
Patrick van Arkel <vampier@users.sourceforge.net>
2009-08-25 23:31:41 +0000

    handheld skin for 320x240 resolution. I need to adjust the TCL script to start using these small icons.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10415 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ share/skins/handheld/skin.tcl
@@ -0,0 +1,37 @@
+# global settings
+#
+#    name         default   description
+# ------------------------------------------------------------------------------
+#  xbase             0       x-coord of bounding box for LEDs
+#  ybase             0       y-coord
+#  xwidth           16       width of images (used to calculate default positions)
+#  yheight          16       height
+#  xspacing         32       space between horizontally placed LEDs
+#  yspacing         35                     vertically
+#  horizontal        1       1 -> horizontal   0 -> vertical
+#  fade_delay        5       time before LEDs start fading
+#  fade_duration     5       time it takes to fade from opaque to transparent
+#  position        default   chosen position, can be left/right/top/bottom/default
+#                            Scripts *should* overrule the value 'default', but
+#                            they *may* also overrule other values.
+#
+# per LED settings
+#   default values are calcluated from the global settings
+#   exact LED names are: power caps kana pause turbo FDD
+#
+#    name(<led>)             description
+# ------------------------------------------------------------------------------
+#  xcoord                     x-coord of LED in bounding box
+#  ycoord                     y-coord
+#  fade_delay_active          see global fade_delay setting
+#  fade_delay_non_active      "
+#  fade_duration_active       see global fade_delay setting
+#  fade_duration_non_active   "
+#  active_image               filename for LED image, default is <led>-on.png
+#  non_active_image           "                                  <led>-off.png
+
+set xwidth 49
+set yheight 36
+set xspacing 60
+set yspacing 45
+if {$position == "default"} { set position "bottom" }
