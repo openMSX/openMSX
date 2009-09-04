29e1bc47ed76f2e56996a7b24afe3c6a26695527
Maarten ter Huurne <maarten@treewalker.org>
2009-09-04 10:26:22 +0000

    Removed unused union "lzo_full_align_t".
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10501 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/lzo.cc
+++ src/utils/lzo.cc
@@ -123,35 +123,6 @@ LZO_COMPILE_TIME_ASSERT_HEADER(sizeof(lzo_uintptr_t) >= sizeof(lzo_voidp))
 
 #define pd(a,b)             ((lzo_uint) ((a)-(b)))
 
-typedef union
-{
-    char            a_char;
-    unsigned char   a_uchar;
-    short           a_short;
-    unsigned short  a_ushort;
-    int             a_int;
-    unsigned int    a_uint;
-    long            a_long;
-    unsigned long   a_ulong;
-    lzo_int         a_lzo_int;
-    lzo_uint        a_lzo_uint;
-    lzo_int32       a_lzo_int32;
-    lzo_uint32      a_lzo_uint32;
-    ptrdiff_t       a_ptrdiff_t;
-    lzo_uintptr_t   a_lzo_uintptr_t;
-    lzo_voidp       a_lzo_voidp;
-    void *          a_void_p;
-    lzo_bytep       a_lzo_bytep;
-    lzo_bytepp      a_lzo_bytepp;
-    lzo_uintp       a_lzo_uintp;
-    lzo_uint *      a_lzo_uint_p;
-    lzo_uint32p     a_lzo_uint32p;
-    lzo_uint32 *    a_lzo_uint32_p;
-    unsigned char * a_uchar_p;
-    char *          a_char_p;
-}
-lzo_full_align_t;
-
 // End of pointer alignment definitions.
 
 #  define lzo_dict_t    const lzo_bytep
