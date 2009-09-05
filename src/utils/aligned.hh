5e32068e91feae01edca164172c151abd6ed76b0
Maarten ter Huurne <maarten@treewalker.org>
2009-09-05 17:37:57 +0000

    Removed double negation and added some comments.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10516 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/aligned.hh
+++ src/utils/aligned.hh
@@ -3,10 +3,10 @@
 #ifndef ALIGNED_HH
 #define ALIGNED_HH
 
-#ifndef _MSC_VER
-#define ALIGNED(EXPRESSION, ALIGNMENT) EXPRESSION __attribute__((__aligned__((ALIGNMENT))));
-#else
+#ifdef _MSC_VER
 #define ALIGNED(EXPRESSION, ALIGNMENT) __declspec (align(ALIGNMENT)) EXPRESSION
+#else // GCC style
+#define ALIGNED(EXPRESSION, ALIGNMENT) EXPRESSION __attribute__((__aligned__((ALIGNMENT))));
 #endif
 
-#endif
+#endif // ALIGNED_HH
