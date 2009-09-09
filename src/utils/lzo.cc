openMSX/openMSX

commit f6501f103f8ae1d210886e85e9ebde243219acfa

Assert decompression success instead of returning result code.
git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10521 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/utils/lzo.cc
@@ -426,7 +426,7 @@ static ALWAYS_INLINE unsigned read16LE(const byte* p)
 	}
 }
 
-int lzo1x_decompress(
+void lzo1x_decompress(
 	const lzo_bytep __restrict src, lzo_uint __restrict src_len,
 	lzo_bytep __restrict dst, lzo_uintp __restrict dst_len
 ) {
@@ -546,9 +546,7 @@ match_next:
 eof_found:
 	assert(t == 1);
 	*dst_len = pd(op, dst);
-	return src == src_end
-	       ? LZO_E_OK
-	       : (src < src_end ? LZO_E_INPUT_NOT_CONSUMED : LZO_E_INPUT_OVERRUN);
+	assert(src == src_end); (void)src_end;
 }
 
 } // namespace openmsx

src/serialize.cc
@@ -244,11 +244,8 @@ void MemInputArchive::serialize_blob(const char*, void* data, unsigned len)
 	// lzo
 	lzo_uint srcLen; load(srcLen);
 	lzo_uint dstLen = len;
-	if (lzo1x_decompress(reinterpret_cast<const lzo_bytep>(buffer.getCurrentPos()),
-	                     srcLen, reinterpret_cast<lzo_bytep>(data), &dstLen)
-	    != LZO_E_OK) {
-		assert(false);
-	}
+	lzo1x_decompress(reinterpret_cast<const lzo_bytep>(buffer.getCurrentPos()),
+	                 srcLen, reinterpret_cast<lzo_bytep>(data), &dstLen);
 	assert(dstLen == len);
 	buffer.skip(srcLen);
 #endif

src/utils/lzo.hh
@@ -68,7 +68,7 @@ int lzo1x_1_compress(const lzo_bytep src, lzo_uint  src_len,
                      lzo_voidp wrkmem);
 
 /* decompression */
-int lzo1x_decompress(
+void lzo1x_decompress(
 	const lzo_bytep __restrict src, lzo_uint __restrict src_len,
 	lzo_bytep __restrict dst, lzo_uintp __restrict dst_len);
 

commit 42312c769457cded709f7f35cd70dac8770badd6

Fixed bug introduced in r10521
git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10539 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/utils/lzo.cc
@@ -414,7 +414,7 @@ first_literal_run:
 				if (m_pos == op) {
 					assert(t == 1);
 					*dst_len = op - dst;
-					assert(src == src_end); (void)src_end;
+					assert(ip == src_end); (void)src_end;
 					return;
 				}
 				m_pos -= 0x4000;