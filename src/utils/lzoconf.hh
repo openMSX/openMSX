2d1d4cce40f1055f4140810714a5bcc87830429e
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-09-09 13:01:35 +0000

    error codes are no longer used
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10541 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/lzoconf.hh
+++ src/utils/lzoconf.hh
@@ -139,24 +139,4 @@
 
 typedef int lzo_bool;
 
-
-/***********************************************************************
-// error codes and prototypes
-************************************************************************/
-
-/* Error codes for the compression/decompression functions. Negative
- * values are errors, positive values will be used for special but
- * normal events.
- */
-#define LZO_E_OK                    0
-#define LZO_E_ERROR                 (-1)
-#define LZO_E_OUT_OF_MEMORY         (-2)    /* [not used right now] */
-#define LZO_E_NOT_COMPRESSIBLE      (-3)    /* [not used right now] */
-#define LZO_E_INPUT_OVERRUN         (-4)
-#define LZO_E_OUTPUT_OVERRUN        (-5)
-#define LZO_E_LOOKBEHIND_OVERRUN    (-6)
-#define LZO_E_EOF_NOT_FOUND         (-7)
-#define LZO_E_INPUT_NOT_CONSUMED    (-8)
-#define LZO_E_NOT_YET_IMPLEMENTED   (-9)    /* [not used right now] */
-
 #endif // LZOCONF_HH
