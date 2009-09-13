ecaffb9f91883c13f51a224437a10810bd9590ed
Max Feingold <mfeingol@users.sourceforge.net>
2009-09-13 01:27:38 +0000

    Add correct inline macros for VC++, leave them disabled by default
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10573 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/inline.hh
+++ src/utils/inline.hh
@@ -9,6 +9,11 @@
 #if __GNUC__ > 3 || (!defined(DEBUG) && __GNUC__ > 2)
 #define ALWAYS_INLINE inline __attribute__((always_inline))
 #define NEVER_INLINE __attribute__((noinline))
+#elif defined _MSC_VER && 0
+// Enabling these macros appears to make openmsx about 5% slower 
+// when compiled with VC++ for x64. Hence we leave the defaults
+#define ALWAYS_INLINE __forceinline
+#define NEVER_INLINE __declspec(noinline)
 #else
 #define ALWAYS_INLINE inline
 #define NEVER_INLINE
