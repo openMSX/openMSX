openMSX/openMSX

commit aca2a09df0057dbf5809ec3dc2172ec4e9f9c907

Removed getSettingsConfig() from CommandController and MSXCommandController, left it on GlobalCommandController.
git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10703 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/settings/SettingImpl.cc
@@ -24,8 +24,8 @@ void SettingImplBase::init()
 {
 	CommandController& commandController = Setting::getCommandController();
 	if (needLoadSave()) {
-		XMLElement& settingsConfig =
-			commandController.getSettingsConfig().getXMLElement();
+		XMLElement& settingsConfig = Setting::getGlobalCommandController()
+			.getSettingsConfig().getXMLElement();
 		const XMLElement* config = settingsConfig.findChild("settings");
 		if (config) {
 			const XMLElement* elem = config->findChildWithAttribute(
@@ -49,9 +49,9 @@ void SettingImplBase::init()
 
 void SettingImplBase::destroy()
 {
-	CommandController& commandController = Setting::getCommandController();
-	sync(commandController.getSettingsConfig().getXMLElement());
-	commandController.unregisterSetting(*this);
+	GlobalCommandController& controller = Setting::getGlobalCommandController();
+	sync(controller.getSettingsConfig().getXMLElement());
+	controller.unregisterSetting(*this);
 }
 
 void SettingImplBase::syncProxy()

src/CommandLineParser.cc
@@ -2,7 +2,7 @@
 
 #include "CommandLineParser.hh"
 #include "CLIOption.hh"
-#include "CommandController.hh"
+#include "GlobalCommandController.hh"
 #include "SettingsConfig.hh"
 #include "File.hh"
 #include "FileContext.hh"
@@ -153,7 +153,7 @@ private:
 
 CommandLineParser::CommandLineParser(Reactor& reactor_)
 	: reactor(reactor_)
-	, settingsConfig(reactor.getCommandController().getSettingsConfig())
+	, settingsConfig(reactor.getGlobalCommandController().getSettingsConfig())
 	, output(reactor.getCliComm())
 	, helpOption(new HelpOption(*this))
 	, versionOption(new VersionOption(*this))
@@ -312,16 +312,16 @@ void CommandLineParser::parse(int argc, char** argv)
 		string sargv(argv[i]);
 #ifdef _WIN32
 		// All strings inside openMSX should be represented as UTF8.
-		// Unfortunately, sometimes strings may arrive here encoded with the system 
+		// Unfortunately, sometimes strings may arrive here encoded with the system
 		// default codepage (e.g. when openmsx is run from the cmd.exe console).
-		// Other times, strings arrive here encoded as UTF8 (e.g. when openmsx is 
-		// launched run from a program like Catapult, using CreateProcessA/W with a 
+		// Other times, strings arrive here encoded as UTF8 (e.g. when openmsx is
+		// launched run from a program like Catapult, using CreateProcessA/W with a
 		// well-encoded UTF8/UTF16 string).
-		// 
-		// This means that we have to use our psychic powers to guess the encoding used 
+		//
+		// This means that we have to use our psychic powers to guess the encoding used
 		// when openmsx was launched.
 		//
-		// The right fix would probably be to change SDL_main to use wchar_t instead 
+		// The right fix would probably be to change SDL_main to use wchar_t instead
 		// of char. This would at least let Windows figure it out instead of us.
 		// However, we don't own SDL_main.
 		sargv = utf8::unknowntoutf8(sargv);

src/GlobalSettings.cc
@@ -6,12 +6,13 @@
 #include "StringSetting.hh"
 #include "SettingsConfig.hh"
 #include "CommandController.hh"
+#include "GlobalCommandController.hh"
 #include "ThrottleManager.hh"
 #include "EnumSetting.hh"
 
 namespace openmsx {
 
-GlobalSettings::GlobalSettings(CommandController& commandController_)
+GlobalSettings::GlobalSettings(GlobalCommandController& commandController_)
 	: commandController(commandController_)
 {
 	speedSetting.reset(new IntegerSetting(commandController, "speed",

src/GlobalSettings.hh
@@ -10,7 +10,7 @@
 
 namespace openmsx {
 
-class CommandController;
+class GlobalCommandController;
 class IntegerSetting;
 class BooleanSetting;
 class StringSetting;
@@ -26,7 +26,7 @@ template <class T> class EnumSetting;
 class GlobalSettings : private Observer<Setting>, private noncopyable
 {
 public:
-	explicit GlobalSettings(CommandController& commandController);
+	explicit GlobalSettings(GlobalCommandController& commandController);
 	~GlobalSettings();
 
 	IntegerSetting& getSpeedSetting();
@@ -43,7 +43,7 @@ private:
 	// Observer<Setting>
 	virtual void update(const Setting& setting);
 
-	CommandController& commandController;
+	GlobalCommandController& commandController;
 
 	std::auto_ptr<IntegerSetting> speedSetting;
 	std::auto_ptr<BooleanSetting> pauseSetting;

src/commands/CommandController.hh
@@ -71,7 +71,6 @@ public:
 
 	virtual CliComm& getCliComm() = 0;
 	virtual Interpreter& getInterpreter() = 0;
-	virtual SettingsConfig& getSettingsConfig() = 0;
 
 protected:
 	CommandController() {}

src/commands/MSXCommandController.cc
@@ -111,7 +111,8 @@ void MSXCommandController::registerSetting(Setting& setting)
 
 	globalCommandController.registerProxySetting(setting);
 	string fullname = "::" + machineID + "::" + name;
-	getSettingsConfig().getSettingsManager().registerSetting(setting, fullname);
+	globalCommandController.getSettingsConfig().getSettingsManager()
+		.registerSetting(setting, fullname);
 	getInterpreter().registerSetting(setting, fullname);
 }
 
@@ -124,7 +125,8 @@ void MSXCommandController::unregisterSetting(Setting& setting)
 	globalCommandController.unregisterProxySetting(setting);
 	string fullname = "::" + machineID + "::" + name;
 	getInterpreter().unregisterSetting(setting, fullname);
-	getSettingsConfig().getSettingsManager().unregisterSetting(setting, fullname);
+	globalCommandController.getSettingsConfig().getSettingsManager()
+		.unregisterSetting(setting, fullname);
 }
 
 void MSXCommandController::changeSetting(Setting& setting, const string& value)
@@ -186,11 +188,6 @@ CliComm& MSXCommandController::getCliComm()
 Interpreter& MSXCommandController::getInterpreter()
 {
 	return globalCommandController.getInterpreter();
-}
-
-SettingsConfig& MSXCommandController::getSettingsConfig()
-{
-	return globalCommandController.getSettingsConfig();
 }
 
 void MSXCommandController::signalEvent(

src/commands/MSXCommandController.hh
@@ -60,7 +60,6 @@ public:
 	virtual std::string makeUniqueSettingName(const std::string& name);
 	virtual CliComm& getCliComm();
 	virtual Interpreter& getInterpreter();
-	virtual SettingsConfig& getSettingsConfig();
 
 private:
 	// MSXEventListener

src/events/HotKey.cc
@@ -2,7 +2,7 @@
 
 #include "HotKey.hh"
 #include "InputEventFactory.hh"
-#include "CommandController.hh"
+#include "GlobalCommandController.hh"
 #include "Command.hh"
 #include "CommandException.hh"
 #include "EventDistributor.hh"
@@ -54,7 +54,7 @@ private:
 };
 
 
-HotKey::HotKey(CommandController& commandController_,
+HotKey::HotKey(GlobalCommandController& commandController_,
                EventDistributor& eventDistributor_)
 	: bindCmd         (new BindCmd  (commandController_, *this, false))
 	, unbindCmd       (new UnbindCmd(commandController_, *this, false))

commit 4b0498a7ede9facb6864d5ecec15e065210e53f8

Fixed bug introduced in r10703: a Setting should unregistered with the same CommandController that it was registered with (Global vs MSX).
git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10709 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/settings/SettingImpl.cc
@@ -22,7 +22,6 @@ SettingImplBase::SettingImplBase(
 
 void SettingImplBase::init()
 {
-	CommandController& commandController = Setting::getCommandController();
 	if (needLoadSave()) {
 		XMLElement& settingsConfig = Setting::getGlobalCommandController()
 			.getSettingsConfig().getXMLElement();
@@ -39,7 +38,7 @@ void SettingImplBase::init()
 			}
 		}
 	}
-	commandController.registerSetting(*this);
+	Setting::getCommandController().registerSetting(*this);
 
 	// This is needed to for example inform catapult of the new setting
 	// value when a setting was destroyed/recreated (by a machine switch
@@ -49,9 +48,9 @@ void SettingImplBase::init()
 
 void SettingImplBase::destroy()
 {
-	GlobalCommandController& controller = Setting::getGlobalCommandController();
-	sync(controller.getSettingsConfig().getXMLElement());
-	controller.unregisterSetting(*this);
+	sync(Setting::getGlobalCommandController()
+		.getSettingsConfig().getXMLElement());
+	Setting::getCommandController().unregisterSetting(*this);
 }
 
 void SettingImplBase::syncProxy()