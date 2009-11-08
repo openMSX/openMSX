50d93640abaad89b1b83dbe3746924692fc5256d
Maarten ter Huurne <maarten@treewalker.org>
2009-11-08 23:40:09 +0000

    Group info by package instead of by info type (package, tarball, URL). Simplifies the code and makes the Makefile look more like "packages.py".
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10770 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/3rdparty_packages2make.py
+++ build/3rdparty_packages2make.py
@@ -4,38 +4,17 @@ from packages import getPackage, iterDownloadablePackages
 
 import sys
 
-makeNames = sorted(
-	package.getMakeName() for package in iterDownloadablePackages()
-	)
-
-def genDownloadURLs():
-	yield '# Download locations for package sources.'
-	for makeName in makeNames:
-		package = getPackage(makeName)
-		yield 'DOWNLOAD_%s:=%s' % (makeName, package.downloadURL)
-
-def genVersions():
-	yield '# The package versions that we use.'
-	yield '# You can select different versions by editing "build/packages.py".'
-	for makeName in makeNames:
-		package = getPackage(makeName)
-		yield 'PACKAGE_%s:=%s' % (makeName, package.getSourceDirName())
-
-def genTarballs():
-	yield '# Source tar file names.'
-	for makeName in makeNames:
-		package = getPackage(makeName)
-		yield 'TARBALL_%s:=%s' % (makeName, package.getTarballName())
-
 def printPackagesMake():
-	for line in genDownloadURLs():
-		print line
+	print '# Information about packages.'
+	print '# Generated from the data in "build/packages.py".'
 	print
-	for line in genVersions():
-		print line
-	print
-	for line in genTarballs():
-		print line
+	for package in iterDownloadablePackages():
+		makeName = package.getMakeName()
+		print '# %s' % package.niceName
+		print 'PACKAGE_%s:=%s' % (makeName, package.getSourceDirName())
+		print 'TARBALL_%s:=%s' % (makeName, package.getTarballName())
+		print 'DOWNLOAD_%s:=%s' % (makeName, package.downloadURL)
+		print
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
