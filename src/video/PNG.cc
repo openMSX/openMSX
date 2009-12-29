bd89e55580a853de29e528c03ca7700b8e6911dd
Maarten ter Huurne <maarten@treewalker.org>
2009-12-29 00:18:41 +0000

    Use OPENMSX_BIGENDIAN instead of SDL_BYTEORDER.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11043 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/video/PNG.cc
+++ src/video/PNG.cc
@@ -160,17 +160,17 @@ static SDL_Surface* IMG_LoadPNG_RW(SDL_RWops* src)
 	{
 		Uint32 Rmask = 0, Gmask = 0, Bmask = 0, Amask = 0;
 		if (color_type != PNG_COLOR_TYPE_PALETTE) {
-			if (SDL_BYTEORDER == SDL_LIL_ENDIAN) {
-				Rmask = 0x000000FF;
-				Gmask = 0x0000FF00;
-				Bmask = 0x00FF0000;
-				Amask = (info_ptr->channels == 4) ? 0xFF000000 : 0;
-			} else {
+			if (OPENMSX_BIGENDIAN) {
 				int s = (info_ptr->channels == 4) ? 0 : 8;
 				Rmask = 0xFF000000 >> s;
 				Gmask = 0x00FF0000 >> s;
 				Bmask = 0x0000FF00 >> s;
 				Amask = 0x000000FF >> s;
+			} else {
+				Rmask = 0x000000FF;
+				Gmask = 0x0000FF00;
+				Bmask = 0x00FF0000;
+				Amask = (info_ptr->channels == 4) ? 0xFF000000 : 0;
 			}
 		}
 		surface = SDL_AllocSurface(SDL_SWSURFACE, width, height,
