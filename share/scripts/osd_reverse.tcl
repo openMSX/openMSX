7b34acd4b8219eff395348e9397b7ff4d1e59770
Patrick van Arkel <vampier@users.sourceforge.net>
2010-01-15 21:27:59 +0000

    initial 'time bar' script that allows you to easily jump to a point in the replay.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11159 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ share/scripts/osd_reverse.tcl
@@ -0,0 +1,61 @@
+namespace eval reverse_widgets {
+
+	variable update_after_id
+	variable mouse_after_id
+
+	proc toggle_reversebar {} {
+		variable update_after_id
+		variable mouse_after_id
+		if {[catch {osd info reverse.bar -rgba} errmsg]} {
+			namespace import ::osd_widgets::*
+			# TODO: get rid of MSX coordinates dependency!
+			osd create rectangle reverse -scaled true -w 1 -h 1
+			create_power_bar reverse.bar 250 3 0x0077ff80 0x00000080 0xffffff80
+			update_reversebar
+			set_mouse_trigger
+			return
+		}
+		after cancel $update_after_id
+		after cancel $mouse_after_id
+		osd destroy reverse
+	}
+	
+	proc update_reversebar {} {
+
+		variable update_after_id
+
+		array set stats [reverse status]
+		if {$stats(status) == "disabled"} {
+			reverse start
+			puts stderr "Reverse Auto-Enabled"
+		} else {
+		
+			set totLenght [expr $stats(end) - $stats(begin)]
+			set playLength [expr $stats(current) - $stats(begin)]
+			set fraction [expr ($playLength / $totLenght)]
+			#puts [expr $playLength/$totLenght]
+			update_power_bar reverse.bar \
+				33\
+				235 \
+				$fraction \
+				[format "%0.2f%%" [expr $fraction * 100]]
+		}
+		set update_after_id [after realtime 0.10 [namespace code update_reversebar]]
+	}
+
+	proc set_mouse_trigger {} {
+		variable mouse_after_id
+		set mouse_after_id [after "mouse button1 down" {
+			foreach {x y} [osd info "reverse.bar" -mousecoord] {}
+			if {$x >= 0 && $x <= 1 && $y >= 0 && $y <= 1} {
+				array set stats [reverse status]
+				reverse goto [expr $stats(begin) + $x * ($stats(end) - $stats(begin))]
+			}
+			reverse_widgets::set_mouse_trigger
+		}]
+	}
+
+	namespace export toggle_reversebar
+}
+
+namespace import reverse_widgets::*
