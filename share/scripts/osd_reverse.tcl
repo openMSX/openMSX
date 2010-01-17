5e7780a7dc10a6dc34dd06f704dd8f41f055ac6a
Wouter Vermaelen <m9710797@users.sourceforge.net>
2010-01-17 09:53:35 +0000

    Various cleanups
    I also split toggle_reversebar into enable_ and disable_reversebar, I might
    need the enable_ proc when I integrate this more with the reverse key bindings.
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11168 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/osd_reverse.tcl
+++ share/scripts/osd_reverse.tcl
@@ -1,70 +1,64 @@
 namespace eval reverse_widgets {
-
 	variable update_after_id
 	variable mouse_after_id
 
 	proc toggle_reversebar {} {
-		variable update_after_id
-		variable mouse_after_id
-		if {[catch {osd info reverse.bar -rgba} errmsg]} {
-			namespace import ::osd_widgets::*
-			# TODO: get rid of MSX coordinates dependency!
-			osd create rectangle reverse -scaled true -w 1 -h 1
-			create_power_bar reverse.bar 250 3 0x0077ff80 0x00000080 0xffffff80
-			update_reversebar
-			set_mouse_trigger
-			return
+		if {[catch {osd info reverse}]} {
+			enable_reversebar
+		} else {
+			disable_reversebar
 		}
-		after cancel $update_after_id
-		after cancel $mouse_after_id
-		osd destroy reverse
+		return ""
 	}
-	
-	proc update_reversebar {} {
 
-		variable update_after_id
-
+	proc enable_reversebar {} {
 		array set stats [reverse status]
 		if {$stats(status) == "disabled"} {
 			reverse start
 			puts stderr "Reverse Auto-Enabled"
-		} else {
-		
-			set totLenght [expr $stats(end) - $stats(begin)]
-			set playLength [expr $stats(current) - $stats(begin)]
-			set fraction [expr ($playLength / $totLenght)]
-			
-			set playTime [reverse_widgets::maketime ($playLength)]
-			set totTime [reverse_widgets::maketime ($totLenght)]
-			#puts [expr $playLength/$totLenght]
-			update_power_bar reverse.bar \
-				33\
-				235 \
-				$fraction \
-				[format "$playTime / $totTime (%0.2f%%)" [expr $fraction * 100]]
 		}
+		osd create rectangle reverse -scaled true -w 1 -h 1
+		osd_widgets::create_power_bar reverse.bar \
+			250 3 0x0077ff80 0x00000080 0xffffff80
+		update_reversebar
+		check_mouse
+	}
+
+	proc disable_reversebar {} {
+		variable update_after_id
+		variable mouse_after_id
+		after cancel $update_after_id
+		after cancel $mouse_after_id
+		osd destroy reverse
+	}
+
+	proc update_reversebar {} {
+		array set stats [reverse status]
+		set totLenght [expr $stats(end) - $stats(begin)]
+		set playLength [expr $stats(current) - $stats(begin)]
+		set fraction [expr ($totLenght != 0) ? ($playLength / $totLenght) : 0]
+		osd_widgets::update_power_bar reverse.bar \
+			33 235 $fraction \
+			"[formatTime $playLength] / [formatTime $totLenght]"
+		variable update_after_id
 		set update_after_id [after realtime 0.10 [namespace code update_reversebar]]
 	}
 
-	proc set_mouse_trigger {} {
+	proc check_mouse {} {
+		foreach {x y} [osd info "reverse.bar" -mousecoord] {}
+		if {0 <= $x && $x <= 1 && 0 <= $y && $y <= 1} {
+			array set stats [reverse status]
+			reverse goto [expr $stats(begin) + $x * ($stats(end) - $stats(begin))]
+		}
 		variable mouse_after_id
-		set mouse_after_id [after "mouse button1 down" {
-			foreach {x y} [osd info "reverse.bar" -mousecoord] {}
-			if {$x >= 0 && $x <= 1 && $y >= 0 && $y <= 1} {
-				array set stats [reverse status]
-				reverse goto [expr $stats(begin) + $x * ($stats(end) - $stats(begin))]
-			}
-			reverse_widgets::set_mouse_trigger
-		}]
+		set mouse_after_id [after "mouse button1 down" [namespace code check_mouse]]
 	}
-	
-	proc maketime {seconds} {
-		set seconds [expr int($seconds)]
-		return  [format "%02d:%02d" [expr $seconds/60] [expr $seconds%60]]
+
+	proc formatTime {seconds} {
+		format "%02d:%02.f" [expr int($seconds / 60)] [expr fmod($seconds, 60)]
 	}
 
 	namespace export toggle_reversebar
-	namespace export maketime
 }
 
 namespace import reverse_widgets::*
