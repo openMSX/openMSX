33666dd407dcd71cfd135e997c3bbac7817e1146
Patrick van Arkel <vampier@users.sourceforge.net>
2010-01-20 23:14:12 +0000

    Added funcionality osd_reverse.tcl: added snapshots to the time line bar (see todo in code)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11173 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/osd_reverse.tcl
+++ share/scripts/osd_reverse.tcl
@@ -1,6 +1,14 @@
+#TODO: 
+# only show snapshots on timebar when 'debug' parameter is given
+#
+# Destory or Add OSD snapshot elements when needed: currently
+# we destroy all snapshots OSD elementes and recreate them
+# 
+
 namespace eval reverse_widgets {
 	variable update_after_id
 	variable mouse_after_id
+	variable drawn_osd_elements 0
 
 	proc toggle_reversebar {} {
 		if {[catch {osd info reverse}]} {
@@ -20,8 +28,10 @@ namespace eval reverse_widgets {
 		osd create rectangle reverse -scaled true -w 1 -h 1
 		osd_widgets::create_power_bar reverse.bar \
 			250 3 0x0077ff80 0x00000080 0xffffff80
+
 		update_reversebar
 		check_mouse
+		
 	}
 
 	proc disable_reversebar {} {
@@ -34,9 +44,38 @@ namespace eval reverse_widgets {
 
 	proc update_reversebar {} {
 		array set stats [reverse status]
+
+		set snapshots [split $stats(snapshots)]
+		
+		if {[llength $snapshots]<2} {
+			set update_after_id [after realtime 0.10 [namespace code update_reversebar]]
+			return
+		}
+		
 		set totLenght [expr $stats(end) - $stats(begin)]
 		set playLength [expr $stats(current) - $stats(begin)]
 		set fraction [expr ($totLenght != 0) ? ($playLength / $totLenght) : 0]
+		
+		variable drawn_osd_elements
+		
+		for {set i 1} {$i <= $drawn_osd_elements} {incr i} {
+				osd destroy reverse.bar.snapshot$i
+			}
+			
+		set snapcount 0
+		
+		foreach snapshot $snapshots {
+			incr snapcount
+			osd create rectangle reverse.bar.snapshot$snapcount \
+				-y 0 \
+				-w 0.5 \
+				-h 3 \
+				-rgba 0x444444ff \
+				-relx [expr (1.0/$totLenght) * ($snapshot - $stats(begin))]
+		}
+		
+		set drawn_osd_elements $snapcount
+	
 		osd_widgets::update_power_bar reverse.bar \
 			33 235 $fraction \
 			"[formatTime $playLength] / [formatTime $totLenght]"
