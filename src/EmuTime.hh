18e2421278e67277c2b80749dd0b0f5aaae0b7f6
Wouter Vermaelen <m9710797@users.sourceforge.net>
2010-02-04 18:38:05 +0000

    Fixed crash in 32-bit builds when EmuTime::dummy() was actually used.
    Usually we pass EmuTime::dummy() function parameter to functions that don't
    actually use this parameter. But in ReverseManager.cc it was also used to
    initialize a EmuTime variable to some dummy value. This works fine in 64-bit
    builds, but crashes in a 32-bit build. (Could for example be triggered with
    "reverse goback 1").
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11236 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/EmuTime.hh
+++ src/EmuTime.hh
@@ -20,7 +20,7 @@ public:
 	static param dummy() { return EmuTime(); }
 #else
 	typedef const EmuTime& param;
-	static param dummy() { EmuTime* d = 0; return *d; }
+	static param dummy() { return zero; }
 #endif
 
 	// Note: default copy constructor and assigment operator are ok.
