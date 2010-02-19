26c445c27dbcf06c875e28fcdf820f1040ea43ec
Wouter Vermaelen <m9710797@users.sourceforge.net>
2010-02-19 21:09:59 +0000

    cleanups in nemesis script
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11281 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/osd_nemesis.tcl
+++ share/scripts/osd_nemesis.tcl
@@ -1,52 +1,76 @@
+set_help_text toggle_nemesis_1_shield \
+{This command (de)activates some Nemesis 1 toys:
+ - A shield around the players ship: enemy bullets will bounce off this shield.
+ - Allow to move the ship with the mouse (click right mouse button to activate, click again to disable).
+}
+
 namespace eval osd_nemesis {
-
-variable scriptlocation [info script]
-
-# todo: help text
-
-variable mouse1_pressed false
+	variable move_active false
+	variable after_frame_id
+	variable after_mouse_button_id
 
 	proc toggle_nemesis_1_shield {} {
-
-		if {![catch {osd info nem -rgba} errmsg]} {
-			osd destroy nem
-			return ""
+		if {[catch {osd info "nemesis1"}]} {
+			enable_nemesis_1_shield
+		} else {
+			disable_nemesis_1_shield
 		}
+		return ""
+	}
+	namespace export toggle_nemesis_1_shield
 
-		osd_widgets::msx_init "nem"
+	proc disable_nemesis_1_shield {} {
+		variable after_frame_id
+		variable after_mouse_button_id
 
-		osd create rectangle nem.shield  \
+		osd destroy "nemesis1"
+		after cancel $after_frame_id
+		after cancel $after_mouse_button_id
+	}
+
+	proc enable_nemesis_1_shield {} {
+		variable move_active
+		variable after_frame_id
+		variable after_mouse_button_id
+
+		osd_widgets::msx_init "nemesis1"
+		osd create rectangle "nemesis1.shield" \
 			-fadeCurrent 0 -fadeTarget 0 -fadePeriod 2 \
 			-image [data_file scripts/shield.png]
 
-		#click to capture and click again to release 
-		bind_default "mouse button1 down" {
-			if { $osd_nemesis::mouse1_pressed } {
-				set osd_nemesis::mouse1_pressed false
-					} else {
-				set osd_nemesis::mouse1_pressed true
-				}
-			osd_nemesis::get_vixen
-		}
+		set move_active false
 
-		create_shield
+		set after_frame_id [after frame osd_nemesis::after_frame]
+		set after_mouse_button_id [after "mouse button1 down" osd_nemesis::after_mouse]
 	}
 
-	proc create_shield {} {
+	proc after_mouse {} {
+		# click to capture and click again to release
+		variable move_active
+		variable after_mouse_button_id
+		set move_active [expr !$move_active]
+		set after_mouse_button_id [after "mouse button1 down" osd_nemesis::after_mouse]
+	}
 
-		if {[catch {osd info nem -rgba} errmsg]} {
-			return ""
+	proc after_frame {} {
+		variable move_active
+		variable after_frame_id
+
+		osd_widgets::msx_update "nemesis1"
+
+		if {$move_active} {
+			# move vic viper to mouse position
+			foreach {x y} [osd info "nemesis1" -mousecoord] {}
+			poke 0xe206 [utils::clip 0 255 [expr int($x)]]
+			poke 0xe204 [utils::clip 0 212 [expr int($y)]]
 		}
-
-		osd_widgets::msx_update nem
 
 		# vic viper location
 		set x [peek 0xe206]
 		set y [peek 0xe204]
-		osd configure nem.shield -relx [expr $x - 9] -rely [expr $y - 9]
+		osd configure "nemesis1.shield" -relx [expr $x - 9] -rely [expr $y - 9]
 
 		for {set i 0} {$i < 32} {incr i} {
-
 			# set base address
 			set addr [expr 0xe300 + ($i*0x20)]
 
@@ -61,7 +85,6 @@ variable mouse1_pressed false
 			set b [peek [expr $addr + 4]]
 
 			# Not in contact with shield? Then do nothing.
-
 			if {(pow($a - ($x+6), 2) + pow($b - ($y + 7), 2)) >= 961} continue
 
 			# ground turrets change into razor discs :)
@@ -79,27 +102,16 @@ variable mouse1_pressed false
 			set dy [expr ($y > $b) ? -$shieldstrength : $shieldstrength]
 			set xn [expr $a + $dx]
 			set yn [expr $b + $dy]
-			poke [expr $addr + 6] [utils::clip 0 255 $xn]
-			poke [expr $addr + 4] [utils::clip 0 255 $yn]
-			poke [expr $addr + 8] [expr $dy & 255]
+			poke [expr $addr +  6] [utils::clip 0 255 $xn]
+			poke [expr $addr +  4] [utils::clip 0 255 $yn]
+			poke [expr $addr +  8] [expr $dy & 255]
 			poke [expr $addr + 10] [expr $dx & 255]
 
 			# make shield visible
-			osd configure nem.shield -fadeCurrent 0.5
+			osd configure "nemesis1.shield" -fadeCurrent 0.5
 		}
-		after frame [namespace code create_shield]
+		set after_frame_id [after frame osd_nemesis::after_frame]
 	}
-
-	proc get_vixen {} {
-		osd_widgets::msx_update "nem"
-		foreach {x y} [osd info "nem" -mousecoord] {}
-			poke 0xe206 [utils::clip 0 255 [expr int($x)]]
-			poke 0xe204 [utils::clip 0 212 [expr int($y)]]
-		if {$osd_nemesis::mouse1_pressed} { after frame osd_nemesis::get_vixen }
-	}
-
-	namespace export toggle_nemesis_1_shield
-
-} ;# namespace osd_nemesis
+}
 
 namespace import osd_nemesis::*
