329e26c1f13c333e92471486440aff3a986d2189
Joost Yervante Damad <joost@damad.be>
2010-02-28 10:19:31 +0000

    revert accidental checkin of patched files
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11310 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/custom.mk
+++ build/custom.mk
@@ -1,8 +1,23 @@
-INSTALL_BASE:=/usr
-INSTALL_SHARE_DIR:=/usr/share/openmsx
+# $Id$
+#
+# Customize openMSX to your system/preferences.
+
+# Directory to install to.
+# openMSX is always installed into a single self-contained directory.
+# But you can change that directory to for example /usr/local/openMSX
+# or /usr/games/openMSX if you like.
+INSTALL_BASE:=/opt/openMSX
+
+# Add ChangeLog version number to executable file name? This applies only to
+# development versions, not to release versions (see version.py).
 VERSION_EXEC:=false
-SYMLINK_FOR_BINARY:=false
-INSTALL_CONTRIB:=false
-INSTALL_DOC_DIR:=/usr/share/doc/openmsx-data
-INSTALL_BINARY_DIR:=/usr/bin
-OPENMSX_INSTALL:=/usr
+
+# Create a symbolic link to the installed binary?
+# This link is placed in a location that is typically in a user's path:
+# /usr/local/bin for system-wide installs and ~/bin for personal installs.
+# This setting is only relevant on systems that support symbolic links.
+SYMLINK_FOR_BINARY:=true
+
+# Install content of Contrib/ directory?
+# Currently this contains a version of C-BIOS.
+INSTALL_CONTRIB:=true
