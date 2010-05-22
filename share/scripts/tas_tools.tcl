39ffeae4d0b326f314d064182fdfb4edd0652245
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2010-05-22 20:15:49 +0000

    Cleanups in initial TAS script (no updates anymore before the release)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11440 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/tas_tools.tcl
+++ share/scripts/tas_tools.tcl
@@ -1,30 +1,25 @@
 namespace eval tas {
 
 set_help_text toggle_frame_counter\
-{toggle_frame_counter let's you toggle(on/off) a frame counter in the lower right corner
+{Toggles display of a frame counter in the lower right corner.
 
 The frame counter will be wrong if at any time there is a change of VDP
-register 9 bit 1 during the run time of the machine! (aka switching between 50/60hz)
+register 9 bit 1 during the run time of the machine! (So switching between 50/60Hz.)
 }
 
 proc toggle_frame_counter {} {
-
-# Note: the frame counter will be wrong if at any time there is a change of VDP
-# register 9 bit 1 during the run time of the machine!
-
-	variable frame_trigger_id
 
 	if {![catch {osd info framecount}]} {
 		osd destroy framecount
 		return ""
 	}
 
-		osd create rectangle framecount -x 280 -y 235 -h 6 -w 50 -rgba 0x0077FF80 -scaled true
-		osd create text framecount.text -text "" -size 5 -rgba 0xffffffff
-		
-		framecount_update
-		return ""
-	}
+	osd create rectangle framecount -x 280 -y 235 -h 6 -w 50 -rgba 0x0077FF80 -scaled true
+	osd create text framecount.text -text "" -size 5 -rgba 0xffffffff
+
+	framecount_update
+	return ""
+}
 
 proc framecount_current {} {
 	set freq [expr {(6 * 3579545) / (1368 * (([vdpreg 9] & 1) ? 313 : 262))}]
@@ -38,20 +33,23 @@ proc framecount_update {} {
 	return ""
 }
 
-set_help_text frame_advance_mode\
-{frame_advance_mode let's you go through frames by pushing f7}
+# TODO: this doesn't work properly, why?
+# The best way is to enable some kind of global TAS mode (a boolean setting),
+# which if enabled sets up some useful key bindings for TAS
+#
+#set_help_text frame_advance_mode \
+#{frame_advance_mode let's you go through frames by pushing f7}
+#
+#proc frame_advance_mode {} {
+#	bind_default f7 "set pause off;after frame \"set pause on\""
+#}
 
-proc frame_advance_mode {} {
-	bind_default f7 "set pause off;after frame \"set pause on\""
-}
-
-# TODO:
+# more TODO:
 # real time OSD memory watch
-# TAS mode indactors
+# TAS mode indicators
 
-	namespace export toggle_frame_counter
-	namespace export frame_advance_mode
+namespace export toggle_frame_counter
+#namespace export frame_advance_mode
 }
-
 
 namespace import tas::*
