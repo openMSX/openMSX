7d0c20145daf14737a93c23c6d669582306bef66
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2010-09-26 10:29:45 +0000

    Fix initial slot and do not hardcode the number of slots where it is not necessary
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11689 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/tas_tools.tcl
+++ share/scripts/tas_tools.tcl
@@ -64,7 +64,7 @@ proc load_replay { name } {
 
 ### Very basic replay slot selector ###
 
-user_setting create string current_replay_slot "Name of the current replay slot." slot1
+user_setting create string current_replay_slot "Name of the current replay slot." slot0
 
 proc list_slots {} {
 	set slots [list]
@@ -77,15 +77,15 @@ proc list_slots {} {
 proc menu_create_slot_menu {} {
 	set items [list_slots]
 	set menu_def \
-	       { execute tas::set_slot
-	         font-size 8
-	         border-size 2
-	         width 100
-	         xpos 100
-	         ypos 100
-	         header { text "Select Replay Slot"
-	                  font-size 10
-	                  post-spacing 6 }}
+		{ execute tas::set_slot
+			font-size 8
+			border-size 2
+			width 100
+			xpos 100
+			ypos 100
+			header { text "Select Replay Slot"
+					font-size 10
+					post-spacing 6 }}
 
 	return [osd_menu::prepare_menu_list $items 10 $menu_def]
 }
@@ -98,13 +98,13 @@ proc set_slot { item } {
 proc open_select_slot_menu {} {
 	osd_menu::do_menu_open [menu_create_slot_menu]
 	osd_menu::select_menu_item $::current_replay_slot
-	for {set i 0} {$i <= 9} {incr i} {
+	for {set i 0} {$i < [llength [tas::list_slots]]} {incr i} {
 		bind_default "$i" "tas::set_slot [lindex [tas::list_slots] $i]; tas::unbind_number_keys"
 	}
 }
 
 proc unbind_number_keys {} {
-	for {set i 0} {$i <= 9} {incr i} {
+	for {set i 0} {$i < [llength [tas::list_slots]]} {incr i} {
 		unbind_default "$i"
 	}
 }
