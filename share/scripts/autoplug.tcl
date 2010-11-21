b5882415af68e6bbcbc1baac13dfceaaf539f72d
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2010-11-21 13:25:06 +0000

    Autoplug real joysticks to joystick ports (if available)
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11755 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/autoplug.tcl
+++ share/scripts/autoplug.tcl
@@ -1,7 +1,7 @@
 # if this machine has a cassetteport, then automatically plug
 # in the cassetteplayer
-# on a Dingoo, automatically plug in the joystick 
-# and assign shoulder keys [L/R] to the volume widget
+# on a Dingoo, automatically plug in the keyjoystick and if there are real
+# joysticks, plug them as well for non-Dingoo platofrms
 
 namespace eval autoplug {
 
@@ -11,12 +11,21 @@ proc do_autoplug {} {
 		#can fail when you activate an 'empty' machine
 		set connectors [machine_info connector]
 	}
+	set pluggables [list]
+	catch {
+		#can fail when you activate an 'empty' machine
+		set pluggables [machine_info pluggable]
+	}
+
+	# cassette port
 	if {[lsearch $connectors "cassetteport"] != -1} {
 		if {[string first "--empty--" [plug cassetteport]] != -1} {
 			# only when nothing already plugged
 			plug cassetteport cassetteplayer
 		}
 	}
+
+	# joystick ports
 	if {[string match *-dingux* $::tcl_platform(osVersion)]} { ;# Dingoo
 
 		if {[lsearch $connectors "joyporta"] != -1} {
@@ -27,7 +36,23 @@ proc do_autoplug {} {
 				plug joyporta keyjoystick1
 			}
 		}
+	} else {
+		if {([lsearch $connectors "joyporta"] != -1) &&
+			  ([lsearch $pluggables "joystick1"] != -1)} {
+			if {[string first "--empty--" [plug joyporta]] != -1} {
+				# only when nothing already plugged
+				plug joyporta joystick1
+			}
+		}
+		if {([lsearch $connectors "joyportb"] != -1) &&
+			  ([lsearch $pluggables "joystick2"] != -1)} {
+			if {[string first "--empty--" [plug joyportb]] != -1} {
+				# only when nothing already plugged
+				plug joyportb joystick2
+			}
+		}
 	}
+
 	after boot [namespace code do_autoplug]
 
 	# See comment in reverse.tcl (search for 'after boot')
