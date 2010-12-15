4d22325d6195d40e4824f5d8648c701fddecb475
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2010-12-15 22:07:16 +0000

    Add script to automatically create some directories for the user, so he knows where to put his systemroms and software.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11824 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ share/scripts/create_user_dirs.tcl
@@ -0,0 +1,14 @@
+# pre-create directories in the openMSX user dir, for ease of use
+
+set subdirs [list "systemroms" "software" "scripts" "skins" "machines" "extensions"]
+
+foreach subdir $subdirs {
+	set directory [file normalize $::env(OPENMSX_USER_DATA)/$subdir]
+	if {![file exists $directory]} {
+		set sourcedir [file normalize $::env(OPENMSX_SYSTEM_DATA)/$subdir]
+		file mkdir $directory
+		if {[file exists $sourcedir/README]} {
+			file copy $sourcedir/README $directory
+		}
+	}
+}
