ee4b316e5b700f126658ede4dcba1dd369f483ba
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2011-07-20 19:23:50 +0000

    Let about also search for settings, next to commands.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12206 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/about.tcl
+++ share/scripts/about.tcl
@@ -1,29 +1,66 @@
-set_help_text about \
-"Shows a list of commands that seem related to the given keyword.
-If there is only one such command the helptext for that command is also shown."
+namespace eval about {
 
-proc about {cmd} {
-	set matches [list]
-	foreach command [info commands] {
-		catch {
-			if {[regexp -nocase -- $cmd [help $command]] ||
-			    ($command eq $cmd)} {
-				lappend matches $command
-			}
-		}
-	}
+set_help_text about \
+"Shows a list of commands and/or settings that seem related to the given keyword.
+If there is only one such command or setting the helptext for that command or
+setting is also shown."
+
+proc about {keyword} {
+	set command_matches [get_matching_commands $keyword]
+	set setting_matches [get_matching_settings $keyword]
 	set result ""
-	if {[llength $matches] == 0} {
+	set lc [llength $command_matches]
+	set ls [llength $setting_matches]
+	if {$lc == 0  && $ls == 0} {
 		error "No candidates found."
-	} elseif {[llength $matches] == 1} {
-		set match [lindex $matches 0]
-		append result "$match:\n"
+	} elseif {$lc == 1 && $ls == 0} {
+		set match [lindex $command_matches 0]
+		append result "Command $match:\n"
 		append result "[help $match]\n"
+	} elseif {$lc == 0 && $ls == 1} {
+		set match [lindex $setting_matches 0]
+		append result "Setting $match:\n"
+		append result "[help set $match]\n"
 	} else {
 		append result "Multiple candidates found:\n"
-		foreach match $matches {
-			append result "  $match\n"
+		foreach match $command_matches {
+			append result "  command: $match\n"
+		}
+		foreach match $setting_matches {
+			append result "  setting: $match\n"
 		}
 	}
 	return $result
 }
+
+proc get_matching_commands {keyword} {
+	set matches [list]
+	foreach command [info commands] {
+		catch {
+			if {[regexp -nocase -- $keyword [help $command]] ||
+			    ($command eq $keyword)} {
+				lappend matches $command
+			}
+		}
+	}
+	return $matches
+}
+
+proc get_matching_settings {keyword} {
+	set matches [list]
+	foreach setting [openmsx_info setting] {
+		catch {
+			if {[regexp -nocase -- $keyword [help set $setting]] ||
+			    ($setting eq $keyword)} {
+				lappend matches $setting
+			}
+		}
+	}
+	return $matches
+}
+
+namespace export about
+
+} ;# namespace about
+
+namespace import about::*
