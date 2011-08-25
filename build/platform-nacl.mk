47168f8fa59598f42e4c5f1bb74828da763edb69
Maarten ter Huurne <maarten@treewalker.org>
2011-08-25 23:07:33 +0000

    Initial untested version of build system support for Chrome Native Client (NaCl).
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12240 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/platform-nacl.mk
@@ -0,0 +1,20 @@
+# $Id$
+#
+# Configuration for Chrome Native Client (NaCl).
+
+# Pick the right compiler.
+ifeq ($(OPENMSX_TARGET_CPU),x86)
+NACL_CPU:=i686
+else
+NACL_CPU:=$(OPENMSX_TARGET_CPU)
+endif
+CXX:=$(NACL_CPU)-nacl-g++
+
+# Does platform support symlinks?
+USE_SYMLINK:=false
+
+# File name extension of executables.
+EXEEXT:=.nexe
+
+# Build a minimal set of components.
+LINK_MODE:=3RD_STA_MIN
