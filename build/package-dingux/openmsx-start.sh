6de22512c75b5c1684fc9264863fa37672dc30f5
Maarten ter Huurne <maarten@treewalker.org>
2011-09-17 23:51:37 +0000

    OpenDingux sets HOME properly, so no need to override it.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12275 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/package-dingux/openmsx-start.sh
+++ build/package-dingux/openmsx-start.sh
@@ -3,7 +3,6 @@
 # Startup script for openMSX.
 # Run this to start openMSX; running the executable directly does not work.
 echo -n 1 > /proc/sys/vm/overcommit_memory
-export HOME=/usr/local/home
 export OPENMSX_SYSTEM_DATA=/usr/local/share/openmsx
 exec /usr/local/bin/openmsx.dge
-#exec /usr/local/bin/openmsx.dge &> ~/openmsx-log.txt
+#exec /usr/local/bin/openmsx.dge &> /tmp/openmsx-log.txt
