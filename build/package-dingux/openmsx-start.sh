91875523e1254f12e9693a9ed18c1037db89ac96
Maarten ter Huurne <maarten@treewalker.org>
2011-09-19 15:41:57 +0000

    Memory overcommit is now enabled on boot by OpenDingux.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12279 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/package-dingux/openmsx-start.sh
+++ build/package-dingux/openmsx-start.sh
@@ -2,7 +2,6 @@
 # $Id$
 # Startup script for openMSX.
 # Run this to start openMSX; running the executable directly does not work.
-echo -n 1 > /proc/sys/vm/overcommit_memory
 export OPENMSX_SYSTEM_DATA=/usr/local/share/openmsx
 exec /usr/local/bin/openmsx.dge
 #exec /usr/local/bin/openmsx.dge &> /tmp/openmsx-log.txt
