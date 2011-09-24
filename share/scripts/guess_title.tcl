1f7e6cabbd22867c462ec89fecf18d6a3ac30cc6
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2011-09-24 09:46:56 +0000

    Documented failed strategies to improve guess_rom_title
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12286 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/guess_title.tcl
+++ share/scripts/guess_title.tcl
@@ -1,11 +1,11 @@
 set_help_text guess_title \
 {Guess the title of the current game.
 This proc uses some heuristics to guess the name of the current game, based on
-the inserted ROM cartridges or disks.
+the inserted ROM cartridges, disks or cassettes.
 }
 
 proc guess_rom_title {} {
-	# first try external slots
+	# try only external slots
 	for {set ps 0} {$ps < 4} {incr ps} {
 		for {set ss 0} {$ss < 4} {incr ss} {
 			if {[machine_info isexternalslot $ps $ss]} {
@@ -18,6 +18,21 @@ proc guess_rom_title {} {
 
 proc guess_rom_title_in {ps ss} {
 	# check device name at address #4000 in given slot
+	#
+	# Note: this kind of fails for cases where the ROM is in an extension
+	# (so in external slot), but not actually running (e.g. the ROM from
+	# the Philips Music Module, after starting up with ESC, or the FMPAC
+	# ROM). The guesser will find it and report it as title. I thought of
+	# two strategies to overcome this, but both are inadequate:
+	# 1. check whether the found ROM is actually mapped into Z80 space by
+	# checking the value of I/O port 0xA8 (bit 2 and 3 should match $ps).
+	# Pretty neat: you really see what is actually running. But fails for
+	# ROMs that copy themselves to RAM (e.g. Koei games, tape games
+	# converted to ROM, etc.).
+	# 2. check whether the ROM came from an extension. But then you discard
+	# the ROM even when it is actually running (e.g. after _FMPAC)
+	# So, if someone has a good idea for this, which is not too CPU
+	# intensive, please go ahead.
 	set rom [machine_info slot $ps $ss 1]
 	if {$rom ne "empty"} {return $rom}
 	return ""
