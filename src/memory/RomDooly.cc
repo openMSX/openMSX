bdbb7601e063f8ffccde116703fb336f04a93c05
Albert Beevendorp <bifimsx@users.sourceforge.net>
2011-10-26 10:06:23 +0000

    added remaining features of the Dooly manipulation chip.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12306 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/memory/RomDooly.cc
+++ src/memory/RomDooly.cc
@@ -31,12 +31,35 @@ byte RomDooly::peekMem(word address, EmuTime::param /*time*/) const
 {
 	if ((0x4000 <= address) && (address < 0xc000)) {
 		byte value = (*rom)[address - 0x4000];
-		return (conversion == 4)
-		     ? (value & 0xf8) | ((value >> 2) & 0x01) | ((value << 1) & 0x06)
-		     : value;
-	} else {
-		return 0xff;
+		switch (conversion) {
+		case 0:
+			return value;
+		case 1:
+			return (value & 0xf8) | (value << 2 & 0x04) | (value >> 1 & 0x03);
+		case 4:
+			return (value & 0xf8) | (value >> 2 & 0x01) | (value << 1 & 0x06);
+		case 2:
+		case 5:
+		case 6:
+			switch (value & 0x07) {
+			case 1:
+			case 2:
+			case 4:
+				return value & 0xf8;
+			case 3:
+			case 5:
+			case 6:
+				if (conversion == 2) return (value & 0xf8) | (((value << 2 & 0x04) | (value >> 1 & 0x03)) ^ 0x07);
+				if (conversion == 5) return value ^ 0x07;
+				if (conversion == 6) return (value & 0xf8) | (((value >> 2 & 0x01) | (value << 1 & 0x06)) ^ 0x07);
+			default:
+				return value;
+			}
+		default:
+			return value | 0x07;
+		}
 	}
+	return 0xff;
 }
 
 byte RomDooly::readMem(word address, EmuTime::param time)
@@ -46,14 +69,14 @@ byte RomDooly::readMem(word address, EmuTime::param time)
 
 void RomDooly::writeMem(word address, byte value, EmuTime::param /*time*/)
 {
-	if ((address & 0xf000) == 0x8000) {
+	if ((0x4000 <= address) && (address < 0xc000)) {
 		conversion = value & 0x07;
 	}
 }
 
 byte* RomDooly::getWriteCacheLine(word address) const
 {
-	if ((address & 0xf000) == (0x8000 & CacheLine::HIGH)) {
+	if (((0x4000 & CacheLine::HIGH) <= address) && (address < (0xc000 & CacheLine::HIGH))) {
 		return NULL;
 	} else {
 		return unmappedWrite;
