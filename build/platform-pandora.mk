7089857ccc95733adde673741d109dc3fb6e08c2
Maarten ter Huurne <maarten@treewalker.org>
2011-12-03 18:37:30 +0000

    Initial build support for Pandora. Thanks to SladeCraven.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12319 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/platform-pandora.mk
@@ -0,0 +1,15 @@
+# $Id$
+#
+# Configuration for Pandora.
+
+ifeq ($(OPENMSX_TARGET_CPU),arm)
+# Note: CXX is automatically set by the "setprj" command.
+# Target Cortex A8 CPU.
+TARGET_FLAGS+=-march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
+endif
+
+# Pandora is a Linux system.
+include build/platform-linux.mk
+
+# Identify the binary executable file.
+EXEEXT:=.bin
