f1033c46954ebe1ec30638356d352988234deeb1
Wouter Vermaelen <m9710797@users.sourceforge.net>
2012-05-26 14:19:19 +0000

    Optimized tileviewer script
    The first invocation of tileviewer::showall in still incredibly slow (23
    seconds on my system!!). The next invocations are now a factor 4x faster
    (approx 400ms -> 100ms).
    
    The first invocation creates a lot of very small OSD rectangles, something
    seems wrong there (I mean in the c++ part, the Tcl script seems fine).
    
    BTW I still think this functionality really belongs in the debugger instead
    of in Tcl scripts.
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12556 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/tileviewer.tcl
+++ share/scripts/tileviewer.tcl
@@ -4,14 +4,18 @@ proc checkclick {} {
 
 	if {![osd exists tile_viewer]} {
 		unbind_default "mouse button1 down"
-		return ""
+		return
 	}
 
 	#check editor matrix
+	# TODO instead of checking each cell in the matrix, calculate the cell from
+	#      the mouse coordinates in the whole matrix
 	for {set xm 0} {$xm < 8} {incr xm} {
 		for {set ym 0} {$ym < 8} {incr ym} {
-			foreach {x y} [osd info "tile_viewer.matrix.${xm}_${ym}" -mousecoord] {}
+			lassign [osd info "tile_viewer.matrix.${xm}_${ym}" -mousecoord] x y
 			if {($x >= 0 && $x <= 1) && ($y >= 0 && $y <= 1)} {
+				# TODO we shouldn't use the state from the OSD elements (all OSD info commands),
+				#      instead we should vpeek from VRAM and recalculate the vram address
 
 				#lets chop this up in small pieces
 				set fc [osd info tile_viewer.fc${ym} -rgba]
@@ -19,19 +23,16 @@ proc checkclick {} {
 				set tc [osd info tile_viewer.matrix.${xm}_${ym} -rgba]
 
 				#toggle switch
-				if {$fc == $tc} \
-					{osd configure tile_viewer.matrix.${xm}_${ym} -rgba $bc -bordersize 1 -borderrgba 0xffffff80} \
-				else \
-					{osd configure tile_viewer.matrix.${xm}_${ym} -rgba $fc -bordersize 1 -borderrgba 0xffffff80}
-
+				osd configure tile_viewer.matrix.${xm}_${ym} -rgba [expr {($fc == $tc) ? $bc : $fc}]
 				set tile_add_val 0
 				for {set i 0} {$i < 8} {incr i} {
-					if {[osd info tile_viewer.matrix.${i}_${ym} -rgba] == $bc} {incr tile_add_val [expr 2**(7 - $i)]}
+					if {[osd info tile_viewer.matrix.${i}_${ym} -rgba] == $bc} {
+						incr tile_add_val [expr {0x80 >> $i}]
+					}
 				}
 
 				vpoke [osd info tile_viewer.matrix.text$ym -text] $tile_add_val
-				return ""
-
+				return
 			}
 		}
 	}
@@ -39,9 +40,9 @@ proc checkclick {} {
 
 proc showtile {tile} {
 
-	bind_default "mouse button1 down" {checkclick}
+	bind_default "mouse button1 down" {tileviewer::checkclick}
 
-	set addr [expr $tile * 8]
+	set addr [expr {$tile * 8}]
 
 	osd destroy tile_viewer
 	osd create rectangle tile_viewer -x 30 -y 30 -h 180 -w 330 -rgba 0x00007080
@@ -49,40 +50,39 @@ proc showtile {tile} {
 	osd create text tile_viewer.text -x 0 -y 0 -size 20 -text "Tile: $tile" -rgba 0xffffffff
 
 	for {set i 0} {$i < 16} {incr i} {
-		set color [getcolor $i]
-
-		set r [string range $color 0 0]
-		set g [string range $color 1 1]
-		set b [string range $color 2 2]
-
-		set rgbval($i) [expr ($r << (5 + 16)) + ($g << (5 + 8)) + ($b << 5)]
+		lassign [split [getcolor $i] {}] r g b
+		set rgbval($i) [expr {($r << (5 + 16)) + ($g << (5 + 8)) + ($b << 5)}]
 	}
 
 	# Build colors
+	set color_base   [expr {$addr + ((([vdpreg 10] << 8 | [vdpreg 3]) << 6) & 0x1e000)}]
+	set pattern_base [expr {$addr + (([vdpreg 4] & 0x3c) << 11)}]
 	for {set i 0} {$i < 8} {incr i} {
+		if {[get_screen_mode] != 1} {
+			set col [vpeek [expr {$color_base + $i}]]
+		} else {
+			set col 0xf0
+		}
 
-	if {[get_screen_mode] != 1} {set col [vpeek [expr $addr + ((([vdpreg 10] << 8 | [vdpreg 3]) << 6) & 0x1e000) + $i]]} else {set col 0xf0}
+		set bg [expr {($col & 0xf0) >> 4}]
+		set fc [expr {$col & 0x0f}]
+		osd create rectangle tile_viewer.bc$i -x 242 -w 16 -y [expr {24 + $i * 18}] -h 16 -rgb $rgbval($bg) -bordersize 1 -borderrgba 0xffffff80
+		osd create rectangle tile_viewer.fc$i -x 260 -w 16 -y [expr {24 + $i * 18}] -h 16 -rgb $rgbval($fc) -bordersize 1 -borderrgba 0xffffff80
 
-	set bg [expr ($col & 0xf0) >> 4]
-	set fc [expr $col & 0x0f]
-	osd create rectangle tile_viewer.bc$i -x 242 -w 16 -y [expr 24 + $i * 18] -h 16 -rgb $rgbval($bg) -bordersize 1 -borderrgba 0xffffff80
-	osd create rectangle tile_viewer.fc$i -x 260 -w 16 -y [expr 24 + $i * 18] -h 16 -rgb $rgbval($fc) -bordersize 1 -borderrgba 0xffffff80
-
-	osd create text tile_viewer.matrix.text$i  -x -50 -y [expr $i * 18] -text [format 0x%4.4x [expr $addr + (([vdpreg 4] & 0x3c) << 11) + $i]] -rgba 0xffffffff
-	osd create text tile_viewer.matrix.color$i -x 188 -y [expr $i * 18] -text [format 0x%2.2x $col] -rgba 0xffffffff
-}
+		osd create text tile_viewer.matrix.text$i  -x -50 -y [expr {$i * 18}] -text [format 0x%4.4x [expr {$pattern_base + $i}]] -rgba 0xffffffff
+		osd create text tile_viewer.matrix.color$i -x 188 -y [expr {$i * 18}] -text [format 0x%2.2x $col] -rgba 0xffffffff
+	}
 
 	# Build patterns
 	for {set y 0} {$y < 8} {incr y} {
 
-		set pattern [vpeek [expr $addr+ (([vdpreg 4] & 0x3c) << 11) + $y]]
+		set pattern [vpeek [expr {$pattern_base + $y}]]
 		set mask 0x80
-
+		set bc [osd info tile_viewer.bc$y -rgba]
+		set fc [osd info tile_viewer.fc$y -rgba]
 		for {set x 0} {$x < 8} {incr x} {
-			if {$pattern & $mask} {
-				osd configure tile_viewer.matrix.${x}_${y} -rgba [osd info tile_viewer.bc$y -rgba] -bordersize 1 -borderrgba 0xffffff80
-			} else {osd configure tile_viewer.matrix.${x}_${y} -rgba [osd info tile_viewer.fc$y -rgba] -bordersize 1 -borderrgba 0xffffff80}
-			set mask [expr $mask >> 1]
+			osd configure tile_viewer.matrix.${x}_${y} -rgba [expr {($pattern & $mask) ? $bc : $fc}] -bordersize 1 -borderrgba 0xffffff80
+			set mask [expr {$mask >> 1}]
 		}
 	}
 }
@@ -100,44 +100,42 @@ proc showall {} {
 		}
 	}
 
-	set screen [get_screen_mode]
-
-	for {set i 0} {$i < 16} {incr i} {
-		set color [getcolor $i]
-
-		set r [string range $color 0 0]
-		set g [string range $color 1 1]
-		set b [string range $color 2 2]
-
-		set rgbval($i) [expr ($r << (5 + 16)) + ($g << (5 + 8)) + ($b << 5)]
+	if {[get_screen_mode] != 2} {
+		for {set x 0} {$x < 256} {incr x} {
+			for {set y 0} {$y < 64} {incr y} {
+				osd configure all_tiles.${x}_${y} -rgb 0xffffff
+			}
+		}
+		return
 	}
 
-	for {set x 0} {$x < 32} {incr x} {
-		for {set y 0} {$y < 8} {incr y} {
+	for {set i 0} {$i < 16} {incr i} {
+		lassign [split [getcolor $i] {}] r g b
+		set rgbval($i) [expr {($r << (5 + 16)) + ($g << (5 + 8)) + ($b << 5)}]
+	}
 
+	set color_addr   [expr {(([vdpreg 10] << 8 | [vdpreg 3]) << 6) & 0x1e000}]
+	set pattern_addr [expr {([vdpreg 4] & 0x3c) << 11}]
+	for {set y 0} {$y < 64} {incr y 8} {
+		for {set x 0} {$x < 256} {incr x 8} {
+			set ypos $y
 			for {set yt 0} {$yt < 8} {incr yt} {
+				set pattern [vpeek $pattern_addr]
+				set col     [vpeek $color_addr]
+				incr pattern_addr
+				incr color_addr
+
+				set bc [expr {$col >> 4}]
+				set fc [expr {$col & 0x0f}]
 
 				set mask 0x80
-				set pattern [vpeek [expr (([vdpreg 4] & 0x3c) << 11) + ($x * 8) + ($y * 256) + $yt]]
-
-				if {$screen > 1} {set col [vpeek [expr ((([vdpreg 10] << 8 | [vdpreg 3]) << 6) & 0x1e000) + ($x * 8) + ($y * 256) + $yt]]} else {set col 0xf0}
-
-				set bg [expr ($col & 0xf0) >> 4]
-				set fc [expr $col & 0x0f]
-
+				set xpos $x
 				for {set xt 0} {$xt < 8} {incr xt} {
-
-					set xpos [expr ($x * 8) + $xt]
-					set ypos [expr ($y * 8) + $yt]
-
-					incr pix
-
-					if {$pattern & $mask} {
-						osd configure all_tiles.${xpos}_${ypos}  -rgb $rgbval($bg) -alpha 255
-					} else {osd configure all_tiles.${xpos}_${ypos} -rgb $rgbval($fc) -alpha 255}
-					set mask [expr $mask >> 1]
-
+					osd configure all_tiles.${xpos}_${ypos} -rgb $rgbval([expr {($pattern & $mask) ? $bc : $fc}])
+					set mask [expr {$mask >> 1}]
+					incr xpos
 				}
+				incr ypos
 			}
 		}
 	}
