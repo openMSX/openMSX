d75bd6686b1ce5342153a9fafb5d1f5e569835e6
Wouter Vermaelen <m9710797@users.sourceforge.net>
2012-05-26 15:59:45 +0000

    Replaced implementation of STATIC_ASSERT
    Because the old version gave compiler warnings like the following on recent gcc
    snapshots:
       warning: typedef ‘STATIC_ASSERT_283’ locally defined but not used [-Wunused-local-typedefs]
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12557 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/static_assert.hh
+++ src/utils/static_assert.hh
@@ -3,18 +3,18 @@
 #ifndef STATIC_ASSERT_HH
 #define STATIC_ASSERT_HH
 
-// Static assert macro
-// Based upon Boost Static Assert, see
+// Static assert macro. For documentation see
 //    http://www.boost.org/doc/html/boost_staticassert.html
-// for documentation and implementation details
+// Implementation replaced with version from
+//    http://www.pixelbeat.org/programming/gcc/static_assert.html
+// because the boost version gave compiler warnings with recent gcc versions.
+//
+// Once we switch to c++11, we should use the builtin 'static_assert' feature
+// instead of this macro.
 
-template<bool> struct STATIC_ASSERT_FAILURE;
-template<>     struct STATIC_ASSERT_FAILURE<true> {};
-template<int>  struct STATIC_ASSERT_TEST {};
 #define STATIC_ASSERT_JOIN(X, Y) STATIC_ASSERT_DO_JOIN(X, Y)
 #define STATIC_ASSERT_DO_JOIN(X, Y) X##Y
 #define STATIC_ASSERT(B) \
-  typedef STATIC_ASSERT_TEST<sizeof(STATIC_ASSERT_FAILURE<bool(B)>)> \
-    STATIC_ASSERT_JOIN(STATIC_ASSERT_, __LINE__)
+	enum { STATIC_ASSERT_JOIN(assert_line_, __LINE__) = 1 / (!!(B)) }
 
 #endif
