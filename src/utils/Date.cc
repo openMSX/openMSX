c83e130fcef7076fa3f1d33950773d9b79b2de60
Wouter Vermaelen <m9710797@users.sourceforge.net>
2012-06-06 20:32:04 +0000

    [1/2] Optimized parsing of .filecache
    In the previous patch we avoided writing the .filecache file when possible. In
    the same profile run as described in the previous patch, reading the .filecache
    file takes about 7%, and there's no easy way to skip this step. So this and the
    next patch optimize the .filecache parsing routine.
    
    The largest part of the time was spend in parsing the file modification dates.
    This patch replaces the implementation of the Date::fromString() function with
    a more efficient implementation. Though the new implementation is more lines of
    code and less easy to understand.
    
    Now the largest part of the Date::fromString() function is spend in mktime()
    (before it was sscanf()). I guess repeated calls to mktime() perform quite some
    redundant work (e.g. all the stuff related to timezones), but I couldn't find a
    portable way to speed this up.
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12589 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/utils/Date.cc
+++ src/utils/Date.cc
@@ -19,33 +19,140 @@ const char* const months[12] = {
 	"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 };
 
+template<bool FIRST, unsigned MUL, typename T>
+static inline bool parseDigit(unsigned c, T& t)
+{
+	c -= '0';
+	if (c > 9) return false;
+	if (FIRST) {
+		t = c * MUL;
+	} else {
+		t += c * MUL;
+	}
+	return true;
+}
+
 time_t fromString(const std::string& line)
 {
-	char day[4];
-	char month[4];
 	struct tm tm;
-	int items = sscanf(line.c_str(), "%3s %3s %2d %2d:%2d:%2d %4d",
-	                   day, month, &tm.tm_mday, &tm.tm_hour,
-	                   &tm.tm_min, &tm.tm_sec, &tm.tm_year);
-	tm.tm_year -= 1900;
-	tm.tm_isdst = -1;
-	tm.tm_mon = -1;
-	if ((tm.tm_sec  < 0) || (59 < tm.tm_sec)  ||
-	    (tm.tm_min  < 0) || (59 < tm.tm_min)  ||
-	    (tm.tm_hour < 0) || (23 < tm.tm_hour) ||
-	    (tm.tm_mday < 1) || (31 < tm.tm_mday) ||
-	    (tm.tm_year < 0)) {
-		return time_t(-1);
-	}
-	for (int i = 0; i < 12; ++i) {
-		if (strcmp(month, months[i]) == 0) {
-			tm.tm_mon = i;
+	const char* p = line.c_str();
+
+	// skip day
+	if (*p++ == 0) return time_t(-1);
+	if (*p++ == 0) return time_t(-1);
+	if (*p++ == 0) return time_t(-1);
+
+	// space
+	if (*p++ != ' ') return time_t(-1);
+
+	// Parse month
+	switch (*p++) {
+	case 'J': // Jan Jun Jul
+		switch (*p++) {
+		case 'a': // Jan
+			if (*p++ != 'n') return time_t(-1);
+			tm.tm_mon = 0; break;
+		case 'u': // Jun Jul
+			switch (*p++) {
+			case 'n': tm.tm_mon = 5; break;
+			case 'l': tm.tm_mon = 6; break;
+			default: return time_t(-1);
+			}
 			break;
+		default: return time_t(-1);
 		}
+		break;
+	case 'F': // Feb
+		if (*p++ != 'e') return time_t(-1);
+		if (*p++ != 'b') return time_t(-1);
+		tm.tm_mon = 1;
+		break;
+	case 'M': // Mar May
+		if (*p++ != 'a') return time_t(-1);
+		switch (*p++) {
+		case 'r': tm.tm_mon = 2; break;
+		case 'y': tm.tm_mon = 4; break;
+		default: return time_t(-1);
+		}
+		break;
+	case 'A': // Apr Aug
+		switch (*p++) {
+		case 'p': // Apr
+			if (*p++ != 'r') return time_t(-1);
+			tm.tm_mon = 3; break;
+		case 'u': // Aug
+			if (*p++ != 'g') return time_t(-1);
+			tm.tm_mon = 7; break;
+		default: return time_t(-1);
+		}
+		break;
+	case 'S': // Sep
+		if (*p++ != 'e') return time_t(-1);
+		if (*p++ != 'p') return time_t(-1);
+		tm.tm_mon = 8;
+		break;
+	case 'O': // Oct
+		if (*p++ != 'c') return time_t(-1);
+		if (*p++ != 't') return time_t(-1);
+		tm.tm_mon = 9;
+		break;
+	case 'N': // Nov
+		if (*p++ != 'o') return time_t(-1);
+		if (*p++ != 'v') return time_t(-1);
+		tm.tm_mon = 10;
+		break;
+	case 'D': // Dec
+		if (*p++ != 'e') return time_t(-1);
+		if (*p++ != 'c') return time_t(-1);
+		tm.tm_mon = 11;
+		break;
+	default: return time_t(-1);
 	}
-	if ((items != 7) || (tm.tm_mon == -1)) {
-		return time_t(-1);
-	}
+
+	// space
+	if (*p++ != ' ') return time_t(-1);
+
+	// parse mday
+	if (!parseDigit<true, 10>(*p++, tm.tm_mday)) return time_t(-1);
+	if (!parseDigit<false, 1>(*p++, tm.tm_mday)) return time_t(-1);
+	if ((tm.tm_mday < 1) || (31 < tm.tm_mday) ) return time_t(-1);
+
+	// space
+	if (*p++ != ' ') return time_t(-1);
+
+	// parse hour
+	if (!parseDigit<true, 10>(*p++, tm.tm_hour)) return time_t(-1);
+	if (!parseDigit<false, 1>(*p++, tm.tm_hour)) return time_t(-1);
+	if ((tm.tm_hour < 0) || (23 < tm.tm_hour)) return time_t(-1);
+
+	// colon
+	if (*p++ != ':') return time_t(-1);
+
+	// parse minute
+	if (!parseDigit<true, 10>(*p++, tm.tm_min)) return time_t(-1);
+	if (!parseDigit<false, 1>(*p++, tm.tm_min)) return time_t(-1);
+	if ((tm.tm_min < 0) || (59 < tm.tm_min)) return time_t(-1);
+
+	// colon
+	if (*p++ != ':') return time_t(-1);
+
+	// parse second
+	if (!parseDigit<true, 10>(*p++, tm.tm_sec)) return time_t(-1);
+	if (!parseDigit<false, 1>(*p++, tm.tm_sec)) return time_t(-1);
+	if ((tm.tm_sec < 0) || (59 < tm.tm_sec)) return time_t(-1);
+
+	// space
+	if (*p++ != ' ') return time_t(-1);
+
+	// parse year
+	if (!parseDigit<true, 1000>(*p++, tm.tm_year)) return time_t(-1);
+	if (!parseDigit<false, 100>(*p++, tm.tm_year)) return time_t(-1);
+	if (!parseDigit<false,  10>(*p++, tm.tm_year)) return time_t(-1);
+	if (!parseDigit<false,   1>(*p++, tm.tm_year)) return time_t(-1);
+	tm.tm_year -= 1900;
+	if (tm.tm_year < 0) return time_t(-1);
+
+	tm.tm_isdst = -1;
 	return mktime(&tm);
 }
 
