b631b1cb469cdbf8371c96062eaec02b2dbb4a0a
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2012-11-11 17:29:55 +0000

    Add extensions for ROM and disk images used on the MCCM cd, for convenience.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12956 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/osd_menu.tcl
+++ share/scripts/osd_menu.tcl
@@ -946,7 +946,7 @@ proc ls {directory extensions} {
 }
 
 proc menu_create_ROM_list {path} {
-	return [prepare_menu_list [concat "--eject--" [ls $path "rom|zip|gz"]] \
+	return [prepare_menu_list [concat "--eject--" [ls $path "rom|ri|zip|gz"]] \
 	                          10 \
 	                          { execute menu_select_rom
 	                            font-size 8
@@ -980,7 +980,7 @@ proc menu_select_rom {item} {
 }
 
 proc menu_create_disk_list {path} {
-	return [prepare_menu_list [concat "--eject--" [ls $path "dsk|zip|gz|xsa|dmk"]] \
+	return [prepare_menu_list [concat "--eject--" [ls $path "dsk|zip|gz|xsa|dmk|di1|di2"]] \
 	                          10 \
 	                          { execute menu_select_disk
 	                            font-size 8
