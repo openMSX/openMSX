a13b29e8672f2b60c27fcfd2327c320a042bcce4
Wouter Vermaelen <m9710797@users.sourceforge.net>
2012-11-17 16:31:41 +0000

    [1/2] Update LTO build flavour
    Don't pass -fno-fat-lto-objects: gcc-4.6 doesn't know this option yet.
    
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@12969 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/flavour-lto.mk
+++ build/flavour-lto.mk
@@ -1,7 +1,7 @@
 # $Id$
 #
 # This flavour enables link-time optimization (LTO).
-# This requires GCC 4.5 or higher; it's known to work on GCC 4.7.1.
+# This requires GCC 4.5 or higher; it's known to work on GCC 4.6.3.
 
 include build/flavour-opt.mk
 
@@ -10,5 +10,12 @@ include build/flavour-opt.mk
 # will be generated in the link phase.
 # Ideally LTO would be enabled for the 3rdparty libs as well, but I haven't
 # been able to make that work.
-COMPILE_FLAGS+=-flto -fno-fat-lto-objects
+COMPILE_FLAGS+=-flto
 LINK_FLAGS+=-flto
+
+# Enable this line to speed up compilation. This is supported from gcc-4.7
+# onward. Quote from the gcc manual:
+#     -fno-fat-lto-objects improves compilation time over plain LTO, but
+#     requires the complete toolchain to be aware of LTO. It requires a
+#     linker with linker plugin support for basic functionality.
+#COMPILE_FLAGS+=-fno-fat-lto-objects
