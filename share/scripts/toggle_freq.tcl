366b7d267f7e04a5cff8a01e9c8dc5245474127c
Patrick van Arkel <vampier@users.sourceforge.net>
2012-12-18 20:56:36 +0000

    visual feedback when using the toggle_freq command.
    git-svn-id: file:///openmsx-svn/code/openmsx/trunk@13060 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- share/scripts/toggle_freq.tcl
+++ share/scripts/toggle_freq.tcl
@@ -3,4 +3,7 @@ set_help_text toggle_freq "Switch between PAL/NTSC."
 proc toggle_freq {} {
 	debug write "VDP regs" 9    [expr {[debug read "VDP regs" 9]    ^ 2}]
 	debug write "memory" 0xFFE8 [expr {[debug read "memory" 0xFFE8] ^ 2}]
+	
+	if {[debug read "VDP regs" 9]==0} {osd::display_message "Frequency set to 60hz" info} else {osd::display_message "Frequency set to 50hz" info}
+	
 }
