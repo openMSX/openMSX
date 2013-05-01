1d97a3a4860dea6cd668e7b981ee5f1a264a2af7
Alex Wulms <awulms@nospam.thevoid>
2013-05-01 22:18:13 +0200

    Fix android 'version code'. It must be an increasing number.

--- build/android/openmsx/launch_anddev_build.sh
+++ build/android/openmsx/launch_anddev_build.sh
@@ -20,13 +20,19 @@ my_app_android_dir="$(pwd)"
 PYTHONPATH="${my_home_dir}/build"
 export PYTHONPATH
 cd "${my_home_dir}"
-REVISION=$(python -c "import version; print version.extractRevisionString()")
-if [ "${REVISION}" = "unknown" ]; then
-  echo "LAB:ERROR Could not determine revision"
-  exit 1
-fi
+# The (Android) version code must be an increasing number so that Android application
+# manager can recognize that it is a new version of the same application and take
+# appropriate action like retaining or migrating the user settings
+# The easiest way to get an increasing number for new builds in git is by
+# counting the number of commit messages.
+VERSION_CODE=$(git log --oneline | wc -l)
+
+# The (Android) version name can be any arbitrary string that is hopefully
+# meaningfull to the user. Best is to use the version package name
+# to be aligned with the version name used for builds for other platforms.
 VERSION_NAME=$(python -c "import version; print version.getVersionedPackageName()")
-echo "LAB:INFO building revision ${REVISION} with version name ${VERSION_NAME}"
+
+echo "LAB:INFO building version ${VERSION_CODE} with version name ${VERSION_NAME}"
 
 # Return to the directory containing this script and the AndroidAppSettings files
 cd "${my_app_android_dir}"
@@ -35,9 +41,9 @@ APP_SETTINGS_CFG="${my_home_dir}/build/android/openmsx/AndroidAppSettings.cfg"
 cp ${APP_SETTINGS_CFG}.template ${APP_SETTINGS_CFG}
 . ${APP_SETTINGS_CFG}
 MANIFEST="${sdl_android_port_path}/project/AndroidManifest.xml"
-if [ "$AppVersionCode" != "${REVISION}" ]; then
-	sed -i "s/^AppVersionCode=.*$/AppVersionCode=${REVISION}/" ${APP_SETTINGS_CFG}
-#	sed -i "s^android:versionCode=.*^android:versionCode=\"${REVISION}\"^" ${MANIFEST}
+if [ "$AppVersionCode" != "${VERSION_CODE}" ]; then
+	sed -i "s/^AppVersionCode=.*$/AppVersionCode=${VERSION_CODE}/" ${APP_SETTINGS_CFG}
+#	sed -i "s^android:versionCode=.*^android:versionCode=\"${VERSION_CODE}\"^" ${MANIFEST}
 fi
 if [ "$AppVersionName" != "${VERSION_NAME}" ]; then
 	sed -i "s/^AppVersionName=.*$/AppVersionName=${VERSION_NAME}/" ${APP_SETTINGS_CFG}
