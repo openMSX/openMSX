openMSX/openMSX

commit b3b49aeeba719bada07f2cc173649fd16e75e088

Improve openGL wrapper objects
The classes Texture, FrameBufferObject and PixelBuffer are wrappers around the
corresponding openGL concepts. Basically these objects just wrap an integer
that is a handle for the openGL concept. They manage the lifetime of this
concept (properly initialize and cleanup) and add some convenient helper
methods.

Because these classes internally store just one int, it's a shame to have to
allocate these objects on the heap. Similar to the previous patches, this patch
makes the objects movable, but not copyable. And that allows to pass them around
as values while still protecting against double uses of the same resource.

--

Apart from turning these wrapper into value objects, this patch also makes a
slight change in the Texture class. Initially this class could handle 1D, 2D
and 3D texture types (or any other special texture type). It stored the type of
the texture in the wrapper object. Though we only ever used 2D textures (I
think way in the past we also used 1D textures). So now I've removed this type
member and always assume 2D textures.

git-svn-id: file:///openmsx-svn/code/openmsx/trunk@13183 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/video/SDLGLOutputSurface.cc
@@ -92,8 +92,7 @@ void SDLGLOutputSurface::init(OutputSurface& output)
 		texCoordX = double(width)  / texW;
 		texCoordY = double(height) / texH;
 
-		fbTex = make_unique<Texture>();
-		fbTex->bind();
+		fbTex.bind();
 		if (frameBuffer == FB_16BPP) {
 			// TODO: Why use RGB texture instead of RGBA?
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texW, texH, 0,
@@ -109,7 +108,7 @@ void SDLGLOutputSurface::flushFrameBuffer(unsigned width, unsigned height)
 {
 	assert((frameBuffer == FB_16BPP) || (frameBuffer == FB_32BPP));
 
-	fbTex->bind();
+	fbTex.bind();
 	if (frameBuffer == FB_16BPP) {
 		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
 		                GL_RGB, GL_UNSIGNED_SHORT_5_6_5, fbBuf.data());

src/video/SDLGLOutputSurface.hh
@@ -38,7 +38,7 @@ protected:
 
 private:
 	double texCoordX, texCoordY;
-	std::unique_ptr<Texture> fbTex;
+	Texture fbTex;
 	MemBuffer<char> fbBuf;
 	const FrameBuffer frameBuffer;
 };

src/video/GLPostProcessor.cc
@@ -62,10 +62,9 @@ GLPostProcessor::GLPostProcessor(
 
 	storedFrame = false;
 	for (int i = 0; i < 2; ++i) {
-		colorTex[i] = make_unique<Texture>();
-		colorTex[i]->bind();
-		colorTex[i]->setWrapMode(false);
-		colorTex[i]->enableInterpolation();
+		colorTex[i].bind();
+		colorTex[i].setWrapMode(false);
+		colorTex[i].enableInterpolation();
 		glTexImage2D(GL_TEXTURE_2D,     // target
 			     0,                 // level
 			     GL_RGB8,           // internal format
@@ -75,7 +74,7 @@ GLPostProcessor::GLPostProcessor(
 			     GL_RGB,            // format
 			     GL_UNSIGNED_BYTE,  // type
 			     nullptr);          // data
-		fbo[i] = make_unique<FrameBufferObject>(*colorTex[i]);
+		fbo[i] = FrameBufferObject(colorTex[i]);
 	}
 
 	monitor3DList = glGenLists(1);
@@ -174,7 +173,7 @@ void GLPostProcessor::paint(OutputSurface& /*output*/)
 	if (renderToTexture) {
 		glViewport(0, 0, screen.getWidth(), screen.getHeight());
 		glBindTexture(GL_TEXTURE_2D, 0);
-		fbo[frameCounter & 1]->push();
+		fbo[frameCounter & 1].push();
 	}
 
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
@@ -184,9 +183,9 @@ void GLPostProcessor::paint(OutputSurface& /*output*/)
 		//	r.srcStartY, r.srcEndY, r.lineWidth);
 		assert(textures.find(r.lineWidth) != textures.end());
 		auto superImpose = superImposeVideoFrame
-		                 ? superImposeTex.get() : nullptr;
+		                 ? &superImposeTex : nullptr;
 		currScaler->scaleImage(
-			*textures[r.lineWidth].tex, superImpose,
+			textures[r.lineWidth].tex, superImpose,
 			r.srcStartY, r.srcEndY, r.lineWidth,       // src
 			r.dstStartY, r.dstEndY, screen.getWidth(), // dst
 			paintFrame->getHeight()); // dst
@@ -199,8 +198,8 @@ void GLPostProcessor::paint(OutputSurface& /*output*/)
 	drawGlow(glow);
 
 	if (renderToTexture) {
-		fbo[frameCounter & 1]->pop();
-		colorTex[frameCounter & 1]->bind();
+		fbo[frameCounter & 1].pop();
+		colorTex[frameCounter & 1].bind();
 		glViewport(screen.getX(), screen.getY(),
 		           screen.getWidth(), screen.getHeight());
 
@@ -269,13 +268,12 @@ void GLPostProcessor::uploadFrame()
 	if (superImposeVideoFrame) {
 		int width  = superImposeVideoFrame->getWidth();
 		int height = superImposeVideoFrame->getHeight();
-		if (!superImposeTex.get() ||
-		    superImposeTex->getWidth()  != width ||
-		    superImposeTex->getHeight() != height) {
-			superImposeTex = make_unique<ColorTexture>(width, height);
-			superImposeTex->enableInterpolation();
+		if (superImposeTex.getWidth()  != width ||
+		    superImposeTex.getHeight() != height) {
+			superImposeTex.resize(width, height);
+			superImposeTex.enableInterpolation();
 		}
-		superImposeTex->bind();
+		superImposeTex.bind();
 		glTexSubImage2D(
 			GL_TEXTURE_2D,     // target
 			0,                 // level
@@ -297,31 +295,28 @@ void GLPostProcessor::uploadBlock(
 	if (it == textures.end()) {
 		TextureData textureData;
 
-		textureData.tex = make_unique<ColorTexture>(
+		textureData.tex = ColorTexture(
 			lineWidth, height * 2); // *2 for interlace
-		textureData.tex->setWrapMode(false);
+		textureData.tex.setWrapMode(false);
 
-		textureData.pbo = make_unique<PixelBuffer<unsigned>>();
-		if (textureData.pbo->openGLSupported()) {
-			textureData.pbo->setImage(lineWidth, height * 2);
-		} else {
-			textureData.pbo = nullptr;
+		if (textureData.pbo.openGLSupported()) {
+			textureData.pbo.setImage(lineWidth, height * 2);
 		}
 
 		it = textures.insert(std::make_pair(
 			lineWidth, std::move(textureData))).first;
 	}
-	ColorTexture& tex = *it->second.tex;
-	PixelBuffer<unsigned>* pbo = it->second.pbo.get();
+	auto& tex = it->second.tex;
+	auto& pbo = it->second.pbo;
 
 	// bind texture
 	tex.bind();
 
 	// upload data
 	unsigned* mapped;
-	if (pbo) {
-		pbo->bind();
-		mapped = pbo->mapWrite();
+	if (pbo.openGLSupported()) {
+		pbo.bind();
+		mapped = pbo.mapWrite();
 	} else {
 		mapped = nullptr;
 	}
@@ -333,7 +328,7 @@ void GLPostProcessor::uploadBlock(
 				mapped + y * lineWidth, data, lineWidth);
 			paintFrame->freeLineBuffers(); // ASAP to keep cache warm
 		}
-		pbo->unmap();
+		pbo.unmap();
 #if defined(__APPLE__)
 		// The nVidia GL driver for the GeForce 8000/9000 series seems to hang
 		// on texture data replacements that are 1 pixel wide and start on a
@@ -351,10 +346,10 @@ void GLPostProcessor::uploadBlock(
 			srcEndY - srcStartY, // height
 			GL_BGRA,             // format
 			GL_UNSIGNED_BYTE,    // type
-			pbo->getOffset(0, srcStartY)); // data
+			pbo.getOffset(0, srcStartY)); // data
 	}
-	if (pbo) {
-		pbo->unbind();
+	if (pbo.openGLSupported()) {
+		pbo.unbind();
 	}
 	if (!mapped) {
 		glPixelStorei(GL_UNPACK_ROW_LENGTH, paintFrame->getRowLength());
@@ -392,7 +387,7 @@ void GLPostProcessor::drawGlow(int glow)
 {
 	if ((glow == 0) || !storedFrame) return;
 
-	colorTex[(frameCounter & 1) ^ 1]->bind();
+	colorTex[(frameCounter & 1) ^ 1].bind();
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);

src/video/GLPostProcessor.hh
@@ -15,8 +15,6 @@ namespace openmsx {
 class GLScaler;
 class MSXMotherBoard;
 class Display;
-class Texture;
-class FrameBufferObject;
 
 /** Rasterizer using SDL.
   */
@@ -56,8 +54,8 @@ private:
 	  */
 	std::unique_ptr<GLScaler> currScaler;
 
-	std::unique_ptr<Texture> colorTex[2];
-	std::unique_ptr<FrameBufferObject> fbo[2];
+	Texture colorTex[2];
+	FrameBufferObject fbo[2];
 
 	// Noise effect:
 	LuminanceTexture noiseTextureA;
@@ -69,12 +67,12 @@ private:
 		TextureData();
 		TextureData(TextureData&& rhs);
 
-		std::unique_ptr<ColorTexture> tex;
-		std::unique_ptr<PixelBuffer<unsigned>> pbo;
+		ColorTexture tex;
+		PixelBuffer<unsigned> pbo;
 	};
 	std::map<unsigned, TextureData> textures;
 
-	std::unique_ptr<ColorTexture> superImposeTex;
+	ColorTexture superImposeTex;
 
 	struct Region {
 		Region(unsigned srcStartY_, unsigned srcEndY_,

src/video/GLUtil.cc
@@ -44,8 +44,7 @@ void checkGLError(const string& prefix)
 
 // class Texture
 
-Texture::Texture(int type_)
-	: type(type_)
+Texture::Texture()
 {
 	glGenTextures(1, &textureId);
 	disableInterpolation();
@@ -53,36 +52,35 @@ Texture::Texture(int type_)
 
 Texture::~Texture()
 {
-	glDeleteTextures(1, &textureId);
+	glDeleteTextures(1, &textureId); // ok to delete 0-texture
 }
 
 void Texture::enableInterpolation()
 {
 	bind();
-	glTexParameteri(type, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(type, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 void Texture::disableInterpolation()
 {
 	bind();
-	glTexParameteri(type, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(type, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 
 void Texture::setWrapMode(bool wrap)
 {
 	bind();
 	int mode = wrap ? GL_REPEAT : GL_CLAMP;
-	glTexParameteri(type, GL_TEXTURE_WRAP_S, mode);
-	glTexParameteri(type, GL_TEXTURE_WRAP_T, mode);
-	glTexParameteri(type, GL_TEXTURE_WRAP_R, mode);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mode);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mode);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, mode);
 }
 
 void Texture::drawRect(GLfloat tx, GLfloat ty, GLfloat twidth, GLfloat theight,
                        GLint   x,  GLint   y,  GLint   width,  GLint   height)
 {
-	assert(type == GL_TEXTURE_2D);
 	const GLint x2 = x + width;
 	const GLint y2 = y + height;
 	const GLfloat tx2 = tx + twidth;
@@ -102,10 +100,15 @@ void Texture::drawRect(GLfloat tx, GLfloat ty, GLfloat twidth, GLfloat theight,
 // class ColorTexture
 
 ColorTexture::ColorTexture(GLsizei width_, GLsizei height_)
-	: Texture(GL_TEXTURE_2D)
+{
+	resize(width_, height_);
+}
+
+void ColorTexture::resize(GLsizei width_, GLsizei height_)
 {
 	width = width_;
 	height = height_;
+	bind();
 	glTexImage2D(
 		GL_TEXTURE_2D,    // target
 		0,                // level
@@ -122,7 +125,6 @@ ColorTexture::ColorTexture(GLsizei width_, GLsizei height_)
 // class LuminanceTexture
 
 LuminanceTexture::LuminanceTexture(GLsizei width, GLsizei height)
-	: Texture(GL_TEXTURE_2D)
 {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -160,9 +162,13 @@ void LuminanceTexture::updateImage(
 static GLuint currentId = 0;
 static std::vector<GLuint> stack;
 
+FrameBufferObject::FrameBufferObject()
+	: bufferId(0) // 0 is not a valid openGL name
+{
+}
+
 FrameBufferObject::FrameBufferObject(Texture& texture)
 {
-	assert(texture.type == GL_TEXTURE_2D);
 	glGenFramebuffersEXT(1, &bufferId);
 	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, bufferId);
 	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT,
@@ -180,6 +186,10 @@ FrameBufferObject::FrameBufferObject(Texture& texture)
 
 FrameBufferObject::~FrameBufferObject()
 {
+	// It's ok to delete '0' (it's a NOP), but we anyway have to check
+	// for pop().
+	if (!bufferId) return;
+
 	if (currentId == bufferId) {
 		pop();
 	}

src/video/GLUtil.hh
@@ -17,6 +17,7 @@
 #endif
 
 #include "MemBuffer.hh"
+#include "noncopyable.hh"
 #include "build-info.hh"
 #include <string>
 #include <cassert>
@@ -62,12 +63,27 @@ typedef ExpandFilter<GLuint>::ExpandType ExpandGL;
 
 
 /** Most basic/generic texture: only contains a texture ID.
+  * Current implementation always assumes 2D textures.
   */
-class Texture
+class Texture : public noncopyable
 {
 public:
-	explicit Texture(int type = GL_TEXTURE_2D);
-	virtual ~Texture();
+	/** Default constructor, allocate a openGL texture name. */
+	Texture();
+
+	/** Move constructor and assignment. */
+	Texture(Texture&& other)
+		: textureId(other.textureId)
+	{
+		other.textureId = 0; // 0 is not a valid openGL texture name
+	}
+	Texture& operator=(Texture&& other) {
+		std::swap(textureId, other.textureId);
+		return *this;
+	}
+
+	/** Release openGL texture name. */
+	~Texture();
 
 	/** Makes this texture the active GL texture.
 	  * The other methods of this class and its subclasses will implicitly
@@ -75,7 +91,7 @@ public:
 	  * this texture for use in GL function calls outside of this class.
 	  */
 	void bind() {
-		glBindTexture(type, textureId);
+		glBindTexture(GL_TEXTURE_2D, textureId);
 	}
 
 	/** Enables bilinear interpolation for this texture.
@@ -95,7 +111,6 @@ public:
 	              GLint   x,  GLint   y,  GLint   width,  GLint   height);
 
 protected:
-	const int type;
 	GLuint textureId;
 
 	friend class FrameBufferObject;
@@ -104,10 +119,13 @@ protected:
 class ColorTexture : public Texture
 {
 public:
+	ColorTexture() : width(0), height(0) {}
+
 	/** Create color texture with given size.
 	  * Initial content is undefined.
 	  */
 	ColorTexture(GLsizei width, GLsizei height);
+	void resize(GLsizei width, GLsizei height);
 
 	GLsizei getWidth () const { return width;  }
 	GLsizei getHeight() const { return height; }
@@ -132,10 +150,20 @@ public:
 	                 GLbyte* data);
 };
 
-class FrameBufferObject
+class FrameBufferObject : public noncopyable
 {
 public:
+	FrameBufferObject();
 	explicit FrameBufferObject(Texture& texture);
+	FrameBufferObject(FrameBufferObject&& other)
+		: bufferId(other.bufferId)
+	{
+		other.bufferId = 0;
+	}
+	FrameBufferObject& operator=(FrameBufferObject&& other) {
+		std::swap(bufferId, other.bufferId);
+		return *this;
+	}
 	~FrameBufferObject();
 
 	void push();
@@ -157,10 +185,12 @@ struct PixelBuffers
   * otherwise.
   * The pixel type is templatized T.
   */
-template <typename T> class PixelBuffer
+template <typename T> class PixelBuffer : public noncopyable
 {
 public:
 	PixelBuffer();
+	PixelBuffer(PixelBuffer&& other);
+	PixelBuffer& operator=(PixelBuffer&& other);
 	~PixelBuffer();
 
 	/** Are PBOs supported by this openGL implementation?
@@ -243,6 +273,26 @@ PixelBuffer<T>::PixelBuffer()
 		//std::cerr << "OpenGL pixel buffers are not available" << std::endl;
 		bufferId = 0;
 	}
+}
+
+template <typename T>
+PixelBuffer<T>::PixelBuffer(PixelBuffer<T>&& other)
+	: allocated(std::move(other.allocated))
+	, bufferId(other.bufferId)
+	, width(other.width)
+	, height(other.height)
+{
+	other.bufferId = 0;
+}
+
+template <typename T>
+PixelBuffer<T>& PixelBuffer<T>::operator=(PixelBuffer<T>&& other)
+{
+	std::swap(allocated, other.allocated);
+	std::swap(bufferId,  other.bufferId);
+	std::swap(width,     other.width);
+	std::swap(height,    other.height);
+	return *this;
 }
 
 template <typename T>
@@ -346,8 +396,6 @@ void PixelBuffer<T>::unmap() const
 class Shader
 {
 public:
-	virtual ~Shader();
-
 	/** Returns true iff this shader is loaded and compiled without errors.
 	  */
 	bool isOK() const;
@@ -360,6 +408,7 @@ protected:
 	Shader(GLenum type, const std::string& filename);
 	Shader(GLenum type, const std::string& header,
 	                    const std::string& filename);
+	~Shader();
 
 private:
 	void init(GLenum type, const std::string& header,
@@ -399,7 +448,7 @@ public:
 /** Wrapper around an OpenGL program:
   * a collection of vertex and fragment shaders.
   */
-class ShaderProgram
+class ShaderProgram : public noncopyable
 {
 public:
 	ShaderProgram();

src/video/SDLGLOffScreenSurface.cc
@@ -14,10 +14,9 @@ SDLGLOffScreenSurface::SDLGLOffScreenSurface(const SDLGLVisibleSurface& output)
 	setSDLDisplaySurface(
 		const_cast<SDL_Surface*>(output.getSDLDisplaySurface()));
 
-	fboTex = make_unique<Texture>();
-	fboTex->bind();
-	fboTex->setWrapMode(false);
-	fboTex->enableInterpolation();
+	fboTex.bind();
+	fboTex.setWrapMode(false);
+	fboTex.enableInterpolation();
 	glTexImage2D(GL_TEXTURE_2D,    // target
 	             0,                // level
 	             GL_RGB8,          // internal format
@@ -27,8 +26,8 @@ SDLGLOffScreenSurface::SDLGLOffScreenSurface(const SDLGLVisibleSurface& output)
 	             GL_RGB,           // format
 	             GL_UNSIGNED_BYTE, // type
 	             nullptr);         // data
-	fbo = make_unique<FrameBufferObject>(*fboTex);
-	fbo->push();
+	fbo = FrameBufferObject(fboTex);
+	fbo.push();
 
 	SDLGLOutputSurface::init(*this);
 }

src/video/SDLGLOffScreenSurface.hh
@@ -5,13 +5,11 @@
 
 #include "OutputSurface.hh"
 #include "SDLGLOutputSurface.hh"
-#include <memory>
+#include "GLUtil.hh"
 
 namespace openmsx {
 
 class SDLGLVisibleSurface;
-class Texture;
-class FrameBufferObject;
 
 /** This class installs a FrameBufferObject (FBO). So as long as this object
   * is live, all openGL draw commands will be redirected to this FBO.
@@ -27,8 +25,8 @@ private:
 	virtual void saveScreenshot(const std::string& filename);
 	virtual void flushFrameBuffer();
 
-	std::unique_ptr<Texture> fboTex;
-	std::unique_ptr<FrameBufferObject> fbo;
+	Texture fboTex;
+	FrameBufferObject fbo;
 };
 
 } // namespace openmsx

commit 5a937fa74cb96cab6edfc6421d5ffd76e03ccd46

Fix SDLGL-FB{16,32} renderers
These were broken since revision b3b49ae (2013/02/26). We created a texture
before openGL was initialized.

These renderers are not enabled in release mode, they are only useful
during development. See also next patch.

src/video/SDLGLOutputSurface.cc
@@ -90,7 +90,8 @@ void SDLGLOutputSurface::init(OutputSurface& output)
 		texCoordX = double(width)  / texW;
 		texCoordY = double(height) / texH;
 
-		fbTex.bind();
+		fbTex = make_unique<Texture>();
+		fbTex->bind();
 		if (frameBuffer == FB_16BPP) {
 			// TODO: Why use RGB texture instead of RGBA?
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texW, texH, 0,
@@ -106,7 +107,7 @@ void SDLGLOutputSurface::flushFrameBuffer(unsigned width, unsigned height)
 {
 	assert((frameBuffer == FB_16BPP) || (frameBuffer == FB_32BPP));
 
-	fbTex.bind();
+	fbTex->bind();
 	if (frameBuffer == FB_16BPP) {
 		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
 		                GL_RGB, GL_UNSIGNED_SHORT_5_6_5, fbBuf.data());

src/video/SDLGLOutputSurface.hh
@@ -36,7 +36,7 @@ protected:
 
 private:
 	double texCoordX, texCoordY;
-	Texture fbTex;
+	std::unique_ptr<Texture> fbTex;
 	MemBuffer<char> fbBuf;
 	const FrameBuffer frameBuffer;
 };