078a1cc4a97e2d61faf3dd8d4a43b7314dca30fa
wouter <vermaelen.wouter@gmail.com>
2013-05-25 09:36:42 +0200

    Fixed new ResampleHQ for 32-bit x86
    The intrinsic _mm_cvtsi128_si64() only exists for 64-bit. So the previous patch
    broke the 32-bit build on x86 machines. Luckily the workaround is not too
    difficult. Though apparently it is difficult to get this asm stuff right on the
    first try ;-)
    
    The old asm code always went via the (old) MMX registers to store the 64-bit
    result to memory. Though because this does require adding an extra (slow)
    'emms' instruction I explicitly wanted to avoid that in the new code. Anyway,
    this is not on the hot-code-path, so the performace point is mood, but the
    new code may actually be faster even on 32-bit CPUs.

--- src/sound/ResampleHQ.cc
+++ src/sound/ResampleHQ.cc
@@ -287,7 +287,12 @@ static inline void calcSseStereo(const float* buf_, const float* tab_, long len,
 	// Can faster with SSE3, but (like above) not worth the trouble.
 	__m128 s = _mm_add_ps(a, _mm_movehl_ps(a, a));
 	__m128i si = _mm_cvtps_epi32(s);
+#if defined(__x86_64__) || defined(_WIN64)
 	*reinterpret_cast<int64_t*>(out) = _mm_cvtsi128_si64(si);
+#else
+	out[0] = _mm_cvtsi128_si32(si);
+	out[1] = _mm_cvtsi128_si32(_mm_shuffle_epi32(si, 0x55));
+#endif
 }
 #endif
 
