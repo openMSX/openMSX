345bc31ba032448dc1d2029ca50b4c75d20d6b15
wouter <vermaelen.wouter@gmail.com>
2013-05-30 16:18:51 +0200

    Replace MemoryOps::memset_128_SSE inline asm routine with intrinsics
    Similar to the previous few patches. Both the old and the new routine use SSE2.
    There is no measurable performance difference between the two. Actually gcc's
    auto-vectorizer generates almost equally well performing code from the c++
    version. Maybe in the future when clang has a good vectorizer we can drop our
    own SSE2 version. I don't know how good vc++'s vectorizer is.

--- src/utils/MemoryOps.cc
+++ src/utils/MemoryOps.cc
@@ -1,279 +1,110 @@
 #include "MemoryOps.hh"
-#include "HostCPU.hh"
 #include "likely.hh"
-#include "openmsx.hh"
 #include "build-info.hh"
 #include "systemfuncs.hh"
 #include "Math.hh"
 #include "unreachable.hh"
-#include <type_traits>
 #include <map>
 #include <cassert>
-#include <cstring>
 #include <cstdlib>
 #include <new> // for std::bad_alloc
 #if ASM_X86 && defined _MSC_VER
 #include <intrin.h>	// for __stosd intrinsic
 #endif
+#ifdef __SSE2__
+#include <emmintrin.h>
+#endif
 
 namespace openmsx {
 namespace MemoryOps {
 
-// This provides no noticeable performance improvement in
-// emulator benchmarks with VC++. Consequently, there's no reason
-// to write a Win64 ASM version of this.
-#if ASM_X86 && !defined _WIN64
-// note: xmm0 must already be filled in
-//       bit0 of num is ignored
-
-static inline void memset_128_SSE(uint64_t* dest, size_t num)
-{
-	assert((size_t(dest) & 15) == 0); // must be 16-byte aligned
-	uint64_t* e = dest + num - 3;
-	for (/**/; dest < e; dest += 4) {
-#if defined _MSC_VER
-		__asm {
-			mov			eax,dest
-			movaps		xmmword ptr [eax],xmm0
-			movaps		xmmword ptr [eax+10h],xmm0
-		}
-#else
-		asm volatile (
-			"movaps %%xmm0,   (%[OUT]);"
-			"movaps %%xmm0, 16(%[OUT]);"
-			: // no output
-			: [OUT] "r" (dest)
-			: "memory"
-		);
-#endif
-	}
-	if (unlikely(num & 2)) {
-#if defined _MSC_VER
-		__asm {
-			mov			eax,dest
-			movaps		xmmword ptr [eax],xmm0
-		}
-#else
-		asm volatile (
-			"movaps %%xmm0, (%[OUT]);"
-			: // no output
-			: [OUT] "r" (dest)
-			: "memory"
-		);
-#endif
-	}
-}
-
+#ifdef __SSE2__
 static inline void memset_64_SSE(
-	uint64_t* dest, size_t num, uint64_t val)
+	uint64_t* dest, size_t num64, uint64_t val64)
 {
-	assert((size_t(dest) & 7) == 0); // must be 8-byte aligned
+	if (unlikely(num64 == 0)) return;
 
-	if (unlikely(num == 0)) return;
+	// Align at 16-byte boundary.
 	if (unlikely(size_t(dest) & 8)) {
-		// SSE *must* have 16-byte aligned data
-		dest[0] = val;
-		++dest; --num;
+		dest[0] = val64;
+		++dest; --num64;
 	}
-#if ASM_X86_64
-	asm volatile (
-		// The 'movd' instruction below actually moves a Quad-word (not a
-		// Double word). Though very old binutils don't support the more
-		// logical 'movq' syntax. See this bug report for more details.
-		//    [2492575] (0.7.0) compilation fails on FreeBSD/amd64
-		"movd     %[VAL], %%xmm0;"
-		"unpcklps %%xmm0, %%xmm0;"
-		: // no output
-		: [VAL] "r" (val)
-		#if defined __SSE__
-		: "xmm0"
-		#endif
-	);
-#else
-	uint32_t _low_  = uint32_t(val >>  0);
-	uint32_t _high_ = uint32_t(val >> 32);
-#if defined _MSC_VER
-	__asm {
-		movss		xmm0,dword ptr [_low_]
-		movss		xmm1,dword ptr [_high_]
-		unpcklps	xmm0,xmm0
-		unpcklps	xmm1,xmm1
-		unpcklps	xmm0,xmm1
-	}
-#else
-	asm volatile (
-		"movss    %[LOW], %%xmm0;"
-		"movss   %[HIGH], %%xmm1;"
-		"unpcklps %%xmm0, %%xmm0;"
-		"unpcklps %%xmm1, %%xmm1;"
-		"unpcklps %%xmm1, %%xmm0;"
-		: // no output
-		: [LOW]  "m" (_low_)
-		, [HIGH] "m" (_high_)
-		#if defined __SSE__
-		: "xmm0", "xmm1"
-		#endif
-	);
-#endif
-#endif
-	memset_128_SSE(dest, num);
-	if (unlikely(num & 1)) {
-		dest[num - 1] = val;
-	}
-}
 
-static inline void memset_64_MMX(
-	uint64_t* dest, size_t num, uint64_t val)
-{
-    assert((size_t(dest) & 7) == 0); // must be 8-byte aligned
-
-#if defined _MSC_VER
-	uint32_t lo = uint32_t(val >> 0);
-	uint32_t hi = uint32_t(val >> 32);
-	uint64_t* e = dest + num - 3;
-
-	__asm {
-		movd		mm0,dword ptr [lo]
-		movd		mm1,dword ptr [hi]
-		punpckldq	mm0,mm1
-
-		mov			eax,e
-		mov			ecx,dest
-		mov			edx,dword ptr [num]
-mainloop:
-		movq		mmword ptr [ecx],mm0
-		movq		mmword ptr [ecx+8],mm0
-		movq		mmword ptr [ecx+10h],mm0
-		movq		mmword ptr [ecx+18h],mm0
-		add			ecx,20h
-		cmp			ecx,eax
-		jb			mainloop
-
-		test		edx,2
-		je			test1
-		movq		mmword ptr [ecx],mm0
-		movq		mmword ptr [ecx+8],mm0
-		add			ecx,10h
-test1:
-		test		edx,1
-		je			end
-		movq		mmword ptr [ecx],mm0
-end:
-		emms
-	}
-#else
-	// note can be better on X86_64, but there we anyway use SSE
-	asm volatile (
-		"movd     %[LOW],%%mm0;"
-		"movd    %[HIGH],%%mm1;"
-		"punpckldq %%mm1,%%mm0;"
-		: // no output
-		: [LOW]  "r" (uint32_t(val >>  0))
-		, [HIGH] "r" (uint32_t(val >> 32))
-		#if defined __MMX__
-		: "mm0", "mm1"
-		#endif
-	);
-	uint64_t* e = dest + num - 3;
+	__m128i val128 = _mm_set1_epi64x(val64);
+	uint64_t* e = dest + num64 - 3;
 	for (/**/; dest < e; dest += 4) {
-		asm volatile (
-			"movq %%mm0,   (%[OUT]);"
-			"movq %%mm0,  8(%[OUT]);"
-			"movq %%mm0, 16(%[OUT]);"
-			"movq %%mm0, 24(%[OUT]);"
-			: // no output
-			: [OUT] "r" (dest)
-			: "memory"
-		);
+		_mm_store_si128(reinterpret_cast<__m128i*>(dest + 0), val128);
+		_mm_store_si128(reinterpret_cast<__m128i*>(dest + 2), val128);
 	}
-	if (unlikely(num & 2)) {
-		asm volatile (
-			"movq %%mm0,  (%[OUT]);"
-			"movq %%mm0, 8(%[OUT]);"
-			: // no output
-			: [OUT] "r" (dest)
-			: "memory"
-		);
+	if (unlikely(num64 & 2)) {
+		_mm_store_si128(reinterpret_cast<__m128i*>(dest), val128);
 		dest += 2;
 	}
-	if (unlikely(num & 1)) {
-		asm volatile (
-			"movq %%mm0, (%[OUT]);"
-			: // no output
-			: [OUT] "r" (dest)
-			: "memory"
-		);
+	if (unlikely(num64 & 1)) {
+		dest[0] = val64;
 	}
-	asm volatile ("emms");
-#endif
 }
 #endif
 
 static inline void memset_64(
-        uint64_t* dest, size_t num, uint64_t val)
+        uint64_t* dest, size_t num64, uint64_t val64)
 {
-	assert((size_t(dest) & 7) == 0); // must be 8-byte aligned
+	assert((size_t(dest) % 8) == 0); // must be 8-byte aligned
 
-#if ASM_X86 && !defined _WIN64
-	if (HostCPU::hasSSE()) {
-		memset_64_SSE(dest, num, val);
-		return;
-	}
-	if (HostCPU::hasMMX()) {
-		memset_64_MMX(dest, num, val);
-		return;
-	}
+#ifdef __SSE2__
+	memset_64_SSE(dest, num64, val64);
+	return;
 #endif
-	uint64_t* e = dest + num - 3;
+	uint64_t* e = dest + num64 - 3;
 	for (/**/; dest < e; dest += 4) {
-		dest[0] = val;
-		dest[1] = val;
-		dest[2] = val;
-		dest[3] = val;
+		dest[0] = val64;
+		dest[1] = val64;
+		dest[2] = val64;
+		dest[3] = val64;
 	}
-	if (unlikely(num & 2)) {
-		dest[0] = val;
-		dest[1] = val;
+	if (unlikely(num64 & 2)) {
+		dest[0] = val64;
+		dest[1] = val64;
 		dest += 2;
 	}
-	if (unlikely(num & 1)) {
-		dest[0] = val;
+	if (unlikely(num64 & 1)) {
+		dest[0] = val64;
 	}
 }
 
 static inline void memset_32_2(
-	uint32_t* dest, size_t num, uint32_t val0, uint32_t val1)
+	uint32_t* dest, size_t num32, uint32_t val0, uint32_t val1)
 {
-	assert((size_t(dest) & 3) == 0); // must be 4-byte aligned
+	assert((size_t(dest) % 4) == 0); // must be 4-byte aligned
+	if (unlikely(num32 == 0)) return;
 
-	if (unlikely(num == 0)) return;
+	// Align at 8-byte boundary.
 	if (unlikely(size_t(dest) & 4)) {
 		dest[0] = val1; // start at odd pixel
-		++dest; --num;
+		++dest; --num32;
 	}
 
-	uint64_t val = OPENMSX_BIGENDIAN
-		? (static_cast<uint64_t>(val0) << 32) | val1
-		: val0 | (static_cast<uint64_t>(val1) << 32);
-	memset_64(reinterpret_cast<uint64_t*>(dest), num / 2, val);
+	uint64_t val64 = OPENMSX_BIGENDIAN ? (uint64_t(val0) << 32) | val1
+	                                   : val0 | (uint64_t(val1) << 32);
+	memset_64(reinterpret_cast<uint64_t*>(dest), num32 / 2, val64);
 
-	if (unlikely(num & 1)) {
-		dest[num - 1] = val0;
+	if (unlikely(num32 & 1)) {
+		dest[num32 - 1] = val0;
 	}
 }
 
-static inline void memset_32(uint32_t* dest, size_t num, uint32_t val)
+static inline void memset_32(uint32_t* dest, size_t num32, uint32_t val32)
 {
-	assert((size_t(dest) & 3) == 0); // must be 4-byte aligned
+	assert((size_t(dest) % 4) == 0); // must be 4-byte aligned
 
 #if ASM_X86
 #if defined _MSC_VER
 	// VC++'s __stosd intrinsic results in emulator benchmarks
 	// running about 7% faster than with memset_32_2, streaming or not,
 	// and about 3% faster than the C code below.
-	__stosd(reinterpret_cast<unsigned long*>(dest), val, num);
+	__stosd(reinterpret_cast<unsigned long*>(dest), val32, num32);
 #else
-	memset_32_2(dest, num, val, val);
+	memset_32_2(dest, num32, val32, val32);
 #endif
 #elif defined __arm__
 	// Ideally the first mov(*) instruction could be omitted (and then
@@ -308,68 +139,67 @@ static inline void memset_32(uint32_t* dest, size_t num, uint32_t val)
 		"strne   r3,[%[dest]]\n\t"
 
 		: [dest] "=r"    (dest)
-		, [num] "=r"     (num)
+		, [num] "=r"     (num32)
 		:       "[dest]" (dest)
-		,       "[num]"  (num)
-		, [val] "r"      (val)
+		,       "[num]"  (num32)
+		, [val] "r"      (val32)
 		: "memory"
 		, "r3","r4","r5","r6","r8","r9","r10","r12"
 	);
 	return;
 #else
-	uint32_t* e = dest + num - 7;
+	uint32_t* e = dest + num32 - 7;
 	for (/**/; dest < e; dest += 8) {
-		dest[0] = val;
-		dest[1] = val;
-		dest[2] = val;
-		dest[3] = val;
-		dest[4] = val;
-		dest[5] = val;
-		dest[6] = val;
-		dest[7] = val;
+		dest[0] = val32;
+		dest[1] = val32;
+		dest[2] = val32;
+		dest[3] = val32;
+		dest[4] = val32;
+		dest[5] = val32;
+		dest[6] = val32;
+		dest[7] = val32;
 	}
-	if (unlikely(num & 4)) {
-		dest[0] = val;
-		dest[1] = val;
-		dest[2] = val;
-		dest[3] = val;
+	if (unlikely(num32 & 4)) {
+		dest[0] = val32;
+		dest[1] = val32;
+		dest[2] = val32;
+		dest[3] = val32;
 		dest += 4;
 	}
-	if (unlikely(num & 2)) {
-		dest[0] = val;
-		dest[1] = val;
+	if (unlikely(num32 & 2)) {
+		dest[0] = val32;
+		dest[1] = val32;
 		dest += 2;
 	}
-	if (unlikely(num & 1)) {
-		dest[0] = val;
+	if (unlikely(num32 & 1)) {
+		dest[0] = val32;
 	}
 #endif
 }
 
 static inline void memset_16_2(
-	uint16_t* dest, size_t num, uint16_t val0, uint16_t val1)
+	uint16_t* dest, size_t num16, uint16_t val0, uint16_t val1)
 {
-	assert((size_t(dest) & 1) == 0); // must be 2-byte aligned
+	if (unlikely(num16 == 0)) return;
 
-	if (unlikely(num == 0)) return;
+	// Align at 4-byte boundary.
 	if (unlikely(size_t(dest) & 2)) {
 		dest[0] = val1; // start at odd pixel
-		++dest; --num;
+		++dest; --num16;
 	}
 
-	uint32_t val = OPENMSX_BIGENDIAN
-	             ? (val0 << 16) | val1
-	             : val0 | (val1 << 16);
-	memset_32(reinterpret_cast<uint32_t*>(dest), num / 2, val);
+	uint32_t val32 = OPENMSX_BIGENDIAN ? (uint32_t(val0) << 16) | val1
+	                                   : val0 | (uint32_t(val1) << 16);
+	memset_32(reinterpret_cast<uint32_t*>(dest), num16 / 2, val32);
 
-	if (unlikely(num & 1)) {
-		dest[num - 1] = val0;
+	if (unlikely(num16 & 1)) {
+		dest[num16 - 1] = val0;
 	}
 }
 
-static inline void memset_16(uint16_t* dest, size_t num, uint16_t val)
+static inline void memset_16(uint16_t* dest, size_t num16, uint16_t val16)
 {
-	memset_16_2(dest, num, val, val);
+	memset_16_2(dest, num16, val16, val16);
 }
 
 template<typename Pixel> void MemSet<Pixel>::operator()(
