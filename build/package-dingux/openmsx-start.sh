63389eacb809f4265992eb5f449b66ec69b6a15e
Maarten ter Huurne <maarten@treewalker.org>
2013-07-10 22:04:46 +0200

    Build package for OpenDingux in OPK file instead of ZIP file

--- build/package-dingux/openmsx-start.sh
+++ build/package-dingux/openmsx-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Startup script for openMSX.
 # Run this to start openMSX; running the executable directly does not work.
-export OPENMSX_SYSTEM_DATA=/usr/local/share/openmsx
-exec /usr/local/bin/openmsx.dge
+export OPENMSX_SYSTEM_DATA=$PWD/share
+exec bin/openmsx
 #exec /usr/local/bin/openmsx.dge &> /tmp/openmsx-log.txt
