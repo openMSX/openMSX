openMSX/openMSX

commit 4dca3ffcb9c385e5078ac74542421831e295888a

Implement UNREACHABLE for clang
This also fixes some compiler warnings in a non-assert clang build.

src/utils/unreachable.hh
@@ -30,6 +30,10 @@
     // gcc-4.5 or gcc-4.6 on x86, x86_64 or arm (all without delay slots)
     #define UNREACHABLE __builtin_unreachable()
 
+  #elif defined(__clang__) && __has_builtin(__builtin_unreachable)
+    // clang
+    #define UNREACHABLE __builtin_unreachable()
+
   #elif defined(_MSC_VER)
     // visual studio
     #define UNREACHABLE __assume(0)

commit 7347fda121b2e0f84f48ab0d4351d26d3b650aa8

Follow-up on revision 4dca3ffc
__has_feature() is a clang-specific macro. Add a workaround for non-clang
compilers.

This patch fixes a ton of warnings in vc++. And errors in (older?) gcc versions
(e.g. our dingoo and android build broke). All these problem were introduced in
rev 4dca3ffc.

src/utils/unreachable.hh
@@ -20,18 +20,24 @@
 // to only enable this optimization starting from gcc-4.7 or from gcc-4.5 for a
 // few white-listed architectures that we know don't use delay slots.
 
+// Clang has a very convenient way of testing features, unfortunately (for now)
+// it's clang-only so add a fallback for non-clang compilers.
+#ifndef __has_builtin
+  #define __has_builtin(x) 0
+#endif
+
 #if defined(NDEBUG)
+  // clang
+  #if __has_builtin(__builtin_unreachable)
+    #define UNREACHABLE __builtin_unreachable()
+
   // __builtin_unreachable() was introduced in gcc-4.5
-  #if ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7)))
+  #elif ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7)))
     // gcc-4.7 or above
     #define UNREACHABLE __builtin_unreachable()
 
   #elif ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5) && (defined(__i386__) || defined(__x86_64__) || defined(__arm__)))
     // gcc-4.5 or gcc-4.6 on x86, x86_64 or arm (all without delay slots)
-    #define UNREACHABLE __builtin_unreachable()
-
-  #elif defined(__clang__) && __has_builtin(__builtin_unreachable)
-    // clang
     #define UNREACHABLE __builtin_unreachable()
 
   #elif defined(_MSC_VER)