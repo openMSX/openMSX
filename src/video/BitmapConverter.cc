openMSX/openMSX

commit 6b193d216844d1f2cd208836653d0e12af14e052

Make __restrict declarations match implementations
The GCC documentation states:

"As with all outermost parameter qualifiers, __restrict__ is ignored
in function definition matching. This means you only need to specify
__restrict__ in a function definition, rather than in a function
prototype as well."
  http://gcc.gnu.org/onlinedocs/gcc/Restricted-Pointers.html

However, it seems Clang (tested with version 3.3 on Linux) does require
the __restrict qualifier to be present on both: it issues "no matching
member function" errors if the prototype and the definition are out of
sync.

Also, Clang refuses to call non-restricted methods from within
restricted methods.

src/video/BitmapConverter.cc
@@ -21,7 +21,7 @@ BitmapConverter<Pixel>::BitmapConverter(
 }
 
 template <class Pixel>
-void BitmapConverter<Pixel>::calcDPalette()
+void BitmapConverter<Pixel>::calcDPalette() __restrict
 {
 	dPaletteValid = true;
 	unsigned bits = sizeof(Pixel) * 8;
@@ -368,7 +368,7 @@ void BitmapConverter<Pixel>::renderYAE(
 
 // TODO: Check what happens on real V9938.
 template <class Pixel>
-void BitmapConverter<Pixel>::renderBogus(Pixel* pixelPtr)
+void BitmapConverter<Pixel>::renderBogus(Pixel* pixelPtr) __restrict
 {
 	Pixel color = palette16[0];
 	for (unsigned i = 0; i < 256; ++i) {

src/video/BitmapConverter.hh
@@ -74,19 +74,19 @@ public:
 	}
 
 private:
-	void calcDPalette();
+	void calcDPalette() __restrict;
 
-	inline void renderGraphic4(Pixel* pixelPtr, const byte* vramPtr0);
-	inline void renderGraphic5(Pixel* pixelPtr, const byte* vramPtr0);
+	inline void renderGraphic4(Pixel* pixelPtr, const byte* vramPtr0) __restrict;
+	inline void renderGraphic5(Pixel* pixelPtr, const byte* vramPtr0) __restrict;
 	inline void renderGraphic6(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
 	inline void renderGraphic7(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
 	inline void renderYJK(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
 	inline void renderYAE(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
-	inline void renderBogus(Pixel* pixelPtr);
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
+	inline void renderBogus(Pixel* pixelPtr) __restrict;
 
 	const Pixel* const __restrict palette16;
 	const Pixel* const __restrict palette256;

src/video/CharacterConverter.cc
@@ -227,7 +227,7 @@ void CharacterConverter<Pixel>::renderText2(
 }
 
 template <class Pixel>
-const byte* CharacterConverter<Pixel>::getNamePtr(int line, int scroll)
+const byte* CharacterConverter<Pixel>::getNamePtr(int line, int scroll) __restrict
 {
 	// no need to test whether multi-page scrolling is enabled,
 	// indexMask in the nameTable already takes care of it

src/video/CharacterConverter.hh
@@ -42,18 +42,18 @@ public:
 	void setDisplayMode(DisplayMode mode);
 
 private:
-	inline void renderText1   (Pixel* pixelPtr, int line);
-	inline void renderText1Q  (Pixel* pixelPtr, int line);
-	inline void renderText2   (Pixel* pixelPtr, int line);
-	inline void renderGraphic1(Pixel* pixelPtr, int line);
-	inline void renderGraphic2(Pixel* pixelPtr, int line);
-	inline void renderMulti   (Pixel* pixelPtr, int line);
-	inline void renderMultiQ  (Pixel* pixelPtr, int line);
-	inline void renderBogus   (Pixel* pixelPtr);
+	inline void renderText1   (Pixel* pixelPtr, int line) __restrict;
+	inline void renderText1Q  (Pixel* pixelPtr, int line) __restrict;
+	inline void renderText2   (Pixel* pixelPtr, int line) __restrict;
+	inline void renderGraphic1(Pixel* pixelPtr, int line) __restrict;
+	inline void renderGraphic2(Pixel* pixelPtr, int line) __restrict;
+	inline void renderMulti   (Pixel* pixelPtr, int line) __restrict;
+	inline void renderMultiQ  (Pixel* pixelPtr, int line) __restrict;
+	inline void renderBogus   (Pixel* pixelPtr) __restrict;
 	inline void renderMultiHelper(Pixel* pixelPtr, int line,
-	                       int mask, int patternQuarter);
+	                       int mask, int patternQuarter) __restrict;
 
-	const byte* getNamePtr(int line, int scroll);
+	const byte* getNamePtr(int line, int scroll) __restrict;
 
 	VDP& vdp;
 	VDPVRAM& vram;

src/video/v9990/V9990BitmapConverter.cc
@@ -271,6 +271,7 @@ void V9990BitmapConverter<Pixel>::rasterBP2HiRes(
 template <class Pixel>
 void V9990BitmapConverter<Pixel>::rasterP(
 	Pixel* /*pixelPtr*/, unsigned /*x*/, unsigned /*y*/, int /*nrPixels*/)
+	__restrict
 {
 	UNREACHABLE;
 }

src/video/v9990/V9990BitmapConverter.hh
@@ -39,7 +39,8 @@ private:
 	/** Rastering method for the current color mode
 	  */
 	typedef void (V9990BitmapConverter<Pixel>::*RasterMethod)
-	             (Pixel* pixelPtr, unsigned x, unsigned y, int nrPixels);
+	             (Pixel* pixelPtr, unsigned x, unsigned y, int nrPixels)
+	             __restrict;
 	RasterMethod rasterMethod;
 
 	/** The 64 color palette for P1, P2 and BP* modes
@@ -58,22 +59,22 @@ private:
 	const Pixel* const palette32768;
 
 	/* private Raster methods */
-	void rasterP       (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBYUV    (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBYUVP   (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBYJK    (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBYJKP   (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBD16    (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBD8     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBP6     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBP4     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBP2     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBP4HiRes(Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
-	void rasterBP2HiRes(Pixel* outPixels, unsigned x, unsigned y, int nrPixels);
+	void rasterP       (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBYUV    (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBYUVP   (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBYJK    (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBYJKP   (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBD16    (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBD8     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBP6     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBP4     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBP2     (Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBP4HiRes(Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
+	void rasterBP2HiRes(Pixel* outPixels, unsigned x, unsigned y, int nrPixels) __restrict;
 
 	/* Cursor drawing methods */
 	void drawCursor(Pixel* buffer, int displayY,
-	                unsigned attrAddr, unsigned patAddr);
+	                unsigned attrAddr, unsigned patAddr) __restrict;
 	void drawCursors(Pixel* buffer, int displayY);
 };
 

src/video/v9990/V9990P1Converter.cc
@@ -82,7 +82,7 @@ void V9990P1Converter<Pixel>::renderPattern(
 	unsigned displayAX, unsigned displayAY, unsigned nameA,
 	unsigned patternA, byte palA,
 	unsigned displayBX, unsigned displayBY, unsigned nameB,
-	unsigned patternB, byte palB)
+	unsigned patternB, byte palB) __restrict
 {
 	renderPattern2(&buffer[0],      width1, displayAX, displayAY,
 	               nameA, patternA, palA);

src/video/v9990/V9990P1Converter.hh
@@ -28,13 +28,13 @@ private:
 	                   unsigned displayAX, unsigned displayAY,
 	                   unsigned nameA, unsigned patternA, byte palA,
 	                   unsigned displayBX, unsigned displayBY,
-	                   unsigned nameB, unsigned patternB, byte palB);
+	                   unsigned nameB, unsigned patternB, byte palB) __restrict;
 	void renderPattern2(Pixel* buffer, unsigned width,
 	                    unsigned AX, unsigned AY, unsigned name,
-	                    unsigned pattern, byte pal);
-	void determineVisibleSprites(int* visibleSprites, unsigned displayY);
+	                    unsigned pattern, byte pal) __restrict;
+	void determineVisibleSprites(int* visibleSprites, unsigned displayY) __restrict;
 	void renderSprites(Pixel* buffer, int displayX, int displayEnd,
-	                   unsigned displayY, int* visibleSprites, bool front);
+	                   unsigned displayY, int* visibleSprites, bool front) __restrict;
 };
 
 } // namespace openmsx

commit 6f61dd3aed69ba63801be7145b2be9a0039a509e

Remove __restrict qualifiers on methods in templated classes
Clang versions before 3.3 cannot handle them. This includes the Clang
build currently included with Xcode. When Clang 3.3+ is widespread,
this commit could be reverted.

These problems started in commit 6b193d21.

src/video/BitmapConverter.cc
@@ -21,7 +21,7 @@ BitmapConverter<Pixel>::BitmapConverter(
 }
 
 template <class Pixel>
-void BitmapConverter<Pixel>::calcDPalette() __restrict
+void BitmapConverter<Pixel>::calcDPalette()
 {
 	dPaletteValid = true;
 	unsigned bits = sizeof(Pixel) * 8;
@@ -101,7 +101,7 @@ void BitmapConverter<Pixel>::convertLinePlanar(
 template <class Pixel>
 void BitmapConverter<Pixel>::renderGraphic4(
 	Pixel*      __restrict pixelPtr,
-	const byte* __restrict vramPtr0) __restrict
+	const byte* __restrict vramPtr0)
 {
 	/*for (unsigned i = 0; i < 128; i += 2) {
 		unsigned data0 = vramPtr0[i + 0];
@@ -236,7 +236,7 @@ void BitmapConverter<Pixel>::renderGraphic4(
 template <class Pixel>
 void BitmapConverter<Pixel>::renderGraphic5(
 	Pixel*      __restrict pixelPtr,
-	const byte* __restrict vramPtr0) __restrict
+	const byte* __restrict vramPtr0)
 {
 	for (unsigned i = 0; i < 128; ++i) {
 		unsigned data = vramPtr0[i];
@@ -251,7 +251,7 @@ template <class Pixel>
 void BitmapConverter<Pixel>::renderGraphic6(
 	Pixel*      __restrict pixelPtr,
 	const byte* __restrict vramPtr0,
-	const byte* __restrict vramPtr1) __restrict
+	const byte* __restrict vramPtr1)
 {
 	/*for (unsigned i = 0; i < 128; ++i) {
 		unsigned data0 = vramPtr0[i];
@@ -297,7 +297,7 @@ template <class Pixel>
 void BitmapConverter<Pixel>::renderGraphic7(
 	Pixel*      __restrict pixelPtr,
 	const byte* __restrict vramPtr0,
-	const byte* __restrict vramPtr1) __restrict
+	const byte* __restrict vramPtr1)
 {
 	for (unsigned i = 0; i < 128; ++i) {
 		pixelPtr[2 * i + 0] = palette256[vramPtr0[i]];
@@ -309,7 +309,7 @@ template <class Pixel>
 void BitmapConverter<Pixel>::renderYJK(
 	Pixel*      __restrict pixelPtr,
 	const byte* __restrict vramPtr0,
-	const byte* __restrict vramPtr1) __restrict
+	const byte* __restrict vramPtr1)
 {
 	for (unsigned i = 0; i < 64; ++i) {
 		unsigned p[4];
@@ -336,7 +336,7 @@ template <class Pixel>
 void BitmapConverter<Pixel>::renderYAE(
 	Pixel*      __restrict pixelPtr,
 	const byte* __restrict vramPtr0,
-	const byte* __restrict vramPtr1) __restrict
+	const byte* __restrict vramPtr1)
 {
 	for (unsigned i = 0; i < 64; ++i) {
 		unsigned p[4];
@@ -368,7 +368,7 @@ void BitmapConverter<Pixel>::renderYAE(
 
 // TODO: Check what happens on real V9938.
 template <class Pixel>
-void BitmapConverter<Pixel>::renderBogus(Pixel* pixelPtr) __restrict
+void BitmapConverter<Pixel>::renderBogus(Pixel* pixelPtr)
 {
 	Pixel color = palette16[0];
 	for (unsigned i = 0; i < 256; ++i) {

src/video/BitmapConverter.hh
@@ -74,19 +74,19 @@ public:
 	}
 
 private:
-	void calcDPalette() __restrict;
+	void calcDPalette();
 
-	inline void renderGraphic4(Pixel* pixelPtr, const byte* vramPtr0) __restrict;
-	inline void renderGraphic5(Pixel* pixelPtr, const byte* vramPtr0) __restrict;
+	inline void renderGraphic4(Pixel* pixelPtr, const byte* vramPtr0);
+	inline void renderGraphic5(Pixel* pixelPtr, const byte* vramPtr0);
 	inline void renderGraphic6(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
 	inline void renderGraphic7(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
 	inline void renderYJK(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
 	inline void renderYAE(
-		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1) __restrict;
-	inline void renderBogus(Pixel* pixelPtr) __restrict;
+		Pixel* pixelPtr, const byte* vramPtr0, const byte* vramPtr1);
+	inline void renderBogus(Pixel* pixelPtr);
 
 	const Pixel* const __restrict palette16;
 	const Pixel* const __restrict palette256;

src/video/CharacterConverter.cc
@@ -57,7 +57,7 @@ void CharacterConverter<Pixel>::convertLine(Pixel* linePtr, int line)
 
 template <class Pixel>
 void CharacterConverter<Pixel>::renderText1(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 	Pixel fg = palFg[vdp.getForegroundColor()];
 	Pixel bg = palFg[vdp.getBackgroundColor()];
@@ -86,7 +86,7 @@ void CharacterConverter<Pixel>::renderText1(
 
 template <class Pixel>
 void CharacterConverter<Pixel>::renderText1Q(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 	Pixel fg = palFg[vdp.getForegroundColor()];
 	Pixel bg = palFg[vdp.getBackgroundColor()];
@@ -116,7 +116,7 @@ void CharacterConverter<Pixel>::renderText1Q(
 
 template <class Pixel>
 void CharacterConverter<Pixel>::renderText2(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 	Pixel plainFg = palFg[vdp.getForegroundColor()];
 	Pixel plainBg = palFg[vdp.getBackgroundColor()];
@@ -227,7 +227,7 @@ void CharacterConverter<Pixel>::renderText2(
 }
 
 template <class Pixel>
-const byte* CharacterConverter<Pixel>::getNamePtr(int line, int scroll) __restrict
+const byte* CharacterConverter<Pixel>::getNamePtr(int line, int scroll)
 {
 	// no need to test whether multi-page scrolling is enabled,
 	// indexMask in the nameTable already takes care of it
@@ -236,7 +236,7 @@ const byte* CharacterConverter<Pixel>::getNamePtr(int line, int scroll) __restri
 }
 template <class Pixel>
 void CharacterConverter<Pixel>::renderGraphic1(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 	const byte* patternArea = vram.patternTable.getReadArea(0, 256 * 8);
 	patternArea += line & 7;
@@ -266,7 +266,7 @@ void CharacterConverter<Pixel>::renderGraphic1(
 
 template <class Pixel>
 void CharacterConverter<Pixel>::renderGraphic2(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 #ifdef __arm__
 	bool misAligned =
@@ -408,7 +408,7 @@ void CharacterConverter<Pixel>::renderGraphic2(
 template <class Pixel>
 void CharacterConverter<Pixel>::renderMultiHelper(
 	Pixel* __restrict pixelPtr, int line,
-	int mask, int patternQuarter) __restrict
+	int mask, int patternQuarter)
 {
 	unsigned baseLine = mask | ((line / 4) & 7);
 	unsigned scroll = vdp.getHorizontalScrollHigh();
@@ -428,7 +428,7 @@ void CharacterConverter<Pixel>::renderMultiHelper(
 }
 template <class Pixel>
 void CharacterConverter<Pixel>::renderMulti(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 	int mask = (-1 << 11);
 	renderMultiHelper(pixelPtr, line, mask, 0);
@@ -436,7 +436,7 @@ void CharacterConverter<Pixel>::renderMulti(
 
 template <class Pixel>
 void CharacterConverter<Pixel>::renderMultiQ(
-	Pixel* __restrict pixelPtr, int line) __restrict
+	Pixel* __restrict pixelPtr, int line)
 {
 	int mask = (-1 << 13);
 	int patternQuarter = (line * 4) & ~0xFF;  // (line / 8) * 32
@@ -445,7 +445,7 @@ void CharacterConverter<Pixel>::renderMultiQ(
 
 template <class Pixel>
 void CharacterConverter<Pixel>::renderBogus(
-	Pixel* __restrict pixelPtr) __restrict
+	Pixel* __restrict pixelPtr)
 {
 	Pixel fg = palFg[vdp.getForegroundColor()];
 	Pixel bg = palFg[vdp.getBackgroundColor()];

src/video/CharacterConverter.hh
@@ -42,18 +42,18 @@ public:
 	void setDisplayMode(DisplayMode mode);
 
 private:
-	inline void renderText1   (Pixel* pixelPtr, int line) __restrict;
-	inline void renderText1Q  (Pixel* pixelPtr, int line) __restrict;
-	inline void renderText2   (Pixel* pixelPtr, int line) __restrict;
-	inline void renderGraphic1(Pixel* pixelPtr, int line) __restrict;
-	inline void renderGraphic2(Pixel* pixelPtr, int line) __restrict;
-	inline void renderMulti   (Pixel* pixelPtr, int line) __restrict;
-	inline void renderMultiQ  (Pixel* pixelPtr, int line) __restrict;
-	inline void renderBogus   (Pixel* pixelPtr) __restrict;
+	inline void renderText1   (Pixel* pixelPtr, int line);
+	inline void renderText1Q  (Pixel* pixelPtr, int line);
+	inline void renderText2   (Pixel* pixelPtr, int line);
+	inline void renderGraphic1(Pixel* pixelPtr, int line);
+	inline void renderGraphic2(Pixel* pixelPtr, int line);
+	inline void renderMulti   (Pixel* pixelPtr, int line);
+	inline void renderMultiQ  (Pixel* pixelPtr, int line);
+	inline void renderBogus   (Pixel* pixelPtr);
 	inline void renderMultiHelper(Pixel* pixelPtr, int line,
-	                       int mask, int patternQuarter) __restrict;
+	                       int mask, int patternQuarter);
 
-	const byte* getNamePtr(int line, int scroll) __restrict;
+	const byte* getNamePtr(int line, int scroll);
 
 	VDP& vdp;
 	VDPVRAM& vram;

src/video/v9990/V9990BitmapConverter.cc
@@ -306,7 +306,7 @@ void V9990BitmapConverter<Pixel>::setColorMode(V9990ColorMode color,
 
 template <class Pixel>
 void V9990BitmapConverter<Pixel>::drawCursor(
-	Pixel* __restrict buffer, int displayY, unsigned attrAddr, unsigned patAddr) __restrict
+	Pixel* __restrict buffer, int displayY, unsigned attrAddr, unsigned patAddr)
 {
 	int cursorY = vram.readVRAMBx(attrAddr + 0) +
 	             (vram.readVRAMBx(attrAddr + 2) & 1) * 256;

src/video/v9990/V9990BitmapConverter.hh
@@ -73,7 +73,7 @@ private:
 
 	/* Cursor drawing methods */
 	void drawCursor(Pixel* buffer, int displayY,
-	                unsigned attrAddr, unsigned patAddr) __restrict;
+	                unsigned attrAddr, unsigned patAddr);
 	void drawCursors(Pixel* buffer, int displayY);
 };
 

src/video/v9990/V9990P1Converter.cc
@@ -82,7 +82,7 @@ void V9990P1Converter<Pixel>::renderPattern(
 	unsigned displayAX, unsigned displayAY, unsigned nameA,
 	unsigned patternA, byte palA,
 	unsigned displayBX, unsigned displayBY, unsigned nameB,
-	unsigned patternB, byte palB) __restrict
+	unsigned patternB, byte palB)
 {
 	renderPattern2(&buffer[0],      width1, displayAX, displayAY,
 	               nameA, patternA, palA);
@@ -93,7 +93,7 @@ void V9990P1Converter<Pixel>::renderPattern(
 template <class Pixel>
 void V9990P1Converter<Pixel>::renderPattern2(
 	Pixel* __restrict buffer, unsigned width, unsigned x, unsigned y,
-	unsigned nameTable, unsigned patternTable, byte pal) __restrict
+	unsigned nameTable, unsigned patternTable, byte pal)
 {
 	x &= 511;
 	const Pixel* palette = palette64 + pal;
@@ -184,7 +184,7 @@ void V9990P1Converter<Pixel>::renderPattern2(
 
 template <class Pixel>
 void V9990P1Converter<Pixel>::determineVisibleSprites(
-	int* __restrict visibleSprites, unsigned displayY) __restrict
+	int* __restrict visibleSprites, unsigned displayY)
 {
 	static const unsigned spriteTable = 0x3FE00;
 
@@ -214,7 +214,7 @@ void V9990P1Converter<Pixel>::determineVisibleSprites(
 template <class Pixel>
 void V9990P1Converter<Pixel>::renderSprites(
 	Pixel* __restrict buffer, int displayX, int displayEnd, unsigned displayY,
-	int* __restrict visibleSprites, bool front) __restrict
+	int* __restrict visibleSprites, bool front)
 {
 	static const unsigned spriteTable = 0x3FE00;
 	unsigned spritePatternTable = vdp.getSpritePatternAddress(P1);

src/video/v9990/V9990P1Converter.hh
@@ -28,13 +28,13 @@ private:
 	                   unsigned displayAX, unsigned displayAY,
 	                   unsigned nameA, unsigned patternA, byte palA,
 	                   unsigned displayBX, unsigned displayBY,
-	                   unsigned nameB, unsigned patternB, byte palB) __restrict;
+	                   unsigned nameB, unsigned patternB, byte palB);
 	void renderPattern2(Pixel* buffer, unsigned width,
 	                    unsigned AX, unsigned AY, unsigned name,
-	                    unsigned pattern, byte pal) __restrict;
-	void determineVisibleSprites(int* visibleSprites, unsigned displayY) __restrict;
+	                    unsigned pattern, byte pal);
+	void determineVisibleSprites(int* visibleSprites, unsigned displayY);
 	void renderSprites(Pixel* buffer, int displayX, int displayEnd,
-	                   unsigned displayY, int* visibleSprites, bool front) __restrict;
+	                   unsigned displayY, int* visibleSprites, bool front);
 };
 
 } // namespace openmsx

src/video/v9990/V9990P2Converter.cc
@@ -57,7 +57,7 @@ void V9990P2Converter<Pixel>::convertLine(
 template <class Pixel>
 void V9990P2Converter<Pixel>::renderPattern(
 	Pixel* __restrict buffer, unsigned width, unsigned x, unsigned y,
-	byte pal) __restrict
+	byte pal)
 {
 	static const unsigned patternTable = 0x00000;
 	static const unsigned nameTable    = 0x7C000;
@@ -151,7 +151,7 @@ void V9990P2Converter<Pixel>::renderPattern(
 
 template <class Pixel>
 void V9990P2Converter<Pixel>::determineVisibleSprites(
-	int* __restrict visibleSprites, int displayY) __restrict
+	int* __restrict visibleSprites, int displayY)
 {
 	static const unsigned spriteTable = 0x3FE00;
 
@@ -182,7 +182,7 @@ void V9990P2Converter<Pixel>::determineVisibleSprites(
 template <class Pixel>
 void V9990P2Converter<Pixel>::renderSprites(
 	Pixel* __restrict buffer, int displayX, int displayEnd, unsigned displayY,
-	int* __restrict visibleSprites, bool front) __restrict
+	int* __restrict visibleSprites, bool front)
 {
 	static const unsigned spriteTable = 0x3FE00;
 	unsigned spritePatternTable = vdp.getSpritePatternAddress(P1);

src/video/v9990/V9990P2Converter.hh
@@ -20,10 +20,10 @@ public:
 
 private:
 	void renderPattern(Pixel* buffer, unsigned width,
-	                   unsigned x, unsigned y, byte pal) __restrict;
-	void determineVisibleSprites(int* visibleSprites, int displayY) __restrict;
+	                   unsigned x, unsigned y, byte pal);
+	void determineVisibleSprites(int* visibleSprites, int displayY);
 	void renderSprites(Pixel* buffer, int displayX, int displayEnd,
-	                   unsigned displayY, int* visibleSprites, bool front) __restrict;
+	                   unsigned displayY, int* visibleSprites, bool front);
 
 	V9990& vdp;
 	V9990VRAM& vram;