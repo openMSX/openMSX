63fb16423d2544808f50332a804dae02e6b24d51
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2013-12-29 22:06:29 +0100

    Oops, forgot to commit this change... needed to get Android working in make dist created tarballs.

--- build/node.mk
+++ build/node.mk
@@ -2,7 +2,7 @@ include build/node-start.mk
 
 SUBDIRS:= \
 	3rdparty package-darwin package-slackware msvc package-arch \
-	package-dingux package-windows
+	package-dingux package-windows android
 
 DIST:= \
 	main.mk node-end.mk node-start.mk entry.mk entry-seq.mk \
