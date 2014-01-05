7584a743343b4e4b5f32f6d6377cdedaa438b2b0
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2014-01-05 21:59:40 +0100

    Last updates for release 0.10.0.

--- build/version.py
+++ build/version.py
@@ -14,7 +14,7 @@ packageName = 'openmsx'
 # Version number.
 packageVersionNumber = '0.10.0'
 # Note: suffix should be empty or with dash, like "-rc1" or "-test1"
-packageVersionSuffix = '-rc1'
+packageVersionSuffix = ''
 packageVersion = packageVersionNumber + packageVersionSuffix
 
 # Is this a release version ("True") or development version ("False").
