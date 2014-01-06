openMSX/openMSX

commit dffe5f98a787cb750d786f9662f877bcf37b6f3c

Big setting class refactoring
First an (over-)simplified historic overview of the openMSX settings:
- Initially we had simple setting classes that stored their specific value type
  (so int, bool, ... in the subclass instead of string in the base class). We
  choose for a policy based design with static polymorphism instead of dynamic
  polymorphism with virtual functions.
- Later we added Tcl scripting to openMSX, and the settings had to be kept
  in sync with Tcl variables. This requires to/from string conversions.
- Next we added per-machine and global settings. For convenience and backwards
  compatibility, the per-machine settings still have a representative in the
  global namespace. This is implemented via the concept of a proxy-setting.
- Over time we also introduced special cases like read-only settings, settings
  with a dynamically changing list of allowed values, settings with special
  default values, ...
Anyway, just to say that the setting classes have changed a lot over time and
that the original design, which is currently still present in some way, may not
be the best for the current needs.

This patch simplifies the internal implementation of the core setting
classes. The integration of the settings with Tcl and the rest of openMSX is
mostly left unchanged.

New design: instead of storing the setting value in the specific setting
classes (IntegerSetting stores an 'int', etc) the value is stored in the base
'Setting' class. The value is stored as a TclObject, so this still allows
efficient querying of the native type and avoids needless from/to string
conversions.

The 'check for allowed values' stuff has changed: before we did the basic
checks in the policy-classes (e.g. check that an IntegerSetting only receives
strings that can be converted to int). Extra checks were done via an optional
SettingChecker object (e.g. check that the string passed to the joystick config
setting has the required format). Now both checks are unified in a single

src/settings/BooleanSetting.cc
@@ -1,42 +1,25 @@
 #include "BooleanSetting.hh"
-#include "CommandException.hh"
 #include "Completer.hh"
-#include "StringOp.hh"
-
-using std::string;
-using std::vector;
 
 namespace openmsx {
 
-// class BooleanSettingPolicy
-
-BooleanSettingPolicy::BooleanSettingPolicy()
+BooleanSetting::BooleanSetting(
+		CommandController& commandController, string_ref name,
+		string_ref description, bool initialValue, SaveSetting save)
+	: Setting(commandController, name, description,
+	          (initialValue ? "true" : "false"), save)
 {
+	setChecker([this](TclObject& newValue) {
+		newValue.getBoolean(); // may throw
+	});
 }
 
-string BooleanSettingPolicy::toString(bool value) const
-{
-	return value ? "true" : "false";
-}
-
-bool BooleanSettingPolicy::fromString(const string& str) const
-{
-	string s = StringOp::toLower(str);
-	if        ((s == "true")  || (s == "on") || (s == "yes")) {
-		return true;
-	} else if ((s == "false") || (s == "off") || (s == "no")) {
-		return false;
-	} else {
-		throw CommandException("not a valid boolean: " + str);
-	}
-}
-
-string_ref BooleanSettingPolicy::getTypeString() const
+string_ref BooleanSetting::getTypeString() const
 {
 	return "boolean";
 }
 
-void BooleanSettingPolicy::tabCompletion(vector<string>& tokens) const
+void BooleanSetting::tabCompletion(std::vector<std::string>& tokens) const
 {
 	static const char* const values[] = {
 		"true",  "on",  "yes",
@@ -45,15 +28,5 @@ void BooleanSettingPolicy::tabCompletion(vector<string>& tokens) const
 	Completer::completeString(tokens, values, false); // case insensitive
 }
 
-
-// class BooleanSetting
-
-BooleanSetting::BooleanSetting(
-		CommandController& commandController, string_ref name,
-		string_ref description, bool initialValue, SaveSetting save)
-	: SettingImpl<BooleanSettingPolicy>(
-		commandController, name, description, initialValue, save)
-{
-}
 
 } // namespace openmsx

src/settings/EnumSetting.hh
@@ -1,142 +1,103 @@
 #ifndef ENUMSETTING_HH
 #define ENUMSETTING_HH
 
-#include "SettingPolicy.hh"
-#include "SettingImpl.hh"
+#include "Setting.hh"
 #include <map>
 
 namespace openmsx {
 
 class TclObject;
 
-// non-templatized base class for EnumSettingPolicy<T>
-class EnumSettingPolicyBase
+// non-templatized base class
+class EnumSettingBase
 {
-public:
-	std::vector<std::string> getPossibleValues() const;
-
 protected:
-	virtual ~EnumSettingPolicyBase() {}
+	typedef std::map<std::string, int> BaseMap;
+	EnumSettingBase(BaseMap&& m);
+
+	int fromStringBase(string_ref str) const;
+	string_ref toStringBase(int value) const;
+
+	std::vector<string_ref> getPossibleValues() const;
 	void additionalInfoBase(TclObject& result) const;
 	void tabCompletionBase(std::vector<std::string>& tokens) const;
-	int fromStringBase(const std::string& str) const;
-	std::string toStringBase(int value) const;
-	virtual void checkSetValueBase(int& value) const = 0;
 
-	std::map<std::string, int> baseMap;
+private:
+	BaseMap baseMap;
 };
 
-template <typename T> class EnumSettingPolicy
-	: public EnumSettingPolicyBase, public SettingPolicy<T>
+template<typename T> class EnumSetting : private EnumSettingBase, public Setting
 {
 public:
 	typedef std::map<std::string, T> Map;
 
-protected:
-	EnumSettingPolicy(const Map& map_);
-	virtual ~EnumSettingPolicy();
-
-	std::string toString(T value) const;
-	T fromString(const std::string& str) const;
-	virtual void checkSetValue(T& value) const;
-	void tabCompletion(std::vector<std::string>& tokens) const;
-	string_ref getTypeString() const;
-	void additionalInfo(TclObject& result) const;
-
-private:
-	virtual void checkSetValueBase(int& value) const;
-};
-
-
-template <typename T> class EnumSetting : public SettingImpl<EnumSettingPolicy<T>>
-{
-public:
 	EnumSetting(CommandController& commandController, string_ref name,
 	            string_ref description, T initialValue,
-	            const typename EnumSettingPolicy<T>::Map& map_,
-	            Setting::SaveSetting save = Setting::SAVE);
+	            Map& map_, SaveSetting save = SAVE);
 
-	T getEnum() const { return this->getValue(); }
-	void setEnum(T t) { this->changeValue(t); }
+	virtual string_ref getTypeString() const;
+	virtual void additionalInfo(TclObject& result) const;
+	virtual void tabCompletion(std::vector<std::string>& tokens) const;
+
+	T getEnum() const;
+	void setEnum(T value);
+
+private:
+	string_ref toString(T value) const;
 };
 
 
 //-------------
-
-template <typename T>
-EnumSettingPolicy<T>::EnumSettingPolicy(const Map& map)
-	: SettingPolicy<T>()
-{
-	baseMap.insert(map.begin(), map.end());
-}
-
-template <typename T>
-EnumSettingPolicy<T>::~EnumSettingPolicy()
-{
-}
-
-template<typename T>
-std::string EnumSettingPolicy<T>::toString(T value) const
-{
-	return toStringBase(static_cast<int>(value));
-}
-
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable:4800)
-// C4800: 'int' : forcing value to bool 'true' or 'false' (performance warning)
-#endif
-template<typename T>
-T EnumSettingPolicy<T>::fromString(const std::string& str) const
-{
-	return static_cast<T>(fromStringBase(str));
-}
-
-template<typename T>
-void EnumSettingPolicy<T>::checkSetValueBase(int& value) const
-{
-	auto t = static_cast<T>(value);
-	checkSetValue(t);
-	value = static_cast<int>(t);
-}
-
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-
-template<typename T>
-void EnumSettingPolicy<T>::checkSetValue(T& /*value*/) const
-{
-}
-
-template<typename T>
-void EnumSettingPolicy<T>::tabCompletion(std::vector<std::string>& tokens) const
-{
-	tabCompletionBase(tokens);
-}
-
-template<typename T>
-string_ref EnumSettingPolicy<T>::getTypeString() const
-{
-	return "enumeration";
-}
-
-template<typename T>
-void EnumSettingPolicy<T>::additionalInfo(TclObject& result) const
-{
-	additionalInfoBase(result);
-}
 
 
 template <typename T>
 EnumSetting<T>::EnumSetting(
 		CommandController& commandController, string_ref name,
 		string_ref description, T initialValue,
-		const typename EnumSettingPolicy<T>::Map& map_,
-		Setting::SaveSetting save)
-	: SettingImpl<EnumSettingPolicy<T>>(
-		commandController, name, description, initialValue, save, map_)
+		Map& map, SaveSetting save)
+	: EnumSettingBase(BaseMap(map.begin(), map.end()))
+	, Setting(commandController, name, description,
+	          toString(initialValue).str(), save)
 {
+	setChecker([this](TclObject& newValue) {
+		fromStringBase(newValue.getString()); // may throw
+	});
+}
+
+template<typename T>
+string_ref EnumSetting<T>::getTypeString() const
+{
+	return "enumeration";
+}
+
+template<typename T>
+void EnumSetting<T>::additionalInfo(TclObject& result) const
+{
+	additionalInfoBase(result);
+}
+
+template<typename T>
+void EnumSetting<T>::tabCompletion(std::vector<std::string>& tokens) const
+{
+	tabCompletionBase(tokens);
+}
+
+template<typename T>
+T EnumSetting<T>::getEnum() const
+{
+	return static_cast<T>(fromStringBase(getValue().getString()));
+}
+
+template<typename T>
+void EnumSetting<T>::setEnum(T value)
+{
+	setString(toString(value).str());
+}
+
+template<typename T>
+string_ref EnumSetting<T>::toString(T value) const
+{
+	return toStringBase(static_cast<int>(value));
 }
 
 } // namespace openmsx

src/settings/FilenameSetting.cc
@@ -4,29 +4,23 @@
 
 namespace openmsx {
 
-FilenameSettingPolicy::FilenameSettingPolicy()
-{
-}
-
-void FilenameSettingPolicy::tabCompletion(std::vector<std::string>& tokens) const
-{
-	Completer::completeFileName(tokens, SystemFileContext());
-}
-
-string_ref FilenameSettingPolicy::getTypeString() const
-{
-	return "filename";
-}
-
-
 FilenameSetting::FilenameSetting(
 		CommandController& commandController,
 		string_ref name, string_ref description,
 		string_ref initialValue)
-	: SettingImpl<FilenameSettingPolicy>(
-		commandController, name, description, initialValue.str(),
-		Setting::SAVE)
+	: Setting(commandController, name, description,
+	          initialValue.str(), Setting::SAVE)
 {
+}
+
+string_ref FilenameSetting::getTypeString() const
+{
+	return "filename";
+}
+
+void FilenameSetting::tabCompletion(std::vector<std::string>& tokens) const
+{
+	Completer::completeFileName(tokens, SystemFileContext());
 }
 
 } // namespace openmsx

src/settings/FloatSetting.cc
@@ -1,52 +1,46 @@
 #include "FloatSetting.hh"
-#include "CommandException.hh"
-#include <sstream>
-#include <iomanip>
-#include <cstdio>
-
-using std::string;
 
 namespace openmsx {
 
-// class FloatSettingPolicy
-
-FloatSettingPolicy::FloatSettingPolicy(double minValue, double maxValue)
-	: SettingRangePolicy<double>(minValue, maxValue)
+static std::string toString(double d)
 {
+	TclObject obj;
+	obj.setDouble(d);
+	return obj.getString().str();
 }
 
-string FloatSettingPolicy::toString(double value) const
+FloatSetting::FloatSetting(CommandController& commandController,
+                           string_ref name, string_ref description,
+                           double initialValue,
+                           double minValue_, double maxValue_)
+	: Setting(commandController, name, description,
+	          toString(initialValue), SAVE)
+	, minValue(minValue_)
+	, maxValue(maxValue_)
 {
-	std::stringstream out;
-	out << std::setprecision(2) << std::fixed << std::showpoint << value;
-	return out.str();
+	setChecker([this](TclObject& newValue) {
+		double value = newValue.getDouble(); // may throw
+		double clipped = std::min(std::max(value, minValue), maxValue);
+		newValue.setDouble(clipped);
+	});
 }
 
-double FloatSettingPolicy::fromString(const string& str) const
-{
-	char* endPtr;
-	double result = strtod(str.c_str(), &endPtr);
-	if (*endPtr != '\0') {
-		throw CommandException("not a valid float: " + str);
-	}
-	return result;
-}
-
-string_ref FloatSettingPolicy::getTypeString() const
+string_ref FloatSetting::getTypeString() const
 {
 	return "float";
 }
 
-
-// class FloatSetting
-
-FloatSetting::FloatSetting(CommandController& commandController,
-                           string_ref name, string_ref description,
-                           double initialValue, double minValue, double maxValue)
-	: SettingImpl<FloatSettingPolicy>(
-		commandController, name, description, initialValue,
-		Setting::SAVE, minValue, maxValue)
+void FloatSetting::additionalInfo(TclObject& result) const
 {
+	TclObject range(result.getInterpreter());
+	range.addListElement(minValue);
+	range.addListElement(maxValue);
+	result.addListElement(range);
+}
+
+void FloatSetting::setDouble (double d)
+{
+	setString(toString(d));
 }
 
 } // namespace openmsx

src/settings/IntegerSetting.cc
@@ -1,51 +1,39 @@
 #include "IntegerSetting.hh"
-#include "CommandException.hh"
-#include <sstream>
-#include <cstdio>
-
-using std::string;
+#include "StringOp.hh"
 
 namespace openmsx {
 
-// class IntegerSettingPolicy
-
-IntegerSettingPolicy::IntegerSettingPolicy(int minValue, int maxValue)
-	: SettingRangePolicy<int>(minValue, maxValue)
+IntegerSetting::IntegerSetting(CommandController& commandController,
+                               string_ref name, string_ref description,
+                               int initialValue, int minValue_, int maxValue_)
+	: Setting(commandController, name, description,
+	          StringOp::toString(initialValue), SAVE)
+	, minValue(minValue_)
+	, maxValue(maxValue_)
 {
+	setChecker([this](TclObject& newValue) {
+		int value = newValue.getInt(); // may throw
+		int clipped = std::min(std::max(value, minValue), maxValue);
+		newValue.setInt(clipped);
+	});
 }
 
-string IntegerSettingPolicy::toString(int value) const
-{
-	std::ostringstream out;
-	out << value;
-	return out.str();
-}
-
-int IntegerSettingPolicy::fromString(const string& str) const
-{
-	char* endPtr;
-	int result = strtol(str.c_str(), &endPtr, 0);
-	if (*endPtr != '\0') {
-		throw CommandException("not a valid integer: " + str);
-	}
-	return result;
-}
-
-string_ref IntegerSettingPolicy::getTypeString() const
+string_ref IntegerSetting::getTypeString() const
 {
 	return "integer";
 }
 
-
-// class IntegerSetting
-
-IntegerSetting::IntegerSetting(CommandController& commandController,
-                               string_ref name, string_ref description,
-                               int initialValue, int minValue, int maxValue)
-	: SettingImpl<IntegerSettingPolicy>(
-		commandController, name, description, initialValue,
-		Setting::SAVE, minValue, maxValue)
+void IntegerSetting::additionalInfo(TclObject& result) const
 {
+	TclObject range(result.getInterpreter());
+	range.addListElement(minValue);
+	range.addListElement(maxValue);
+	result.addListElement(range);
+}
+
+void IntegerSetting::setInt(int i)
+{
+	setString(StringOp::toString(i));
 }
 
 } // namespace openmsx

src/settings/KeyCodeSetting.cc
@@ -1,44 +1,30 @@
 #include "KeyCodeSetting.hh"
 #include "CommandException.hh"
 
-using std::string;
-
 namespace openmsx {
-
-// class KeyCodeSettingPolicy
-
-KeyCodeSettingPolicy::KeyCodeSettingPolicy()
-{
-}
-
-string KeyCodeSettingPolicy::toString(Keys::KeyCode key) const
-{
-	return Keys::getName(key);
-}
-
-Keys::KeyCode KeyCodeSettingPolicy::fromString(const string& str) const
-{
-	Keys::KeyCode code = Keys::getCode(str);
-	if (code == Keys::K_NONE) {
-		throw CommandException("Not a valid key: " + str);
-	}
-	return code;
-}
-
-string_ref KeyCodeSettingPolicy::getTypeString() const
-{
-	return "key";
-}
-
-// class KeyCodeSetting
 
 KeyCodeSetting::KeyCodeSetting(CommandController& commandController,
                                string_ref name, string_ref description,
                                Keys::KeyCode initialValue)
-	: SettingImpl<KeyCodeSettingPolicy>(
-		commandController, name, description, initialValue,
-		Setting::SAVE)
+	: Setting(commandController, name, description,
+	          Keys::getName(initialValue), SAVE)
 {
+	setChecker([this](TclObject& newValue) {
+		const auto& str = newValue.getString();
+		if (Keys::getCode(str) == Keys::K_NONE) {
+			throw CommandException("Not a valid key: " + str);
+		}
+	});
+}
+
+string_ref KeyCodeSetting::getTypeString() const
+{
+	return "key";
+}
+
+Keys::KeyCode KeyCodeSetting::getKey() const
+{
+	return Keys::getCode(getValue().getString());
 }
 
 } // namespace openmsx

src/settings/ReadOnlySetting.cc
@@ -1,0 +1,32 @@
+#include "ReadOnlySetting.hh"
+#include "MSXException.hh"
+
+namespace openmsx {
+
+ReadOnlySetting::ReadOnlySetting(
+		CommandController& commandController,
+		string_ref name, string_ref description,
+		const std::string& initialValue)
+	: Setting(commandController, name, description, initialValue,
+	          Setting::DONT_TRANSFER)
+	, roValue(initialValue)
+{
+	setChecker([this](TclObject& newValue) {
+		if (newValue.getString() != roValue) {
+			throw MSXException("Read-only setting");
+		}
+	});
+}
+
+void ReadOnlySetting::setReadOnlyValue(const std::string& value)
+{
+	roValue = value;
+	setString(value);
+}
+
+string_ref ReadOnlySetting::getTypeString() const
+{
+	return "read-only";
+}
+
+} // namespace openmsx

src/settings/Setting.cc
@@ -7,6 +7,7 @@
 #include "TclObject.hh"
 #include "CliComm.hh"
 #include "XMLElement.hh"
+#include "MSXException.hh"
 #include "checked_cast.hh"
 #include <algorithm>
 #include <cassert>
@@ -15,45 +16,109 @@ using std::string;
 
 namespace openmsx {
 
-Setting::Setting(CommandController& commandController_, string_ref name_,
-                 string_ref desc_, SaveSetting save_)
-	: commandController(commandController_)
-	, name       (name_.str())
+// class BaseSetting
+
+BaseSetting::BaseSetting(string_ref name_)
+	: name       (name_.str())
+{
+}
+
+const string& BaseSetting::getName() const
+{
+	return name;
+}
+
+void BaseSetting::info(TclObject& result) const
+{
+	result.addListElement(getTypeString());
+	result.addListElement(getDefaultValue());
+	additionalInfo(result);
+}
+
+
+// class Setting
+
+Setting::Setting(CommandController& commandController_,
+                 string_ref name_, string_ref desc_,
+                 const string& initialValue, SaveSetting save_)
+	: BaseSetting(name_)
+	, commandController(commandController_)
 	, description(desc_.str())
+	, value(TclObject(getInterpreter(), initialValue))
+	, defaultValue(initialValue)
+	, restoreValue(initialValue)
 	, save(save_)
 {
+	checkFunc = [](TclObject&) { /* nothing */ };
+
+	if (needLoadSave()) {
+		auto& settingsConfig = getGlobalCommandController()
+			.getSettingsConfig().getXMLElement();
+		if (auto* config = settingsConfig.findChild("settings")) {
+			if (auto* elem = config->findChildWithAttribute(
+			                                "setting", "id", getName())) {
+				try {
+					setStringDirect(elem->getData());
+				} catch (MSXException&) {
+					// saved value no longer valid, just keep default
+				}
+			}
+		}
+	}
+	getCommandController().registerSetting(*this);
+
+	// This is needed to for example inform catapult of the new setting
+	// value when a setting was destroyed/recreated (by a machine switch
+	// for example).
+	notify();
 }
 
 Setting::~Setting()
 {
+	getCommandController().unregisterSetting(*this);
 }
 
-const string& Setting::getName() const
-{
-	return name;
-}
 
 string Setting::getDescription() const
 {
 	return description;
 }
 
-void Setting::setString(const std::string& valueString)
+void Setting::setString(const string& value)
 {
-	getCommandController().changeSetting(*this, valueString);
+	getCommandController().changeSetting(*this, value);
 }
 
 void Setting::notify() const
 {
+	// Notify all subsystems of a change in the setting value. There
+	// are actually quite a few subsystems involved with the settings:
+	//  - the setting classes themselves
+	//  - the Tcl variables (and possibly traces on those variables)
+	//  - Subject/Observers
+	//  - CliComm setting-change events (for external GUIs)
+	//  - SettingsConfig (keeps values, also of not yet created settings)
+	// This method takes care of the last 3 in this list.
 	Subject<Setting>::notify();
 	commandController.getCliComm().update(
-		CliComm::SETTING, getName(), getValueString());
+		CliComm::SETTING, getName(), getString());
 
 	// Always keep SettingsConfig in sync.
-	// TODO At the moment (partly because of this change) the whole Settings
-	//  structure is more complicated than it could be. Though we're close
-	//  to a release, so now is not the time to do big refactorings.
-	sync(getGlobalCommandController().getSettingsConfig().getXMLElement());
+	auto& config = getGlobalCommandController().getSettingsConfig().getXMLElement();
+	auto& settings = config.getCreateChild("settings");
+	string value = getString();
+	if (!needLoadSave() || (value == getDefaultValue())) {
+		// remove setting
+		if (auto* elem = settings.findChildWithAttribute(
+				"setting", "id", getName())) {
+			settings.removeChild(*elem);
+		}
+	} else {
+		// add (or overwrite) setting
+		auto& elem = settings.getCreateChildWithAttribute(
+				"setting", "id", getName());
+		elem.setData(value);
+	}
 }
 
 void Setting::notifyPropertyChange() const
@@ -73,39 +138,6 @@ bool Setting::needTransfer() const
 	return save != DONT_TRANSFER;
 }
 
-void Setting::setDontSaveValue(const std::string& dontSaveValue_)
-{
-	dontSaveValue = dontSaveValue_;
-}
-
-void Setting::sync(XMLElement& config) const
-{
-	auto& settings = config.getCreateChild("settings");
-	if (!needLoadSave() || hasDefaultValue()) {
-		// remove setting
-		if (auto* elem = settings.findChildWithAttribute(
-				"setting", "id", getName())) {
-			settings.removeChild(*elem);
-		}
-	} else {
-		// add (or overwrite) setting
-		auto& elem = settings.getCreateChildWithAttribute(
-				"setting", "id", getName());
-		// check for non-saveable value
-		// (mechanism can be generalize later when needed)
-		string tmp = getValueString();
-		if (tmp == dontSaveValue) tmp = getRestoreValueString();
-		elem.setData(tmp);
-	}
-}
-
-void Setting::info(TclObject& result) const
-{
-	result.addListElement(getTypeString());
-	result.addListElement(getDefaultValueString());
-	additionalInfo(result);
-}
-
 CommandController& Setting::getCommandController() const
 {
 	return commandController;
@@ -113,7 +145,7 @@ CommandController& Setting::getCommandController() const
 
 GlobalCommandController& Setting::getGlobalCommandController() const
 {
-	if (auto globalCommandController =
+	if (auto* globalCommandController =
 	    dynamic_cast<GlobalCommandController*>(&commandController)) {
 		return *globalCommandController;
 	} else {
@@ -124,7 +156,73 @@ GlobalCommandController& Setting::getGlobalCommandController() const
 
 Interpreter& Setting::getInterpreter() const
 {
-	return getGlobalCommandController().getInterpreter();
+	return commandController.getInterpreter();
+}
+
+void Setting::tabCompletion(std::vector<string>& /*tokens*/) const
+{
+	// nothing
+}
+
+void Setting::additionalInfo(TclObject& /*result*/) const
+{
+	// nothing
+}
+
+void Setting::setRestoreValue(const string& value)
+{
+	restoreValue = value;
+}
+
+void Setting::setChecker(std::function<void(TclObject&)> checkFunc_)
+{
+	checkFunc = checkFunc_;
+}
+
+
+string Setting::getString() const
+{
+	return value.getString().str();
+}
+
+string Setting::getDefaultValue() const
+{
+	return defaultValue;
+}
+
+string Setting::getRestoreValue() const
+{
+	return restoreValue;
+}
+
+void Setting::setStringDirect(const string& str)
+{
+	TclObject newValue(getInterpreter(), str);
+	checkFunc(newValue);
+	if (newValue != value) {
+		value = newValue;
+		notify();
+	}
+
+	// synchronize proxy
+	auto* controller = dynamic_cast<MSXCommandController*>(
+		&getCommandController());
+	if (!controller) {
+		// This is not a machine specific setting.
+		return;
+	}
+	if (!controller->isActive()) {
+		// This setting does not belong to the active machine.
+		return;
+	}
+
+	auto& globalController = controller->getGlobalCommandController();
+	// Tcl already makes sure this doesn't result in an endless loop.
+	try {
+		globalController.changeSetting(getName(), getString());
+	} catch (MSXException&) {
+		// ignore
+	}
 }
 
 } // namespace openmsx

commit 8fc547588efa44120570a31cd4fbab79b9996d99

Fix loading invalid setting values from settings.xml
Before this patch:
 - manually edit settings.xml, e.g. add/modify the line
      <setting id="renderer">blabla</setting>
 - start openmsx
     -> crash
This is a regression (likely introduced in dffe5f98a). We used to fall back to
the default value in case settings.xml contains an invalid value (unknown enum
value or an integer/float outside the allowed range).

The problem was that the Settings base class constructor already loads the
value from settings.xml. But only (shortly) later does the subclass constructor
set the correct setting-value-checker function. To fix this I've split the base
class constructor in two parts. The 2nd part (a new init() method) should
manually be called from each subclass constructor.

We already knew about this problem before the 0.10.0 release. But because the
settings code is always tricky I felt more comfortable fixing this after the
release rather than just a few days before. It's anyway (normally) not possible
to trigger this bug without manually editing settings.xml.

src/settings/BooleanSetting.cc
@@ -14,6 +14,7 @@ BooleanSetting::BooleanSetting(
 		// Re-set the queried value to get a normalized value.
 		newValue.setString(toString(newValue.getBoolean()));
 	});
+	init();
 }
 
 string_ref BooleanSetting::getTypeString() const

src/settings/EnumSetting.hh
@@ -66,6 +66,7 @@ EnumSetting<T>::EnumSetting(
 	setChecker([this](TclObject& newValue) {
 		fromStringBase(newValue.getString()); // may throw
 	});
+	init();
 }
 
 template<typename T>

src/settings/FilenameSetting.cc
@@ -11,6 +11,7 @@ FilenameSetting::FilenameSetting(
 	: Setting(commandController, name, description,
 	          initialValue.str(), Setting::SAVE)
 {
+	init();
 }
 
 string_ref FilenameSetting::getTypeString() const

src/settings/FloatSetting.cc
@@ -23,6 +23,7 @@ FloatSetting::FloatSetting(CommandController& commandController,
 		double clipped = std::min(std::max(value, minValue), maxValue);
 		newValue.setDouble(clipped);
 	});
+	init();
 }
 
 string_ref FloatSetting::getTypeString() const

src/settings/IntegerSetting.cc
@@ -16,6 +16,7 @@ IntegerSetting::IntegerSetting(CommandController& commandController,
 		int clipped = std::min(std::max(value, minValue), maxValue);
 		newValue.setInt(clipped);
 	});
+	init();
 }
 
 string_ref IntegerSetting::getTypeString() const

src/settings/KeyCodeSetting.cc
@@ -15,6 +15,7 @@ KeyCodeSetting::KeyCodeSetting(CommandController& commandController,
 			throw CommandException("Not a valid key: " + str);
 		}
 	});
+	init();
 }
 
 string_ref KeyCodeSetting::getTypeString() const

src/settings/ReadOnlySetting.cc
@@ -16,6 +16,7 @@ ReadOnlySetting::ReadOnlySetting(
 			throw MSXException("Read-only setting");
 		}
 	});
+	init();
 }
 
 void ReadOnlySetting::setReadOnlyValue(const std::string& value)

src/settings/Setting.cc
@@ -47,7 +47,10 @@ Setting::Setting(CommandController& commandController_,
 	, save(save_)
 {
 	checkFunc = [](TclObject&) { /* nothing */ };
+}
 
+void Setting::init()
+{
 	if (needLoadSave()) {
 		auto& settingsConfig = getGlobalCommandController()
 			.getSettingsConfig().getXMLElement();

src/settings/Setting.hh
@@ -151,6 +151,7 @@ protected:
 	Setting(CommandController& commandController,
 	        string_ref name, string_ref description,
 	        const std::string& initialValue, SaveSetting save = SAVE);
+	void init();
 	void notifyPropertyChange() const;
 
 private:

src/settings/StringSetting.cc
@@ -8,6 +8,7 @@ StringSetting::StringSetting(CommandController& commandController,
 	: Setting(commandController, name, description,
 	          initialValue.str(), save)
 {
+	init();
 }
 
 string_ref StringSetting::getTypeString() const

src/settings/VideoSourceSetting.cc
@@ -15,6 +15,7 @@ VideoSourceSetting::VideoSourceSetting(CommandController& commandController)
 	setChecker([this](TclObject& newValue) {
 		checkSetValue(newValue.getString()); // may throw
 	});
+	init();
 }
 
 void VideoSourceSetting::checkSetValue(string_ref value) const