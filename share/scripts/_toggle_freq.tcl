9a51add1ee164a54031399768f1e721808343c27
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2014-03-02 22:59:29 +0100

    Tweak toggle_freq to be more robust.

--- share/scripts/_toggle_freq.tcl
+++ share/scripts/_toggle_freq.tcl
@@ -1,8 +1,12 @@
-set_help_text toggle_freq "Switch between PAL/NTSC."
+set_help_text toggle_freq "Switch the VDP between PAL (50 Hz)/NTSC (60 Hz)."
 
 proc toggle_freq {} {
 	debug write "VDP regs" 9    [expr {[debug read "VDP regs" 9]    ^ 2}]
 	debug write "memory" 0xFFE8 [expr {[debug read "memory" 0xFFE8] ^ 2}]
 
-	if {[debug read "VDP regs" 9]==0} {osd::display_message "Frequency set to 60hz" info} else {osd::display_message "Frequency set to 50hz" info}
+	set freq 60
+	if {[expr {[debug read "VDP regs" 9] & 0x2}]} {
+		set freq 50
+	}
+	osd::display_message "Frequency set to $freq Hz" info
 }
