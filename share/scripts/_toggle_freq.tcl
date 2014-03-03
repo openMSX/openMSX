f438a158d62b4fbcf9e1d7118213bdbbb33393f5
m9710797 <vermaelen.wouter@gmail.com>
2014-03-03 12:46:30 +0100

    Tweaks in _toggle_freq.tcl
    In constructs like
       if {[expr <some-expression]} {
    the 'expr' part is superfluous because the first argument of an if command is
    already interpreted as an expression. So it can be simplified to
       if {<some-expression>} {
    
    In this specific case the script could be simplified even more by using ?: and
    by using the peek/poke/vdpreg helper procs.

--- share/scripts/_toggle_freq.tcl
+++ share/scripts/_toggle_freq.tcl
@@ -1,12 +1,9 @@
 set_help_text toggle_freq "Switch the VDP between PAL (50 Hz)/NTSC (60 Hz)."
 
 proc toggle_freq {} {
-	debug write "VDP regs" 9    [expr {[debug read "VDP regs" 9]    ^ 2}]
-	debug write "memory" 0xFFE8 [expr {[debug read "memory" 0xFFE8] ^ 2}]
+	vdpreg 9    [expr {[vdpreg 9   ] ^ 2}]
+	poke 0xFFE8 [expr {[peek 0xFFE8] ^ 2}]
 
-	set freq 60
-	if {[expr {[debug read "VDP regs" 9] & 0x2}]} {
-		set freq 50
-	}
+	set freq [expr {([vdpreg 9] & 2) ? 50 : 60}]
 	osd::display_message "Frequency set to $freq Hz" info
 }
