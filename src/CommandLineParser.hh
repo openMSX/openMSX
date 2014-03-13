openMSX/openMSX

commit e05b519f4c1d665f07604fece3f94a1f84830250

Process -control command line option last
Consider the following scenario:
 - (wx)Catapult start openmsx with
      openmsx -ext ide -hda hd.dsk -control stdio
 - Openmsx starts up, but during the startup sequence the filepool mechanism
   starts scanning.
 - During this scanning the event loop is triggered to be able to show progress
   info
 - But the event loop also starts accepting external CliComm commands
 - One of the command send by catapult is 'set power on'
 - After the filepool scanning is done, the openmsx startup sequence continue,
   one of the remaining things is processing the '-hda' option.
 - But because power is already turned on, this now gives a
    "can only change hard disk image when MSX is powered down"
   error message.

This patch changes the priority of the '-control' command line option. It's now
processed after all other options. So external command are only accepted after
the full commandline has been processed (and thus after the full MSX machine
has been initialized). This fixes the above scenario. Though note that the
execution of all Tcl scripts is still done after the -control option. Fixing
that requires a larger refactoring. But let's only do that once it becomes a
problem.

src/CommandLineParser.hh
@@ -48,13 +48,14 @@ public:
 		PHASE_BEFORE_MACHINE,    // -machine
 		PHASE_LOAD_MACHINE,      // loads machine hardwareconfig.xml
 		PHASE_EXT,               // -ext
-		PHASE_LAST,              // all the rest
+		PHASE_DEFAULT,           // all the rest
+		PHASE_LAST,              // -control
 	};
 
 	explicit CommandLineParser(Reactor& reactor);
 	~CommandLineParser();
 	void registerOption(const char* str, CLIOption& cliOption,
-		ParsePhase phase = PHASE_LAST, unsigned length = 2);
+		ParsePhase phase = PHASE_DEFAULT, unsigned length = 2);
 	void registerFileType(string_ref extensions, CLIFileType& cliFileType);
 	void parse(int argc, char** argv);
 	ParseStatus getParseStatus() const;

src/CommandLineParser.cc
@@ -177,7 +177,6 @@ CommandLineParser::CommandLineParser(Reactor& reactor_)
 	registerOption("-bash",       *bashOption,    PHASE_BEFORE_INIT, 1);
 
 	registerOption("-setting",    *settingOption, PHASE_BEFORE_SETTINGS);
-	registerOption("-control",    *controlOption, PHASE_BEFORE_SETTINGS, 1);
 	registerOption("-script",     *scriptOption,  PHASE_BEFORE_SETTINGS, 1); // correct phase?
 	#if COMPONENT_GL
 	registerOption("-nopbo",      *noPBOOption,   PHASE_BEFORE_SETTINGS, 1);
@@ -185,6 +184,8 @@ CommandLineParser::CommandLineParser(Reactor& reactor_)
 	registerOption("-testconfig", *testConfigOption, PHASE_BEFORE_SETTINGS, 1);
 
 	registerOption("-machine",    *machineOption, PHASE_BEFORE_MACHINE);
+
+	registerOption("-control",    *controlOption, PHASE_LAST, 1);
 
 	registerFileType("tcl", *scriptOption);
 

commit 18458dffb43cfdb73b02597a9ea18c327e7bf2bc

Fixed -romtype command line option regression
This works:
	openmsx -carta myrom.rom -romtype KonamiSCC
This used to work as well, but was now broken:
	openmsx myrom.rom -romtype KonamiSCC

The regression was introduced in e05b519f4c where I (unintentionally) parse
command line filenames in a different pass than the -romtype option. Fixed now.

Actually in the future it might be a good idea to break backwards compatibility
and just parse the commandline from left to right. Lots of the historical
reasons to have multi-pass parsing are no longer valid (e.g. possibility to
specify filetypes in the settings file).

src/CommandLineParser.hh
@@ -47,14 +47,13 @@ public:
 		PHASE_LOAD_SETTINGS,     // loads settings.xml
 		PHASE_BEFORE_MACHINE,    // -machine
 		PHASE_LOAD_MACHINE,      // loads machine hardwareconfig.xml
-		PHASE_DEFAULT,           // all the rest
-		PHASE_LAST,              // -control
+		PHASE_LAST,              // all the rest
 	};
 
 	explicit CommandLineParser(Reactor& reactor);
 	~CommandLineParser();
 	void registerOption(const char* str, CLIOption& cliOption,
-		ParsePhase phase = PHASE_DEFAULT, unsigned length = 2);
+		ParsePhase phase = PHASE_LAST, unsigned length = 2);
 	void registerFileType(string_ref extensions, CLIFileType& cliFileType);
 	void parse(int argc, char** argv);
 	ParseStatus getParseStatus() const;