ef58f026538803de792473013cea28f3f6c2a5ec
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2014-04-28 15:28:58 +0200

    Also dist the dmk tools and instructions.

--- Contrib/node.mk
+++ Contrib/node.mk
@@ -5,6 +5,7 @@ DIST:= \
 	README.cbios cbios \
 	README.openmsx-control \
 	openmsx-control-stdio.cc openmsx-control-socket.cc \
-	basictorom.tcl
+	basictorom.tcl \
+	dmk
 
 include build/node-end.mk
