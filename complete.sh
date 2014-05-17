957614f9cb3e494d6a3b5c95116bdc1190f78adb
m9710797 <vermaelen.wouter@gmail.com>
2014-05-17 09:54:53 +0200

    Remove complete.sh
    Commit 719d5646 added these 2 files:
      complete.sh
      Contrib/openmsx-complete.bash
    Except for the comments both have identical content. The former was my work
    in progress version of the latter. It was only added by accident.

--- complete.sh
+++
@@ -1,8 +0,0 @@
-_openmsx_complete()
-{
-	local cur tmp
-	cur=${COMP_WORDS[COMP_CWORD]}
-	tmp=$(openmsx -bash $3)
-	COMPREPLY=($(compgen -W '$tmp' -- "$cur"))
-}
-complete -f -F _openmsx_complete openmsx
