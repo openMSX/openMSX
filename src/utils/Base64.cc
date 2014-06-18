openMSX/openMSX

commit 178edb626ed4dab790906d58779da15608258d7a

Speedup Base64::encode() and Base64::decode()
Immediately set output buffer to a big enough (too big) size (and shrink to
correct size at the end). This avoids the occasional reallocations that were
needed before this patch.

An alternative implementation is to use std::string::reserve() instead of
resize(). This has the advantage that initially the buffer can be left
uninitialized (now it is filled with dummy values). But the disadvantage is
that each append() still checks whether the buffer is big enough (will always
be the case). I measured both alternatives and this was the fastest.

This results in a small speedup when loading a replay file.

git-svn-id: file:///openmsx-svn/code/openmsx/trunk@11579 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/utils/Base64.cc
@@ -36,21 +36,24 @@ static inline byte decode(unsigned char c)
 	}
 }
 
-string encode(const void* input_, int len, bool newlines)
+string encode(const void* input_, int inSize)
 {
 	const byte* input = static_cast<const byte*>(input_);
-	string ret;
+	unsigned outSize = ((inSize + 44) / 45) * 61; // overestimation
+	string ret(outSize, 0); // too big
+
 	int n = 0;
-	for (/**/; len > 0; len -= 45) {
-		if (newlines && !ret.empty()) ret += '\n';
-		n = std::min(45, len);
+	unsigned out = 0;
+	for (/**/; inSize > 0; inSize -= 45) {
+		if (!ret.empty()) ret += '\n';
+		n = std::min(45, inSize);
 		for (/**/; n >= 3; n -= 3) {
-			ret += encode( (input[0] & 0xfc) >> 2);
-			ret += encode(((input[0] & 0x03) << 4) +
-			              ((input[1] & 0xf0) >> 4));
-			ret += encode(((input[1] & 0x0f) << 2) +
-			              ((input[2] & 0xc0) >> 6));
-			ret += encode( (input[2] & 0x3f) >> 0);
+			ret[out++] = encode( (input[0] & 0xfc) >> 2);
+			ret[out++] = encode(((input[0] & 0x03) << 4) +
+			                    ((input[1] & 0xf0) >> 4));
+			ret[out++] = encode(((input[1] & 0x0f) << 2) +
+			                    ((input[2] & 0xc0) >> 6));
+			ret[out++] = encode( (input[2] & 0x3f) >> 0);
 			input += 3;
 		}
 		if (n) {
@@ -66,45 +69,54 @@ string encode(const void* input_, int len, bool newlines)
 				  ((buf3[2] & 0xc0) >> 6);
 			buf4[3] =  (buf3[2] & 0x3f) >> 0;
 			for (int j = 0; (j < n + 1); ++j) {
-				ret += encode(buf4[j]);
+				ret[out++] = encode(buf4[j]);
 			}
 			for (/**/; n < 3; ++n) {
-				ret += '=';
+				ret[out++] = '=';
 			}
 		}
 	}
+
+	assert(outSize >= out);
+	ret.resize(out); // shrink to correct size
 	return ret;
 }
 
 string decode(const string& input)
 {
-	string ret;
-	int i = 0;
+	const size_t inSize = input.size();
+	unsigned outSize = (inSize * 3 + 3) / 4; // overestimation
+	string ret(outSize, 0); // too big
+
+	unsigned i = 0;
+	unsigned out = 0;
 	byte buf4[4];
-	const size_t len = input.size();
-	for (size_t in = 0; in < len; ++in) {
+	for (size_t in = 0; in < inSize; ++in) {
 		byte d = decode(input[in]);
 		if (d == (byte)-1) continue;
 		buf4[i++] = d;
 		if (i == 4) {
-			ret += char(((buf4[0] & 0xff) << 2) + ((buf4[1] & 0x30) >> 4));
-			ret += char(((buf4[1] & 0x0f) << 4) + ((buf4[2] & 0x3c) >> 2));
-			ret += char(((buf4[2] & 0x03) << 6) + ((buf4[3] & 0xff) >> 0));
 			i = 0;
+			ret[out++] = char(((buf4[0] & 0xff) << 2) + ((buf4[1] & 0x30) >> 4));
+			ret[out++] = char(((buf4[1] & 0x0f) << 4) + ((buf4[2] & 0x3c) >> 2));
+			ret[out++] = char(((buf4[2] & 0x03) << 6) + ((buf4[3] & 0xff) >> 0));
 		}
 	}
 	if (i) {
-		for (int j = i; j < 4; ++j) {
+		for (unsigned j = i; j < 4; ++j) {
 			buf4[j] = 0;
 		}
 		byte buf3[3];
 		buf3[0] = ((buf4[0] & 0xff) << 2) + ((buf4[1] & 0x30) >> 4);
 		buf3[1] = ((buf4[1] & 0x0f) << 4) + ((buf4[2] & 0x3c) >> 2);
 		buf3[2] = ((buf4[2] & 0x03) << 6) + ((buf4[3] & 0xff) >> 0);
-		for (int j = 0; (j < i - 1); ++j) {
-			ret += buf3[j];
+		for (unsigned j = 0; (j < i - 1); ++j) {
+			ret[out++] = buf3[j];
 		}
 	}
+
+	assert(outSize >= out);
+	ret.resize(out); // shrink to correct size
 	return ret;
 }
 

src/utils/Base64.hh
@@ -6,7 +6,7 @@
 #include <string>
 
 namespace Base64 {
-	std::string encode(const void* input, int len, bool newlines = true);
+	std::string encode(const void* input, int len);
 	std::string decode(const std::string& input);
 }
 

commit 7e50b4bff193db5df70c81b25f54dae76b880ad7

Fixed newline stuff in Base64
Commit 178edb626ed accidentally broke the insertion of newline characters in
base64 encoded strings. This wasn't really a problem because when decoding
base64 strings newlines are completely ignored. Though the code didn't behave
as it was intended.

Found while writing a unittest for the base64 class. For now those unittest
still live on my personal (local) branch. The build system still needs to be
changed before those tests are more generally useable.

This patch also changes the output format (line length) to be identical to
the output of the coreutils 'base64' tool.

src/utils/Base64.cc
@@ -38,14 +38,18 @@ static inline byte decode(unsigned char c)
 
 string encode(const void* input_, size_t inSize)
 {
+	static const int CHUNKS = 19;
+	static const int IN_CHUNKS  = 3 * CHUNKS;
+	static const int OUT_CHUNKS = 4 * CHUNKS; // 76 chars per line
+
 	auto input = static_cast<const byte*>(input_);
-	auto outSize = ((inSize + 44) / 45) * 61; // overestimation
+	auto outSize = ((inSize + (IN_CHUNKS - 1)) / IN_CHUNKS) * (OUT_CHUNKS + 1); // overestimation
 	string ret(outSize, 0); // too big
 
 	size_t out = 0;
 	while (inSize) {
-		if (!ret.empty()) ret += '\n';
-		auto n2 = std::min<size_t>(45, inSize);
+		if (out) ret[out++] = '\n';
+		auto n2 = std::min<size_t>(IN_CHUNKS, inSize);
 		auto n = unsigned(n2);
 		for (/**/; n >= 3; n -= 3) {
 			ret[out++] = encode( (input[0] & 0xfc) >> 2);