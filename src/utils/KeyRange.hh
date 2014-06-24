c6ac84ecf7073c89ee70081469924ad3516fbf0d
m9710797 <vermaelen.wouter@gmail.com>
2014-06-24 22:00:30 +0200

    Generalize KeyRange utility
    KeyRange could be used like this:
    	map<T1, T2> m;
    	for (auto& e : keys(m)) {...
    So it could transform a std::map into a range of all the keys in that map.
    
    This patch generalizes the keys() function so that it also works on e.g.
    a vector<pair<T1, T2>> or even vector<tuple<T1, T2, ...>>. It also adds a
    values() function (that gives a range of the values in a map, or the 2nd
    element in a pair/tuple). And it adds a elements<N>() function to pick the
    N-the element out of a collection of tuples.
    
    This patch also makes this utility more STL-compliant (e.g. the iterators can
    now also be used in algorithms like 'distance').
    
    I made both these changes while working on a unittest for this utility.

--- src/utils/KeyRange.hh
+++ src/utils/KeyRange.hh
@@ -2,26 +2,59 @@
 #define KEYRANGE_HH
 
 #include "StringMap.hh"
+#include <iterator>
+#include <tuple>
 
-template<typename MAP> class KeyIterator
+namespace detail {
+
+template<typename MAP, size_t N> class KeyIterator
 {
+	typedef typename MAP::const_iterator                        map_iter;
+	typedef typename std::iterator_traits<map_iter>::value_type pair_type;
 public:
-	KeyIterator(typename MAP::const_iterator it_)
-		: it(it_) {}
-	const typename MAP::key_type& operator*() const { return it->first; }
+
+	typedef const typename std::tuple_element<N, pair_type>::type     value_type;
+	typedef value_type*                                               pointer;
+	typedef value_type&                                               reference;
+	typedef typename std::iterator_traits<map_iter>::difference_type  difference_type;
+	typedef std::forward_iterator_tag                                 iterator_category;
+
+	KeyIterator(map_iter it_) : it(it_) {}
+	reference operator*() const { return std::get<N>(*it); }
 	KeyIterator& operator++() { ++it; return *this; }
 	bool operator==(KeyIterator& other) const { return it == other.it; }
 	bool operator!=(KeyIterator& other) const { return it != other.it; }
 private:
-	typename MAP::const_iterator it;
+	map_iter it;
 };
 
-template<typename T> class KeyIterator2
+template<typename T, size_t N> struct StringMapGetter;
+template<typename T> struct StringMapGetter<T, 0> {
+	typedef string_ref  value_type;
+	typedef string_ref* pointer;
+	typedef string_ref  reference;
+	reference operator()(const StringMapEntry<T>& entry) { return entry.first(); }
+};
+template<typename T> struct StringMapGetter<T, 1> {
+	typedef const T  value_type;
+	typedef const T* pointer;
+	typedef const T& reference;
+	reference operator()(const StringMapEntry<T>& entry) { return entry.second; }
+};
+
+template<typename T, size_t N> class KeyIterator2
 {
+	static_assert(N==0 || N==1, "StringMap only has key and value");
+	typedef StringMapGetter<T, N> Getter;
 public:
-	KeyIterator2(typename StringMap<T>::const_iterator it_)
-		: it(it_) {}
-	string_ref operator*() const { return it->first(); }
+	typedef typename Getter::value_type value_type;
+	typedef typename Getter::pointer    pointer;
+	typedef typename Getter::reference  reference;
+	typedef int                         difference_type;
+	typedef std::forward_iterator_tag   iterator_category;
+
+	KeyIterator2(typename StringMap<T>::const_iterator it_) : it(it_) {}
+	reference operator*() const { Getter getter; return getter(*it); }
 	KeyIterator2& operator++() { ++it; return *this; }
 	bool operator==(KeyIterator2& other) const { return it == other.it; }
 	bool operator!=(KeyIterator2& other) const { return it != other.it; }
@@ -29,33 +62,50 @@ private:
 	typename StringMap<T>::const_iterator it;
 };
 
-template<typename MAP> class KeyRange
+template<typename MAP, size_t N> class KeyRange
 {
 public:
 	KeyRange(const MAP& map_)
 		: map(map_) {}
 
-	KeyIterator<MAP> begin() const { return map.begin(); }
-	KeyIterator<MAP> end()   const { return map.end();   }
+	KeyIterator<MAP, N> begin() const { return map.begin(); }
+	KeyIterator<MAP, N> end()   const { return map.end();   }
 private:
 	const MAP& map;
 };
 
-template<typename T> class KeyRange<StringMap<T>>
+template<typename T, size_t N> class KeyRange<StringMap<T>, N>
 {
 public:
 	KeyRange(const StringMap<T>& map_)
 		: map(map_) {}
 
-	KeyIterator2<T> begin() const { return map.begin(); }
-	KeyIterator2<T> end()   const { return map.end();   }
+	KeyIterator2<T, N> begin() const { return map.begin(); }
+	KeyIterator2<T, N> end()   const { return map.end();   }
 private:
 	const StringMap<T>& map;
 };
 
-template<typename MAP> KeyRange<MAP> keys(const MAP& map)
+} // namespace detail
+
+
+// Input: a collection that contains key-value pairs or tuples.
+// Output: a range that represents (only) the keys, the values or the N-th
+//         elements of the items in the input.
+
+template<typename MAP> detail::KeyRange<MAP, 0> keys(const MAP& map)
 {
-	return KeyRange<MAP>(map);
+	return detail::KeyRange<MAP, 0>(map);
+}
+
+template<typename MAP> detail::KeyRange<MAP, 1> values(const MAP& map)
+{
+	return detail::KeyRange<MAP, 1>(map);
+}
+
+template<size_t N, typename MAP> detail::KeyRange<MAP, N> elements(const MAP& map)
+{
+	return detail::KeyRange<MAP, N>(map);
 }
 
 #endif
