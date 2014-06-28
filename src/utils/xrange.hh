c1cef689bf13509aa23c93205fab01b5fd46c3c2
m9710797 <vermaelen.wouter@gmail.com>
2014-06-28 16:51:36 +0200

    Made xrange utility more STL compliant
    Found while working on a unit test for xrange: before this patch it wasn't
    possible to use std::distance() on the begin()/end() of an xrange. I doubt this
    fix has much practical value (the intended use-case for xrange already worked
    fine), but who knows.

--- src/utils/xrange.hh
+++ src/utils/xrange.hh
@@ -39,6 +39,12 @@ public:
 	class XRangeIter
 	{
 	public:
+		typedef size_t difference_type;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef std::forward_iterator_tag iterator_category;
+
 		XRangeIter(T x_)
 			: x(x_)
 		{
@@ -74,12 +80,12 @@ public:
 	{
 	}
 
-	XRangeIter begin()
+	XRangeIter begin() const
 	{
 		return XRangeIter(b);
 	}
 
-	XRangeIter end()
+	XRangeIter end() const
 	{
 		return XRangeIter(e);
 	}
