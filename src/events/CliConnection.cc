openMSX/openMSX

commit 1855d64eb47938eab40dfd62e99978fe4afa7be7

Replace CliComm libxml input parser with ad-hoc parser
The goal of this patch is to get rid of the dependency on libxml. We only
used this library (anymore) for parsing the stream of incoming commands
send over a CliComm connection (e.g. via stdin or via a socket). Catapult
and the openMSX-debugger both use this.

The input side of this connection is very simple:
  <openmsx-control>
    <command>my command</command>
    <command>another command</command>
    ...
So one opening 'openmsx-control' tag (typically there's no corresponding
closing tag because we're sending a whole stream). And each command is enclosed
in a <command> tag. There are no attributes.

For such a simple format it's relatively easy to write a custom parser. The
parser in this patch does not accept full-xml anymore. E.g. it cannot handle
attributes, it cannot handle nested tags, it ignores the opening tag, but it
can handle xml-entities like '&lt;'. See the (commented out) unittest in the
code for examples of what it can and cannot handle. I've tested that both
catapult and openMSX-debugger still work fine.

This patch changes the parser, but it does not yet update the build system
to actually remove libxml. That's for a later patch.

src/events/CliConnection.cc
@@ -67,22 +67,11 @@ private:
 
 CliConnection::CliConnection(CommandController& commandController_,
                              EventDistributor& eventDistributor_)
-	: thread(this)
+	: parser([this](const std::string& cmd) { execute(cmd); })
+	, thread(this)
 	, commandController(commandController_)
 	, eventDistributor(eventDistributor_)
 {
-	user_data.state = START;
-	user_data.unknownLevel = 0;
-	user_data.object = this;
-	memset(&sax_handler, 0, sizeof(sax_handler));
-	sax_handler.startElementNs = cb_start_element;
-	sax_handler.endElementNs   = cb_end_element;
-	sax_handler.characters     = cb_text;
-	sax_handler.initialized    = XML_SAX2_MAGIC;
-
-	parser_context = xmlCreatePushParserCtxt(
-		&sax_handler, &user_data, nullptr, 0, nullptr);
-
 	for (auto& en : updateEnabled) {
 		en = false;
 	}
@@ -93,7 +82,6 @@ CliConnection::CliConnection(CommandController& commandController_,
 CliConnection::~CliConnection()
 {
 	eventDistributor.unregisterEventListener(OPENMSX_CLICOMMAND_EVENT, *this);
-	xmlFreeParserCtxt(parser_context);
 }
 
 void CliConnection::log(CliComm::LogLevel level, string_ref message)
@@ -170,75 +158,6 @@ int CliConnection::signalEvent(const std::shared_ptr<const Event>& event)
 	return 0;
 }
 
-void CliConnection::cb_start_element(
-	void* user_data,
-	const xmlChar* localname, const xmlChar* /*prefix*/, const xmlChar* /*uri*/,
-	int /*nb_namespaces*/, const xmlChar** /*namespaces*/,
-	int /*nb_attributes*/, int /*nb_defaulted*/, const xmlChar** /*attrs*/
-	)
-{
-	auto parseState = static_cast<ParseState*>(user_data);
-	if (parseState->unknownLevel) {
-		++(parseState->unknownLevel);
-		return;
-	}
-	switch (parseState->state) {
-		case START:
-			if (strcmp(reinterpret_cast<const char*>(localname),
-					"openmsx-control") == 0) {
-				parseState->state = TAG_OPENMSX;
-			} else {
-				++(parseState->unknownLevel);
-			}
-			break;
-		case TAG_OPENMSX:
-			if (strcmp(reinterpret_cast<const char*>(localname),
-					"command") == 0) {
-				parseState->state = TAG_COMMAND;
-			} else {
-				++(parseState->unknownLevel);
-			}
-			break;
-		default:
-			++(parseState->unknownLevel);
-			break;
-	}
-	parseState->content.clear();
-}
-
-void CliConnection::cb_end_element(
-	void* user_data,
-	const xmlChar* /*localname*/, const xmlChar* /*prefix*/,
-	const xmlChar* /*uri*/
-	)
-{
-	auto parseState = static_cast<ParseState*>(user_data);
-	if (parseState->unknownLevel) {
-		--(parseState->unknownLevel);
-		return;
-	}
-	switch (parseState->state) {
-		case TAG_OPENMSX:
-			parseState->object->end();
-			parseState->state = END;
-			break;
-		case TAG_COMMAND:
-			parseState->object->execute(parseState->content);
-			parseState->state = TAG_OPENMSX;
-			break;
-		default:
-			break;
-	}
-}
-
-void CliConnection::cb_text(void* user_data, const xmlChar* chars, int len)
-{
-	auto parseState = static_cast<ParseState*>(user_data);
-	if (parseState->state == TAG_COMMAND) {
-		parseState->content.append(reinterpret_cast<const char*>(chars), len);
-	}
-}
-
 
 // class StdioConnection
 
@@ -263,7 +182,7 @@ void StdioConnection::run()
 		char buf[BUF_SIZE];
 		int n = read(STDIN_FILENO, buf, sizeof(buf));
 		if (n > 0) {
-			xmlParseChunk(parser_context, buf, n, 0);
+			parser.parse(buf, n);
 		} else if (n < 0) {
 			close();
 			break;
@@ -438,7 +357,7 @@ void SocketConnection::run()
 		char buf[BUF_SIZE];
 		int n = sock_recv(sd, buf, BUF_SIZE);
 		if (n > 0) {
-			xmlParseChunk(parser_context, buf, n, 0);
+			parser.parse(buf, n);
 		} else if (n < 0) {
 			close();
 			break;

src/events/AdhocCliCommParser.cc
@@ -1,0 +1,227 @@
+#include "AdhocCliCommParser.hh"
+#include "utf8_unchecked.hh"
+
+
+AdhocCliCommParser::AdhocCliCommParser(std::function<void(const std::string&)> callback_)
+	: callback(callback_)
+	, state(O0)
+{
+}
+
+void AdhocCliCommParser::parse(const char* buf, size_t n)
+{
+	for (size_t i = 0; i < n; ++i) parse(buf[i]);
+}
+
+void AdhocCliCommParser::parse(char c)
+{
+	// Whenever there is a parse error we return to the initial state
+	switch (state) {
+	case O0: // looking for opening tag
+		state = (c == '<') ? O1 : O0; break;
+	case O1: // matched <
+		state = (c == 'c') ? O2 : O0; break;
+	case O2: // matched <c
+		state = (c == 'o') ? O3 : O0; break;
+	case O3: // matched <co
+		state = (c == 'm') ? O4 : O0; break;
+	case O4: // matched <com
+		state = (c == 'm') ? O5 : O0; break;
+	case O5: // matched <comm
+		state = (c == 'a') ? O6 : O0; break;
+	case O6: // matched <comma
+		state = (c == 'n') ? O7 : O0; break;
+	case O7: // matched <comman
+		state = (c == 'd') ? O8 : O0; break;
+	case O8: // matched <command
+		if (c == '>') {
+			state = C0;
+			command.clear();
+		} else {
+			state = O0;
+		}
+		break;
+	case C0: // matched <command>, now parsing xml entities and </command>
+		if      (c == '<') state = C1;
+		else if (c == '&') state = A1;
+		else command += c;
+		break;
+	case C1: // matched <
+		state = (c == '/') ? C2 : O0; break;
+	case C2: // matched </
+		state = (c == 'c') ? C3 : O0; break;
+	case C3: // matched </c
+		state = (c == 'o') ? C4 : O0; break;
+	case C4: // matched </co
+		state = (c == 'm') ? C5 : O0; break;
+	case C5: // matched </com
+		state = (c == 'm') ? C6 : O0; break;
+	case C6: // matched </comm
+		state = (c == 'a') ? C7 : O0; break;
+	case C7: // matched </comma
+		state = (c == 'n') ? C8 : O0; break;
+	case C8: // matched </comman
+		state = (c == 'd') ? C9 : O0; break;
+	case C9: // matched </command
+		if (c == '>') callback(command);
+		state = O0;
+		break;
+	case A1: // matched &
+		if      (c == 'l') state = L2;
+		else if (c == 'a') state = A2;
+		else if (c == 'g') state = G2;
+		else if (c == 'q') state = Q2;
+		else if (c == '#') { state = H2; unicode = 0; }
+		else               state = O0; // error
+		break;
+	case A2: // matched &a
+		if      (c == 'm') state = A3;
+		else if (c == 'p') state = P3;
+		else               state = O0; // error
+		break;
+	case A3: // matched &am
+		state = (c == 'p') ? A4 : O0; break;
+	case A4: // matched &amp
+		if (c == ';') {
+			command += '&';
+			state = C0;
+		} else {
+			state = O0; // error
+		}
+		break;
+	case P3: // matched &ap
+		state = (c == 'o') ? P4 : O0; break;
+	case P4: // matched &apo
+		state = (c == 's') ? P5 : O0; break;
+	case P5: // matched &apos
+		if (c == ';') {
+			command += '\'';
+			state = C0;
+		} else {
+			state = O0; // error
+		}
+		break;
+	case Q2: // matched &q
+		state = (c == 'u') ? Q3 : O0; break;
+	case Q3: // matched &qu
+		state = (c == 'o') ? Q4 : O0; break;
+	case Q4: // matched &quo
+		state = (c == 't') ? Q5 : O0; break;
+	case Q5: // matched &quot
+		if (c == ';') {
+			command += '"';
+			state = C0;
+		} else {
+			state = O0; // error
+		}
+		break;
+	case G2: // matched &g
+		state = (c == 't') ? G3 : O0; break;
+	case G3: // matched &gt
+		if (c == ';') {
+			command += '>';
+			state = C0;
+		} else {
+			state = O0; // error
+		}
+		break;
+	case L2: // matched &l
+		state = (c == 't') ? L3 : O0; break;
+	case L3: // matched &lt
+		if (c == ';') {
+			command += '<';
+			state = C0;
+		} else {
+			state = O0; // error
+		}
+		break;
+	case H2: // matched &#
+		if (c == ';') {
+			utf8::unchecked::append(unicode, back_inserter(command));
+			state = C0;
+		} else {
+			unicode *= 16;
+			if      (('0' <= c) && (c <= '9')) unicode += c - '0';
+			else if (('a' <= c) && (c <= 'f')) unicode += c - 'a';
+			else if (('A' <= c) && (c <= 'F')) unicode += c - 'A';
+			else state = O0;
+		}
+		break;
+	}
+}
+
+
+
+#if 0
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void print(const T& t)
+{
+	for (auto& i : t) cout << i << std::endl;
+}
+
+void test(const string& stream, const vector<string>& expectedCommands)
+{
+	vector<string> result;
+	AdhocCliCommParser parser([&](const string& cmd) { result.push_back(cmd); });
+	parser.parse(stream.data(), stream.size());
+	if (result != expectedCommands) {
+		cout << "ERROR while parsing " << stream << endl;
+		cout << "Expected to see:" << endl;
+		print(expectedCommands);
+		cout << "but got:" << endl;
+		print(result);
+	} else {
+		cout << "ok" << endl;
+	}
+}
+
+int main()
+{
+	test("<command>foo</command>", {"foo"});
+	test("  <command>foo</command>", {"foo"});
+	test("<command>foo</command>  ", {"foo"});
+	test("  <command>foo</command>  ", {"foo"});
+
+	test("<command>foo</command><command>bar</command>", {"foo", "bar"});
+	test("<command>foo</command>  <command>bar</command>", {"foo", "bar"});
+
+	test("<command>&amp;</command>",  {"&"});
+	test("<command>&apos;</command>", {"'"});
+	test("<command>&quot;</command>", {"\""});
+	test("<command>&lt;</command>",   {"<"});
+	test("<command>&gt;</command>",   {">"});
+	test("<command>&#41;</command>",  {"A"});
+	test("<command>&lt;command&gt;</command>", {"<command>"});
+
+	test("<openmsx-control> <command>foo</command> </openmsx-control>",
+	     {"foo"});
+	test("<openmsx-control> <unknown>foo</unknown> <command>foo</command>",
+	     {"foo"});
+
+	// errors, but we do recover
+	test("<command>&unknown;</command><command>foo</command>", {"foo"});
+	test("<command>&#fffffff;</command><command>foo</command>", {"foo"});
+	test("<<<<<command>foo</command>", {"foo"});
+	test("<foo></bar><command>foo</command>", {"foo"});
+	test("</command><command>foo</command>", {"foo"});
+	test("<command>foo</foobar><command>foo</command>", {"foo"});
+
+	// This was not accepted before, but now is
+	test("<openmsx-control></openmsx-control><command>foo</command>", {"foo"});
+
+	// This was accepted before but now isn't
+	test("<command value=\"3\">foo</command>",
+	     {}); // should be "foo"
+	test("<command><bla></bla>foo</command>",
+	     {}); // should be "foo"
+	test("<command/>",
+	     {}); // sort-of OK, was before an empty command, now no command
+}
+
+#endif

src/events/AdhocCliCommParser.hh
@@ -1,0 +1,59 @@
+#ifndef ADHOCCLICOMMPARSER_HH
+#define ADHOCCLICOMMPARSER_HH
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+class AdhocCliCommParser
+{
+public:
+	AdhocCliCommParser(std::function<void(const std::string&)> callback);
+	void parse(const char* buf, size_t n);
+
+private:
+	void parse(char c);
+
+	std::function<void(const std::string&)> callback;
+	std::string command;
+	uint32_t unicode;
+	enum State {
+		O0, // no tag char matched yet
+		O1, // matched <
+		O2, //         <c
+		O3, //         <co
+		O4, //         <com
+		O5, //         <comm
+		O6, //         <comma
+		O7, //         <comman
+		O8, //         <command
+		C0, // matched <command>, now parsing xml entities and </command>
+		C1, // matched <
+		C2, //         </
+		C3, //         </c
+		C4, //         </co
+		C5, //         </com
+		C6, //         </comm
+		C7, //         </comma
+		C8, //         </comman
+		C9, //         </command
+		A1, // matched &
+		A2, //         &a
+		A3, //         &am
+		A4, //         &amp
+		P3, // matched &ap
+		P4, //         &apo
+		P5, //         &apos
+		Q2, // matched &q
+		Q3, //         &qu
+		Q4, //         &quo
+		Q5, //         &quot
+		G2, // matched &g
+		G3, //         &gt
+		L2, // matched &l
+		L3, //         &lt
+		H2, // matched &#
+	} state;
+};
+
+#endif

src/events/CliConnection.hh
@@ -7,7 +7,7 @@
 #include "EventListener.hh"
 #include "Socket.hh"
 #include "CliComm.hh"
-#include <libxml/parser.h>
+#include "AdhocCliCommParser.hh"
 #include <string>
 
 namespace openmsx {
@@ -59,7 +59,7 @@ protected:
 	  */
 	void startOutput();
 
-	xmlParserCtxt* parser_context;
+	AdhocCliCommParser parser;
 	Thread thread; // TODO: Possible to make this private?
 
 private:
@@ -72,28 +72,6 @@ private:
 
 	// EventListener
 	virtual int signalEvent(const std::shared_ptr<const Event>& event);
-
-	enum State {
-		START, TAG_OPENMSX, TAG_COMMAND, END
-	};
-	struct ParseState {
-		State state;
-		unsigned unknownLevel;
-		std::string content;
-		CliConnection* object;
-	};
-
-	static void cb_start_element(void* user_data, const xmlChar* localname,
-	                             const xmlChar* prefix, const xmlChar* uri,
-	                             int nb_namespaces, const xmlChar** namespaces,
-	                             int nb_attributes, int nb_defaulted,
-	                             const xmlChar** attrs);
-	static void cb_end_element(void* user_data, const xmlChar* localname,
-	                           const xmlChar* prefix, const xmlChar* uri);
-	static void cb_text(void* user_data, const xmlChar* chars, int len);
-
-	xmlSAXHandler sax_handler;
-	ParseState user_data;
 
 	CommandController& commandController;
 	EventDistributor& eventDistributor;

commit a35908416a4e14787e0aac9b6f78e72d902fdab9

Fix windows build (broke with 1855d64e)
Also adjust the windows specific PipeConnection code for the new parser code.

src/events/CliConnection.cc
@@ -279,7 +279,7 @@ void PipeConnection::run()
 			if (!GetOverlappedResult(pipeHandle, &overlapped, &bytesRead, TRUE)) {
 				break; // Pipe broke
 			}
-			xmlParseChunk(parser_context, buf, bytesRead, 0);
+			parser.parse(buf, bytesRead);
 		}
 		else if (wait == WAIT_OBJECT_0) {
 			break; // Shutdown