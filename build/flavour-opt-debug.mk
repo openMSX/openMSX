2a6454adc2bd777ef66505be82f98f70ea1c9a5f
m9710797 <vermaelen.wouter@gmail.com>
2014-09-29 18:03:51 +0200

    Remove -funroll-loops optimization flag
    Quote from the gcc manual:
      -funroll-loops
        Unroll loops whose number of iterations can be determined at compile time
        or upon entry to the loop.  ...[skipped some part]... This option makes
        code larger, and may or may not make it run faster.
    
    I measured that it indeed makes the code bigger (7.27MB vs 5.58MB). But I
    couldn't measure any performance difference (actually the binary built without
    -funroll-loop was in a few tests a tiny bit faster, though the difference was
    always within the measurement noise).
    
    Loop unrolling is less important than it was in the past, especially with
    modern Intel CPUs (those have special optimizations for small loop (around 20
    instr)) and with the increasing importance of memory caching (instruction cache
    in this case). I didn't measure, but I suspect that on other CPU architectures
    like ARM and MIPS the instruction cache effect is also becomming more important
    than the unrolling benefits.
    
    Loop unrolling is still important in case the loop body is very small or in
    case it allows important secondary optimizations (like auto-vectorization). I
    verified that gcc is smart enough to still unroll in those cases, even without
    the -funroll-loops flag.
    
    I remember that in the early openMSX days (10+ years ago) this option did have
    a positive effect. Today I can't measure any benefit anymore, so I'm removing
    the -funroll-loops flag.

--- build/flavour-opt-debug.mk
+++ build/flavour-opt-debug.mk
@@ -2,8 +2,7 @@
 # does not target any specific CPU.
 
 # Optimisation flags.
-CXXFLAGS+=-O3 -DNDEBUG \
-	-ffast-math -funroll-loops -g
+CXXFLAGS+=-O3 -DNDEBUG -ffast-math -g
 
 # Strip executable?
 OPENMSX_STRIP:=false
--- build/flavour-opt.mk
+++ build/flavour-opt.mk
@@ -2,8 +2,7 @@
 # does not target any specific CPU.
 
 # Optimisation flags.
-CXXFLAGS+=-O3 -DNDEBUG \
-	-ffast-math -funroll-loops
+CXXFLAGS+=-O3 -DNDEBUG -ffast-math
 
 # openMSX crashes when the win32 version is built with -fomit-frame-pointer
 # TODO: investigate and recheck when new compiler is used.
