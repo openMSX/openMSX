openMSX/openMSX

commit 259fc71b7dda6341dd68826245479919a472d3be

[13/15] map->vector in EnumSetting
Same thing. EnumSetting keeps a map from string-value to enum-value. Once
constructed this map no longer changes. So we can instead use a sorted vector.

Initializing the vectors (with constant values) currently uses more ugly syntax
than constructing the equivalent map. The new C++11 'initializer-list' feature
allows to greatly simplify this syntax (and is likely more efficient as well).
But unfortunately vc++2012 doesn't support this yet (next version will).

src/GlobalSettings.cc
@@ -35,9 +35,9 @@ GlobalSettings::GlobalSettings(GlobalCommandController& commandController_)
 		"Tcl proc called when the MSX program has set invalid PSG port directions",
 		"");
 	EnumSetting<ResampledSoundDevice::ResampleType>::Map resampleMap;
-	resampleMap["hq"]   = ResampledSoundDevice::RESAMPLE_HQ;
-	resampleMap["fast"] = ResampledSoundDevice::RESAMPLE_LQ;
-	resampleMap["blip"] = ResampledSoundDevice::RESAMPLE_BLIP;
+	resampleMap.push_back(std::make_pair("hq",   ResampledSoundDevice::RESAMPLE_HQ));
+	resampleMap.push_back(std::make_pair("fast", ResampledSoundDevice::RESAMPLE_LQ));
+	resampleMap.push_back(std::make_pair("blip", ResampledSoundDevice::RESAMPLE_BLIP));
 	resampleSetting = make_unique<EnumSetting<ResampledSoundDevice::ResampleType>>(
 		commandController, "resampler", "Resample algorithm",
 #if PLATFORM_DINGUX

src/RP5C01.cc
@@ -43,8 +43,8 @@ static std::unique_ptr<EnumSetting<RP5C01::RTCMode>> createModeSetting(
 	CommandController& commandController, const std::string& name)
 {
 	EnumSetting<RP5C01::RTCMode>::Map modeMap;
-	modeMap["EmuTime"]  = RP5C01::EMUTIME;
-	modeMap["RealTime"] = RP5C01::REALTIME;
+	modeMap.push_back(std::make_pair("EmuTime",  RP5C01::EMUTIME));
+	modeMap.push_back(std::make_pair("RealTime", RP5C01::REALTIME));
 	return make_unique<EnumSetting<RP5C01::RTCMode>>(
 		commandController,
 		((name == "Real time clock") ? "rtcmode" // bw-compat

src/Reactor.cc
@@ -385,9 +385,9 @@ void Reactor::createMachineSetting()
 	EnumSetting<int>::Map machines; // int's are unique dummy values
 	int count = 1;
 	for (auto& name : getHwConfigs("machines")) {
-		machines[name] = count++;
+		machines.push_back(std::make_pair(name, count++));
 	}
-	machines["C-BIOS_MSX2+"] = 0; // default machine
+	machines.push_back(std::make_pair("C-BIOS_MSX2+", 0)); // default machine
 
 	machineSetting = make_unique<EnumSetting<int>>(
 		*globalCommandController, "default_machine",

src/console/OSDConsoleRenderer.cc
@@ -123,15 +123,15 @@ OSDConsoleRenderer::OSDConsoleRenderer(
 
 	// placement
 	EnumSetting<Placement>::Map placeMap;
-	placeMap["topleft"]     = CP_TOPLEFT;
-	placeMap["top"]         = CP_TOP;
-	placeMap["topright"]    = CP_TOPRIGHT;
-	placeMap["left"]        = CP_LEFT;
-	placeMap["center"]      = CP_CENTER;
-	placeMap["right"]       = CP_RIGHT;
-	placeMap["bottomleft"]  = CP_BOTTOMLEFT;
-	placeMap["bottom"]      = CP_BOTTOM;
-	placeMap["bottomright"] = CP_BOTTOMRIGHT;
+	placeMap.push_back(std::make_pair("topleft",     CP_TOPLEFT));
+	placeMap.push_back(std::make_pair("top",         CP_TOP));
+	placeMap.push_back(std::make_pair("topright",    CP_TOPRIGHT));
+	placeMap.push_back(std::make_pair("left",        CP_LEFT));
+	placeMap.push_back(std::make_pair("center",      CP_CENTER));
+	placeMap.push_back(std::make_pair("right",       CP_RIGHT));
+	placeMap.push_back(std::make_pair("bottomleft",  CP_BOTTOMLEFT));
+	placeMap.push_back(std::make_pair("bottom",      CP_BOTTOM));
+	placeMap.push_back(std::make_pair("bottomright", CP_BOTTOMRIGHT));
 	consolePlacementSetting = make_unique<EnumSetting<Placement>>(
 		commandController, "consoleplacement",
 		"position of the console within the emulator",

src/fdc/DiskFactory.cc
@@ -25,16 +25,16 @@ DiskFactory::DiskFactory(Reactor& reactor_)
 	CommandController& controller = reactor.getCommandController();
 
 	EnumSetting<DirAsDSK::SyncMode>::Map syncDirAsDSKMap;
-	syncDirAsDSKMap["read_only"] = DirAsDSK::SYNC_READONLY;
-	syncDirAsDSKMap["full"] = DirAsDSK::SYNC_FULL;
+	syncDirAsDSKMap.push_back(std::make_pair("read_only", DirAsDSK::SYNC_READONLY));
+	syncDirAsDSKMap.push_back(std::make_pair("full",      DirAsDSK::SYNC_FULL));
 	syncDirAsDSKSetting = make_unique<EnumSetting<DirAsDSK::SyncMode>>(
 		controller, "DirAsDSKmode",
 		"type of syncronisation between host directory and dir-as-dsk diskimage",
 		DirAsDSK::SYNC_FULL, syncDirAsDSKMap);
 
 	EnumSetting<DirAsDSK::BootSectorType>::Map bootsectorMap;
-	bootsectorMap["DOS1"] = DirAsDSK::BOOTSECTOR_DOS1;
-	bootsectorMap["DOS2"] = DirAsDSK::BOOTSECTOR_DOS2;
+	bootsectorMap.push_back(std::make_pair("DOS1", DirAsDSK::BOOTSECTOR_DOS1));
+	bootsectorMap.push_back(std::make_pair("DOS2", DirAsDSK::BOOTSECTOR_DOS2));
 	bootSectorSetting = make_unique<EnumSetting<DirAsDSK::BootSectorType>>(
 		controller, "bootsector", "boot sector type for dir-as-dsk",
 		DirAsDSK::BOOTSECTOR_DOS2, bootsectorMap);

src/input/KeyboardSettings.cc
@@ -21,20 +21,20 @@ KeyboardSettings::KeyboardSettings(CommandController& commandController)
 		true))
 {
 	EnumSetting<Keys::KeyCode>::Map allowedKeys;
-	allowedKeys["RALT"]        = Keys::K_RALT;
-	allowedKeys["MENU"]        = Keys::K_MENU;
-	allowedKeys["RCTRL"]       = Keys::K_RCTRL;
-	allowedKeys["HENKAN_MODE"] = Keys::K_HENKAN_MODE;
-	allowedKeys["RSHIFT"]      = Keys::K_RSHIFT;
-	allowedKeys["RMETA"]       = Keys::K_RMETA;
-	allowedKeys["LMETA"]       = Keys::K_LMETA;
-	allowedKeys["LSUPER"]      = Keys::K_LSUPER;
-	allowedKeys["RSUPER"]      = Keys::K_RSUPER;
-	allowedKeys["HELP"]        = Keys::K_HELP;
-	allowedKeys["UNDO"]        = Keys::K_UNDO;
-	allowedKeys["END"]         = Keys::K_END;
-	allowedKeys["PAGEUP"]      = Keys::K_PAGEUP;
-	allowedKeys["PAGEDOWN"]    = Keys::K_PAGEDOWN;
+	allowedKeys.push_back(std::make_pair("RALT",        Keys::K_RALT));
+	allowedKeys.push_back(std::make_pair("MENU",        Keys::K_MENU));
+	allowedKeys.push_back(std::make_pair("RCTRL",       Keys::K_RCTRL));
+	allowedKeys.push_back(std::make_pair("HENKAN_MODE", Keys::K_HENKAN_MODE));
+	allowedKeys.push_back(std::make_pair("RSHIFT",      Keys::K_RSHIFT));
+	allowedKeys.push_back(std::make_pair("RMETA",       Keys::K_RMETA));
+	allowedKeys.push_back(std::make_pair("LMETA",       Keys::K_LMETA));
+	allowedKeys.push_back(std::make_pair("LSUPER",      Keys::K_LSUPER));
+	allowedKeys.push_back(std::make_pair("RSUPER",      Keys::K_RSUPER));
+	allowedKeys.push_back(std::make_pair("HELP",        Keys::K_HELP));
+	allowedKeys.push_back(std::make_pair("UNDO",        Keys::K_UNDO));
+	allowedKeys.push_back(std::make_pair("END",         Keys::K_END));
+	allowedKeys.push_back(std::make_pair("PAGEUP",      Keys::K_PAGEUP));
+	allowedKeys.push_back(std::make_pair("PAGEDOWN",    Keys::K_PAGEDOWN));
 	codeKanaHostKey = make_unique<EnumSetting<Keys::KeyCode>>(
 		commandController, "kbd_code_kana_host_key",
 		"Host key that maps to the MSX CODE/KANA key. Please note that the HENKAN_MODE key only exists on Japanese host keyboards)",
@@ -56,16 +56,16 @@ KeyboardSettings::KeyboardSettings(CommandController& commandController)
 		Keys::K_PAGEDOWN, allowedKeys);
 
 	EnumSetting<KpEnterMode>::Map kpEnterModeMap;
-	kpEnterModeMap["KEYPAD_COMMA"] = MSX_KP_COMMA;
-	kpEnterModeMap["ENTER"] = MSX_ENTER;
+	kpEnterModeMap.push_back(std::make_pair("KEYPAD_COMMA", MSX_KP_COMMA));
+	kpEnterModeMap.push_back(std::make_pair("ENTER",        MSX_ENTER));
 	kpEnterMode = make_unique<EnumSetting<KpEnterMode>>(
 		commandController, "kbd_numkeypad_enter_key",
 		"MSX key that the enter key on the host numeric keypad must map to",
 		MSX_KP_COMMA, kpEnterModeMap);
 
 	EnumSetting<MappingMode>::Map mappingModeMap;
-	mappingModeMap["KEY"] = KEY_MAPPING;
-	mappingModeMap["CHARACTER"] = CHARACTER_MAPPING;
+	mappingModeMap.push_back(std::make_pair("KEY",       KEY_MAPPING));
+	mappingModeMap.push_back(std::make_pair("CHARACTER", CHARACTER_MAPPING));
 	mappingMode = make_unique<EnumSetting<MappingMode>>(
 		commandController, "kbd_mapping_mode",
 		"Keyboard mapping mode",

src/settings/EnumSetting.hh
@@ -2,7 +2,9 @@
 #define ENUMSETTING_HH
 
 #include "Setting.hh"
-#include <map>
+#include <iterator>
+#include <utility>
+#include <vector>
 
 namespace openmsx {
 
@@ -12,7 +14,8 @@ class TclObject;
 class EnumSettingBase
 {
 protected:
-	typedef std::map<std::string, int> BaseMap;
+	// cannot be string_ref because of the 'default_machine' setting
+	typedef std::vector<std::pair<std::string, int>> BaseMap;
 	EnumSettingBase(BaseMap&& m);
 
 	int fromStringBase(string_ref str) const;
@@ -29,7 +32,7 @@ private:
 template<typename T> class EnumSetting : private EnumSettingBase, public Setting
 {
 public:
-	typedef std::map<std::string, T> Map;
+	typedef std::vector<std::pair<std::string, T>> Map;
 
 	EnumSetting(CommandController& commandController, string_ref name,
 	            string_ref description, T initialValue,
@@ -55,7 +58,8 @@ EnumSetting<T>::EnumSetting(
 		CommandController& commandController, string_ref name,
 		string_ref description, T initialValue,
 		Map& map, SaveSetting save)
-	: EnumSettingBase(BaseMap(map.begin(), map.end()))
+	: EnumSettingBase(BaseMap(std::make_move_iterator(map.begin()),
+	                          std::make_move_iterator(map.end())))
 	, Setting(commandController, name, description,
 	          toString(initialValue).str(), save)
 {

src/sound/Mixer.cc
@@ -44,18 +44,16 @@ Mixer::Mixer(Reactor& reactor_, CommandController& commandController_)
 	, muteCount(0)
 {
 	EnumSetting<SoundDriverType>::Map soundDriverMap;
-	soundDriverMap["null"]    = SND_NULL;
-	soundDriverMap["sdl"]     = SND_SDL;
+	soundDriverMap.push_back(std::make_pair("null", SND_NULL));
+	soundDriverMap.push_back(std::make_pair("sdl",  SND_SDL));
 	SoundDriverType defaultSoundDriver = SND_SDL;
-
 #ifdef _WIN32
-	soundDriverMap["directx"] = SND_DIRECTX;
+	soundDriverMap.push_back(std::make_pair("directx", SND_DIRECTX));
 	defaultSoundDriver = SND_DIRECTX;
 #endif
 #if COMPONENT_AO
-	soundDriverMap["libao"] = SND_LIBAO;
+	soundDriverMap.push_back(std::make_pair("libao", SND_LIBAO));
 #endif
-
 	soundDriverSetting = make_unique<EnumSetting<SoundDriverType>>(
 		commandController, "sound_driver",
 		"select the sound output driver",

commit 0d27f97e4f3fd9dbccf99e73c485554e6592ee18

Fix bug#539 KANA/CODE key not working in CHARACTER mode
This was a regression introduced in 259fc71b. That patch changed an internal
structure in EnumSetting from map<string,T> to a sorted vector<pair<string,T>>,
with T some enum type. This structure represents the mapping between the user
visible setting string and the internally used enum value. The (internal) base
class has an equivalent structure with 'T' replaced by 'int'. EnumSetting takes
such a structure as one of its constructor parameters. This data is actually
stored in the base class, so at some point we convert 'T' to 'int'. To avoid
copying all the string objects they are moved instead. The move-vs-copy change
was also part of patch 259fc71b.

Now the problem: the KeyboardSettings class defines 4 enum settings
(kbd_code_kana_host_key, kbd_deadkey1_host_key, kbd_deadkey2_host_key and
kbd_deadkey3_host_key). They all have the same string<->enum mapping. The code
(obviously) constructs this mapping structure only once and passes it to the 4
constructors. Though because internally the constructor moves from this
structure, the structure changes and the last 3 settings get created with an
invalid mapping (this in undefined behavior according to the c++ standard, in
practice it meant that all enum values were associated with empty strings).

So the move-vs-copy change introduced the problem, one possible fix is to
revert that. On the other hand it's a useful optimization for the vast majority
of the enum settings. An alternative fix is to pass 4 distinct copies of the
mapping structure in the KeyboardSettings code. Though that makes the
EnumSetting code very fragile, I mean this makes it very easy to use this class
in a wrong way, thus reintroduce similar problems in the future. Luckily there
exists a fix that gives us the best of both:

Before this patch, the EnumSetting class took a 'Map&' constructor parameter (a
non-const reference to this mapping structure). The fact that it's non-const

src/GlobalSettings.cc
@@ -35,10 +35,6 @@ GlobalSettings::GlobalSettings(GlobalCommandController& commandController_)
 		"invalid_psg_directions_callback",
 		"Tcl proc called when the MSX program has set invalid PSG port directions",
 		"");
-	EnumSetting<ResampledSoundDevice::ResampleType>::Map resampleMap = {
-		{ "hq",   ResampledSoundDevice::RESAMPLE_HQ },
-		{ "fast", ResampledSoundDevice::RESAMPLE_LQ },
-		{ "blip", ResampledSoundDevice::RESAMPLE_BLIP } };
 	resampleSetting = make_unique<EnumSetting<ResampledSoundDevice::ResampleType>>(
 		commandController, "resampler", "Resample algorithm",
 #if PLATFORM_DINGUX
@@ -51,7 +47,10 @@ GlobalSettings::GlobalSettings(GlobalCommandController& commandController_)
 		// For other platforms, default setting may be changed in future
 		ResampledSoundDevice::RESAMPLE_BLIP,
 #endif
-		resampleMap);
+		EnumSetting<ResampledSoundDevice::ResampleType>::Map{
+			{"hq",   ResampledSoundDevice::RESAMPLE_HQ},
+			{"fast", ResampledSoundDevice::RESAMPLE_LQ},
+			{"blip", ResampledSoundDevice::RESAMPLE_BLIP}});
 
 	for (auto i : xrange(SDL_NumJoysticks())) {
 		std::string name = "joystick" + std::to_string(i) + "_deadzone";

src/RP5C01.cc
@@ -42,14 +42,14 @@ static const nibble mask[4][13] = {
 static std::unique_ptr<EnumSetting<RP5C01::RTCMode>> createModeSetting(
 	CommandController& commandController, const std::string& name)
 {
-	EnumSetting<RP5C01::RTCMode>::Map modeMap = {
-		{ "EmuTime",  RP5C01::EMUTIME },
-		{ "RealTime", RP5C01::REALTIME } };
 	return make_unique<EnumSetting<RP5C01::RTCMode>>(
 		commandController,
 		((name == "Real time clock") ? "rtcmode" // bw-compat
 		                             : (name + " mode")),
-		"Real Time Clock mode", RP5C01::EMUTIME, modeMap);
+		"Real Time Clock mode", RP5C01::EMUTIME,
+		EnumSetting<RP5C01::RTCMode>::Map{
+			{"EmuTime",  RP5C01::EMUTIME},
+			{"RealTime", RP5C01::REALTIME}});
 }
 
 RP5C01::RP5C01(CommandController& commandController, SRAM& regs_,

src/Reactor.cc
@@ -325,7 +325,7 @@ void Reactor::createMachineSetting()
 	machineSetting = make_unique<EnumSetting<int>>(
 		*globalCommandController, "default_machine",
 		"default machine (takes effect next time openMSX is started)",
-		0, machines);
+		0, std::move(machines));
 }
 
 MSXMotherBoard* Reactor::getMotherBoard() const

src/console/OSDConsoleRenderer.cc
@@ -118,23 +118,22 @@ OSDConsoleRenderer::OSDConsoleRenderer(
 	adjustColRow();
 
 	// placement
-	EnumSetting<Placement>::Map placeMap = {
-		{ "topleft",     CP_TOPLEFT },
-		{ "top",         CP_TOP },
-		{ "topright",    CP_TOPRIGHT },
-		{ "left",        CP_LEFT },
-		{ "center",      CP_CENTER },
-		{ "right",       CP_RIGHT },
-		{ "bottomleft",  CP_BOTTOMLEFT },
-		{ "bottom",      CP_BOTTOM },
-		{ "bottomright", CP_BOTTOMRIGHT } };
 	// On Android, console must by default be placed on top, in order to prevent
 	// that it overlaps with the virtual Android keyboard, which is always placed
 	// at the bottom of the screen
 	consolePlacementSetting = make_unique<EnumSetting<Placement>>(
 		commandController, "consoleplacement",
 		"position of the console within the emulator",
-		PLATFORM_ANDROID ? CP_TOP : CP_BOTTOM, placeMap);
+		PLATFORM_ANDROID ? CP_TOP : CP_BOTTOM, EnumSetting<Placement>::Map{
+			{"topleft",     CP_TOPLEFT},
+			{"top",         CP_TOP},
+			{"topright",    CP_TOPRIGHT},
+			{"left",        CP_LEFT},
+			{"center",      CP_CENTER},
+			{"right",       CP_RIGHT},
+			{"bottomleft",  CP_BOTTOMLEFT},
+			{"bottom",      CP_BOTTOM},
+			{"bottomright", CP_BOTTOMRIGHT}});
 
 	// background (only load backgound on first paint())
 	backgroundSetting = make_unique<FilenameSetting>(commandController,

src/fdc/DiskFactory.cc
@@ -22,20 +22,18 @@ DiskFactory::DiskFactory(Reactor& reactor_)
 {
 	CommandController& controller = reactor.getCommandController();
 
-	EnumSetting<DirAsDSK::SyncMode>::Map syncDirAsDSKMap = {
-		{ "read_only", DirAsDSK::SYNC_READONLY },
-		{ "full",      DirAsDSK::SYNC_FULL } };
 	syncDirAsDSKSetting = make_unique<EnumSetting<DirAsDSK::SyncMode>>(
 		controller, "DirAsDSKmode",
 		"type of syncronisation between host directory and dir-as-dsk diskimage",
-		DirAsDSK::SYNC_FULL, syncDirAsDSKMap);
+		DirAsDSK::SYNC_FULL, EnumSetting<DirAsDSK::SyncMode>::Map{
+			{"read_only", DirAsDSK::SYNC_READONLY},
+			{"full",      DirAsDSK::SYNC_FULL}});
 
-	EnumSetting<DirAsDSK::BootSectorType>::Map bootsectorMap = {
-		{ "DOS1", DirAsDSK::BOOTSECTOR_DOS1 },
-		{ "DOS2", DirAsDSK::BOOTSECTOR_DOS2 } };
 	bootSectorSetting = make_unique<EnumSetting<DirAsDSK::BootSectorType>>(
 		controller, "bootsector", "boot sector type for dir-as-dsk",
-		DirAsDSK::BOOTSECTOR_DOS2, bootsectorMap);
+		DirAsDSK::BOOTSECTOR_DOS2, EnumSetting<DirAsDSK::BootSectorType>::Map{
+			{"DOS1", DirAsDSK::BOOTSECTOR_DOS1},
+			{"DOS2", DirAsDSK::BOOTSECTOR_DOS2}});
 }
 
 std::unique_ptr<Disk> DiskFactory::createDisk(

src/input/KeyboardSettings.cc
@@ -20,56 +20,55 @@ KeyboardSettings::KeyboardSettings(CommandController& commandController)
 		"Automatically toggle the CODE/KANA lock, based on the characters entered on the host keyboard",
 		true))
 {
-	EnumSetting<Keys::KeyCode>::Map allowedKeys = {
-		{ "RALT",        Keys::K_RALT },
-		{ "MENU",        Keys::K_MENU },
-		{ "RCTRL",       Keys::K_RCTRL },
-		{ "HENKAN_MODE", Keys::K_HENKAN_MODE },
-		{ "RSHIFT",      Keys::K_RSHIFT },
-		{ "RMETA",       Keys::K_RMETA },
-		{ "LMETA",       Keys::K_LMETA },
-		{ "LSUPER",      Keys::K_LSUPER },
-		{ "RSUPER",      Keys::K_RSUPER },
-		{ "HELP",        Keys::K_HELP },
-		{ "UNDO",        Keys::K_UNDO },
-		{ "END",         Keys::K_END },
-		{ "PAGEUP",      Keys::K_PAGEUP },
-		{ "PAGEDOWN",    Keys::K_PAGEDOWN } };
+	typedef EnumSetting<Keys::KeyCode>::Map KeyMap;
+	KeyMap allowedKeys = {
+		{"RALT",        Keys::K_RALT},
+		{"MENU",        Keys::K_MENU},
+		{"RCTRL",       Keys::K_RCTRL},
+		{"HENKAN_MODE", Keys::K_HENKAN_MODE},
+		{"RSHIFT",      Keys::K_RSHIFT},
+		{"RMETA",       Keys::K_RMETA},
+		{"LMETA",       Keys::K_LMETA},
+		{"LSUPER",      Keys::K_LSUPER},
+		{"RSUPER",      Keys::K_RSUPER},
+		{"HELP",        Keys::K_HELP},
+		{"UNDO",        Keys::K_UNDO},
+		{"END",         Keys::K_END},
+		{"PAGEUP",      Keys::K_PAGEUP},
+		{"PAGEDOWN",    Keys::K_PAGEDOWN}};
 	codeKanaHostKey = make_unique<EnumSetting<Keys::KeyCode>>(
 		commandController, "kbd_code_kana_host_key",
 		"Host key that maps to the MSX CODE/KANA key. Please note that the HENKAN_MODE key only exists on Japanese host keyboards)",
-		Keys::K_RALT, allowedKeys);
+		Keys::K_RALT, KeyMap(allowedKeys));
 
 	deadkeyHostKey[0] = make_unique<EnumSetting<Keys::KeyCode>>(
 		commandController, "kbd_deadkey1_host_key",
 		"Host key that maps to deadkey 1. Not applicable to Japanese and Korean MSX models",
-		Keys::K_RCTRL, allowedKeys);
+		Keys::K_RCTRL, KeyMap(allowedKeys));
 
 	deadkeyHostKey[1] = make_unique<EnumSetting<Keys::KeyCode>>(
 		commandController, "kbd_deadkey2_host_key",
 		"Host key that maps to deadkey 2. Only applicable to Brazilian MSX models (Sharp Hotbit and Gradiente)",
-		Keys::K_PAGEUP, allowedKeys);
+		Keys::K_PAGEUP, KeyMap(allowedKeys));
 
 	deadkeyHostKey[2] = make_unique<EnumSetting<Keys::KeyCode>>(
 		commandController, "kbd_deadkey3_host_key",
 		"Host key that maps to deadkey 3. Only applicable to Brazilian Sharp Hotbit MSX models",
-		Keys::K_PAGEDOWN, allowedKeys);
+		Keys::K_PAGEDOWN, std::move(allowedKeys));
 
-	EnumSetting<KpEnterMode>::Map kpEnterModeMap = {
-		{ "KEYPAD_COMMA", MSX_KP_COMMA },
-		{ "ENTER",        MSX_ENTER } };
 	kpEnterMode = make_unique<EnumSetting<KpEnterMode>>(
 		commandController, "kbd_numkeypad_enter_key",
 		"MSX key that the enter key on the host numeric keypad must map to",
-		MSX_KP_COMMA, kpEnterModeMap);
+		MSX_KP_COMMA, EnumSetting<KpEnterMode>::Map{
+			{"KEYPAD_COMMA", MSX_KP_COMMA},
+			{"ENTER",        MSX_ENTER}});
 
-	EnumSetting<MappingMode>::Map mappingModeMap = {
-		{ "KEY",       KEY_MAPPING },
-		{ "CHARACTER", CHARACTER_MAPPING } };
 	mappingMode = make_unique<EnumSetting<MappingMode>>(
 		commandController, "kbd_mapping_mode",
 		"Keyboard mapping mode",
-		CHARACTER_MAPPING, mappingModeMap);
+		CHARACTER_MAPPING, EnumSetting<MappingMode>::Map{
+			{"KEY",       KEY_MAPPING},
+			{"CHARACTER", CHARACTER_MAPPING}});
 }
 
 KeyboardSettings::~KeyboardSettings()

src/settings/EnumSetting.hh
@@ -36,7 +36,7 @@ public:
 
 	EnumSetting(CommandController& commandController, string_ref name,
 	            string_ref description, T initialValue,
-	            Map& map_, SaveSetting save = SAVE);
+	            Map&& map_, SaveSetting save = SAVE);
 
 	string_ref getTypeString() const override;
 	void additionalInfo(TclObject& result) const override;
@@ -57,7 +57,7 @@ template <typename T>
 EnumSetting<T>::EnumSetting(
 		CommandController& commandController, string_ref name,
 		string_ref description, T initialValue,
-		Map& map, SaveSetting save)
+		Map&& map, SaveSetting save)
 	: EnumSettingBase(BaseMap(std::make_move_iterator(begin(map)),
 	                          std::make_move_iterator(end(map))))
 	, Setting(commandController, name, description,

src/sound/Mixer.cc
@@ -54,7 +54,7 @@ Mixer::Mixer(Reactor& reactor_, CommandController& commandController_)
 	soundDriverSetting = make_unique<EnumSetting<SoundDriverType>>(
 		commandController, "sound_driver",
 		"select the sound output driver",
-		defaultSoundDriver, soundDriverMap);
+		defaultSoundDriver, std::move(soundDriverMap));
 
 	muteSetting->attach(*this);
 	frequencySetting->attach(*this);

src/video/RenderSettings.cc
@@ -18,12 +18,12 @@ namespace openmsx {
 
 RenderSettings::RenderSettings(CommandController& commandController)
 {
-	EnumSetting<Accuracy>::Map accMap = {
-		{ "screen", ACC_SCREEN },
-		{ "line",   ACC_LINE },
-		{ "pixel",  ACC_PIXEL } };
 	accuracySetting = make_unique<EnumSetting<Accuracy>>(commandController,
-		"accuracy", "rendering accuracy", ACC_PIXEL, accMap);
+		"accuracy", "rendering accuracy", ACC_PIXEL,
+		EnumSetting<Accuracy>::Map{
+			{"screen", ACC_SCREEN},
+			{"line",   ACC_LINE},
+			{"pixel",  ACC_PIXEL}});
 
 	deinterlaceSetting = make_unique<BooleanSetting>(commandController,
 		"deinterlace", "deinterlacing on/off", true);
@@ -109,7 +109,7 @@ RenderSettings::RenderSettings(CommandController& commandController)
 	}
 	scaleAlgorithmSetting = make_unique<EnumSetting<ScaleAlgorithm>>(
 		commandController, "scale_algorithm", "scale algorithm",
-		SCALER_SIMPLE, scalerMap);
+		SCALER_SIMPLE, std::move(scalerMap));
 
 	scaleFactorSetting = make_unique<IntegerSetting>(commandController,
 		"scale_factor", "scale factor",
@@ -127,31 +127,28 @@ RenderSettings::RenderSettings(CommandController& commandController)
 		"disablesprites", "disable sprite rendering",
 		false, Setting::DONT_SAVE);
 
-	EnumSetting<bool>::Map cmdMap = {
-		{ "real",   false },
-		{ "broken", true } };
 	cmdTimingSetting = make_unique<EnumSetting<bool>>(commandController,
-		"cmdtiming", "VDP command timing", false, cmdMap,
+		"cmdtiming", "VDP command timing", false,
+		EnumSetting<bool>::Map{{"real", false}, {"broken", true}},
 		Setting::DONT_SAVE);
 
-	EnumSetting<bool>::Map accessMap = {
-		{ "real",   false },
-		{ "ignore", true } };
 	tooFastAccessSetting = make_unique<EnumSetting<bool>>(commandController,
 		"too_fast_vram_access",
 		"Should too fast VDP VRAM access be correctly emulated.\n"
 		"Possible values are:\n"
 		" real -> too fast accesses are dropped\n"
 		" ignore -> access speed is ignored, all accesses are executed",
-		false, accessMap, Setting::DONT_SAVE);
+		false,
+		EnumSetting<bool>::Map{{"real", false }, {"ignore", true}},
+		Setting::DONT_SAVE);
 
-	EnumSetting<DisplayDeform>::Map deformMap = {
-		{ "normal", DEFORM_NORMAL },
-		{ "3d",     DEFORM_3D } };
 	displayDeformSetting = make_unique<EnumSetting<DisplayDeform>>(
 		commandController,
 		"display_deform", "Display deform (for the moment this only "
-		"works with the SDLGL-PP renderer", DEFORM_NORMAL, deformMap);
+		"works with the SDLGL-PP renderer", DEFORM_NORMAL,
+		EnumSetting<DisplayDeform>::Map{
+			{"normal", DEFORM_NORMAL},
+			{"3d",     DEFORM_3D}});
 
 	// Many android devices are relatively low powered. Therefore use
 	// no stretch (value 320) as default for Android because it gives

src/video/RendererFactory.cc
@@ -114,7 +114,7 @@ unique_ptr<RendererSetting> createRendererSetting(
 #endif
 	auto setting = make_unique<RendererSetting>(commandController,
 		"renderer", "rendering back-end used to display the MSX screen",
-		SDL, rendererMap);
+		SDL, std::move(rendererMap));
 
 	// Make sure the value 'none' never gets saved in settings.xml.
 	// This happened in the following scenario: