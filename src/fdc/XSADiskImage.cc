openMSX/openMSX

commit 74a1f4cfa1b0235ea9fb3d586ab54b95bc6bc800

Use SectorBuffer in read/writeSector()
The previous patch introduced SectorBuffer, this patch uses it in the various
read/writeSector() methods. This means sector data is now always transfered via
SectorBuffer instead of raw byte arrays. There are no reinterpret_casts anymore,
this is important for the next patch.

The bulk of this patch are fairly mechanical changes from byte-array/pointer to
SectorBuffer (XSADiskImage has non trivial changes).

src/fdc/XSADiskImage.cc
@@ -12,7 +12,7 @@ class XSAExtractor
 {
 public:
 	explicit XSAExtractor(File& file);
-	void getData(vector<byte>& data);
+	void getData(MemBuffer<SectorBuffer>& data);
 
 private:
 	static const int MAXSTRLEN = 254;
@@ -34,7 +34,7 @@ private:
 		int weight;
 	};
 
-	vector<byte> outBuf;	// the output buffer
+	MemBuffer<SectorBuffer> outBuf;	// the output buffer
 	const byte* inBufPos;	// pos in input buffer
 	const byte* inBufEnd;
 
@@ -58,15 +58,15 @@ XSADiskImage::XSADiskImage(Filename& filename, File& file)
 {
 	XSAExtractor extractor(file);
 	extractor.getData(data);
-	setNbSectors(data.size() / 512);
+	setNbSectors(data.size());
 }
 
-void XSADiskImage::readSectorImpl(size_t sector, byte* buf)
+void XSADiskImage::readSectorImpl(size_t sector, SectorBuffer& buf)
 {
-	memcpy(buf, &data[sector * SECTOR_SIZE], SECTOR_SIZE);
+	memcpy(&buf, &data[sector], sizeof(buf));
 }
 
-void XSADiskImage::writeSectorImpl(size_t /*sector*/, const byte* /*buf*/)
+void XSADiskImage::writeSectorImpl(size_t /*sector*/, const SectorBuffer& /*buf*/)
 {
 	throw WriteProtectedException("Write protected");
 }
@@ -99,10 +99,10 @@ XSAExtractor::XSAExtractor(File& file)
 	unLz77();
 }
 
-void XSAExtractor::getData(vector<byte>& data)
+void XSAExtractor::getData(MemBuffer<SectorBuffer>& data)
 {
 	// destroys internal outBuf, but that's ok
-	swap(data, outBuf);
+	data.swap(outBuf);
 }
 
 // Get the next character from the input buffer
@@ -122,9 +122,7 @@ void XSAExtractor::chkHeader()
 	for (int i = 0, base = 1; i < 4; ++i, base <<= 8) {
 		outBufLen += base * charIn();
 	}
-	// is only used as an optimization, it will work correctly
-	// even if this field was corrupt (intentionally or not)
-	outBuf.reserve(outBufLen);
+	outBuf.resize((outBufLen + 511) / 512);
 
 	// skip compressed length
 	inBufPos += 4;
@@ -138,6 +136,9 @@ void XSAExtractor::unLz77()
 {
 	bitCnt = 0; // no bits read yet
 
+	size_t remaining = outBuf.size() * sizeof(SectorBuffer);
+	byte* out = outBuf.data()->raw;
+	size_t outIdx = 0;
 	while (true) {
 		if (bitIn()) {
 			// 1-bit
@@ -146,16 +147,25 @@ void XSAExtractor::unLz77()
 				 return;
 			}
 			unsigned strPos = rdStrPos();
-			if ((strPos == 0) || (strPos > outBuf.size())) {
+			if ((strPos == 0) || (strPos > outIdx)) {
 				throw MSXException(
 					"Corrupt XSA image: invalid offset");
 			}
-			while (strLen--) {
-				outBuf.push_back(*(outBuf.end() - strPos));
+			if (remaining < strLen) {
+				throw MSXException(
+					"Invalid XSA image: too small output buffer");
 			}
+			remaining -= strLen;
+			memmove(&out[outIdx], &out[outIdx - strPos], strLen);
+			outIdx += strLen;
 		} else {
 			// 0-bit
-			outBuf.push_back(charIn());
+			if (remaining == 0) {
+				throw MSXException(
+					"Invalid XSA image: too small output buffer");
+			}
+			--remaining;
+			out[outIdx++] = charIn();
 		}
 	}
 }

src/fdc/DMKDiskImage.cc
@@ -149,7 +149,7 @@ void DMKDiskImage::writeTrackImpl(byte track, byte side, const RawTrack& input)
 }
 
 
-void DMKDiskImage::readSectorImpl(size_t logicalSector, byte* buf)
+void DMKDiskImage::readSectorImpl(size_t logicalSector, SectorBuffer& buf)
 {
 	byte track, side, sector;
 	logToPhys(logicalSector, track, side, sector);
@@ -162,10 +162,10 @@ void DMKDiskImage::readSectorImpl(size_t logicalSector, byte* buf)
 	}
 	// TODO should we check sector size == 512?
 	//      crc errors? correct track/head?
-	rawTrack.readBlock(sectorInfo.dataIdx, 512, buf);
+	rawTrack.readBlock(sectorInfo.dataIdx, sizeof(buf), buf.raw);
 }
 
-void DMKDiskImage::writeSectorImpl(size_t logicalSector, const byte* buf)
+void DMKDiskImage::writeSectorImpl(size_t logicalSector, const SectorBuffer& buf)
 {
 	byte track, side, sector;
 	logToPhys(logicalSector, track, side, sector);
@@ -177,7 +177,7 @@ void DMKDiskImage::writeSectorImpl(size_t logicalSector, const byte* buf)
 		throw NoSuchSectorException("Sector not found");
 	}
 	// TODO do checks? see readSectorImpl()
-	rawTrack.writeBlock(sectorInfo.dataIdx, 512, buf);
+	rawTrack.writeBlock(sectorInfo.dataIdx, sizeof(buf), buf.raw);
 
 	writeTrack(track, side, rawTrack);
 }

src/fdc/DMKDiskImage.hh
@@ -23,8 +23,8 @@ public:
 	virtual void writeTrackImpl(byte track, byte side, const RawTrack& input);
 
 	// logical sector emulation for SectorAccessibleDisk
-	virtual void readSectorImpl(size_t sector, byte* buf);
-	virtual void writeSectorImpl(size_t sector, const byte* buf);
+	virtual void readSectorImpl (size_t sector,       SectorBuffer& buf);
+	virtual void writeSectorImpl(size_t sector, const SectorBuffer& buf);
 	virtual size_t getNbSectorsImpl() const;
 	virtual bool isWriteProtectedImpl() const;
 	virtual Sha1Sum getSha1Sum();

src/fdc/DSKDiskImage.cc
@@ -7,7 +7,7 @@ DSKDiskImage::DSKDiskImage(const Filename& fileName)
 	: SectorBasedDisk(fileName)
 	, file(std::make_shared<File>(fileName, File::PRE_CACHE))
 {
-	setNbSectors(file->getSize() / SECTOR_SIZE);
+	setNbSectors(file->getSize() / sizeof(SectorBuffer));
 }
 
 DSKDiskImage::DSKDiskImage(const Filename& fileName,
@@ -15,23 +15,23 @@ DSKDiskImage::DSKDiskImage(const Filename& fileName,
 	: SectorBasedDisk(fileName)
 	, file(file_)
 {
-	setNbSectors(file->getSize() / SECTOR_SIZE);
+	setNbSectors(file->getSize() / sizeof(SectorBuffer));
 }
 
 DSKDiskImage::~DSKDiskImage()
 {
 }
 
-void DSKDiskImage::readSectorImpl(size_t sector, byte* buf)
+void DSKDiskImage::readSectorImpl(size_t sector, SectorBuffer& buf)
 {
-	file->seek(sector * SECTOR_SIZE);
-	file->read(buf, SECTOR_SIZE);
+	file->seek(sector * sizeof(buf));
+	file->read(&buf, sizeof(buf));
 }
 
-void DSKDiskImage::writeSectorImpl(size_t sector, const byte* buf)
+void DSKDiskImage::writeSectorImpl(size_t sector, const SectorBuffer& buf)
 {
-	file->seek(sector * SECTOR_SIZE);
-	file->write(buf, SECTOR_SIZE);
+	file->seek(sector * sizeof(buf));
+	file->write(&buf, sizeof(buf));
 }
 
 bool DSKDiskImage::isWriteProtectedImpl() const

src/fdc/DSKDiskImage.hh
@@ -16,8 +16,8 @@ public:
 	virtual ~DSKDiskImage();
 
 private:
-	virtual void readSectorImpl(size_t sector, byte* buf);
-	virtual void writeSectorImpl(size_t sector, const byte* buf);
+	virtual void readSectorImpl (size_t sector,       SectorBuffer& buf);
+	virtual void writeSectorImpl(size_t sector, const SectorBuffer& buf);
 	virtual bool isWriteProtectedImpl() const;
 	virtual Sha1Sum getSha1Sum();
 

src/fdc/DirAsDSK.cc
@@ -330,7 +330,7 @@ void DirAsDSK::checkCaches()
 	}
 }
 
-void DirAsDSK::readSectorImpl(size_t sector, byte* buf)
+void DirAsDSK::readSectorImpl(size_t sector, SectorBuffer& buf)
 {
 	assert(sector < NUM_SECTORS);
 
@@ -360,7 +360,7 @@ void DirAsDSK::readSectorImpl(size_t sector, byte* buf)
 	}
 
 	// Simply return the sector from our virtual disk image.
-	memcpy(buf, &sectors[sector], SECTOR_SIZE);
+	memcpy(&buf, &sectors[sector], sizeof(buf));
 }
 
 void DirAsDSK::syncWithHost()
@@ -556,7 +556,7 @@ void DirAsDSK::importHostFile(DirIndex dirIndex, FileOperations::Stat& fst)
 			for (unsigned i = 0; i < SECTORS_PER_CLUSTER; ++i) {
 				unsigned sector = logicalSector + i;
 				assert(sector < NUM_SECTORS);
-				byte* buf = sectors[sector].raw;
+				auto* buf = &sectors[sector];
 				memset(buf, 0, SECTOR_SIZE); // in case (end of) file only fills partial sector
 				auto sz = std::min(remainingSize, SECTOR_SIZE);
 				file.read(buf, sz);
@@ -823,7 +823,7 @@ DirAsDSK::DirIndex DirAsDSK::getFreeDirEntry(unsigned msxDirSector)
 	return DirIndex(sector, 0);
 }
 
-void DirAsDSK::writeSectorImpl(size_t sector_, const byte* buf)
+void DirAsDSK::writeSectorImpl(size_t sector_, const SectorBuffer& buf)
 {
 	assert(sector_ < NUM_SECTORS);
 	assert(syncMode != SYNC_READONLY);
@@ -846,7 +846,7 @@ void DirAsDSK::writeSectorImpl(size_t sector_, const byte* buf)
 	} else if (sector < FIRST_DIR_SECTOR) {
 		// Write to 2nd FAT, only buffer it. Don't interpret the data
 		// in FAT2 in any way (nor trigger any action on this write).
-		memcpy(&sectors[sector], buf, SECTOR_SIZE);
+		memcpy(&sectors[sector], &buf, sizeof(buf));
 	} else if (isDirSector(sector, dirDirIndex)) {
 		// Either root- or sub-directory.
 		writeDIRSector(sector, dirDirIndex, buf);
@@ -855,14 +855,14 @@ void DirAsDSK::writeSectorImpl(size_t sector_, const byte* buf)
 	}
 }
 
-void DirAsDSK::writeFATSector(unsigned sector, const byte* buf)
+void DirAsDSK::writeFATSector(unsigned sector, const SectorBuffer& buf)
 {
 	// Create copy of old FAT (to be able to detect changes).
 	SectorBuffer oldFAT[SECTORS_PER_FAT];
 	memcpy(&oldFAT[0], fat(), sizeof(oldFAT));
 
 	// Update current FAT with new data.
-	memcpy(&sectors[sector], buf, SECTOR_SIZE);
+	memcpy(&sectors[sector], &buf, sizeof(buf));
 
 	// Look for changes.
 	for (unsigned i = FIRST_CLUSTER; i < MAX_CLUSTER; ++i) {
@@ -1196,19 +1196,18 @@ void DirAsDSK::exportToHostFile(DirIndex dirIndex, const string& hostName)
 }
 
 void DirAsDSK::writeDIRSector(unsigned sector, DirIndex dirDirIndex,
-                              const byte* buf)
+                              const SectorBuffer& buf)
 {
 	// Look for changed directory entries.
 	for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
-		auto& newEntry = *reinterpret_cast<const MSXDirEntry*>(
-			&buf[sizeof(MSXDirEntry) * idx]);
+		auto& newEntry = buf.dirEntry[idx];
 		DirIndex dirIndex(sector, idx);
 		if (memcmp(&msxDir(dirIndex), &newEntry, sizeof(newEntry)) != 0) {
 			writeDIREntry(dirIndex, dirDirIndex, newEntry);
 		}
 	}
 	// At this point sector should be updated.
-	assert(memcmp(&sectors[sector], buf, SECTOR_SIZE) == 0);
+	assert(memcmp(&sectors[sector], &buf, sizeof(buf)) == 0);
 }
 
 void DirAsDSK::writeDIREntry(DirIndex dirIndex, DirIndex dirDirIndex,
@@ -1246,19 +1245,19 @@ void DirAsDSK::writeDIREntry(DirIndex dirIndex, DirIndex dirDirIndex,
 	exportToHost(dirIndex, dirDirIndex);
 }
 
-void DirAsDSK::writeDataSector(unsigned sector, const byte* buf)
+void DirAsDSK::writeDataSector(unsigned sector, const SectorBuffer& buf)
 {
 	assert(sector >= FIRST_DATA_SECTOR);
 	assert(sector < NUM_SECTORS);
 
 	// Buffer the write, whether the sector is mapped to a file or not.
-	memcpy(&sectors[sector], buf, SECTOR_SIZE);
+	memcpy(&sectors[sector], &buf, sizeof(buf));
 
 	// Get first cluster in the FAT chain that contains this sector.
 	unsigned cluster, offset, chainLength;
 	sectorToCluster(sector, cluster, offset);
 	unsigned startCluster = getChainStart(cluster, chainLength);
-	offset += (SECTOR_SIZE * SECTORS_PER_CLUSTER) * chainLength;
+	offset += (sizeof(buf) * SECTORS_PER_CLUSTER) * chainLength;
 
 	// Get corresponding directory entry.
 	DirIndex dirIndex = getDirEntryForCluster(startCluster);
@@ -1276,8 +1275,8 @@ void DirAsDSK::writeDataSector(unsigned sector, const byte* buf)
 		file.seek(offset);
 		unsigned msxSize = msxDir(dirIndex).size;
 		if (msxSize > offset) {
-			auto writeSize = std::min<size_t>(msxSize - offset, SECTOR_SIZE);
-			file.write(buf, writeSize);
+			auto writeSize = std::min<size_t>(msxSize - offset, sizeof(buf));
+			file.write(&buf, writeSize);
 		}
 	} catch (FileException& e) {
 		cliComm.printWarning("Couldn't write to file " + fullHostName +

src/fdc/DirAsDSK.hh
@@ -26,8 +26,8 @@ public:
 	         BootSectorType bootSectorType);
 
 	// SectorBasedDisk
-	virtual void readSectorImpl(size_t sector, byte* buf);
-	virtual void writeSectorImpl(size_t sector, const byte* buf);
+	virtual void readSectorImpl (size_t sector,       SectorBuffer& buf);
+	virtual void writeSectorImpl(size_t sector, const SectorBuffer& buf);
 	virtual bool isWriteProtectedImpl() const;
 	virtual void checkCaches();
 
@@ -57,10 +57,10 @@ private:
 	SectorBuffer* fat();
 	SectorBuffer* fat2();
 	MSXDirEntry& msxDir(DirIndex dirIndex);
-	void writeFATSector (unsigned sector, const byte* buf);
+	void writeFATSector (unsigned sector, const SectorBuffer& buf);
 	void writeDIRSector (unsigned sector, DirIndex dirDirIndex,
-	                     const byte* buf);
-	void writeDataSector(unsigned sector, const byte* buf);
+	                     const SectorBuffer& buf);
+	void writeDataSector(unsigned sector, const SectorBuffer& buf);
 	void writeDIREntry(DirIndex dirIndex, DirIndex dirDirIndex,
 	                   const MSXDirEntry& newEntry);
 	void syncWithHost();

src/fdc/Disk.cc
@@ -128,12 +128,12 @@ void Disk::detectGeometry()
 	//     ...
 
 	try {
-		byte buf[SECTOR_SIZE];
+		SectorBuffer buf;
 		readSector(0, buf); // bootsector
-		if ((buf[0] == 0xE9) || (buf[0] ==0xEB)) {
+		if ((buf.raw[0] == 0xE9) || (buf.raw[0] == 0xEB)) {
 			// use values from bootsector
-			sectorsPerTrack = buf[0x18] + 256 * buf[0x19];
-			nbSides         = buf[0x1A] + 256 * buf[0x1B];
+			sectorsPerTrack = buf.bootSector.sectorsTrack;
+			nbSides         = buf.bootSector.nrSides;
 			if ((sectorsPerTrack == 0) || (sectorsPerTrack > 255) ||
 			    (nbSides         == 0) || (nbSides         > 255)) {
 				// seems like bogus values, use defaults
@@ -141,7 +141,7 @@ void Disk::detectGeometry()
 			}
 		} else {
 			readSector(1, buf); // 1st fat sector
-			byte mediaDescriptor = buf[0];
+			byte mediaDescriptor = buf.raw[0];
 			if (mediaDescriptor >= 0xF8) {
 				sectorsPerTrack = (mediaDescriptor & 2) ? 8 : 9;
 				nbSides         = (mediaDescriptor & 1) ? 2 : 1;

commit 26d54732b498123c587f3ee7c9c45c0f6309af62

Fixed corruption in (some) XSA disk images
Regression introduced in revision 74a1f4cfa. The problem was that a manual
memory copy loop was replaced with a call to memmove() but both have different
semantics when the source and destination memory ranges overlap.

More in detail: I was aware that using memcpy() results in undefined behavior
when the source and destination buffers overlap. The manual page of memcpy()
explicitly points to memmove() for that case. Though memmove acts as-if the
source buffer is first copied to temporary storage and then copied to the
destination (practical implementations do either a forward or a backward copy).
This avoids mirroring parts of the input buffer multiple times in the
destination buffer. Though in case of a LZ77-like decompresser this mirroring
effect is exactly what you want. So neither memcpy() nor memmove() is a correct
replacement for the original loop.

Once the problem was identified the fix is easy: again do the copy using a
manual loop similar to the original one (can't be the same as the original code
because the buffer management was totally changed, and that was also the reason
why this copy routine was changed in revision 74a1f4cfa).

src/fdc/XSADiskImage.cc
@@ -156,8 +156,10 @@ void XSAExtractor::unLz77()
 					"Invalid XSA image: too small output buffer");
 			}
 			remaining -= strLen;
-			memmove(&out[outIdx], &out[outIdx - strPos], strLen);
-			outIdx += strLen;
+			while (strLen--) {
+				out[outIdx] = out[outIdx - strPos];
+				++outIdx;
+			}
 		} else {
 			// 0-bit
 			if (remaining == 0) {