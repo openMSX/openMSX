10e6d76055b6a80b5bffb6a9cbf66a67fb564d9e
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2014-11-08 22:07:33 +0100

    Back to development mode after releasing 0.11.0

--- build/version.py
+++ build/version.py
@@ -25,7 +25,7 @@ packageVersionSuffix = ''
 packageVersion = packageVersionNumber + packageVersionSuffix
 
 # Is this a release version ("True") or development version ("False").
-releaseFlag = True
+releaseFlag = False
 
 def _extractRevisionFromStdout(log, command, regex):
 	text = captureStdout(log, command)
