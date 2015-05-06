da45d6bb27b5edc0ca7a20a7e8eca532907cfcd6
Maarten ter Huurne <maarten@treewalker.org>
2015-05-06 10:35:52 +0200

    Reimplement CondVar using C++11 primitives
    This removes the dependency on SDL.

--- src/thread/CondVar.cc
+++ src/thread/CondVar.cc
@@ -1,41 +1,30 @@
 #include "CondVar.hh"
-#include <SDL.h>
+
+#include <chrono>
 
 namespace openmsx {
 
-CondVar::CondVar()
-{
-	mutex = SDL_CreateMutex();
-	cond  = SDL_CreateCond();
-}
-
-CondVar::~CondVar()
-{
-	SDL_DestroyCond(cond);
-	SDL_DestroyMutex(mutex);
-}
-
 void CondVar::wait()
 {
-	SDL_mutexP(mutex);
-	SDL_CondWait(cond, mutex);
+	std::unique_lock<std::mutex> lock(mutex);
+	condition.wait(lock);
 }
 
 bool CondVar::waitTimeout(unsigned us)
 {
-	SDL_mutexP(mutex);
-	int result = SDL_CondWaitTimeout(cond, mutex, us / 1000);
-	return result == SDL_MUTEX_TIMEDOUT;
+	std::chrono::microseconds duration(us);
+	std::unique_lock<std::mutex> lock(mutex);
+	return condition.wait_for(lock, duration) == std::cv_status::timeout;
 }
 
 void CondVar::signal()
 {
-	SDL_CondSignal(cond);
+	condition.notify_one();
 }
 
 void CondVar::signalAll()
 {
-	SDL_CondBroadcast(cond);
+	condition.notify_all();
 }
 
 } // namespace openmsx
--- src/thread/CondVar.hh
+++ src/thread/CondVar.hh
@@ -1,17 +1,14 @@
 #ifndef CONDVAR_HH
 #define CONDVAR_HH
 
-struct SDL_cond;
-struct SDL_mutex;
+#include <condition_variable>
+#include <mutex>
 
 namespace openmsx {
 
 class CondVar
 {
 public:
-	CondVar();
-	~CondVar();
-
 	/** Block till another thread signals this condition variable.
 	  */
 	void wait();
@@ -31,8 +28,8 @@ public:
 	void signalAll();
 
 private:
-	SDL_cond* cond;
-	SDL_mutex* mutex;
+	std::mutex mutex;
+	std::condition_variable condition;
 };
 
 } // namespace openmsx
