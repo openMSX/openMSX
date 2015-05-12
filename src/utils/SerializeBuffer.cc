openMSX/openMSX

commit 79aeb0df593a81392e74cb150e7f08011ae41b95

Make more use of MemBuffer
The previous patch made MemBuffer more light-weight. Actually there's no
overhead anymore compared to directly using malloc/realloc/free. So convert
(most) code that was still doing this to MemBuffer.

src/utils/SerializeBuffer.cc
@@ -10,9 +10,9 @@ namespace openmsx {
 size_t OutputBuffer::lastSize = 50000; // initial estimate
 
 OutputBuffer::OutputBuffer()
-	: begin(static_cast<byte*>(malloc(lastSize)))
-	, end(begin)
-	, finish(begin + lastSize)
+	: buf(lastSize)
+	, end(buf.data())
+	, finish(buf.data() + lastSize)
 {
 	// We've allocated a buffer with an estimated initial size. This
 	// estimate is based on the largest intermediate size of the previously
@@ -22,20 +22,12 @@ OutputBuffer::OutputBuffer()
 	// performance. If later we discover the buffer is too small, we have
 	// to reallocate (and thus make a copy). In profiling this reallocation
 	// step was noticable.
-	if (!begin) {
-		throw std::bad_alloc();
-	}
 
 	// Slowly drop the estimated required size. This makes sure that when
 	// we've overestimated the size once, we don't forever keep this too
 	// high value. For performance an overestimation is less bad than an
 	// underestimation.
 	lastSize -= lastSize >> 7;
-}
-
-OutputBuffer::~OutputBuffer()
-{
-	free(begin);
 }
 
 #ifdef __GNUC__
@@ -67,15 +59,15 @@ void OutputBuffer::insertN(const void* __restrict data, size_t len) __restrict
 	}
 }
 
-byte* OutputBuffer::release(size_t& size)
+MemBuffer<byte> OutputBuffer::release(size_t& size)
 {
-	size = end - begin;
+	size = end - buf.data();
 
 	// Deallocate unused buffer space.
-	byte* result = static_cast<byte*>(realloc(begin, size));
+	buf.resize(size);
 
-	begin = end = finish = nullptr;
-	return result;
+	end = finish = nullptr;
+	return std::move(buf);
 }
 
 void OutputBuffer::insertGrow(const void* __restrict data, size_t len) __restrict
@@ -86,16 +78,12 @@ void OutputBuffer::insertGrow(const void* __restrict data, size_t len) __restric
 
 byte* OutputBuffer::allocateGrow(size_t len) __restrict
 {
-	size_t oldSize = end - begin;
+	size_t oldSize = end - buf.data();
 	size_t newSize = std::max(oldSize + len, oldSize + oldSize / 2);
-	auto newBegin = static_cast<byte*>(realloc(begin, newSize));
-	if (!newBegin) {
-		throw std::bad_alloc();
-	}
-	begin = newBegin;
-	end = begin + oldSize + len;
-	finish = begin + newSize;
-	return begin + oldSize;
+	buf.resize(newSize);
+	end = buf.data() + oldSize + len;
+	finish = buf.data() + newSize;
+	return buf.data() + oldSize;
 }
 
 

src/utils/SerializeBuffer.hh
@@ -1,6 +1,7 @@
 #ifndef SERIALIZEBUFFER_HH
 #define SERIALIZEBUFFER_HH
 
+#include "MemBuffer.hh"
 #include "openmsx.hh"
 #include "noncopyable.hh"
 #include <algorithm>
@@ -28,10 +29,6 @@ public:
 	/** Create an empty output buffer.
 	 */
 	OutputBuffer();
-
-	/** Delete the buffer again. The data may not be used anymore.
-	  */
-	~OutputBuffer();
 
 	/** Insert data at the end of this buffer.
 	  * This will automatically grow this buffer.
@@ -64,8 +61,8 @@ public:
 	  */
 	void insertAt(size_t pos, const void* __restrict data, size_t len) __restrict
 	{
-		assert(begin + pos + len <= finish);
-		memcpy(begin + pos, data, len);
+		assert(buf.data() + pos + len <= finish);
+		memcpy(buf.data() + pos, data, len);
 	}
 
 	/** Reserve space to insert the given number of bytes.
@@ -81,7 +78,7 @@ public:
 		byte* newEnd = end + len;
 		// Make sure the next OutputBuffer will start with an initial size
 		// that can hold this much space plus some slack.
-		size_t newSize = newEnd - begin;
+		size_t newSize = newEnd - buf.data();
 		lastSize = std::max(lastSize, newSize + 1000);
 		if (newEnd <= finish) {
 			byte* result = end;
@@ -105,7 +102,7 @@ public:
 	  */
 	void deallocate(byte* pos)
 	{
-		assert(begin <= pos);
+		assert(buf.data() <= pos);
 		assert(pos <= end);
 		end = pos;
 	}
@@ -114,23 +111,23 @@ public:
 	 */
 	size_t getPosition() const
 	{
-		return end - begin;
+		return end - buf.data();
 	}
 
 	/** Release ownership of the buffer.
-	 * Returns both a pointer to the raw buffer and its size.
+	 * Returns both the buffer and its size.
 	 */
-	byte* release(size_t& size);
+	MemBuffer<byte> release(size_t& size);
 
 private:
 	void insertGrow(const void* __restrict data, size_t len) __restrict;
 	byte* allocateGrow(size_t len) __restrict;
 
-	byte* begin;   // begin of allocated memory
-	byte* end;     // points right after the last used byte
-	               // so   end - begin == size
-	byte* finish;  // points right after the last allocated byte
-	               // so   finish - begin == capacity
+	MemBuffer<byte> buf; // begin of allocated memory
+	byte* end;           // points right after the last used byte
+	                     // so   end - buf == size
+	byte* finish;        // points right after the last allocated byte
+	                     // so   finish - buf == capacity
 
 	static size_t lastSize;
 };

src/SchedulerQueue.hh
@@ -1,10 +1,13 @@
 #ifndef SCHEDULERQUEUE_HH
 #define SCHEDULERQUEUE_HH
 
+#include "MemBuffer.hh"
 #include "likely.hh"
 #include <algorithm>
 #include <cassert>
 #include <cstdlib>
+
+namespace openmsx {
 
 // This is similar to a sorted vector<T>. Though this container can have spare
 // capacity both at the front and at the end (vector only at the end). This
@@ -16,25 +19,18 @@
 template<typename T> class SchedulerQueue
 {
 public:
+	static const int CAPACITY = 32; // initial capacity
+	static const int SPARE_FRONT = 1;
 	SchedulerQueue()
+		: storage   (CAPACITY + 1) // one extra for sentinel
+		, storageEnd(storage.data() + CAPACITY)
+		, useBegin  (storage.data() + SPARE_FRONT)
+		, useEnd    (useBegin)
 	{
-		static const int CAPACITY = 32; // initial capacity
-		static const int SPARE_FRONT = 1;
-
-		// Allocate one extra to be able to store sentinel.
-		storageBegin = static_cast<T*>(malloc((CAPACITY + 1) * sizeof(T)));
-		storageEnd = storageBegin + CAPACITY;
-		useBegin   = storageBegin + SPARE_FRONT;
-		useEnd     = useBegin;
 	}
 
-	~SchedulerQueue()
-	{
-		free(storageBegin);
-	}
-
-	size_t capacity()   const { return storageEnd - storageBegin; }
-	size_t spareFront() const { return useBegin   - storageBegin; }
+	size_t capacity()   const { return storageEnd - storage.data(); }
+	size_t spareFront() const { return useBegin   - storage.data(); }
 	size_t spareBack()  const { return storageEnd - useEnd;       }
 	size_t size()  const { return useEnd -  useBegin; }
 	bool   empty() const { return useEnd == useBegin; }
@@ -66,7 +62,7 @@ public:
 		while (!less(t, *it)) ++it;
 
 		if ((it - useBegin) <= (useEnd - it)) {
-			if (likely(useBegin != storageBegin)) {
+			if (likely(useBegin != storage.data())) {
 				insertFront(it, t);
 			} else if (useEnd != storageEnd) {
 				insertBack(it, t);
@@ -76,7 +72,7 @@ public:
 		} else {
 			if (likely(useEnd != storageEnd)) {
 				insertBack(it, t);
-			} else if (useBegin != storageBegin) {
+			} else if (useBegin != storage.data()) {
 				insertFront(it, t);
 			} else {
 				insertRealloc(it, t);
@@ -131,29 +127,29 @@ private:
 	{
 		static const int SPARE_FRONT = 1;
 
-		size_t oldSize = storageEnd - storageBegin;
+		size_t oldSize = storageEnd - storage.data();
 		size_t newSize = oldSize * 2;
 
-		// Allocate one extra to be able to store sentinel.
-		T* newStorage = static_cast<T*>(malloc((newSize + 1) * sizeof(T)));
-		T* newUseBegin = newStorage + SPARE_FRONT;
+		MemBuffer<T> newStorage(newSize + 1); // one extra for sentinel
+		T* newUseBegin = newStorage.data() + SPARE_FRONT;
 		std::copy(useBegin, it, newUseBegin);
 		*(newUseBegin + (it - useBegin)) = t;
 		std::copy(it, useEnd, newUseBegin + (it - useBegin) + 1);
-		free(storageBegin);
 
-		storageBegin = newStorage;
-		storageEnd   = newStorage + newSize;
-		useBegin     = newUseBegin;
-		useEnd       = useBegin + oldSize + 1;
+		storage    = std::move(newStorage);
+		storageEnd = storage.data() + newSize;
+		useBegin   = newUseBegin;
+		useEnd     = useBegin + oldSize + 1;
 	}
 
 private:
-	// Invariant: storageBegin <= useBegin <= useEnd <= storageEnd
-	T* storageBegin;
+	// Invariant: storage.data() <= useBegin <= useEnd <= storageEnd
+	MemBuffer<T> storage;
 	T* storageEnd;
 	T* useBegin;
 	T* useEnd;
 };
+
+} // namespace openmsx
 
 #endif // SCHEDULERQUEUE_HH

src/serial/Midi_w32.cc
@@ -28,6 +28,7 @@
 #ifdef _WIN32
 
 #include "MSXException.hh"
+#include "MemBuffer.hh"
 #include "StringOp.hh"
 #include "cstdiop.hh"
 
@@ -67,11 +68,12 @@ struct outbuf {
 	MIDIHDR  header;
 };
 
-static struct vfn_midi *vfnt_midiout, *vfnt_midiin;
+static MemBuffer<vfn_midi> vfnt_midiout;
+static MemBuffer<vfn_midi> vfnt_midiin;
 static unsigned vfnt_midiout_num, vfnt_midiin_num;
 
-static int *state_out;
-static struct outbuf *buf_out;
+static MemBuffer<int> state_out;
+static MemBuffer<outbuf> buf_out;
 
 static MIDIHDR inhdr;
 static char inlongmes[OPENMSX_W32_MIDI_SYSMES_MAXLEN];
@@ -112,22 +114,15 @@ int w32_midiOutInit()
 {
 	vfnt_midiout_num = 0;
 	unsigned num = midiOutGetNumDevs();
-	if (!num) {
-		return 0;
-	}
-	if ((state_out = static_cast<int*>(malloc((num + 1) * sizeof(int)))) == nullptr) {
-		return 1;
-	}
-	memset(state_out, 0, (num + 1) * sizeof(int));
+	if (!num) return 0;
 
-	if ((buf_out = static_cast<outbuf*>(malloc((num + 1) * sizeof(struct outbuf)))) == nullptr) {
-		return 1;
-	}
-	memset(buf_out, 0, (num + 1) * sizeof(struct outbuf));
+	state_out.resize(num + 1);
+	memset(state_out.data(), 0, (num + 1) * sizeof(int));
 
-	if ((vfnt_midiout = static_cast<vfn_midi*>(malloc((num + 1) * sizeof(struct vfn_midi)))) == nullptr) {
-		return 1;
-	}
+	buf_out.resize(num + 1);
+	memset(buf_out.data(), 0, (num + 1) * sizeof(outbuf));
+
+	vfnt_midiout.resize(num + 1);
 
 	// MIDI_MAPPER is #define's as ((UINT)-1)
 	UINT OPENMSX_MIDI_MAPPER = static_cast<UINT>(-1);
@@ -152,13 +147,9 @@ int w32_midiOutInit()
 	return 0;
 }
 
-int w32_midiOutClean()
+void w32_midiOutClean()
 {
 	vfnt_midiout_num = 0;
-	free(vfnt_midiout);
-	free(buf_out);
-	free(state_out);
-	return 0;
 }
 
 
@@ -333,12 +324,9 @@ int w32_midiInInit()
 {
 	vfnt_midiin_num = 0;
 	unsigned num = midiInGetNumDevs();
-	if (!num) {
-		return 0;
-	}
-	if ((vfnt_midiin = static_cast<vfn_midi*>(malloc((num + 1) * sizeof(struct vfn_midi)))) == nullptr) {
-		return	1;
-	}
+	if (!num) return 0;
+
+	vfnt_midiin.resize(num + 1);
 	for (unsigned i = 0; i < num; ++i) {
 		MIDIINCAPSA cap;
 		if (midiInGetDevCapsA(i, &cap, sizeof(cap)) != MMSYSERR_NOERROR) {
@@ -352,11 +340,9 @@ int w32_midiInInit()
 	return 0;
 }
 
-int w32_midiInClean()
+void w32_midiInClean()
 {
 	vfnt_midiin_num = 0;
-	free(vfnt_midiout);
-	return 0;
 }
 
 

src/serial/Midi_w32.hh
@@ -37,7 +37,7 @@
 namespace openmsx {
 
 extern int w32_midiOutInit();
-extern int w32_midiOutClean();
+extern void w32_midiOutClean();
 extern unsigned w32_midiOutGetVFNsNum();
 extern std::string w32_midiOutGetVFN(unsigned nmb);
 extern std::string w32_midiOutGetRDN(unsigned nmb);
@@ -46,7 +46,7 @@ extern int w32_midiOutClose(unsigned idx);
 extern int w32_midiOutPut(unsigned char value, unsigned idx);
 
 extern int w32_midiInInit();
-extern int w32_midiInClean();
+extern void w32_midiInClean();
 extern unsigned w32_midiInGetVFNsNum();
 extern std::string w32_midiInGetVFN(unsigned nmb);
 extern std::string w32_midiInGetRDN(unsigned nmb);

src/serialize.cc
@@ -176,8 +176,7 @@ void MemOutputArchive::save(const std::string& s)
 
 MemBuffer<byte> MemOutputArchive::releaseBuffer(size_t& size)
 {
-	byte* data = buffer.release(size);
-	return MemBuffer<byte>(data, size);
+	return buffer.release(size);
 }
 
 ////

src/sound/ResampleHQ.cc
@@ -12,7 +12,7 @@
 #include "ResampleHQ.hh"
 #include "ResampledSoundDevice.hh"
 #include "FixedPoint.hh"
-#include "MemoryOps.hh"
+#include "MemBuffer.hh"
 #include "countof.hh"
 #include "likely.hh"
 #include "noncopyable.hh"
@@ -51,16 +51,17 @@ public:
 
 private:
 	using FilterIndex = FixedPoint<16>;
+	using Table = MemBuffer<float, SSE2_ALIGNMENT>;
 
 	ResampleCoeffs();
 	~ResampleCoeffs();
 
 	double getCoeff(FilterIndex index);
-	void calcTable(double ratio, float*& table, unsigned& filterLen);
+	Table calcTable(double ratio, unsigned& filterLen);
 
 	struct Element {
 		double ratio;
-		float* table;
+		Table table;
 		unsigned filterLen;
 		unsigned count;
 	};
@@ -88,13 +89,14 @@ void ResampleCoeffs::getCoeffs(
 	auto it = find_if(begin(cache), end(cache),
 		[=](const Element& e) { return e.ratio == ratio; });
 	if (it != end(cache)) {
-		table     = it->table;
+		table     = it->table.data();
 		filterLen = it->filterLen;
 		it->count++;
 		return;
 	}
-	calcTable(ratio, table, filterLen);
-	cache.push_back({ratio, table, filterLen, 1});
+	Table tab = calcTable(ratio, filterLen);
+	table = tab.data();
+	cache.push_back({ratio, std::move(tab), filterLen, 1});
 }
 
 void ResampleCoeffs::releaseCoeffs(double ratio)
@@ -103,8 +105,9 @@ void ResampleCoeffs::releaseCoeffs(double ratio)
 		[=](const Element& e) { return e.ratio == ratio; });
 	it->count--;
 	if (it->count == 0) {
-		MemoryOps::freeAligned(it->table);
-		*it = cache.back(); // move last element here
+		if (it != (end(cache) - 1)) {
+			*it = std::move(cache.back()); // move last element here
+		}
 		cache.pop_back();   // and erase last
 	}
 }
@@ -117,8 +120,7 @@ double ResampleCoeffs::getCoeff(FilterIndex index)
 	       fraction * (double(coeffs[indx + 1]) - double(coeffs[indx]));
 }
 
-void ResampleCoeffs::calcTable(
-	double ratio, float*& table, unsigned& filterLen)
+ResampleCoeffs::Table ResampleCoeffs::calcTable(double ratio, unsigned& filterLen)
 {
 	double floatIncr = (ratio > 1.0) ? INDEX_INC / ratio : INDEX_INC;
 	double normFactor = floatIncr / INDEX_INC;
@@ -130,9 +132,8 @@ void ResampleCoeffs::calcTable(
 	int idx_cnt = max_idx - min_idx + 1;
 	filterLen = (idx_cnt + 3) & ~3; // round up to multiple of 4
 	min_idx -= (filterLen - idx_cnt);
-	table = static_cast<float*>(MemoryOps::mallocAligned(
-		16, TAB_LEN * filterLen * sizeof(float)));
-	memset(table, 0, TAB_LEN * filterLen * sizeof(float));
+	Table table(TAB_LEN * filterLen);
+	memset(table.data(), 0, TAB_LEN * filterLen * sizeof(float));
 
 	for (unsigned t = 0; t < TAB_LEN; ++t) {
 		double lastPos = double(t) / TAB_LEN;
@@ -160,6 +161,7 @@ void ResampleCoeffs::calcTable(
 			bufIndex -= 1;
 		} while (filterIndex > FilterIndex(0));
 	}
+	return table;
 }
 
 

src/utils/MemBuffer.hh
@@ -58,20 +58,6 @@ public:
 	{
 	}
 
-	/** Take ownership of the given memory block. This pointer should have
-	 * been allocated earlier with malloc() or realloc() (or it should be
-	 * nullptr).
-	  */
-	MemBuffer(T* data, size_t size)
-		: dat(data)
-#ifdef DEBUG
-		, sz(size)
-#endif
-	{
-		(void)size;
-		assert(SIMPLE_MALLOC);
-	}
-
 	/** Move constructor. */
 	MemBuffer(MemBuffer&& other)
 		: dat(other.dat)

commit cd3c4f57ed129af2b962319c1331c28d601a9b13

Don't mark this-pointer restrict in OutputBuffer
Remove the 'restrict this-pointer' annotations from the OutputBuffer class.
This fixes compilation errors in clang. The regression was likely introduced by
revision 79aeb0d (but I didn't verify).

Gcc (and vs) seem to behave slightly different compared to clang when marking
the 'this' pointer restrict. Maybe not so surprising because this is not part
of the c++ standard. Strictly speaking the whole restrict feature is not
covered by the c++(-11) standard, but it is a widely supported extension (by
gcc, visual-studio, clang and probably many other c++ compilers).

Removing these annotations only seems to have a very small negative impact on
the quality of the generated code (I verified using gcc-4.9). In the past I
remember seeing a bigger effect. Possibly the alias analysis passes in gcc have
gotten more powerful since that time. Anyway because of this I didn't bother
trying to keep these annotations for gcc and visual-studio.

src/utils/SerializeBuffer.cc
@@ -31,7 +31,7 @@ OutputBuffer::OutputBuffer()
 }
 
 #ifdef __GNUC__
-template<size_t LEN> void OutputBuffer::insertN(const void* __restrict data) __restrict
+template<size_t LEN> void OutputBuffer::insertN(const void* __restrict data)
 {
 	byte* newEnd = end + LEN;
 	if (likely(newEnd <= finish)) {
@@ -42,13 +42,13 @@ template<size_t LEN> void OutputBuffer::insertN(const void* __restrict data) __r
 	}
 }
 // Force template instantiation
-template void OutputBuffer::insertN<1>(const void* __restrict data) __restrict;
-template void OutputBuffer::insertN<2>(const void* __restrict data) __restrict;
-template void OutputBuffer::insertN<4>(const void* __restrict data) __restrict;
-template void OutputBuffer::insertN<8>(const void* __restrict data) __restrict;
+template void OutputBuffer::insertN<1>(const void* __restrict data);
+template void OutputBuffer::insertN<2>(const void* __restrict data);
+template void OutputBuffer::insertN<4>(const void* __restrict data);
+template void OutputBuffer::insertN<8>(const void* __restrict data);
 #endif
 
-void OutputBuffer::insertN(const void* __restrict data, size_t len) __restrict
+void OutputBuffer::insertN(const void* __restrict data, size_t len)
 {
 	byte* newEnd = end + len;
 	if (likely(newEnd <= finish)) {
@@ -70,13 +70,13 @@ MemBuffer<byte> OutputBuffer::release(size_t& size)
 	return std::move(buf);
 }
 
-void OutputBuffer::insertGrow(const void* __restrict data, size_t len) __restrict
+void OutputBuffer::insertGrow(const void* __restrict data, size_t len)
 {
 	byte* pos = allocateGrow(len);
 	memcpy(pos, data, len);
 }
 
-byte* OutputBuffer::allocateGrow(size_t len) __restrict
+byte* OutputBuffer::allocateGrow(size_t len)
 {
 	size_t oldSize = end - buf.data();
 	size_t newSize = std::max(oldSize + len, oldSize + oldSize / 2);

src/utils/SerializeBuffer.hh
@@ -33,7 +33,7 @@ public:
 	/** Insert data at the end of this buffer.
 	  * This will automatically grow this buffer.
 	  */
-	void insert(const void* __restrict data, size_t len) __restrict
+	void insert(const void* __restrict data, size_t len)
 	{
 #ifdef __GNUC__
 		if (__builtin_constant_p(len)) {
@@ -51,15 +51,15 @@ public:
 		insertN(data, len);
 	}
 #ifdef __GNUC__
-	template<size_t N> void insertN(const void* __restrict data) __restrict;
+	template<size_t N> void insertN(const void* __restrict data);
 #endif
-	void insertN(const void* __restrict data, size_t len) __restrict;
+	void insertN(const void* __restrict data, size_t len);
 
 	/** Insert data at a given position. This will overwrite the old data.
 	  * It's not possible to grow the buffer via this method (so the buffer
 	  * must already be big enough to hold the new data).
 	  */
-	void insertAt(size_t pos, const void* __restrict data, size_t len) __restrict
+	void insertAt(size_t pos, const void* __restrict data, size_t len)
 	{
 		assert(buf.data() + pos + len <= finish);
 		memcpy(buf.data() + pos, data, len);
@@ -120,8 +120,8 @@ public:
 	MemBuffer<byte> release(size_t& size);
 
 private:
-	void insertGrow(const void* __restrict data, size_t len) __restrict;
-	byte* allocateGrow(size_t len) __restrict;
+	void insertGrow(const void* __restrict data, size_t len);
+	byte* allocateGrow(size_t len);
 
 	MemBuffer<byte> buf; // begin of allocated memory
 	byte* end;           // points right after the last used byte