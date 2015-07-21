045f2209a46f42f1e48160d7ebd6d7a9852d61eb
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2015-07-21 21:38:06 +0200

    Initial version of MC6850 dev

--- src/serial/MC6850.cc
+++ src/serial/MC6850.cc
@@ -1,61 +1,259 @@
 #include "MC6850.hh"
+#include "MidiInDevice.hh"
+#include "MSXMotherBoard.hh"
 #include "EmuTime.hh"
 #include "serialize.hh"
 #include "unreachable.hh"
 
+#include <iostream>
+
 namespace openmsx {
 
-// Dummy implementation, just enough to make some Music-Module detection
-// routines pass:
+// MSX interface:
+// port R/W
+// 0    W    Control Register
+// 1    W    Transmit Data Register
+// 4    R    Status Register
+// 5    R    Receive Data Register
+
+// control register bits
+static const unsigned CR_CDS1 = 0x01; // Counter Divide Select 1
+static const unsigned CR_CDS2 = 0x02; // Counter Divide Select 2
+static const unsigned CR_MR   = 0x03; // Master Reset
+// CDS2 CDS1
+// 0    0     divide by 1
+// 0    1     divide by 16
+// 1    0     divide by 64
+// 1    1     master reset (!)
+
+static const unsigned CR_WS1  = 0x04; // Word Select 1 (mostly parity)
+static const unsigned CR_WS2  = 0x08; // Word Select 2 (mostly nof stop bits)
+static const unsigned CR_WS3  = 0x10; // Word Select 3: 7/8 bits
+// WS3 WS2 WS1
+// 0   0   0   7 bits - 2 stop bits - Even parity
+// 0   0   1   7 bits - 2 stop bits - Odd  parity
+// 0   1   0   7 bits - 1 stop bit  - Even parity
+// 0   1   1   7 bits - 1 stop bit  - Odd  Parity
+// 1   0   0   8 bits - 2 stop bits - No   Parity
+// 1   0   1   8 bits - 1 stop bit  - No   Parity
+// 1   1   0   8 bits - 1 stop bit  - Even parity
+// 1   1   1   8 bits - 1 stop bit  - Odd  parity
+
+
+static const unsigned CR_TC1  = 0x20; // Transmit Control 1
+static const unsigned CR_TC2  = 0x40; // Transmit Control 2
+// TC2 TC1
+// 0   0   /RTS low,  Transmitting Interrupt disabled
+// 0   1   /RTS low,  Transmitting Interrupt enabled
+// 1   0   /RTS high, Transmitting Interrupt disabled
+// 1   1   /RTS low,  Transmits a Break level on the Transmit Data Output.
+//                                 Interrupt disabled
+static const unsigned CR_RIE  = 0x80; // Receive Interrupt Enable: interrupt
+// at Receive Data Register Full, Overrun, low-to-high transition on the Data
+// Carrier Detect (/DCD) signal line
+
+// status register bits
+static const unsigned STAT_RDRF = 0x01; // Receive Data Register Full
+static const unsigned STAT_TDRE = 0x02; // Transmit Data Register Empty
+static const unsigned STAT_DCD  = 0x04; // Data Carrier Detect (/DCD)
+static const unsigned STAT_CTS  = 0x08; // Clear-to-Send (/CTS)
+static const unsigned STAT_FE   = 0x10; // Framing Error
+static const unsigned STAT_OVRN = 0x20; // Receiver Overrun
+static const unsigned STAT_PE   = 0x40; // Parity Error
+static const unsigned STAT_IRQ  = 0x80; // Interrupt Request (/IRQ)
+
+
+static const EmuDuration BIT_DURATION = EmuDuration::hz(31250);
+// fixed for now:
+static const EmuDuration CHAR_DURATION = BIT_DURATION * 10; // 1 start-bit, 8 data-bits, 1 stop-bit
+
+// Some existing Music-Module detection routines:
 // - fac demo 5: does OUT 0,3 : OUT 0,21 : INP(4) and expects to read 2
 // - tetris 2 special edition: does INP(4) and expects to read 0
+// - Synthesix: does INP(4), expects 0; OUT 0,3 : OUT 0,21: INP(4) and expects
+//   bit 1 to be 1 and bit 2, 3 and 7 to be 0. Then does OUT 5,0xFE : INP(4)
+//   and expects bit 1 to be 0.
 // I did some _very_basic_ investigation and found the following:
 // - after a reset reading from port 4 returns 0
 // - after initializing the control register, reading port 4 returns 0 (of
 //   course this will change when you start to actually receive/transmit data)
 // - writing any value with the lower 2 bits set to 1 returns to the initial
 //   state, and reading port 4 again returns 0.
+// -  ?INP(4) : OUT0,3 : ?INP(4) : OUT0,21 : ? INP(4) : OUT0,3 :  ?INP(4)
+//    outputs: 0, 0, 2, 0
 
 MC6850::MC6850(const DeviceConfig& config)
 	: MSXDevice(config)
+	, MidiInConnector(getMotherBoard().getPluggingController(), MSXDevice::getName() + "-in")
+	, syncRecv (getMotherBoard().getScheduler())
+	, syncTrans(getMotherBoard().getScheduler())
+	, rxIRQ(getMotherBoard(), MSXDevice::getName() + "-rx-IRQ")
+	, txIRQ(getMotherBoard(), MSXDevice::getName() + "-tx-IRQ")
+	, transmitDataReg(0), transmitShiftReg(0) // avoid UMR
+	, outConnector(getMotherBoard().getPluggingController(), MSXDevice::getName() + "-out")
 {
 	reset(EmuTime::dummy());
 }
 
 void MC6850::reset(EmuTime::param /*time*/)
 {
-	control = 3;
+	std::cerr << "MC6850 reset" << std::endl;
+	syncRecv .removeSyncPoint();
+	syncTrans.removeSyncPoint();
+	rxIRQ.reset();
+	txIRQ.reset();
+	rxReady = false;
+	controlReg = CR_MR;
+	statusReg = 0;
+	receiveDataReg = 0;
 }
 
-byte MC6850::readIO(word port, EmuTime::param time)
+byte MC6850::readIO(word port, EmuTime::param /*time*/)
 {
-	return peekIO(port, time);
+	switch (port & 0x1) {
+	case 0:{// read Status Register
+		byte regVal = statusReg;
+		if (rxIRQ.getState() || txIRQ.getState()) regVal |= STAT_IRQ;
+		std::cerr << "MC6850 reading statusReg: 0x" << std::hex << int(regVal) << std::endl;
+		return regVal; }
+	case 1: // read Receive Data Register
+		std::cerr << "MC6850 reading receiveDataReg: 0x" << std::hex << int(receiveDataReg) << std::endl;
+		statusReg &= ~STAT_RDRF;
+		rxIRQ.reset();
+		return receiveDataReg;
+	}
+	UNREACHABLE;
+	return 0xFF;
 }
 
 byte MC6850::peekIO(word port, EmuTime::param /*time*/) const
 {
-	byte result;
 	switch (port & 0x1) {
-	case 0: // read status
-		result = ((control & 3) == 3) ? 0 : 2;
-		break;
-	case 1: // read data
-		result = 0;
-		break;
-	default: // unreachable, avoid warning
-		UNREACHABLE; result = 0;
+	case 0: // read Status Register
+		return statusReg;
+	case 1: // read Receive Data Register
+		return receiveDataReg;
 	}
-	return result;
+	UNREACHABLE;
+	return 0xFF;
 }
 
-void MC6850::writeIO(word port, byte value, EmuTime::param /*time*/)
+void MC6850::writeIO(word port, byte value, EmuTime::param time)
 {
 	switch (port & 0x01) {
-	case 0: // control
-		control = value;
+	case 0: // write Control Register
+		std::cerr << "MC6850 writing controlReg: 0x" << std::hex << int(value) << std::endl;
+		if ((value & 3) == CR_MR) {
+			reset(time);
+		} else {
+			if ((controlReg & 3) == CR_MR) {
+				// we got out of MR state
+				rxReady = true;
+				statusReg |= STAT_TDRE;
+			}
+		}
+		controlReg = value;
+		// update IRQ status
+		rxIRQ.set(( value & CR_RIE) && (statusReg & STAT_RDRF));
+		txIRQ.set(((value & (CR_TC1 | CR_TC2)) == 0x20) && (statusReg & STAT_TDRE));
 		break;
-	case 1: // write data
+	case 1: // write Transmit Data Register
+		std::cerr << "MC6850 writing TDR: 0x" << std::hex << int(value) << std::endl;
+		if (syncTrans.pendingSyncPoint()) {
+			// We're still sending the previous character, only
+			// buffer this one. Don't accept any further characters
+			// TODO: this is WRONG. TDRE should go low immediately
+			transmitDataReg = value;
+			statusReg &= ~STAT_TDRE;
+			txIRQ.reset();
+		} else {
+			// Immediately start sending this character. We're
+			// still ready to accept a next character
+			send(value, time);
+		}
 		break;
+	}
+}
+
+// Start sending a character. It takes a while before it's finished sending.
+void MC6850::send(byte value, EmuTime::param time)
+{
+	transmitShiftReg = value;
+	syncTrans.setSyncPoint(time + CHAR_DURATION);
+}
+
+// MidiInConnector sends a new character.
+void MC6850::recvByte(byte value, EmuTime::param time)
+{
+	assert(acceptsData() && ready());
+	std::cerr << "MC6850 receiving MIDI in data: 0x" << std::hex << int(value) << std::endl;
+
+	if (statusReg & STAT_RDRF) {
+		// So, there is a byte that has to be read by the MSX still!
+		// This happens when the MSX program doesn't
+		// respond fast enough to an earlier received byte.
+		statusReg |= STAT_OVRN;
+	} else {
+		receiveDataReg = value;
+		statusReg |= STAT_RDRF;
+	}
+	// both for OVRN and RDRF an IRQ is raised
+	if (controlReg & CR_RIE) rxIRQ.set();
+
+	// Not ready now, but we will be in a while
+	rxReady = false;
+	syncRecv.setSyncPoint(time + CHAR_DURATION);
+}
+
+// Triggered when we're ready to receive the next character.
+void MC6850::execRecv(EmuTime::param time)
+{
+	assert((controlReg & 3) != CR_MR);
+	assert(!rxReady);
+	rxReady = true;
+	getPluggedMidiInDev().signal(time); // trigger (possible) send of next char
+}
+
+// MidiInDevice querries whether it can send a new character 'now'.
+bool MC6850::ready()
+{
+	return rxReady;
+}
+
+// MidiInDevice queries whether it can send characters at all.
+bool MC6850::acceptsData()
+{
+	return (controlReg & 3) != CR_MR;
+}
+
+// MidiInDevice informs us about the format of the data it will send
+// (MIDI is always 1 start-bit, 8 data-bits, 1 stop-bit, no parity-bits).
+void  MC6850::setDataBits(DataBits /*bits*/)
+{
+	// ignore
+}
+void MC6850::setStopBits(StopBits /*bits*/)
+{
+	// ignore
+}
+void MC6850::setParityBit(bool /*enable*/, ParityBit /*parity*/)
+{
+	// ignore
+}
+
+// Triggered when a character has finished sending.
+void MC6850::execTrans(EmuTime::param time)
+{
+	outConnector.recvByte(transmitShiftReg, time);
+
+	if (statusReg & STAT_TDRE) {
+		// no next character to send
+	} else {
+		// There already is a next character, start sending that now
+		// and accept a next one
+		statusReg |= STAT_TDRE;
+		if (((controlReg & (CR_TC1 | CR_TC2)) == 0x20)) txIRQ.set();
+		send(transmitDataReg, time);
 	}
 }
 
@@ -65,10 +263,27 @@ template<typename Archive>
 void MC6850::serialize(Archive& ar, unsigned version)
 {
 	ar.template serializeBase<MSXDevice>(*this);
-	if (ar.versionAtLeast(version, 2)) {
-		ar.serialize("control", control);
+	if (ar.versionAtLeast(version, 3)) {
+		ar.template serializeBase<MidiInConnector>(*this);
+		ar.serialize("outConnector", outConnector);
+
+		ar.serialize("syncRecv",  syncRecv);
+		ar.serialize("syncTrans", syncTrans);
+
+		ar.serialize("rxIRQ", rxIRQ);
+		ar.serialize("txIRQ", txIRQ);
+
+		ar.serialize("rxReady",    rxReady);
+		ar.serialize("receiveDataReg", receiveDataReg);
+		ar.serialize("transmitDataReg", transmitDataReg);
+		ar.serialize("transmitShiftReg", transmitShiftReg);
+
+		ar.serialize("statusReg",  statusReg);
+		ar.serialize("controlReg", controlReg);
+	} else if (ar.versionAtLeast(version, 2)) {
+		ar.serialize("control", controlReg);
 	} else {
-		control = 3;
+		controlReg = 3;
 	}
 }
 INSTANTIATE_SERIALIZE_METHODS(MC6850);
--- src/serial/MC6850.hh
+++ src/serial/MC6850.hh
@@ -2,14 +2,24 @@
 #define MC6850_HH
 
 #include "MSXDevice.hh"
+#include "IRQHelper.hh"
+#include "MidiInConnector.hh"
+#include "MidiOutConnector.hh"
+#include "Schedulable.hh"
+#include "openmsx.hh"
+#include "outer.hh"
+
+class MSXMotherBoard;
+class Scheduler;
 
 namespace openmsx {
 
-class MC6850 final : public MSXDevice
+class MC6850 final : public MSXDevice, public MidiInConnector
 {
 public:
 	explicit MC6850(const DeviceConfig& config);
 
+	// MSXDevice
 	void reset(EmuTime::param time) override;
 	byte readIO(word port, EmuTime::param time) override;
 	byte peekIO(word port, EmuTime::param time) const override;
@@ -19,9 +29,48 @@ public:
 	void serialize(Archive& ar, unsigned version);
 
 private:
-	byte control;
+	// MidiInConnector
+	bool ready() override;
+	bool acceptsData() override;
+	void setDataBits(DataBits bits) override;
+	void setStopBits(StopBits bits) override;
+	void setParityBit(bool enable, ParityBit parity) override;
+	void recvByte(byte value, EmuTime::param time) override;
+
+	// Schedulable
+	struct SyncRecv : Schedulable {
+		friend class MC6850;
+		SyncRecv(Scheduler& s) : Schedulable(s) {}
+		void executeUntil(EmuTime::param time) override {
+			auto& mc6850 = OUTER(MC6850, syncRecv);
+			mc6850.execRecv(time);
+		}
+	} syncRecv;
+	struct SyncTrans : Schedulable {
+		friend class MC6850;
+		SyncTrans(Scheduler& s) : Schedulable(s) {}
+		void executeUntil(EmuTime::param time) override {
+			auto& mc6850 = OUTER(MC6850, syncTrans);
+			mc6850.execTrans(time);
+		}
+	} syncTrans;
+	void execRecv (EmuTime::param time);
+	void execTrans(EmuTime::param time);
+
+	void send(byte value, EmuTime::param time);
+
+	IRQHelper rxIRQ;
+	IRQHelper txIRQ;
+	bool rxReady;
+	byte controlReg;
+	byte receiveDataReg;   //<! Byte received from MIDI in connector.
+	byte transmitDataReg;  //<! Next to-be-sent byte.
+	byte transmitShiftReg; //<! Byte currently being sent.
+	byte statusReg;
+
+	MidiOutConnector outConnector;
 };
-SERIALIZE_CLASS_VERSION(MC6850, 2);
+SERIALIZE_CLASS_VERSION(MC6850, 3);
 
 } // namespace openmsx
 
