openMSX/openMSX

commit aae997be36493b99ef9692d235222d4670e01418

Only subsribe to events when plugged in
Also made a Pluggable::isPluggedIn() helper method and used it in several
places.

git-svn-id: file:///openmsx-svn/code/openmsx/trunk@10736 7512d703-071c-0410-b5c2-d2bdd895b7d1

src/input/Mouse.cc
@@ -29,12 +29,13 @@ Mouse::Mouse(MSXEventDistributor& eventDistributor_)
 	faze = FAZE_YLOW;
 	xrel = yrel = curxrel = curyrel = 0;
 	mouseMode = true;
-	eventDistributor.registerEventListener(*this);
 }
 
 Mouse::~Mouse()
 {
-	eventDistributor.unregisterEventListener(*this);
+	if (isPluggedIn()) {
+		Mouse::unplugHelper(EmuTime::dummy());
+	}
 }
 
 
@@ -61,10 +62,12 @@ void Mouse::plugHelper(Connector& /*connector*/, EmuTime::param time)
 		// left mouse button pressed, joystick emulation mode
 		mouseMode = false;
 	}
+	eventDistributor.registerEventListener(*this);
 }
 
 void Mouse::unplugHelper(EmuTime::param /*time*/)
 {
+	eventDistributor.unregisterEventListener(*this);
 }
 
 

src/Pluggable.hh
@@ -46,6 +46,12 @@ public:
 	  */
 	Connector* getConnector() const;
 
+	/** Returns true if this pluggable is currently plugged into a connector.
+	 * The method getConnector() can also be used, but this is more
+	 * descriptive.
+	 */
+	bool isPluggedIn() const { return getConnector() != 0; }
+
 protected:
 	virtual void plugHelper(Connector& newConnector, EmuTime::param time) = 0;
 	/* implementations of unplugHelper() may not throw exceptions. */

src/PluggableFactory.cc
@@ -40,8 +40,8 @@ void PluggableFactory::createAll(PluggingController& controller,
 	// TODO: Support hot-plugging of input devices:
 	// - additional key joysticks can be created by the user
 	// - real joysticks and mice can be hotplugged (USB)
-	controller.registerPluggable(new ArkanoidPad(motherBoard.getMSXEventDistributor()));
-	controller.registerPluggable(new Mouse(motherBoard.getMSXEventDistributor()));
+	controller.registerPluggable(new ArkanoidPad(msxEventDistributor));
+	controller.registerPluggable(new Mouse(msxEventDistributor));
 	controller.registerPluggable(new JoyTap(controller, "joytap"));
 	controller.registerPluggable(new NinjaTap(controller, "ninjatap"));
 	controller.registerPluggable(new KeyJoystick(

src/PrinterPortSimpl.cc
@@ -68,8 +68,7 @@ const std::string& PrinterPortSimpl::getDescription() const
 template<typename Archive>
 void PrinterPortSimpl::serialize(Archive& ar, unsigned /*version*/)
 {
-	if (getConnector()) {
-		// plugged in
+	if (isPluggedIn()) {
 		if (ar.isLoader()) {
 			createDAC();
 		}

src/input/ArkanoidPad.cc
@@ -33,12 +33,13 @@ ArkanoidPad::ArkanoidPad(MSXEventDistributor& eventDistributor_)
 	, buttonStatus(0x3E)
 	, lastValue(0)
 {
-	eventDistributor.registerEventListener(*this);
 }
 
 ArkanoidPad::~ArkanoidPad()
 {
-	eventDistributor.unregisterEventListener(*this);
+	if (isPluggedIn()) {
+		ArkanoidPad::unplugHelper(EmuTime::dummy());
+	}
 }
 
 
@@ -57,10 +58,12 @@ const string& ArkanoidPad::getDescription() const
 
 void ArkanoidPad::plugHelper(Connector& /*connector*/, EmuTime::param /*time*/)
 {
+	eventDistributor.registerEventListener(*this);
 }
 
 void ArkanoidPad::unplugHelper(EmuTime::param /*time*/)
 {
+	eventDistributor.unregisterEventListener(*this);
 }
 
 // JoystickDevice

src/input/Joystick.cc
@@ -51,9 +51,8 @@ Joystick::Joystick(MSXEventDistributor& eventDistributor_, unsigned joyNum_)
 
 Joystick::~Joystick()
 {
-	if (getConnector()) {
-		// still plugged in
-		eventDistributor.unregisterEventListener(*this);
+	if (isPluggedIn()) {
+		Joystick::unplugHelper(EmuTime::dummy());
 	}
 	if (joystick) {
 		SDL_JoystickClose(joystick);

src/input/KeyJoystick.cc
@@ -31,15 +31,15 @@ KeyJoystick::KeyJoystick(CommandController& commandController,
 	, trigB(new KeyCodeSetting(commandController, name + ".trigb",
 		"key for trigger B",       Keys::K_M))
 {
-	eventDistributor.registerEventListener(*this);
-
 	status = JOY_UP | JOY_DOWN | JOY_LEFT | JOY_RIGHT |
 	         JOY_BUTTONA | JOY_BUTTONB;
 }
 
 KeyJoystick::~KeyJoystick()
 {
-	eventDistributor.unregisterEventListener(*this);
+	if (isPluggedIn()) {
+		KeyJoystick::unplugHelper(EmuTime::dummy());
+	}
 }
 
 
@@ -59,10 +59,12 @@ const string& KeyJoystick::getDescription() const
 
 void KeyJoystick::plugHelper(Connector& /*connector*/, EmuTime::param /*time*/)
 {
+	eventDistributor.registerEventListener(*this);
 }
 
 void KeyJoystick::unplugHelper(EmuTime::param /*time*/)
 {
+	eventDistributor.unregisterEventListener(*this);
 }
 
 

src/serial/MidiInReader.cc
@@ -89,7 +89,7 @@ void MidiInReader::run()
 		if (num != 1) {
 			continue;
 		}
-		assert(getConnector());
+		assert(isPluggedIn());
 
 		ScopedLock l(lock);
 		queue.push_back(buf);
@@ -120,7 +120,7 @@ void MidiInReader::signal(EmuTime::param time)
 // EventListener
 bool MidiInReader::signalEvent(shared_ptr<const Event> /*event*/)
 {
-	if (getConnector()) {
+	if (isPluggedIn()) {
 		signal(scheduler.getCurrentTime());
 	} else {
 		ScopedLock l(lock);

commit 2197a97f2eb5b02a9aa529797b7da19bcba6c18b

Restore mouse-joystick-emulation mode
When a MSX mouse is plugged in while holding the left mouse button pressed, it
will go into joystick emulation mode. This was implemented in openMSX, but
commit aae997be (2009/11/02) broke it. Thanks to FRS for reporting the problem.

Commit aae997be makes the input devices (joystick, mouse, trackball, ...) only
listen to input events when they are plugged in. Though what I forgot was that
immediately when plugging in the mouse it already needs to know the status of
the left mouse button. Fixed by explicitly querying the status on plug (instead
of relying on events).

src/input/Mouse.cc
@@ -8,6 +8,7 @@
 #include "serialize.hh"
 #include "serialize_meta.hh"
 #include "unreachable.hh"
+#include <SDL.h>
 
 using std::string;
 using std::min;
@@ -88,13 +89,13 @@ string_ref Mouse::getDescription() const
 
 void Mouse::plugHelper(Connector& /*connector*/, EmuTime::param time)
 {
-	if (status & JOY_BUTTONA) {
+	if (SDL_GetMouseState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+		// left mouse button pressed, joystick emulation mode
+		mouseMode = false;
+	} else {
 		// not pressed, mouse mode
 		mouseMode = true;
 		lastTime = time;
-	} else {
-		// left mouse button pressed, joystick emulation mode
-		mouseMode = false;
 	}
 	plugHelper2();
 }