openMSX/openMSX

commit 7b45c86ce34676b8c65dc3d0a5601e0d939b1c94

Speedup looking-up OSD widgets by name
In revision de8caffa (Aug 2009) we speedup lookup by name of OSD widgets.
Because in some 'extreme' cases it was a bottleneck (only happens with
'extreme' Tcl scripts that change OSD properties very often; simply drawing
doesn't require lookup by name). This patch speeds it up a little further.

A OSDWidget contains a (owning) vector of child OSDWidget objects, those are
sorted on increasing z-coordinate (for fast drawing). Revision de8caffa also
added a (non-owning) (hash)map to speedup lookup by name. These maps exist per
widget, so at each hierarchical level you can do a O(1) lookup. Thus a
full hierarchical name lookup is O(hierarchy-depth).

It turns out we only need lookups on the full hierarchical name (IOW we never
start a lookup in a subtree). So we can replace all those maps with a single
(hash)map in the top element. This makes all name lookups O(1).

--

After the above change a widget no longer needs to know it's own name, so this
patch also removes the name member from the OSDWidget class.

The name is now only stored in the top lookup map. Note that it's the _full_
name, so the common prefix of (long) hierarchical names is now duplicated in
memory.

To compensate for this possible higher memory usage the names are now stored as
TclObjects instead of std::strings. Often the names of the widgets are in some
form present in Tcl scripts (e.g. as the value of some variable). By keeping
the name as a TclObject we avoid any string conversions (an additional speedup)
and also (often) avoid the extra memory cost for the name (because TclObjects
are reference counted).

src/console/OSDGUI.cc
@@ -72,38 +72,40 @@ void OSDGUI::OSDCommand::create(array_ref<TclObject> tokens, TclObject& result)
 		throw SyntaxError();
 	}
 	string_ref type = tokens[2].getString();
-	string_ref fullname = tokens[3].getString();
-	string_ref parentname, name;
-	StringOp::splitOnLast(fullname, '.', parentname, name);
-	if (name.empty()) std::swap(parentname, name);
+	auto& fullname = tokens[3];
+	auto fullnameStr = fullname.getString();
 
 	auto& gui = OUTER(OSDGUI, osdCommand);
-	auto* parent = gui.getTopWidget().findSubWidget(parentname);
+	auto& top = gui.getTopWidget();
+	if (top.findByName(fullnameStr)) {
+		throw CommandException(
+			"There already exists a widget with this name: " +
+			fullnameStr);
+	}
+
+	string_ref parentname, name;
+	StringOp::splitOnLast(fullnameStr, '.', parentname, name);
+	auto* parent = name.empty() ? &top : top.findByName(parentname);
 	if (!parent) {
 		throw CommandException(
 			"Parent widget doesn't exist yet:" + parentname);
 	}
-	if (parent->findSubWidget(name)) {
-		throw CommandException(
-			"There already exists a widget with this name: " +
-			fullname);
-	}
 
-	auto widget = create(type, name.str());
+	auto widget = create(type);
 	configure(*widget, tokens, 4);
+	top.addName(fullname, *widget);
 	parent->addWidget(std::move(widget));
 
-	result.setString(fullname);
+	result = fullname;
 }
 
-unique_ptr<OSDWidget> OSDGUI::OSDCommand::create(
-		string_ref type, const string& name) const
+unique_ptr<OSDWidget> OSDGUI::OSDCommand::create(string_ref type) const
 {
 	auto& gui = OUTER(OSDGUI, osdCommand);
 	if (type == "rectangle") {
-		return make_unique<OSDRectangle>(gui, name);
+		return make_unique<OSDRectangle>(gui);
 	} else if (type == "text") {
-		return make_unique<OSDText>(gui, name);
+		return make_unique<OSDText>(gui);
 	} else {
 		throw CommandException(
 			"Invalid widget type '" + type + "', expected "
@@ -116,17 +118,23 @@ void OSDGUI::OSDCommand::destroy(array_ref<TclObject> tokens, TclObject& result)
 	if (tokens.size() != 3) {
 		throw SyntaxError();
 	}
+	auto fullname = tokens[2].getString();
+
 	auto& gui = OUTER(OSDGUI, osdCommand);
-	auto* widget = gui.getTopWidget().findSubWidget(tokens[2].getString());
+	auto& top = gui.getTopWidget();
+	auto* widget = top.findByName(fullname);
 	if (!widget) {
 		// widget not found, not an error
 		result.setBoolean(false);
 		return;
 	}
+
 	auto* parent = widget->getParent();
 	if (!parent) {
 		throw CommandException("Can't destroy the top widget.");
 	}
+
+	top.removeName(fullname);
 	parent->deleteWidget(*widget);
 	result.setBoolean(true);
 }
@@ -137,9 +145,7 @@ void OSDGUI::OSDCommand::info(array_ref<TclObject> tokens, TclObject& result)
 	switch (tokens.size()) {
 	case 2: {
 		// list widget names
-		vector<string> names;
-		gui.getTopWidget().listWidgetNames("", names);
-		result.addListElements(names);
+		result.addListElements(gui.getTopWidget().getAllWidgetNames());
 		break;
 	}
 	case 3: {
@@ -165,7 +171,7 @@ void OSDGUI::OSDCommand::exists(array_ref<TclObject> tokens, TclObject& result)
 		throw SyntaxError();
 	}
 	auto& gui = OUTER(OSDGUI, osdCommand);
-	auto* widget = gui.getTopWidget().findSubWidget(tokens[2].getString());
+	auto* widget = gui.getTopWidget().findByName(tokens[2].getString());
 	result.setBoolean(widget != nullptr);
 }
 
@@ -270,14 +276,12 @@ void OSDGUI::OSDCommand::tabCompletion(vector<string>& tokens) const
 		completeString(tokens, types);
 	} else if ((tokens.size() == 3) ||
 	           ((tokens.size() == 4) && (tokens[1] == "create"))) {
-		vector<string> names;
-		gui.getTopWidget().listWidgetNames("", names);
-		completeString(tokens, names);
+		completeString(tokens, gui.getTopWidget().getAllWidgetNames());
 	} else {
 		try {
 			vector<string_ref> properties;
 			if (tokens[1] == "create") {
-				auto widget = create(tokens[2], "");
+				auto widget = create(tokens[2]);
 				properties = widget->getProperties();
 			} else if ((tokens[1] == "configure") ||
 			           (tokens[1] == "info")) {
@@ -294,7 +298,7 @@ void OSDGUI::OSDCommand::tabCompletion(vector<string>& tokens) const
 OSDWidget& OSDGUI::OSDCommand::getWidget(string_ref name) const
 {
 	auto& gui = OUTER(OSDGUI, osdCommand);
-	auto* widget = gui.getTopWidget().findSubWidget(name);
+	auto* widget = gui.getTopWidget().findByName(name);
 	if (!widget) {
 		throw CommandException("No widget with name " + name);
 	}

src/console/OSDGUI.hh
@@ -40,8 +40,7 @@ private:
 		void info     (array_ref<TclObject> tokens, TclObject& result);
 		void exists   (array_ref<TclObject> tokens, TclObject& result);
 		void configure(array_ref<TclObject> tokens, TclObject& result);
-		std::unique_ptr<OSDWidget> create(
-			string_ref type, const std::string& name) const;
+		std::unique_ptr<OSDWidget> create(string_ref type) const;
 		void configure(OSDWidget& widget, array_ref<TclObject> tokens,
 			       unsigned skip);
 

src/console/OSDImageBasedWidget.cc
@@ -16,9 +16,8 @@ using namespace gl;
 
 namespace openmsx {
 
-OSDImageBasedWidget::OSDImageBasedWidget(OSDGUI& gui_, const string& name)
-	: OSDWidget(name)
-	, gui(gui_)
+OSDImageBasedWidget::OSDImageBasedWidget(OSDGUI& gui_)
+	: gui(gui_)
 	, startFadeTime(0)
 	, fadePeriod(0.0)
 	, fadeTarget(1.0)

src/console/OSDImageBasedWidget.hh
@@ -25,7 +25,7 @@ public:
 	float getRecursiveFadeValue() const override;
 
 protected:
-	OSDImageBasedWidget(OSDGUI& gui, const std::string& name);
+	explicit OSDImageBasedWidget(OSDGUI& gui);
 	~OSDImageBasedWidget();
 	bool hasConstantAlpha() const;
 	void createImage(OutputRectangle& output);

src/console/OSDRectangle.cc
@@ -18,8 +18,8 @@ using namespace gl;
 
 namespace openmsx {
 
-OSDRectangle::OSDRectangle(OSDGUI& gui, const string& name)
-	: OSDImageBasedWidget(gui, name)
+OSDRectangle::OSDRectangle(OSDGUI& gui)
+	: OSDImageBasedWidget(gui)
 	, scale(1.0), borderSize(0.0), relBorderSize(0.0)
 	, borderRGBA(0x000000ff)
 {

src/console/OSDRectangle.hh
@@ -12,7 +12,7 @@ class BaseImage;
 class OSDRectangle final : public OSDImageBasedWidget
 {
 public:
-	OSDRectangle(OSDGUI& gui, const std::string& name);
+	explicit OSDRectangle(OSDGUI& gui);
 
 	std::vector<string_ref> getProperties() const override;
 	void setProperty(Interpreter& interp,

src/console/OSDText.cc
@@ -22,8 +22,8 @@ using namespace gl;
 
 namespace openmsx {
 
-OSDText::OSDText(OSDGUI& gui, const string& name)
-	: OSDImageBasedWidget(gui, name)
+OSDText::OSDText(OSDGUI& gui)
+	: OSDImageBasedWidget(gui)
 	, fontfile("skins/Vera.ttf.gz")
 	, size(12)
 	, wrapMode(NONE), wrapw(0.0), wraprelw(1.0)

src/console/OSDText.hh
@@ -11,7 +11,7 @@ namespace openmsx {
 class OSDText final : public OSDImageBasedWidget
 {
 public:
-	OSDText(OSDGUI& gui, const std::string& name);
+	explicit OSDText(OSDGUI& gui);
 
 	std::vector<string_ref> getProperties() const override;
 	void setProperty(Interpreter& interp,

commit 87db06e9c85409516f25add6abfaf2eeef2cb97e

Fixed OSD-widget name management bug
This regression was introduced in revision 7b45c86c. That patch replaced the
name-lookup hash_maps located in each OSDWidget with only one hash_map located
in OSDTopWidget.

The problem occurred when destroying a widget that still had children. Those
children are also destroyed, but we forgot to remove their names from the top
map. Before 7b45c86c this wasn't a problem because the sub-maps that contains
the names of the children itself disappear.

Fix is easy enough: when removing a name, also (recursively) remove the names
of the children.

Though to be able to implement this efficiently (O(number-of-children)) I had
to give back each OSDWidget a name member (revision 7b45c86c removed that
member). This may seem like a step-back, but actually it doesn't make any
difference: we add the name to the OSDWidget, but remove it from the hash_map
in OSDTopWidget (so turn hash_map into hash_set).

src/console/OSDGUI.cc
@@ -91,21 +91,22 @@ void OSDGUI::OSDCommand::create(array_ref<TclObject> tokens, TclObject& result)
 			"Parent widget doesn't exist yet:" + parentname);
 	}
 
-	auto widget = create(type);
+	auto widget = create(type, fullname);
 	configure(*widget, tokens, 4);
-	top.addName(fullname, *widget);
+	top.addName(*widget);
 	parent->addWidget(std::move(widget));
 
 	result = fullname;
 }
 
-unique_ptr<OSDWidget> OSDGUI::OSDCommand::create(string_ref type) const
+unique_ptr<OSDWidget> OSDGUI::OSDCommand::create(
+	string_ref type, const TclObject& name) const
 {
 	auto& gui = OUTER(OSDGUI, osdCommand);
 	if (type == "rectangle") {
-		return make_unique<OSDRectangle>(gui);
+		return make_unique<OSDRectangle>(gui, name);
 	} else if (type == "text") {
-		return make_unique<OSDText>(gui);
+		return make_unique<OSDText>(gui, name);
 	} else {
 		throw CommandException(
 			"Invalid widget type '" + type + "', expected "
@@ -134,7 +135,7 @@ void OSDGUI::OSDCommand::destroy(array_ref<TclObject> tokens, TclObject& result)
 		throw CommandException("Can't destroy the top widget.");
 	}
 
-	top.removeName(fullname);
+	top.removeName(*widget);
 	parent->deleteWidget(*widget);
 	result.setBoolean(true);
 }
@@ -281,7 +282,7 @@ void OSDGUI::OSDCommand::tabCompletion(vector<string>& tokens) const
 		try {
 			vector<string_ref> properties;
 			if (tokens[1] == "create") {
-				auto widget = create(tokens[2]);
+				auto widget = create(tokens[2], TclObject());
 				properties = widget->getProperties();
 			} else if ((tokens[1] == "configure") ||
 			           (tokens[1] == "info")) {

src/console/OSDGUI.hh
@@ -40,7 +40,8 @@ private:
 		void info     (array_ref<TclObject> tokens, TclObject& result);
 		void exists   (array_ref<TclObject> tokens, TclObject& result);
 		void configure(array_ref<TclObject> tokens, TclObject& result);
-		std::unique_ptr<OSDWidget> create(string_ref type) const;
+		std::unique_ptr<OSDWidget> create(
+			string_ref type, const TclObject& name) const;
 		void configure(OSDWidget& widget, array_ref<TclObject> tokens,
 			       unsigned skip);
 

src/console/OSDImageBasedWidget.cc
@@ -16,8 +16,9 @@ using namespace gl;
 
 namespace openmsx {
 
-OSDImageBasedWidget::OSDImageBasedWidget(OSDGUI& gui_)
-	: gui(gui_)
+OSDImageBasedWidget::OSDImageBasedWidget(OSDGUI& gui_, const TclObject& name)
+	: OSDWidget(name)
+	, gui(gui_)
 	, startFadeTime(0)
 	, fadePeriod(0.0)
 	, fadeTarget(1.0)

src/console/OSDImageBasedWidget.hh
@@ -25,7 +25,7 @@ public:
 	float getRecursiveFadeValue() const override;
 
 protected:
-	explicit OSDImageBasedWidget(OSDGUI& gui);
+	OSDImageBasedWidget(OSDGUI& gui, const TclObject& name);
 	~OSDImageBasedWidget();
 	bool hasConstantAlpha() const;
 	void createImage(OutputRectangle& output);

src/console/OSDRectangle.cc
@@ -18,8 +18,8 @@ using namespace gl;
 
 namespace openmsx {
 
-OSDRectangle::OSDRectangle(OSDGUI& gui)
-	: OSDImageBasedWidget(gui)
+OSDRectangle::OSDRectangle(OSDGUI& gui, const TclObject& name)
+	: OSDImageBasedWidget(gui, name)
 	, scale(1.0), borderSize(0.0), relBorderSize(0.0)
 	, borderRGBA(0x000000ff)
 {

src/console/OSDRectangle.hh
@@ -12,7 +12,7 @@ class BaseImage;
 class OSDRectangle final : public OSDImageBasedWidget
 {
 public:
-	explicit OSDRectangle(OSDGUI& gui);
+	OSDRectangle(OSDGUI& gui, const TclObject& name);
 
 	std::vector<string_ref> getProperties() const override;
 	void setProperty(Interpreter& interp,

src/console/OSDText.cc
@@ -22,8 +22,8 @@ using namespace gl;
 
 namespace openmsx {
 
-OSDText::OSDText(OSDGUI& gui)
-	: OSDImageBasedWidget(gui)
+OSDText::OSDText(OSDGUI& gui, const TclObject& name)
+	: OSDImageBasedWidget(gui, name)
 	, fontfile("skins/Vera.ttf.gz")
 	, size(12)
 	, wrapMode(NONE), wrapw(0.0), wraprelw(1.0)

src/console/OSDText.hh
@@ -11,7 +11,7 @@ namespace openmsx {
 class OSDText final : public OSDImageBasedWidget
 {
 public:
-	explicit OSDText(OSDGUI& gui);
+	OSDText(OSDGUI& gui, const TclObject& name);
 
 	std::vector<string_ref> getProperties() const override;
 	void setProperty(Interpreter& interp,

src/console/OSDTopWidget.cc
@@ -8,9 +8,10 @@
 namespace openmsx {
 
 OSDTopWidget::OSDTopWidget(OSDGUI& gui_)
-	: gui(gui_)
+	: OSDWidget(TclObject())
+	, gui(gui_)
 {
-	addName(TclObject(), *this);
+	addName(*this);
 }
 
 string_ref OSDTopWidget::getType() const
@@ -56,7 +57,7 @@ void OSDTopWidget::showAllErrors()
 OSDWidget* OSDTopWidget::findByName(string_ref name)
 {
 	auto it = widgetsByName.find(name);
-	return (it != end(widgetsByName)) ? it->second : nullptr;
+	return (it != end(widgetsByName)) ? *it : nullptr;
 }
 
 const OSDWidget* OSDTopWidget::findByName(string_ref name) const
@@ -64,23 +65,27 @@ const OSDWidget* OSDTopWidget::findByName(string_ref name) const
 	return const_cast<OSDTopWidget*>(this)->findByName(name);
 }
 
-void OSDTopWidget::addName(const TclObject& name, OSDWidget& widget)
+void OSDTopWidget::addName(OSDWidget& widget)
 {
-	assert(!widgetsByName.contains(name));
-	widgetsByName.emplace_noDuplicateCheck(name, &widget);
+	assert(!widgetsByName.contains(widget.getName()));
+	widgetsByName.emplace_noDuplicateCheck(&widget);
 }
 
-void OSDTopWidget::removeName(string_ref name)
+void OSDTopWidget::removeName(OSDWidget& widget)
 {
-	assert(widgetsByName.contains(name));
-	widgetsByName.erase(name);
+	auto it = widgetsByName.find(widget.getName());
+	assert(it != end(widgetsByName));
+	for (auto& child : (*it)->getChildren()) {
+		removeName(*child);
+	}
+	widgetsByName.erase(it);
 }
 
 std::vector<string_ref> OSDTopWidget::getAllWidgetNames() const
 {
 	std::vector<string_ref> result;
-	for (auto& p : widgetsByName) {
-		result.push_back(p.first.getString());
+	for (auto* p : widgetsByName) {
+		result.push_back(p->getName());
 	}
 	return result;
 }

src/console/OSDTopWidget.hh
@@ -3,7 +3,7 @@
 
 #include "OSDWidget.hh"
 #include "TclObject.hh"
-#include "hash_map.hh"
+#include "hash_set.hh"
 #include "xxhash.hh"
 #include <vector>
 #include <string>
@@ -33,8 +33,8 @@ public:
 
 	OSDWidget* findByName(string_ref name);
 	const OSDWidget* findByName(string_ref name) const;
-	void addName(const TclObject& name, OSDWidget& widget);
-	void removeName(string_ref name);
+	void addName(OSDWidget& widget);
+	void removeName(OSDWidget& widget);
 	std::vector<string_ref> getAllWidgetNames() const;
 
 protected:
@@ -45,7 +45,13 @@ protected:
 private:
 	OSDGUI& gui;
 	std::vector<std::string> errors;
-	hash_map<TclObject, OSDWidget*, XXTclHasher> widgetsByName;
+
+	struct NameFromWidget {
+		string_ref operator()(const OSDWidget* w) const {
+			return w->getName();
+		}
+	};
+	hash_set<OSDWidget*, NameFromWidget, XXTclHasher> widgetsByName;
 };
 
 } // namespace openmsx

src/console/OSDWidget.cc
@@ -119,8 +119,9 @@ GLScopedClip::~GLScopedClip()
 
 ////
 
-OSDWidget::OSDWidget()
+OSDWidget::OSDWidget(const TclObject& name_)
 	: parent(nullptr)
+	, name(name_)
 	, z(0.0)
 	, scaled(false)
 	, clip(false)

src/console/OSDWidget.hh
@@ -1,6 +1,7 @@
 #ifndef OSDWIDGET_HH
 #define OSDWIDGET_HH
 
+#include "TclObject.hh"
 #include "gl_vec.hh"
 #include "hash_set.hh"
 #include "string_ref.hh"
@@ -17,15 +18,18 @@ class Interpreter;
 
 class OSDWidget
 {
+	using SubWidgets = std::vector<std::unique_ptr<OSDWidget>>;
 public:
 	virtual ~OSDWidget();
 
+	string_ref getName() const { return name.getString(); }
 	gl::vec2 getPos()    const { return pos; }
 	gl::vec2 getRelPos() const { return relPos; }
 	float    getZ()      const { return z; }
 
 	      OSDWidget* getParent()       { return parent; }
 	const OSDWidget* getParent() const { return parent; }
+	const SubWidgets& getChildren() const { return subWidgets; }
 	void addWidget(std::unique_ptr<OSDWidget> widget);
 	void deleteWidget(OSDWidget& widget);
 
@@ -48,7 +52,7 @@ public:
 	virtual gl::vec2 getSize(const OutputRectangle& output) const = 0;
 
 protected:
-	OSDWidget();
+	OSDWidget(const TclObject& name);
 	void invalidateChildren();
 	bool needSuppressErrors() const;
 
@@ -66,10 +70,11 @@ private:
 
 	/** Direct child widgets of this widget, sorted by z-coordinate.
 	  */
-	std::vector<std::unique_ptr<OSDWidget>> subWidgets;
+	SubWidgets subWidgets;
 
 	OSDWidget* parent;
 
+	TclObject name;
 	gl::vec2 pos;
 	gl::vec2 relPos;
 	float z;