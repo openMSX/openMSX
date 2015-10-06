63b58996f990e4801ecae9b5f877642683a1c390
m9710797 <vermaelen.wouter@gmail.com>
2015-10-06 21:26:39 +0200

    Test: remove manually created move constructor/assignment
    According to the C++11 standard, the compiler should automatically create the
    move constructor and move assignment operator for this class. And it does
    exactly that with gcc and clang. However vs2013 didn't do this correctly and as
    a workaround we had to manually define these operations. Now we've switched to
    vs2015 and this problem *should* be fixed.
    
    As a test I've removed the workaround in only one class. Let's see what the
    vs-build-bot reports. If it's OK we can remove similar workarounds from various
    other places.

--- src/sound/WavData.hh
+++ src/sound/WavData.hh
@@ -17,24 +17,6 @@ public:
 	 * bit-depth and sample rate. */
 	WavData(const std::string& filename, unsigned bits = 0, unsigned freq = 0);
 
-	// Move constructor/assignment
-	//  Normally these are auto-generated, but vs2013 isn't fully c++11
-	//  compliant yet.
-	WavData(WavData&& other)
-		: buffer(std::move(other.buffer))
-		, bits(other.bits)
-		, freq(other.freq)
-		, length(other.length)
-		, channels(other.channels) {}
-	WavData& operator=(WavData&& other) {
-		buffer = std::move(other.buffer);
-		bits = other.bits;
-		freq = other.freq;
-		length = other.length;
-		channels = other.channels;
-		return *this;
-	}
-
 	unsigned getFreq() const { return freq; }
 	unsigned getBits() const { return bits; }
 	unsigned getSize() const { return length; }
@@ -42,13 +24,6 @@ public:
 	const void* getData() const { return buffer.data(); }
 
 private:
-#if defined(_MSC_VER)
-	// Make non-copyable/assignable
-	//  work around limitation in vs2013, see comment in MemBuffer.
-	WavData(const WavData&);
-	WavData& operator=(const WavData&);
-#endif
-
 	MemBuffer<byte> buffer;
 	unsigned bits;
 	unsigned freq;
