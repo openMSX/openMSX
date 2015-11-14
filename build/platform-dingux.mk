6693b10a0bf311f4da7dff5b497c0b99f1eed2e6
Maarten ter Huurne <maarten@treewalker.org>
2015-11-14 08:45:46 +0100

    Target GCW0 instead of A320 for OD build
    The A320 toolchain contains a version of GCC that can no longer build
    openMSX.

--- build/platform-dingux.mk
+++ build/platform-dingux.mk
@@ -1,22 +1,15 @@
-# Configuration for Dingux: Linux for Dingoo A320.
+# Configuration for OpenDingux on GCW Zero.
 
 # Set CXX before including platform-linux.mk (see comments in platform-linux.mk)
 ifeq ($(OPENMSX_TARGET_CPU),mipsel)
 # Automatically select the cross compiler.
 ifeq ($(origin CXX),default)
-CXX:=mipsel-a320-linux-uclibc-g++
+CXX:=mipsel-gcw0-linux-uclibc-g++
 endif
-# Use MIPS32 instruction set.
-TARGET_FLAGS+=-march=mips32
 endif
 
-# Dingux is a Linux/uClibc system.
+# OpenDingux is a Linux-based system.
 include build/platform-linux.mk
 
-# Allow GMenu to identify our binary as an executable.
-# Since the file system is FAT this cannot be done with a permission flag.
-EXEEXT:=.dge
-LIBRARYEXT:=.so
-
-# Build a minimal set of components.
-LINK_MODE:=3RD_STA_MIN
+# Build with OpenGL ES and Laserdisc support.
+LINK_MODE:=3RD_STA_GLES
