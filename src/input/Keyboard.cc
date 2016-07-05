openMSX/openMSX

commit fa0b5e55319447ea313d9909f2152fc0c0b2bfa5

[6/7] Remove vector<string> interface from RecordedCommand
The Command class (and related classes) offers 2 interfaces for the same thing:
  void execute(array_ref<TclObject> tokens, TclObject& result);
  string execute(const vector<string>& tokens);
A subclass needs to implement exactly one of these two interfaces. The former
will result in faster code, the latter might be more convenient to use.

Though this dual interface also has some disadvantages:
- C++ doesn't have a way to enforce 'reimplement exactly one of these two', so
  mistakes are only caught at run-time instead of compile- (or link-)time.
- Most command implementations are initially copy-pasted from another already
  existing command. Sometimes picking the wrong (less appropriate) interface.

Also the initial motivation (easier to use) for this dual interface has
diminished over time. It's mostly a matter of adding 'getString()'. And for the
cases where you actually need an int/bool/double the TclObject interface is
already easier to use.

So this and the next patch will remove this dual interface and only keep the
fast (TclObject) variant. This patch changes the 'RecordedCommand' class.

src/input/Keyboard.cc
@@ -51,7 +51,8 @@ public:
 	KeyMatrixUpCmd(CommandController& commandController,
 	               StateChangeDistributor& stateChangeDistributor,
 	               Scheduler& scheduler, Keyboard& keyboard);
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 private:
 	Keyboard& keyboard;
@@ -63,7 +64,8 @@ public:
 	KeyMatrixDownCmd(CommandController& commandController,
 	                 StateChangeDistributor& stateChangeDistributor,
 	                 Scheduler& scheduler, Keyboard& keyboard);
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 private:
 	Keyboard& keyboard;
@@ -95,11 +97,12 @@ public:
 	void serialize(Archive& ar, unsigned version);
 
 private:
-	void type(const string& str);
+	void type(string_ref str);
 	void reschedule(EmuTime::param time);
 
 	// Command
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 	virtual void tabCompletion(vector<string>& tokens) const;
 
@@ -760,16 +763,17 @@ void Keyboard::doKeyGhosting()
 	} while (changedSomething);
 }
 
-string Keyboard::processCmd(const vector<string>& tokens, bool up)
+void Keyboard::processCmd(Interpreter& interp, array_ref<TclObject> tokens, bool up)
 {
 	if (tokens.size() != 3) {
 		throw SyntaxError();
 	}
-	unsigned row, mask;
-	if (!StringOp::stringToUint(tokens[1], row) || (row >= NR_KEYROWS)) {
+	unsigned row  = tokens[1].getInt(interp);
+	unsigned mask = tokens[2].getInt(interp);
+	if (row >= NR_KEYROWS) {
 		throw CommandException("Invalid row");
 	}
-	if (!StringOp::stringToUint(tokens[2], mask) || (mask >= 256)) {
+	if (mask >= 256) {
 		throw CommandException("Invalid mask");
 	}
 	if (up) {
@@ -778,7 +782,6 @@ string Keyboard::processCmd(const vector<string>& tokens, bool up)
 		cmdKeyMatrix[row] &= ~mask;
 	}
 	keysChanged = true;
-	return "";
 }
 
 /*
@@ -999,9 +1002,10 @@ KeyMatrixUpCmd::KeyMatrixUpCmd(CommandController& commandController,
 {
 }
 
-string KeyMatrixUpCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
+void KeyMatrixUpCmd::execute(array_ref<TclObject> tokens,
+                             TclObject& /*result*/, EmuTime::param /*time*/)
 {
-	return keyboard.processCmd(tokens, true);
+	return keyboard.processCmd(getInterpreter(), tokens, true);
 }
 
 string KeyMatrixUpCmd::help(const vector<string>& /*tokens*/) const
@@ -1023,9 +1027,10 @@ KeyMatrixDownCmd::KeyMatrixDownCmd(CommandController& commandController,
 {
 }
 
-string KeyMatrixDownCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
+void KeyMatrixDownCmd::execute(array_ref<TclObject> tokens,
+                               TclObject& /*result*/, EmuTime::param /*time*/)
 {
-	return keyboard.processCmd(tokens, false);
+	return keyboard.processCmd(getInterpreter(), tokens, false);
 }
 
 string KeyMatrixDownCmd::help(const vector<string>& /*tokens*/) const
@@ -1110,7 +1115,8 @@ KeyInserter::KeyInserter(CommandController& commandController,
 	typingFrequency = 15;
 }
 
-string KeyInserter::execute(const vector<string>& tokens, EmuTime::param /*time*/)
+void KeyInserter::execute(array_ref<TclObject> tokens, TclObject& /*result*/,
+                          EmuTime::param /*time*/)
 {
 	if (tokens.size() < 2) {
 		throw SyntaxError();
@@ -1121,22 +1127,24 @@ string KeyInserter::execute(const vector<string>& tokens, EmuTime::param /*time*
 
 	// for full backwards compatibility: one option means type it...
 	if (tokens.size() == 2) {
-		type(tokens[1]);
-		return "";
+		type(tokens[1].getString());
+		return;
 	}
 
-        vector<string> arguments;
+        vector<string_ref> arguments;
 	for (unsigned i = 1; i < tokens.size(); ++i) {
-		const string token = tokens[i];
+		string_ref token = tokens[i].getString();
 		if (token == "-release") {
 			releaseBeforePress = true;
 		} else if (token == "-freq") {
 			if (++i == tokens.size()) {
 				throw CommandException("Missing argument");
 			}
-			if (!StringOp::stringToUint(tokens[i], typingFrequency) || (typingFrequency == 0)) {
+			int tmp = tokens[1].getInt(getInterpreter());
+			if (tmp <= 0) {
 				throw CommandException("Wrong argument for -freq (should be a positive number)");
 			}
+			typingFrequency = tmp;
 		} else {
 			arguments.push_back(token);
 		}
@@ -1145,8 +1153,6 @@ string KeyInserter::execute(const vector<string>& tokens, EmuTime::param /*time*
 	if (arguments.size() != 1) throw SyntaxError();
 
 	type(arguments[0]);
-
-	return "";
 }
 
 string KeyInserter::help(const vector<string>& /*tokens*/) const
@@ -1169,7 +1175,7 @@ void KeyInserter::tabCompletion(vector<string>& tokens) const
 	completeString(tokens, options);
 }
 
-void KeyInserter::type(const string& str)
+void KeyInserter::type(string_ref str)
 {
 	if (str.empty()) {
 		return;
@@ -1180,7 +1186,7 @@ void KeyInserter::type(const string& str)
 	if (text_utf8.empty()) {
 		reschedule(getCurrentTime());
 	}
-	text_utf8 += str;
+	text_utf8.append(str.data(), str.size());
 }
 
 void KeyInserter::executeUntil(EmuTime::param time, int /*userData*/)

src/CartridgeSlotManager.cc
@@ -24,10 +24,11 @@ class CartCmd : public RecordedCommand
 public:
 	CartCmd(CartridgeSlotManager& manager, MSXMotherBoard& motherBoard,
 	        string_ref commandName);
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 	virtual void tabCompletion(vector<string>& tokens) const;
-	virtual bool needRecord(const vector<string>& tokens) const;
+	virtual bool needRecord(array_ref<TclObject> tokens) const;
 private:
 	const HardwareConfig* getExtensionConfig(string_ref cartname);
 	CartridgeSlotManager& manager;
@@ -322,10 +323,10 @@ const HardwareConfig* CartCmd::getExtensionConfig(string_ref cartname)
 	return manager.slots[slot].config;
 }
 
-string CartCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
+void CartCmd::execute(array_ref<TclObject> tokens, TclObject& result,
+                      EmuTime::param /*time*/)
 {
-	string result;
-	string_ref cartname = tokens[0];
+	string_ref cartname = tokens[0].getString();
 
 	// strip namespace qualification
 	//  TODO investigate whether it's a good idea to strip namespace at a
@@ -338,18 +339,19 @@ string CartCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
 	if (tokens.size() == 1) {
 		// query name of cartridge
 		auto* extConf = getExtensionConfig(cartname);
-		TclObject object({
+		result.addListElements({
 			cartname + ':',
 			extConf ? extConf->getName() : ""});
 		if (!extConf) {
-			object.addListElement(TclObject({"empty"}));
+			result.addListElement(TclObject({"empty"}));
 		}
-		result = object.getString().str();
-	} else if ( (tokens[1] == "eject") || (tokens[1] == "-eject") ) {
+	} else if ((tokens[1].getString() ==  "eject") ||
+	           (tokens[1].getString() == "-eject")) {
 		// remove cartridge (or extension)
-		if (tokens[1] == "-eject") {
-			result = "Warning: use of '-eject' is deprecated, "
-			         "instead use the 'eject' subcommand";
+		if (tokens[1].getString() == "-eject") {
+			result.setString(
+				"Warning: use of '-eject' is deprecated, "
+				"instead use the 'eject' subcommand");
 		}
 		if (auto* extConf = getExtensionConfig(cartname)) {
 			try {
@@ -366,17 +368,17 @@ string CartCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
 			? string(1, cartname[4])
 			: "any";
 		size_t extensionNameToken = 1;
-		if (tokens[1] == "insert") {
+		if (tokens[1].getString() == "insert") {
 			if (tokens.size() > 2) {
 				extensionNameToken = 2;
 			} else {
 				throw CommandException("Missing argument to insert subcommand");
 			}
 		}
-		vector<string> options(begin(tokens) + extensionNameToken + 1,
-		                       end(tokens));
+		array_ref<TclObject> options(std::begin(tokens) + extensionNameToken + 1,
+		                             std::end(tokens));
 		try {
-			const auto& romname = tokens[extensionNameToken];
+			string_ref romname = tokens[extensionNameToken].getString();
 			auto extension = HardwareConfig::createRomConfig(
 				manager.motherBoard, romname, slotname, options);
 			if (slotname != "any") {
@@ -385,14 +387,13 @@ string CartCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
 					manager.motherBoard.removeExtension(*extConf);
 				}
 			}
-			result = manager.motherBoard.insertExtension(
-				"ROM", std::move(extension));
+			result.setString(manager.motherBoard.insertExtension(
+				"ROM", std::move(extension)));
 			cliComm.update(CliComm::MEDIA, cartname, romname);
 		} catch (MSXException& e) {
 			throw CommandException(e.getMessage());
 		}
 	}
-	return result;
 }
 
 string CartCmd::help(const vector<string>& tokens) const
@@ -412,7 +413,7 @@ void CartCmd::tabCompletion(vector<string>& tokens) const
 	completeFileName(tokens, UserFileContext(), extra);
 }
 
-bool CartCmd::needRecord(const vector<string>& tokens) const
+bool CartCmd::needRecord(array_ref<TclObject> tokens) const
 {
 	return tokens.size() > 1;
 }

src/MSXMotherBoard.cc
@@ -100,8 +100,8 @@ public:
 	typedef std::vector<std::unique_ptr<HardwareConfig>> Extensions;
 	const Extensions& getExtensions() const;
 	HardwareConfig* findExtension(string_ref extensionName);
-	string loadExtension(MSXMotherBoard& self, const string& extensionName, const string& slotname);
-	string insertExtension(const std::string& name,
+	string loadExtension(MSXMotherBoard& self, string_ref extensionName, string_ref slotname);
+	string insertExtension(string_ref name,
 	                       unique_ptr<HardwareConfig> extension);
 	void removeExtension(const HardwareConfig& extension);
 
@@ -230,8 +230,8 @@ class ResetCmd : public RecordedCommand
 {
 public:
 	ResetCmd(MSXMotherBoard::Impl& motherBoard);
-	virtual string execute(const vector<string>& tokens,
-	                       EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 private:
 	MSXMotherBoard::Impl& motherBoard;
@@ -262,8 +262,8 @@ class RemoveExtCmd : public RecordedCommand
 {
 public:
 	RemoveExtCmd(MSXMotherBoard::Impl& motherBoard);
-	virtual string execute(const vector<string>& tokens,
-	                       EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 	virtual void tabCompletion(vector<string>& tokens) const;
 private:
@@ -470,7 +470,7 @@ string MSXMotherBoard::Impl::loadMachine(MSXMotherBoard& self, const string& mac
 	return machineName;
 }
 
-string MSXMotherBoard::Impl::loadExtension(MSXMotherBoard& self, const string& name, const string& slotname)
+string MSXMotherBoard::Impl::loadExtension(MSXMotherBoard& self, string_ref name, string_ref slotname)
 {
 	unique_ptr<HardwareConfig> extension;
 	try {
@@ -486,7 +486,7 @@ string MSXMotherBoard::Impl::loadExtension(MSXMotherBoard& self, const string& n
 }
 
 string MSXMotherBoard::Impl::insertExtension(
-	const string& name, unique_ptr<HardwareConfig> extension)
+	string_ref name, unique_ptr<HardwareConfig> extension)
 {
 	try {
 		extension->parseSlots();
@@ -982,11 +982,10 @@ ResetCmd::ResetCmd(MSXMotherBoard::Impl& motherBoard_)
 {
 }
 
-string ResetCmd::execute(const vector<string>& /*tokens*/,
-                         EmuTime::param /*time*/)
+void ResetCmd::execute(array_ref<TclObject> /*tokens*/, TclObject& /*result*/,
+                       EmuTime::param /*time*/)
 {
 	motherBoard.doReset();
-	return "";
 }
 
 string ResetCmd::help(const vector<string>& /*tokens*/) const
@@ -1081,7 +1080,8 @@ ExtCmd::ExtCmd(MSXMotherBoard& motherBoard_, string_ref commandName_)
 {
 }
 
-string ExtCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
+void ExtCmd::execute(array_ref<TclObject> tokens, TclObject& result,
+                     EmuTime::param /*time*/)
 {
 	if (tokens.size() != 2) {
 		throw SyntaxError();
@@ -1090,7 +1090,8 @@ string ExtCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
 		auto slotname = (commandName.size() == 4)
 			? string(1, commandName[3])
 			: "any";
-		return motherBoard.loadExtension(tokens[1], slotname);
+		result.setString(motherBoard.loadExtension(
+			tokens[1].getString(), slotname));
 	} catch (MSXException& e) {
 		throw CommandException(e.getMessage());
 	}
@@ -1118,22 +1119,23 @@ RemoveExtCmd::RemoveExtCmd(MSXMotherBoard::Impl& motherBoard_)
 {
 }
 
-string RemoveExtCmd::execute(const vector<string>& tokens, EmuTime::param /*time*/)
+void RemoveExtCmd::execute(array_ref<TclObject> tokens, TclObject& /*result*/,
+                           EmuTime::param /*time*/)
 {
 	if (tokens.size() != 2) {
 		throw SyntaxError();
 	}
-	HardwareConfig* extension = motherBoard.findExtension(tokens[1]);
+	string_ref name = tokens[1].getString();
+	HardwareConfig* extension = motherBoard.findExtension(name);
 	if (!extension) {
-		throw CommandException("No such extension: " + tokens[1]);
+		throw CommandException("No such extension: " + name);
 	}
 	try {
 		motherBoard.removeExtension(*extension);
 	} catch (MSXException& e) {
-		throw CommandException("Can't remove extension '" + tokens[1] +
+		throw CommandException("Can't remove extension '" + name +
 		                       "': " + e.getMessage());
 	}
-	return "";
 }
 
 string RemoveExtCmd::help(const vector<string>& /*tokens*/) const
@@ -1405,12 +1407,12 @@ HardwareConfig* MSXMotherBoard::findExtension(string_ref extensionName)
 {
 	return pimpl->findExtension(extensionName);
 }
-string MSXMotherBoard::loadExtension(const string& extensionName, const string& slotname)
+string MSXMotherBoard::loadExtension(string_ref extensionName, string_ref slotname)
 {
 	return pimpl->loadExtension(*this, extensionName, slotname);
 }
 string MSXMotherBoard::insertExtension(
-	const string& name, unique_ptr<HardwareConfig> extension)
+	string_ref name, unique_ptr<HardwareConfig> extension)
 {
 	return pimpl->insertExtension(name, std::move(extension));
 }

src/MSXMotherBoard.hh
@@ -81,8 +81,8 @@ public:
 	std::string loadMachine(const std::string& machine);
 
 	HardwareConfig* findExtension(string_ref extensionName);
-	std::string loadExtension(const std::string& extensionName, const std::string& slotname);
-	std::string insertExtension(const std::string& name,
+	std::string loadExtension(string_ref extensionName, string_ref slotname);
+	std::string insertExtension(string_ref name,
 	                            std::unique_ptr<HardwareConfig> extension);
 	void removeExtension(const HardwareConfig& extension);
 
@@ -182,8 +182,8 @@ class ExtCmd : public RecordedCommand
 {
 public:
 	ExtCmd(MSXMotherBoard& motherBoard, string_ref commandName);
-	virtual std::string execute(const std::vector<std::string>& tokens,
-	                       EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual std::string help(const std::vector<std::string>& tokens) const;
 	virtual void tabCompletion(std::vector<std::string>& tokens) const;
 private:

src/PluggingController.cc
@@ -28,10 +28,11 @@ public:
 	        StateChangeDistributor& stateChangeDistributor,
 	        Scheduler& scheduler,
 	        PluggingController& pluggingController);
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help   (const vector<string>& tokens) const;
 	virtual void tabCompletion(vector<string>& tokens) const;
-	virtual bool needRecord(const vector<string>& tokens) const;
+	virtual bool needRecord(array_ref<TclObject> tokens) const;
 private:
 	PluggingController& pluggingController;
 };
@@ -43,7 +44,8 @@ public:
 	          StateChangeDistributor& stateChangeDistributor,
 	          Scheduler& scheduler,
 	          PluggingController& pluggingController);
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help   (const vector<string>& tokens) const;
 	virtual void tabCompletion(vector<string>& tokens) const;
 private:
@@ -154,39 +156,41 @@ PlugCmd::PlugCmd(CommandController& commandController,
 {
 }
 
-string PlugCmd::execute(const vector<string>& tokens, EmuTime::param time)
+void PlugCmd::execute(array_ref<TclObject> tokens, TclObject& result_,
+                      EmuTime::param time)
 {
 	StringOp::Builder result;
 	switch (tokens.size()) {
-	case 1: {
+	case 1:
 		for (auto& c : pluggingController.connectors) {
 			result << c->getName() << ": "
 			       << c->getPlugged().getName() << '\n';
 		}
 		break;
-	}
 	case 2: {
-		auto& connector = pluggingController.getConnector(tokens[1]);
+		auto& connector = pluggingController.getConnector(tokens[1].getString());
 		result << connector.getName() << ": "
 		       << connector.getPlugged().getName();
 		break;
 	}
 	case 3: {
-		auto& connector = pluggingController.getConnector(tokens[1]);
-		auto& pluggable = pluggingController.getPluggable(tokens[2]);
+		string_ref connName = tokens[1].getString();
+		string_ref plugName = tokens[2].getString();
+		auto& connector = pluggingController.getConnector(connName);
+		auto& pluggable = pluggingController.getPluggable(plugName);
 		if (&connector.getPlugged() == &pluggable) {
 			// already plugged, don't unplug/replug
 			break;
 		}
 		if (connector.getClass() != pluggable.getClass()) {
-			throw CommandException("plug: " + tokens[2] +
-					   " doesn't fit in " + tokens[1]);
+			throw CommandException("plug: " + plugName +
+					   " doesn't fit in " + connName);
 		}
 		connector.unplug(time);
 		try {
 			connector.plug(pluggable, time);
 			pluggingController.getCliComm().update(
-				CliComm::PLUG, tokens[1], tokens[2]);
+				CliComm::PLUG, connName, plugName);
 		} catch (PlugException& e) {
 			throw CommandException("plug: plug failed: " + e.getMessage());
 		}
@@ -195,7 +199,7 @@ string PlugCmd::execute(const vector<string>& tokens, EmuTime::param time)
 	default:
 		throw SyntaxError();
 	}
-	return result;
+	result_.setString(result); // TODO return Tcl list
 }
 
 string PlugCmd::help(const vector<string>& /*tokens*/) const
@@ -227,7 +231,7 @@ void PlugCmd::tabCompletion(vector<string>& tokens) const
 	}
 }
 
-bool PlugCmd::needRecord(const vector<string>& tokens) const
+bool PlugCmd::needRecord(array_ref<TclObject> tokens) const
 {
 	return tokens.size() == 3;
 }
@@ -245,15 +249,16 @@ UnplugCmd::UnplugCmd(CommandController& commandController,
 {
 }
 
-string UnplugCmd::execute(const vector<string>& tokens, EmuTime::param time)
+void UnplugCmd::execute(array_ref<TclObject> tokens, TclObject& /*result*/,
+                        EmuTime::param time)
 {
 	if (tokens.size() != 2) {
 		throw SyntaxError();
 	}
-	auto& connector = pluggingController.getConnector(tokens[1]);
+	string_ref connName = tokens[1].getString();
+	auto& connector = pluggingController.getConnector(connName);
 	connector.unplug(time);
-	pluggingController.getCliComm().update(CliComm::UNPLUG, tokens[1], "");
-	return "";
+	pluggingController.getCliComm().update(CliComm::UNPLUG, connName, "");
 }
 
 string UnplugCmd::help(const vector<string>& /*tokens*/) const

src/cassette/CassettePlayer.cc
@@ -74,10 +74,11 @@ public:
 	            StateChangeDistributor& stateChangeDistributor,
 	            Scheduler& scheduler,
 	            CassettePlayer& cassettePlayer);
-	virtual string execute(const vector<string>& tokens, EmuTime::param time);
+	virtual void execute(array_ref<TclObject> tokens, TclObject& result,
+	                     EmuTime::param time);
 	virtual string help(const vector<string>& tokens) const;
 	virtual void tabCompletion(vector<string>& tokens) const;
-	virtual bool needRecord(const vector<string>& tokens) const;
+	virtual bool needRecord(array_ref<TclObject> tokens) const;
 private:
 	CassettePlayer& cassettePlayer;
 };
@@ -653,45 +654,44 @@ TapeCommand::TapeCommand(CommandController& commandController,
 {
 }
 
-string TapeCommand::execute(const vector<string>& tokens, EmuTime::param time)
+void TapeCommand::execute(array_ref<TclObject> tokens, TclObject& result,
+                          EmuTime::param time)
 {
-	StringOp::Builder result;
 	if (tokens.size() == 1) {
 		// Returning Tcl lists here, similar to the disk commands in
 		// DiskChanger
-		TclObject tmp({
+		result.addListElements({
 			getName() + ':',
 			cassettePlayer.getImageName().getResolved()});
-		tmp.addListElement(TclObject({cassettePlayer.getStateString()}));
-		result << tmp.getString();
+		result.addListElement(TclObject({cassettePlayer.getStateString()}));
 
-	} else if (tokens[1] == "new") {
+	} else if (tokens[1].getString() == "new") {
 		string directory = "taperecordings";
 		string prefix = "openmsx";
 		string extension = ".wav";
 		string filename = FileOperations::parseCommandFileArgument(
-			(tokens.size() == 3) ? tokens[2] : "",
+			(tokens.size() == 3) ? tokens[2].getString() : "",
 			directory, prefix, extension);
 		cassettePlayer.recordTape(Filename(filename), time);
-		result << "Created new cassette image file: " << filename
-		       << ", inserted it and set recording mode.";
+		result.setString("Created new cassette image file: " + filename +
+		                 ", inserted it and set recording mode.");
 
-	} else if (tokens[1] == "insert" && tokens.size() == 3) {
+	} else if (tokens[1].getString() == "insert" && tokens.size() == 3) {
 		try {
-			result << "Changing tape";
-			Filename filename(tokens[2], UserFileContext());
+			result.setString("Changing tape");
+			Filename filename(tokens[2].getString().str(), UserFileContext());
 			cassettePlayer.playTape(filename, time);
 		} catch (MSXException& e) {
 			throw CommandException(e.getMessage());
 		}
 
-	} else if (tokens[1] == "motorcontrol" && tokens.size() == 3) {
-		if (tokens[2] == "on") {
+	} else if (tokens[1].getString() == "motorcontrol" && tokens.size() == 3) {
+		if (tokens[2].getString() == "on") {
 			cassettePlayer.setMotorControl(true, time);
-			result << "Motor control enabled.";
-		} else if (tokens[2] == "off") {
+			result.setString("Motor control enabled.");
+		} else if (tokens[2].getString() == "off") {
 			cassettePlayer.setMotorControl(false, time);
-			result << "Motor control disabled.";
+			result.setString("Motor control disabled.");
 		} else {
 			throw SyntaxError();
 		}
@@ -699,17 +699,17 @@ string TapeCommand::execute(const vector<string>& tokens, EmuTime::param time)
 	} else if (tokens.size() != 2) {
 		throw SyntaxError();
 
-	} else if (tokens[1] == "motorcontrol") {
-		result << "Motor control is "
-		       << (cassettePlayer.motorControl ? "on" : "off");
+	} else if (tokens[1].getString() == "motorcontrol") {
+		result.setString(string("Motor control is ") +
+		                 (cassettePlayer.motorControl ? "on" : "off"));
 
-	} else if (tokens[1] == "record") {
-			result << "TODO: implement this... (sorry)";
+	} else if (tokens[1].getString() == "record") {
+			result.setString("TODO: implement this... (sorry)");
 
-	} else if (tokens[1] == "play") {
+	} else if (tokens[1].getString() == "play") {
 		if (cassettePlayer.getState() == CassettePlayer::RECORD) {
 			try {
-				result << "Play mode set, rewinding tape.";
+				result.setString("Play mode set, rewinding tape.");
 				cassettePlayer.playTape(
 					cassettePlayer.getImageName(), time);
 			} catch (MSXException& e) {
@@ -719,17 +719,18 @@ string TapeCommand::execute(const vector<string>& tokens, EmuTime::param time)
 			throw CommandException("No tape inserted or tape at end!");
 		} else {
 			// PLAY mode
-			result << "Already in play mode.";
+			result.setString("Already in play mode.");
 		}
 
-	} else if (tokens[1] == "eject") {
-		result << "Tape ejected";
+	} else if (tokens[1].getString() == "eject") {
+		result.setString("Tape ejected");
 		cassettePlayer.removeTape(time);
 
-	} else if (tokens[1] == "rewind") {
+	} else if (tokens[1].getString() == "rewind") {
+		string r;
 		if (cassettePlayer.getState() == CassettePlayer::RECORD) {
 			try {
-				result << "First stopping recording... ";
+				r = "First stopping recording... ";
 				cassettePlayer.playTape(
 					cassettePlayer.getImageName(), time);
 			} catch (MSXException& e) {
@@ -737,18 +738,19 @@ string TapeCommand::execute(const vector<string>& tokens, EmuTime::param time)
 			}
 		}
 		cassettePlayer.rewind(time);
-		result << "Tape rewound";
+		r += "Tape rewound";
+		result.setString(r);
 
-	} else if (tokens[1] == "getpos") {
-		result << cassettePlayer.getTapePos(time);
+	} else if (tokens[1].getString() == "getpos") {
+		result.setDouble(cassettePlayer.getTapePos(time));
 
-	} else if (tokens[1] == "getlength") {
-		result << cassettePlayer.getTapeLength(time);
+	} else if (tokens[1].getString() == "getlength") {
+		result.setDouble(cassettePlayer.getTapeLength(time));
 
 	} else {
 		try {
-			result << "Changing tape";
-			Filename filename(tokens[1], UserFileContext());
+			result.setString("Changing tape");
+			Filename filename(tokens[1].getString().str(), UserFileContext());
 			cassettePlayer.playTape(filename, time);
 		} catch (MSXException& e) {
 			throw CommandException(e.getMessage());
@@ -757,7 +759,6 @@ string TapeCommand::execute(const vector<string>& tokens, EmuTime::param time)
 	//if (!cassettePlayer.getConnector()) {
 	//	cassettePlayer.cliComm.printWarning("Cassetteplayer not plugged in.");
 	//}
-	return result;
 }
 
 string TapeCommand::help(const vector<string>& tokens) const
@@ -855,7 +856,7 @@ void TapeCommand::tabCompletion(vector<string>& tokens) const
 	}
 }
 
-bool TapeCommand::needRecord(const vector<string>& tokens) const
+bool TapeCommand::needRecord(array_ref<TclObject> tokens) const
 {
 	return tokens.size() > 1;
 }

commit 22f39e19ed69079e28e9f0541953b1ecba5656e1

Fixed (old) regression in type command option parsing.
Bug was introduced in fa0b5e55319447ea313d9909f2152fc0c0b2bfa5

src/input/Keyboard.cc
@@ -1014,7 +1014,7 @@ void Keyboard::KeyInserter::execute(
 			if (++i == tokens.size()) {
 				throw CommandException("Missing argument");
 			}
-			int tmp = tokens[1].getInt(getInterpreter());
+			int tmp = tokens[i].getInt(getInterpreter());
 			if (tmp <= 0) {
 				throw CommandException("Wrong argument for -freq (should be a positive number)");
 			}