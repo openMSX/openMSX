4930272267a05f885ea58bc96ed3c34e9f2c993c
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2016-08-15 21:46:22 +0200

    Bit 5 only changes the switching addresses.
    SCC is always available. Fixes SCC sound in KUC games Nemesis 2, F-1 Spirit,
    Pennant Race and probably several more. Updated according to info from Pazos.

--- src/memory/KonamiUltimateCollection.cc
+++ src/memory/KonamiUltimateCollection.cc
@@ -15,12 +15,19 @@
 [MAPPER REGISTER (#7FFF)]
 7     A21 \
 6     A20 / FlashROM address lines to switch 2 MB banks.
-5     Mapper mode  :   Select Konami mapper (0=SCC or 1=normal)
+5     Mapper mode  :   Select Konami mapper (0=SCC or 1=normal). [1]
 4     Write enable
 3     Disable #4000-#5FFF mapper in Konami mode, Enable DAC (works like the DAC of Synthesizer or Majutsushi)
 2     Disable mapper register
 1     Disable mapper
 0     Enable 512K mapper limit in SCC mapper or 256K limit in Konami mapper
+
+[1] bit 5 only changes the address range of the mapper (Konami or Konami SCC)
+but the SCC is always available.  This feature is inherited from MFC SCC+ subslot
+mode because all subslots share the same mapper type. So to make Konami
+combinations (i.e.: Konami ROM with Konami SCC ROM) a "common mapper" is needed
+and the SCC must be available. So I made a "Konami mapper" with SCC.  In fact,
+all Konami and Konami SCC ROMs should work with "Konami" mapper in KUC.
 
 ******************************************************************************/
 
@@ -107,13 +114,11 @@ unsigned KonamiUltimateCollection::getFlashAddr(unsigned addr) const
 
 byte KonamiUltimateCollection::readMem(word addr, EmuTime::param time)
 {
-	if (isKonamiSCCmapperConfigured()) { // Konami SCC
-		SCCEnable enable = getSCCEnable();
-		if (((enable == EN_SCC)     && (0x9800 <= addr) && (addr < 0xA000)) ||
-		    ((enable == EN_SCCPLUS) && (0xB800 <= addr) && (addr < 0xC000))) {
-			byte val = scc.readMem(addr & 0xFF, time);
-			return val;
-		}
+	SCCEnable enable = getSCCEnable();
+	if (((enable == EN_SCC)     && (0x9800 <= addr) && (addr < 0xA000)) ||
+	    ((enable == EN_SCCPLUS) && (0xB800 <= addr) && (addr < 0xC000))) {
+		byte val = scc.readMem(addr & 0xFF, time);
+		return val;
 	}
 
 	unsigned flashAddr = getFlashAddr(addr);
@@ -124,13 +129,11 @@ byte KonamiUltimateCollection::readMem(word addr, EmuTime::param time)
 
 byte KonamiUltimateCollection::peekMem(word addr, EmuTime::param time) const
 {
-	if (isKonamiSCCmapperConfigured()) { // Konami SCC
-		SCCEnable enable = getSCCEnable();
-		if (((enable == EN_SCC)     && (0x9800 <= addr) && (addr < 0xA000)) ||
-		    ((enable == EN_SCCPLUS) && (0xB800 <= addr) && (addr < 0xC000))) {
-			byte val = scc.peekMem(addr & 0xFF, time);
-			return val;
-		}
+	SCCEnable enable = getSCCEnable();
+	if (((enable == EN_SCC)     && (0x9800 <= addr) && (addr < 0xA000)) ||
+	    ((enable == EN_SCCPLUS) && (0xB800 <= addr) && (addr < 0xC000))) {
+		byte val = scc.peekMem(addr & 0xFF, time);
+		return val;
 	}
 
 	unsigned flashAddr = getFlashAddr(addr);
@@ -141,13 +144,11 @@ byte KonamiUltimateCollection::peekMem(word addr, EmuTime::param time) const
 
 const byte* KonamiUltimateCollection::getReadCacheLine(word addr) const
 {
-        if (isKonamiSCCmapperConfigured()) {
-                SCCEnable enable = getSCCEnable();
-                if (((enable == EN_SCC)     && (0x9800 <= addr) && (addr < 0xA000)) ||
-                    ((enable == EN_SCCPLUS) && (0xB800 <= addr) && (addr < 0xC000))) {
-                        return nullptr;
-                }
-        }
+	SCCEnable enable = getSCCEnable();
+	if (((enable == EN_SCC)     && (0x9800 <= addr) && (addr < 0xA000)) ||
+	    ((enable == EN_SCCPLUS) && (0xB800 <= addr) && (addr < 0xC000))) {
+		return nullptr;
+	}
 
 	unsigned flashAddr = getFlashAddr(addr);
 	return (flashAddr != unsigned(-1))
@@ -182,50 +183,49 @@ void KonamiUltimateCollection::writeMem(word addr, byte value, EmuTime::param ti
 		invalidateMemCache(0x0000, 0x10000);
 	}
 
-	if (isKonamiSCCmapperConfigured()) {
-		// Konami-SCC
-		if ((addr & 0xFFFE) == 0xBFFE) {
-			sccMode = value;
-			scc.setChipMode((value & 0x20) ? SCC::SCC_plusmode
-			                               : SCC::SCC_Compatible);
-			invalidateMemCache(0x9800, 0x800);
-			invalidateMemCache(0xB800, 0x800);
-		}
-		SCCEnable enable = getSCCEnable();
-		bool isRamSegment2 = ((sccMode & 0x24) == 0x24) ||
-		                     ((sccMode & 0x10) == 0x10);
-		bool isRamSegment3 = ((sccMode & 0x10) == 0x10);
-		if (((enable == EN_SCC)     && !isRamSegment2 &&
-		     (0x9800 <= addr) && (addr < 0xA000)) ||
-		    ((enable == EN_SCCPLUS) && !isRamSegment3 &&
-		     (0xB800 <= addr) && (addr < 0xC000))) {
-			scc.writeMem(addr & 0xFF, value, time);
-			return; // Pazos: when SCC registers are selected flashROM is not seen, so it does not accept commands.
-		}
+	// Konami-SCC
+	if ((addr & 0xFFFE) == 0xBFFE) {
+		sccMode = value;
+		scc.setChipMode((value & 0x20) ? SCC::SCC_plusmode
+					       : SCC::SCC_Compatible);
+		invalidateMemCache(0x9800, 0x800);
+		invalidateMemCache(0xB800, 0x800);
+	}
+	SCCEnable enable = getSCCEnable();
+	bool isRamSegment2 = ((sccMode & 0x24) == 0x24) ||
+			     ((sccMode & 0x10) == 0x10);
+	bool isRamSegment3 = ((sccMode & 0x10) == 0x10);
+	if (((enable == EN_SCC)     && !isRamSegment2 &&
+	     (0x9800 <= addr) && (addr < 0xA000)) ||
+	    ((enable == EN_SCCPLUS) && !isRamSegment3 &&
+	     (0xB800 <= addr) && (addr < 0xC000))) {
+		scc.writeMem(addr & 0xFF, value, time);
+		return; // Pazos: when SCC registers are selected flashROM is not seen, so it does not accept commands.
 	}
 
-	if (!isKonamiSCCmapperConfigured() && isWritingKonamiBankRegisterDisabled() && (addr < 0x6000)) {
+	if (isWritingKonamiBankRegisterDisabled() && (addr < 0x6000)) {
 		dac.writeDAC(value, time);
 	}
 
 	unsigned page8kB = (addr >> 13) - 2;
 	if (!areBankRegsAndOffsetRegsDisabled() && (page8kB < 4)) {
 		// (possibly) write to bank registers
-		if (isKonamiSCCmapperConfigured()) {
-			// Konami-SCC
-			if ((addr & 0x1800) == 0x1000) {
-				// Storing 'sccBanks' may seem redundant at
-				// first, but it's required to calculate
-				// whether the SCC is enabled or not.
-				sccBanks[page8kB] = value;
+		// Konami-SCC
+		if ((addr & 0x1800) == 0x1000) {
+			// Storing 'sccBanks' may seem redundant at
+			// first, but it's required to calculate
+			// whether the SCC is enabled or not.
+			sccBanks[page8kB] = value;
+			if (isKonamiSCCmapperConfigured()) {
 				// Masking of the mapper bits is done on
 				// write (and only in Konami(-scc) mode)
 				byte mask = areKonamiMapperLimitsEnabled() ? 0x3F : 0xFF;
 				bankRegs[page8kB] = value & mask;
-				invalidateMemCache(0x4000 + 0x2000 * page8kB, 0x2000);
 			}
-		} else {
-			// Konami
+			invalidateMemCache(0x4000 + 0x2000 * page8kB, 0x2000);
+		}
+		// Konami
+		if (!isKonamiSCCmapperConfigured()) {
 			if (isWritingKonamiBankRegisterDisabled() && (addr < 0x6000)) {
 				// Switching 0x4000-0x5FFF disabled.
 				// This bit blocks writing to the bank register
@@ -233,7 +233,7 @@ void KonamiUltimateCollection::writeMem(word addr, byte value, EmuTime::param ti
 				// It only has effect in Konami mode.
 				// DAC is already handled above.
 			} else {
-				// Making of the mapper bits is done on
+				// Masking of the mapper bits is done on
 				// write (and only in Konami(-scc) mode)
 				if (!((addr < 0x5000) || ((0x5800 <= addr) && (addr < 0x6000))))
 				{
