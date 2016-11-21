b01b102f693eecfe525647be7876026e88e5eaf0
Maarten ter Huurne <maarten@treewalker.org>
2016-11-22 00:56:53 +0100

    Decouple our key codes from SDL2
    I used the SDL1 values for the key codes, that way the key mapping
    tables in Keyboard.cc do not require updating.

--- src/events/Keys.cc
+++ src/events/Keys.cc
@@ -319,38 +319,189 @@ KeyCode getCode(string_ref name)
 
 KeyCode getCode(SDL_Keycode key, Uint16 mod, SDL_Scancode scancode, bool release)
 {
-	auto result = static_cast<KeyCode>(key);
-	if (result == 0) {
+	KeyCode result;
+	switch (key) {
+	case SDLK_BACKSPACE:      result = K_BACKSPACE;         break;
+	case SDLK_TAB:            result = K_TAB;               break;
+	case SDLK_CLEAR:          result = K_CLEAR;             break;
+	case SDLK_RETURN:         result = K_RETURN;            break;
+	case SDLK_PAUSE:          result = K_PAUSE;             break;
+	case SDLK_ESCAPE:         result = K_ESCAPE;            break;
+	case SDLK_SPACE:          result = K_SPACE;             break;
+	case SDLK_EXCLAIM:        result = K_EXCLAIM;           break;
+	case SDLK_QUOTEDBL:       result = K_QUOTEDBL;          break;
+	case SDLK_HASH:           result = K_HASH;              break;
+	case SDLK_DOLLAR:         result = K_DOLLAR;            break;
+	case SDLK_AMPERSAND:      result = K_AMPERSAND;         break;
+	case SDLK_QUOTE:          result = K_QUOTE;             break;
+	case SDLK_LEFTPAREN:      result = K_LEFTPAREN;         break;
+	case SDLK_RIGHTPAREN:     result = K_RIGHTPAREN;        break;
+	case SDLK_ASTERISK:       result = K_ASTERISK;          break;
+	case SDLK_PLUS:           result = K_PLUS;              break;
+	case SDLK_COMMA:          result = K_COMMA;             break;
+	case SDLK_MINUS:          result = K_MINUS;             break;
+	case SDLK_PERIOD:         result = K_PERIOD;            break;
+	case SDLK_SLASH:          result = K_SLASH;             break;
+	case SDLK_0:              result = K_0;                 break;
+	case SDLK_1:              result = K_1;                 break;
+	case SDLK_2:              result = K_2;                 break;
+	case SDLK_3:              result = K_3;                 break;
+	case SDLK_4:              result = K_4;                 break;
+	case SDLK_5:              result = K_5;                 break;
+	case SDLK_6:              result = K_6;                 break;
+	case SDLK_7:              result = K_7;                 break;
+	case SDLK_8:              result = K_8;                 break;
+	case SDLK_9:              result = K_9;                 break;
+	case SDLK_COLON:          result = K_COLON;             break;
+	case SDLK_SEMICOLON:      result = K_SEMICOLON;         break;
+	case SDLK_LESS:           result = K_LESS;              break;
+	case SDLK_EQUALS:         result = K_EQUALS;            break;
+	case SDLK_GREATER:        result = K_GREATER;           break;
+	case SDLK_QUESTION:       result = K_QUESTION;          break;
+	case SDLK_AT:             result = K_AT;                break;
+
+	case SDLK_LEFTBRACKET:    result = K_LEFTBRACKET;       break;
+	case SDLK_BACKSLASH:      result = K_BACKSLASH;         break;
+	case SDLK_RIGHTBRACKET:   result = K_RIGHTBRACKET;      break;
+	case SDLK_CARET:          result = K_CARET;             break;
+	case SDLK_UNDERSCORE:     result = K_UNDERSCORE;        break;
+	case SDLK_BACKQUOTE:      result = K_BACKQUOTE;         break;
+	case SDLK_a:              result = K_A;                 break;
+	case SDLK_b:              result = K_B;                 break;
+	case SDLK_c:              result = K_C;                 break;
+	case SDLK_d:              result = K_D;                 break;
+	case SDLK_e:              result = K_E;                 break;
+	case SDLK_f:              result = K_F;                 break;
+	case SDLK_g:              result = K_G;                 break;
+	case SDLK_h:              result = K_H;                 break;
+	case SDLK_i:              result = K_I;                 break;
+	case SDLK_j:              result = K_J;                 break;
+	case SDLK_k:              result = K_K;                 break;
+	case SDLK_l:              result = K_L;                 break;
+	case SDLK_m:              result = K_M;                 break;
+	case SDLK_n:              result = K_N;                 break;
+	case SDLK_o:              result = K_O;                 break;
+	case SDLK_p:              result = K_P;                 break;
+	case SDLK_q:              result = K_Q;                 break;
+	case SDLK_r:              result = K_R;                 break;
+	case SDLK_s:              result = K_S;                 break;
+	case SDLK_t:              result = K_T;                 break;
+	case SDLK_u:              result = K_U;                 break;
+	case SDLK_v:              result = K_V;                 break;
+	case SDLK_w:              result = K_W;                 break;
+	case SDLK_x:              result = K_X;                 break;
+	case SDLK_y:              result = K_Y;                 break;
+	case SDLK_z:              result = K_Z;                 break;
+	case SDLK_DELETE:         result = K_DELETE;            break;
+
+	// Numeric keypad
+	case SDLK_KP_0:           result = K_KP0;               break;
+	case SDLK_KP_1:           result = K_KP1;               break;
+	case SDLK_KP_2:           result = K_KP2;               break;
+	case SDLK_KP_3:           result = K_KP3;               break;
+	case SDLK_KP_4:           result = K_KP4;               break;
+	case SDLK_KP_5:           result = K_KP5;               break;
+	case SDLK_KP_6:           result = K_KP6;               break;
+	case SDLK_KP_7:           result = K_KP7;               break;
+	case SDLK_KP_8:           result = K_KP8;               break;
+	case SDLK_KP_9:           result = K_KP9;               break;
+	case SDLK_KP_PERIOD:      result = K_KP_PERIOD;         break;
+	case SDLK_KP_DIVIDE:      result = K_KP_DIVIDE;         break;
+	case SDLK_KP_MULTIPLY:    result = K_KP_MULTIPLY;       break;
+	case SDLK_KP_MINUS:       result = K_KP_MINUS;          break;
+	case SDLK_KP_PLUS:        result = K_KP_PLUS;           break;
+	case SDLK_KP_ENTER:       result = K_KP_ENTER;          break;
+	case SDLK_KP_EQUALS:      result = K_KP_EQUALS;         break;
+
+	// Arrows + Home/End pad
+	case SDLK_UP:             result = K_UP;                break;
+	case SDLK_DOWN:           result = K_DOWN;              break;
+	case SDLK_RIGHT:          result = K_RIGHT;             break;
+	case SDLK_LEFT:           result = K_LEFT;              break;
+	case SDLK_INSERT:         result = K_INSERT;            break;
+	case SDLK_HOME:           result = K_HOME;              break;
+	case SDLK_END:            result = K_END;               break;
+	case SDLK_PAGEUP:         result = K_PAGEUP;            break;
+	case SDLK_PAGEDOWN:       result = K_PAGEDOWN;          break;
+
+	// Function keys
+	case SDLK_F1:             result = K_F1;                break;
+	case SDLK_F2:             result = K_F2;                break;
+	case SDLK_F3:             result = K_F3;                break;
+	case SDLK_F4:             result = K_F4;                break;
+	case SDLK_F5:             result = K_F5;                break;
+	case SDLK_F6:             result = K_F6;                break;
+	case SDLK_F7:             result = K_F7;                break;
+	case SDLK_F8:             result = K_F8;                break;
+	case SDLK_F9:             result = K_F9;                break;
+	case SDLK_F10:            result = K_F10;               break;
+	case SDLK_F11:            result = K_F11;               break;
+	case SDLK_F12:            result = K_F12;               break;
+	case SDLK_F13:            result = K_F13;               break;
+	case SDLK_F14:            result = K_F14;               break;
+	case SDLK_F15:            result = K_F15;               break;
+
+	// Key state modifier keys
+	case SDLK_NUMLOCKCLEAR:   result = K_NUMLOCK;           break;
+	case SDLK_CAPSLOCK:       result = K_CAPSLOCK;          break;
+	case SDLK_SCROLLLOCK:     result = K_SCROLLOCK;         break;
+	case SDLK_RSHIFT:         result = K_RSHIFT;            break;
+	case SDLK_LSHIFT:         result = K_LSHIFT;            break;
+	case SDLK_RCTRL:          result = K_RCTRL;             break;
+	case SDLK_LCTRL:          result = K_LCTRL;             break;
+	case SDLK_RALT:           result = K_RALT;              break;
+	case SDLK_LALT:           result = K_LALT;              break;
+//	case SDLK_RMETA:          result = K_RMETA;             break;
+//	case SDLK_LMETA:          result = K_LMETA;             break;
+	case SDLK_LGUI:           result = K_LSUPER;            break; // Left "Windows" key
+	case SDLK_RGUI:           result = K_RSUPER;            break; // Right "Windows" key
+	case SDLK_MODE:           result = K_MODE;              break; // "Alt Gr" key
+//	case SDLK_COMPOSE:        result = K_COMPOSE;           break; // Multi-key compose key
+
+	// Miscellaneous function keys
+	case SDLK_HELP:           result = K_HELP;              break;
+	case SDLK_PRINTSCREEN:    result = K_PRINT;             break;
+	case SDLK_SYSREQ:         result = K_SYSREQ;            break;
+//	case SDLK_BREAK:          result = K_BREAK;             break;
+	case SDLK_MENU:           result = K_MENU;              break;
+	case SDLK_POWER:          result = K_POWER;             break; // Power Macintosh power key
+//	case SDLK_EURO:           result = K_EURO;              break; // Some european keyboards
+	case SDLK_UNDO:           result = K_UNDO;              break;
+
+	default:                  result = K_UNKNOWN;           break;
+	}
+
+	// Handle keys that don't have a key code but do have a scan code.
+	if (result == K_UNKNOWN) {
 		// Assume it is a Japanese keyboard and check
 		// scancode to recognize a few japanese
 		// specific keys for which SDL does not have an
 		// SDL_Keycode keysym definition.
 		switch (scancode) {
+		// Keys found on Japanese keyboards:
 		case 49:
-			result = static_cast<KeyCode>(K_ZENKAKU_HENKAKU);
+			result = K_ZENKAKU_HENKAKU;
 			break;
 		case 129:
-			result = static_cast<KeyCode>(K_HENKAN_MODE);
+			result = K_HENKAN_MODE;
 			break;
 		case 131:
-			result = static_cast<KeyCode>(K_MUHENKAN);
+			result = K_MUHENKAN;
 			break;
 		case 208:
-			result = static_cast<KeyCode>(K_HIRAGANA_KATAKANA);
+			result = K_HIRAGANA_KATAKANA;
 			break;
+		// Keys found on Korean keyboards:
+		case 56:
+			// On Korean keyboard this code is used for R-ALT key but SDL does
+			// not seem to recognize it, as reported by Miso Kim.
+			result = K_RALT;
 		default:
 			break; // nothing, silence compiler warning
 		}
 	}
-	if (result == 0) {
-		// Assume it is a Korean keyboard and check
-		// for scancode 56; on Korean keyboard it is used
-		// for R-ALT key but SDL does not seem to recognize it,
-		// as reported by Miso Kim
-		if  (scancode == 56) {
-			result = static_cast<KeyCode>(K_RALT);
-		}
-	}	
+
+	// Apply modifiers.
 	if (mod & KMOD_CTRL) {
 		result = static_cast<KeyCode>(result | KM_CTRL);
 	}
@@ -366,6 +517,7 @@ KeyCode getCode(SDL_Keycode key, Uint16 mod, SDL_Scancode scancode, bool release
 	if (mod & KMOD_MODE) {
 		result = static_cast<KeyCode>(result | KM_MODE);
 	}
+
 	if (release) {
 		result = static_cast<KeyCode>(result | KD_RELEASE);
 	}
