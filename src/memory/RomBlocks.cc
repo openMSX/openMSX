976119f64bee7d25061541fdab5a191d808d8344
m9710797 <vermaelen.wouter@gmail.com>
2017-04-08 14:43:11 +0200

    do not (yet) use terse static_assert
    Fix(?) the visual studio build.
    
    Terse static_assert is a c++17 feature. It's already supported by recent
    versions of gcc, clang and even visual studio (if you pass the /std:c++latest
    flag). Though maybe not everyone has access to such recent compilers yet, so
    let's wait a little longer before using it.

--- src/memory/RomBlocks.cc
+++ src/memory/RomBlocks.cc
@@ -22,7 +22,7 @@ RomBlocks<BANK_SIZE>::RomBlocks(
 		*this,  blockNr, 0x0000, 0x10000,
 		log2<BANK_SIZE>::value, debugBankSizeShift)
 {
-	static_assert(Math::isPowerOfTwo(BANK_SIZE));
+	static_assert(Math::isPowerOfTwo(BANK_SIZE), "BANK_SIZE must be a power of two");
 	auto extendedSize = (rom.getSize() + BANK_SIZE - 1) & ~(BANK_SIZE - 1);
 	rom.addPadding(extendedSize);
 	nrBlocks = rom.getSize() / BANK_SIZE;
