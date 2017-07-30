47db2ea467cc1b78c5531cb08c01dd0b04a0050d
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2017-07-30 23:03:23 +0200

    Prepare to tag openMSX 0.14.0-rc1

--- build/version.py
+++ build/version.py
@@ -12,16 +12,16 @@ import re
 packageName = 'openmsx'
 
 # Version number.
-packageVersionNumber = '0.13.0'
+packageVersionNumber = '0.14.0-rc1'
 
 # Version code for Android must be an incremental number
 # Increase this number for each release build. For a dev build, the
 # version number is based on the git commit count but for a release
 # build, it must be hardcoded
-androidReleaseVersionCode=6
+androidReleaseVersionCode=7
 
 # Note: suffix should be empty or with dash, like "-rc1" or "-test1"
-packageVersionSuffix = ''
+packageVersionSuffix = '-rc1'
 packageVersion = packageVersionNumber + packageVersionSuffix
 
 # Is this a release version ("True") or development version ("False").
