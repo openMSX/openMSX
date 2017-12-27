9123982e6a317eee2133e7bc627919ee2cbfde4c
m9710797 <vermaelen.wouter@gmail.com>
2017-12-27 14:28:45 +0100

    Fixed crash on extreme RenShaTurbo settings
    It was possible to trigger a division by zero when setting {min,max}_int inside
    the <RenShaTurbo> block on extreme values. Now we check whether these settings
    are in a sane range.
    
    Found while running afl-fuzz on openmsx machine configs.

--- src/RenShaTurbo.cc
+++ src/RenShaTurbo.cc
@@ -1,6 +1,7 @@
 #include "RenShaTurbo.hh"
 #include "XMLElement.hh"
 #include "Autofire.hh"
+#include "MSXException.hh"
 #include "memory.hh"
 
 namespace openmsx {
@@ -11,6 +12,11 @@ RenShaTurbo::RenShaTurbo(CommandController& commandController,
 	if (auto* config = machineConfig.findChild("RenShaTurbo")) {
 		int min_ints = config->getChildDataAsInt("min_ints", 47);
 		int max_ints = config->getChildDataAsInt("max_ints", 221);
+		if ((min_ints < 1) || (min_ints > max_ints) || (max_ints > 6000)) {
+			throw MSXException(
+				"Error in RenShaTurbo speed settings: "
+				"1 <= min_ints <= max_ints <= 6000.");
+		}
 		autofire = make_unique<Autofire>(
 			commandController, min_ints, max_ints, "renshaturbo");
 	}
