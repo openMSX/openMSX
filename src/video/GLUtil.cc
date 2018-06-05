09f2965d9512c1562dfbd3f58e519a60b6681616
m9710797 <vermaelen.wouter@gmail.com>
2018-06-05 20:56:01 +0200

    Pass #version directive to glsl shader programs
    Apparently this is needed to supress warnings on some systems. (The glsl
    standard says a missing #version directive should be interpreted as version
    1.10).

--- src/video/GLUtil.cc
+++ src/video/GLUtil.cc
@@ -177,7 +177,7 @@ Shader::Shader(GLenum type, const string& header, const string& filename)
 void Shader::init(GLenum type, const string& header, const string& filename)
 {
 	// Load shader source.
-	string source = header;
+	string source = "#version 110\n" + header;
 	try {
 		source += readTextFile("shaders/" + filename);
 	} catch (FileException& e) {
