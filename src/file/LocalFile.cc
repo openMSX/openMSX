openMSX/openMSX

commit 4fc1763ac39643b5ef1de1e946100f9b794222db

Replace 'array_ref' utility with 'span'
Our 'array_ref' utility was modeled after a proposal for c++ standardization.
That proposal has evolved quite a bit, and there's a good chance it will be
accepted in the c++20 standard. The most important changes compared to earlier
versions of the proposal are:

- It was renamed from 'array_ref' to 'span'.
- Both dynamic- and fixed-size spans are now possible. (Remember under the hood
  array_ref was basically a pointer and a size.) For a fixed-size span it's not
  necessary to explicitly store the size, instead it's part of the type itself.
- Better support for const vs non-const elements.
  E.g. span<int> vs. span<const int>.

This patch replaces our 'array_ref' utility with (still our own
re-implementation of) a 'span' utility. Apart from that, the majority of this
patch are mechanical renames (array_ref->span). In the future we can drop our
implementation and replace 'span' with 'std::span'.

src/file/LocalFile.cc
@@ -125,7 +125,7 @@ void LocalFile::write(const void* buffer, size_t num)
 }
 
 #if defined _WIN32
-array_ref<uint8_t> LocalFile::mmap()
+span<uint8_t> LocalFile::mmap()
 {
 	size_t size = getSize();
 	if (size == 0) return {nullptr, 0};
@@ -180,10 +180,10 @@ void LocalFile::munmap()
 }
 
 #elif HAVE_MMAP
-array_ref<uint8_t> LocalFile::mmap()
+span<uint8_t> LocalFile::mmap()
 {
 	size_t size = getSize();
-	if (size == 0) return {nullptr, 0};
+	if (size == 0) return {static_cast<uint8_t*>(nullptr), size};
 
 	if (!mmem) {
 		mmem = static_cast<uint8_t*>(

commit 26dd7bb4ec21849a8462d6ea95eea119a5af42c2

Fixed compile error in windows specific code
Error was introduced in 4fc1763ac3964.

src/file/LocalFile.cc
@@ -128,7 +128,7 @@ void LocalFile::write(const void* buffer, size_t num)
 span<uint8_t> LocalFile::mmap()
 {
 	size_t size = getSize();
-	if (size == 0) return {nullptr, 0};
+	if (size == 0) return {static_cast<uint8_t*>(nullptr), size};
 
 	if (!mmem) {
 		int fd = _fileno(file.get());