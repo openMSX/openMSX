openMSX/openMSX

commit dbbec50f3c3e8a0efbb1378fc864c8f9fa035020

Fix behaviour of "wrong" port config
As per TomH's CLK:
* a port configured as input will appear to be outputting 0xff to anyone
  elsewhere on the bus
This is now also implemented in openMSX. With this, the same behaviour as on a
real VG 8020/00 appears with the buggy XRacing 1.0 game.

The datasheet of the AY-3-8910 can be interpreted in such a way that it
describes this behaviour.

Fix implemented by Wouter, who asked me to commit it after testing with XRacing
1.0.

src/sound/AY8910.cc
@@ -623,11 +623,17 @@ void AY8910::wrtReg(unsigned reg, byte value, EmuTime::param time)
 		    !(oldValue & PORT_A_DIRECTION)) {
 			// Changed from input to output.
 			periphery.writeA(regs[AY_PORTA], time);
+		} else {
+			// Output -> input
+			periphery.writeA(0xff, time);
 		}
 		if ((value     & PORT_B_DIRECTION) &&
 		    !(oldValue & PORT_B_DIRECTION)) {
 			// Changed from input to output.
 			periphery.writeB(regs[AY_PORTB], time);
+		} else {
+			// Output -> input
+			periphery.writeB(0xff, time);
 		}
 		break;
 	case AY_PORTA:

commit b2811186afe6039fc638189b4d3cf4dceb6e6f77

Fixed bug introduced in dbbec50f3c
My prototype patch contained a mistake. This version should be better. though
still untested.

src/sound/AY8910.cc
@@ -588,7 +588,7 @@ void AY8910::wrtReg(unsigned reg, byte value, EmuTime::param time)
 	}
 
 	// Note: unused bits are stored as well; they can be read back.
-	byte oldValue = regs[reg];
+	byte diff = regs[reg] ^ value;
 	regs[reg] = value;
 
 	switch (reg) {
@@ -619,21 +619,25 @@ void AY8910::wrtReg(unsigned reg, byte value, EmuTime::param time)
 		envelope.setShape(value);
 		break;
 	case AY_ENABLE:
-		if ((value     & PORT_A_DIRECTION) &&
-		    !(oldValue & PORT_A_DIRECTION)) {
-			// Changed from input to output.
-			periphery.writeA(regs[AY_PORTA], time);
-		} else {
-			// Output -> input
-			periphery.writeA(0xff, time);
+		if (diff & PORT_A_DIRECTION) {
+			// port A changed
+			if (value & PORT_A_DIRECTION) {
+				// from input to output
+				periphery.writeA(regs[AY_PORTA], time);
+			} else {
+				// from output to input
+				periphery.writeA(0xff, time);
+			}
 		}
-		if ((value     & PORT_B_DIRECTION) &&
-		    !(oldValue & PORT_B_DIRECTION)) {
-			// Changed from input to output.
-			periphery.writeB(regs[AY_PORTB], time);
-		} else {
-			// Output -> input
-			periphery.writeB(0xff, time);
+		if (diff & PORT_B_DIRECTION) {
+			// port B changed
+			if (value & PORT_B_DIRECTION) {
+				// from input to output
+				periphery.writeB(regs[AY_PORTB], time);
+			} else {
+				// from output to input
+				periphery.writeB(0xff, time);
+			}
 		}
 		break;
 	case AY_PORTA: