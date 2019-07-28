openMSX/openMSX

commit c760c8ad558ba8af0c4a12309981b60184cff0fe

Added 'executeSubCommand() helper function
This simplifies the implementation of Tcl commands that consist out of
subcommands. But more importantly, it adds a better and more uniform error
message when you try to execute a wrong subcommand. For example:

Before:
  > debug probe break z80.acceptIRQ
  Syntax error
After:
  > debug probe break z80.acceptIRQ
  Unknown subcommand 'break'. Must be one of 'list', 'desc', 'read', 'set_bp', 'remove_bp' or 'list_bp'.

src/debugger/Debugger.cc
@@ -222,54 +222,29 @@ void Debugger::Cmd::execute(
 	span<const TclObject> tokens, TclObject& result, EmuTime::param /*time*/)
 {
 	checkNumArgs(tokens, AtLeast{2}, "subcommand ?arg ...?");
-	string_view subCmd = tokens[1].getString();
-	if (subCmd == "read") {
-		read(tokens, result);
-	} else if (subCmd == "read_block") {
-		readBlock(tokens, result);
-	} else if (subCmd == "write") {
-		write(tokens, result);
-	} else if (subCmd == "write_block") {
-		writeBlock(tokens, result);
-	} else if (subCmd == "size") {
-		size(tokens, result);
-	} else if (subCmd == "desc") {
-		desc(tokens, result);
-	} else if (subCmd == "list") {
-		list(result);
-	} else if (subCmd == "step") {
-		debugger().motherBoard.getCPUInterface().doStep();
-	} else if (subCmd == "cont") {
-		debugger().motherBoard.getCPUInterface().doContinue();
-	} else if (subCmd == "disasm") {
-		debugger().cpu->disasmCommand(getInterpreter(), tokens, result);
-	} else if (subCmd == "break") {
-		debugger().motherBoard.getCPUInterface().doBreak();
-	} else if (subCmd == "breaked") {
-		result = debugger().motherBoard.getCPUInterface().isBreaked();
-	} else if (subCmd == "set_bp") {
-		setBreakPoint(tokens, result);
-	} else if (subCmd == "remove_bp") {
-		removeBreakPoint(tokens, result);
-	} else if (subCmd == "list_bp") {
-		listBreakPoints(tokens, result);
-	} else if (subCmd == "set_watchpoint") {
-		setWatchPoint(tokens, result);
-	} else if (subCmd == "remove_watchpoint") {
-		removeWatchPoint(tokens, result);
-	} else if (subCmd == "list_watchpoints") {
-		listWatchPoints(tokens, result);
-	} else if (subCmd == "set_condition") {
-		setCondition(tokens, result);
-	} else if (subCmd == "remove_condition") {
-		removeCondition(tokens, result);
-	} else if (subCmd == "list_conditions") {
-		listConditions(tokens, result);
-	} else if (subCmd == "probe") {
-		probe(tokens, result);
-	} else {
-		throw SyntaxError();
-	}
+	executeSubCommand(tokens[1].getString(),
+		"read",              [&]{ read(tokens, result); },
+		"read_block",        [&]{ readBlock(tokens, result); },
+		"write",             [&]{ write(tokens, result); },
+		"write_block",       [&]{ writeBlock(tokens, result); },
+		"size",              [&]{ size(tokens, result); },
+		"desc",              [&]{ desc(tokens, result); },
+		"list",              [&]{ list(result); },
+		"step",              [&]{ debugger().motherBoard.getCPUInterface().doStep(); },
+		"cont",              [&]{ debugger().motherBoard.getCPUInterface().doContinue(); },
+		"disasm",            [&]{ debugger().cpu->disasmCommand(getInterpreter(), tokens, result); },
+		"break",             [&]{ debugger().motherBoard.getCPUInterface().doBreak(); },
+		"breaked",           [&]{ debugger().motherBoard.getCPUInterface().isBreaked(); },
+		"set_bp",            [&]{ setBreakPoint(tokens, result); },
+		"remove_bp",         [&]{ removeBreakPoint(tokens, result); },
+		"list_bp",           [&]{ listBreakPoints(tokens, result); },
+		"set_watchpoint",    [&]{ setWatchPoint(tokens, result); },
+		"remove_watchpoint", [&]{ removeWatchPoint(tokens, result); },
+		"list_watchpoints",  [&]{ listWatchPoints(tokens, result); },
+		"set_condition",     [&]{ setCondition(tokens, result); },
+		"remove_condition",  [&]{ removeCondition(tokens, result); },
+		"list_conditions",   [&]{ listConditions(tokens, result); },
+		"probe",             [&]{ probe(tokens, result); });
 }
 
 void Debugger::Cmd::list(TclObject& result)
@@ -623,22 +598,13 @@ void Debugger::Cmd::listConditions(
 void Debugger::Cmd::probe(span<const TclObject> tokens, TclObject& result)
 {
 	checkNumArgs(tokens, AtLeast{3}, "subcommand ?arg ...?");
-	string_view subCmd = tokens[2].getString();
-	if (subCmd == "list") {
-		probeList(tokens, result);
-	} else if (subCmd == "desc") {
-		probeDesc(tokens, result);
-	} else if (subCmd == "read") {
-		probeRead(tokens, result);
-	} else if (subCmd == "set_bp") {
-		probeSetBreakPoint(tokens, result);
-	} else if (subCmd == "remove_bp") {
-		probeRemoveBreakPoint(tokens, result);
-	} else if (subCmd == "list_bp") {
-		probeListBreakPoints(tokens, result);
-	} else {
-		throw SyntaxError();
-	}
+	executeSubCommand(tokens[2].getString(),
+		"list",      [&]{ probeList(tokens, result); },
+		"desc",      [&]{ probeDesc(tokens, result); },
+		"read",      [&]{ probeRead(tokens, result); },
+		"set_bp",    [&]{ probeSetBreakPoint(tokens, result); },
+		"remove_bp", [&]{ probeRemoveBreakPoint(tokens, result); },
+		"list_bp",   [&]{ probeListBreakPoints(tokens, result); });
 }
 void Debugger::Cmd::probeList(span<const TclObject> /*tokens*/, TclObject& result)
 {

src/ReverseManager.cc
@@ -967,42 +967,31 @@ void ReverseManager::ReverseCmd::execute(span<const TclObject> tokens, TclObject
 	checkNumArgs(tokens, AtLeast{2}, "subcommand ?arg ...?");
 	auto& manager = OUTER(ReverseManager, reverseCmd);
 	auto& interp = getInterpreter();
-	string_view subcommand = tokens[1].getString();
-	if        (subcommand == "start") {
-		manager.start();
-	} else if (subcommand == "stop") {
-		manager.stop();
-	} else if (subcommand == "status") {
-		manager.status(result);
-	} else if (subcommand == "debug") {
-		manager.debugInfo(result);
-	} else if (subcommand == "goback") {
-		manager.goBack(tokens);
-	} else if (subcommand == "goto") {
-		manager.goTo(tokens);
-	} else if (subcommand == "savereplay") {
-		return manager.saveReplay(interp, tokens, result);
-	} else if (subcommand == "loadreplay") {
-		return manager.loadReplay(interp, tokens, result);
-	} else if (subcommand == "viewonlymode") {
-		auto& distributor = manager.motherBoard.getStateChangeDistributor();
-		switch (tokens.size()) {
-		case 2:
-			result = distributor.isViewOnlyMode();
-			break;
-		case 3:
-			distributor.setViewOnlyMode(tokens[2].getBoolean(interp));
-			break;
-		default:
-			throw SyntaxError();
-		}
-	} else if (subcommand == "truncatereplay") {
-		if (manager.isReplaying()) {
-			manager.signalStopReplay(manager.getCurrentTime());
-		}
-	} else {
-		throw CommandException("Invalid subcommand: ", subcommand);
-	}
+	executeSubCommand(tokens[1].getString(),
+		"start",      [&]{ manager.start(); },
+		"stop",       [&]{ manager.stop(); },
+		"status",     [&]{ manager.status(result); },
+		"debug",      [&]{ manager.debugInfo(result); },
+		"goback",     [&]{ manager.goBack(tokens); },
+		"goto",       [&]{ manager.goTo(tokens); },
+		"savereplay", [&]{ manager.saveReplay(interp, tokens, result); },
+		"loadreplay", [&]{ manager.loadReplay(interp, tokens, result); },
+		"viewonlymode", [&]{
+			auto& distributor = manager.motherBoard.getStateChangeDistributor();
+			switch (tokens.size()) {
+			case 2:
+				result = distributor.isViewOnlyMode();
+				break;
+			case 3:
+				distributor.setViewOnlyMode(tokens[2].getBoolean(interp));
+				break;
+			default:
+				throw SyntaxError();
+			}},
+		"truncatereplay", [&] {
+			if (manager.isReplaying()) {
+				manager.signalStopReplay(manager.getCurrentTime());
+			}});
 }
 
 string ReverseManager::ReverseCmd::help(const vector<string>& /*tokens*/) const

src/commands/Command.hh
@@ -4,6 +4,8 @@
 #include "Completer.hh"
 #include "span.hh"
 #include "string_view.hh"
+#include "strCat.hh"
+#include "CommandException.hh"
 #include <vector>
 
 namespace openmsx {
@@ -37,6 +39,8 @@ private:
 
 class Command : public CommandCompleter
 {
+	struct UnknownSubCommand {};
+
 public:
 	/** Execute this command.
 	  * @param tokens Tokenized command line;
@@ -64,9 +68,49 @@ public:
 	void setToken(void* token_) { assert(!token); token = token_; }
 	void* getToken() const { return token; }
 
+	// helper to delegate to a subcommand
+	template<typename... Args>
+	void executeSubCommand(string_view subcmd, Args&&... args) {
+		try {
+			executeSubCommandImpl(subcmd, std::forward<Args>(args)...);
+		} catch (UnknownSubCommand) {
+			unknownSubCommand(subcmd, std::forward<Args>(args)...);
+		}
+	}
+
 protected:
 	Command(CommandController& controller, string_view name);
 	~Command();
+
+private:
+	template<typename Func, typename... Args>
+	void executeSubCommandImpl(string_view subcmd, string_view candidate, Func func, Args&&... args) {
+		if (subcmd == candidate) {
+			func();
+		} else {
+			executeSubCommandImpl(subcmd, std::forward<Args>(args)...);
+		}
+	}
+	void executeSubCommandImpl(string_view /*subcmd*/) {
+		throw UnknownSubCommand{}; // exhausted all possible candidates
+	}
+
+	template<typename Func, typename... Args>
+	void unknownSubCommand(string_view subcmd, string_view firstCandidate, Func /*func*/, Args&&... args) {
+		unknownSubCommandImpl(strCat("Unknown subcommand '", subcmd, "'. Must be one of '", firstCandidate, '\''),
+		                      std::forward<Args>(args)...);
+	}
+	template<typename Func, typename... Args>
+	void unknownSubCommandImpl(std::string message, string_view candidate, Func /*func*/, Args&&... args) {
+		strAppend(message, ", '", candidate, '\'');
+		unknownSubCommandImpl(message, std::forward<Args>(args)...);
+		throw SyntaxError();
+	}
+	template<typename Func>
+	void unknownSubCommandImpl(std::string message, string_view lastCandidate, Func /*func*/) {
+		strAppend(message, " or '", lastCandidate, "'.");
+		throw CommandException(message);
+	}
 
 private:
 	bool allowInEmptyMachine;

src/console/OSDGUI.cc
@@ -41,26 +41,12 @@ OSDGUI::OSDCommand::OSDCommand(CommandController& commandController_)
 void OSDGUI::OSDCommand::execute(span<const TclObject> tokens, TclObject& result)
 {
 	checkNumArgs(tokens, AtLeast{2}, "subcommand ?arg ...?");
-	auto& gui = OUTER(OSDGUI, osdCommand);
-	string_view subCommand = tokens[1].getString();
-	if (subCommand == "create") {
-		create(tokens, result);
-		gui.refresh();
-	} else if (subCommand == "destroy") {
-		destroy(tokens, result);
-		gui.refresh();
-	} else if (subCommand == "info") {
-		info(tokens, result);
-	} else if (subCommand == "exists") {
-		exists(tokens, result);
-	} else if (subCommand == "configure") {
-		configure(tokens, result);
-		gui.refresh();
-	} else {
-		throw CommandException(
-			"Invalid subcommand '", subCommand, "', expected "
-			"'create', 'destroy', 'info', 'exists' or 'configure'.");
-	}
+	executeSubCommand(tokens[1].getString(),
+		"create",    [&]{ create(tokens, result); },
+		"destroy",   [&]{ destroy(tokens, result); },
+		"info",      [&]{ info(tokens, result); },
+		"exists",    [&]{ exists(tokens, result); },
+		"configure", [&]{ configure(tokens, result); });
 }
 
 void OSDGUI::OSDCommand::create(span<const TclObject> tokens, TclObject& result)
@@ -92,6 +78,7 @@ void OSDGUI::OSDCommand::create(span<const TclObject> tokens, TclObject& result)
 	parent->addWidget(std::move(widget));
 
 	result = fullname;
+	gui.refresh();
 }
 
 unique_ptr<OSDWidget> OSDGUI::OSDCommand::create(
@@ -131,6 +118,7 @@ void OSDGUI::OSDCommand::destroy(span<const TclObject> tokens, TclObject& result
 	top.removeName(*widget);
 	parent->deleteWidget(*widget);
 	result = true;
+	gui.refresh();
 }
 
 void OSDGUI::OSDCommand::info(span<const TclObject> tokens, TclObject& result)
@@ -170,6 +158,8 @@ void OSDGUI::OSDCommand::configure(span<const TclObject> tokens, TclObject& /*re
 {
 	checkNumArgs(tokens, AtLeast{3}, "name ?property value ...?");
 	configure(getWidget(tokens[2].getString()), tokens.subspan(3));
+	auto& gui = OUTER(OSDGUI, osdCommand);
+	gui.refresh();
 }
 
 void OSDGUI::OSDCommand::configure(OSDWidget& widget, span<const TclObject> tokens)

src/settings/UserSettings.cc
@@ -57,18 +57,10 @@ UserSettings::Cmd::Cmd(CommandController& commandController_)
 void UserSettings::Cmd::execute(span<const TclObject> tokens, TclObject& result)
 {
 	checkNumArgs(tokens, AtLeast{2}, "subcommand ?arg ...?");
-	const auto& subCommand = tokens[1].getString();
-	if (subCommand == "create") {
-		create(tokens, result);
-	} else if (subCommand == "destroy") {
-		destroy(tokens, result);
-	} else if (subCommand == "info") {
-		info(tokens, result);
-	} else {
-		throw CommandException(
-			"Invalid subcommand '", subCommand,
-			"', expected 'create', 'destroy' or 'info'.");
-	}
+	executeSubCommand(tokens[1].getString(),
+		"create",  [&]{ create(tokens, result); },
+		"destroy", [&]{ destroy(tokens, result); },
+		"info",    [&]{ info(tokens, result); });
 }
 
 void UserSettings::Cmd::create(span<const TclObject> tokens, TclObject& result)

src/video/AviRecorder.cc
@@ -293,20 +293,15 @@ void AviRecorder::Cmd::execute(span<const TclObject> tokens, TclObject& result)
 		throw CommandException("Missing argument");
 	}
 	auto& recorder = OUTER(AviRecorder, recordCommand);
-	const string_view subcommand = tokens[1].getString();
-	if (subcommand == "start") {
-		recorder.processStart(getInterpreter(), tokens, result);
-	} else if (subcommand == "stop") {
-		checkNumArgs(tokens, 2, Prefix{2}, nullptr);
-		recorder.processStop(tokens);
-	} else if (subcommand == "toggle") {
-		recorder.processToggle(getInterpreter(), tokens, result);
-	} else if (subcommand == "status") {
-		checkNumArgs(tokens, 2, Prefix{2}, nullptr);
-		recorder.status(tokens, result);
-	} else {
-		throw SyntaxError();
-	}
+	executeSubCommand(tokens[1].getString(),
+		"start",  [&]{ recorder.processStart(getInterpreter(), tokens, result); },
+		"stop",   [&]{
+			checkNumArgs(tokens, 2, Prefix{2}, nullptr);
+			recorder.processStop(tokens); },
+		"toggle", [&]{ recorder.processToggle(getInterpreter(), tokens, result); },
+		"status", [&]{
+			checkNumArgs(tokens, 2, Prefix{2}, nullptr);
+			recorder.status(tokens, result); });
 }
 
 string AviRecorder::Cmd::help(const vector<string>& /*tokens*/) const

commit 09c7e786029edcc94c36bfa393b696dddb2d758d

Fix Coverity#1484554 Useless call
This fixes the 'debug breaked' command. Got broken in c760c8ad55 during
subcommand refactoring.

src/debugger/Debugger.cc
@@ -234,7 +234,7 @@ void Debugger::Cmd::execute(
 		"cont",              [&]{ debugger().motherBoard.getCPUInterface().doContinue(); },
 		"disasm",            [&]{ debugger().cpu->disasmCommand(getInterpreter(), tokens, result); },
 		"break",             [&]{ debugger().motherBoard.getCPUInterface().doBreak(); },
-		"breaked",           [&]{ debugger().motherBoard.getCPUInterface().isBreaked(); },
+		"breaked",           [&]{ result = debugger().motherBoard.getCPUInterface().isBreaked(); },
 		"set_bp",            [&]{ setBreakPoint(tokens, result); },
 		"remove_bp",         [&]{ removeBreakPoint(tokens, result); },
 		"list_bp",           [&]{ listBreakPoints(tokens, result); },