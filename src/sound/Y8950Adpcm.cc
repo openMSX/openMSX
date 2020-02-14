openMSX/openMSX

commit e5dc580e0dfee868b0852b676d34802e653765db

Partial fix for #1160: Y8950 ADPCM implementation
This fixes points 1 and 3 of
  https://github.com/openMSX/openMSX/issues/1160
See the discussion on that issue for more details. Very short summary:
 1) openMSX sets EOS one byte too late
 3) continue to write after EOS wraps around
Still TODO:
 2) Timing of the BUFRDY and EOS status bits

src/sound/Y8950Adpcm.cc
@@ -276,11 +276,16 @@ void Y8950Adpcm::writeData(byte data)
 			// set the flag in zero time, so that the IRQ
 			// will work.
 
-			// set BRDY bit in status register
-			y8950.setStatus(Y8950::STATUS_BUF_RDY);
-		} else {
-			// set EOS bit in status register
-			y8950.setStatus(Y8950::STATUS_EOS);
+			if (emu.memPntr <= stopAddr) {
+				// there's more to transfer: set BRDY
+				y8950.setStatus(Y8950::STATUS_BUF_RDY);
+			} else {
+				// we just received the last byte: set EOS
+				y8950.setStatus(Y8950::STATUS_EOS);
+				// Eugeny tested that pointer wraps when
+				// continue writing after EOS
+				emu.memPntr = startAddr;
+			}
 		}
 
 	} else if ((reg7 & R07_MODE) == 0x80) {

commit 5e972f28e32e76ebd24d5ac8ed009c57b6acc112

Fix issue #1160, regression introduced in e5dc580
In case of EOS, BUF_RDY must be set as well.
See comments in the code for details.

src/sound/Y8950Adpcm.cc
@@ -270,17 +270,17 @@ void Y8950Adpcm::writeData(byte data)
 
 			// setup a timer that will callback us in 10
 			// master clock cycles for Y8950. In the
-			// callback set the BRDY flag to 1 , which
+			// callback set the BRDY flag to 1, which
 			// means we have written the data. For now, we
 			// don't really do this; we simply reset and
 			// set the flag in zero time, so that the IRQ
 			// will work.
+			y8950.setStatus(Y8950::STATUS_BUF_RDY);
 
-			if (emu.memPntr <= stopAddr) {
-				// there's more to transfer: set BRDY
-				y8950.setStatus(Y8950::STATUS_BUF_RDY);
-			} else {
+			if (emu.memPntr > stopAddr) {
 				// we just received the last byte: set EOS
+				// verified on real HW:
+				//  in case of EOS, BUF_RDY is set as well
 				y8950.setStatus(Y8950::STATUS_EOS);
 				// Eugeny tested that pointer wraps when
 				// continue writing after EOS