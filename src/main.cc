1ccd78ac127399466c1294511fd7f8a413103e62
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2020-05-18 22:13:34 +0200

    Provide workaround for Windows not having setenv...
    I hope...

--- src/main.cc
+++ src/main.cc
@@ -39,6 +39,22 @@
 
 namespace openmsx {
 
+#ifdef _WIN32
+// wrapper for Windows, as the MS runtime doesn't provide setenv!?
+int setenv(const char *name, const char *value, int overwrite)
+{
+	int errcode = 0;
+	if (!overwrite) {
+		size_t envsize = 0;
+		errcode = getenv_s(&envsize, NULL, 0, name);
+		if (errcode || envsize) {
+			return errcode;
+		}
+	}
+	return _putenv_s(name, value);
+}
+#endif
+
 static void initializeSDL()
 {
 	int flags = 0;
