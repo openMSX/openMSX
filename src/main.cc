27f1e1d0662c433010382c3de79065052e965b4f
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2020-05-19 21:59:31 +0200

    Fix MinGW build by using simple getenv in workaround

--- src/main.cc
+++ src/main.cc
@@ -40,15 +40,15 @@
 namespace openmsx {
 
 #ifdef _WIN32
+#include <stdlib.h>
 // wrapper for Windows, as the MS runtime doesn't provide setenv!?
+int setenv(const char *name, const char *value, int overwrite);
 int setenv(const char *name, const char *value, int overwrite)
 {
-	int errcode = 0;
 	if (!overwrite) {
-		size_t envsize = 0;
-		errcode = getenv_s(&envsize, NULL, 0, name);
-		if (errcode || envsize) {
-			return errcode;
+		char* oldvalue = getenv(name);
+		if (oldvalue) {
+			return 0;
 		}
 	}
 	return _putenv_s(name, value);
