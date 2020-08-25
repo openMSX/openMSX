a46a81485a852101252f9319617aac67cb9ff54a
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2020-08-25 23:16:08 +0200

    Update for final release 16.0.

--- build/version.py
+++ build/version.py
@@ -18,10 +18,10 @@ packageVersionNumber = '16.0'
 # Increase this number for each release build. For a dev build, the
 # version number is based on the git commit count but for a release
 # build, it must be hardcoded
-androidReleaseVersionCode=11
+androidReleaseVersionCode=12
 
 # Note: suffix should be empty or with dash, like "-rc1" or "-test1"
-packageVersionSuffix = '-rc2'
+packageVersionSuffix = ''
 packageVersion = packageVersionNumber + packageVersionSuffix
 
 # Is this a release version ("True") or development version ("False").
