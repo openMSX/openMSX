39a1d21bc896c7e343bf33fa5bbf15ca42b2ce63
m9710797 <vermaelen.wouter@gmail.com>
2020-08-27 20:13:46 +0200

    [4/5] Added SimpleHashSet
    Similar to the previous patch. The new FilePool implementation needs a
    new SimpleHashSet utility. This patch adds that utility with a test but
    doesn't actually use the utility yet.
    
    See the comments in the code for lots of details. But here's a summary:
     * As the name implies SimpleHashSet implements a simple collection
       which models a mathematical set whose implementation is based on
       hashing.
     * So functionally this is similar to the standard std::unordered_set
       class or our hash_set class. Compared to these two classes
       SimpleHashSet has some limitation. The two most important of these
       are:
       - There must be a special value that cannot occur in the set (e.g. if
         the set contains integers, that special value could be '-1' or
         '0').
       - Objects in the set do not have stable addresses. That is when the
         set runs out of capacity, a new memory block is allocated and all
         objects are moved to that new block.
     * If these limitations are acceptable, then this class (typically)
       performs better than the above two alternatives and also uses less
       memory. This is especially true if the set contains simple datatypes
       (like integers or pointers).

---
+++ src/unittest/SimpleHashSet_test.cc
@@ -0,0 +1,77 @@
+#include "catch.hpp"
+
+#include "SimpleHashSet.hh"
+
+
+TEST_CASE("SimpleHashSet")
+{
+	// construct
+	SimpleHashSet<int, -1, std::hash<int>, std::equal_to<>> s;
+	CHECK(s.capacity() == 0);
+	CHECK(s.size() == 0);
+	CHECK(s.empty());
+
+	// insert 42
+	CHECK(!s.contains(42));
+	CHECK(!s.find(42));
+	CHECK(s.insert(42)); // {42}
+	CHECK(s.contains(42));
+	CHECK(*(s.find(42)) == 42);
+	CHECK(s.capacity() == 2);
+	CHECK(s.size() == 1);
+	CHECK(!s.empty());
+
+	// reserve
+	s.reserve(4);
+	CHECK(s.capacity() == 4);
+	CHECK(s.size() == 1);
+
+	// insert 42 again
+	CHECK(!s.insert(42)); // {42}
+	CHECK(s.contains(42));
+	CHECK(*(s.find(42)) == 42);
+	CHECK(s.capacity() == 4);
+	CHECK(s.size() == 1);
+
+	// insert 1, 2, 3
+	CHECK(s.insert(1));
+	CHECK(s.insert(2));
+	CHECK(s.insert(3)); // {1, 2, 3, 42}
+	CHECK(*s.find(42) == 42);
+	CHECK(*s.find(1) == 1);
+	CHECK(*s.find(2) == 2);
+	CHECK(*s.find(3) == 3);
+	CHECK(!s.find(4));
+	CHECK(!s.find(5));
+	CHECK(s.size() == 4);
+	CHECK(s.capacity() == 4);
+
+	// insert 10, must grow
+	CHECK(s.insert(10)); // {1, 2, 3, 10, 42}
+	CHECK(*s.find(42) == 42);
+	CHECK(*s.find(1) == 1);
+	CHECK(*s.find(2) == 2);
+	CHECK(*s.find(3) == 3);
+	CHECK(!s.find(4));
+	CHECK(!s.find(5));
+	CHECK(*s.find(10) == 10);
+	CHECK(s.size() == 5);
+	CHECK(s.capacity() == 8);
+
+	// erase non-existing
+	CHECK(!s.erase(99));
+	CHECK(s.size() == 5);
+
+	// erase 1, 3
+	CHECK(s.erase(1));
+	CHECK(s.erase(3)); // {2, 10, 42}
+	CHECK(*s.find(42) == 42);
+	CHECK(!s.find(1));
+	CHECK(*s.find(2) == 2);
+	CHECK(!s.find(3));
+	CHECK(!s.find(4));
+	CHECK(!s.find(5));
+	CHECK(*s.find(10) == 10);
+	CHECK(s.size() == 3);
+	CHECK(s.capacity() == 8);
+}
