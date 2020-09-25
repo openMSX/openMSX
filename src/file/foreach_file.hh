openMSX/openMSX

commit cab2f971bc7af92a26c5c9ce7c4ae551aa28a2b0

[3/7] Optimize looping over files/sub-directories in a directory
This patch adds 3 new utility functions:
  foreach_file()
  foreach_file_and_directory()
  foreach_file_recursive()
See the comments in the code for lots more details on these functions.
Here I'll only motivate the need for these new functions.

Before this patch, to loop over files, we typically had code like this:

	ReadDir dir(path);
	while (dirent* entry = dir.getEntry()) {
		string_view name = entry->d_name;
		std::string fullname = FileOperations::join(path, name);
		if (FileOperations::isRegularFile(fullname)) {
			// do something for this file
		}
	}

The 'heaptrack' tool showed that (during startup) loops like this were
creating lots of temporary string objects. It's not difficult to
eliminate these: move the 'fullname' string before the loop and
initially only append a '/' character. Inside the loop append the
entry-name and remove it again at the end of the iteration. This way the
existing string capacity gets reused over the loop iterations and we
only need to allocate it  once (or a few times to grow to maximum
capacity).

In some cases we can also eliminate a system call. The libc function
readdir() returns information in a struct 'dirent'. One field of this
struct is 'd_type'. This indicate whether the entry is a file, a
directory, ... So with this we don't need the isRegularFile() call
anymore. However the 'd_type' field is allowed to contain the value
'DT_UNKOWN' because not all operating systems (e.g. Windows) support it,
and systems that do support it (e.g. Linux) only do for specific
filesystems (e.g. ext4 does support this. I assume this info is only
filled in when it's available without doing extra disk reads). For this
reason it's still required to have fallback code that does call
isRegularFile().

Both of the above optimizations make the example code quite a bit more
complex. To solve this I've hidden that complexity in the new helper

src/file/foreach_file.hh
@@ -1,0 +1,172 @@
+#ifndef FOREACH_FILE_HH
+#define FOREACH_FILE_HH
+
+#include "FileOperations.hh"
+#include "ReadDir.hh"
+#include "one_of.hh"
+#include <functional>
+#include <string_view>
+#include <tuple>
+
+/** This file implemented 3 utility functions:
+ *  - foreach_file              (path, fileAction)
+ *  - foreach_file_and_directory(path, fileAction, dirAction)
+ *  - foreach_file_recursive    (path, fileAction)
+ *
+ * These traverse all files/directories in a directory and invoke some action
+ * for respectively:
+ *  - All files in this directory.
+ *  - All files and all sub-directory in this directory (different action for both).
+ *  - All files in the directory and recursively all sub-directories.
+ *
+ * The action(s) must accept the following parameters:
+ *  - a '(const) std::string&' parameter:
+ *     This is the full path for the current directory entry. Thus the initial
+ *     path passed to foreach_xxx() appended with the name of the current
+ *     entry. If this parameter is non-const, the action is allowed to modify
+ *     it, BUT it MUST restore it to its original value before the action
+ *     returns (e.g. you may temporarily extend this string).
+ *  - Optionally: a 'std::string_view' parameter:
+ *     This is the name of the current entry. It's a view on the last
+ *     path-component from the previous parameter. If an action is not
+ *     interested in this parameter it can omit it from its function signature.
+ *  - Optionally: a 'FileOperations::Stat' parameter:
+ *     This is the result of calling 'FileOperations::getStat()' on the current
+ *     directory entry. If an action is not interested in this parameter it can
+ *     omit it from its function signature. On some systems foreach_xxx() can
+ *     be a bit more efficient when this information is not needed.
+ *
+ * The actions' return type must either be:
+ *  - void
+ *  - bool: when an action returns 'false', directory traversal will be aborted.
+ *
+ * The result of the foreach_xxx() function is
+ *  - true: directory was fully traversed.
+ *  - false: some action requested an abort.
+ */
+
+namespace openmsx {
+	namespace detail {
+		using namespace FileOperations;
+
+		// Take an action that accepts the following parameters:
+		// - a 'std::string&' or 'const std::string&' parameter
+		// - optionally a std::string_view parameter
+		// - optionally a 'FileOperations::Stat&' parameter
+		// And returns a wrapper that makes the 2nd and 3rd parameter
+		// non-optional, but (possibly) ignores those parameters when
+		// delegating to the wrapped action.
+		template<typename Action>
+		auto adaptParams(Action action) {
+			if constexpr (std::is_invocable_v<Action, std::string&, std::string_view, Stat&>) {
+				return std::tuple(action, true);
+			} else if constexpr (std::is_invocable_v<Action, std::string&, std::string_view>) {
+				return std::tuple([action](std::string& p, std::string_view f, Stat& /*st*/) {
+				                          return action(p, f);
+				                  }, false);
+			} else if constexpr (std::is_invocable_v<Action, std::string&, Stat&>) {
+				return std::tuple([action](std::string& p, std::string_view /*f*/, Stat& st) {
+				                          return action(p, st);
+				                  }, true);
+			} else if constexpr (std::is_invocable_v<Action, std::string&>) {
+				return std::tuple([action](std::string& p, std::string_view /*f*/, Stat& /*st*/) {
+				                          return action(p);
+				                  }, false);
+			} else {
+				static_assert((Action{}, false), "Wrong signature for action");
+			}
+		}
+
+		// Take an action and
+		// - return it unchanged if it already returned a non-void result
+		// - otherwise return a wrapper that invokes the given action and then returns 'true'.
+		template<typename Action>
+		auto adaptReturn(Action action) {
+			using ResultType = std::invoke_result_t<Action, std::string&, std::string_view, Stat&>;
+			if constexpr (std::is_same_v<ResultType, void>) {
+				return [=](auto&&... params) {
+					action(std::forward<decltype(params)>(params)...);
+					return true; // continue directory-traversal
+				};
+			} else {
+				return action;
+			}
+		}
+
+		template<typename FileAction, typename DirAction>
+		bool foreach_dirent(std::string& path, FileAction fileAction, DirAction dirAction) {
+			auto [invokeFile, statFile] = adaptParams(fileAction);
+			auto [invokeDir,  statDir ] = adaptParams(dirAction);
+			auto invokeFileAction = adaptReturn(invokeFile);
+			auto invokeDirAction  = adaptReturn(invokeDir);
+			bool needStat = statFile || statDir;
+
+			ReadDir dir(path);
+			path += '/';
+			auto origLen = path.size();
+			while (dirent* d = dir.getEntry()) {
+				std::string_view f(d->d_name);
+				if (f == one_of(".", "..")) continue;
+				path += f;
+				auto file = std::string_view(path).substr(origLen);
+
+				// When the action is not interested in 'Stat' and on operation systems
+				// that support it: avoid doing an extra 'stat()' system call.
+				auto type = needStat ? static_cast<unsigned char>(DT_UNKNOWN) : d->d_type;
+				Stat st;
+				if (type == DT_REG) {
+					if (!invokeFileAction(path, file, st)) {
+						return false; // aborted
+					}
+				} else if (type == DT_DIR) {
+					if (!invokeDirAction(path, file, st)) {
+						return false;
+					}
+				} else {
+					if (getStat(path, st)) {
+						if (isRegularFile(st)) {
+							if (!invokeFileAction(path, file, st)) {
+								return false;
+							}
+						} else if (isDirectory(st)) {
+							if (!invokeDirAction(path, file, st)) {
+								return false;
+							}
+						}
+					}
+				}
+
+				path.resize(origLen);
+			}
+			path.pop_back();
+
+			return true; // finished normally
+		}
+	}
+
+	template<typename FileAction>
+	bool foreach_file(std::string path, FileAction fileAction)
+	{
+		auto dirAction = [&](const std::string& /*dirPath*/) { /*nothing*/ };
+		return detail::foreach_dirent(path, fileAction, dirAction);
+	}
+
+	template<typename FileAction, typename DirAction>
+	bool foreach_file_and_directory(std::string path, FileAction fileAction, DirAction dirAction)
+	{
+		return detail::foreach_dirent(path, fileAction, dirAction);
+	}
+
+	template<typename FileAction>
+	bool foreach_file_recursive(std::string path, FileAction fileAction)
+	{
+		std::function<bool(std::string&)> dirAction;
+		dirAction = [&](std::string& dirPath) {
+			return detail::foreach_dirent(dirPath, fileAction, dirAction);
+		};
+		return dirAction(path);
+	}
+
+} // namespace openmsx
+
+#endif

src/Reactor.cc
@@ -34,7 +34,7 @@
 #include "FileContext.hh"
 #include "FileException.hh"
 #include "FileOperations.hh"
-#include "ReadDir.hh"
+#include "foreach_file.hh"
 #include "Thread.hh"
 #include "Timer.hh"
 #include "serialize.hh"
@@ -327,23 +327,21 @@ vector<string> Reactor::getHwConfigs(string_view type)
 {
 	vector<string> result;
 	for (auto& p : systemFileContext().getPaths()) {
-		const auto& path = FileOperations::join(p, type);
-		ReadDir configsDir(path);
-		while (auto* entry = configsDir.getEntry()) {
-			string_view name = entry->d_name;
-			const auto& fullname = FileOperations::join(path, name);
-			if (StringOp::endsWith(name, ".xml") &&
-			    FileOperations::isRegularFile(fullname)) {
+		auto fileAction = [&](const std::string& /*path*/, std::string_view name) {
+			if (StringOp::endsWith(name, ".xml")) {
 				name.remove_suffix(4);
 				result.emplace_back(name);
-			} else if (FileOperations::isDirectory(fullname)) {
-				const auto& config = FileOperations::join(
-					fullname, "hardwareconfig.xml");
-				if (FileOperations::isRegularFile(config)) {
-					result.emplace_back(name);
-				}
 			}
-		}
+		};
+		auto dirAction = [&](std::string& path, std::string_view name) {
+			auto size = path.size();
+			path += "/hardwareconfig.xml";
+			if (FileOperations::isRegularFile(path)) {
+				result.emplace_back(name);
+			}
+			path.resize(size);
+		};
+		foreach_file_and_directory(FileOperations::join(p, type), fileAction, dirAction);
 	}
 	// remove duplicates
 	ranges::sort(result);
@@ -959,25 +957,20 @@ void RestoreMachineCommand::execute(span<const TclObject> tokens,
 	switch (tokens.size()) {
 	case 1: {
 		// load last saved entry
-		struct stat st;
-		string dirName = FileOperations::getUserOpenMSXDir() + "/savestates/";
 		string lastEntry;
 		time_t lastTime = 0;
-		ReadDir dir(dirName);
-		while (dirent* d = dir.getEntry()) {
-			int res = stat(strCat(dirName, d->d_name).c_str(), &st);
-			if ((res == 0) && S_ISREG(st.st_mode)) {
+		foreach_file(
+			FileOperations::getUserOpenMSXDir() + "/savestates",
+			[&](const string& path, const FileOperations::Stat& st) {
 				time_t modTime = st.st_mtime;
 				if (modTime > lastTime) {
-					lastEntry = string(d->d_name);
+					filename = path;
 					lastTime = modTime;
 				}
-			}
-		}
-		if (lastEntry.empty()) {
+			});
+		if (filename.empty()) {
 			throw CommandException("Can't find last saved state.");
 		}
-		filename = dirName + lastEntry;
 		break;
 	}
 	case 2:

src/commands/Completer.cc
@@ -3,7 +3,7 @@
 #include "InterpreterOutput.hh"
 #include "FileContext.hh"
 #include "FileOperations.hh"
-#include "ReadDir.hh"
+#include "foreach_file.hh"
 #include "ranges.hh"
 #include "stl.hh"
 #include "strCat.hh"
@@ -151,21 +151,20 @@ void Completer::completeFileNameImpl(vector<string>& tokens,
 
 	vector<string> filenames;
 	for (auto& p : paths) {
-		string dirname = FileOperations::join(p, dirname1);
-		ReadDir dir(FileOperations::getNativePath(dirname));
-		while (dirent* de = dir.getEntry()) {
-			string name = FileOperations::join(dirname, de->d_name);
-			if (FileOperations::exists(name)) {
-				string nm = FileOperations::join(dirname1, de->d_name);
-				if (FileOperations::isDirectory(name)) {
-					nm += '/';
-				}
-				nm = FileOperations::getConventionalPath(nm);
-				if (equalHead(filename, nm, true)) {
-					filenames.push_back(nm);
-				}
+		auto fileAction = [&](const string& path) {
+			const auto& nm = FileOperations::getConventionalPath(path);
+			if (equalHead(filename, nm, true)) {
+				filenames.push_back(nm);
 			}
-		}
+		};
+		auto dirAction = [&](string& path) {
+			path += '/';
+			fileAction(path);
+			path.pop_back();
+		};
+		foreach_file_and_directory(
+			FileOperations::getNativePath(FileOperations::join(p, dirname1)),
+			fileAction, dirAction);
 	}
 	append(matches, filenames);
 	bool t = completeImpl(filename, matches, true);

src/fdc/DiskChanger.cc
@@ -170,7 +170,7 @@ int DiskChanger::insertDisk(std::string_view filename)
 
 void DiskChanger::insertDisk(span<const TclObject> args)
 {
-	const string& diskImage = FileOperations::getConventionalPath(args[1].getString());
+	string diskImage = FileOperations::getConventionalPath(string(args[1].getString()));
 	auto& diskFactory = reactor.getDiskFactory();
 	std::unique_ptr<Disk> newDisk(diskFactory.createDisk(diskImage, *this));
 	for (size_t i = 2; i < args.size(); ++i) {

src/fdc/MSXtar.cc
@@ -7,9 +7,9 @@
 #endif
 
 #include "MSXtar.hh"
-#include "ReadDir.hh"
 #include "SectorAccessibleDisk.hh"
 #include "FileOperations.hh"
+#include "foreach_file.hh"
 #include "MSXException.hh"
 #include "StringOp.hh"
 #include "strCat.hh"
@@ -366,7 +366,7 @@ static string makeSimpleMSXFileName(string_view fullFilename)
 // returns: the first sector of the new subdir
 // @throws in case no directory could be created
 unsigned MSXtar::addSubdir(
-	const string& msxName, unsigned t, unsigned d, unsigned sector)
+	std::string_view msxName, unsigned t, unsigned d, unsigned sector)
 {
 	// returns the sector for the first cluster of this subdir
 	DirEntry result = addEntryToDir(sector);
@@ -452,7 +452,7 @@ static void getTimeDate(const string& filename, unsigned& time, unsigned& date)
 
 // Add an MSXsubdir with the time properties from the HOST-OS subdir
 // @throws when subdir could not be created
-unsigned MSXtar::addSubdirToDSK(const string& hostName, const string& msxName,
+unsigned MSXtar::addSubdirToDSK(const string& hostName, std::string_view msxName,
                                 unsigned sector)
 {
 	unsigned time, date;
@@ -605,46 +605,37 @@ string MSXtar::addFileToDSK(const string& fullHostName, unsigned rootSector)
 string MSXtar::recurseDirFill(string_view dirName, unsigned sector)
 {
 	string messages;
-	ReadDir readDir{string(dirName)};
-	while (dirent* d = readDir.getEntry()) {
-		string name(d->d_name);
-		string fullName = strCat(dirName, '/', name);
 
-		FileOperations::Stat st;
-		if (!FileOperations::getStat(fullName, st)) {
-			// ignore, normally this should not happen
-			continue;
-		}
-
-		if (FileOperations::isRegularFile(st)) {
-			// add new file
-			messages += addFileToDSK(fullName, sector);
-
-		} else if (FileOperations::isDirectory(st) && name != one_of(".", "..")) {
-			string msxFileName = makeSimpleMSXFileName(name);
-			SectorBuffer buf;
-			DirEntry entry = findEntryInDir(msxFileName, sector, buf);
-			if (entry.sector != 0) {
-				// entry already exists ..
-				auto& msxDirEntry = buf.dirEntry[entry.index];
-				if (msxDirEntry.attrib & T_MSX_DIR) {
-					// .. and is a directory
-					unsigned nextSector = clusterToSector(
-						getStartCluster(msxDirEntry));
-					messages += recurseDirFill(fullName, nextSector);
-				} else {
-					// .. but is NOT a directory
-					strAppend(messages,
-					          "MSX file ", msxFileName,
-					          " is not a directory.\n");
-				}
+	auto fileAction = [&](const string& path) {
+		// add new file
+		messages += addFileToDSK(path, sector);
+	};
+	auto dirAction = [&](const string& path, std::string_view name) {
+		string msxFileName = makeSimpleMSXFileName(name);
+		SectorBuffer buf;
+		DirEntry entry = findEntryInDir(msxFileName, sector, buf);
+		if (entry.sector != 0) {
+			// entry already exists ..
+			auto& msxDirEntry = buf.dirEntry[entry.index];
+			if (msxDirEntry.attrib & T_MSX_DIR) {
+				// .. and is a directory
+				unsigned nextSector = clusterToSector(
+					getStartCluster(msxDirEntry));
+				messages += recurseDirFill(path, nextSector);
 			} else {
-				// add new directory
-				unsigned nextSector = addSubdirToDSK(fullName, name, sector);
-				messages += recurseDirFill(fullName, nextSector);
+				// .. but is NOT a directory
+				strAppend(messages,
+					  "MSX file ", msxFileName,
+					  " is not a directory.\n");
 			}
+		} else {
+			// add new directory
+			unsigned nextSector = addSubdirToDSK(path, name, sector);
+			messages += recurseDirFill(path, nextSector);
 		}
-	}
+	};
+	foreach_file_and_directory(std::string(dirName), fileAction, dirAction);
+
 	return messages;
 }
 

src/fdc/MSXtar.hh
@@ -49,11 +49,11 @@ private:
 	unsigned getStartCluster(const MSXDirEntry& entry);
 	unsigned appendClusterToSubdir(unsigned sector);
 	DirEntry addEntryToDir(unsigned sector);
-	unsigned addSubdir(const std::string& msxName,
+	unsigned addSubdir(std::string_view msxName,
 	                   unsigned t, unsigned d, unsigned sector);
 	void alterFileInDSK(MSXDirEntry& msxDirEntry, const std::string& hostName);
 	unsigned addSubdirToDSK(const std::string& hostName,
-	                   const std::string& msxName, unsigned sector);
+	                        std::string_view msxName, unsigned sector);
 	DirEntry findEntryInDir(const std::string& name, unsigned sector,
 	                        SectorBuffer& sectorBuf);
 	std::string addFileToDSK(const std::string& fullHostName, unsigned sector);

src/file/FileOperations.cc
@@ -458,14 +458,13 @@ string getNativePath(string_view path)
 	return result;
 }
 
-string getConventionalPath(string_view path)
-{
-	string result(path);
 #ifdef _WIN32
-	ranges::replace(result, '\\', '/');
-#endif
-	return result;
+string getConventionalPath(string path)
+{
+	ranges::replace(path, '\\', '/');
+	return path;
 }
+#endif
 
 string getCurrentWorkingDirectory()
 {
@@ -632,19 +631,20 @@ string expandCurrentDirFromDrive(string_view path)
 	return result;
 }
 
-bool getStat(std::string filename, Stat& st)
+bool getStat(const std::string& filename, Stat& st)
 {
+#ifdef _WIN32
+	std::string filename2 = filename;
 	// workaround for VC++: strip trailing slashes (but keep it if it's the
 	// only character in the path)
-	auto pos = filename.find_last_not_of('/');
+	auto pos = filename2.find_last_not_of('/');
 	if (pos == string::npos) {
 		// string was either empty or a (sequence of) '/' character(s)
-		if (!filename.empty()) filename.resize(1);
+		if (!filename2.empty()) filename2.resize(1);
 	} else {
-		filename.resize(pos + 1);
+		filename2.resize(pos + 1);
 	}
-#ifdef _WIN32
-	return _wstat(utf8to16(filename).c_str(), &st) == 0;
+	return _wstat(utf8to16(filename2).c_str(), &st) == 0;
 #else
 	return stat(filename.c_str(), &st) == 0;
 #endif

src/file/FileOperations.hh
@@ -141,7 +141,12 @@ namespace openmsx::FileOperations {
 	 *    On UNI*Y systems, it will have no effect indeed.
 	 *    Just for portability issue. (Especially for Win32)
 	 */
-	std::string getConventionalPath(std::string_view path);
+#ifdef _WIN32
+	std::string getConventionalPath(std::string path);
+#else
+	inline const std::string& getConventionalPath(const std::string& path) { return path; }
+	inline std::string getConventionalPath(std::string&& path) { return std::move(path); }
+#endif
 
 	/**
 	 * Returns the path in native path-delimiter.
@@ -217,7 +222,7 @@ namespace openmsx::FileOperations {
 	 * @param st The stat structute that will be filled in
 	 * @result true iff success
 	 */
-	bool getStat(std::string filename, Stat& st);
+	bool getStat(const std::string& filename, Stat& st);
 
 	/**
 	 * Is this a regular file (no directory, device, ..)?

commit 381d0c0371b3af0082dee32683c5560293bf17dd

Fix filename-tab-completion
Was broken after revision cab2f971bc. The reason was that the new
foreach_file_xxx() utility functions always add a '/' character between
the given start-directory and the files/sub-directories in that
directory. Also when the start-directory already ended in a '/'
character.

Normally that's not a problem, except for tab-completion because it
relies on exact string matches.

Now we only add '/' when it doesn't already end in '/'. This was also
the behavior of the old 'ReadDir' utility.

src/file/foreach_file.hh
@@ -3,6 +3,7 @@
 
 #include "FileOperations.hh"
 #include "ReadDir.hh"
+#include "StringOp.hh"
 #include "one_of.hh"
 #include <functional>
 #include <string_view>
@@ -102,7 +103,7 @@ namespace openmsx {
 			bool needStat = statFile || statDir;
 
 			ReadDir dir(path);
-			path += '/';
+			if (!StringOp::endsWith(path, '/')) path += '/';
 			auto origLen = path.size();
 			while (dirent* d = dir.getEntry()) {
 				std::string_view f(d->d_name);