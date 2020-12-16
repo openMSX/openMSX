b9611dbb87cd5c93b3171484dc62cabd224b2a0b
m9710797 <vermaelen.wouter@gmail.com>
2020-12-16 17:54:05 +0100

    gl_{vec,mat} remove manual SSE optimizations
    The type 'vecN<4, float>' (aka 'vec4') is mostly used in openGL related
    calculations. For us the speed of these calculations is not super
    important, but of course faster is always better. For this reason we
    made sure that (on x86) the compiler emits SSE instructions to roughly
    speedup these operations by a factor 4x. In order to accomplish this we
    manually vectorized (parts of) the code using intrinsics.
    
    Part of the implementation used this union:
        union {
            __m128 e;
            float e_[4];
        };
    And sometimes we wrote data via one member of the union, but then read
    it back via the other member. In practice this happened to work fine on
    all compilers we use, but strictly speaking it's undefined behavior
    according to the C/C++ standard.
    
    I could fix this, but unfortunately I had to give up the following
    syntax:
        vec4 v = ...
        // This no longer works:
           v[1] = 2.0;    // modify a mutable reference
        // This replacement works, but it's not as nice
           v.set(1, 2.0); // setter method
    More technically, I couldn't find a (standard compliant) way to return
    references to individual elements. Instead I had to use setter methods.
    
    Taking a step back: is manually vectorizing this code still needed? This
    code was originally written in May 2014 (that's around the time gcc-4.9
    was released, now we have gcc-10). At that time these optimization
    certainly helped. But if I test again today, I found that gcc/clang are
    able to auto-vectorize this code pretty well. Not in all cases,
    sometimes the manually optimized code is still better. But for the
    functions that matter most (vector and matrix multiplication) both
    versions are pretty similar. So there's no reason anymore to keep these
    manual optimizations.

--- src/video/gl_mat.hh
+++ src/video/gl_mat.hh
@@ -11,12 +11,10 @@
 // operation 'matrix x column-vector' is supported and not e.g. 'row-vector x
 // matrix'. The internal layout is also compatible with openGL(ES).
 //
-// This code itself doesn't have many SSE optimizations, but because it is built
-// on top of an optimized vector type the generated code for 4x4 matrix
-// multiplication is as efficient as a hand-written SSE assembly routine
-// (verified with gcc-4.8 on x86_64). The efficiency for other matrix sizes was
-// not a goal for this code (smaller sizes might be ok, bigger sizes most
-// likely not).
+// In the past we had (some) manual SSE optimizations in this code. Though for
+// the functions that matter (matrix-vector and matrix-matrix multiplication),
+// the compiler's auto-vectorization has become as good as the manually
+// vectorized code.
 
 #include "gl_vec.hh"
 #include "xrange.hh"
@@ -110,50 +108,6 @@ private:
 };
 
 
-// Specialization for 4x4 matrix.
-// This specialization is not needed for correctness, but it does help gcc-4.8
-// to generate better code for the constructors (clang-3.4 doesn't need help).
-// Maybe remove this specialization in the future?
-template<typename T> class matMxN<4, 4, T>
-{
-public:
-	matMxN()
-	{
-		T one(1); T zero(0);
-		c[0] = vecN<4,T>( one, zero, zero, zero);
-		c[1] = vecN<4,T>(zero,  one, zero, zero);
-		c[2] = vecN<4,T>(zero, zero,  one, zero);
-		c[3] = vecN<4,T>(zero, zero, zero,  one);
-	}
-
-	explicit matMxN(vecN<4, T> d)
-	{
-		T zero(0);
-		c[0] = vecN<4,T>(d[0], zero, zero, zero);
-		c[1] = vecN<4,T>(zero, d[1], zero, zero);
-		c[2] = vecN<4,T>(zero, zero, d[2], zero);
-		c[3] = vecN<4,T>(zero, zero, zero, d[3]);
-	}
-
-	matMxN(vecN<4, T> x, vecN<4, T> y, vecN<4, T> z, vecN<4, T> w)
-	{
-		c[0] = x; c[1] = y; c[2] = z; c[3] = w;
-	}
-
-	[[nodiscard]] vecN<4, T>  operator[](int i) const { return c[i]; }
-	[[nodiscard]] vecN<4, T>& operator[](int i)       { return c[i]; }
-
-	// Assignment version of the +,-,* operations defined below.
-	matMxN& operator+=(const matMxN& x) { *this = *this + x; return *this; }
-	matMxN& operator-=(const matMxN& x) { *this = *this - x; return *this; }
-	matMxN& operator*=(T             x) { *this = *this * x; return *this; }
-	matMxN& operator*=(const matMxN& x) { *this = *this * x; return *this; }
-
-private:
-	vecN<4, T> c[4];
-};
-
-
 // Convenience typedefs (same names as used by GLSL).
 using mat2 = matMxN<2, 2, float>;
 using mat3 = matMxN<3, 3, float>;
@@ -225,12 +179,6 @@ template<int M, int N, typename T>
 	vecN<M, T> r;
 	for (auto i : xrange(N)) r += A[i] * x[i];
 	return r;
-}
-template<typename T>
-[[nodiscard]] inline vecN<4, T> operator*(const matMxN<4, 4, T>& A, vecN<4, T> x)
-{
-	// Small optimization for 4x4 matrix: reassociate add-chain
-	return (A[0] * x[0] + A[1] * x[1]) + (A[2] * x[2] + A[3] * x[3]);
 }
 
 // matrix * matrix
@@ -395,113 +343,5 @@ std::ostream& operator<<(std::ostream& os, const matMxN<M, N, T>& A)
 }
 
 } // namespace gl
-
-
-// --- SSE optimizations ---
-
-#ifdef __SSE__
-#include <xmmintrin.h>
-
-namespace gl {
-
-// transpose of 4x4-float matrix
-[[nodiscard]] inline mat4 transpose(const mat4& A)
-{
-	mat4 R;
-	__m128 t0 = _mm_shuffle_ps(A[0].sse(), A[1].sse(), _MM_SHUFFLE(1,0,1,0));
-	__m128 t1 = _mm_shuffle_ps(A[0].sse(), A[1].sse(), _MM_SHUFFLE(3,2,3,2));
-	__m128 t2 = _mm_shuffle_ps(A[2].sse(), A[3].sse(), _MM_SHUFFLE(1,0,1,0));
-	__m128 t3 = _mm_shuffle_ps(A[2].sse(), A[3].sse(), _MM_SHUFFLE(3,2,3,2));
-	R[0] = vec4(_mm_shuffle_ps(t0, t2, _MM_SHUFFLE(2,0,2,0)));
-	R[1] = vec4(_mm_shuffle_ps(t0, t2, _MM_SHUFFLE(3,1,3,1)));
-	R[2] = vec4(_mm_shuffle_ps(t1, t3, _MM_SHUFFLE(2,0,2,0)));
-	R[3] = vec4(_mm_shuffle_ps(t1, t3, _MM_SHUFFLE(3,1,3,1)));
-	return R;
-}
-
-// inverse of a 4x4-float matrix
-[[nodiscard]] inline mat4 inverse(const mat4& A)
-{
-	// Implementation based on glm:  http://glm.g-truc.net
-
-	__m128 sa0 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 sb0 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 s00 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 s10 = _mm_shuffle_ps(sa0,        sa0,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s20 = _mm_shuffle_ps(sb0,        sb0,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s30 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 f0  = _mm_sub_ps(_mm_mul_ps(s00, s10), _mm_mul_ps(s20, s30));
-
-	__m128 sa1 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 sb1 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 s01 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 s11 = _mm_shuffle_ps(sa1,        sa1,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s21 = _mm_shuffle_ps(sb1,        sb1,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s31 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 f1  = _mm_sub_ps(_mm_mul_ps(s01, s11), _mm_mul_ps(s21, s31));
-
-	__m128 sa2 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 sb2 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 s02 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 s12 = _mm_shuffle_ps(sa2,        sa2,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s22 = _mm_shuffle_ps(sb2,        sb2,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s32 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 f2  = _mm_sub_ps(_mm_mul_ps(s02, s12), _mm_mul_ps(s22, s32));
-
-	__m128 sa3 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 sb3 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 s03 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 s13 = _mm_shuffle_ps(sa3,        sa3,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s23 = _mm_shuffle_ps(sb3,        sb3,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s33 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 f3  = _mm_sub_ps(_mm_mul_ps(s03, s13), _mm_mul_ps(s23, s33));
-
-	__m128 sa4 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 sb4 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 s04 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 s14 = _mm_shuffle_ps(sa4,        sa4,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s24 = _mm_shuffle_ps(sb4,        sb4,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s34 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 f4  = _mm_sub_ps(_mm_mul_ps(s04, s14), _mm_mul_ps(s24, s34));
-
-	__m128 sa5 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 sb5 = _mm_shuffle_ps(A[3].sse(), A[2].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 s05 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 s15 = _mm_shuffle_ps(sa5,        sa5,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s25 = _mm_shuffle_ps(sb5,        sb5,        _MM_SHUFFLE(2,0,0,0));
-	__m128 s35 = _mm_shuffle_ps(A[2].sse(), A[1].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 f5  = _mm_sub_ps(_mm_mul_ps(s05, s15), _mm_mul_ps(s25, s35));
-
-	__m128 t0 = _mm_shuffle_ps(A[1].sse(), A[0].sse(), _MM_SHUFFLE(0,0,0,0));
-	__m128 t1 = _mm_shuffle_ps(A[1].sse(), A[0].sse(), _MM_SHUFFLE(1,1,1,1));
-	__m128 t2 = _mm_shuffle_ps(A[1].sse(), A[0].sse(), _MM_SHUFFLE(2,2,2,2));
-	__m128 t3 = _mm_shuffle_ps(A[1].sse(), A[0].sse(), _MM_SHUFFLE(3,3,3,3));
-	__m128 v0 = _mm_shuffle_ps(t0, t0, _MM_SHUFFLE(2,2,2,0));
-	__m128 v1 = _mm_shuffle_ps(t1, t1, _MM_SHUFFLE(2,2,2,0));
-	__m128 v2 = _mm_shuffle_ps(t2, t2, _MM_SHUFFLE(2,2,2,0));
-	__m128 v3 = _mm_shuffle_ps(t3, t3, _MM_SHUFFLE(2,2,2,0));
-
-	__m128 s0 = _mm_sub_ps(_mm_mul_ps(v1, f0), _mm_mul_ps(v2, f1));
-	__m128 s1 = _mm_sub_ps(_mm_mul_ps(v0, f0), _mm_mul_ps(v2, f3));
-	__m128 s2 = _mm_sub_ps(_mm_mul_ps(v0, f1), _mm_mul_ps(v1, f3));
-	__m128 s3 = _mm_sub_ps(_mm_mul_ps(v0, f2), _mm_mul_ps(v1, f4));
-	__m128 sa = _mm_set_ps( 1.0f,-1.0f, 1.0f,-1.0f);
-	__m128 sb = _mm_set_ps(-1.0f, 1.0f,-1.0f, 1.0f);
-	__m128 i0 = _mm_mul_ps(sb, _mm_add_ps(s0, _mm_mul_ps(v3, f2)));
-	__m128 i1 = _mm_mul_ps(sa, _mm_add_ps(s1, _mm_mul_ps(v3, f4)));
-	__m128 i2 = _mm_mul_ps(sb, _mm_add_ps(s2, _mm_mul_ps(v3, f5)));
-	__m128 i3 = _mm_mul_ps(sa, _mm_add_ps(s3, _mm_mul_ps(v2, f5)));
-
-	__m128 ra = _mm_shuffle_ps(i0, i1, _MM_SHUFFLE(0,0,0,0));
-	__m128 rb = _mm_shuffle_ps(i2, i3, _MM_SHUFFLE(0,0,0,0));
-	vec4 row0 = vec4(_mm_shuffle_ps(ra, rb, _MM_SHUFFLE(2,0,2,0)));
-
-	vec4 rd =  recip(dot_broadcast(A[0], row0));
-	return mat4(vec4(i0) * rd, vec4(i1) * rd, vec4(i2) * rd, vec4(i3) * rd);
-}
-
-} // namespace gl
-
-#endif // __SSE__
 
 #endif
