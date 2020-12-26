openMSX/openMSX

commit c2f8543039eafe8f6efc5407415047ef275b9719

Optimize usage of FileOperation::expandTilde()
Tilde-expansion is an idempotent operation. That is doing it just once,
or more than once has the same effect. Though on some code paths we did
perform this operation multiple times.

The idea is to (only) do tilde-expansion close to the place where
user-input is processed (e.g. command line parsing, or Tcl command
parsing), but no longer in any deeper code.

This patch takes some steps in that direction, but the work is not
finished yet. So I expect some follow up patches (and in fact this patch
is already a continuation of previous work).

src/file/FileOperations.cc
@@ -76,19 +76,24 @@ using namespace utf8;
 
 namespace openmsx::FileOperations {
 
-string expandTilde(string_view path)
+bool needsTildeExpansion(std::string_view path)
 {
-	if (path.empty() || path[0] != '~') {
-		return string(path);
+	return !path.empty() && (path[0] == '~');
+}
+
+string expandTilde(string path)
+{
+	if (!needsTildeExpansion(path)) {
+		return path;
 	}
 	auto pos = path.find_first_of('/');
 	string_view user = ((path.size() == 1) || (pos == 1))
 	                ? string_view{}
-	                : path.substr(1, (pos == string_view::npos) ? pos : pos - 1);
+	                : string_view(path).substr(1, (pos == string::npos) ? pos : pos - 1);
 	string result = getUserHomeDir(user);
 	if (result.empty()) {
 		// failed to find homedir, return the path unchanged
-		return string(path);
+		return path;
 	}
 	if (pos == string_view::npos) {
 		return result;
@@ -129,14 +134,15 @@ static bool isUNCPath(string_view path)
 #endif
 }
 
-void mkdirp(string_view path_)
+void mkdirp(string path)
 {
-	if (path_.empty()) {
+	if (path.empty()) {
 		return;
 	}
 
 	// We may receive platform-specific paths here, so conventionalize early
-	string path = getConventionalPath(expandTilde(path_));
+	// TODO move tilde-expansion outside
+	path = getConventionalPath(expandTilde(std::move(path)));
 
 	// If the directory already exists, don't try to recreate it
 	if (isDirectory(path))
@@ -477,9 +483,8 @@ static bool driveExists(char driveLetter)
 }
 #endif
 
-string expandCurrentDirFromDrive(string_view path)
+string expandCurrentDirFromDrive(string result)
 {
-	string result(path);
 #ifdef _WIN32
 	if (((path.size() == 2) && (path[1] == ':')) ||
 		((path.size() >= 3) && (path[1] == ':') && (path[2] != '/'))) {
@@ -610,7 +615,7 @@ string parseCommandFileArgument(
 		mkdirp(dir);
 		filename = strCat(dir, '/', filename);
 	} else {
-		filename = expandTilde(filename);
+		filename = expandTilde(std::move(filename));
 	}
 
 	if (!StringOp::endsWith(filename, extension) &&

src/DebugDevice.cc
@@ -146,7 +146,7 @@ void DebugDevice::openOutput(std::string_view name)
 	} else if (name == "stderr") {
 		outputstrm = &std::cerr;
 	} else {
-		string realName = FileOperations::expandTilde(name);
+		string realName = FileOperations::expandTilde(fileNameString);
 		FileOperations::openofstream(debugOut, realName, std::ios::app);
 		outputstrm = &debugOut;
 	}

src/cassette/CassettePlayer.cc
@@ -643,9 +643,9 @@ void CassettePlayer::TapeCommand::execute(
 		                      options);
 
 	} else if (tokens[1] == "new") {
-		string directory = "taperecordings";
-		string prefix = "openmsx";
-		string extension = ".wav";
+		std::string_view directory = "taperecordings";
+		std::string_view prefix = "openmsx";
+		std::string_view extension = ".wav";
 		string filename = FileOperations::parseCommandFileArgument(
 			(tokens.size() == 3) ? tokens[2].getString() : string{},
 			directory, prefix, extension);

src/commands/Completer.cc
@@ -139,8 +139,8 @@ void Completer::completeFileNameImpl(vector<string>& tokens,
                                      vector<string_view> matches)
 {
 	string& filename = tokens.back();
-	filename = FileOperations::expandTilde(filename);
-	filename = FileOperations::expandCurrentDirFromDrive(filename);
+	filename = FileOperations::expandTilde(std::move(filename));
+	filename = FileOperations::expandCurrentDirFromDrive(std::move(filename));
 	string_view dirname1 = FileOperations::getDirName(filename);
 
 	vector<string> paths;

src/fdc/DiskManipulator.cc
@@ -150,7 +150,7 @@ void DiskManipulator::execute(span<const TclObject> tokens, TclObject& result)
 
 	if (subcmd == "export") {
 		string_view dir = tokens[3].getString();
-		auto directory = FileOperations::expandTilde(dir);
+		auto directory = FileOperations::expandTilde(string(dir));
 		if (!FileOperations::isDirectory(directory)) {
 			throw CommandException(dir, " is not a directory");
 		}
@@ -500,7 +500,7 @@ string DiskManipulator::import(DriveSettings& driveData,
 	auto& interp = getInterpreter();
 	for (const auto& l : lists) {
 		for (auto i : xrange(l.getListLength(interp))) {
-			auto s = FileOperations::expandTilde(l.getListIndex(interp, i).getString());
+			auto s = FileOperations::expandTilde(string(l.getListIndex(interp, i).getString()));
 			try {
 				FileOperations::Stat st;
 				if (!FileOperations::getStat(s, st)) {
@@ -510,7 +510,7 @@ string DiskManipulator::import(DriveSettings& driveData,
 				if (FileOperations::isDirectory(st)) {
 					messages += workhorse->addDir(s);
 				} else if (FileOperations::isRegularFile(st)) {
-					messages += workhorse->addFile(string(s));
+					messages += workhorse->addFile(s);
 				} else {
 					// ignore other stuff (sockets, device nodes, ..)
 					strAppend(messages, "Ignoring ", s, '\n');

src/fdc/MSXtar.cc
@@ -475,7 +475,7 @@ void MSXtar::alterFileInDSK(MSXDirEntry& msxDirEntry, const string& hostName)
 	unsigned remaining = hostSize;
 
 	// open host file for reading
-	File file(FileOperations::expandTilde(hostName), "rb");
+	File file(hostName, "rb");
 
 	// copy host file to image
 	unsigned prevCl = 0;
@@ -757,7 +757,7 @@ void MSXtar::fileExtract(const string& resultFile, const MSXDirEntry& dirEntry)
 	unsigned size = dirEntry.size;
 	unsigned sector = clusterToSector(getStartCluster(dirEntry));
 
-	File file(FileOperations::expandTilde(resultFile), "wb");
+	File file(resultFile, "wb");
 	while (size && sector) {
 		SectorBuffer buf;
 		readLogicalSector(sector, buf);
@@ -833,7 +833,7 @@ void MSXtar::recurseDirExtract(string_view dirName, unsigned sector)
 
 string MSXtar::addDir(string_view rootDirName)
 {
-	return recurseDirFill(FileOperations::expandTilde(rootDirName), chrootSector);
+	return recurseDirFill(rootDirName, chrootSector);
 }
 
 string MSXtar::addFile(const string& filename)

src/fdc/NowindCommand.cc
@@ -183,7 +183,7 @@ void NowindCommand::execute(span<const TclObject> tokens, TclObject& result)
 			} else {
 				try {
 					auto hdImage = FileOperations::expandTilde(
-						args.front().getString());
+						string(args.front().getString()));
 					args = args.subspan(1);
 					processHdimage(hdImage, tmpDrives);
 					changeDrives = true;

src/file/File.cc
@@ -18,7 +18,8 @@ File::File() = default;
 	static constexpr uint8_t GZ_HEADER[3]  = { 0x1F, 0x8B, 0x08 };
 	static constexpr uint8_t ZIP_HEADER[4] = { 0x50, 0x4B, 0x03, 0x04 };
 
-	std::unique_ptr<FileBase> file = std::make_unique<LocalFile>(filename, mode);
+	// TODO string(filename) because expandTilde()
+	std::unique_ptr<FileBase> file = std::make_unique<LocalFile>(string(filename), mode);
 	if (file->getSize() >= 4) {
 		uint8_t buf[4];
 		file->read(buf, 4);
@@ -48,7 +49,8 @@ File::File(const Filename& filename, OpenMode mode)
 }
 
 File::File(std::string_view filename, const char* mode)
-	: file(std::make_unique<LocalFile>(filename, mode))
+	// TODO string(filename) because expandTilde()
+	: file(std::make_unique<LocalFile>(string(filename), mode))
 {
 }
 

commit d45be2d372b912b74c7d00fd3bd194ade2b7fca5

Fix use-after-free bug introduced in c2f8543039eaf
In practice it would often work, but a bug nevertheless.

Caught by unittests (because these are bult using the address
sanitizer).

src/file/FileOperations.cc
@@ -101,7 +101,7 @@ string expandTilde(string path)
 	if (result.back() != '/') {
 		result += '/';
 	}
-	string_view last = path.substr(pos + 1);
+	string_view last = string_view(path).substr(pos + 1);
 	result.append(last.data(), last.size());
 	return result;
 }