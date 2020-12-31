openMSX/openMSX

commit ae30ae2befd01f1d983924cc8e85c23b06127f44

Added 'zstring_view'
Consider this code fragment:
    string text(tokens[1].getString());
    SDL_SetClipboardText(text.c_str());

Here 'tokens[1]' is a TclObject. The getString() method returns a
std::string_view, not a std::string (this avoids making a copy of the
internal TclObject string-representation). But we want to pass this to
the SDL_SetClipboardText() function which expects a zero-terminated
c-string. 'std::string_view' does not guarantee that it refers to a
zero-terminated string. That's why the current code first needs to copy
the std::string_view to a std::string (because std::string does offer
this guarantee).

But actually the original internal string representation in TclObject
_does_ already guarantee zero-terminated strings. It's only because we
return a std::string_view (to avoid making a copy) that we loose this
guarantee (and then are required to make a copy later).

This patch introduce the 'zstring_view' class. Like 'string_view' it
only refers to a string (it does not own the data, so it does not
require making a copy). But it has the additional guarantee that it
refers to a zero-terminated string (because of this it e.g. cannot refer
to a part in the middle of a larger string). This allows to fully
eliminate making copies in the above example.

More in general using zstring_view can be useful when interacting with
3rd-party APIs which expect C-style strings (so also e.g. file operations
in libc). You could ask why not simply use 'const char*' for such
situations. Often that's possible, but:
- This looses the length information, (this info is available for free,
  and loosing it is sometimes the source of other performance
  inefficiencies).
- 'zstring_view' carries more semantic information than 'const char*'.
  In some cases this helps to detect programming errors.

src/file/FileOperations.cc
@@ -106,7 +106,7 @@ string expandTilde(string path)
 	return result;
 }
 
-void mkdir(const string& path, mode_t mode)
+void mkdir(zstring_view path, mode_t mode)
 {
 #ifdef _WIN32
 	(void)&mode; // Suppress C4100 VC++ warning
@@ -168,7 +168,7 @@ void mkdirp(string path)
 	}
 }
 
-int unlink(const std::string& path)
+int unlink(zstring_view path)
 {
 #ifdef _WIN32
 	return _wunlink(utf8to16(path).c_str());
@@ -177,7 +177,7 @@ int unlink(const std::string& path)
 #endif
 }
 
-int rmdir(const std::string& path)
+int rmdir(zstring_view path)
 {
 #ifdef _WIN32
 	return _wrmdir(utf8to16(path).c_str());
@@ -187,7 +187,7 @@ int rmdir(const std::string& path)
 }
 
 #ifdef _WIN32
-int deleteRecursive(const std::string& path)
+int deleteRecursive(zstring_view path)
 {
 	std::wstring pathW = utf8to16(path);
 
@@ -209,7 +209,7 @@ static int deleteRecursive_cb(const char* fpath, const struct stat* /*sb*/,
 {
 	return remove(fpath);
 }
-int deleteRecursive(const std::string& path)
+int deleteRecursive(zstring_view path)
 {
 	return nftw(path.c_str(), deleteRecursive_cb, 64, FTW_DEPTH | FTW_PHYS);
 }
@@ -239,7 +239,7 @@ int deleteRecursive(const std::string& path)
 }
 #endif
 
-FILE_t openFile(const std::string& filename, const std::string& mode)
+FILE_t openFile(zstring_view filename, zstring_view mode)
 {
 	// Mode must contain a 'b' character. On unix this doesn't make any
 	// difference. But on windows this is required to open the file
@@ -253,7 +253,7 @@ FILE_t openFile(const std::string& filename, const std::string& mode)
 #endif
 }
 
-void openofstream(std::ofstream& stream, const std::string& filename)
+void openofstream(std::ofstream& stream, zstring_view filename)
 {
 #if defined _WIN32 && defined _MSC_VER
 	// MinGW 3.x doesn't support ofstream.open(wchar_t*)
@@ -264,8 +264,8 @@ void openofstream(std::ofstream& stream, const std::string& filename)
 #endif
 }
 
-void openofstream(std::ofstream& stream, const std::string& filename,
-				  std::ios_base::openmode mode)
+void openofstream(std::ofstream& stream, zstring_view filename,
+                  std::ios_base::openmode mode)
 {
 #if defined _WIN32 && defined _MSC_VER
 	// MinGW 3.x doesn't support ofstream.open(wchar_t*)
@@ -512,10 +512,10 @@ string expandCurrentDirFromDrive(string path)
 }
 #endif
 
-bool getStat(const std::string& filename, Stat& st)
+bool getStat(zstring_view filename, Stat& st)
 {
 #ifdef _WIN32
-	std::string filename2 = filename;
+	std::string filename2(filename);
 	// workaround for VC++: strip trailing slashes (but keep it if it's the
 	// only character in the path)
 	if (auto pos = filename2.find_last_not_of('/'); pos != string::npos) {
@@ -534,7 +534,7 @@ bool isRegularFile(const Stat& st)
 {
 	return S_ISREG(st.st_mode);
 }
-bool isRegularFile(const std::string& filename)
+bool isRegularFile(zstring_view filename)
 {
 	Stat st;
 	return getStat(filename, st) && isRegularFile(st);
@@ -545,13 +545,13 @@ bool isDirectory(const Stat& st)
 	return S_ISDIR(st.st_mode);
 }
 
-bool isDirectory(const std::string& directory)
+bool isDirectory(zstring_view directory)
 {
 	Stat st;
 	return getStat(directory, st) && isDirectory(st);
 }
 
-bool exists(const std::string& filename)
+bool exists(zstring_view filename)
 {
 	Stat st; // dummy
 	return getStat(filename, st);

src/Reactor.cc
@@ -1053,7 +1053,7 @@ SetClipboardCommand::SetClipboardCommand(CommandController& commandController_)
 void SetClipboardCommand::execute(span<const TclObject> tokens, TclObject& /*result*/)
 {
 	checkNumArgs(tokens, 2, "text");
-	string text(tokens[1].getString());
+	auto text = tokens[1].getString();
 	if (SDL_SetClipboardText(text.c_str()) != 0) {
 		const char* err = SDL_GetError();
 		SDL_ClearError();

src/commands/CommandController.hh
@@ -2,6 +2,7 @@
 #define COMMANDCONTROLLER_HH
 
 #include "TclObject.hh"
+#include "zstring_view.hh"
 #include <string_view>
 
 namespace openmsx {
@@ -29,14 +30,14 @@ public:
 	 * (Un)register a command
 	 */
 	virtual void   registerCommand(Command& command,
-	                               const std::string& str) = 0;
+	                               zstring_view str) = 0;
 	virtual void unregisterCommand(Command& command,
 	                               std::string_view str) = 0;
 
 	/**
 	 * Execute the given command
 	 */
-	virtual TclObject executeCommand(const std::string& command,
+	virtual TclObject executeCommand(zstring_view command,
 	                                 CliConnection* connection = nullptr) = 0;
 
 	/** TODO

src/commands/GlobalCommandController.cc
@@ -58,7 +58,7 @@ GlobalCommandControllerBase::~GlobalCommandControllerBase()
 	assert(commandCompleters.empty());
 }
 
-void GlobalCommandController::registerProxyCommand(const string& name)
+void GlobalCommandController::registerProxyCommand(std::string_view name)
 {
 	auto it = proxyCommandMap.find(name);
 	if (it == end(proxyCommandMap)) {
@@ -128,7 +128,7 @@ Interpreter& GlobalCommandController::getInterpreter()
 }
 
 void GlobalCommandController::registerCommand(
-	Command& command, const string& str)
+	Command& command, zstring_view str)
 {
 #ifdef DEBUG
 	assert(!commands.contains(str));
@@ -295,13 +295,13 @@ static string addEscaping(const string& str, bool quote, bool finished)
 	return result;
 }
 
-bool GlobalCommandController::isComplete(const string& command)
+bool GlobalCommandController::isComplete(zstring_view command)
 {
 	return interpreter.isComplete(command);
 }
 
 TclObject GlobalCommandController::executeCommand(
-	const string& command, CliConnection* connection_)
+	zstring_view command, CliConnection* connection_)
 {
 	ScopedAssign sa(connection, connection_);
 	return interpreter.execute(command);

src/commands/GlobalCommandController.hh
@@ -53,7 +53,7 @@ public:
 	 */
 	void source(const std::string& script);
 
-	void registerProxyCommand(const std::string& name);
+	void registerProxyCommand(std::string_view name);
 	void unregisterProxyCommand(std::string_view name);
 
 	void registerProxySetting(Setting& setting);
@@ -65,10 +65,10 @@ public:
 	void unregisterCompleter(CommandCompleter& completer,
 	                         std::string_view str) override;
 	void   registerCommand(Command& command,
-	                       const std::string& str) override;
+	                       zstring_view str) override;
 	void unregisterCommand(Command& command,
 	                       std::string_view str) override;
-	TclObject executeCommand(const std::string& command,
+	TclObject executeCommand(zstring_view command,
 	                         CliConnection* connection = nullptr) override;
 	void registerSetting(Setting& setting) override;
 	void unregisterSetting(Setting& setting) override;
@@ -84,7 +84,7 @@ public:
 	 * Returns true iff the command is complete (all braces, quotes etc. are
 	 * balanced).
 	 */
-	[[nodiscard]] bool isComplete(const std::string& command);
+	[[nodiscard]] bool isComplete(zstring_view command);
 
 	[[nodiscard]] SettingsConfig& getSettingsConfig() { return settingsConfig; }
 	[[nodiscard]] SettingsManager& getSettingsManager() { return settingsConfig.getSettingsManager(); }

src/commands/Interpreter.cc
@@ -142,14 +142,14 @@ int Interpreter::outputProc(ClientData clientData, const char* buf,
 	return toWrite;
 }
 
-bool Interpreter::hasCommand(const std::string& name) const
+bool Interpreter::hasCommand(zstring_view name) const
 {
 	// Note: these are not only the commands registered via
 	// registerCommand(), but all commands know to this Tcl-interpreter.
 	return Tcl_FindCommand(interp, name.c_str(), nullptr, 0);
 }
 
-void Interpreter::registerCommand(const string& name, Command& command)
+void Interpreter::registerCommand(zstring_view name, Command& command)
 {
 	auto token = Tcl_CreateObjCommand(
 		interp, name.c_str(), commandProc,
@@ -205,12 +205,12 @@ TclObject Interpreter::getCommandNames()
 	return execute("openmsx::all_command_names");
 }
 
-bool Interpreter::isComplete(const string& command) const
+bool Interpreter::isComplete(zstring_view command) const
 {
 	return Tcl_CommandComplete(command.c_str()) != 0;
 }
 
-TclObject Interpreter::execute(const string& command)
+TclObject Interpreter::execute(zstring_view command)
 {
 	int success = Tcl_Eval(interp, command.c_str());
 	if (success != TCL_OK) {
@@ -219,16 +219,7 @@ TclObject Interpreter::execute(const string& command)
 	return TclObject(Tcl_GetObjResult(interp));
 }
 
-TclObject Interpreter::execute(const TemporaryString& command)
-{
-	int success = Tcl_Eval(interp, command.c_str());
-	if (success != TCL_OK) {
-		throw CommandException(Tcl_GetStringResult(interp));
-	}
-	return TclObject(Tcl_GetObjResult(interp));
-}
-
-TclObject Interpreter::executeFile(const string& filename)
+TclObject Interpreter::executeFile(zstring_view filename)
 {
 	int success = Tcl_EvalFile(interp, filename.c_str());
 	if (success != TCL_OK) {

src/commands/Interpreter.hh
@@ -4,6 +4,7 @@
 #include "TemporaryString.hh"
 #include "TclParser.hh"
 #include "TclObject.hh"
+#include "zstring_view.hh"
 #include <tcl.h>
 #include <string_view>
 #include <string>
@@ -26,14 +27,13 @@ public:
 	void setOutput(InterpreterOutput* output_) { output = output_; }
 
 	void init(const char* programName);
-	bool hasCommand(const std::string& name) const;
-	void registerCommand(const std::string& name, Command& command);
+	bool hasCommand(zstring_view name) const;
+	void registerCommand(zstring_view name, Command& command);
 	void unregisterCommand(Command& command);
 	[[nodiscard]] TclObject getCommandNames();
-	[[nodiscard]] bool isComplete(const std::string& command) const;
-	TclObject execute(const std::string& command);
-	TclObject execute(const TemporaryString& command);
-	TclObject executeFile(const std::string& filename);
+	[[nodiscard]] bool isComplete(zstring_view command) const;
+	TclObject execute(zstring_view command);
+	TclObject executeFile(zstring_view filename);
 
 	void setVariable(const TclObject& name, const TclObject& value);
 	void unsetVariable(const char* name);

src/commands/MSXCommandController.cc
@@ -9,6 +9,7 @@
 #include "Setting.hh"
 #include "Event.hh"
 #include "MSXException.hh"
+#include "TemporaryString.hh"
 #include "stl.hh"
 #include <iostream>
 #include <memory>
@@ -58,18 +59,18 @@ MSXCommandController::~MSXCommandController()
 	globalCommandController.getInterpreter().deleteNamespace(machineID);
 }
 
-string MSXCommandController::getFullName(string_view name)
+TemporaryString MSXCommandController::getFullName(string_view name)
 {
-	return strCat(machineID, name);
+	return tmpStrCat(machineID, name);
 }
 
-void MSXCommandController::registerCommand(Command& command, const string& str)
+void MSXCommandController::registerCommand(Command& command, zstring_view str)
 {
 	assert(!hasCommand(str));
 	assert(command.getName() == str);
 	commandMap.insert_noDuplicateCheck(&command);
 
-	string fullname = getFullName(str);
+	auto fullname = getFullName(str);
 	globalCommandController.registerCommand(command, fullname);
 	globalCommandController.registerProxyCommand(str);
 
@@ -83,21 +84,21 @@ void MSXCommandController::unregisterCommand(Command& command, string_view str)
 	commandMap.erase(str);
 
 	globalCommandController.unregisterProxyCommand(str);
-	string fullname = getFullName(str);
+	auto fullname = getFullName(str);
 	globalCommandController.unregisterCommand(command, fullname);
 }
 
 void MSXCommandController::registerCompleter(CommandCompleter& completer,
                                              string_view str)
 {
-	string fullname = getFullName(str);
+	auto fullname = getFullName(str);
 	globalCommandController.registerCompleter(completer, fullname);
 }
 
 void MSXCommandController::unregisterCompleter(CommandCompleter& completer,
                                                string_view str)
 {
-	string fullname = getFullName(str);
+	auto fullname = getFullName(str);
 	globalCommandController.unregisterCompleter(completer, fullname);
 }
 
@@ -132,7 +133,7 @@ bool MSXCommandController::hasCommand(string_view command) const
 	return findCommand(command) != nullptr;
 }
 
-TclObject MSXCommandController::executeCommand(const string& command,
+TclObject MSXCommandController::executeCommand(zstring_view command,
                                                CliConnection* connection)
 {
 	return globalCommandController.executeCommand(command, connection);

commit fea41799b059eb2f9c3742443efccaf109012188

Fix compiler error in windows specific code
Introduced in ae30ae2be.
Easy to fix. Unfortunately I can't easily test-compile on windows before
pushing.

src/file/FileOperations.cc
@@ -115,7 +115,7 @@ void mkdir(zstring_view path, mode_t mode)
 		StringOp::endsWith(path, ":/")) {
 		return;
 	}
-	int result = _wmkdir(utf8to16(getNativePath(path)).c_str());
+	int result = _wmkdir(utf8to16(getNativePath(string(path))).c_str());
 #else
 	int result = ::mkdir(path.c_str(), mode);
 #endif