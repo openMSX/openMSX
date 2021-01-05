openMSX/openMSX

commit b6fd8329802f0a3db651825688e6076bc752b5dc

[6/6] Repaint via VideoSystem
This allows a GUI toolkit to initialize its drawing procedure.

src/ReverseManager.cc
@@ -320,7 +320,7 @@ static void reportProgress(Reactor& reactor, const EmuTime& targetTime, int perc
 		std::fmod(targetTimeDisp, 60.0) <<
 		"... " << percentage << '%';
 	reactor.getCliComm().printProgress(sstr.str());
-	reactor.getDisplay().repaint();
+	reactor.getDisplay().repaintDelayed(0);
 }
 
 void ReverseManager::goTo(

src/file/FilePool.cc
@@ -332,7 +332,7 @@ static void reportProgress(const string& filename, size_t percentage,
 {
 	reactor.getCliComm().printProgress(
 		"Calculating SHA1 sum for ", filename, "... ", percentage, '%');
-	reactor.getDisplay().repaint();
+	reactor.getDisplay().repaintDelayed(0);
 }
 
 static Sha1Sum calcSha1sum(File& file, Reactor& reactor)
@@ -467,7 +467,7 @@ File FilePool::scanFile(const Sha1Sum& sha1sum, const string& filename,
 			sha1sum.toString(), "...\nIndexing filepool ", poolPath,
 			": [", progress.amountScanned, "]: ",
 			std::string_view(filename).substr(poolPath.size()));
-		reactor.getDisplay().repaint();
+		reactor.getDisplay().repaintDelayed(0);
 	}
 
 	// Note: do NOT call 'reactor.getEventDistributor().deliverEvents()'.

src/ide/HD.cc
@@ -139,7 +139,7 @@ void HD::showProgress(size_t position, size_t maxPosition)
 		motherBoard.getMSXCliComm().printProgress(
 			"Calculating hash for ", filename.getResolved(),
 			"... ", percentage, '%');
-		motherBoard.getReactor().getDisplay().repaint();
+		motherBoard.getReactor().getDisplay().repaintDelayed(0);
 		everDidProgress = true;
 	}
 }

src/video/Display.cc
@@ -175,7 +175,7 @@ Display::Layers::iterator Display::baseLayer()
 
 void Display::executeRT()
 {
-	repaint();
+	videoSystem->repaint();
 }
 
 int Display::signalEvent(const std::shared_ptr<const Event>& event)
@@ -183,7 +183,7 @@ int Display::signalEvent(const std::shared_ptr<const Event>& event)
 	if (event->getType() == OPENMSX_FINISH_FRAME_EVENT) {
 		auto& ffe = checked_cast<const FinishFrameEvent&>(*event);
 		if (ffe.needRender()) {
-			repaint();
+			videoSystem->repaint();
 			reactor.getEventDistributor().distributeEvent(
 				std::make_shared<SimpleEvent>(
 					OPENMSX_FRAME_DRAWN_EVENT));
@@ -211,7 +211,7 @@ int Display::signalEvent(const std::shared_ptr<const Event>& event)
 		//  port discovers that the graphics context is gone.
 		// -When gaining the focus, this repaint does nothing as
 		//  the renderFrozen flag is still false
-		repaint();
+		videoSystem->repaint();
 		auto& focusEvent = checked_cast<const FocusEvent&>(*event);
 		ad_printf("Setting renderFrozen to %d", !focusEvent.getGain());
 		renderFrozen = !focusEvent.getGain();

src/video/Display.hh
@@ -46,6 +46,7 @@ public:
 	CommandConsole& getCommandConsole() { return commandConsole; }
 
 	/** Redraw the display.
+	  * repaint() should only be called from the VideoSystem.
 	  */
 	void repaint();
 	void repaint(OutputSurface& surface);

src/video/DummyVideoSystem.cc
@@ -42,4 +42,8 @@ void DummyVideoSystem::showCursor(bool /*show*/)
 {
 }
 
+void DummyVideoSystem::repaint()
+{
+}
+
 } // namespace openmsx

src/video/DummyVideoSystem.hh
@@ -20,6 +20,7 @@ public:
 	void flush() override;
 	OutputSurface* getOutputSurface() override;
 	void showCursor(bool show) override;
+	void repaint() override;
 };
 
 } // namespace openmsx

src/video/SDLVideoSystem.cc
@@ -249,6 +249,11 @@ void SDLVideoSystem::showCursor(bool show)
 	SDL_ShowCursor(show ? SDL_ENABLE : SDL_DISABLE);
 }
 
+void SDLVideoSystem::repaint()
+{
+	display.repaint();
+}
+
 void SDLVideoSystem::resize()
 {
 	auto& rtScheduler         = reactor.getRTScheduler();

commit ca95ac844ca1112d8b6b9b96f328fd6a8355687c

Fix drawing of OSD progress info
E.g. while calculating sha1, tth, or during a long "reverse goto".

This got broken in revision b6fd832980.

That commit changed 'repaint()' to 'repaintDelayed(0)'. The former
immediately draws a frame, the latter waits till the main event loop
triggers it. Though in case of these long calculations we
(intentionally) don't trigger the main event loop.

Fixed by partially reverting this commit.

src/ReverseManager.cc
@@ -317,7 +317,7 @@ static void reportProgress(Reactor& reactor, const EmuTime& targetTime, int perc
 		std::fmod(targetTimeDisp, 60.0) <<
 		"... " << percentage << '%';
 	reactor.getCliComm().printProgress(sstr.str());
-	reactor.getDisplay().repaintDelayed(0);
+	reactor.getDisplay().repaint();
 }
 
 void ReverseManager::goTo(

src/file/FilePool.cc
@@ -151,7 +151,7 @@ void FilePool::reportProgress(std::string_view message)
 {
 	if (quit) core.abort();
 	reactor.getCliComm().printProgress(message);
-	reactor.getDisplay().repaintDelayed(0);
+	reactor.getDisplay().repaint();
 }
 
 int FilePool::signalEvent(const std::shared_ptr<const Event>& event)

src/ide/HD.cc
@@ -139,7 +139,7 @@ void HD::showProgress(size_t position, size_t maxPosition)
 		motherBoard.getMSXCliComm().printProgress(
 			"Calculating hash for ", filename.getResolved(),
 			"... ", percentage, '%');
-		motherBoard.getReactor().getDisplay().repaintDelayed(0);
+		motherBoard.getReactor().getDisplay().repaint();
 		everDidProgress = true;
 	}
 }