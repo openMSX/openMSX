a11a8d8291644d584290dc9c5edc050f09c40d20
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2021-03-31 20:50:56 +0200

    Go to release mode for openMSX 17.0-rc1

--- build/version.py
+++ build/version.py
@@ -12,20 +12,20 @@ import re
 packageName = 'openmsx'
 
 # Version number.
-packageVersionNumber = '16.0'
+packageVersionNumber = '17.0'
 
 # Version code for Android must be an incremental number
 # Increase this number for each release build. For a dev build, the
 # version number is based on the git commit count but for a release
 # build, it must be hardcoded
-androidReleaseVersionCode=12
+androidReleaseVersionCode=13
 
 # Note: suffix should be empty or with dash, like "-rc1" or "-test1"
-packageVersionSuffix = ''
+packageVersionSuffix = '-rc1'
 packageVersion = packageVersionNumber + packageVersionSuffix
 
 # Is this a release version ("True") or development version ("False").
-releaseFlag = False
+releaseFlag = True
 
 def _extractRevisionFromStdout(log, command, regex):
 	text = captureStdout(log, command)
