bb2f1193ed2f3d2b44ece1d7453830edcc0f950c
m9710797 <vermaelen.wouter@gmail.com>
2021-04-24 15:04:25 +0200

    Extended CasImage to also support SVI .cas files
    Before it only understood MSX .cas files.
    
    This patch both simplifies and generalizes the CasImage class:
    - Generalize: before the frequency of the generated wave data was fixed
      (set at compile-time). Now it's dynamic (set at run-time).
    - Simplify: move all of the .cas file parsing code out of the class. All
      that remains is a buffer of the final wave data and the frequency of
      this data.
    
    The CasImage constructor checks the first few bytes of the .cas file and
    then parses it either as a SVI or as a MSX cas file. (And this parsing
    code is implemented outside of the CasImage classr. Bonus: the result
    can now be made 'const').

--- src/cassette/CasImage.cc
+++ src/cassette/CasImage.cc
@@ -3,25 +3,14 @@
 #include "FilePool.hh"
 #include "Filename.hh"
 #include "CliComm.hh"
-#include "Clock.hh"
 #include "MSXException.hh"
+#include "span.hh"
 #include "stl.hh"
 #include "xrange.hh"
 #include <cstring> // for memcmp
 
 namespace openmsx {
 
-// output settings
-
-// a higher baudrate doesn't work anymore, but it is unclear why, because 4600
-// baud should work (known from Speedsave 4000 and Turbo 5000 programs).
-// 3765 still works on a Toshiba HX-10 and Philips NMS 8250, but not on a
-// Panasonic FS-A1WSX, on which 3763 is the max. National CF-2000 has 3762 as
-// the max. Let's take 3760 then as a safe value.
-// UPDATE: that seems to break RUN"CAS:" type of programs. 3744 seems to work
-// for those as well (we don't understand why yet)
-constexpr unsigned BAUDRATE = 3744;
-constexpr unsigned OUTPUT_FREQUENCY = 4 * BAUDRATE; // 4 samples per bit
 // We oversample the audio signal for better sound quality (especially in
 // combination with the hq resampler). Without oversampling the audio output
 // could contain portions like this:
@@ -34,6 +23,28 @@ constexpr unsigned OUTPUT_FREQUENCY = 4 * BAUDRATE; // 4 samples per bit
 // So every sample repeated 4 times.
 constexpr unsigned AUDIO_OVERSAMPLE = 4;
 
+static void append(std::vector<int8_t>& wave, size_t count, int8_t value)
+{
+	wave.insert(wave.end(), count, value);
+}
+
+static void writeSilence(std::vector<int8_t>& wave, unsigned s)
+{
+	append(wave, s, 0);
+}
+
+namespace MSX_CAS {
+
+// a higher baudrate doesn't work anymore, but it is unclear why, because 4600
+// baud should work (known from Speedsave 4000 and Turbo 5000 programs).
+// 3765 still works on a Toshiba HX-10 and Philips NMS 8250, but not on a
+// Panasonic FS-A1WSX, on which 3763 is the max. National CF-2000 has 3762 as
+// the max. Let's take 3760 then as a safe value.
+// UPDATE: that seems to break RUN"CAS:" type of programs. 3744 seems to work
+// for those as well (we don't understand why yet)
+constexpr unsigned BAUDRATE = 3744;
+constexpr unsigned OUTPUT_FREQUENCY = 4 * BAUDRATE; // 4 samples per bit
+
 // number of output bytes for silent parts
 constexpr unsigned SHORT_SILENCE = OUTPUT_FREQUENCY * 1; // 1 second
 constexpr unsigned LONG_SILENCE  = OUTPUT_FREQUENCY * 2; // 2 seconds
@@ -43,44 +54,248 @@ constexpr unsigned LONG_HEADER  = 16000 / 2;
 constexpr unsigned SHORT_HEADER =  4000 / 2;
 
 // headers definitions
-constexpr byte CAS_HEADER   [ 8] = { 0x1F,0xA6,0xDE,0xBA,0xCC,0x13,0x7D,0x74 };
-constexpr byte ASCII_HEADER [10] = { 0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA };
-constexpr byte BINARY_HEADER[10] = { 0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0 };
-constexpr byte BASIC_HEADER [10] = { 0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3 };
+constexpr uint8_t CAS_HEADER   [ 8] = { 0x1F,0xA6,0xDE,0xBA,0xCC,0x13,0x7D,0x74 };
+constexpr uint8_t ASCII_HEADER [10] = { 0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA,0xEA };
+constexpr uint8_t BINARY_HEADER[10] = { 0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0,0xD0 };
+constexpr uint8_t BASIC_HEADER [10] = { 0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3,0xD3 };
 
-
-CasImage::CasImage(const Filename& filename, FilePool& filePool, CliComm& cliComm)
+static void write0(std::vector<int8_t>& wave)
 {
-	setFirstFileType(CassetteImage::UNKNOWN);
-	convert(filename, filePool, cliComm);
+	::append(wave, {127, 127, -127, -127});
+}
+static void write1(std::vector<int8_t>& wave)
+{
+	::append(wave, {127, -127, 127, -127});
 }
 
-int16_t CasImage::getSampleAt(EmuTime::param time)
+static void writeHeader(std::vector<int8_t>& wave, unsigned s)
 {
-	constexpr Clock<OUTPUT_FREQUENCY> zero(EmuTime::zero());
-	unsigned pos = zero.getTicksTill(time);
-	return pos < output.size() ? output[pos] * 256 : 0;
+	repeat(s, [&] { write1(wave); });
+}
+
+static void writeByte(std::vector<int8_t>& wave, uint8_t b)
+{
+	// one start bit
+	write0(wave);
+	// eight data bits
+	for (auto i : xrange(8)) {
+		if (b & (1 << i)) {
+			write1(wave);
+		} else {
+			write0(wave);
+		}
+	}
+	// two stop bits
+	write1(wave);
+	write1(wave);
+}
+
+// write data until a header is detected
+static bool writeData(std::vector<int8_t>& wave, span<const uint8_t> cas, size_t& pos)
+{
+	bool eof = false;
+	while ((pos + 8) <= cas.size()) {
+		if (memcmp(&cas[pos], CAS_HEADER, 8) == 0) {
+			return eof;
+		}
+		writeByte(wave, cas[pos]);
+		if (cas[pos] == 0x1A) {
+			eof = true;
+		}
+		pos++;
+	}
+	while (pos < cas.size()) {
+		writeByte(wave, cas[pos++]);
+	}
+	return false;
+}
+
+static CasImage::Data convert(span<const uint8_t> cas, const std::string& filename, CliComm& cliComm,
+                              CassetteImage::FileType& firstFileType)
+{
+	CasImage::Data data;
+	data.frequency = OUTPUT_FREQUENCY;
+	auto& wave = data.wave;
+
+	// search for a header in the .cas file
+	bool issueWarning = false;
+	bool headerFound = false;
+	bool firstFile = true;
+	size_t pos = 0;
+	while ((pos + 8) <= cas.size()) {
+		if (memcmp(&cas[pos], CAS_HEADER, 8) == 0) {
+			// it probably works fine if a long header is used for every
+			// header but since the msx bios makes a distinction between
+			// them, we do also (hence a lot of code).
+			headerFound = true;
+			pos += 8;
+			writeSilence(wave, LONG_SILENCE);
+			writeHeader(wave, LONG_HEADER);
+			if ((pos + 10) <= cas.size()) {
+				// determine file type
+				auto type = [&] {
+					if (memcmp(&cas[pos], ASCII_HEADER, 10) == 0) {
+						return CassetteImage::ASCII;
+					} else if (memcmp(&cas[pos], BINARY_HEADER, 10) == 0) {
+						return CassetteImage::BINARY;
+					} else if (memcmp(&cas[pos], BASIC_HEADER, 10) == 0) {
+						return CassetteImage::BASIC;
+					} else {
+						return CassetteImage::UNKNOWN;
+					}
+				}();
+				if (firstFile) firstFileType = type;
+				switch (type) {
+					case CassetteImage::ASCII:
+						writeData(wave, cas, pos);
+						bool eof;
+						do {
+							pos += 8;
+							writeSilence(wave, SHORT_SILENCE);
+							writeHeader(wave, SHORT_HEADER);
+							eof = writeData(wave, cas, pos);
+						} while (!eof && ((pos + 8) <= cas.size()));
+						break;
+					case CassetteImage::BINARY:
+					case CassetteImage::BASIC:
+						writeData(wave, cas, pos);
+						writeSilence(wave, SHORT_SILENCE);
+						writeHeader(wave, SHORT_HEADER);
+						pos += 8;
+						writeData(wave, cas, pos);
+						break;
+					default:
+						// unknown file type: using long header
+						writeData(wave, cas, pos);
+						break;
+				}
+			} else {
+				// unknown file type: using long header
+				writeData(wave, cas, pos);
+			}
+			firstFile = false;
+		} else {
+			// skipping unhandled data, shouldn't occur in normal cas file
+			pos++;
+			issueWarning = true;
+		}
+	}
+	if (!headerFound) {
+		throw MSXException(filename, ": not a valid CAS image");
+	}
+	if (issueWarning) {
+		 cliComm.printWarning("Skipped unhandled data in ", filename);
+	}
+
+	return data;
+}
+
+} // namespace MSX_CAS
+
+namespace SVI_CAS {
+
+static constexpr std::array<uint8_t, 17> header = {
+	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55,
+	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55,
+	0x7f,
+};
+
+static void writeBit(std::vector<int8_t>& wave, bool bit)
+{
+	size_t count = bit ? 1 : 2;
+	append(wave, count,  127);
+	append(wave, count, -127);
+}
+
+static void writeByte(std::vector<int8_t>& wave, uint8_t byte)
+{
+	for (int i = 7; i >= 0; --i) {
+		writeBit(wave, (byte >> i) & 1);
+	}
+}
+
+static void processBlock(span<const uint8_t> subBuf, std::vector<int8_t>& wave)
+{
+	writeSilence(wave, 1200);
+	writeBit(wave, true);
+	repeat(199, [&] { writeByte(wave, 0x55); });
+	writeByte(wave, 0x7f);
+	for (uint8_t val : subBuf) {
+		writeBit(wave, false);
+		writeByte(wave, val);
+	}
+}
+
+static CasImage::Data convert(span<const uint8_t> cas)
+{
+	CasImage::Data data;
+	data.frequency = 4800;
+
+	auto prevHeader = cas.begin() + header.size();
+	while (true) {
+		auto nextHeader = std::search(prevHeader, cas.end(),
+		                              header.begin(), header.end());
+		processBlock(span(prevHeader, nextHeader), data.wave);
+		if (nextHeader == cas.end()) break;
+		prevHeader = nextHeader + header.size();
+	}
+	return data;
+}
+
+} // namespace SVI_CAS
+
+CasImage::Data CasImage::init(const Filename& filename, FilePool& filePool, CliComm& cliComm)
+{
+	File file(filename);
+	auto cas = file.mmap();
+
+	auto fileType = CassetteImage::UNKNOWN;
+	auto result = [&] {
+		if ((cas.size() >= SVI_CAS::header.size()) &&
+		    (memcmp(cas.data(), SVI_CAS::header.data(), SVI_CAS::header.size()) == 0)) {
+			return SVI_CAS::convert(cas);
+		} else {
+			return MSX_CAS::convert(cas, filename.getOriginal(), cliComm, fileType);
+		}
+	}();
+	setFirstFileType(fileType);
+
+	// conversion successful, now calc sha1sum
+	setSha1Sum(filePool.getSha1Sum(file));
+
+	return result;
+}
+
+CasImage::CasImage(const Filename& filename, FilePool& filePool, CliComm& cliComm)
+	: data(init(filename, filePool, cliComm))
+{
+}
+
+int16_t CasImage::getSampleAt(EmuTime::param time) const
+{
+	EmuDuration d = time - EmuTime::zero();
+	unsigned pos = d.getTicksAt(data.frequency);
+	return pos < data.wave.size() ? data.wave[pos] * 256 : 0;
 }
 
 EmuTime CasImage::getEndTime() const
 {
-	Clock<OUTPUT_FREQUENCY> clk(EmuTime::zero());
-	clk += unsigned(output.size());
-	return clk.getTime();
+	EmuDuration d = EmuDuration::hz(data.frequency) * data.wave.size();
+	return EmuTime::zero() + d;
 }
 
 unsigned CasImage::getFrequency() const
 {
-	return OUTPUT_FREQUENCY * AUDIO_OVERSAMPLE;
+	return data.frequency * AUDIO_OVERSAMPLE;
 }
 
 void CasImage::fillBuffer(unsigned pos, float** bufs, unsigned num) const
 {
-	size_t nbSamples = output.size();
+	size_t nbSamples = data.wave.size();
 	if ((pos / AUDIO_OVERSAMPLE) < nbSamples) {
 		for (auto i : xrange(num)) {
 			bufs[0][i] = ((pos / AUDIO_OVERSAMPLE) < nbSamples)
-			           ? output[pos / AUDIO_OVERSAMPLE]
+			           ? data.wave[pos / AUDIO_OVERSAMPLE]
 			           : 0.0f;
 			++pos;
 		}
@@ -92,142 +307,6 @@ void CasImage::fillBuffer(unsigned pos, float** bufs, unsigned num) const
 float CasImage::getAmplificationFactorImpl() const
 {
 	return 1.0f / 128;
-}
-
-void CasImage::write0()
-{
-	append(output, {127, 127, -127, -127});
-}
-void CasImage::write1()
-{
-	append(output, {127, -127, 127, -127});
-}
-
-// write a header signal
-void CasImage::writeHeader(unsigned s)
-{
-	repeat(s, [&] { write1(); });
-}
-
-// write silence
-void CasImage::writeSilence(unsigned s)
-{
-	output.insert(end(output), s, 0);
-}
-
-// write a byte
-void CasImage::writeByte(byte b)
-{
-	// one start bit
-	write0();
-	// eight data bits
-	for (auto i : xrange(8)) {
-		if (b & (1 << i)) {
-			write1();
-		} else {
-			write0();
-		}
-	}
-	// two stop bits
-	write1();
-	write1();
-}
-
-// write data until a header is detected
-bool CasImage::writeData(span<const byte> buf, size_t& pos)
-{
-	bool eof = false;
-	while ((pos + 8) <= buf.size()) {
-		if (memcmp(&buf[pos], CAS_HEADER, 8) == 0) {
-			return eof;
-		}
-		writeByte(buf[pos]);
-		if (buf[pos] == 0x1A) {
-			eof = true;
-		}
-		pos++;
-	}
-	while (pos < buf.size()) {
-		writeByte(buf[pos++]);
-	}
-	return false;
-}
-
-void CasImage::convert(const Filename& filename, FilePool& filePool, CliComm& cliComm)
-{
-	File file(filename);
-	auto buf = file.mmap();
-
-	// search for a header in the .cas file
-	bool issueWarning = false;
-	bool headerFound = false;
-	bool firstFile = true;
-	size_t pos = 0;
-	while ((pos + 8) <= buf.size()) {
-		if (memcmp(&buf[pos], CAS_HEADER, 8) == 0) {
-			// it probably works fine if a long header is used for every
-			// header but since the msx bios makes a distinction between
-			// them, we do also (hence a lot of code).
-			headerFound = true;
-			pos += 8;
-			writeSilence(LONG_SILENCE);
-			writeHeader(LONG_HEADER);
-			if ((pos + 10) <= buf.size()) {
-				// determine file type
-				FileType type = CassetteImage::UNKNOWN;
-				if (memcmp(&buf[pos], ASCII_HEADER, 10) == 0) {
-					type = CassetteImage::ASCII;
-				} else if (memcmp(&buf[pos], BINARY_HEADER, 10) == 0) {
-					type = CassetteImage::BINARY;
-				} else if (memcmp(&buf[pos], BASIC_HEADER, 10) == 0) {
-					type = CassetteImage::BASIC;
-				}
-				if (firstFile) setFirstFileType(type);
-				switch (type) {
-					case CassetteImage::ASCII:
-						writeData(buf, pos);
-						bool eof;
-						do {
-							pos += 8;
-							writeSilence(SHORT_SILENCE);
-							writeHeader(SHORT_HEADER);
-							eof = writeData(buf, pos);
-						} while (!eof && ((pos + 8) <= buf.size()));
-						break;
-					case CassetteImage::BINARY:
-					case CassetteImage::BASIC:
-						writeData(buf, pos);
-						writeSilence(SHORT_SILENCE);
-						writeHeader(SHORT_HEADER);
-						pos += 8;
-						writeData(buf, pos);
-						break;
-					default:
-						// unknown file type: using long header
-						writeData(buf, pos);
-						break;
-				}
-			} else {
-				// unknown file type: using long header
-				writeData(buf, pos);
-			}
-			firstFile = false;
-		} else {
-			// skipping unhandled data, shouldn't occur in normal cas file
-			pos++;
-			issueWarning = true;
-		}
-	}
-	if (!headerFound) {
-		throw MSXException(filename.getOriginal(), ": not a valid CAS image");
-	}
-	if (issueWarning) {
-		 cliComm.printWarning("Skipped unhandled data in ",
-		                      filename.getOriginal());
-	}
-
-	// conversion successful, now calc sha1sum
-	setSha1Sum(filePool.getSha1Sum(file));
 }
 
 } // namespace openmsx
--- src/cassette/WavImage.cc
+++ src/cassette/WavImage.cc
@@ -114,7 +114,7 @@ WavImage::~WavImage()
 	WavImageCache::instance().release(wav);
 }
 
-int16_t WavImage::getSampleAt(EmuTime::param time)
+int16_t WavImage::getSampleAt(EmuTime::param time) const
 {
 	// The WAV file is typically sampled at 44kHz, but the MSX may sample
 	// the signal at arbitrary moments in time. Initially we would simply
