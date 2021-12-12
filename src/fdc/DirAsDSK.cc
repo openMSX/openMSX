openMSX/openMSX

commit b0b3d6bcd123402a3c5abb8d451f66c56480d2c6

Use xrange() for simple counting for-loops
This code refactoring is a bit experimental. I think it's a good idea,
but it's hard to know for sure without actually trying it on a
non-trivial codebase and gaining some more experience with it. This
patch starts that experiment, and who knows, maybe in half a year I'll
have a different opinion.

We have many simple for-loops in the code of the form:
    for (int i = 0; i < NUM_LEDS; ++i) { ...
    for (unsigned i = 0; i < v.size(); ++i) { ...

This patch rewrites those to:
    for (auto i : xrange(NUM_LEDS)) { ...
    for (auto i : xrange(v.size())) { ...

The meaning of both versions is the same, and often the compiler even
generates the exact same (x86) code for them. Then why move away for
this typical c-style loop?

The c-style loop has some problems:
- In the first example the type of the constant 'NUM_LEDS' is not
  immediately visible. It's defined in a completely different part of
  the source code. It might be 'int', 'unsigned', 'size_t', ... Ideally
  the induction variable 'i' in the loop should have the same type. In
  the example I picked 'int', but from looking only at the loop you
  cannot tell if that's correct. In the xrange-variant we could declare
  the type as 'auto' and then the compiler will automatically deduce the
  correct type. (We can write 'auto i = 0' in the first variant, but
  that doesn't help: this deduces the type of '0' (which is 'int') not
  the type of 'NUM_LEDS').
- In the second example it's even more important to pick the correct
  type. If we assume 'v' is some container (e.g. a std::vector), then
  probably 'unsigned' is the wrong type, it likely should be 'size_t'.
  You could rightfully argue that this doesn't matter in practice: as
  long as there are no more than 4 billion element in the vector, using
  a 32bit rather than a 64bit induction variable should work fine.
  However this also forces the compiler to generate slower code because

src/fdc/DirAsDSK.cc
@@ -10,6 +10,7 @@
 #include "one_of.hh"
 #include "ranges.hh"
 #include "stl.hh"
+#include "xrange.hh"
 #include <cassert>
 #include <cstring>
 #include <vector>
@@ -190,7 +191,7 @@ bool DirAsDSK::checkMSXFileExists(
 		}
 		visited[msxDirSector] = true;
 
-		for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
+		for (auto idx : xrange(DIR_ENTRIES_PER_SECTOR)) {
 			DirIndex dirIndex(msxDirSector, idx);
 			if (memcmp(msxDir(dirIndex).filename,
 			           msxFilename.data(), 8 + 3) == 0) {
@@ -476,7 +477,7 @@ void DirAsDSK::deleteMSXFilesInDir(unsigned msxDirSector)
 		}
 		visited[msxDirSector] = true;
 
-		for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
+		for (auto idx : xrange(DIR_ENTRIES_PER_SECTOR)) {
 			deleteMSXFile(DirIndex(msxDirSector, idx));
 		}
 		msxDirSector = nextMsxDirSector(msxDirSector);
@@ -573,7 +574,7 @@ void DirAsDSK::importHostFile(DirIndex dirIndex, FileOperations::Stat& fst)
 
 		while (remainingSize && (curCl < maxCluster)) {
 			unsigned logicalSector = clusterToSector(curCl);
-			for (unsigned i = 0; i < SECTORS_PER_CLUSTER; ++i) {
+			for (auto i : xrange(SECTORS_PER_CLUSTER)) {
 				unsigned sector = logicalSector + i;
 				assert(sector < nofSectors);
 				auto* buf = &sectors[sector];
@@ -750,7 +751,7 @@ void DirAsDSK::addNewDirectory(const string& hostSubDir, const string& hostName,
 
 		// Initialize the new directory.
 		newMsxDirSector = clusterToSector(cluster);
-		for (unsigned i = 0; i < SECTORS_PER_CLUSTER; ++i) {
+		for (auto i : xrange(SECTORS_PER_CLUSTER)) {
 			memset(&sectors[newMsxDirSector + i], 0, SECTOR_SIZE);
 		}
 		DirIndex idx0(newMsxDirSector, 0); // entry for "."
@@ -847,7 +848,7 @@ DirAsDSK::DirIndex DirAsDSK::getFreeDirEntry(unsigned msxDirSector)
 		}
 		visited[msxDirSector] = true;
 
-		for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
+		for (auto idx : xrange(DIR_ENTRIES_PER_SECTOR)) {
 			DirIndex dirIndex(msxDirSector, idx);
 			const char* msxName = msxDir(dirIndex).filename;
 			if (msxName[0] == one_of(char(0x00), char(0xE5))) {
@@ -922,7 +923,7 @@ void DirAsDSK::writeFATSector(unsigned sector, const SectorBuffer& buf)
 	memcpy(&sectors[sector], &buf, sizeof(buf));
 
 	// Look for changes.
-	for (unsigned i = FIRST_CLUSTER; i < maxCluster; ++i) {
+	for (auto i : xrange(FIRST_CLUSTER, maxCluster)) {
 		if (readFAT(i) != readFATHelper(oldFAT.data(), i)) {
 			exportFileFromFATChange(i, oldFAT.data());
 		}
@@ -934,8 +935,8 @@ void DirAsDSK::writeFATSector(unsigned sector, const SectorBuffer& buf)
 	// that actually contains FAT info. E.g. not the media ID at the
 	// beginning nor the unused part at the end. And for example the 'CALL
 	// FORMAT' routine also writes these parts of the FAT.
-	for (unsigned i = FIRST_CLUSTER; i < maxCluster; ++i) {
-		assert(readFAT(i) == readFATHelper(oldFAT.data(), i));
+	for (auto i : xrange(FIRST_CLUSTER, maxCluster)) {
+		assert(readFAT(i) == readFATHelper(oldFAT.data(), i)); (void)i;
 	}
 }
 
@@ -975,7 +976,7 @@ std::pair<unsigned, unsigned> DirAsDSK::getChainStart(unsigned cluster)
 	// because usually FAT chains are allocated in ascending order, this
 	// search is fast O(N).
 	unsigned chainLength = 0;
-	for (unsigned i = FIRST_CLUSTER; i < maxCluster; ++i) {
+	for (auto i : xrange(FIRST_CLUSTER, maxCluster)) {
 		if (readFAT(i) == cluster) {
 			// Found a predecessor.
 			cluster = i;
@@ -999,7 +1000,7 @@ template<typename FUNC> bool DirAsDSK::scanMsxDirs(FUNC func, unsigned sector)
 			// About to process a new directory sector.
 			if (func.onDirSector(sector)) return true;
 
-			for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
+			for (auto idx : xrange(DIR_ENTRIES_PER_SECTOR)) {
 				// About to process a new directory entry.
 				DirIndex dirIndex(sector, idx);
 				const MSXDirEntry& entry = msxDir(dirIndex);
@@ -1219,7 +1220,7 @@ void DirAsDSK::exportToHostDir(DirIndex dirIndex, const string& hostName)
 				// the directory entry is updated.
 				return;
 			}
-			for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
+			for (auto idx : xrange(DIR_ENTRIES_PER_SECTOR)) {
 				exportToHost(DirIndex(msxDirSector, idx), dirIndex);
 			}
 			msxDirSector = nextMsxDirSector(msxDirSector);
@@ -1253,7 +1254,7 @@ void DirAsDSK::exportToHostFile(DirIndex dirIndex, const string& hostName)
 			visited[curCl] = true;
 
 			unsigned logicalSector = clusterToSector(curCl);
-			for (unsigned i = 0; i < SECTORS_PER_CLUSTER; ++i) {
+			for (auto i : xrange(SECTORS_PER_CLUSTER)) {
 				if (offset >= msxSize) break;
 				unsigned sector = logicalSector + i;
 				assert(sector < nofSectors);
@@ -1274,7 +1275,7 @@ void DirAsDSK::writeDIRSector(unsigned sector, DirIndex dirDirIndex,
                               const SectorBuffer& buf)
 {
 	// Look for changed directory entries.
-	for (unsigned idx = 0; idx < DIR_ENTRIES_PER_SECTOR; ++idx) {
+	for (auto idx : xrange(DIR_ENTRIES_PER_SECTOR)) {
 		const auto& newEntry = buf.dirEntry[idx];
 		DirIndex dirIndex(sector, idx);
 		if (memcmp(&msxDir(dirIndex), &newEntry, sizeof(newEntry)) != 0) {

commit 0d342a9d17eff9451561928463e3dc47d015b2af

Bugfix in DirAsDSK::getChainStart()
This bug was introduced in b0b3d6bcd12 (2020/12/13).
That patch replaced loops like:
  for (unsigned i = start; i < stop; ++i) { ..
with
  for (auto i : xrange(start, stop)) { ..
But that's not correct when 'i' is changed in the body of the loop.

Discovered purely by accident while playing with a new version of
clang-tidy (but older versions would likely have found it as well). More
specifically the pass "clang-analyzer-deadcode.DeadStores" reported that
in the above loop the variable 'i' was written to but later that
overwritten value is no longer used.

I checked the other code for similar bugs, but found none. But this did
trigger me to run many more clang-tidy checks on the code. None found
any additional bugs, just some (minor) code cleanups. I'll add those in
separate patches.

src/fdc/DirAsDSK.cc
@@ -972,7 +972,7 @@ std::pair<unsigned, unsigned> DirAsDSK::getChainStart(unsigned cluster)
 	// because usually FAT chains are allocated in ascending order, this
 	// search is fast O(N).
 	unsigned chainLength = 0;
-	for (auto i : xrange(FIRST_CLUSTER, maxCluster)) {
+	for (unsigned i = FIRST_CLUSTER; i < maxCluster; ++i) { // note: i changed in loop!
 		if (readFAT(i) == cluster) {
 			// Found a predecessor.
 			cluster = i;