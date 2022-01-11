openMSX/openMSX

commit c9b952c9826554fcd88a712a9560f777763e759b

Apply some of the suggestions made by clamg-tidy cppcoreguidelines-init-variables
Avoid (even temporary) uninitialized local variables.

I only applied a small part of all the suggestions. For example it's
common to have uninitialized local variables when calling a C-API:
   int x, y;
   SDL_GetMouseState(&x, &y);
I kept such variables uninitialized rather than dummy zero-initializing
them.

Another example:
   uint8_t buf;
   size_t num = fread(&buf, 1, 1, file);
   use(buf);
I did transform that to:
   uint8_t buf[1];
   size_t num = fread(buf, 1, 1, file);
   use(buf[0]);
Rationale: the clang-tidy pass does understand that arrays can be output
parameters, and that (in general) zero-initializing an array has a too
high cost. But also this is a good preparation to move to an API that
reads into a span (1 function parameter) rather than a pointer + size (2
function parameters).

Last example. Before:
    int value;
    if (..) {
        value = ..;
    } else if (..) {
        value = ..;
    } else {
        value = ..;
    }
after:
    const int value = [&] {
        if (..) {
            return ..;
        } else if (..) {
            return ..;
        } else {
            return ..;
        }
    }();
Bonus (optional), this style also allows to declare 'value' as a 'const'
variable.

src/serialize.hh
@@ -3,6 +3,7 @@
 
 #include "serialize_core.hh"
 #include "SerializeBuffer.hh"
+#include "StringOp.hh"
 #include "XMLElement.hh"
 #include "XMLOutputStream.hh"
 #include "MemBuffer.hh"
@@ -13,13 +14,14 @@
 #include "unreachable.hh"
 #include "zstring_view.hh"
 #include <zlib.h>
+#include <cassert>
+#include <memory>
+#include <optional>
+#include <sstream>
 #include <string>
 #include <typeindex>
 #include <type_traits>
 #include <vector>
-#include <sstream>
-#include <cassert>
-#include <memory>
 
 namespace openmsx {
 
@@ -987,8 +989,16 @@ public:
 	void attribute(const char* name, unsigned& u);
 
 	[[nodiscard]] bool hasAttribute(const char* name);
-	bool findAttribute(const char* name, unsigned& value);
 	[[nodiscard]] int countChildren() const;
+
+	template<typename T>
+	std::optional<T> findAttributeAs(const char* name)
+	{
+		if (const auto* attr = currentElement()->findAttribute(name)) {
+			return StringOp::stringTo<T>(attr->getValue());
+		}
+		return {};
+	}
 
 private:
 	XMLDocument xmlDoc{16384}; // tweak: initial allocator buffer size

src/FirmwareSwitch.cc
@@ -20,9 +20,9 @@ FirmwareSwitch::FirmwareSwitch(const DeviceConfig& config_)
 	try {
 		File file(config.getFileContext().resolveCreate(filename),
 		          File::LOAD_PERSISTENT);
-		byte bytebuf;
-		file.read(&bytebuf, 1);
-		setting.setBoolean(bytebuf != 0);
+		byte bytebuf[1];
+		file.read(bytebuf, 1);
+		setting.setBoolean(bytebuf[0] != 0);
 	} catch (FileException& e) {
 		config.getCliComm().printWarning(
 			"Couldn't load firmwareswitch status: ", e.getMessage());

src/cassette/CasImage.cc
@@ -158,13 +158,13 @@ static CasImage::Data convert(span<const uint8_t> cas, const std::string& filena
 				switch (type) {
 					case CassetteImage::ASCII:
 						writeData(wave, cas, pos);
-						bool eof;
 						do {
 							pos += CAS_HEADER.size();
 							writeSilence(wave, SHORT_SILENCE);
 							writeHeader(wave, SHORT_HEADER);
-							eof = writeData(wave, cas, pos);
-						} while (!eof && ((pos + CAS_HEADER.size()) <= cas.size()));
+							bool eof = writeData(wave, cas, pos);
+							if (eof) break;
+						} while ((pos + CAS_HEADER.size()) <= cas.size());
 						break;
 					case CassetteImage::BINARY:
 					case CassetteImage::BASIC:

src/serial/MidiInReader.cc
@@ -75,8 +75,8 @@ void MidiInReader::run()
 			break;
 		}
 #endif
-		byte buf;
-		size_t num = fread(&buf, 1, 1, file.get());
+		byte buf[1];
+		size_t num = fread(buf, 1, 1, file.get());
 		if (poller.aborted()) {
 			break;
 		}
@@ -87,7 +87,7 @@ void MidiInReader::run()
 
 		{
 			std::lock_guard<std::mutex> lock(mutex);
-			queue.push_back(buf);
+			queue.push_back(buf[0]);
 		}
 		eventDistributor.distributeEvent(
 			Event::create<MidiInReaderEvent>());

src/serial/RS232Tester.cc
@@ -91,8 +91,8 @@ void RS232Tester::run()
 			break;
 		}
 #endif
-		byte buf;
-		size_t num = fread(&buf, 1, 1, inFile.get());
+		byte buf[1];
+		size_t num = fread(buf, 1, 1, inFile.get());
 		if (poller.aborted()) {
 			break;
 		}
@@ -101,7 +101,7 @@ void RS232Tester::run()
 		}
 		assert(isPluggedIn());
 		std::lock_guard<std::mutex> lock(mutex);
-		queue.push_back(buf);
+		queue.push_back(buf[0]);
 		eventDistributor.distributeEvent(
 			Event::create<Rs232TesterEvent>());
 	}

src/serialize.cc
@@ -564,16 +564,6 @@ bool XmlInputArchive::hasAttribute(const char* name)
 {
 	return currentElement()->findAttribute(name);
 }
-bool XmlInputArchive::findAttribute(const char* name, unsigned& value)
-{
-	if (const auto* attr = currentElement()->findAttribute(name)) {
-		if (auto r = StringOp::stringTo<int>(attr->getValue())) {
-			value = *r;
-			return true;
-		}
-	}
-	return false;
-}
 int XmlInputArchive::countChildren() const
 {
 	return int(currentElement()->numChildren());

src/serialize_core.cc
@@ -37,14 +37,12 @@ unsigned loadVersionHelper(XmlInputArchive& ar, const char* className,
                            unsigned latestVersion)
 {
 	assert(ar.CAN_HAVE_OPTIONAL_ATTRIBUTES);
-	unsigned version;
-	if (!ar.findAttribute("version", version)) {
-		return 1;
+	auto version = ar.findAttributeAs<unsigned>("version");
+	if (!version) return 1;
+	if (unlikely(*version > latestVersion)) {
+		versionError(className, latestVersion, *version);
 	}
-	if (unlikely(version > latestVersion)) {
-		versionError(className, latestVersion, version);
-	}
-	return version;
+	return *version;
 }
 
 } // namespace openmsx

src/sound/ResampleHQ.cc
@@ -259,18 +259,17 @@ static void calcPermute(double ratio, int16_t* permute)
 	// initially set all as unassigned
 	std::fill_n(permute, N2, -1);
 
-	unsigned nxt1, nxt2;
 	unsigned restart = incr ? 0 : N2 - 1;
 	unsigned curr = restart;
 	// check for chain (instead of cycles)
 	if (incr) {
 		for (auto i : xrange(N2)) {
-			std::tie(nxt1, nxt2) = next(i, step);
+			auto [nxt1, nxt2] = next(i, step);
 			if ((nxt1 == i) || (nxt2 == i)) { curr = i; break; }
 		}
 	} else {
 		for (unsigned i = N2 - 1; int(i) >= 0; --i) {
-			std::tie(nxt1, nxt2) = next(i, step);
+			auto [nxt1, nxt2] = next(i, step);
 			if ((nxt1 == i) || (nxt2 == i)) { curr = i; break; }
 		}
 	}
@@ -282,7 +281,7 @@ static void calcPermute(double ratio, int16_t* permute)
 		assert(cnt < N2);
 		permute[curr] = cnt++;
 
-		std::tie(nxt1, nxt2) = next(curr, step);
+		auto [nxt1, nxt2] = next(curr, step);
 		if (permute[nxt1] == -1) {
 			curr = nxt1;
 			continue;

commit f5d1d61104869a121d7c7f55455034eb5ea56788

Fix possible crash on loadstate
Regression introduced in c9b952c98. That patch replaced
XmlInputArchive::findAttribute() with findAttributeAs<T>. But I
overlooked findAttribute() in ArchiveBase.

Thanks to 'openMSXCompile' for reporting!

src/serialize.hh
@@ -258,7 +258,8 @@ public:
 	/** Optimization: combination of hasAttribute() and getAttribute().
 	 * Returns true if hasAttribute() and (if so) also fills in the value.
 	 */
-	[[nodiscard]] bool findAttribute(const char* /*name*/, unsigned& /*value*/)
+	template<typename T>
+	[[nodiscard]] std::optional<T> findAttributeAs(const char* /*name*/)
 	{
 		UNREACHABLE; return false;
 	}

src/serialize_core.hh
@@ -662,26 +662,30 @@ template<typename TP> struct PointerLoader
 		              "must be serialized as a pointer");
 		// in XML archives we use 'id_ref' or 'id', in other archives
 		// we don't care about the name
-		unsigned id;
-		if (ar.CAN_HAVE_OPTIONAL_ATTRIBUTES &&
-		    ar.findAttribute("id_ref", id)) {
-			// nothing, 'id' already filled in
-		} else {
-			ar.attribute("id", id);
-		}
+		unsigned id = [&] {
+			if constexpr (ar.CAN_HAVE_OPTIONAL_ATTRIBUTES) {
+				if (auto i = ar.template findAttributeAs<unsigned>("id_ref")) {
+					return *i;
+				}
+			}
+			unsigned i;
+			ar.attribute("id", i);
+			return i;
+		}();
 
 		using T = typename serialize_as_pointer<TP>::type;
-		T* tp;
-		if (id == 0) {
-			tp = nullptr;
-		} else {
-			if (void* p = ar.getPointer(id)) {
-				tp = static_cast<T*>(p);
+		T* tp = [&]() -> T* {
+			if (id == 0) {
+				return nullptr;
 			} else {
-				PointerLoader2<T> loader;
-				tp = loader(ar, id, globalArgs);
+				if (void* p = ar.getPointer(id)) {
+					return static_cast<T*>(p);
+				} else {
+					PointerLoader2<T> loader;
+					return loader(ar, id, globalArgs);
+				}
 			}
-		}
+		}();
 		serialize_as_pointer<TP>::setPointer(tp2, tp, ar);
 	}
 };