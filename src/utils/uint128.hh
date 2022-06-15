a96571fe619ee208c2f0fe07a736e9eb3623e81a
m9710797 <vermaelen.wouter@gmail.com>
2022-06-15 20:09:10 +0200

    Fix bug/typo in uint128
    I overlooked this error because on x86_64,gcc/clang we use the builtin
    type '__uint128_t' rather than our own 128-bit emulation type.

--- src/utils/uint128.hh
+++ src/utils/uint128.hh
@@ -25,6 +25,8 @@ class uint128
 public:
 	constexpr uint128(uint64_t a) : lo(a), hi(0) {}
 
+	[[nodiscard]] constexpr bool operator==(const uint128&) const = default;
+
 	[[nodiscard]] constexpr bool operator!() const
 	{
 		return !(hi || lo);
@@ -221,10 +223,9 @@ private:
 
 [[nodiscard]] constexpr auto operator<=>(const uint128& a, const uint128& b)
 {
-	if (auto cmp = high64(a) <=> high(b); cmp != 0) return cmp;
+	if (auto cmp = high64(a) <=> high64(b); cmp != 0) return cmp;
 	return low64(a) <=> low64(b);
 }
-[[nodiscard]] inline constexpr bool operator==(const uint128& a, const uint128& b) = default;
 
 [[nodiscard]] constexpr bool operator&&(const uint128& a, const uint128& b)
 {
