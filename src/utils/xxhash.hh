openMSX/openMSX

commit f898607b3ef6ddb555d58aed8966182dbc42f589

Fix VDPCmdEngine::stealAccessSlot()
Follow-up on 910a58ad1a9d (sleeping on a problem often gives new
insights :-).

The version before 910a58ad1a9d was too aggressive (command too fast),
but 910a58ad1a9d makes it too conservative (command too slow). The
(more) accurate version is a hybrid between the two.

Revision 910a58ad1a9d made stealAccessSlot() _always_ steal the slot
that the command engine had reserved. But we shouldn't do that when
there's already another slot available before that (but also at least 16
cycles in the future).

src/video/VDPCmdEngine.hh
@@ -43,11 +43,15 @@ public:
 	 * Used when the CPU reads/writes VRAM.
 	 * @param time The moment in time the CPU read/write is performed.
 	 */
-	void stealAccessSlot(EmuTime::param time) {
-		if (!CMD) return;
-		if (engineTime < time) engineTime = time;
-		nextAccessSlot(VDPAccessSlots::DELTA_1); // skip one slot
-		assert(engineTime > time);
+	EmuTime stealAccessSlot(EmuTime::param time) {
+		auto cpuSlot = getNextAccessSlot(time, VDPAccessSlots::DELTA_16);
+		assert(cpuSlot > time);
+		if (CMD && engineTime <= cpuSlot) {
+			// take the next available slot
+			engineTime = getNextAccessSlot(cpuSlot, VDPAccessSlots::DELTA_1);
+			assert(engineTime > cpuSlot);
+		}
+		return cpuSlot;
 	}
 
 	/** Gets the command engine status (part of S#2).
@@ -153,13 +157,19 @@ private:
 	template<typename Mode>                 void executeHmmc(EmuTime::param limit);
 
 	// Advance to the next access slot at or past the given time.
+	inline EmuTime getNextAccessSlot(EmuTime::param time) {
+		return vdp.getAccessSlot(time, VDPAccessSlots::DELTA_0);
+	}
 	inline void nextAccessSlot(EmuTime::param time) {
-		engineTime = vdp.getAccessSlot(time, VDPAccessSlots::DELTA_0);
+		engineTime = getNextAccessSlot(time);
 	}
 	// Advance to the next access slot that is at least 'delta' cycles past
 	// the current one.
+	inline EmuTime getNextAccessSlot(EmuTime::param time, VDPAccessSlots::Delta delta) {
+		return vdp.getAccessSlot(time, delta);
+	}
 	inline void nextAccessSlot(VDPAccessSlots::Delta delta) {
-		engineTime = vdp.getAccessSlot(engineTime, delta);
+		engineTime = getNextAccessSlot(engineTime, delta);
 	}
 	inline VDPAccessSlots::Calculator getSlotCalculator(
 			EmuTime::param limit) const {

commit 0300e7e701ba0b55d55735005e75a85f34b201ec

Fix assertion when running with cmdtiming=broken
Follow-up on f898607b3ef. That commit would advance the cpu-access-time
to 16 VDP cycles in the future in the VDPCmdEngine::stealAccessSlot()
method. Though the VDP itself already took care of this (via the
VDP::scheduleCpuVramAccess() stuff).

A side effect of this fix is that the VDP commands will execute slightly
faster again during simultaneous CPU VRAM access (was a regression
introduced in f898607b3ef).

src/utils/xxhash.hh
@@ -43,6 +43,7 @@
 #define XXHASH_HH
 
 #include <bit>
+#include <cassert>
 #include <cstdint>
 #include <cstring>
 #include <string_view>

src/video/VDPCmdEngine.hh
@@ -43,15 +43,12 @@ public:
 	 * Used when the CPU reads/writes VRAM.
 	 * @param time The moment in time the CPU read/write is performed.
 	 */
-	EmuTime stealAccessSlot(EmuTime::param time) {
-		auto cpuSlot = getNextAccessSlot(time, VDPAccessSlots::DELTA_16);
-		assert(cpuSlot > time);
-		if (CMD && engineTime <= cpuSlot) {
+	void stealAccessSlot(EmuTime::param time) {
+		if (CMD && engineTime <= time) {
 			// take the next available slot
-			engineTime = getNextAccessSlot(cpuSlot, VDPAccessSlots::DELTA_1);
-			assert(engineTime > cpuSlot);
+			engineTime = getNextAccessSlot(time, VDPAccessSlots::DELTA_1);
+			assert(engineTime > time);
 		}
-		return cpuSlot;
 	}
 
 	/** Gets the command engine status (part of S#2).