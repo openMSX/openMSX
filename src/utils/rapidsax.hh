openMSX/openMSX

commit 1240f5058adfc48b42fed573c8267d50cff2e924

3a: Added zero-terminate option in rapidsax
Extend our rapidsax XML parser (based on
http://rapidxml.sourceforge.net/) to optionally zero-terminate strings.

This parser reads the XML file in memory and then modifies this memory
block in-place. For example undo XML escaping (e.g. "&lt;" -> "<"). Now
it can also (optionally) place a zero-character after the strings (tag-
and attribute-names, data and attribute-values, ...).

Without zero-termination you can use a std::string_view (basically a
pointer + length) to refer into the buffer. With zero-termination a
pointer is sufficient (but retrieving the length requires some
calculation). So this patch allows callers to make this tradeoff (and in
a later patch the refactored XMLElement class will choose for the
zero-terminate option).

src/utils/rapidsax.hh
@@ -50,6 +50,8 @@ constexpr int trimWhitespace      = 0x2;
 // Should sequences of whitespace characters be replaced with a single
 // space character?
 constexpr int normalizeWhitespace = 0x4;
+// Should strings be modified (in-place) with a zero-terminator?
+constexpr int zeroTerminateStrings = 0x8;
 
 
 // Callback handler with all empty implementations (can be used as a base
@@ -406,6 +408,9 @@ private:
 			}
 			++text;
 		}
+		if (FLAGS & zeroTerminateStrings) {
+			*text = '\0';
+		}
 		handler.comment(std::string_view(value, text - value));
 		text += 3; // skip '-->'
 	}
@@ -443,6 +448,9 @@ private:
 			}
 		}
 
+		if (FLAGS & zeroTerminateStrings) {
+			*text = '\0';
+		}
 		handler.doctype(std::string_view(value, text - value));
 		text += 1; // skip '>'
 	}
@@ -469,6 +477,10 @@ private:
 			++text;
 		}
 		// Set pi value (verbatim, no entity expansion or ws normalization)
+		if (FLAGS & zeroTerminateStrings) {
+			*nameEnd = '\0';
+			*text = '\0';
+		}
 		handler.procInstr(std::string_view(name,  nameEnd - name),
 			          std::string_view(value, text - value));
 		text += 2; // skip '?>'
@@ -513,7 +525,12 @@ private:
 
 		// Handle text, but only if non-empty.
 		auto len = end - value;
-		if (len) handler.text(std::string_view(value, len));
+		if (len) {
+			if (FLAGS & zeroTerminateStrings) {
+				*text = '\0';
+			}
+			handler.text(std::string_view(value, len));
+		}
 	}
 
 	void parseCdata(char*& text)
@@ -525,6 +542,9 @@ private:
 				throw ParseError("unexpected end of data", text);
 			}
 			++text;
+		}
+		if (FLAGS & zeroTerminateStrings) {
+			*text = '\0';
 		}
 		handler.cdata(std::string_view(value, text - value));
 		text += 3; // skip ]]>
@@ -546,9 +566,15 @@ private:
 
 		// Determine ending type
 		if (*text == '>') {
+			if (FLAGS & zeroTerminateStrings) {
+				*nameEnd = '\0';
+			}
 			++text;
 			parseNodeContents(text);
 		} else if (*text == '/') {
+			if (FLAGS & zeroTerminateStrings) {
+				*nameEnd = '\0';
+			}
 			handler.stop();
 			++text;
 			if (*text != '>') {
@@ -711,6 +737,10 @@ afterText:		// After parseText() jump here instead of continuing
 			}
 			++text; // skip quote
 
+			if (FLAGS & zeroTerminateStrings) {
+				*nameEnd = '\0';
+				*valueEnd = '\0';
+			}
 			if (!declaration) {
 				handler.attribute(std::string_view(name, nameEnd - name),
 				                  std::string_view(value, valueEnd - value));

commit de765e02e8c119e2cfbb1a7c29311a7fa531e946

Fix XML-entity replacement in machine/extension configs
There are two aspects of our XML parser that combine and triggered a
bug:

First: our 'rapidsax' XML parser can process strings in two modes:
* Pass std::string_view objects via callbacks.
* Same as above, but in addition place a 0-terminator after the string.

Many string algorithms need to know, or can work faster, if the length
of the string is known. So usually it's a good idea to store the full
string_view object. Internally string_view contains a pointer to the
actual data plus a length of this data.

Though in some cases having a more compact representation can be more
important. In that case we can only store a pointer to the start of the
string, and then have the length of the string stored indirectly by
zero-terminating the string. In other words: store it as a C-style
string.

Second: our 'rapidsax' XML parser operates in-place on the XML data.
That is: we read the file in memory, and then change this data in-place.

For example when we perform XML-entity-replacement (stuff like &lt;
or &amp;) we rewrite the strings inside their original location (luckily
these replacements only make the strings shorter, so we always have room
to transform in-place).

A second replacement is to (optionally) zero-terminate the strings. For
example in "<tag>data</tag>", zero-terminating "data" will overwrite the
'<' character of the closing tag. But that's OK, after parsing we don't
need those tags anymore.

Though there was a bug:
When zero-terminating is enabled, and when we do entity-replacement, the
zero-character was wrongly placed in the original location (e.g. the '<'
character of the closing tag), not in a possibly earlier position
because the string has become shorter.

In openMSX we only hit this bug when parsing machine/extension XML
files. Not when parsing the software database or the settings.xml file.
Because only the former use the zero-termination option.

This bug got introduced in commit 1240f5058adfc4 (and follow-ups) which

src/utils/rapidsax.hh
@@ -564,7 +564,7 @@ private:
 		auto len = end - value;
 		if (len) {
 			if (FLAGS & zeroTerminateStrings) {
-				*text = '\0';
+				*end = '\0';
 			}
 			handler.text(std::string_view(value, len));
 		}