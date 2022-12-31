openMSX/openMSX

commit d4c561dd02877825d63a39a28b70bcc760b503e4

Workaround clang-13/libc++ bug (missing feature) in std::span
See here:
  https://godbolt.org/z/ebocabzM6
It works with gcc-10, it works with clang-14/libc++, but not with
clang-13/libc++.

clang-14 is probably too new. So unfortunately we'll have to add a
workaround for now.

src/fdc/DiskManipulator.cc
@@ -151,12 +151,16 @@ void DiskManipulator::execute(std::span<const TclObject> tokens, TclObject& resu
 			throw CommandException(dir, " is not a directory");
 		}
 		auto& settings = getDriveSettings(tokens[2].getString());
-		std::span<const TclObject> lists(std::begin(tokens) + 4, std::end(tokens));
+		// Workaround clang-13/libc++ bug
+		//std::span<const TclObject> lists(std::begin(tokens) + 4, std::end(tokens));
+		std::span<const TclObject> lists(&*(std::begin(tokens) + 4), std::end(tokens) - std::begin(tokens) + 4);
 		exprt(settings, directory, lists);
 
 	} else if (subcmd == "import") {
 		auto& settings = getDriveSettings(tokens[2].getString());
-		std::span<const TclObject> lists(std::begin(tokens) + 3, std::end(tokens));
+		// Workaround clang-13/libc++ bug
+		//std::span<const TclObject> lists(std::begin(tokens) + 3, std::end(tokens));
+		std::span<const TclObject> lists(&*(std::begin(tokens) + 3), std::end(tokens) - std::begin(tokens) + 3);
 		result = import(settings, lists);
 
 	} else if (subcmd == "savedsk") {

build/main.mk
@@ -337,7 +337,8 @@ DEPEND_FLAGS:=
 ifneq ($(filter %clang++,$(CXX))$(filter clang++%,$(CXX)),)
   # Enable C++20 (partially supported since clang-8)
   COMPILE_FLAGS+=-std=c++2a -fconstexpr-steps=2000000
-  COMPILE_FLAGS+=-Wall -Wextra -Wundef -Wno-invalid-offsetof -Wunused-macros -Wdouble-promotion -Wmissing-declarations -Wshadow -Wold-style-cast -Wzero-as-null-pointer-constant
+  #COMPILE_FLAGS+=-Wall -Wextra -Wundef -Wno-invalid-offsetof -Wunused-macros -Wdouble-promotion -Wmissing-declarations -Wshadow -Wold-style-cast -Wzero-as-null-pointer-constant
+  COMPILE_FLAGS+=-Wall -Wextra -Wundef -Wno-invalid-offsetof -Wunused-macros -Wdouble-promotion -Wmissing-declarations -Wshadow -Wzero-as-null-pointer-constant
   # Hardware descriptions can contain constants that are not used in the code
   # but still useful as documentation.
   COMPILE_FLAGS+=-Wno-unused-const-variable

src/cassette/CasImage.cc
@@ -255,7 +255,9 @@ static CasImage::Data convert(std::span<const uint8_t> cas, CassetteImage::FileT
 	while (true) {
 		auto nextHeader = std::search(prevHeader, cas.end(),
 		                              header.begin(), header.end());
-		processBlock(std::span(prevHeader, nextHeader), data.wave);
+		// Workaround clang-13/libc++ bug
+		//processBlock(std::span(prevHeader, nextHeader), data.wave);
+		processBlock(std::span(&*prevHeader, nextHeader - prevHeader), data.wave);
 		if (nextHeader == cas.end()) break;
 		prevHeader = nextHeader + header.size();
 	}

src/config/HardwareConfig.cc
@@ -189,7 +189,9 @@ void HardwareConfig::testRemove() const
 	auto et = devices.end();
 	for (auto rit = devices.rbegin(), ret = devices.rend();
 	     rit != ret; ++rit) {
-		std::span alreadyRemoved{rit.base(), et};
+		// Workaround clang-13/libc++ bug
+		//std::span alreadyRemoved{rit.base(), et};
+		std::span alreadyRemoved(&*rit.base(), et - rit.base());
 		(*rit)->testRemove(alreadyRemoved);
 	}
 

commit f855853aab560af30dd53012b869721db767d9a7

Fix 'diskmanipulator import' command
Was broken after commit d4c561dd02
  Workaround clang-13/libc++ bug (missing feature) in std::span

This workaround replaced:
  std::span(b, e)       // two-iterator constructor
with
  std::span(&*b, e - b) // pointer+length constructor

This is correct. However if 'b' is itself an expression then we may need
extra parenthesis. For example with 'b = x + 4' the following two are
not the same:
  std::span(&*x + 4, e - x + 4);
  std::span(&*x + 4, e - (x + 4));

src/fdc/DiskManipulator.cc
@@ -155,14 +155,14 @@ void DiskManipulator::execute(std::span<const TclObject> tokens, TclObject& resu
 		auto& settings = getDriveSettings(tokens[2].getString());
 		// Workaround clang-13/libc++ bug
 		//std::span<const TclObject> lists(std::begin(tokens) + 4, std::end(tokens));
-		std::span<const TclObject> lists(&*(std::begin(tokens) + 4), std::end(tokens) - std::begin(tokens) + 4);
+		std::span<const TclObject> lists(&*(std::begin(tokens) + 4), std::end(tokens) - (std::begin(tokens) + 4));
 		exprt(settings, directory, lists);
 
 	} else if (subCmd == "import") {
 		auto& settings = getDriveSettings(tokens[2].getString());
 		// Workaround clang-13/libc++ bug
 		//std::span<const TclObject> lists(std::begin(tokens) + 3, std::end(tokens));
-		std::span<const TclObject> lists(&*(std::begin(tokens) + 3), std::end(tokens) - std::begin(tokens) + 3);
+		std::span<const TclObject> lists(&*(std::begin(tokens) + 3), std::end(tokens) - (std::begin(tokens) + 3));
 		result = import(settings, lists);
 
 	} else if (subCmd == "savedsk") {