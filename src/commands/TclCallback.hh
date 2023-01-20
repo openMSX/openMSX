f80721db7e293b4713d062e7ae465e232bec6dad
Pedro de Medeiros <pedro.medeiros@gmail.com>
2023-01-19 21:42:38 -0300

    TclCallback: const modifier on execute function

--- src/commands/TclCallback.hh
+++ src/commands/TclCallback.hh
@@ -22,17 +22,17 @@ public:
 	            bool useCliComm = true);
 	explicit TclCallback(StringSetting& setting);
 
-	TclObject execute();
-	TclObject execute(int arg1);
-	TclObject execute(int arg1, int arg2);
-	TclObject execute(int arg1, std::string_view arg2);
-	TclObject execute(std::string_view arg1, std::string_view arg2);
+	TclObject execute() const;
+	TclObject execute(int arg1) const;
+	TclObject execute(int arg1, int arg2) const;
+	TclObject execute(int arg1, std::string_view arg2) const;
+	TclObject execute(std::string_view arg1, std::string_view arg2) const;
 
 	[[nodiscard]] TclObject getValue() const;
 	[[nodiscard]] StringSetting& getSetting() const { return callbackSetting; }
 
 private:
-	TclObject executeCommon(TclObject& command);
+	TclObject executeCommon(TclObject& command) const;
 
 	std::optional<StringSetting> callbackSetting2;
 	StringSetting& callbackSetting;
