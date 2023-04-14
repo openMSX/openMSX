6859b19d7c1ec25dfa98670aed7fe15ab9d49e1a
m9710797 <vermaelen.wouter@gmail.com>
2023-04-14 17:16:54 +0200

    Use SDL_VERSION_ATLEAST() in #ifdef
    Not in a regular if-statement.
    
    Fixes compilation error with old SDL versions.

--- src/imgui/ImGuiMarkdown.cc
+++ src/imgui/ImGuiMarkdown.cc
@@ -30,9 +30,9 @@ ImFont* ImGuiMarkdown::get_font() const
 
 void ImGuiMarkdown::open_url() const
 {
-	if (SDL_VERSION_ATLEAST(2, 0, 14)) {
-		SDL_OpenURL(m_href.c_str());
-	}
+#if SDL_VERSION_ATLEAST(2, 0, 14)
+	SDL_OpenURL(m_href.c_str());
+#endif
 }
 
 /*bool ImGuiMarkdown::get_image(image_info& nfo) const
