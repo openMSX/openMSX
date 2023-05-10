openMSX/openMSX

commit 6faecb4e47f99a3b3affa8a339da9ad14650097f

Tweak Ram interface (and CheckedRam, SRAM, TrackedRam, Rom)
The Ram class already gives access to the full memory content via the
operator[] method. This patch extends that interface with begin()/end()
methods to make it possible to operate on this memory range via STL
algorithms.

Classes with a pair of begin()/end() methods implement the c++20 "range"
concept (that is: they can be passed to functions that require a range
as input). If they also implement a size() method then they satify the
"sized_range" concept. The Ram class already implemented a 'getSize()'
method. We renamed that to 'size()'. And to keep the code consistent, we
did the same rename in the CheckedRam, TrackedRAm, SRAM and Rom classes.

The old 'getSize()' method returned an 'unsigned' (32 bit). The renamed
'size()' method returns a 'size_t' (32 or 64 bit depending on the CPU).
Not that we'll likely need more than 32 bit for the (emulated) Ram size.
But (because of alignment/padding) storing a size_t does not actually
increase the size of the Ram object. And using 'size_t' instead of
'unsigned' in array-index operations does actually generate (a tiny bit)
better x86_64 assembly code (we don't have to explicitly truncate
calculated values to 32 bit).

src/memory/RomBlocks.cc
@@ -21,8 +21,8 @@ RomBlocks<BANK_SIZE>::RomBlocks(
 		std::bit_width(BANK_SIZE) - 1, debugBankSizeShift)
 {
 	static_assert(std::has_single_bit(BANK_SIZE), "BANK_SIZE must be a power of two");
-	auto extendedSize = (rom.getSize() + BANK_SIZE - 1) & ~(BANK_SIZE - 1);
-	if (extendedSize != rom.getSize() && alreadyWarnedForSha1Sum != rom.getOriginalSHA1()) {
+	auto extendedSize = (rom.size() + BANK_SIZE - 1) & ~(BANK_SIZE - 1);
+	if (extendedSize != rom.size() && alreadyWarnedForSha1Sum != rom.getOriginalSHA1()) {
 		config.getCliComm().printWarning(
 			"(uncompressed) ROM image filesize was not a multiple "
 			"of ", BANK_SIZE / 1024, "kB (which is required for mapper type ",
@@ -33,8 +33,8 @@ RomBlocks<BANK_SIZE>::RomBlocks(
 		alreadyWarnedForSha1Sum = rom.getOriginalSHA1();
 	}
 	rom.addPadding(extendedSize);
-	nrBlocks = rom.getSize() / BANK_SIZE;
-	assert((nrBlocks * BANK_SIZE) == rom.getSize());
+	nrBlocks = rom.size() / BANK_SIZE;
+	assert((nrBlocks * BANK_SIZE) == rom.size());
 
 	// by default no extra mappable memory block
 	extraMem = nullptr;
@@ -79,9 +79,9 @@ void RomBlocks<BANK_SIZE>::setBank(byte region, const byte* adr, int block)
 {
 	assert("address passed to setBank() is not serializable" &&
 	       ((adr == unmappedRead) ||
-	        ((&rom[0] <= adr) && (adr <= &rom[rom.getSize() - 1])) ||
+	        ((&rom[0] <= adr) && (adr <= &rom[rom.size() - 1])) ||
 	        (sram && (&(*sram)[0] <= adr) &&
-	                       (adr <= &(*sram)[sram->getSize() - 1])) ||
+	                       (adr <= &(*sram)[sram->size() - 1])) ||
 	        ((extraMem <= adr) && (adr <= &extraMem[extraSize - 1]))));
 	bankPtr[region] = adr;
 	blockNr[region] = block; // only for debuggable
@@ -125,13 +125,13 @@ void RomBlocks<BANK_SIZE>::serialize(Archive& ar, unsigned /*version*/)
 
 	if (sram) ar.serialize("sram", *sram);
 
-	unsigned offsets[NUM_BANKS];
-	unsigned romSize = rom.getSize();
-	unsigned sramSize = sram ? sram->getSize() : 0;
+	size_t offsets[NUM_BANKS];
+	auto romSize = rom.size();
+	auto sramSize = sram ? sram->size() : 0;
 	if constexpr (Archive::IS_LOADER) {
 		ar.serialize("banks", offsets);
 		for (auto i : xrange(NUM_BANKS)) {
-			if (offsets[i] == unsigned(-1)) {
+			if (offsets[i] == size_t(-1)) {
 				bankPtr[i] = unmappedRead;
 			} else if (offsets[i] < romSize) {
 				bankPtr[i] = &rom[offsets[i]];
@@ -148,16 +148,16 @@ void RomBlocks<BANK_SIZE>::serialize(Archive& ar, unsigned /*version*/)
 	} else {
 		for (auto i : xrange(NUM_BANKS)) {
 			if (bankPtr[i] == unmappedRead) {
-				offsets[i] = unsigned(-1);
+				offsets[i] = size_t(-1);
 			} else if ((&rom[0] <= bankPtr[i]) &&
 			           (bankPtr[i] <= &rom[romSize - 1])) {
-				offsets[i] = unsigned(bankPtr[i] - &rom[0]);
+				offsets[i] = size_t(bankPtr[i] - &rom[0]);
 			} else if (sram && (&(*sram)[0] <= bankPtr[i]) &&
 			           (bankPtr[i] <= &(*sram)[sramSize - 1])) {
-				offsets[i] = unsigned(bankPtr[i] - &(*sram)[0] + romSize);
+				offsets[i] = size_t(bankPtr[i] - &(*sram)[0] + romSize);
 			} else if ((extraMem <= bankPtr[i]) &&
 			           (bankPtr[i] <= &extraMem[extraSize - 1])) {
-				offsets[i] = unsigned(bankPtr[i] - extraMem + romSize + sramSize);
+				offsets[i] = size_t(bankPtr[i] - extraMem + romSize + sramSize);
 			} else {
 				UNREACHABLE;
 			}

src/MSXKanji.cc
@@ -12,8 +12,8 @@ MSXKanji::MSXKanji(const DeviceConfig& config)
 	, isLascom(config.getChildData("type", {}) == "lascom")
 	, highAddressMask(config.getChildData("type", {}) == "hangul" ? 0x7F : 0x3F)
 {
-	int size = rom.getSize();
-	if (size != one_of(0x20000, 0x40000)) {
+	auto size = rom.size();
+	if (size != one_of(0x20000u, 0x40000u)) {
 		throw MSXException("MSXKanji: wrong kanji ROM, it should be either 128kB or 256kB.");
 	}
 	if ((highAddressMask == 0x7F) && (size != 0x40000)) {
@@ -76,10 +76,10 @@ byte MSXKanji::peekIO(word port, EmuTime::param /*time*/) const
 		}
 		[[fallthrough]];
 	case 1:
-		result = rom[adr1 & (rom.getSize() - 1)]; // mask to be safe
+		result = rom[adr1 & (rom.size() - 1)]; // mask to be safe
 		break;
 	case 3:
-		if (rom.getSize() == 0x40000) { // temp workaround
+		if (rom.size() == 0x40000) { // temp workaround
 			result = rom[adr2];
 		}
 		break;

src/MSXKanji12.cc
@@ -6,14 +6,14 @@
 
 namespace openmsx {
 
-constexpr byte ID = 0xF7;
+static constexpr byte ID = 0xF7;
 
 MSXKanji12::MSXKanji12(const DeviceConfig& config)
 	: MSXDevice(config)
 	, MSXSwitchedDevice(getMotherBoard(), ID)
 	, rom(getName(), "Kanji-12 ROM", config)
 {
-	if (rom.getSize() != one_of(0x20000u, 0x40000u)) {
+	if (rom.size() != one_of(0x20000u, 0x40000u)) {
 		throw MSXException("MSXKanji12: wrong kanji ROM, it should be either 128kB or 256kB.");
 	}
 
@@ -44,7 +44,7 @@ byte MSXKanji12::peekSwitchedIO(word port, EmuTime::param /*time*/) const
 		case 1:
 			return 0x08; // TODO what is this
 		case 9:
-			if (address < rom.getSize()) {
+			if (address < rom.size()) {
 				return rom[address];
 			} else {
 				return 0xFF;

src/MSXToshibaTcx200x.cc
@@ -55,7 +55,7 @@ byte MSXToshibaTcx200x::peekMem(word address, EmuTime::param /*time*/) const
 	} else if ((0x8000 <= address) && (address < 0xC000)) {
 		if (sramEnabled()) {
 			// TODO: assuming SRAM mirrors all over page 2. Not verified!
-			return sram[address & (sram.getSize() - 1)];
+			return sram[address & (sram.size() - 1)];
 		} else {
 			return wordProcessorRom[(address - 0x8000) + getSelectedSegment() * 0x4000];
 		}
@@ -75,7 +75,7 @@ void MSXToshibaTcx200x::writeMem(word address, byte value, EmuTime::param /*time
 		controlReg = value & 0b0110'0011; // TODO which bits can be read back?
 		invalidateDeviceRWCache(0x8000, 0x4000);
 	} else if ((0x8000 <= address) && (address < 0xC000) && sramEnabled()) {
-		sram.write(address & (sram.getSize() - 1), value);
+		sram.write(address & (sram.size() - 1), value);
 	}
 }
 
@@ -87,7 +87,7 @@ const byte* MSXToshibaTcx200x::getReadCacheLine(word start) const
 		return &rs232Rom[start - 0x4000];
 	} else if ((0x8000 <= start) && (start < 0xC000)) {
 		if (sramEnabled()) {
-			return &sram[start & (sram.getSize() - 1)];
+			return &sram[start & (sram.size() - 1)];
 		} else {
 			return &wordProcessorRom[(start - 0x8000) + getSelectedSegment() * 0x4000];
 		}

src/fdc/MSXFDC.cc
@@ -15,7 +15,7 @@ MSXFDC::MSXFDC(const DeviceConfig& config, const std::string& romId, bool needRO
 		? std::optional<Rom>(std::in_place, getName() + " ROM", "rom", config, romId)
 		: std::nullopt) // e.g. Spectravideo_SVI-328 doesn't have a diskrom
 {
-	if (needROM && (rom->getSize() == 0)) {
+	if (needROM && (rom->size() == 0)) {
 		throw MSXException(
 			"Empty ROM not allowed for \"", getName(), "\".");
 	}

src/fdc/NowindInterface.cc
@@ -14,7 +14,7 @@ namespace openmsx {
 NowindInterface::NowindInterface(const DeviceConfig& config)
 	: MSXDevice(config)
 	, rom(getName() + " ROM", "rom", config)
-	, flashConfig(rom.getSize() / 0x10000, {0x10000, false})
+	, flashConfig(rom.size() / 0x10000, {0x10000, false})
 	, flash(rom, flashConfig, 0x01A4,
 	        AmdFlash::Addressing::BITS_11, config)
 	, host(drives)
@@ -108,7 +108,7 @@ void NowindInterface::writeMem(word address, byte value, EmuTime::param time)
 		host.write(value, clock.getTicksTill(time));
 	} else if (((0x6000 <= address) && (address < 0x8000)) ||
 	           ((0xA000 <= address) && (address < 0xC000))) {
-		byte max = rom.getSize() / (16 * 1024);
+		byte max = rom.size() / (16 * 1024);
 		bank = (value < max) ? value : value & (max - 1);
 		invalidateDeviceRCache(0x4000, 0x4000);
 		invalidateDeviceRCache(0xA000, 0x2000);

src/fdc/SpectravideoFDC.cc
@@ -24,7 +24,7 @@ SpectravideoFDC::SpectravideoFDC(const DeviceConfig& config)
 	: WD2793BasedFDC(config, "msxdos")
 	, cpmRom(getName() + " CP/M ROM", "rom", config, "cpm")
 {
-	if (cpmRom.getSize() != 0x1000) {
+	if (cpmRom.size() != 0x1000) {
 		throw MSXException("CP/M ROM must be exactly 4kB in size.");
 	}
 	reset(getCurrentTime());

src/fdc/TurboRFDC.cc
@@ -42,7 +42,7 @@ TurboRFDC::TurboRFDC(const DeviceConfig& config)
 	, controller(getScheduler(), reinterpret_cast<DiskDrive**>(drives),
 	             getCliComm(), getCurrentTime())
 	, romBlockDebug(*this, &bank, 0x4000, 0x4000, 14)
-	, blockMask((rom->getSize() / 0x4000) - 1)
+	, blockMask((rom->size() / 0x4000) - 1)
 	, type(parseType(config))
 {
 	reset(getCurrentTime());

commit f05c23a703c4e7fadc18592631b3980255d611e2

Fix loading old savestates (or replays)
Regression introduced in 6faecb4e47f99.

The 'RomBlocks' class (de)serializes the selected bank (as an offset in
the full ROM) for each of its pages (e.g. a 8kb block mapper has 8
pages). Some of these pages may be unmapped (e.g. a Konami mapper only
maps the region [0x4000,0xc000), but not [0x0000,0x4000) or
[0xc000,0x10000)). Such unmapped pages are represented by the special
offset-value '-1'.

Commit 6faecb4e47f99 changed the type of this offset from 'unsigned' to
'size_t' (see commit message for the motivation). Thus on 64-bit
platforms it changes from 32-bit to 64-bit. But because the type is
unsigned (as in: it has no negative values), the value '-1' is
serialized as '4294967296' (for old savestates) or
'18446744073709551616' (for new savestates). And then when loading an
old savestate, that value '4294967296' is not recognized as the special
value '-1'.

Fixed by also still recognizing '4294967296'. This should be fine
because that value should never be a legal offset value.

I was concerned that savestates created on a 64-bit system would not
load correctly on a 32-bit system (after commit 6faecb4e47f99). But that
doesn't seem to be a problem. Nevertheless we should be more careful in
the future when using 'size_t' in serialized data (or any type whose
size changes between platforms).

src/memory/RomBlocks.cc
@@ -131,7 +131,8 @@ void RomBlocks<BANK_SIZE>::serialize(Archive& ar, unsigned /*version*/)
 	if constexpr (Archive::IS_LOADER) {
 		ar.serialize("banks", offsets);
 		for (auto i : xrange(NUM_BANKS)) {
-			if (offsets[i] == size_t(-1)) {
+			if (offsets[i] == size_t(-1) ||
+			    offsets[i] == unsigned(-1)) { // for bw-compat with old savestates
 				bankPtr[i] = unmappedRead.data();
 			} else if (offsets[i] < romSize) {
 				bankPtr[i] = &rom[offsets[i]];