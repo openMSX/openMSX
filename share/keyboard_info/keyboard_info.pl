165d7142d141718148d15f2b7b75059b394abb4c
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2023-05-25 00:27:50 +0200

    Added missing Polish keyboard info file.

---
+++ share/keyboard_info/keyboard_info.pl
@@ -0,0 +1,16 @@
+UnicodeMap: pl
+
+MSX-Video-Characterset: MSXVIDPL.TXT
+
+# MSX key names in the keyboard matrix
+#------+- bit 7 ---+- bit 6 --------------+- bit 5 -------------+- bit 4 ---+- bit 3 --+- bit 2 -+- bit 1 ---+- bit 0 -+
+row  0 | 7         | 6                    | 5                   | 4         | 3        | 2       | 1         | 0       |
+row  1 | SEMICOLON | RIGHT_SQUARE_BRACKET | LEFT_SQUARE_BRACKET | BACKSLASH | EQUALS   | MINUS   | 9         | 8       |
+row  2 | B         | A                    | DEADKEY1            | SLASH     | PERIOD   | COMMA   | BACKQUOTE | QUOTE   |
+row  3 | J         | I                    | H                   | G         | F        | E       | D         | C       |
+row  4 | R         | Q                    | P                   | O         | N        | M       | L         | K       |
+row  5 | Z         | Y                    | X                   | W         | V        | U       | T         | S       |
+row  6 | F3        | F2                   | F1                  | CODE      | CAPSLOCK | GRAPH   | CTRL      | SHIFT   |
+row  7 | RETURN    | SELECT               | BACKSPACE           | STOP      | TAB      | ESCAPE  | F5        | F4      |
+row  8 | RIGHT     | DOWN                 | UP                  | LEFT      | DELETE   | INSERT  | HOME      | SPACE   |
+#------+-----------+----------------------+---------------------+-----------+----------+---------+-----------+---------+
