openMSX/openMSX

commit be64198a10efe140f4e25102ca189703cd6797e2

[2/8] remove getBpp()
Our openGL renderer always produces 32bpp output.

src/laserdisc/yuv2rgb.cc
@@ -13,6 +13,8 @@
 
 namespace openmsx::yuv2rgb {
 
+using Pixel = uint32_t;
+
 #ifdef __SSE2__
 
 /*
@@ -43,7 +45,7 @@ namespace openmsx::yuv2rgb {
 static inline void yuv2rgb_sse2(
 	const uint8_t* u_ , const uint8_t* v_,
 	const uint8_t* y0_, const uint8_t* y1_,
-	uint32_t* out0_, uint32_t* out1_)
+	Pixel* out0_, Pixel* out1_)
 {
 	// This routine calculates 32x2 RGBA pixels. Each output pixel uses a
 	// unique corresponding input Y value, but a group of 2x2 ouput pixels
@@ -234,8 +236,8 @@ static inline void convertHelperSSE2(
 		const uint8_t* pY2 = buffer[0].data + (y + 1) * y_stride;
 		const uint8_t* pCb = buffer[1].data + (y + 0) * uv_stride2;
 		const uint8_t* pCr = buffer[2].data + (y + 0) * uv_stride2;
-		auto out0 = output.getLineDirect<uint32_t>(y + 0);
-		auto out1 = output.getLineDirect<uint32_t>(y + 1);
+		auto out0 = output.getLineDirect(y + 0);
+		auto out1 = output.getLineDirect(y + 1);
 
 		for (int x = 0; x < width; x += 32) {
 			// convert a block of (32 x 2) pixels
@@ -281,7 +283,6 @@ struct Coefs {
 	return coefs;
 }
 
-template<std::unsigned_integral Pixel>
 [[nodiscard]] static inline Pixel calc(
 	const PixelFormat& format, int y, int ruv, int guv, int buv)
 {
@@ -295,7 +296,6 @@ template<std::unsigned_integral Pixel>
 	}
 }
 
-template<std::unsigned_integral Pixel>
 static void convertHelper(const th_ycbcr_buffer& buffer, RawFrame& output,
                           const PixelFormat& format)
 {
@@ -312,8 +312,8 @@ static void convertHelper(const th_ycbcr_buffer& buffer, RawFrame& output,
 		const uint8_t* pY  = buffer[0].data + y * y_stride;
 		const uint8_t* pCb = buffer[1].data + y * uv_stride2;
 		const uint8_t* pCr = buffer[2].data + y * uv_stride2;
-		auto out0 = output.getLineDirect<Pixel>(y + 0);
-		auto out1 = output.getLineDirect<Pixel>(y + 1);
+		auto out0 = output.getLineDirect(y + 0);
+		auto out1 = output.getLineDirect(y + 1);
 
 		for (int x = 0; x < width; x += 2, pY += 2, ++pCr, ++pCb) {
 			int ruv = coefs.rv[*pCr];
@@ -321,16 +321,16 @@ static void convertHelper(const th_ycbcr_buffer& buffer, RawFrame& output,
 			int buv = coefs.bu[*pCb];
 
 			int Y00 = coefs.y[pY[0]];
-			out0[x + 0] = calc<Pixel>(format, Y00, ruv, guv, buv);
+			out0[x + 0] = calc(format, Y00, ruv, guv, buv);
 
 			int Y01 = coefs.y[pY[1]];
-			out0[x + 1] = calc<Pixel>(format, Y01, ruv, guv, buv);
+			out0[x + 1] = calc(format, Y01, ruv, guv, buv);
 
 			int Y10 = coefs.y[pY[y_stride + 0]];
-			out1[x + 0] = calc<Pixel>(format, Y10, ruv, guv, buv);
+			out1[x + 0] = calc(format, Y10, ruv, guv, buv);
 
 			int Y11 = coefs.y[pY[y_stride + 1]];
-			out1[x + 1] = calc<Pixel>(format, Y11, ruv, guv, buv);
+			out1[x + 1] = calc(format, Y11, ruv, guv, buv);
 		}
 
 		output.setLineWidth(y + 0, width);
@@ -340,16 +340,16 @@ static void convertHelper(const th_ycbcr_buffer& buffer, RawFrame& output,
 
 void convert(const th_ycbcr_buffer& input, RawFrame& output)
 {
-	const PixelFormat& format = output.getPixelFormat();
-	if (format.getBytesPerPixel() == 4) {
 #ifdef __SSE2__
-		convertHelperSSE2(input, output);
+	bool sse2 = true;
 #else
-		convertHelper<uint32_t>(input, output, format);
+	bool sse2 = false;
 #endif
+
+	if (sse2) {
+		convertHelperSSE2(input, output);
 	} else {
-		assert(format.getBytesPerPixel() == 2);
-		convertHelper<uint16_t>(input, output, format);
+		convertHelper(input, output, output.getPixelFormat());
 	}
 }
 

src/video/AviRecorder.cc
@@ -75,14 +75,13 @@ void AviRecorder::start(bool recordAudio, bool recordVideo, bool recordMono,
 				"Current renderer doesn't support video recording.");
 		}
 		// any source is fine because they all have the same bpp
-		unsigned bpp = postProcessors.front()->getBpp();
 		warnedFps = false;
 		duration = EmuDuration::infinity();
 		prevTime = EmuTime::infinity();
 
 		try {
 			aviWriter = std::make_unique<AviWriter>(
-				filename, frameWidth, frameHeight, bpp,
+				filename, frameWidth, frameHeight,
 				(recordAudio && stereo) ? 2 : 1, sampleRate);
 		} catch (MSXException& e) {
 			throw CommandException("Can't start recording: ",

src/video/AviWriter.cc
@@ -21,10 +21,9 @@ namespace openmsx {
 static constexpr unsigned AVI_HEADER_SIZE = 500;
 
 AviWriter::AviWriter(const Filename& filename, unsigned width_,
-                     unsigned height_, unsigned bpp, unsigned channels_,
-                     unsigned freq_)
+                     unsigned height_, unsigned channels_, unsigned freq_)
 	: file(filename, "wb")
-	, codec(width_, height_, bpp)
+	, codec(width_, height_)
 	, width(width_)
 	, height(height_)
 	, channels(channels_)

src/video/AviWriter.hh
@@ -19,7 +19,7 @@ class AviWriter
 {
 public:
 	AviWriter(const Filename& filename, unsigned width, unsigned height,
-	          unsigned bpp, unsigned channels, unsigned freq);
+	          unsigned channels, unsigned freq);
 	~AviWriter();
 	void addFrame(FrameSource* video, std::span<const int16_t> audio);
 	void setFps(float fps_) { fps = fps_; }

src/video/Deflicker.cc
@@ -33,10 +33,7 @@ std::unique_ptr<Deflicker> Deflicker::create(
 	const PixelFormat& format,
 	std::span<std::unique_ptr<RawFrame>, 4> lastFrames)
 {
-	if (format.getBytesPerPixel() == 4) {
-		return std::make_unique<DeflickerImpl<uint32_t>>(format, lastFrames);
-	}
-	UNREACHABLE; return nullptr; // avoid warning
+	return std::make_unique<DeflickerImpl<uint32_t>>(format, lastFrames);
 }
 
 
@@ -121,10 +118,10 @@ const void* DeflickerImpl<Pixel>::getLineInfo(
 	unsigned width1 = lastFrames[1]->getLineWidthDirect(line);
 	unsigned width2 = lastFrames[2]->getLineWidthDirect(line);
 	unsigned width3 = lastFrames[3]->getLineWidthDirect(line);
-	const Pixel* line0 = lastFrames[0]->template getLineDirect<Pixel>(line).data();
-	const Pixel* line1 = lastFrames[1]->template getLineDirect<Pixel>(line).data();
-	const Pixel* line2 = lastFrames[2]->template getLineDirect<Pixel>(line).data();
-	const Pixel* line3 = lastFrames[3]->template getLineDirect<Pixel>(line).data();
+	const Pixel* line0 = lastFrames[0]->getLineDirect(line).data();
+	const Pixel* line1 = lastFrames[1]->getLineDirect(line).data();
+	const Pixel* line2 = lastFrames[2]->getLineDirect(line).data();
+	const Pixel* line3 = lastFrames[3]->getLineDirect(line).data();
 	if ((width0 != width3) || (width0 != width2) || (width0 != width1)) {
 		// Not all the same width.
 		width = width0;

src/video/GLPostProcessor.cc
@@ -290,7 +290,7 @@ void GLPostProcessor::uploadFrame()
 			h,                 // height
 			GL_RGBA,           // format
 			GL_UNSIGNED_BYTE,  // type
-			const_cast<RawFrame*>(superImposeVideoFrame)->getLineDirect<unsigned>(0).data()); // data
+			const_cast<RawFrame*>(superImposeVideoFrame)->getLineDirect(0).data()); // data
 	}
 }
 

src/video/OutputSurface.cc
@@ -24,7 +24,6 @@ void OutputSurface::calculateViewPort(gl::ivec2 logSize_, gl::ivec2 physSize_)
 void OutputSurface::setOpenGlPixelFormat()
 {
 	setPixelFormat(PixelFormat(
-		32,
 		Endian::BIG ? 0xFF000000 : 0x000000FF, Endian::BIG ? 24 :  0, 0,
 		Endian::BIG ? 0x00FF0000 : 0x0000FF00, Endian::BIG ? 16 :  8, 0,
 		Endian::BIG ? 0x0000FF00 : 0x00FF0000, Endian::BIG ?  8 : 16, 0,

src/video/PNG.cc
@@ -381,12 +381,13 @@ void save(size_t width, std::span<const void*> rowPointers,
 {
 	// this implementation creates 1 extra copy, can be optimized if required
 	auto height = narrow<unsigned>(rowPointers.size());
+	static constexpr int bpp = 32;
 	SDLSurfacePtr surface(
-		narrow<unsigned>(width), height, format.getBpp(),
+		narrow<unsigned>(width), height, bpp,
 		format.getRmask(), format.getGmask(), format.getBmask(), format.getAmask());
 	for (auto y : xrange(height)) {
 		memcpy(surface.getLinePtr(y),
-		       rowPointers[y], width * format.getBytesPerPixel());
+		       rowPointers[y], width * sizeof(uint32_t));
 	}
 	save(surface.get(), filename);
 }

commit 638e22274ec2a0378ff2d8f3f9973d4a61a1b54b

Fix compile error on non-SSE2 architectures
Error introduced in be64198a10.

src/laserdisc/yuv2rgb.cc
@@ -335,16 +335,10 @@ static void convertHelper(const th_ycbcr_buffer& buffer, RawFrame& output)
 void convert(const th_ycbcr_buffer& input, RawFrame& output)
 {
 #ifdef __SSE2__
-	bool sse2 = true;
-#else
-	bool sse2 = false;
-#endif
-
-	if (sse2) {
 		convertHelperSSE2(input, output);
-	} else {
+		return;
+#endif
 		convertHelper(input, output);
-	}
 }
 
 } // namespace openmsx::yuv2rgb