87d9800db2eda4472284b8db14c8eb77b1b869ef
m9710797 <vermaelen.wouter@gmail.com>
2023-06-30 18:03:18 +0200

    Alternative implementation for the 'Connectors' menu
    The main difference is:
    * old version had pluggable-combobox followed by connector-name-label
    * new version has connector-name-label followed by pluggable-combobox
    
    Quibus thought the new order is more logical. Personally I don't have a
    strong opinion either way. It's like the difference between:
    * The mouse is plugged in joystick-port-A.
    * Joystick-port-a contains the mouse.
    
    Not looking at the semantics, purely at the technical implementation:
    * The order combobox followed by label is the default in "Dear ImGui",
      so it's easier to implement.
    * In fact the only(?) way (that I know of) to implement the order 'label
      followed by combobox (and have the boxes on the different rows all
      aligned) is via a 2-column table. And then hide the label on the
      combobox. This is a bit more work, but not excessively (30 vs 38 lines
      of code). (IMHO only 38 lines for the whole connector menu is still
      impressive).
    
    When there's a large difference between the longest and the shortest
    label:
    * In the default Dear-ImGui-order the labels are 'connected' to the
      combobox. The empty space is on the side of the window.
    * In the reverse order: shorter labels are 'disconnected' from the
      combobox, and there's an empty space in the middle of the window.
    
    So I can understand why Dear ImGui has chosen for this order. I can also
    understand why Quibus thinks the reverse order is more logical (even
    though personally I think both orders are fine).
    
    (For now) I kept both versions in the source code (switchable via #if).
    Try it out yourself, and let us know which you like best.

--- src/imgui/ImGuiConnector.cc
+++ src/imgui/ImGuiConnector.cc
@@ -13,6 +13,10 @@
 
 namespace openmsx {
 
+// Kept both implementations to be able to compare them more easily.
+// In the end we'll only keep one of these two.
+#if 0
+
 void ImGuiConnector::showMenu(MSXMotherBoard* motherBoard)
 {
 	im::Menu("Connectors", motherBoard != nullptr, [&]{
@@ -39,8 +43,52 @@ void ImGuiConnector::showMenu(MSXMotherBoard* motherBoard)
 					simpleToolTip(plug->getDescription());
 				}
 			});
+			simpleToolTip(connector->getDescription());
 		}
 	});
 }
+
+#else
+
+void ImGuiConnector::showMenu(MSXMotherBoard* motherBoard)
+{
+	im::Menu("Connectors", motherBoard != nullptr, [&]{
+		im::Table("table", 2, [&]{
+			const auto& pluggingController = motherBoard->getPluggingController();
+			const auto& pluggables = pluggingController.getPluggables();
+			for (auto* connector : pluggingController.getConnectors()) {
+				const auto& connectorName = connector->getName();
+				if (ImGui::TableNextColumn()) { // connector
+					ImGui::TextUnformatted(connectorName);
+					simpleToolTip(connector->getDescription());
+				}
+				if (ImGui::TableNextColumn()) { // pluggable
+					auto connectorClass = connector->getClass();
+					const auto& currentPluggable = connector->getPlugged();
+					ImGui::SetNextItemWidth(150.0f);
+					im::Combo(tmpStrCat("##", connectorName).c_str(), std::string(currentPluggable.getName()).c_str(), [&]{
+						if (!currentPluggable.getName().empty()) {
+							if (ImGui::Selectable("[unplug]")) {
+								manager.executeDelayed(makeTclList("unplug", connectorName));
+							}
+						}
+						for (auto& plug : pluggables) {
+							if (plug->getClass() != connectorClass) continue;
+							auto plugName = std::string(plug->getName());
+							bool selected = plug.get() == &currentPluggable;
+							int flags = !selected && plug->getConnector() ? ImGuiSelectableFlags_Disabled : 0; // plugged in another connector
+							if (ImGui::Selectable(plugName.c_str(), selected, flags)) {
+								manager.executeDelayed(makeTclList("plug", connectorName, plugName));
+							}
+							simpleToolTip(plug->getDescription());
+						}
+					});
+				}
+			}
+		});
+	});
+}
+
+#endif
 
 } // namespace openmsx
