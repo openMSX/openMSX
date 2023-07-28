openMSX/openMSX

commit 96e43d900aa9042226cecef3f67acb2daf9de38a

V9990: warns about writes (reads) to read-only (write-only) registers

src/video/v9990/V9990.cc
@@ -63,6 +63,16 @@ V9990::V9990(const DeviceConfig& config)
 	, v9990PalDebug(*this)
 	, irq(getMotherBoard(), getName() + ".IRQ")
 	, display(getReactor().getDisplay())
+	, invalidRegisterReadCallback(getCommandController(),
+		"v9990_invalid_register_read_callback",
+		"Tcl proc to call when a write-only register was read from. "
+		"Input: register number (0-63)",
+                {}, Setting::SAVE)
+	, invalidRegisterWriteCallback(getCommandController(),
+		"v9990_invalid_register_write_callback",
+		"Tcl proc to call when a read-only register was written to. "
+		"Input: register number (0-63), 8-bit data",
+		{}, Setting::SAVE)
 	, vram(*this, getCurrentTime())
 	, cmdEngine(*this, getCurrentTime(), display.getRenderSettings())
 	, frameStartTime(getCurrentTime())
@@ -543,6 +553,7 @@ byte V9990::readRegister(byte reg, EmuTime::param time) const
 			       : narrow_cast<byte>(borderX >> 8);
 		}
 	} else {
+		invalidRegisterReadCallback.execute(reg);
 		return 0xFF;
 	}
 }
@@ -569,6 +580,7 @@ void V9990::writeRegister(byte reg, byte val, EmuTime::param time)
 	assert(reg < 64);
 	if (!(regAccess[reg] & ALLOW_WRITE)) {
 		// register not writable
+		invalidRegisterWriteCallback.execute(reg, val);
 		return;
 	}
 	if (reg >= CMD_PARAM_SRC_ADDRESS_0) {

share/scripts/callbackprocs.tcl
@@ -107,6 +107,14 @@ proc sensorkidacquirecallback {port} {
 set sensor_kid_port_status_callback sensorkidportstatuscallback
 set sensor_kid_acquire_callback     sensorkidacquirecallback
 
+proc default_V9990_invalid_register_read_callback {reg} {
+	message [format "Reading from V9990 write-only register 0x%x" $reg] warning
+}
+proc default_V9990_invalid_register_write_callback {reg value} {
+	message [format "Writing 0x%x to V9990 read-only register %i" $value $reg] warning
+}
+#set v9990_invalid_register_read_callback  default_V9990_invalid_register_read_callback
+#set v9990_invalid_register_write_callback default_V9990_invalid_register_write_callback
 
 # show message (also) as OSD message
 interp alias {} default_message_callback {} osd::display_message

src/video/v9990/V9990.hh
@@ -3,6 +3,7 @@
 
 #include "MSXDevice.hh"
 #include "Schedulable.hh"
+#include "TclCallback.hh"
 #include "VideoSystemChangeListener.hh"
 #include "IRQHelper.hh"
 #include "V9990CmdEngine.hh"
@@ -528,6 +529,9 @@ private:
 	IRQHelper irq;
 
 	Display& display;
+
+	TclCallback   invalidRegisterReadCallback;
+	TclCallback   invalidRegisterWriteCallback;
 
 	/** VRAM
 	  */

commit 12ea52f047473d30cb66e253e797af480c91d91c

Fixed crash when using 2 V9990 devices at once
This can (unintentionally) happen when e.g. using the Boosted_MSX2_EN
machine, which has a Video9000 builtin, in combination with the '-ext
video9000' command line option.

The problem were these 2 callbacks (introduced in 96e43d900a):
  v9990_invalid_register_read_callback
  v9990_invalid_register_write_callback
With two V9990 instances, this now creates two (pairs of) settings with
the same name. In an assert build this immediately triggers an
assertion. (In a non-assert build it appears to work fine, but no
guarantees).

Fixed by changing the name to
  <device-name>_invalid_register_read_callback
  <device-name>_invalid_register_write_callback
That's not backwards compatible, but this feature is only used rarely.
This should be fine.

src/video/v9990/V9990.cc
@@ -64,12 +64,12 @@ V9990::V9990(const DeviceConfig& config)
 	, irq(getMotherBoard(), getName() + ".IRQ")
 	, display(getReactor().getDisplay())
 	, invalidRegisterReadCallback(getCommandController(),
-		"v9990_invalid_register_read_callback",
+		tmpStrCat(getName(), "_invalid_register_read_callback"),
 		"Tcl proc to call when a write-only register was read from. "
 		"Input: register number (0-63)",
                 {}, Setting::SAVE)
 	, invalidRegisterWriteCallback(getCommandController(),
-		"v9990_invalid_register_write_callback",
+		tmpStrCat(getName(), "_invalid_register_write_callback"),
 		"Tcl proc to call when a read-only register was written to. "
 		"Input: register number (0-63), 8-bit data",
 		{}, Setting::SAVE)