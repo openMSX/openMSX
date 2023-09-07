494ad3536953d99e3e205488efaa3e6e507976c7
m9710797 <vermaelen.wouter@gmail.com>
2023-09-07 18:07:53 +0200

    Fix crash in glDeleteTextures() on macOS
    See comments in code for more details.

--- src/video/GLUtil.cc
+++ src/video/GLUtil.cc
@@ -40,8 +40,19 @@ void Texture::allocate()
 
 void Texture::reset()
 {
-	glDeleteTextures(1, &textureId); // ok to delete 0-texture
-	textureId = 0;
+	// Calling glDeleteTextures with a 0-texture-id is OK, it doesn't do
+	// anything. So from that point of view we don't need this test. However
+	// during initialization, before the openGL context is created, we
+	// already create some Texture(null) objects. These can also be moved
+	// around (std::move()), and then when the moved-from object gets
+	// deleted that also calls this method. That should be fine as calling
+	// glDeleteTextures() with 0 does nothing. EXCEPT that on macOS it does
+	// crash when calling an openGL function before an openGL context is
+	// created (it works fine on Linux and Windows).
+	if (textureId) {
+		glDeleteTextures(1, &textureId);
+		textureId = 0;
+	}
 }
 
 void Texture::setInterpolation(bool interpolation)
@@ -205,8 +216,11 @@ void ShaderProgram::allocate()
 
 void ShaderProgram::reset()
 {
-	glDeleteProgram(handle); // ok to delete '0'
-	handle = 0;
+	// ok to delete '0', but see comment in Texture::reset()
+	if (handle) {
+		glDeleteProgram(handle);
+		handle = 0;
+	}
 }
 
 bool ShaderProgram::isOK() const
