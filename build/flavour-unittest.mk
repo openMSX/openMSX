ad4ca6781e14d054521a0110448385178a6cf6f6
John Helmert III <ajak@gentoo.org>
2023-11-21 10:57:01 -0800

    build/flavour-unittest: space between CXXFLAGS assignment operator
    Without this, GNU Make (at least) seems to treat the variable as
    `CXXFLAGS+` as the variable when CXXFLAGS is already defined,
    resulting in the unittest CXXFLAGS being effectively ignored rather
    than appending to the already existing CXXFLAGS.
    
    Signed-off-by: John Helmert III <ajak@gentoo.org>

--- build/flavour-unittest.mk
+++ build/flavour-unittest.mk
@@ -2,7 +2,7 @@
 # Build executable that runs unit tests.
 
 # Debug flags.
-CXXFLAGS+=-O3 -g -DUNITTEST -IContrib/catch2 -fsanitize=address
+CXXFLAGS += -O3 -g -DUNITTEST -IContrib/catch2 -fsanitize=address
 
 # Strip executable?
 OPENMSX_STRIP:=false
