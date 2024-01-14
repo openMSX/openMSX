88bda826f5aca22729e992589846016e1942c543
m9710797 <vermaelen.wouter@gmail.com>
2024-01-14 21:20:34 +0100

    [5/n] rewrite data-preview printing code
    Simplify it by reusing existing openMSX helper functions to print dec/hex/bin.
    This also allows to remove uses of snprintf (via a macro to make it
    portable to windows), and that allows to remove some warning-suppression
    code.

--- src/imgui/DebuggableEditor.hh
+++ src/imgui/DebuggableEditor.hh
@@ -15,28 +15,27 @@
 #ifndef DEBUGGABLE_EDITOR_HH
 #define DEBUGGABLE_EDITOR_HH
 
+#include "ImGuiUtils.hh"
+
 #include "unreachable.hh"
 
 #include <imgui.h>
 
 #include <algorithm>
 #include <array>
+#include <bit>
 #include <cassert>
 #include <cstdint>
 #include <cstdio>
+#include <span>
 
 #ifdef _MSC_VER
 #define _PRISizeT   "I"
-#define ImSnprintf  _snprintf
 #else
 #define _PRISizeT   "z"
-#define ImSnprintf  snprintf
 #endif
 
-#ifdef _MSC_VER
-#pragma warning (push)
-#pragma warning (disable: 4996) // warning C4996: 'sprintf': This function or variable may be unsafe.
-#endif
+using namespace std::literals; // TODO not in .hh
 
 struct MemoryEditor
 {
@@ -200,8 +199,6 @@ struct MemoryEditor
 
 		const char* format_address = "%0*" _PRISizeT "X: ";
 		const char* format_data = "%0*" _PRISizeT "X";
-		const char* format_byte = "%02X";
-		const char* format_byte_space = "%02X ";
 
 		while (clipper.Step()) {
 			for (int line_i = clipper.DisplayStart; line_i < clipper.DisplayEnd; ++line_i) {
@@ -237,7 +234,7 @@ struct MemoryEditor
 						if (DataEditingTakeFocus) {
 							ImGui::SetKeyboardFocusHere(0);
 							sprintf(AddrInputBuf.data(), format_data, s.AddrDigitsCount, addr);
-							sprintf(DataInputBuf.data(), format_byte, ReadFn ? ReadFn(mem_data, addr) : mem_data[addr]);
+							sprintf(DataInputBuf.data(), "%02X", ReadFn ? ReadFn(mem_data, addr) : mem_data[addr]);
 						}
 						struct UserData {
 							// FIXME: We should have a way to retrieve the text edit cursor position more easily in the API, this is rather tedious. This is such a ugly mess we may be better off not using InputText() at all here.
@@ -261,7 +258,7 @@ struct MemoryEditor
 							int CursorPos = -1; // Output
 						};
 						UserData user_data;
-						sprintf(user_data.CurrentBufOverwrite.data(), format_byte, ReadFn ? ReadFn(mem_data, addr) : mem_data[addr]);
+						sprintf(user_data.CurrentBufOverwrite.data(), "%02X", ReadFn ? ReadFn(mem_data, addr) : mem_data[addr]);
 						ImGuiInputTextFlags flags = ImGuiInputTextFlags_CharsHexadecimal
 						                          | ImGuiInputTextFlags_EnterReturnsTrue
 						                          | ImGuiInputTextFlags_AutoSelectAll
@@ -297,7 +294,7 @@ struct MemoryEditor
 						if (b == 0 && OptGreyOutZeroes) {
 							ImGui::TextDisabled("00 ");
 						} else {
-							ImGui::Text(format_byte_space, b);
+							ImGui::Text("%02X ", b);
 						}
 						if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(0)) {
 							DataEditingTakeFocus = true;
@@ -421,207 +418,130 @@ struct MemoryEditor
 		ImGui::SetNextItemWidth((s.GlyphWidth * 6.0f) + style.FramePadding.x * 2.0f + style.ItemInnerSpacing.x);
 		ImGui::Combo("##combo_endianess", &PreviewEndianess, "LE\0BE\0\0");
 
-		std::array<char, 128> buf;
-		float x = 6.0f * s.GlyphWidth;
-		bool has_value = DataPreviewAddr != size_t(-1);
+		if (DataPreviewAddr != size_t(-1)) {
+			std::array<uint8_t, 8> dataBuf = {};
+			size_t elem_size = DataTypeGetSize(PreviewDataType);
+			size_t size = DataPreviewAddr + elem_size > mem_size ? mem_size - DataPreviewAddr : elem_size;
+			if (ReadFn) {
+				for (int i = 0, n = (int)size; i < n; ++i) {
+					dataBuf[i] = ReadFn(mem_data, DataPreviewAddr + i);
+				}
+			} else {
+				memcpy(dataBuf.data(), mem_data + DataPreviewAddr, size);
+			}
 
-		if (has_value) {
-			DrawPreviewData(DataPreviewAddr, mem_data, mem_size, PreviewDataType, DataFormat_Dec, buf.data(), buf.size());
-		}
-		ImGui::Text("Dec");
-		ImGui::SameLine(x);
-		ImGui::TextUnformatted(has_value ? buf.data() : "N/A");
+			static constexpr bool native_is_little = std::endian::native == std::endian::little;
+			bool preview_is_little = PreviewEndianess == 0;
+			if (native_is_little != preview_is_little) {
+				std::reverse(dataBuf.begin(), dataBuf.begin() + elem_size);
+			}
 
-		if (has_value) {
-			DrawPreviewData(DataPreviewAddr, mem_data, mem_size, PreviewDataType, DataFormat_Hex, buf.data(), buf.size());
-		}
-		ImGui::Text("Hex");
-		ImGui::SameLine(x);
-		ImGui::TextUnformatted(has_value ? buf.data() : "N/A");
+			ImGui::TextUnformatted("Dec "sv);
+			ImGui::SameLine();
+			formatDec(dataBuf, PreviewDataType);
 
-		if (has_value) {
-			DrawPreviewData(DataPreviewAddr, mem_data, mem_size, PreviewDataType, DataFormat_Bin, buf.data(), buf.size());
+			ImGui::TextUnformatted("Hex "sv);
+			ImGui::SameLine();
+			formatHex(dataBuf, PreviewDataType);
+
+			ImGui::TextUnformatted("Bin "sv);
+			ImGui::SameLine();
+			formatBin(subspan(dataBuf, 0, elem_size));
+		} else {
+			ImGui::TextUnformatted("Dec  N/A"sv);
+			ImGui::TextUnformatted("Hex  N/A"sv);
+			ImGui::TextUnformatted("Bin  N/A"sv);
 		}
-		buf[buf.size() - 1] = 0;
-		ImGui::Text("Bin");
-		ImGui::SameLine(x);
-		ImGui::TextUnformatted(has_value ? buf.data() : "N/A");
 	}
 
-	// Utilities for Data Preview
-	[[nodiscard]] const char* DataTypeGetDesc(ImGuiDataType data_type) const
+	[[nodiscard]] static const char* DataTypeGetDesc(ImGuiDataType data_type)
 	{
 		std::array<const char*, ImGuiDataType_COUNT - 2> desc = { "Int8", "Uint8", "Int16", "Uint16", "Int32", "Uint32", "Int64", "Uint64" };
 		assert(data_type >= 0 && data_type < (ImGuiDataType_COUNT - 2));
 		return desc[data_type];
 	}
 
-	[[nodiscard]] size_t DataTypeGetSize(ImGuiDataType data_type) const
+	[[nodiscard]] static size_t DataTypeGetSize(ImGuiDataType data_type)
 	{
 		std::array<size_t, ImGuiDataType_COUNT - 2> sizes = { 1, 1, 2, 2, 4, 4, 8, 8 };
 		assert(data_type >= 0 && data_type < (ImGuiDataType_COUNT - 2));
 		return sizes[data_type];
 	}
 
-	[[nodiscard]] const char* DataFormatGetDesc(DataFormat data_format) const
+	template<typename T>
+	[[nodiscard]] static T read(std::span<const uint8_t> buf)
 	{
-		std::array<const char*, DataFormat_COUNT> desc = { "Bin", "Dec", "Hex" };
-		assert(data_format >= 0 && data_format < DataFormat_COUNT);
-		return desc[data_format];
+		assert(buf.size() >= sizeof(T));
+		T t = 0;
+		memcpy(&t, buf.data(), sizeof(T));
+		return t;
 	}
 
-	[[nodiscard]] bool IsBigEndian() const
+	static void formatDec(std::span<const uint8_t> buf, ImGuiDataType data_type)
 	{
-		uint16_t x = 1;
-		char c[2];
-		memcpy(c, &x, 2);
-		return c[0] != 0;
-	}
-
-	static void* EndianessCopyBigEndian(void* _dst, void* _src, size_t s, int is_little_endian)
-	{
-		if (is_little_endian) {
-			auto* dst = static_cast<uint8_t*>(_dst);
-			auto* src = static_cast<uint8_t*>(_src) + s - 1;
-			for (int i = 0, n = (int)s; i < n; ++i) {
-			    memcpy(dst++, src--, 1);
-			}
-			return _dst;
-		} else {
-			return memcpy(_dst, _src, s);
-		}
-	}
-
-	static void* EndianessCopyLittleEndian(void* _dst, void* _src, size_t s, int is_little_endian)
-	{
-		if (is_little_endian) {
-			return memcpy(_dst, _src, s);
-		} else {
-			auto* dst = static_cast<uint8_t*>(_dst);
-			auto* src = static_cast<uint8_t*>(_src) + s - 1;
-			for (int i = 0, n = (int)s; i < n; ++i) {
-				memcpy(dst++, src--, 1);
-			}
-			return _dst;
-		}
-	}
-
-	void* EndianessCopy(void* dst, void* src, size_t size) const
-	{
-		static void* (*fp)(void*, void*, size_t, int) = nullptr;
-		if (!fp) {
-			fp = IsBigEndian() ? EndianessCopyBigEndian : EndianessCopyLittleEndian;
-		}
-		return fp(dst, src, size, PreviewEndianess);
-	}
-
-	const char* FormatBinary(const uint8_t* buf, int width) const
-	{
-		assert(width <= 64);
-		size_t out_n = 0;
-		static std::array<char, 64 + 8 + 1> out_buf;
-		int n = width / 8;
-		for (int j = n - 1; j >= 0; --j) {
-			for (int i = 0; i < 8; ++i) {
-				out_buf[out_n++] = (buf[j] & (1 << (7 - i))) ? '1' : '0';
-			}
-			out_buf[out_n++] = ' ';
-		}
-		assert(out_n < out_buf.size());
-		out_buf[out_n] = 0;
-		return out_buf.data();
-	}
-
-	// [Internal]
-	void DrawPreviewData(size_t addr, const uint8_t* mem_data, size_t mem_size, ImGuiDataType data_type, DataFormat data_format, char* out_buf, size_t out_buf_size) const
-	{
-		std::array<uint8_t, 8> buf;
-		size_t elem_size = DataTypeGetSize(data_type);
-		size_t size = addr + elem_size > mem_size ? mem_size - addr : elem_size;
-		if (ReadFn) {
-			for (int i = 0, n = (int)size; i < n; ++i) {
-				buf[i] = ReadFn(mem_data, addr + i);
-			}
-		} else {
-			memcpy(buf.data(), mem_data + addr, size);
-		}
-
-		if (data_format == DataFormat_Bin) {
-			std::array<uint8_t, 8> binBuf;
-			EndianessCopy(binBuf.data(), buf.data(), size);
-			ImSnprintf(out_buf, out_buf_size, "%s", FormatBinary(binBuf.data(), (int)size * 8));
-			return;
-		}
-
-		out_buf[0] = 0;
 		switch (data_type) {
-		case ImGuiDataType_S8: {
-			int8_t int8 = 0;
-			EndianessCopy(&int8, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%hhd", int8); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%02x", int8 & 0xFF); return; }
+		case ImGuiDataType_S8:
+			ImGui::StrCat(read<int8_t>(buf));
 			break;
-		}
-		case ImGuiDataType_U8: {
-			uint8_t uint8 = 0;
-			EndianessCopy(&uint8, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%hhu", uint8); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%02x", uint8 & 0XFF); return; }
+		case ImGuiDataType_U8:
+			ImGui::StrCat(read<uint8_t>(buf));
 			break;
-		}
-		case ImGuiDataType_S16: {
-			int16_t int16 = 0;
-			EndianessCopy(&int16, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%hd", int16); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%04x", int16 & 0xFFFF); return; }
+		case ImGuiDataType_S16:
+			ImGui::StrCat(read<int16_t>(buf));
 			break;
-		}
-		case ImGuiDataType_U16: {
-			uint16_t uint16 = 0;
-			EndianessCopy(&uint16, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%hu", uint16); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%04x", uint16 & 0xFFFF); return; }
+		case ImGuiDataType_U16:
+			ImGui::StrCat(read<uint16_t>(buf));
 			break;
-		}
-		case ImGuiDataType_S32: {
-			int32_t int32 = 0;
-			EndianessCopy(&int32, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%d", int32); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%08x", int32); return; }
+		case ImGuiDataType_S32:
+			ImGui::StrCat(read<int32_t>(buf));
 			break;
-		}
-		case ImGuiDataType_U32: {
-			uint32_t uint32 = 0;
-			EndianessCopy(&uint32, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%u", uint32); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%08x", uint32); return; }
+		case ImGuiDataType_U32:
+			ImGui::StrCat(read<uint32_t>(buf));
 			break;
-		}
-		case ImGuiDataType_S64: {
-			int64_t int64 = 0;
-			EndianessCopy(&int64, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%lld", (long long)int64); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%016llx", (long long)int64); return; }
+		case ImGuiDataType_S64:
+			ImGui::StrCat(read<int64_t>(buf));
 			break;
-		}
-		case ImGuiDataType_U64: {
-			uint64_t uint64 = 0;
-			EndianessCopy(&uint64, buf.data(), size);
-			if (data_format == DataFormat_Dec) { ImSnprintf(out_buf, out_buf_size, "%llu", (long long)uint64); return; }
-			if (data_format == DataFormat_Hex) { ImSnprintf(out_buf, out_buf_size, "0x%016llx", (long long)uint64); return; }
+		case ImGuiDataType_U64:
+			ImGui::StrCat(read<uint64_t>(buf));
 			break;
-		}
 		default:
 			UNREACHABLE;
+		}
+	}
+
+	static void formatHex(std::span<const uint8_t> buf, ImGuiDataType data_type)
+	{
+		switch (data_type) {
+		case ImGuiDataType_S8:
+		case ImGuiDataType_U8:
+			ImGui::StrCat(hex_string<2>(read<uint8_t>(buf)));
+			break;
+		case ImGuiDataType_S16:
+		case ImGuiDataType_U16:
+			ImGui::StrCat(hex_string<4>(read<uint16_t>(buf)));
+			break;
+		case ImGuiDataType_S32:
+		case ImGuiDataType_U32:
+			ImGui::StrCat(hex_string<8>(read<uint32_t>(buf)));
+			break;
+		case ImGuiDataType_S64:
+		case ImGuiDataType_U64:
+			ImGui::StrCat(hex_string<16>(read<uint64_t>(buf)));
+			break;
+		default:
+			UNREACHABLE;
+		}
+	}
+
+	static void formatBin(std::span<const uint8_t> buf)
+	{
+		for (int i = int(buf.size()) - 1; i >= 0; --i) {
+			ImGui::StrCat(bin_string<8>(buf[i]));
+			if (i != 0) ImGui::SameLine();
 		}
 	}
 };
 
 #undef _PRISizeT
-#undef ImSnprintf
-
-#ifdef _MSC_VER
-#pragma warning (pop)
-#endif
 
 #endif
