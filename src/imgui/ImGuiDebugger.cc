openMSX/openMSX

commit 338a44154f26430cd48d53607e317331072feef6

Fix clang warning: -Wimplicit-int-conversion
Most of these warnings are harmless, and are only there because of c/c++
integer promotion rules, but the value can not actually get out of
range.

In a few places I added extra input-string validation (for stuff like
"button1000" but we 'only' support 256 joystick buttons).

After this the clang build is warning free again. Except for some
warnings in the the src/3rdpart subdirectory. Ideally we only compile
3rdparty files with standard warning settings, or without any warnings.
But unfortunately that's not supported in our homegrown build system.

src/imgui/ImGuiDebugger.cc
@@ -277,7 +277,7 @@ struct CurrentSlot {
 		const auto* device = cpuInterface.getVisibleMSXDevice(page);
 		Debuggable* romBlocks = nullptr;
 		if (const auto* mapper = dynamic_cast<const MSXMemoryMapperBase*>(device)) {
-			result.seg = mapper->getSelectedSegment(page);
+			result.seg = mapper->getSelectedSegment(narrow<uint8_t>(page));
 		} else if (!dynamic_cast<const RomPlain*>(device) &&
 		           (romBlocks = debugger.findDebuggable(device->getName() + " romblocks"))) {
 			result.seg = romBlocks->read(addr);
@@ -356,7 +356,8 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 			std::optional<unsigned> nextGotoTarget;
 			while (clipper.Step()) {
 				auto bpIt = ranges::lower_bound(breakPoints, clipper.DisplayStart, {}, &BreakPoint::getAddress);
-				unsigned addr = instructionBoundary(cpuInterface, clipper.DisplayStart, time);
+				auto addr16 = instructionBoundary(cpuInterface, narrow<uint16_t>(clipper.DisplayStart), time);
+				unsigned addr = addr16;
 				for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; ++row) {
 					ImGui::TableNextRow();
 					if (addr >= 0x10000) continue;
@@ -377,7 +378,7 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 							bool simpleBp = !multiBp;
 							if (hasBp) {
 								ParsedSlotCond bpSlot("pc_in_slot", bpIt->getCondition());
-								bool inSlot = addrInSlot(bpSlot, cpuInterface, debugger, addr);
+								bool inSlot = addrInSlot(bpSlot, cpuInterface, debugger, addr16);
 								bool defaultBp = bpSlot.rest.empty() && (bpIt->getCommand() == "debug break");
 								simpleBp &= defaultBp;
 
@@ -400,7 +401,7 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 									}
 								} else {
 									// schedule creation of new bp
-									auto slot = getCurrentSlot(cpuInterface, debugger, addr);
+									auto slot = getCurrentSlot(cpuInterface, debugger, addr16);
 									addBp.emplace(addr, TclObject("debug break"), toTclExpression(slot), false);
 								}
 							}
@@ -409,7 +410,7 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 						mnemonic.clear();
 						std::optional<uint16_t> mnemonicAddr;
 						std::span<const Symbol* const> mnemonicLabels;
-						auto len = dasm(cpuInterface, addr, opcodes, mnemonic, time,
+						auto len = dasm(cpuInterface, addr16, opcodes, mnemonic, time,
 							[&](std::string& output, uint16_t a) {
 								mnemonicAddr = a;
 								mnemonicLabels = symbolManager.lookupValue(a);
@@ -491,11 +492,11 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 
 								auto setPc = strCat("Set PC to 0x", addrStr);
 								if (ImGui::MenuItem(setPc.c_str())) {
-									regs.setPC(addr);
+									regs.setPC(addr16);
 								}
 							});
 
-							auto addrLabels = symbolManager.lookupValue(addr);
+							auto addrLabels = symbolManager.lookupValue(addr16);
 							std::string_view displayAddr = addrLabels.empty()
 								? std::string_view(addrStr)
 								: std::string_view(addrLabels[cycleLabelsCounter % addrLabels.size()]->name);
@@ -599,7 +600,7 @@ void ImGuiDebugger::drawSlots(MSXCPUInterface& cpuInterface, Debugger& debugger)
 			ImGui::TableSetupColumn("Segment");
 			ImGui::TableHeadersRow();
 
-			for (auto page : xrange(4)) {
+			for (auto page : xrange(uint8_t(4))) {
 				auto addr = 0x4000 * page;
 				if (ImGui::TableNextColumn()) { // page
 					ImGui::StrCat(page);

src/cpu/Dasm.cc
@@ -62,7 +62,7 @@ unsigned dasm(const MSXCPUInterface& interface, uint16_t pc, std::span<uint8_t, 
 			break;
 		case 'R':
 			buf[i] = interface.peekMem(narrow_cast<uint16_t>(pc + i), time);
-			appendAddr(dest, pc + 2 + static_cast<int8_t>(buf[i]));
+			appendAddr(dest, uint16_t(pc + 2 + static_cast<int8_t>(buf[i])));
 			i += 1;
 			break;
 		case 'A':
@@ -132,7 +132,7 @@ static std::pair<uint16_t, unsigned> findGuaranteedBoundary(const MSXCPUInterfac
 	}
 	std::array<unsigned, 4> length;
 	for (auto i : xrange(4)) {
-		length[i] = instructionLength(interface, addr - i, time);
+		length[i] = instructionLength(interface, uint16_t(addr - i), time);
 	}
 
 	while (true) {
@@ -180,7 +180,7 @@ uint16_t instructionBoundary(const MSXCPUInterface& interface, uint16_t addr,
 uint16_t nInstructionsBefore(const MSXCPUInterface& interface, uint16_t addr,
                              EmuTime::param time, int n)
 {
-	unsigned start = std::max(0, int(addr - 4 * n)); // for sure small enough
+	auto start = uint16_t(std::max(0, int(addr - 4 * n))); // for sure small enough
 	auto [tmp, len] = instructionBoundaryAndLength(interface, start, time);
 
 	std::vector<uint16_t> addresses;

src/events/BooleanInput.cc
@@ -3,6 +3,7 @@
 #include "Event.hh"
 #include "SDLKey.hh"
 
+#include "narrow.hh"
 #include "one_of.hh"
 #include "stl.hh"
 #include "strCat.hh"
@@ -73,7 +74,8 @@ std::optional<BooleanInput> parseBooleanInput(std::string_view text)
 		if (it != et) return std::nullopt;
 		auto n = parseValueWithPrefix(button, "button");
 		if (!n) return std::nullopt;
-		return BooleanMouseButton(*n);
+		if (*n > 255) return std::nullopt;
+		return BooleanMouseButton(narrow<uint8_t>(*n));
 
 	} else if (auto joystick = parseValueWithPrefix(type, "joy")) {
 		if (*joystick == 0) return std::nullopt;
@@ -81,10 +83,12 @@ std::optional<BooleanInput> parseBooleanInput(std::string_view text)
 
 		auto subType = *it++;
 		if (auto button = parseValueWithPrefix(subType, "button")) {
+			if (*button > 255) return std::nullopt;
 			if (it != et) return std::nullopt;
-			return BooleanJoystickButton(joyId, *button);
+			return BooleanJoystickButton(joyId, narrow<uint8_t>(*button));
 
 		} else if (auto hat = parseValueWithPrefix(subType, "hat")) {
+			if (*hat > 255) return std::nullopt;
 			if (it == et) return std::nullopt;
 			auto valueStr = *it++;
 			if (it != et) return std::nullopt;
@@ -96,14 +100,16 @@ std::optional<BooleanInput> parseBooleanInput(std::string_view text)
 			else if (valueStr == "left" ) value = BooleanJoystickHat::LEFT;
 			else return std::nullopt;
 
-			return BooleanJoystickHat(joyId, *hat, value);
+			return BooleanJoystickHat(joyId, narrow<uint8_t>(*hat), value);
 
 		} else if (auto pAxis = parseValueWithPrefix(subType, "+axis")) {
+			if (*pAxis > 255) return std::nullopt;
 			if (it != et) return std::nullopt;
-			return BooleanJoystickAxis(joyId, *pAxis, BooleanJoystickAxis::POS);
+			return BooleanJoystickAxis(joyId, narrow<uint8_t>(*pAxis), BooleanJoystickAxis::POS);
 		} else if (auto nAxis = parseValueWithPrefix(subType, "-axis")) {
+			if (*nAxis > 255) return std::nullopt;
 			if (it != et) return std::nullopt;
-			return BooleanJoystickAxis(joyId, *nAxis, BooleanJoystickAxis::NEG);
+			return BooleanJoystickAxis(joyId, narrow<uint8_t>(*nAxis), BooleanJoystickAxis::NEG);
 		}
 	}
 	return std::nullopt;
@@ -119,7 +125,7 @@ std::optional<BooleanInput> captureBooleanInput(const Event& event, std::functio
 			return BooleanMouseButton(e.getButton());
 		},
 		[](const JoystickButtonDownEvent& e) -> std::optional<BooleanInput> {
-			return BooleanJoystickButton(e.getJoystick(), e.getButton());
+			return BooleanJoystickButton(e.getJoystick(), narrow<uint8_t>(e.getButton()));
 		},
 		[](const JoystickHatEvent& e) -> std::optional<BooleanInput> {
 			auto value = e.getValue();

src/fdc/MSXtar.cc
@@ -959,7 +959,7 @@ std::string MSXtar::dir()
 	for (unsigned i = 0; i < num; ++i) {
 		auto entry = list.getListIndexUnchecked(i);
 		auto filename = std::string(entry.getListIndexUnchecked(0).getString());
-		auto attrib = DiskImageUtils::formatAttrib(entry.getListIndexUnchecked(1).getOptionalInt().value_or(0));
+		auto attrib = DiskImageUtils::formatAttrib(narrow<uint8_t>(entry.getListIndexUnchecked(1).getOptionalInt().value_or(0)));
 		//time_t time = entry.getListIndexUnchecked(2).getOptionalInt().value_or(0); // ignored
 		auto size = entry.getListIndexUnchecked(3).getOptionalInt().value_or(0);
 

src/imgui/ImGuiBreakPoints.cc
@@ -402,13 +402,13 @@ static const std::vector<DebugCondition>& getOpenMSXItems(DebugCondition*, MSXCP
 [[nodiscard]] static unsigned getId(const std::shared_ptr<WatchPoint>& wp) { return wp->getId(); }
 [[nodiscard]] static unsigned getId(const DebugCondition& cond) { return cond.getId(); }
 
-[[nodiscard]] static unsigned getAddress(const BreakPoint& bp) { return bp.getAddress(); }
-[[nodiscard]] static unsigned getAddress(const std::shared_ptr<WatchPoint>& wp) { return wp->getBeginAddress(); }
-[[nodiscard]] static unsigned getAddress(const DebugCondition& cond) = delete;
+[[nodiscard]] static uint16_t getAddress(const BreakPoint& bp) { return bp.getAddress(); }
+[[nodiscard]] static uint16_t getAddress(const std::shared_ptr<WatchPoint>& wp) { return narrow<uint16_t>(wp->getBeginAddress()); }
+[[nodiscard]] static uint16_t getAddress(const DebugCondition& cond) = delete;
 
-[[nodiscard]] static unsigned getEndAddress(const BreakPoint& bp) { return bp.getAddress(); } // same as begin
-[[nodiscard]] static unsigned getEndAddress(const std::shared_ptr<WatchPoint>& wp) { return wp->getEndAddress(); }
-[[nodiscard]] static unsigned getEndAddress(const DebugCondition& cond) = delete;
+[[nodiscard]] static uint16_t getEndAddress(const BreakPoint& bp) { return bp.getAddress(); } // same as begin
+[[nodiscard]] static uint16_t getEndAddress(const std::shared_ptr<WatchPoint>& wp) { return narrow<uint16_t>(wp->getEndAddress()); }
+[[nodiscard]] static uint16_t getEndAddress(const DebugCondition& cond) = delete;
 
 [[nodiscard]] static TclObject getConditionObj(const BreakPointBase& bp) { return bp.getConditionObj(); }
 [[nodiscard]] static TclObject getConditionObj(const std::shared_ptr<WatchPoint>& wp) { return wp->getConditionObj(); }

src/imgui/ImGuiDiskManipulator.cc
@@ -89,7 +89,7 @@ std::vector<ImGuiDiskManipulator::FileInfo> ImGuiDiskManipulator::dirMSX(DrivePa
 	for (unsigned i = 0; i < num; ++i) {
 		auto entry = dir.getListIndexUnchecked(i);
 		FileInfo info;
-		info.attrib = entry.getListIndexUnchecked(1).getOptionalInt().value_or(0);
+		info.attrib = narrow<uint8_t>(entry.getListIndexUnchecked(1).getOptionalInt().value_or(0));
 		if (info.attrib & MSXDirEntry::Attrib::VOLUME) continue; // skip
 		info.filename = std::string(entry.getListIndexUnchecked(0).getString());
 		if (info.filename == one_of(".", "..")) continue; // skip

src/imgui/ImGuiVdpRegs.cc
@@ -187,7 +187,7 @@ static std::string spacing(uint32_t) { return "\n"s; }
 
 static const VDP* g_vdp; // HACK: global!!
 static std::array<uint8_t, 64 + 10> registerValues; // HACK: global!!
-static uint8_t getMode() { return ((registerValues[1] & 0x18) >> 3) | ((registerValues[0] & 0x0E) << 1); }
+static uint8_t getMode() { return uint8_t(((registerValues[1] & 0x18) >> 3) | ((registerValues[0] & 0x0E) << 1)); }
 static bool isText2Mode() { return getMode() == 0b01010; } // Note: M1 and M2 swapped!
 static bool isGraph23Mode() { return getMode() == one_of(0b00100, 0b01000); }
 static bool isSprite2Mode() { return getMode() == one_of(0b01000, 0b01100, 0b10000, 0b10100, 0b11100); }

src/memory/RomNeo16.cc
@@ -25,9 +25,9 @@ void RomNeo16::writeMem(word address, byte value, EmuTime::param /*time*/)
 	if ((bbb < 2) || (bbb & 1)) return;
 	unsigned region = (bbb >> 1) - 1;
 	if ((address & 1) == 0) {
-		blockReg[region] = (blockReg[region] & 0xFF00) | value;
+		blockReg[region] = uint16_t((blockReg[region] & 0xFF00) | value);
 	} else {
-		blockReg[region] = (blockReg[region] & 0x00FF) | ((value & 0b1111) << 8);
+		blockReg[region] = uint16_t((blockReg[region] & 0x00FF) | ((value & 0b1111) << 8));
 	}
 	setRom(region, blockReg[region]);
 }

commit 537ef7d0f3b34038b4c742bf424364d7d81fa169

Fix disassembly view, was broken after 338a44154f
Thanks to 'turbor' for reporting it.

src/imgui/ImGuiDebugger.cc
@@ -357,8 +357,8 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 			while (clipper.Step()) {
 				auto bpIt = ranges::lower_bound(breakPoints, clipper.DisplayStart, {}, &BreakPoint::getAddress);
 				auto addr16 = instructionBoundary(cpuInterface, narrow<uint16_t>(clipper.DisplayStart), time);
-				unsigned addr = addr16;
 				for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; ++row) {
+					unsigned addr = addr16;
 					ImGui::TableNextRow();
 					if (addr >= 0x10000) continue;
 					im::ID(narrow<int>(addr), [&]{
@@ -557,7 +557,7 @@ void ImGuiDebugger::drawDisassembly(CPURegs& regs, MSXCPUInterface& cpuInterface
 								}
 							}
 						}
-						addr += len;
+						addr16 += len;
 					});
 				}
 			}