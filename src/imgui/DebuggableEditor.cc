openMSX/openMSX

commit 084d90b8577296882bd7c005ade3a34b43360e09

DebuggableEditor: remove 'dataEditingActive=NONE' state
After adding the ASCII-edit mode, there were 3 possibles modes:
* Hex-edit active
* Ascii-edit active
* None active
This 'none' state was inherited from the original 'imgui_memory_editor' code
(https://github.com/ocornut/imgui_club?tab=readme-ov-file#imgui_memory_editor)

In our refactored code the 'none' state was only active:
* At the very start.
* Or when explicitly canceling an edit (press esc).

I tried removing this mode, and personally I like it better. Now always
exactly one of the hex or the ASCII cells will be selected (colored
blue-ish), the corresponding address on the opposite side (hex <->
ascii) is colored grey. (It also simplifies the code a tiny bit).

src/imgui/DebuggableEditor.cc
@@ -294,8 +294,6 @@ void DebuggableEditor::drawContents(const Sizes& s, Debuggable& debuggable, unsi
 		im::ID(int(addr), [&]{
 			if (ImGui::InputText("##data", &dataInput, flags, UserData::Callback, &userData)) {
 				dataWrite = true;
-			} else if (!dataEditingTakeFocus && ImGui::IsItemDeactivatedAfterEdit()) {
-				dataEditingActive = NONE;
 			}
 		});
 		dataEditingTakeFocus = false;

src/imgui/DebuggableEditor.hh
@@ -91,9 +91,9 @@ private:
 	// [Internal State]
 	std::string dataInput;
 	std::string addrStr;
-	enum EditType { NONE, HEX, ASCII };
-	EditType dataEditingActive = NONE;
-	bool dataEditingTakeFocus = false;
+	enum EditType { HEX, ASCII };
+	EditType dataEditingActive = HEX;
+	bool dataEditingTakeFocus = true;
 	bool updateAddr = false;
 };
 

commit 353b5d4e0f80d21859f6e04a869ad6ee5f29423b

DebuggableEditor: bugfix update changing field when not editing
This recently broke in commit 084d90b857729. Thanks to bas for reporting
(and discovering this so quickly).

In the memory view, when the cursor is an a changing field (e.g. 0xfc9e
JIFFY), and when the edit field is deactivated (e.g. click somewhere
else), then the (changing) value is no longer updated.

Now I understand why the original code went back to the 'NONE' state
(see previous commit message) ;-)

src/imgui/DebuggableEditor.cc
@@ -294,6 +294,8 @@ void DebuggableEditor::drawContents(const Sizes& s, Debuggable& debuggable, unsi
 		im::ID(int(addr), [&]{
 			if (ImGui::InputText("##data", &dataInput, flags, UserData::Callback, &userData)) {
 				dataWrite = true;
+			} else if (!ImGui::IsItemActive()) {
+				setStrings();
 			}
 		});
 		dataEditingTakeFocus = false;