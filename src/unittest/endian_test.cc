bd387152eea6ae4ff03812fd1618b44880df28bf
m9710797 <vermaelen.wouter@gmail.com>
2024-03-30 10:55:39 +0100

    Removed union from endian unittest
    Strictly speaking it was undefinded behavior. Though in practice it
    worked fine (and even guaranteed with gcc).
    
    But the union wasn't really required for the test. So it's good to clean
    it up anyway.

--- src/unittest/endian_test.cc
+++ src/unittest/endian_test.cc
@@ -1,17 +1,9 @@
 #include "catch.hpp"
 #include "endian.hh"
 
+#include <array>
+
 using namespace Endian;
-
-union T16 {
-	B16 be;
-	L16 le;
-};
-
-union T32 {
-	B32 be;
-	L32 le;
-};
 
 TEST_CASE("endian: byteswap")
 {
@@ -32,62 +24,48 @@ TEST_CASE("endian: byteswap")
 // TODO better coverage for aligned vs unaligned versions of the functions
 TEST_CASE("endian: 16-bit")
 {
-	T16 t;
-	REQUIRE(sizeof(t) == 2);
+	REQUIRE(sizeof(B16) == 2);
+	REQUIRE(sizeof(L16) == 2);
 
-	t.le = 0x1234;
-	CHECK(t.le == 0x1234);
-	CHECK(t.be == 0x3412);
-	CHECK(read_UA_L16(&t) == 0x1234);
-	CHECK(read_UA_B16(&t) == 0x3412);
+	L16 l = 0x1234;
+	CHECK(read_UA_L16(&l) == 0x1234);
+	CHECK(read_UA_B16(&l) == 0x3412);
 
-	t.be = 0x1234;
-	CHECK(t.le == 0x3412);
-	CHECK(t.be == 0x1234);
-	CHECK(read_UA_L16(&t) == 0x3412);
-	CHECK(read_UA_B16(&t) == 0x1234);
+	B16 b = 0x1234;
+	CHECK(read_UA_L16(&b) == 0x3412);
+	CHECK(read_UA_B16(&b) == 0x1234);
 
-	write_UA_L16(&t, 0xaabb);
-	CHECK(t.le == 0xaabb);
-	CHECK(t.be == 0xbbaa);
-	CHECK(read_UA_L16(&t) == 0xaabb);
-	CHECK(read_UA_B16(&t) == 0xbbaa);
+	std::array<uint8_t, 2> buf;
+	write_UA_L16(buf.data(), 0xaabb);
+	CHECK(read_UA_L16(buf.data()) == 0xaabb);
+	CHECK(read_UA_B16(buf.data()) == 0xbbaa);
 
-	write_UA_B16(&t, 0xaabb);
-	CHECK(t.le == 0xbbaa);
-	CHECK(t.be == 0xaabb);
-	CHECK(read_UA_L16(&t) == 0xbbaa);
-	CHECK(read_UA_B16(&t) == 0xaabb);
+	write_UA_B16(buf.data(), 0xaabb);
+	CHECK(read_UA_L16(buf.data()) == 0xbbaa);
+	CHECK(read_UA_B16(buf.data()) == 0xaabb);
 }
 
 TEST_CASE("endian: 32-bit")
 {
-	T32 t;
-	REQUIRE(sizeof(t) == 4);
+	REQUIRE(sizeof(B32) == 4);
+	REQUIRE(sizeof(L32) == 4);
 
-	t.le = 0x12345678;
-	CHECK(t.le == 0x12345678);
-	CHECK(t.be == 0x78563412);
-	CHECK(read_UA_L32(&t) == 0x12345678);
-	CHECK(read_UA_B32(&t) == 0x78563412);
+	L32 l = 0x12345678;
+	CHECK(read_UA_L32(&l) == 0x12345678);
+	CHECK(read_UA_B32(&l) == 0x78563412);
 
-	t.be = 0x12345678;
-	CHECK(t.le == 0x78563412);
-	CHECK(t.be == 0x12345678);
-	CHECK(read_UA_L32(&t) == 0x78563412);
-	CHECK(read_UA_B32(&t) == 0x12345678);
+	B32 b = 0x12345678;
+	CHECK(read_UA_L32(&b) == 0x78563412);
+	CHECK(read_UA_B32(&b) == 0x12345678);
 
-	write_UA_L32(&t, 0xaabbccdd);
-	CHECK(t.le == 0xaabbccdd);
-	CHECK(t.be == 0xddccbbaa);
-	CHECK(read_UA_L32(&t) == 0xaabbccdd);
-	CHECK(read_UA_B32(&t) == 0xddccbbaa);
+	std::array<uint8_t, 4> buf;
+	write_UA_L32(buf.data(), 0xaabbccdd);
+	CHECK(read_UA_L32(buf.data()) == 0xaabbccdd);
+	CHECK(read_UA_B32(buf.data()) == 0xddccbbaa);
 
-	write_UA_B32(&t, 0xaabbccdd);
-	CHECK(t.le == 0xddccbbaa);
-	CHECK(t.be == 0xaabbccdd);
-	CHECK(read_UA_L32(&t) == 0xddccbbaa);
-	CHECK(read_UA_B32(&t) == 0xaabbccdd);
+	write_UA_B32(buf.data(), 0xaabbccdd);
+	CHECK(read_UA_L32(buf.data()) == 0xddccbbaa);
+	CHECK(read_UA_B32(buf.data()) == 0xaabbccdd);
 }
 
 
@@ -100,21 +78,10 @@ uint16_t testSwap16()           { return byteswap16(0x1234); }
 uint32_t testSwap32(uint32_t x) { return byteswap32(x); }
 uint32_t testSwap32()           { return byteswap32(0x12345678); }
 
-void test1(T16& t, uint16_t x) { t.le = x; }
-void test2(T16& t, uint16_t x) { t.be = x; }
-uint16_t test3(T16& t) { return t.le; }
-uint16_t test4(T16& t) { return t.be; }
-
 void testA(uint16_t& s, uint16_t x) { write_UA_L16(&s, x); }
 void testB(uint16_t& s, uint16_t x) { write_UA_B16(&s, x); }
 uint16_t testC(uint16_t& s) { return read_UA_L16(&s); }
 uint16_t testD(uint16_t& s) { return read_UA_B16(&s); }
-
-
-void test1(T32& t, uint32_t x) { t.le = x; }
-void test2(T32& t, uint32_t x) { t.be = x; }
-uint32_t test3(T32& t) { return t.le; }
-uint32_t test4(T32& t) { return t.be; }
 
 void testA(uint32_t& s, uint32_t x) { write_UA_L32(&s, x); }
 void testB(uint32_t& s, uint32_t x) { write_UA_B32(&s, x); }
