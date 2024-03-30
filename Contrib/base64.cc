b36687e114bd6e8b4b7f6a0f9bb488afaf89de23
m9710797 <vermaelen.wouter@gmail.com>
2024-03-30 14:47:23 +0100

    Modernize Contrib/base64.cc
    Before this patch the code also didn't compile anymore (after changes in
    src/utils/base64.hh). The code now requires c++20.

--- Contrib/base64.cc
+++ Contrib/base64.cc
@@ -1,85 +1,88 @@
 // compile with:
-//   g++ -Wall -Os base64.cc -I ../src/utils/ ../src/utils/Base64.cc -lz -o encode-gz-base64
-//   g++ -Wall -Os base64.cc -I ../src/utils/ ../src/utils/Base64.cc -lz -o decode-gz-base64
+//   g++ -std=c++20 -Wall -Os base64.cc -I ../src/utils/ ../src/utils/Base64.cc -lz -o encode-gz-base64
+//   g++ -std=c++20 -Wall -Os base64.cc -I ../src/utils/ ../src/utils/Base64.cc -lz -o decode-gz-base64
 
 #include "Base64.hh"
-#include <vector>
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <sys/stat.h>
+
 #include <zlib.h>
 
-using namespace std;
+#include <bit>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <span>
+#include <sys/stat.h>
+#include <vector>
 
-string encode(const void* data, unsigned len)
+std::string encode(const void* data, unsigned len)
 {
 	uLongf dstLen = len + len / 1000 + 12 + 1; // worst-case
-	vector<unsigned char> buf(dstLen);
+	std::vector<unsigned char> buf(dstLen);
 	if (compress2(buf.data(), &dstLen,
-		      reinterpret_cast<const Bytef*>(data), len, 9)
+	              std::bit_cast<const Bytef*>(data), len, 9)
 	    != Z_OK) {
-		cerr << "Error while compressing blob." << endl;
+		std::cerr << "Error while compressing blob.\n";
 		exit(1);
 	}
-	return Base64::encode(buf.data(), dstLen);
+	return Base64::encode(std::span(buf.data(), dstLen));
 }
 
-string decode(const char* data, unsigned len)
+std::string decode(const char* data, unsigned len)
 {
 	static const unsigned MAX_SIZE = 1024 * 1024; // 1MB
-	string tmp = Base64::decode(string(data, len));
-	vector<char> buf(MAX_SIZE);
+	const auto& [decBuf, decBufSize] = Base64::decode(std::string_view(data, len));
+	std::vector<char> buf(MAX_SIZE);
 	uLongf dstLen = MAX_SIZE;
-	if (uncompress(reinterpret_cast<Bytef*>(buf.data()), &dstLen,
-			reinterpret_cast<const Bytef*>(tmp.data()), uLong(tmp.size()))
+	if (uncompress(std::bit_cast<Bytef*>(buf.data()), &dstLen,
+	               std::bit_cast<const Bytef*>(decBuf.data()), uLong(decBufSize))
 	    != Z_OK) {
-		cerr << "Error while decompressing blob." << endl;
+		std::cerr << "Error while decompressing blob.\n";
 		exit(1);
 	}
-	return string(buf.data(), dstLen);
+	return {buf.data(), dstLen};
 }
 
-int main(int argc, char** argv)
+int main(int argc, const char** argv)
 {
-	if (argc != 3) {
-		cerr << "Usage: " << argv[0] << " <input> <output>\n";
+	std::span<const char*> arg(argv, argc);
+	if (arg.size() != 3) {
+		std::cerr << "Usage: " << arg[0] << " <input> <output>\n";
 		exit(1);
 	}
-	FILE* inf = fopen(argv[1], "rb");
+	FILE* inf = fopen(arg[1], "rb");
 	if (!inf) {
-		cerr << "Error while opening " << argv[1] << endl;
+		std::cerr << "Error while opening " << arg[1] << '\n';
 		exit(1);
 	}
 	struct stat st;
 	fstat(fileno(inf), &st);
 	size_t size = st.st_size;
-	vector<char> inBuf(size);
+	std::vector<char> inBuf(size);
 	if (fread(inBuf.data(), size, 1, inf) != 1) {
-		cerr << "Error whle reading " << argv[1] << endl;
+		std::cerr << "Error whle reading " << arg[1] << '\n';
 		exit(1);
 	}
 
-	string result;
-	if        (strstr(argv[0], "encode-gz-base64")) {
+	std::string result;
+	if        (strstr(arg[0], "encode-gz-base64")) {
 		result = encode(inBuf.data(), inBuf.size());
-	} else if (strstr(argv[0], "decode-gz-base64")) {
+	} else if (strstr(arg[0], "decode-gz-base64")) {
 		result = decode(inBuf.data(), inBuf.size());
 	} else {
-		cerr << "This executable should be named 'encode-gz-base64' or "
-		        "'decode-gz-base64'." << endl;
+		std::cerr << "This executable should be named 'encode-gz-base64' or "
+		             "'decode-gz-base64'.\n";
 		exit(1);
 	}
 
-	FILE* outf = fopen(argv[2], "wb+");
+	FILE* outf = fopen(arg[2], "wb+");
 	if (!outf) {
-		cerr << "Error while opening " << argv[2] << endl;
+		std::cerr << "Error while opening " << arg[2] << '\n';
 		exit(1);
 	}
 
 	if (fwrite(result.data(), result.size(), 1, outf) != 1) {
-		cerr << "Error whle writing " << argv[2] << endl;
+		std::cerr << "Error whle writing " << arg[2] << '\n';
 		exit(1);
 	}
 }
