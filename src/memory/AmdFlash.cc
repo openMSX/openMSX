ec091b2b04648cb30f89f078b023cde82cedd4be
Laurens Holst <laurens@grauw.nl>
2024-06-10 19:34:28 +0200

    AmdFlash: Base status dataPolling on value written by CPU.
    Not on the definitive value in memory.

--- src/memory/AmdFlash.cc
+++ src/memory/AmdFlash.cc
@@ -713,6 +713,8 @@ bool AmdFlash::checkCommandProgramHelper(size_t numBytes, std::span<const uint8_
 {
 	if (numBytes <= chip.program.pageSize && partialMatch(cmdSeq)) {
 		if (cmd.size() == cmdSeq.size()) clearStatus();
+		if (cmd.size() <= cmdSeq.size()) return true;
+		status.dataPolling = ~cmd.back().value & 0x80;
 		if (cmd.size() < (cmdSeq.size() + numBytes)) return true;
 		for (auto i : xrange(cmdSeq.size(), cmdSeq.size() + numBytes)) {
 			const Sector& sector = getSector(cmd[i].addr);
@@ -720,12 +722,11 @@ bool AmdFlash::checkCommandProgramHelper(size_t numBytes, std::span<const uint8_
 				auto ramAddr = sector.writeAddress + cmd[i].addr - sector.address;
 				uint8_t ramValue = (*ram)[ramAddr] & cmd[i].value;
 				ram->write(ramAddr, ramValue);
-
-				status.dataPolling = ramValue & 0x80; // immediate completion
 			} else {
 				status.sectorLock = true;
 			}
 		}
+		status.dataPolling = !status.dataPolling; // immediate completion
 	}
 	return false;
 }
@@ -756,12 +757,11 @@ bool AmdFlash::checkCommandBufferProgram()
 						auto ramAddr = sector.writeAddress + cmd[i].addr - sector.address;
 						uint8_t ramValue = (*ram)[ramAddr] & cmd[i].value;
 						ram->write(ramAddr, ramValue);
-
-						status.dataPolling = ramValue & 0x80; // immediate completion
 					}
 				} else {
 					status.sectorLock = true;
 				}
+				status.dataPolling = !status.dataPolling; // immediate completion
 				return false;
 			}
 		}
