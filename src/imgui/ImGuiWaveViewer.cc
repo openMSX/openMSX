6a2ab358d869050ce8a6d8c7313e08f798bddafa
m9710797 <vermaelen.wouter@gmail.com>
2024-07-13 19:21:58 +0200

    ImGuiWaveViewer: replace ImGui::Plot{Lines,Histogram}() with our own version
    The reason is that the ImGui version has some behavior that we don't
    want. For example the popups, and they can't be disabled. Our version can
    also be more specific, thus it may be a bit faster (less conditionals in
    the inner loop).
    
    The original ImGui::PlotLines() and ImGui::PlotHistogram() functions
    weren't that complex (and much less so after our simplications). In fact
    the (internal) documentation on those functions has this section:
        // Plot/Graph widgets are not very good.
        // Consider writing your own, or using a third-party one, see:
        // - ImPlot https://github.com/epezent/implot
        // - others https://github.com/ocornut/imgui/wiki/Useful-Extensions
    And thus indeed, for now we've written our own version. Later we might
    consider using "implot".
    
    --
    
    A future extension could be to re-add popups in the spectrum plot. But
    then with meaningful information, e.g. the frequency and power-level.
    (For the waveform plot I don't think a popup will be useful).

--- src/imgui/ImGuiWaveViewer.cc
+++ src/imgui/ImGuiWaveViewer.cc
@@ -22,6 +22,105 @@
 
 namespace openmsx {
 
+// Simplified/optimized version of ImGui::PlotLines()
+static void plotLines(std::span<const float> values, float scale_min, float scale_max, gl::vec2 outer_size)
+{
+	const auto& style = ImGui::GetStyle();
+
+	gl::vec2 pos = ImGui::GetCursorScreenPos();
+	auto outer_tl = pos;              // top-left
+	auto outer_br = pos + outer_size; // bottom-right
+	auto inner_tl = outer_tl + gl::vec2(style.FramePadding);
+	auto inner_br = outer_br - gl::vec2(style.FramePadding);
+
+	ImGui::RenderFrame(outer_tl, outer_br, ImGui::GetColorU32(ImGuiCol_FrameBg),
+	                   true, style.FrameRounding);
+
+	int num_values = narrow<int>(values.size());
+	if (num_values < 2) return;
+	int num_items = num_values - 1;
+
+	int inner_width = std::max(1, static_cast<int>(inner_br.x - inner_tl.x));
+	int res_w = std::min(inner_width, num_items);
+
+	float t_step = 1.0f / (float)res_w;
+	float scale_range = scale_max - scale_min;
+	float inv_scale = (scale_range != 0.0f) ? (1.0f / scale_range) : 0.0f;
+
+	auto color = ImGui::GetColorU32(ImGuiCol_PlotLines);
+
+	auto valueToY = [&](float v) {
+		return 1.0f - std::clamp((v - scale_min) * inv_scale, 0.0f, 1.0f);
+	};
+	float t = 0.0f;
+	auto tp0 = gl::vec2(t, valueToY(values[0]));
+	auto pos0 = ImLerp(inner_tl, inner_br, tp0);
+
+	auto* drawList = ImGui::GetWindowDrawList();
+	for (int n = 0; n < res_w; n++) {
+		int idx = static_cast<int>(t * num_items + 0.5f);
+		assert(0 <= idx && idx < num_values);
+		float v = values[(idx + 1) % num_values];
+
+		t += t_step;
+		auto tp1 = gl::vec2(t, valueToY(v));
+		auto pos1 = ImLerp(inner_tl, inner_br, tp1);
+		drawList->AddLine(pos0, pos1, color);
+		pos0 = pos1;
+	}
+}
+
+// Simplified/optimized version of ImGui::PlotHistogram()
+static void plotHistogram(std::span<const float> values, float scale_min, float scale_max, gl::vec2 outer_size)
+{
+	const auto& style = ImGui::GetStyle();
+
+	gl::vec2 pos = ImGui::GetCursorScreenPos();
+	auto outer_tl = pos;              // top-left
+	auto outer_br = pos + outer_size; // bottom-right
+	auto inner_tl = outer_tl + gl::vec2(style.FramePadding);
+	auto inner_br = outer_br - gl::vec2(style.FramePadding);
+
+	ImGui::RenderFrame(outer_tl, outer_br, ImGui::GetColorU32(ImGuiCol_FrameBg),
+	                   true, style.FrameRounding);
+	if (values.empty()) return;
+
+	int num_values = narrow<int>(values.size());
+	int inner_width = std::max(1, static_cast<int>(inner_br.x - inner_tl.x));
+	int res_w = std::min(inner_width, num_values);
+
+	float t_step = 1.0f / static_cast<float>(res_w);
+	float scale_range = scale_max - scale_min;
+	float inv_scale = (scale_range != 0.0f) ? (1.0f / scale_range) : 0.0f;
+
+	auto valueToY = [&](float v) {
+		return 1.0f - std::clamp((v - scale_min) * inv_scale, 0.0f, 1.0f);
+	};
+	float zero_line = (scale_min * scale_max < 0.0f)
+	                ? (1 + scale_min * inv_scale)
+	                : (scale_min < 0.0f ? 0.0f : 1.0f);
+
+	auto color = ImGui::GetColorU32(ImGuiCol_PlotHistogram);
+
+	float t0 = 0.0f;
+	auto* drawList = ImGui::GetWindowDrawList();
+	drawList->PrimReserve(6 * res_w, 4 * res_w);
+
+	for (int n = 0; n < res_w; n++) {
+		int idx = static_cast<int>(t0 * num_values + 0.5f);
+		assert(0 <= idx && idx < num_values);
+		float y0 = valueToY(values[idx]);
+		float t1 = t0 + t_step;
+
+		auto pos0 = ImLerp(inner_tl, inner_br, gl::vec2(t0, y0));
+		auto pos1 = ImLerp(inner_tl, inner_br, gl::vec2(t1, zero_line));
+		drawList->PrimRect(pos0, pos1, color);
+
+		t0 = t1;
+	}
+}
+
+
 void ImGuiWaveViewer::save(ImGuiTextBuffer& buf)
 {
 	savePersistent(buf, *this, persistentElements);
@@ -71,9 +170,8 @@ static void paintWave(std::span<const float> buf)
 	// skip if not visible
 	auto pos = ImGui::GetCursorPos();
 	ImGui::SetNextItemWidth(-FLT_MIN); // full cell-width
-	auto width = ImGui::CalcItemWidth();
-	auto height = ImGui::GetFrameHeight();
-	ImGui::Dummy(gl::vec2{width, height});
+	auto size = gl::vec2{ImGui::CalcItemWidth(), ImGui::GetFrameHeight()};
+	ImGui::Dummy(size);
 	if (!ImGui::IsItemVisible()) return;
 	ImGui::SetCursorPos(pos);
 
@@ -87,8 +185,7 @@ static void paintWave(std::span<const float> buf)
 		[](auto x) { return std::abs(x); });
 	if (peak == 0.0f) peak = 1.0f; // force line in the middle
 
-	ImGui::SetNextItemWidth(width);
-	ImGui::PlotLines("", buf.data(), narrow<int>(buf.size()), 0, nullptr, -peak, peak);
+	plotLines(buf, -peak, peak, size);
 }
 
 static constexpr int FFT_LEN_L2 = 11; // 2048-point FFT -> 1024 bins in the spectrum
@@ -157,9 +254,8 @@ static void paintSpectrum(std::span<const float> buf, float factor)
 	// skip if not visible
 	auto pos = ImGui::GetCursorPos();
 	ImGui::SetNextItemWidth(-FLT_MIN); // full cell-width
-	auto width = ImGui::CalcItemWidth();
-	auto height = ImGui::GetFrameHeight();
-	ImGui::Dummy(gl::vec2{width, height});
+	auto size = gl::vec2{ImGui::CalcItemWidth(), ImGui::GetFrameHeight()};
+	ImGui::Dummy(size);
 	if (!ImGui::IsItemVisible()) return;
 	ImGui::SetCursorPos(pos);
 
@@ -193,8 +289,7 @@ static void paintSpectrum(std::span<const float> buf, float factor)
 	}
 
 	// actually plot the result
-	ImGui::SetNextItemWidth(width);
-	ImGui::PlotHistogram("", magnitude.data(), narrow<int>(magnitude.size()), 0, nullptr, 0.0f, 6.0f);
+	plotHistogram(magnitude, 0.0f, 6.0f, size);
 }
 
 static void stereoToMono(std::span<const float> stereo, float factorL, float factorR,
