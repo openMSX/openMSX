openMSX/openMSX

commit 078e8f506121efed95e78f3b0118255338165441

Remove support for savestates with .xml.gz extension.
Also, make the internal store_machine and restore_machine commands less smart.
They are not designed to be used by users, so they don't need to provide all
kinds of smart convenience stuff. Instead, this is all implemented in the
savestate and loadstate Tcl commands.

share/scripts/_savestate.tcl
@@ -6,15 +6,7 @@ proc savestate_common {} {
 	uplevel {
 		if {$name eq ""} {set name "quicksave"}
 		set directory [file normalize $::env(OPENMSX_USER_DATA)/../savestates]
-		set fullname_oms [file join $directory ${name}.oms]
-		set fullname_gz  [file join $directory ${name}.xml.gz]
-		if {![file exists $fullname_oms] &&
-		     [file exists $fullname_gz]} {
-			# only when old name exists but new doesn't
-			set fullname_bwcompat $fullname_gz
-		} else {
-			set fullname_bwcompat $fullname_oms
-		}
+		set fullname [file join $directory ${name}.oms]
 		set png [file join $directory ${name}.png]
 	}
 }
@@ -31,17 +23,13 @@ proc savestate {{name ""}} {
 		}
 	}
 	set currentID [machine]
-	# always save using the new (.oms) name
-	store_machine $currentID $fullname_oms
-	# if successful, delete the old (.gz) filename (deleting a non-exiting
-	# file is not an error)
-	file delete -- $fullname_gz
+	store_machine $currentID $fullname
 	return $name
 }
 
 proc loadstate {{name ""}} {
 	savestate_common
-	set newID [restore_machine $fullname_bwcompat]
+	set newID [restore_machine $fullname]
 	set currentID [machine]
 	if {$currentID ne ""} {delete_machine $currentID}
 	activate_machine $newID
@@ -52,11 +40,9 @@ proc loadstate {{name ""}} {
 proc list_savestates_raw {} {
 	set directory [file normalize $::env(OPENMSX_USER_DATA)/../savestates]
 	set results [list]
-	foreach f [glob -tails -directory $directory -nocomplain *.xml.gz *.oms] {
+	foreach f [glob -tails -directory $directory -nocomplain *.oms] {
 		if       {[string range $f end-3 end] eq ".oms"} {
 			set name [string range $f 0 end-4]
-		} elseif {[string range $f end-6 end] eq ".xml.gz"} {
-			set name [string range $f 0 end-7]
 		} else {
 			set name $f
 		}

doc/manual/commands.html
@@ -2053,14 +2053,6 @@
 
   <table>
     <tr>
-      <td><code>store_machine</code></td>
-      <td>Save state of current machine to file "openmsxNNNN.xml.gz"</td>
-    </tr>
-    <tr>
-      <td><code>store_machine &lt;machineID&gt;</code></td>
-      <td>Save state of indicated machine to file "openmsxNNNN.xml.gz"</td>
-    </tr>
-    <tr>
       <td><code>store_machine &lt;machineID&gt; &lt;filename&gt;</code></td>
       <td>Save state of indicated machine to specified file</td>
     </tr>

share/scripts/session_management.tcl
@@ -98,7 +98,7 @@ proc load_session {name} {
 
 	# get all savestate files
 	set directory [file normalize $::env(OPENMSX_USER_DATA)/../sessions/${name}]
-	set states_to_restore [glob -tails -directory $directory -nocomplain *.oms *.xml.gz]
+	set states_to_restore [glob -tails -directory $directory -nocomplain *.oms]
 
 	# abort if we have nothing to restore
 	if {[llength $states_to_restore] == 0} {

src/Reactor.cc
@@ -926,23 +926,9 @@ StoreMachineCommand::StoreMachineCommand(
 
 void StoreMachineCommand::execute(std::span<const TclObject> tokens, TclObject& result)
 {
-	checkNumArgs(tokens, Between{1, 3}, Prefix{1}, "?id? ?filename?");
-	string filename;
-	string_view machineID;
-	switch (tokens.size()) {
-	case 1:
-		machineID = reactor.getMachineID();
-		filename = FileOperations::getNextNumberedFileName("savestates", "openmsxstate", ".xml.gz");
-		break;
-	case 2:
-		machineID = tokens[1].getString();
-		filename = FileOperations::getNextNumberedFileName("savestates", "openmsxstate", ".xml.gz");
-		break;
-	case 3:
-		machineID = tokens[1].getString();
-		filename = tokens[2].getString();
-		break;
-	}
+	checkNumArgs(tokens, 3, Prefix{1}, "id filename");
+	const auto& machineID = tokens[1].getString();
+	const auto& filename = tokens[2].getString();
 
 	auto& board = *reactor.getMachine(machineID);
 
@@ -955,8 +941,6 @@ void StoreMachineCommand::execute(std::span<const TclObject> tokens, TclObject& 
 string StoreMachineCommand::help(std::span<const TclObject> /*tokens*/) const
 {
 	return
-		"store_machine                       Save state of current machine to file \"openmsxNNNN.xml.gz\"\n"
-		"store_machine machineID             Save state of machine \"machineID\" to file \"openmsxNNNN.xml.gz\"\n"
 		"store_machine machineID <filename>  Save state of machine \"machineID\" to indicated file\n"
 		"\n"
 		"This is a low-level command, the 'savestate' script is easier to use.";
@@ -980,33 +964,10 @@ RestoreMachineCommand::RestoreMachineCommand(
 void RestoreMachineCommand::execute(std::span<const TclObject> tokens,
                                     TclObject& result)
 {
-	checkNumArgs(tokens, Between{1, 2}, Prefix{1}, "?filename?");
+	checkNumArgs(tokens, 2, Prefix{1}, "filename");
 	auto newBoard = reactor.createEmptyMotherBoard();
 
-	string filename;
-	switch (tokens.size()) {
-	case 1: {
-		// load last saved entry
-		string lastEntry;
-		time_t lastTime = 0;
-		foreach_file(
-			FileOperations::getUserOpenMSXDir() + "/savestates",
-			[&](const string& path, const FileOperations::Stat& st) {
-				time_t modTime = st.st_mtime;
-				if (modTime > lastTime) {
-					filename = path;
-					lastTime = modTime;
-				}
-			});
-		if (filename.empty()) {
-			throw CommandException("Can't find last saved state.");
-		}
-		break;
-	}
-	case 2:
-		filename = FileOperations::expandTilde(string(tokens[1].getString()));
-		break;
-	}
+	const auto filename = FileOperations::expandTilde(string(tokens[1].getString()));
 
 	//std::cerr << "Loading " << filename << '\n';
 	try {
@@ -1038,7 +999,6 @@ string RestoreMachineCommand::help(std::span<const TclObject> /*tokens*/) const
 
 void RestoreMachineCommand::tabCompletion(vector<string>& tokens) const
 {
-	// TODO: add the default files (state files in user's savestates dir)
 	completeFileName(tokens, userFileContext());
 }
 

src/imgui/ImGuiReverseBar.cc
@@ -159,9 +159,6 @@ void ImGuiReverseBar::showMenu(MSXMotherBoard* motherBoard)
 						if (name.ends_with(ReverseManager::REPLAY_EXTENSION)) {
 							name.remove_suffix(ReverseManager::REPLAY_EXTENSION.size());
 							names.emplace_back(fullName, std::string(name));
-						} else if (name.ends_with(".xml.gz")) {
-							name.remove_suffix(7);
-							names.emplace_back(fullName, std::string(name));
 						}
 					});
 				}

commit e124afbf0e5472dd500bd4fb7fffe7845d2dd53f

Fix deleting savestates.
This was broken after 078e8f506121efed95e78f3b0118255338165441 (oops).

share/scripts/_savestate.tcl
@@ -83,7 +83,7 @@ proc list_savestates {args} {
 
 proc delete_savestate {{name ""}} {
 	savestate_common
-	catch {file delete -- $fullname_bwcompat}
+	catch {file delete -- $fullname}
 	catch {file delete -- $png}
 	return ""
 }