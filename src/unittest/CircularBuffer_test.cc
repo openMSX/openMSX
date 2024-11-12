3859ca2685ea01347ab3e82b62d6e0f3fd21960d
m9710797 <vermaelen.wouter@gmail.com>
2024-11-12 19:42:58 +0100

    Fix/update unittest after 61f6f03d8a0af0f7

--- src/unittest/CircularBuffer_test.cc
+++ src/unittest/CircularBuffer_test.cc
@@ -5,26 +5,30 @@ using namespace openmsx;
 
 TEST_CASE("CircularBuffer")
 {
-	CircularBuffer<int, 2> buf;
-	CHECK(buf.isEmpty());
-	CHECK(!buf.isFull());
+	CircularBuffer<int, 3> buf;
+	CHECK(buf.empty());
+	CHECK(!buf.full());
 	CHECK(buf.size() == 0);
 
-	buf.addBack(15);
-	CHECK(!buf.isEmpty());
-	CHECK(!buf.isFull());
+	buf.push_back(15);
+	CHECK(!buf.empty());
+	CHECK(!buf.full());
 	CHECK(buf.size() == 1);
 	CHECK(buf[0] == 15);
+	CHECK(buf.front() == 15);
+	CHECK(buf.back() == 15);
 
 	buf[0] = 25;
 	CHECK(buf[0] == 25);
 
-	buf.addFront(17);
-	CHECK(!buf.isEmpty());
-	CHECK(buf.isFull());
+	buf.push_front(17);
+	CHECK(!buf.empty());
+	CHECK(!buf.full());
 	CHECK(buf.size() == 2);
 	CHECK(buf[0] == 17);
 	CHECK(buf[1] == 25);
+	CHECK(buf.front() == 17);
+	CHECK(buf.back() == 25);
 
 	buf[1] = 35;
 	CHECK(buf[0] == 17);
@@ -33,12 +37,23 @@ TEST_CASE("CircularBuffer")
 	CHECK(buf[0] == 27);
 	CHECK(buf[1] == 35);
 
-	int a = buf.removeBack();
-	CHECK(a == 35);
-	CHECK(buf.size() == 1);
-	CHECK(buf[0] == 27);
+	buf.push_back(11);
+	CHECK(buf.full());
+	CHECK(!buf.empty());
+	CHECK(buf.size() == 3);
+	CHECK(buf.front() == 27);
+	CHECK(buf.back() == 11);
 
-	int b = buf.removeFront();
-	CHECK(b == 27);
-	CHECK(buf.isEmpty());
+	CHECK(buf.pop_front() == 27);
+	CHECK(!buf.full());
+	CHECK(buf.size() == 2);
+	CHECK(buf.front() == 35);
+	CHECK(buf.back() == 11);
+
+	CHECK(buf.pop_back() == 11);
+	CHECK(buf.size() == 1);
+	CHECK(buf[0] == 35);
+
+	CHECK(buf.pop_front() == 35);
+	CHECK(buf.empty());
 }
