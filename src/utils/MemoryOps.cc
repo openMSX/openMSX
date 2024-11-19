13053562c42719cf7048e30a79e41e629c62f3ee
m9710797 <vermaelen.wouter@gmail.com>
2024-11-19 09:37:57 +0100

    Simplify MemSet and MemSet2
    We had manually unrolled these functions using SSE2 intrinsics. This
    commit replaces that with plain c++ code. The greatly simplifies the
    code.
    
    In the past our tuned versions were faster, but I verified that new gcc
    versions generate quasi equivalent code for both. In other words: newer
    gcc versions can perform the same optimizations themselves. Moreover
    when compiling for AVX2 or AVX512 the compiler can take advantage of
    that, while our manually tuned code is stuck with SSE2.

--- src/utils/MemoryOps.cc
+++ src/utils/MemoryOps.cc
@@ -1,175 +1,48 @@
 #include "MemoryOps.hh"
 
-#include "build-info.hh"
 #include "systemfuncs.hh"
 
 #include "endian.hh"
-#include "narrow.hh"
+#include "ranges.hh"
 #include "stl.hh"
-#include "unreachable.hh"
 
 #include <bit>
 #include <cassert>
 #include <cstdlib>
 #include <cstdint>
 #include <new> // for std::bad_alloc
-#if ASM_X86 && defined _MSC_VER
-#include <intrin.h>	// for __stosd intrinsic
-#endif
-#ifdef __SSE2__
-#include <emmintrin.h>
-#endif
+#include <type_traits>
 
 namespace openmsx::MemoryOps {
 
-#ifdef __SSE2__
-#if ASM_X86_32 && defined _MSC_VER
-// Gcc has the _mm_set1_epi64x() function for both 32 and 64 bit. Visual studio
-// only has it for 64 bit. So we add it ourselves for vc++/32-bit. An
-// alternative would be to always use this routine, but this generates worse
-// code than the real _mm_set1_epi64x() function for gcc (both 32 and 64 bit).
-[[nodiscard]] static inline __m128i _mm_set1_epi64x(uint64_t val)
+template<typename Pixel>
+void MemSet<Pixel>::operator()(std::span<Pixel> out, Pixel val) const
 {
-	uint32_t low  = val >> 32;
-	uint32_t high = val >>  0;
-	return _mm_set_epi32(low, high, low, high);
-}
-#endif
-
-static inline void memset_64_SSE(
-	uint64_t* out, size_t num64, uint64_t val64)
-{
-	if (num64 == 0) [[unlikely]] return;
-
-	// Align at 16-byte boundary.
-	if (size_t(out) & 8) [[unlikely]] {
-		out[0] = val64;
-		++out; --num64;
-	}
-
-	__m128i val128 = _mm_set1_epi64x(narrow_cast<int64_t>(val64));
-	const uint64_t* e = out + num64 - 3;
-	for (/**/; out < e; out += 4) {
-		_mm_storeu_si128(std::bit_cast<__m128i*>(out + 0), val128);
-		_mm_storeu_si128(std::bit_cast<__m128i*>(out + 2), val128);
-	}
-	if (num64 & 2) [[unlikely]] {
-		_mm_storeu_si128(std::bit_cast<__m128i*>(out), val128);
-		out += 2;
-	}
-	if (num64 & 1) [[unlikely]] {
-		out[0] = val64;
-	}
-}
-#endif
-
-static inline void memset_64(
-	uint64_t* out, size_t num64, uint64_t val64)
-{
-	assert((size_t(out) % 8) == 0); // must be 8-byte aligned
-
-#ifdef __SSE2__
-	memset_64_SSE(out, num64, val64);
-	return;
-#endif
-	const uint64_t* e = out + num64 - 3;
-	for (/**/; out < e; out += 4) {
-		out[0] = val64;
-		out[1] = val64;
-		out[2] = val64;
-		out[3] = val64;
-	}
-	if (num64 & 2) [[unlikely]] {
-		out[0] = val64;
-		out[1] = val64;
-		out += 2;
-	}
-	if (num64 & 1) [[unlikely]] {
-		out[0] = val64;
-	}
+	ranges::fill(out, val);
 }
 
-static inline void memset_32_2(
-	uint32_t* out, size_t num32, uint32_t val0, uint32_t val1)
+template<typename Pixel>
+void MemSet2<Pixel>::operator()(std::span<Pixel> out, Pixel val0, Pixel val1) const
 {
-	assert((size_t(out) % 4) == 0); // must be 4-byte aligned
-	if (num32 == 0) [[unlikely]] return;
+	static_assert(std::is_same_v<Pixel, uint32_t>); // (for now) only implemented for 32-bit
 
-	// Align at 8-byte boundary.
-	if (size_t(out) & 4) [[unlikely]] {
-		out[0] = val1; // start at odd pixel
-		++out; --num32;
+	if (out.empty()) [[unlikely]] return;
+
+	// Align at 64-bit boundary.
+	if (std::bit_cast<uintptr_t>(out.data()) & alignof(uint32_t)) [[unlikely]] {
+		out.front() = val1; // start at odd pixel
+		out = out.subspan(1);
 	}
 
+	// Main loop: (aligned) 64-bit fill.
+	std::span<uint64_t> out64{std::bit_cast<uint64_t*>(out.data()), out.size() / 2};
 	uint64_t val64 = Endian::BIG ? (uint64_t(val0) << 32) | val1
 	                             : val0 | (uint64_t(val1) << 32);
-	memset_64(std::bit_cast<uint64_t*>(out), num32 / 2, val64);
+	ranges::fill(out64, val64);
 
-	if (num32 & 1) [[unlikely]] {
-		out[num32 - 1] = val0;
-	}
-}
-
-static inline void memset_32(uint32_t* out, size_t num32, uint32_t val32)
-{
-	assert((size_t(out) % 4) == 0); // must be 4-byte aligned
-
-#if ASM_X86
-#if defined _MSC_VER
-	// VC++'s __stosd intrinsic results in emulator benchmarks
-	// running about 7% faster than with memset_32_2, streaming or not,
-	// and about 3% faster than the C code below.
-	__stosd(std::bit_cast<unsigned long*>(out), val32, num32);
-#else
-	memset_32_2(out, num32, val32, val32);
-#endif
-#else
-	uint32_t* e = out + num32 - 7;
-	for (/**/; out < e; out += 8) {
-		out[0] = val32;
-		out[1] = val32;
-		out[2] = val32;
-		out[3] = val32;
-		out[4] = val32;
-		out[5] = val32;
-		out[6] = val32;
-		out[7] = val32;
-	}
-	if (num32 & 4) [[unlikely]] {
-		out[0] = val32;
-		out[1] = val32;
-		out[2] = val32;
-		out[3] = val32;
-		out += 4;
-	}
-	if (num32 & 2) [[unlikely]] {
-		out[0] = val32;
-		out[1] = val32;
-		out += 2;
-	}
-	if (num32 & 1) [[unlikely]] {
-		out[0] = val32;
-	}
-#endif
-}
-
-template<typename Pixel> void MemSet<Pixel>::operator()(
-	std::span<Pixel> out, Pixel val) const
-{
-	if constexpr (sizeof(Pixel) == 4) {
-		memset_32(std::bit_cast<uint32_t*>(out.data()), out.size(), val);
-	} else {
-		UNREACHABLE;
-	}
-}
-
-template<typename Pixel> void MemSet2<Pixel>::operator()(
-	std::span<Pixel> out, Pixel val0, Pixel val1) const
-{
-	if constexpr (sizeof(Pixel) == 4) {
-		memset_32_2(std::bit_cast<uint32_t*>(out.data()), out.size(), val0, val1);
-	} else {
-		UNREACHABLE;
+	// Single remaining pixel.
+	if (out.size() & 1) [[unlikely]] {
+		out.back() = val0;
 	}
 }
 
