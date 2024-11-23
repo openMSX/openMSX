1b6610ca210e73043b78bd332fe3a9783fd1d695
m9710797 <vermaelen.wouter@gmail.com>
2024-11-23 09:35:19 +0100

    Small tweak on c9533bc1ccd1784
    That commit adds the text "page=n" (with 'n' a digit 0-3). Because 'n'
    can vary we want to show it in a monospace font, otherwise the text that
    follows on the same line can shift around.
    
    I discussed this with David (turbor). Initially we thought it was fine
    to show the full text (page=n) with the same monospace font. Though now
    when I actually see the result, you can notice the difference in font
    between "page=" and the other text (non-digits) on the line.
    
    It's a detail, but let's fix it anyway.

--- src/imgui/ImGuiBitmapViewer.cc
+++ src/imgui/ImGuiBitmapViewer.cc
@@ -286,13 +286,15 @@ void ImGuiBitmapViewer::paint(MSXMotherBoard* motherBoard)
 
 				ImGui::TextUnformatted("x="sv); dec3(x);
 				ImGui::SameLine();
-				ImGui::TextUnformatted("y="sv); dec3(y&255);
+				ImGui::TextUnformatted("y="sv); dec3(y % 256);
 
 				if (bitmapPage == -1) {
-					im::ScopedFont sf(manager.fontMono);
 					ImGui::SameLine();
-					ImGui::Text("page=%1d", y>>8);
-				};
+					ImGui::TextUnformatted("page="sv);
+					im::ScopedFont sf(manager.fontMono);
+					ImGui::SameLine(0.0f, 0.0f);
+					ImGui::StrCat(y / 256);
+				}
 
 				unsigned physAddr = 0x8000 * page + 128 * y;
 				switch (mode) {
