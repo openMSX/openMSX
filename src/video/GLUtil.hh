openMSX/openMSX

commit 3435d2335dfb06eafe90acfa05a405138a666687

Simplify usage of MemBuffer in PixelBuffer

src/video/GLUtil.hh
@@ -200,10 +200,6 @@ private:
 	  */
 	openmsx::MemBuffer<T> allocated;
 
-	/** Available buffer size
-	 */
-	GLuint size = 0;
-
 	/** Handle of the GL buffer, or 0 if no GL buffer is available.
 	  */
 	//GLuint bufferId;
@@ -226,7 +222,6 @@ PixelBuffer<T>::~PixelBuffer()
 template<typename T>
 PixelBuffer<T>::PixelBuffer(PixelBuffer<T>&& other) noexcept
 	: allocated(std::move(other.allocated))
-	, size(other.size)
 	//, bufferId(other.bufferId)
 {
 	other.size = 0;
@@ -237,15 +232,13 @@ template<typename T>
 PixelBuffer<T>& PixelBuffer<T>::operator=(PixelBuffer<T>&& other) noexcept
 {
 	std::swap(allocated, other.allocated);
-	std::swap(size,      other.size);
 	//std::swap(bufferId,  other.bufferId);
 	return *this;
 }
 
 template<typename T>
-void PixelBuffer<T>::allocate(GLuint size_)
+void PixelBuffer<T>::allocate(GLuint size)
 {
-	size = size_;
 	//if (bufferId != 0) {
 	//	bind();
 	//	// TODO make performance hint configurable?
@@ -282,7 +275,7 @@ std::span<T> PixelBuffer<T>::mapWrite()
 	//	return std::bit_cast<T*>(glMapBuffer(
 	//		GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY));
 	//} else {
-		return {allocated.data(), size};
+		return std::span{allocated};
 	//}
 }
 

commit 2fe7d6cde058ea6bce7b7894dcd17b936237d20a

Fix clang compiler error in GLUtil.hh
Introduced in 3435d2335dfb06eafe90acfa05a405138a666687.
This should not have compiled in any compiler, but nevertheless it did
with both gcc, msvc, and (older) clang versions. clang-19 caught it.

The method containing the error was likely never called.

src/video/GLUtil.hh
@@ -224,7 +224,6 @@ PixelBuffer<T>::PixelBuffer(PixelBuffer<T>&& other) noexcept
 	: allocated(std::move(other.allocated))
 	//, bufferId(other.bufferId)
 {
-	other.size = 0;
 	//other.bufferId = 0;
 }
 