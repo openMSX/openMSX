openMSX/openMSX

commit 6f8546be1c29f7496a6cdd0a626fc1b6aeabec8d

Keep formatting of {Break,Watch}Point address
Before this patch the (begin or end) address of a breakpoint/watchpoint
was stored as an integer. And then e.g. for the list command we would
format it as a 4-digit hex number. In the GUI we would already
separately keep track of the original address string as entered by the
user. This patch now also stores the original string in the breakpoint
object itself (and then later patches will remove it from the GUI side).

To convert the string to a number we evaluate it as a Tcl expression.
That means that symbols are supported via the syntax:
    $sym(mySymbolName)
Via the GUI we still allow the simplified syntax of using plain symbol
names. But internally that is then transformed into the above Tcl
expression.

Note that for performance reasons those Tcl expressions are not
constantly being re-evaluated. E.g. an expression like '[reg HL]' to set
a breakpoint on the address where the HL register points to will not
automatically move the breakpoint whenever the HL register changes. It
is _intentionally_ left unspecified when the expressions are being
re-evaluated.

src/cpu/MSXCPUInterface.cc
@@ -977,9 +977,11 @@ void MSXCPUInterface::updateMemWatch(WatchPoint::Type type)
 	}
 	for (const auto& w : watchPoints) {
 		if (w->getType() == type) {
-			unsigned beginAddr = std::min(w->getBeginAddress(), 0xffffu);
-			unsigned endAddr   = std::min(w->getEndAddress(),   0xffffu);
-			for (unsigned addr = beginAddr; addr <= endAddr; ++addr) {
+			auto begin = w->getBeginAddress();
+			auto end = w->getEndAddress();
+			if (!begin || !end) continue;
+			assert(*begin < 0x1000 && *end < 0x1000 && begin <= end);
+			for (unsigned addr = *begin; addr <= *end; ++addr) {
 				watchSet[addr >> CacheLine::BITS].set(
 				         addr  & CacheLine::LOW);
 			}

src/cpu/BreakPoint.hh
@@ -3,16 +3,14 @@
 
 #include "BreakPointBase.hh"
 #include "CommandException.hh"
-#include "openmsx.hh"
 
 #include "strCat.hh"
 
+#include <cstdint>
+#include <optional>
+
 namespace openmsx {
 
-/** Base class for CPU breakpoints.
- *  For performance reasons every bp is associated with exactly one
- *  (immutable) address.
- */
 class BreakPoint final : public BreakPointBase
 {
 public:
@@ -21,27 +19,42 @@ public:
 public:
 	BreakPoint()
 		: id(++lastId) {}
-	BreakPoint(word address_, TclObject command_, TclObject condition_, bool once_)
+	BreakPoint(Interpreter& interp, TclObject address_, TclObject command_, TclObject condition_, bool once_)
 		: BreakPointBase(std::move(command_), std::move(condition_), once_)
 		, id(++lastId)
-		, address(address_) {}
+		, addrStr(std::move(address_))
+	{
+		evaluateAddress(interp);
+	}
 
 	[[nodiscard]] unsigned getId() const { return id; }
 	[[nodiscard]] std::string getIdStr() const { return strCat(prefix, id); }
-	[[nodiscard]] word getAddress() const { return address; }
 
-	void setAddress(Interpreter& interp, const TclObject& a) {
-		// TODO store value with given formatting
-		auto tmp = a.getInt(interp); // may throw
-		if ((tmp < 0) || (tmp > 0xffff)) {
-			throw CommandException("address outside of range: ", a.getString());
+	[[nodiscard]] std::optional<uint16_t> getAddress() const { return address; }
+	[[nodiscard]] TclObject getAddressString() const { return addrStr; }
+	void setAddress(Interpreter& interp, const TclObject& addr) {
+		addrStr = addr;
+		evaluateAddress(interp);
+	}
+
+	std::string evaluateAddress(Interpreter& interp) {
+		try {
+			auto tmp = addrStr.eval(interp).getInt(interp); // may throw
+			if ((tmp < 0) || (tmp > 0xffff)) {
+				throw CommandException("address outside of range");
+			}
+			address = uint16_t(tmp);
+			return {}; // success
+		} catch (MSXException& e) {
+			address = {};
+			return e.getMessage();
 		}
-		address = word(tmp);
 	}
 
 private:
 	unsigned id;
-	word address = 0;
+	TclObject addrStr;
+	std::optional<uint16_t> address = 0;
 
 	static inline unsigned lastId = 0;
 };

src/cpu/WatchPoint.cc
@@ -20,10 +20,11 @@ namespace openmsx {
 
 void WatchPoint::registerIOWatch(MSXMotherBoard& motherBoard, std::span<MSXDevice*, 256> devices)
 {
+	assert(!registered);
 	assert(getType() == one_of(Type::READ_IO, Type::WRITE_IO));
-	unsigned beginPort = std::min(getBeginAddress(), 0xffu);
-	unsigned endPort   = std::min(getEndAddress(),   0xffu);
-	for (unsigned port = beginPort; port <= endPort; ++port) {
+	if (!beginAddr || !endAddr) return;
+	assert(*beginAddr < 0x100 && *endAddr < 0x100 && *beginAddr <= *endAddr);
+	for (unsigned port = *beginAddr; port <= *endAddr; ++port) {
 		// create new MSXWatchIOdevice ...
 		auto& dev = ios.emplace_back(std::make_unique<MSXWatchIODevice>(
 			*motherBoard.getMachineConfig(), *this));
@@ -31,23 +32,26 @@ void WatchPoint::registerIOWatch(MSXMotherBoard& motherBoard, std::span<MSXDevic
 		dev->getDevicePtr() = devices[port];
 		devices[port] = dev.get();
 	}
+	registered = true;
 }
 
 void WatchPoint::unregisterIOWatch(std::span<MSXDevice*, 256> devices)
 {
+	assert(registered);
 	assert(getType() == one_of(Type::READ_IO, Type::WRITE_IO));
-	unsigned beginPort = std::min(getBeginAddress(), 0xffu);
-	unsigned endPort   = std::min(getEndAddress(),   0xffu);
-	for (unsigned port = beginPort; port <= endPort; ++port) {
+	if (!beginAddr || !endAddr) return;
+	assert(*beginAddr < 0x100 && *endAddr < 0x100 && *beginAddr <= *endAddr);
+	for (unsigned port = *beginAddr; port <= *endAddr; ++port) {
 		// find pointer to watchpoint
 		MSXDevice** prev = &devices[port];
-		while (*prev != ios[port - beginPort].get()) {
+		while (*prev != ios[port - *beginAddr].get()) {
 			prev = &checked_cast<MSXWatchIODevice*>(*prev)->getDevicePtr();
 		}
 		// remove watchpoint from chain
 		*prev = checked_cast<MSXWatchIODevice*>(*prev)->getDevicePtr();
 	}
 	ios.clear();
+	registered = false;
 }
 
 void WatchPoint::doReadCallback(MSXMotherBoard& motherBoard, unsigned port)

src/cpu/WatchPoint.hh
@@ -5,12 +5,15 @@
 #include "CommandException.hh"
 #include "MSXMultiDevice.hh"
 
+#include "narrow.hh"
 #include "one_of.hh"
 #include "strCat.hh"
 #include "unreachable.hh"
 
 #include <cassert>
+#include <cstdint>
 #include <memory>
+#include <optional>
 #include <string_view>
 
 namespace openmsx {
@@ -49,52 +52,82 @@ public:
 	{
 		return (type == one_of(Type::READ_IO, Type::WRITE_IO)) ? 0x100 : 0x10000;
 	}
-	static std::pair<unsigned, unsigned> parseAddress(Interpreter& interp, const TclObject& a, Type type)
+	static std::pair<TclObject, TclObject> parseAddress(Interpreter& interp, const TclObject& a)
 	{
-		unsigned begin = a.getListIndex(interp, 0).getInt(interp);
-		unsigned end = (a.getListLength(interp) == 2)
-		             ? a.getListIndex(interp, 1).getInt(interp)
-		             : begin;
-		if (end < begin) {
-			throw CommandException(
-				"Not a valid range: end address may "
-				"not be smaller than begin address.");
+		auto len = a.getListLength(interp);
+		if (len != one_of(1u, 2u)) {
+			throw CommandException("Invalid address: must be a single address, or a begin/end pair");
 		}
-		if (end >= rangeForType(type)) {
-			throw CommandException("Invalid address: out of range");
-		}
+		auto begin = a.getListIndex(interp, 0);
+		auto end = (len == 2) ? a.getListIndex(interp, 1) : TclObject{};
 		return {begin, end};
 	}
 
 public:
 	WatchPoint()
 		: id(++lastId) {}
-	WatchPoint(TclObject command_, TclObject condition_,
-	           Type type_, unsigned beginAddr_, unsigned endAddr_,
-	           bool once_, unsigned newId = -1)
+	WatchPoint(Interpreter& interp, TclObject command_, TclObject condition_,
+	           Type type_, TclObject address, bool once_, unsigned newId = -1)
 		: BreakPointBase(std::move(command_), std::move(condition_), once_)
 		, id((newId == unsigned(-1)) ? ++lastId : newId)
-		, beginAddr(beginAddr_), endAddr(endAddr_), type(type_)
+		, type(type_)
 	{
-		assert(beginAddr <= endAddr);
+		setAddress(interp, address);
+	}
+
+	struct clone_tag {};
+	WatchPoint(clone_tag, const WatchPoint& wp)
+		: BreakPointBase(wp)
+		, id(wp.id), type(wp.type)
+		, beginAddrStr(wp.beginAddrStr), endAddrStr(wp.endAddrStr)
+		, beginAddr(wp.beginAddr), endAddr(wp.endAddr)
+	{
+		assert(ios.empty());
+		assert(!registered);
 	}
 
 	[[nodiscard]] unsigned getId() const { return id; }
 	[[nodiscard]] std::string getIdStr() const { return strCat(prefix, id); }
-	[[nodiscard]] Type     getType()         const { return type; }
-	[[nodiscard]] unsigned getBeginAddress() const { return beginAddr; }
-	[[nodiscard]] unsigned getEndAddress()   const { return endAddr; }
 
-	void registerIOWatch(MSXMotherBoard& motherBoard, std::span<MSXDevice*, 256> devices);
-	void unregisterIOWatch(std::span<MSXDevice*, 256> devices);
-
+	[[nodiscard]] Type getType() const { return type; }
 	void setType(const TclObject& t) {
 		type = parseType(t.getString());
 	}
+
+	[[nodiscard]] auto getBeginAddress() const { return beginAddr; }
+	[[nodiscard]] auto getEndAddress()   const { return endAddr; }
+	[[nodiscard]] auto getBeginAddressString() const { return beginAddrStr; }
+	[[nodiscard]] auto getEndAddressString()   const { return endAddrStr; }
 	void setAddress(Interpreter& interp, const TclObject& a) {
-		// TODO store value with given formatting
-		std::tie(beginAddr, endAddr) = parseAddress(interp, a, type);
+		std::tie(beginAddrStr, endAddrStr) = parseAddress(interp, a);
+		evaluateAddress(interp);
 	}
+
+	std::string evaluateAddress(Interpreter& interp) {
+		assert(!registered);
+		try {
+			auto begin = beginAddrStr.eval(interp).getInt(interp); // may throw
+			auto end = endAddrStr.getString().empty()
+			         ? begin
+				 : endAddrStr.eval(interp).getInt(interp); // may throw
+			if (end < begin) {
+				throw CommandException("begin may not be larger than end: ", begin, " > ", end);
+			}
+			auto max = rangeForType(type);
+			if ((begin < 0) || (end >= int(max))) {
+				throw CommandException("address outside of range [0, ", max , ')');
+			}
+			beginAddr = narrow<uint16_t>(begin);
+			endAddr   = narrow<uint16_t>(end);
+			return {}; // success
+		} catch (MSXException& e) {
+			beginAddr = endAddr = {};
+			return e.getMessage();
+		}
+	}
+
+	void registerIOWatch(MSXMotherBoard& motherBoard, std::span<MSXDevice*, 256> devices);
+	void unregisterIOWatch(std::span<MSXDevice*, 256> devices);
 
 private:
 	void doReadCallback(MSXMotherBoard& motherBoard, unsigned port);
@@ -102,11 +135,14 @@ private:
 
 private:
 	unsigned id;
-	unsigned beginAddr = 0; // begin and end address are inclusive (IOW range = [begin, end])
-	unsigned endAddr = 0;
 	Type type = Type::WRITE_MEM;
+	TclObject beginAddrStr;
+	TclObject endAddrStr;
+	std::optional<uint16_t> beginAddr; // begin and end address are inclusive (IOW range = [begin, end])
+	std::optional<uint16_t> endAddr;
 
 	std::vector<std::unique_ptr<MSXWatchIODevice>> ios;
+	bool registered = false; // for debugging only
 
 	static inline unsigned lastId = 0;
 

src/debugger/Debugger.cc
@@ -154,26 +154,14 @@ void Debugger::removeProbeBreakPoint(ProbeBreakPoint& bp)
 		[](auto& v) { return v.get(); }));
 }
 
-unsigned Debugger::setWatchPoint(TclObject command, TclObject condition,
-                                 WatchPoint::Type type,
-                                 unsigned beginAddr, unsigned endAddr,
-                                 bool once, unsigned newId /*= -1*/)
-{
-	auto wp = std::make_shared<WatchPoint>(
-		std::move(command), std::move(condition), type, beginAddr, endAddr, once, newId);
-	motherBoard.getCPUInterface().setWatchPoint(wp);
-	return wp->getId();
-}
-
 void Debugger::transfer(Debugger& other)
 {
 	// Copy watchpoints to new machine.
-	assert(motherBoard.getCPUInterface().getWatchPoints().empty());
+	auto& cpuInterface = motherBoard.getCPUInterface();
+	assert(cpuInterface.getWatchPoints().empty());
 	for (const auto& wp : other.motherBoard.getCPUInterface().getWatchPoints()) {
-		setWatchPoint(wp->getCommand(),    wp->getCondition(),
-		              wp->getType(),       wp->getBeginAddress(),
-		              wp->getEndAddress(), wp->onlyOnce(),
-		              wp->getId());
+		cpuInterface.setWatchPoint(std::make_shared<WatchPoint>(
+			WatchPoint::clone_tag{}, *wp));
 	}
 
 	// Copy probes to new machine.
@@ -191,6 +179,10 @@ void Debugger::transfer(Debugger& other)
 	// copy those.
 }
 
+Interpreter& Debugger::getInterpreter()
+{
+	return motherBoard.getReactor().getInterpreter();
+}
 
 // class Debugger::Cmd
 
@@ -467,16 +459,12 @@ DebugCondition* Debugger::Cmd::lookupCondition(std::string_view str)
 	return {};
 }
 
-static auto formatAddr(word addr)
-{
-	return tmpStrCat("0x", hex_string<4>(addr));
-}
 static auto formatWpAddr(const WatchPoint& wp)
 {
 	TclObject result;
-	result.addListElement(formatAddr(wp.getBeginAddress()));
-	if (wp.getBeginAddress() != wp.getEndAddress()) {
-		result.addListElement(formatAddr(wp.getEndAddress()));
+	result.addListElement(wp.getBeginAddressString());
+	if (auto end = wp.getEndAddressString(); !end.getString().empty()) {
+		result.addListElement(end);
 	}
 	return result;
 }
@@ -485,7 +473,7 @@ void Debugger::Cmd::breakPointList(std::span<const TclObject> /*tokens*/, TclObj
 {
 	for (const auto& bp : MSXCPUInterface::getBreakPoints()) {
 		TclObject dict = makeTclDict(
-			TclObject("-address"), formatAddr(bp.getAddress()), // TODO keep original format
+			TclObject("-address"), bp.getAddressString(),
 			TclObject("-condition"), bp.getCondition(),
 			TclObject("-command"), bp.getCommand(),
 			TclObject("-once"), bp.onlyOnce());
@@ -692,8 +680,7 @@ void Debugger::Cmd::setBreakPoint(std::span<const TclObject> tokens, TclObject& 
 		condition = arguments[1];
 		[[fallthrough]];
 	case 1: { // address
-		word addr = getAddress(getInterpreter(), arguments[0]);
-		BreakPoint bp(addr, command, condition, once);
+		BreakPoint bp(getInterpreter(), arguments[0], command, condition, once);
 		result = bp.getIdStr();
 		debugger().motherBoard.getCPUInterface().insertBreakPoint(std::move(bp));
 		break;
@@ -739,10 +726,8 @@ void Debugger::Cmd::listBreakPoints(std::span<const TclObject> /*tokens*/, TclOb
 	string res;
 	for (const auto& bp : MSXCPUInterface::getBreakPoints()) {
 		TclObject line = makeTclList(
-			bp.getIdStr(),
-			tmpStrCat("0x", hex_string<4>(bp.getAddress())),
-			bp.getCondition(),
-			bp.getCommand());
+			bp.getIdStr(), bp.getAddressString(),
+			bp.getCondition(), bp.getCommand());
 		strAppend(res, line.getString(), '\n');
 	}
 	result = res;
@@ -754,7 +739,7 @@ void Debugger::Cmd::setWatchPoint(std::span<const TclObject> tokens, TclObject& 
 	checkNumArgs(tokens, AtLeast{4}, Prefix{2}, "type address ?-once? ?condition? ?command?");
 	TclObject command("debug break");
 	TclObject condition;
-	unsigned beginAddr, endAddr;
+	TclObject address;
 	WatchPoint::Type type;
 	bool once = false;
 
@@ -773,14 +758,16 @@ void Debugger::Cmd::setWatchPoint(std::span<const TclObject> tokens, TclObject& 
 		[[fallthrough]];
 	case 2: // address + type
 		type = WatchPoint::parseType(arguments[0].getString());
-		std::tie(beginAddr, endAddr) = WatchPoint::parseAddress(getInterpreter(), arguments[1], type);
+		address = arguments[1];
+		//std::tie(beginAddr, endAddr) = WatchPoint::parseAddress(getInterpreter(), arguments[1], type);
 		break;
 	default:
 		UNREACHABLE;
 	}
-	unsigned id = debugger().setWatchPoint(
-		command, condition, type, beginAddr, endAddr, once);
-	result = tmpStrCat(WatchPoint::prefix, id);
+	auto wp = std::make_shared<WatchPoint>(
+		getInterpreter(), std::move(command), std::move(condition), type, address, once);
+	result = wp->getIdStr();
+	debugger().motherBoard.getCPUInterface().setWatchPoint(std::move(wp));
 }
 
 void Debugger::Cmd::removeWatchPoint(
@@ -809,19 +796,16 @@ void Debugger::Cmd::listWatchPoints(
 	string res;
 	const auto& interface = debugger().motherBoard.getCPUInterface();
 	for (const auto& wp : interface.getWatchPoints()) {
-		TclObject line = makeTclList(wp->getIdStr());
-		line.addListElement(WatchPoint::format(wp->getType()));
-		unsigned beginAddr = wp->getBeginAddress();
-		unsigned endAddr   = wp->getEndAddress();
-		if (beginAddr == endAddr) {
-			line.addListElement(tmpStrCat("0x", hex_string<4>(beginAddr)));
-		} else {
-			line.addListElement(makeTclList(
-				tmpStrCat("0x", hex_string<4>(beginAddr)),
-				tmpStrCat("0x", hex_string<4>(endAddr))));
+		auto address = makeTclList(wp->getBeginAddressString());
+		if (auto end = wp->getEndAddressString(); !end.getString().empty()) {
+			address.addListElement(end);
 		}
-		line.addListElement(wp->getCondition(),
-		                    wp->getCommand());
+		TclObject line = makeTclList(
+			wp->getIdStr(),
+			WatchPoint::format(wp->getType()),
+			address,
+			wp->getCondition(),
+			wp->getCommand());
 		strAppend(res, line.getString(), '\n');
 	}
 	result = res;

src/debugger/Debugger.hh
@@ -43,17 +43,13 @@ public:
 	void unregisterProbe(ProbeBase& probe);
 	[[nodiscard]] ProbeBase* findProbe(std::string_view name);
 
-	unsigned setWatchPoint(TclObject command, TclObject condition,
-	                       WatchPoint::Type type,
-	                       unsigned beginAddr, unsigned endAddr,
-	                       bool once, unsigned newId = -1);
-
 	void removeProbeBreakPoint(ProbeBreakPoint& bp);
 	void setCPU(MSXCPU* cpu_) { cpu = cpu_; }
 
 	void transfer(Debugger& other);
 
 	[[nodiscard]] MSXMotherBoard& getMotherBoard() { return motherBoard; }
+	[[nodiscard]] Interpreter& getInterpreter();
 
 private:
 	[[nodiscard]] Debuggable& getDebuggable(std::string_view name);

src/debugger/SymbolManager.cc
@@ -477,7 +477,7 @@ void SymbolManager::removeAllFiles()
 	refresh();
 }
 
-std::optional<uint16_t> SymbolManager::parseSymbolOrValue(std::string_view str) const
+std::optional<uint16_t> SymbolManager::lookupSymbol(std::string_view str) const
 {
 	// linear search is fine: only used interactively
 	// prefer an exact match
@@ -495,7 +495,14 @@ std::optional<uint16_t> SymbolManager::parseSymbolOrValue(std::string_view str) 
 			return it->value;
 		}
 	}
-	// also not found, then try to parse as a numerical value
+	return {};
+}
+
+std::optional<uint16_t> SymbolManager::parseSymbolOrValue(std::string_view str) const
+{
+	// first try symbol
+	if (auto r = lookupSymbol(str)) return r;
+	// if not found, then try to parse as a numerical value
 	return parseValue<uint16_t>(str);
 }
 

src/debugger/SymbolManager.hh
@@ -86,6 +86,7 @@ public:
 	[[nodiscard]] const auto& getFiles() const { return files; }
 	[[nodiscard]] SymbolFile* findFile(std::string_view filename);
 	[[nodiscard]] std::span<Symbol const * const> lookupValue(uint16_t value);
+	[[nodiscard]] std::optional<uint16_t> lookupSymbol(std::string_view s) const;
 	[[nodiscard]] std::optional<uint16_t> parseSymbolOrValue(std::string_view s) const;
 
 	[[nodiscard]] static std::string getFileFilters();

commit 4bac1ef8513026644e7c12afc2ce4404bcc4bb80

Fix typo in assert (introduced in 6f8546be1c29f)

src/cpu/MSXCPUInterface.cc
@@ -983,7 +983,7 @@ void MSXCPUInterface::updateMemWatch(WatchPoint::Type type)
 			auto begin = w->getBeginAddress();
 			auto end = w->getEndAddress();
 			if (!begin || !end) continue;
-			assert(*begin < 0x1000 && *end < 0x1000 && begin <= end);
+			assert(*begin < 0x10000 && *end < 0x10000 && begin <= end);
 			for (unsigned addr = *begin; addr <= *end; ++addr) {
 				watchSet[addr >> CacheLine::BITS].set(
 				         addr  & CacheLine::LOW);