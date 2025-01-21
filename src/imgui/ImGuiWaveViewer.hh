5a441b23b5ddcd20100c4ca7ca44d15f45ca6ef2
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2025-01-21 23:12:21 +0100

    Add persistency to open/close status of chips in wave viewer.

--- src/imgui/ImGuiWaveViewer.hh
+++ src/imgui/ImGuiWaveViewer.hh
@@ -2,6 +2,7 @@
 #define IMGUI_WAVEVIEWER_HH
 
 #include "ImGuiPart.hh"
+#include <set>
 
 namespace openmsx {
 
@@ -12,6 +13,7 @@ public:
 
 	[[nodiscard]] zstring_view iniName() const override { return "wave-viewer"; }
 	void save(ImGuiTextBuffer& buf) override;
+	void loadStart() override;
 	void loadLine(std::string_view name, zstring_view value) override;
 	void paint(MSXMotherBoard* motherBoard) override;
 
@@ -22,6 +24,8 @@ private:
 	static constexpr auto persistentElements = std::tuple{
 		PersistentElement{"show", &ImGuiWaveViewer::show}
 	};
+
+	std::set<std::string> openChips;
 };
 
 } // namespace openmsx
