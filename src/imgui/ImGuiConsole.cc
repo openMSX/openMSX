openMSX/openMSX

commit a7ff9027212b3802c0241e7877c0d740a4c92a34

ImGuiConsole: workaround missing callback on ESC
When typing in the openMSX console, pressing ESC clears the current
line. Though because of some (to me) unexpected behavior in Dear ImGui
InputText() callback handling, we didn't properly account for this
change. This commit adds a workaround.

src/imgui/ImGuiConsole.cc
@@ -184,12 +184,19 @@ void ImGuiConsole::paint(MSXMotherBoard* /*motherBoard*/)
 
 		ImGuiInputTextFlags flags = ImGuiInputTextFlags_EnterReturnsTrue |
 					ImGuiInputTextFlags_EscapeClearsAll |
-					ImGuiInputTextFlags_CallbackEdit |
+					//ImGuiInputTextFlags_CallbackEdit |
 					ImGuiInputTextFlags_CallbackCompletion |
 					ImGuiInputTextFlags_CallbackHistory;
 		bool enter = false;
 		im::StyleColor(ImGuiCol_Text, 0x00000000, [&]{ // transparent, see HACK below
 			enter = ImGui::InputTextWithHint("##Input", "enter command", &inputBuf, flags, &textEditCallbackStub, this);
+			// Workaround missing 'ImGuiInputTextFlags_CallbackEdit' on ESC.
+			// See: https://github.com/ocornut/imgui/issues/8273
+			if (historyBackupLine != inputBuf) {
+				historyBackupLine = inputBuf;
+				historyPos = -1;
+				colorize(inputBuf);
+			}
 		});
 		if (enter && (prompt != PROMPT_BUSY)) {
 			// print command in output buffer, with prompt prepended
@@ -335,12 +342,12 @@ int ImGuiConsole::textEditCallback(ImGuiInputTextCallbackData* data)
 		}
 		break;
 	}
-	case ImGuiInputTextFlags_CallbackEdit: {
-		historyBackupLine.assign(data->Buf, narrow<size_t>(data->BufTextLen));
-		historyPos = -1;
-		colorize(historyBackupLine);
-		break;
-	}
+	//case ImGuiInputTextFlags_CallbackEdit: {
+	//	historyBackupLine.assign(data->Buf, narrow<size_t>(data->BufTextLen));
+	//	historyPos = -1;
+	//	colorize(historyBackupLine);
+	//	break;
+	//}
 	}
 	return 0;
 }

commit b0fa5d381ab56ecd7125b5c72245503408382602

ImGuiConsole: fix scrolling through console history
Regression introduced in a7ff9027212b38.
But now that we upgraded to Dear ImGui v1.91.7 we can go back to our
original solution.

The underlying problem is was that 'ImGui::IsItemEdited()' also returns
'true' for up/down, while with the old callback approach we could
separate this wrong other edit actions.

src/imgui/ImGuiConsole.cc
@@ -184,16 +184,12 @@ void ImGuiConsole::paint(MSXMotherBoard* /*motherBoard*/)
 
 		ImGuiInputTextFlags flags = ImGuiInputTextFlags_EnterReturnsTrue |
 					ImGuiInputTextFlags_EscapeClearsAll |
+					ImGuiInputTextFlags_CallbackEdit |
 					ImGuiInputTextFlags_CallbackCompletion |
 					ImGuiInputTextFlags_CallbackHistory;
 		bool enter = false;
 		im::StyleColor(ImGuiCol_Text, 0x00000000, [&]{ // transparent, see HACK below
 			enter = ImGui::InputTextWithHint("##Input", "enter command", &inputBuf, flags, &textEditCallbackStub, this);
-			if (ImGui::IsItemEdited()) {
-				historyBackupLine = inputBuf;
-				historyPos = -1;
-				colorize(inputBuf);
-			}
 			if (ImGui::IsItemFocused() && ImGui::IsKeyPressed(ImGuiKey_Escape)) {
 				commandBuffer.clear();
 				prompt = PROMPT_NEW;
@@ -341,6 +337,12 @@ int ImGuiConsole::textEditCallback(ImGuiInputTextCallbackData* data)
 			data->InsertChars(0, historyStr.c_str());
 			colorize(std::string_view{data->Buf, narrow<size_t>(data->BufTextLen)});
 		}
+		break;
+	}
+	case ImGuiInputTextFlags_CallbackEdit: {
+		historyBackupLine.assign(data->Buf, narrow<size_t>(data->BufTextLen));
+		historyPos = -1;
+		colorize(historyBackupLine);
 		break;
 	}
 	}