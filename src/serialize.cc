openMSX/openMSX

commit 4d667f5151890d9d8b3b84d7e047470772153528

Use c++23 resize_and_overwrite in strCat()
and strAppend().

This and the previous few patches use
std::string::resize_and_overwrite() (new in c++23), but I didn't explain
yet what that does:

Before we would have code like:
    std::string myString(size, ' '); // create a string containing 'size' number of spaces
    initialize(myString); // overwrite the content of 'myString'
The 'initialize()' function could e.g. be a C-style function (taking a
pointer to some buffer that will be initialized) or it could be a c++
function like strCat (as in this patch).

So we create a string with an internal buffer of the correct size, then
needlessly initialize that buffer, only to immediately after overwrite
that buffer with new contents.

This pattern can be "solved" via the new (c++23) resize_and_overwrite()
function. The above code then becomes something like:
    std::string myString;
    myString.resize_and_overwrite(size, [](char* buf, size_t max_capacity) {
        auto actualSize = initialize(buf, max_capacity);
        return actualSize; // actualSize is allowed to be smaller than max_capacity
    });
So this also allows to overestimate the required capacity and then only
after initialization report the actual size.

For more details see:
    https://en.cppreference.com/w/cpp/string/basic_string/resize_and_overwrite

src/utils/strCat.hh
@@ -17,6 +17,12 @@
 #include <tuple>
 #include <utility>
 
+// Suppress false positive -Wstringop-overflow warnings in GCC for template instantiations in this header.
+// No push/pop because the warnings come from template instantiations inside this header itself.
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic ignored "-Wstringop-overflow"
+#endif
+
 // strCat and strAppend()
 //
 // Inspired by google's absl::StrCat (similar interface, different implementation).
@@ -674,12 +680,12 @@ template<typename... Ts>
 
 	auto t = std::tuple(strCatImpl::makeConcatUnit(std::forward<Ts>(ts))...);
 	auto size = strCatImpl::calcTotalSize(t);
-	// Ideally we want an uninitialized string with given size, but that's not
-	// yet possible. Though see the following proposal (for c++20):
-	//   www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p1072r0.html
-	std::string result(size, ' ');
-	char* dst = result.data();
-	strCatImpl::copyUnits(dst, t);
+
+	std::string result;
+	result.resize_and_overwrite(size, [&](char* dst, size_t sz) {
+		strCatImpl::copyUnits(dst, t);
+		return sz;
+	});
 	return result;
 }
 
@@ -750,9 +756,12 @@ void strAppend(std::string& result, Ts&& ...ts)
 	auto t = std::tuple(strCatImpl::makeConcatUnit(std::forward<Ts>(ts))...);
 	auto extraSize = strCatImpl::calcTotalSize(t);
 	auto oldSize = result.size();
-	result.append(extraSize, ' '); // see note in strCat() about uninitialized string
-	char* dst = &result[oldSize];
-	strCatImpl::copyUnits(dst, t);
+
+	result.resize_and_overwrite(oldSize + extraSize, [&](char* p, size_t sz) {
+		// p[0..oldSize-1] still/already contains old content
+		strCatImpl::copyUnits(&p[oldSize], t);
+		return sz;
+	});
 }
 
 // Degenerate case

commit f112285713ae8973c5bf16468af8dfdb0d59bdaf

Workaround gcc-12 bug in std::string::resize_and_overwrite()
'david_' reported a bug introduced in commit 4d667f5151890d9d8, after
some investigation it turned out to be a bug in gcc-12 (pre gcc-12.4).
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=104222
Luckily there's a relatively easy workaround.

src/serialize.cc
@@ -118,11 +118,12 @@ void MemInputArchive::load(std::string& s)
 {
 	size_t length;
 	load(length);
-	s.resize_and_overwrite(length, [&](char* dst, size_t n) {
-		if (n) {
-			buffer.read(dst, n);
+	s.resize_and_overwrite(length, [&](char* dst, size_t /*n*/) {
+		//assert(length == n); <-- not true with gcc-12 (bug)
+		if (length) {
+			buffer.read(dst, length);
 		}
-		return n;
+		return length;
 	});
 }
 

src/utils/strCat.hh
@@ -683,9 +683,11 @@ template<typename... Ts>
 	auto size = strCatImpl::calcTotalSize(t);
 
 	std::string result;
-	result.resize_and_overwrite(size, [&](char* dst, size_t sz) {
+	result.resize_and_overwrite(size, [&](char* dst, size_t /*sz*/) {
+		//assert(sz == size) <-- not true with gcc-12 (bug)
+		// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=104222
 		strCatImpl::copyUnits(dst, t);
-		return sz;
+		return size;
 	});
 	return result;
 }
@@ -758,10 +760,12 @@ void strAppend(std::string& result, Ts&& ...ts)
 	auto extraSize = strCatImpl::calcTotalSize(t);
 	auto oldSize = result.size();
 
-	result.resize_and_overwrite(oldSize + extraSize, [&](char* p, size_t sz) {
+	auto newSize = oldSize + extraSize;
+	result.resize_and_overwrite(newSize, [&](char* p, size_t /*sz*/) {
+		//assert(sz == newSize); // see above (gcc-12 bug)
 		// p[0..oldSize-1] still/already contains old content
 		strCatImpl::copyUnits(&p[oldSize], t);
-		return sz;
+		return newSize;
 	});
 }
 