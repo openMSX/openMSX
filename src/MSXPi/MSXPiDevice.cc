36a69294ef66f46ba00043d1fd26313689bb3ed4
U-MSI\roniv <ronivon.costa@gmail.com>
2025-09-03 19:36:38 +0100

    MSXPi: Added fix to compile under Windows; Improved the socket reading thread to avoid polling loop; Changed the I/O allocation in the device to use only two ports: 0x56 and 0x57

--- src/MSXPi/MSXPiDevice.cc
+++ src/MSXPi/MSXPiDevice.cc
@@ -1,7 +1,17 @@
-#include "MSXPiDevice.hh"
-#include <arpa/inet.h>
-#include <unistd.h>
+#include "MSXPi/MSXPiDevice.hh"
+#ifdef _WIN32
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+  #pragma comment(lib, "ws2_32.lib")
+#else
+  #include <arpa/inet.h>
+  #include <unistd.h>
+#endif
+
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <queue>
 
 namespace openmsx {
 
@@ -18,8 +28,11 @@ MSXPiDevice::~MSXPiDevice()
 {
     running = false;
     if (worker.joinable()) worker.join();
+#ifdef _WIN32
+    if (sockfd >= 0) closesocket(sockfd);
+#else
     if (sockfd >= 0) close(sockfd);
-    // std::cout << "[MSXPi] Device destroyed\n";
+#endif
 }
 
 void MSXPiDevice::reset(EmuTime /*time*/)
@@ -50,7 +63,7 @@ byte MSXPiDevice::readIO(uint16_t port, EmuTime /*time*/)
             return status;
         }
 
-        case 0x5A: // DATA
+        case 0x57: // DATA
         {
             if (readRequested && !rxQueue.empty()) {
                 byte val = rxQueue.front();
@@ -87,17 +100,12 @@ void MSXPiDevice::writeIO(uint16_t port, byte value, EmuTime /*time*/)
             }
             break;
 
-        case 0x5A: // DATA
-            if (serverAvailable && sockfd >= 0) {
-                std::lock_guard<std::mutex> lock(mtx);
-                txQueue.push(value);
-                // std::cout << "[MSXPi] Write DATA (0x5A): 0x" << std::hex << int(value)
-                //         << " queued for sending to Python server\n";
-            } else {
-                // std::cout << "[MSXPi] Write DATA (0x5A) ignored, server not connected\n";
-            }
+        case 0x57: // DATA
+			if (serverAvailable && sockfd >= 0) {
+				send(sockfd, reinterpret_cast<const char*>(&value), 1, 0);
+			}
             break;
-
+			
         default:
             break;
     }
@@ -116,7 +124,7 @@ byte MSXPiDevice::peekIO(uint16_t port, EmuTime /*time*/) const
 			return 0x00;
 		}
 
-        case 0x5A: // DATA
+        case 0x57: // DATA
             if (readRequested && !rxQueue.empty()) {
                 return rxQueue.front();
             } else {
@@ -130,71 +138,92 @@ byte MSXPiDevice::peekIO(uint16_t port, EmuTime /*time*/) const
 
 void MSXPiDevice::serverThread()
 {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        // std::cerr << "[MSXPi] WSAStartup failed\n";
+        return;
+    }
+#endif
+
+    // std::cout << "[MSXPi] Server thread started\n";
+
     while (running) {
         if (sockfd < 0) {
+            // std::cout << "[MSXPi] Attempting to create socket...\n";
             sockfd = socket(AF_INET, SOCK_STREAM, 0);
-            if (sockfd < 0) { sleep(1); continue; }
+            if (sockfd < 0) {
+                // std::cerr << "[MSXPi] Failed to create socket\n";
+#ifdef _WIN32
+                Sleep(1000);
+#else
+                sleep(1);
+#endif
+                continue;
+            }
 
             sockaddr_in addr{};
             addr.sin_family = AF_INET;
             addr.sin_port = htons(serverPort);
             inet_pton(AF_INET, serverIP.c_str(), &addr.sin_addr);
 
+            std::cout << "[MSXPi] Attempting to connect to " << serverIP << ":" << serverPort << "...\n";
             if (connect(sockfd, (sockaddr*)&addr, sizeof(addr)) == 0) {
                 serverAvailable = true;
-                // std::cout << "[MSXPi] Python server connected at " << serverIP << ":" << serverPort << "\n";
+                // std::cout << "[MSXPi] Connected to MSXPi Server server\n";
             } else {
+                // std::cerr << "[MSXPi] Connection failed\n";
+#ifdef _WIN32
+                closesocket(sockfd);
+#else
                 close(sockfd);
+#endif
                 sockfd = -1;
                 serverAvailable = false;
-                // std::cout << "[MSXPi] Failed to connect, retrying...\n";
+#ifdef _WIN32
+                Sleep(1000);
+#else
                 sleep(1);
+#endif
                 continue;
             }
         }
 
-        // Send queued DATA bytes
-        while (serverAvailable && !txQueue.empty()) {
-            byte val;
-            {
-                std::lock_guard<std::mutex> lock(mtx);
-                val = txQueue.front();
-                txQueue.pop();
-            }
-            ssize_t sent = send(sockfd, &val, 1, 0);
-            if (sent <= 0) {
-                // std::cout << "[MSXPi] Connection lost while sending\n";
-                close(sockfd);
-                sockfd = -1;
-                serverAvailable = false;
-                break;
-            }
-            // std::cout << "[MSXPi] Sent byte 0x" << std::hex << int(val) << " to Python server\n";
-        }
-
-        // Receive data from Python server
-        byte buf[64];
-        ssize_t rec = recv(sockfd, buf, sizeof(buf), MSG_DONTWAIT);
-        if (rec > 0) {
-            std::lock_guard<std::mutex> lock(mtx);
-            for (ssize_t i = 0; i < rec; i++) {
-                rxQueue.push(buf[i]);
-                // std::cout << "[MSXPi] Received byte 0x" << std::hex << int(buf[i]) << " from Python server\n";
-            }
-        }
-
-        if (rec == 0) {
-            // std::cout << "[MSXPi] Python server closed connection\n";
-            close(sockfd);
-            sockfd = -1;
-            serverAvailable = false;
-        }
-
-        usleep(1000); // 1 ms
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(sockfd, &readfds);
+		
+		timeval timeout{};
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 500000; // 0.5 seconds
+		
+		int activity = select(sockfd + 1, &readfds, nullptr, nullptr, &timeout);
+		if (activity > 0 && FD_ISSET(sockfd, &readfds)) {
+			byte buf[64];
+			ssize_t rec = recv(sockfd, reinterpret_cast<char*>(buf), sizeof(buf), 0);
+			if (rec > 0) {
+				std::lock_guard<std::mutex> lock(mtx);
+				for (ssize_t i = 0; i < rec; ++i) {
+					rxQueue.push(buf[i]);
+					// std::cout << "[MSXPi] Queued byte: 0x" << std::hex << int(buf[i]) << "\n";
+				}
+			} else {
+				// std::cerr << "[MSXPi] Connection closed or error\n";
+		#ifdef _WIN32
+				closesocket(sockfd);
+		#else
+				close(sockfd);
+		#endif
+				sockfd = -1;
+				serverAvailable = false;
+			}
+		}
     }
 
-    if (sockfd >= 0) close(sockfd);
-    serverAvailable = false;
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
     // std::cout << "[MSXPi] Server thread exiting\n";
 }
 
