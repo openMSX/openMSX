openMSX/openMSX

commit 185ec2899b865978626de2e04f4424d13f15cfdf

Rework File::mmap() API
Before the `mmap()` functionality was implemented in the `File` class
itself (more correct in the subclasses of `FileBase`). After this patch
there's still a `File::mmap()` function, but the implementation itself has
moved to a new `MappedFile` class.

The semantics have also changed:
* Before `File::mmap()` returned a `std::span<const uint8_t>`, and the
  `File` object was responsible for managing the lifetime of the
  mapping.
* Now `File::mmap<T>()` has become a templatized function. So the type
  of the mapping must be explicitly given. Typically it will be `const
  uint8_t` as before, but it can also be `const char` (avoiding to need
  for `reinterpret_cast` in the user code). Or it can be mapped
  `non-const` (see documentation in the code for details). Also the
  result is a (new) `MappedFile` object, this is an RAII class that
  manages the lifetime of the mapping. In other words, the mapping is no
  longer tied to the lifetime of the `File` object: the mapping can be
  destroyed both sooner or later than the `File` (possibly freeing
  unneeded resources sooner).

This change also makes the `File` object more light-weight in case the
mmap() functionality isn't needed at all.

Other more internal changes:
* (On unix) we pass the flag `MAP_POPULATE` to mmap(). This is a hint
  that the mapping should be populated immediately, not only lazily once
  the mapped pages are actually being accessed (though I think there are
  also heuristics to perform (some) readahead once pages are accessed).
* The template parameter `T` in `File::mmap<>()` can be a `const` type
  (like `const char`). This will create a `FILE_MAP_READ` (as opposed to
  `FILE_MAP_COPY`) mapping. This makes the mapping read-only at a
  hardware (MMU) level. Not just via the C++ type system.

src/memory/Rom.cc
@@ -187,8 +187,8 @@ void Rom::init(MSXMotherBoard& motherBoard, XMLElement& config,
 				"supported.");
 		}
 		try {
-			auto mmap = file.mmap();
-			rom = mmap;
+			mmap = file.mmap<const uint8_t>();
+			rom = *mmap;
 		} catch (FileException&) {
 			throw MSXException("Error reading ROM image: ", file.getURL());
 		}
@@ -330,6 +330,7 @@ Rom::Rom(Rom&& r) noexcept
 	: rom          (r.rom)
 	, extendedRom  (std::move(r.extendedRom))
 	, file         (std::move(r.file))
+	, mmap         (std::move(r.mmap))
 	, originalSha1 (r.originalSha1)
 	, actualSha1   (r.actualSha1)
 	, name         (std::move(r.name))

src/memory/Rom.hh
@@ -2,6 +2,7 @@
 #define ROM_HH
 
 #include "File.hh"
+
 #include "MemBuffer.hh"
 #include "sha1.hh"
 #include "static_string_view.hh"
@@ -9,6 +10,7 @@
 #include <cassert>
 #include <cstdint>
 #include <memory>
+#include <optional>
 #include <span>
 #include <string>
 #include <string_view>
@@ -62,6 +64,7 @@ private:
 	MemBuffer<uint8_t> extendedRom;
 
 	File file; // can be a closed file
+	std::optional<MappedFile<const uint8_t>> mmap;
 
 	mutable Sha1Sum originalSha1;
 	mutable Sha1Sum actualSha1;

src/cassette/CasImage.cc
@@ -274,10 +274,11 @@ static CasImage::Data convert(std::span<const uint8_t> cas, CassetteImage::FileT
 CasImage::Data CasImage::init(const Filename& filename, FilePool& filePool, CliComm& cliComm)
 {
 	File file(filename);
-	auto cas = file.mmap();
+	auto cas = file.mmap<const uint8_t>();
 
 	auto fileType = CassetteImage::FileType::UNKNOWN;
 	auto result = [&] {
+		// TODO c++23 std::ranges::starts_with()
 		if ((cas.size() >= SVI_CAS::header.size()) &&
 		    (compare(cas.data(), SVI_CAS::header))) {
 			return SVI_CAS::convert(cas, fileType);

src/cassette/TsxImage.cc
@@ -21,7 +21,7 @@ TsxImage::TsxImage(const Filename& filename, FilePool& filePool, CliComm& cliCom
 {
 	File file(filename);
 	try {
-		TsxParser parser(file.mmap());
+		TsxParser parser(file.mmap<const uint8_t>());
 
 		// Move the parsed waveform here
 		output = std::move(parser.stealOutput());

src/debugger/SymbolManager.cc
@@ -391,9 +391,8 @@ template std::optional<uint32_t> SymbolManager::parseValue<uint32_t>(std::string
 
 [[nodiscard]] SymbolFile SymbolManager::loadSymbolFile(const std::string& filename, SymbolFile::Type type, std::optional<uint8_t> slot)
 {
-	File file(filename);
-	auto buf = file.mmap();
-	std::string_view buffer(std::bit_cast<const char*>(buf.data()), buf.size());
+	auto buf = File(filename).mmap<const char>();
+	std::string_view buffer(buf.data(), buf.size());
 
 	using enum SymbolFile::Type;
 	if (type == AUTO_DETECT) {

src/fdc/XSADiskImage.cc
@@ -10,7 +10,8 @@ namespace openmsx {
 XSADiskImage::XSADiskImage(const Filename& filename, File& file)
 	: SectorBasedDisk(DiskName(filename))
 {
-	XSAExtractor extractor(file.mmap());
+	auto mmap = file.mmap<const uint8_t>();
+	XSAExtractor extractor(mmap);
 	data = std::move(extractor).extractData();
 	setNbSectors(data.size());
 }

src/file/CompressedFileAdapter.cc
@@ -1,6 +1,7 @@
 #include "CompressedFileAdapter.hh"
 
 #include "FileException.hh"
+#include "MappedFile.hh"
 
 #include "hash_set.hh"
 #include "ranges.hh"
@@ -73,15 +74,10 @@ void CompressedFileAdapter::write(std::span<const uint8_t> /*buffer*/)
 	throw FileException("Writing to compressed files not yet supported");
 }
 
-std::span<const uint8_t> CompressedFileAdapter::mmap()
+MappedFileImpl CompressedFileAdapter::mmap(bool is_const)
 {
 	decompress();
-	return std::span{decompressed->buf};
-}
-
-void CompressedFileAdapter::munmap()
-{
-	// nothing
+	return {std::span{decompressed->buf}, is_const};
 }
 
 size_t CompressedFileAdapter::getSize()

src/file/CompressedFileAdapter.hh
@@ -20,8 +20,7 @@ public:
 
 	void read(std::span<uint8_t> buffer) final;
 	void write(std::span<const uint8_t> buffer) final;
-	[[nodiscard]] std::span<const uint8_t> mmap() final;
-	void munmap() final;
+	[[nodiscard]] MappedFileImpl mmap(bool is_const) final;
 	[[nodiscard]] size_t getSize() final;
 	void seek(size_t pos) final;
 	[[nodiscard]] size_t getPos() final;

commit f58e38d86c77472c01d117cad60e0804c935e63a

mmap() .rom files as 'non-const'
To allow for IPS-patches. In the c++ code we lie and say this is 'const'
data. But then later for IPS patches we cast away this 'const'. That's
fine when the underlying memory is in fact also writable. But if that
memory was really read-only (because it's mmap()ed as read-only) then
trying to write triggers an MMU protection error.

Got recently broken in commit 185ec2899b865978. At least it shows that
mapping read-only does work ;-)

src/memory/Rom.cc
@@ -187,7 +187,7 @@ void Rom::init(MSXMotherBoard& motherBoard, XMLElement& config,
 				"supported.");
 		}
 		try {
-			mmap = file.mmap<const uint8_t>();
+			mmap = file.mmap<uint8_t>();
 			rom = *mmap;
 		} catch (FileException&) {
 			throw MSXException("Error reading ROM image: ", file.getURL());

src/memory/Rom.hh
@@ -64,7 +64,7 @@ private:
 	MemBuffer<uint8_t> extendedRom;
 
 	File file; // can be a closed file
-	std::optional<MappedFile<const uint8_t>> mmap;
+	std::optional<MappedFile<uint8_t>> mmap; // non-const to allow patching
 
 	mutable Sha1Sum originalSha1;
 	mutable Sha1Sum actualSha1;