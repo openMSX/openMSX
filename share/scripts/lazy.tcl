286723df0ffa10a7ffabf963231f93422e5132b1
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-10-06 16:39:09 -0300

    tcl_bridge: allow guest machine to execute Tcl commands (#1985)

--- share/scripts/lazy.tcl
+++ share/scripts/lazy.tcl
@@ -82,3 +82,4 @@ register_lazy "_vdp_access_test.tcl" toggle_vdp_access_test
 register_lazy "_vdp_busy.tcl" toggle_vdp_busy
 register_lazy "_vdrive.tcl" vdrive
 register_lazy "_vgmrecorder.tcl" {vgm_rec vgm_rec_next vgm_rec_end}
+register_lazy "_tcl_bridge.tcl" tcl_bridge
