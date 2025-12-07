openMSX/openMSX

commit 6174c91897abfd57fba3cfed319976650d62aaa6

ImGuiCharacter: implement override color table
See:
    Add an option to highlight patterns in tile viewer #2044
    https://github.com/openMSX/openMSX/issues/2044

src/imgui/ImGuiCharacter.cc
@@ -286,6 +286,25 @@ void ImGuiCharacter::paint(MSXMotherBoard* motherBoard)
 				});
 				ImGui::Checkbox("Name table overlay", &nameTableOverlay);
 
+				ImGui::Checkbox("Override color table", &overrideColorTable);
+				HelpMarker("Ignore the content of the color table and instead use this value for everything.\n"
+				           "Could be useful to visualize the patterns when the color table has the same value for fg and bg.");
+				const char* col0_15 = "0\0001\0002\0003\0004\0005\0006\0007\000"
+				                      "8\0009\00010\00011\00012\00013\00014\00015\000";
+				im::DisabledIndent(!overrideColorTable, [&]{
+					int fg = (overrideColorValue >> 4) & 0x0f;
+					int bg = (overrideColorValue >> 0) & 0x0f;
+					ImGui::SetNextItemWidth(ImGui::GetFontSize() * 3.0f);
+					if (ImGui::Combo("fg", &fg, col0_15)) {
+						overrideColorValue = (fg << 4) | bg;
+					}
+					ImGui::SetNextItemWidth(ImGui::GetFontSize() * 3.0f);
+					ImGui::SameLine();
+					if (ImGui::Combo("bg", &bg, col0_15)) {
+						overrideColorValue = (fg << 4) | bg;
+					}
+				});
+
 				ImGui::Separator();
 				ImGui::Checkbox("beam", &rasterBeam);
 				ImGui::SameLine();
@@ -317,6 +336,13 @@ void ImGuiCharacter::paint(MSXMotherBoard* motherBoard)
 		VramTable colTable(vram);
 		unsigned colReg = (manCol ? (manualColBase | (colMult(manualMode) - 1)) : vdp->getColorTableBase()) >> 6;
 		colTable.setRegister(colReg, 6);
+
+		VramTable colTable2 = colTable; // possible override
+		auto colorTabVal = uint8_t(overrideColorValue);
+		if (overrideColorTable) {
+			colTable2 = VramTable(std::span(&colorTabVal, 1));
+			colTable2.setRegister(0, 0);
+		}
 
 		VramTable namTable(vram);
 		unsigned namReg = (manNam ? (manualNamBase | (namMult(manualMode) - 1)) : vdp->getNameTableBase()) >> 10;
@@ -361,7 +387,7 @@ void ImGuiCharacter::paint(MSXMotherBoard* motherBoard)
 			return {256,  64}; // SCR1, OTHER
 		}();
 		std::array<uint32_t, 256 * 256> pixels; // max size for SCR2
-		renderPatterns(mode, palette, fgCol, bgCol, fgBlink, bgBlink, patTable, colTable, lines, pixels);
+		renderPatterns(mode, palette, fgCol, bgCol, fgBlink, bgBlink, patTable, colTable2, lines, pixels);
 		if (!patternTex.get()) {
 			patternTex = gl::Texture(false, false); // no interpolation, no wrapping
 		}

src/imgui/ImGuiCharacter.hh
@@ -33,6 +33,7 @@ private:
 	std::string title;
 
 	int zoom = 0; // 0->1x, 1->2x, ..., 7->8x
+	int overrideColorValue = 0xf1;
 	bool overrideAll     = false;
 	bool overrideMode    = false;
 	bool overrideFgCol   = false;
@@ -47,6 +48,7 @@ private:
 	bool overrideColor0  = false;
 	bool grid = true;
 	bool nameTableOverlay = false;
+	bool overrideColorTable = false;
 	bool rasterBeam = false;
 	gl::vec4 gridColor{0.0f, 0.0f, 0.0f, 0.5f}; // RGBA
 	gl::vec4 rasterBeamColor{1.0f, 0.0f, 0.0f, 0.8f}; // RGBA
@@ -86,6 +88,8 @@ private:
 		PersistentElementMax{"zoom",            &ImGuiCharacter::zoom, 8},
 		PersistentElement   {"showGrid",        &ImGuiCharacter::grid},
 		PersistentElement   {"overlay",         &ImGuiCharacter::nameTableOverlay},
+		PersistentElement   {"overrideColorTable", &ImGuiCharacter::overrideColorTable},
+		PersistentElement   {"overrideColorValue", &ImGuiCharacter::overrideColorValue},
 		PersistentElement   {"showRasterBeam",  &ImGuiCharacter::rasterBeam},
 		PersistentElement   {"rasterBeamColor", &ImGuiCharacter::rasterBeamColor},
 		PersistentElement   {"gridColor",       &ImGuiCharacter::gridColor},

commit 9aab65445144dacf7488afa080bba8eeed2a9233

Fix issue #2050 Color address not shown correctly in debugger tile view
See:
    https://github.com/openMSX/openMSX/issues/2050

This got broken in revision 6174c91897ab while implementing
    https://github.com/openMSX/openMSX/issues/2044
Internally the tile viewer then kept two color tables (I mean the
'VramTable' class that implements the VDP table address calculations),
one used for rendering (which can be overridden) and one to show the
color address. Though because of that we no longer correctly called
'setIndex' on that 2nd table, and because of that the address was shown
as-if you always selected address 0x00000 for the color table. The
rendering was still correct because that uses a different table.

I fixed it by going back to a single table. A consequence is that now
when the 'override color tabel' option is active, the color address is
always shown as 0x0000-0x0007. But that's probably OK.

src/imgui/ImGuiCharacter.cc
@@ -337,11 +337,10 @@ void ImGuiCharacter::paint(MSXMotherBoard* motherBoard)
 		unsigned colReg = (manCol ? (manualColBase | (colMult(manualMode) - 1)) : vdp->getColorTableBase()) >> 6;
 		colTable.setRegister(colReg, 6);
 
-		VramTable colTable2 = colTable; // possible override
 		auto colorTabVal = uint8_t(overrideColorValue);
 		if (overrideColorTable) {
-			colTable2 = VramTable(std::span(&colorTabVal, 1));
-			colTable2.setRegister(0, 0);
+			colTable = VramTable(std::span(&colorTabVal, 1));
+			colTable.setRegister(0, 0);
 		}
 
 		VramTable namTable(vram);
@@ -387,7 +386,7 @@ void ImGuiCharacter::paint(MSXMotherBoard* motherBoard)
 			return {256,  64}; // SCR1, OTHER
 		}();
 		std::array<uint32_t, 256 * 256> pixels; // max size for SCR2
-		renderPatterns(mode, palette, fgCol, bgCol, fgBlink, bgBlink, patTable, colTable2, lines, pixels);
+		renderPatterns(mode, palette, fgCol, bgCol, fgBlink, bgBlink, patTable, colTable, lines, pixels);
 		if (!patternTex.get()) {
 			patternTex = gl::Texture(false, false); // no interpolation, no wrapping
 		}