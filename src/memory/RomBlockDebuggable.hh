faa9549b4273d8acbb2c8a8d4e9bed4065c6146e
m9710797 <vermaelen.wouter@gmail.com>
2026-04-14 18:27:38 +0200

    Implemented 'romblock16' debuggable
    Inspired by pvmm's version:
        https://github.com/openMSX/openMSX/pull/2103
    But heavily simplified.

--- src/memory/RomBlockDebuggable.hh
+++ src/memory/RomBlockDebuggable.hh
@@ -4,33 +4,53 @@
 #include "MSXDevice.hh"
 #include "SimpleDebuggable.hh"
 
+#include "outer.hh"
+
 #include <span>
-#include <string>
 
 namespace openmsx {
 
-class RomBlockDebuggableBase : public SimpleDebuggable
+class RomBlockDebuggableBase
 {
 public:
 	explicit RomBlockDebuggableBase(const MSXDevice& device)
-		: SimpleDebuggable(
-			device.getMotherBoard(),
-			device.getName() + " romblocks",
-		        "Shows for each byte of the mapper which memory block is selected.",
-		        0x10000)
-	{
-	}
-
-	// For the 'Debuggable' interface we need to have an 8-bit read(). For most mappers that's sufficient.
-	[[nodiscard]] byte read(unsigned address) override {
-		return narrow_cast<byte>(readExt(address));
-	}
+		: debuggable8 (device)
+		, debuggable16(device) {}
 
 	// To support larger than 8 bit segment numbers.
 	[[nodiscard]] virtual unsigned readExt(unsigned address) = 0;
 
 protected:
 	~RomBlockDebuggableBase() = default;
+
+private:
+	struct Debuggable8 final : SimpleDebuggable {
+                explicit Debuggable8(const MSXDevice& device)
+			: SimpleDebuggable(device.getMotherBoard(),
+			                   device.getName() + " romblocks",
+			                   "Shows for each byte of the mapper which memory block is selected.",
+			                   0x10000) {}
+
+		[[nodiscard]] uint8_t read(unsigned address) override {
+			auto& outer = OUTER(RomBlockDebuggableBase, debuggable8);
+			return narrow_cast<uint8_t>(outer.readExt(address));
+		}
+	} debuggable8;
+
+	struct Debuggable16 final : SimpleDebuggable {
+		explicit Debuggable16(const MSXDevice& device)
+			: SimpleDebuggable(device.getMotherBoard(),
+			                   device.getName() + " romblocks16",
+			                   "Shows for each byte of the mapper which memory block is selected. "
+			                   "16-bit version, bits 0-7 on even / bits 8-15 on odd addresses.",
+			                   0x10000) {}
+
+		[[nodiscard]] uint8_t read(unsigned address) override {
+			auto& outer = OUTER(RomBlockDebuggableBase, debuggable16);
+			auto full = outer.readExt(address);
+			return narrow_cast<uint8_t>((address & 1) ? (full >> 8) : (full & 0xff));
+		}
+	} debuggable16;
 };
 
 // Generic implementation. This (only) works when the segment numbers are stored
@@ -38,7 +58,7 @@ protected:
 class RomBlockDebuggable final : public RomBlockDebuggableBase
 {
 public:
-	RomBlockDebuggable(const MSXDevice& device, std::span<const byte> blockNr_,
+	RomBlockDebuggable(const MSXDevice& device, std::span<const uint8_t> blockNr_,
 	                   unsigned startAddress_, unsigned mappedSize_,
 	                   unsigned bankSizeShift_, unsigned debugShift_ = 0)
 		: RomBlockDebuggableBase(device)
@@ -48,7 +68,7 @@ public:
 	{
 		assert((mappedSize >> bankSizeShift) == blockNr_.size()); // no need to include 'debugMask' here
 	}
-	RomBlockDebuggable(const MSXDevice& device, std::span<const byte> blockNr_,
+	RomBlockDebuggable(const MSXDevice& device, std::span<const uint8_t> blockNr_,
 	                   unsigned startAddress_, unsigned mappedSize_,
 	                   unsigned bankSizeShift_, unsigned debugShift_,
 	                   unsigned debugMask_)
@@ -64,7 +84,7 @@ public:
 	{
 		unsigned addr = address - startAddress;
 		if (addr < mappedSize) {
-			byte tmp = blockNr[(addr >> bankSizeShift) & debugMask];
+			uint8_t tmp = blockNr[(addr >> bankSizeShift) & debugMask];
 			return (tmp != 255) ? (tmp >> debugShift) : tmp;
 		} else {
 			return unsigned(-1); // outside mapped address space
@@ -72,7 +92,7 @@ public:
 	}
 
 private:
-	const byte* blockNr;
+	const uint8_t* blockNr;
 	const unsigned startAddress;
 	const unsigned mappedSize;
 	const unsigned bankSizeShift;
