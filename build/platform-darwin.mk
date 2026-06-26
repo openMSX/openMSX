5b1ba9690bf9778139de9f3bfbd82df62d30ff86
Laurens Holst <laurens@grauw.nl>
2026-06-26 02:51:19 +0200

    platform-darwin: No longer explicitly specify stdlib.
    When targeting OS X 10.9 (2014) or later libc++ is the default.
    
    Removing because Clang warns when this is passed while compiling C code.

--- build/platform-darwin.mk
+++ build/platform-darwin.mk
@@ -23,16 +23,14 @@ LIBRARYEXT:=.so
 # In theory it is possible to compile against an OS X version number lower
 # than the SDK version number, but in practice this doesn't seem to work
 # since libraries such as libxml2 can change soname between OS X versions.
-# Clang as shipped with Xcode requires OS X 10.7 or higher for compiling with
-# libc++, when compiling Clang and libc++ from source 10.6 works as well.
+# For OS X 10.9 or later Clang uses libc++ by default.
 #
 # Version 13.3 required for std::format
 OSX_VER:=13.3
 TARGET_FLAGS+=-mmacosx-version-min=$(OSX_VER)
 
-# Select Clang as the compiler and libc++ as the standard library.
+# Select Clang as the compiler.
 CXX:=clang++
-TARGET_FLAGS+=-stdlib=libc++
 
 # Enable automatic reference counting in Objective-C
 ifneq ($(3RDPARTY_FLAG),true)
