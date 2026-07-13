openMSX/openMSX

commit d24d37d66acb1c4b808310dab5fbe6be6907c9ec

[8/10] Switch keyboard events to SDL_Event
This patch converts our keyboards events to use SDL_Event internally.
Similarly to what was done before for joystick and mouse events.

This patch is the largest in the series because simultaneously with
converting the event, we needed to change how key-presses are
represented:
* SDL2 has 'SDL_Keycode' and 'SDL_Scancode' these represent a single key
  without modifiers. It also has 'SDL_Keysym' which is the combination
  of a keycode and a scancode plus modifiers. SDL2 offers functions to
  convert SDL_Keycode and SDL_Scancode to and from a string, but there
  are no such functions for 'SDL_Keysym'.
* In openMSX we had a 'Keycode' class which is more or less the
  combination of a SDL_Keycode plus modifiers plus press/release status
  (and we also abused it for scancodes). We had to/from string functions
  that do include the modifiers and press/release status.

For the MSX keyboard code (for the character mode) we need the
combination of unicode and keycode. That combination was present in
SDL1, but SDL2 splits that information in two different event types:
key-events and text-events. For the vast majority of the applications
that's a better approach, unfortunately not for openMSX. In openMSX we
have some heuristic that tries to reconstruct a SDL1-like key event
(thus including unicode info) by combining key- and text-events.

This patch introduces a new class 'SDLKey' as a replacement for the old
'KeyCode' class. SDLKey is the combination of a SDL_Keysym (so
key+scancode plus modifiers) plus a boolean to indicate press or
release. In addition we abuse an unused field in the SDL_Keysym struct
(a field with the name 'unsued') to store (reconstructed) unicode
information.

Together with this new 'SDLKey' class this patch introduces fromString()
and toString() functions that work in a backwards-compatible way (I mean
with the previous to/from string functions in openMSX). See comments in

src/events/SDLKey.hh
@@ -1,0 +1,39 @@
+#ifndef SDLKEY_HH
+#define SDLKEY_HH
+
+#include <SDL.h>
+#include <optional>
+#include <string>
+#include <string_view>
+
+namespace openmsx {
+
+// The combination of:
+//   SDL_Keysym: to indicate a specific key (key+scancode) and modifiers
+//   bool:       to indicate up/down
+struct SDLKey {
+	SDL_Keysym sym;
+	bool down; // down=press / up=release
+
+	[[nodiscard]] static SDLKey create(SDL_Keycode code, bool down) {
+		SDL_Keysym sym;
+		sym.scancode = SDL_SCANCODE_UNKNOWN;
+		sym.sym = code;
+		sym.mod = 0;
+		sym.unused = 0; // unicode
+		return {sym, down};
+	}
+
+	[[nodiscard]] static SDLKey createDown(SDL_Keycode code) {
+		return create(code, true);
+	}
+
+	[[nodiscard]] static std::optional<SDLKey> fromString(std::string_view name);
+
+	// only uses the 'sym.sym', 'sym.mod' and 'down' fields (ignores sym.scancode and 'sym.unicode')
+	[[nodiscard]] std::string toString() const;
+};
+
+} // namespace openmsx
+
+#endif

src/unittest/Keys_test.cc
@@ -1,5 +1,5 @@
 #include "catch.hpp"
-#include "Keys.hh"
+#include "SDLKey.hh"
 
 using namespace openmsx::Keys;
 

build/msvc/openmsx.vcxproj
@@ -387,7 +387,7 @@
     <ClCompile Include="$(OpenMSXSrcDir)\events\HotKey.cc" />
     <ClCompile Include="$(OpenMSXSrcDir)\events\InputEventFactory.cc" />
     <ClCompile Include="$(OpenMSXSrcDir)\events\InputEventGenerator.cc" />
-    <ClCompile Include="$(OpenMSXSrcDir)\events\Keys.cc" />
+    <ClCompile Include="$(OpenMSXSrcDir)\events\SDLKey.cc" />
     <ClCompile Include="$(OpenMSXSrcDir)\events\MSXCliComm.cc" />
     <ClCompile Include="$(OpenMSXSrcDir)\events\Socket.cc" />
     <ClCompile Include="$(OpenMSXSrcDir)\events\StdioMessages.cc" />
@@ -943,7 +943,7 @@
     <None Include="$(OpenMSXSrcDir)\events\HotKey.hh" />
     <None Include="$(OpenMSXSrcDir)\events\InputEventFactory.hh" />
     <None Include="$(OpenMSXSrcDir)\events\InputEventGenerator.hh" />
-    <None Include="$(OpenMSXSrcDir)\events\Keys.hh" />
+    <None Include="$(OpenMSXSrcDir)\events\SDLKey.hh" />
     <None Include="$(OpenMSXSrcDir)\events\MSXCliComm.hh" />
     <None Include="$(OpenMSXSrcDir)\events\Socket.hh" />
     <None Include="$(OpenMSXSrcDir)\events\TclCallbackMessages.hh" />

build/msvc/openmsx.vcxproj.filters
@@ -300,7 +300,7 @@
     <ClCompile Include="$(OpenMSXSrcDir)\events\InputEventGenerator.cc">
       <Filter>events</Filter>
     </ClCompile>
-    <ClCompile Include="$(OpenMSXSrcDir)\events\Keys.cc">
+    <ClCompile Include="$(OpenMSXSrcDir)\events\SDLKey.cc">
       <Filter>events</Filter>
     </ClCompile>
     <ClCompile Include="$(OpenMSXSrcDir)\events\MSXCliComm.cc">
@@ -1726,7 +1726,7 @@
     <None Include="$(OpenMSXSrcDir)\events\InputEventGenerator.hh">
       <Filter>events</Filter>
     </None>
-    <None Include="$(OpenMSXSrcDir)\events\Keys.hh">
+    <None Include="$(OpenMSXSrcDir)\events\SDLKey.hh">
       <Filter>events</Filter>
     </None>
     <None Include="$(OpenMSXSrcDir)\events\MSXCliComm.hh">

src/SG1000Pause.cc
@@ -1,6 +1,6 @@
 #include "SG1000Pause.hh"
 #include "Event.hh"
-#include "Keys.hh"
+#include "SDLKey.hh"
 #include "MSXCPU.hh"
 #include "MSXEventDistributor.hh"
 #include "MSXMotherBoard.hh"
@@ -25,7 +25,7 @@ void SG1000Pause::signalMSXEvent(const Event& event, EmuTime::param time) noexce
 {
 	visit(overloaded{
 		[&](const KeyDownEvent& keyEvent) {
-			if (keyEvent.getKeyCode() == Keys::K_F5) {
+			if (keyEvent.getKeyCode() == SDLK_F5) {
 				setSyncPoint(time);
 			}
 		},

src/events/Event.cc
@@ -8,16 +8,30 @@ using namespace std::literals;
 
 namespace openmsx {
 
+[[nodiscard]] static constexpr uint16_t normalizeKeyMod(uint16_t m)
+{
+	// when either left or right modifier is pressed, add the other one as well
+	if (m & KMOD_SHIFT) m |= KMOD_SHIFT;
+	if (m & KMOD_CTRL)  m |= KMOD_CTRL;
+	if (m & KMOD_ALT)   m |= KMOD_ALT;
+	if (m & KMOD_GUI)   m |= KMOD_GUI;
+	// ignore stuff like: KMOD_NUM, KMOD_CAPS, KMOD_SCROLL
+	m &= (KMOD_SHIFT | KMOD_CTRL | KMOD_ALT | KMOD_GUI | KMOD_MODE);
+	return m;
+}
+
 bool operator==(const Event& x, const Event& y)
 {
 	return std::visit(overloaded{
 		[](const KeyUpEvent& a, const KeyUpEvent& b) {
 			// note: don't compare scancode, unicode
-			return a.getKeyCode() == b.getKeyCode();
+			return std::tuple(a.getKeyCode(), normalizeKeyMod(a.getModifiers())) ==
+			       std::tuple(b.getKeyCode(), normalizeKeyMod(b.getModifiers()));
 		},
 		[](const KeyDownEvent& a, const KeyDownEvent& b) {
 			// note: don't compare scancode, unicode
-			return a.getKeyCode() == b.getKeyCode();
+			return std::tuple(a.getKeyCode(), normalizeKeyMod(a.getModifiers())) ==
+			       std::tuple(b.getKeyCode(), normalizeKeyMod(b.getModifiers()));
 		},
 		[](const MouseMotionEvent& a, const MouseMotionEvent& b) {
 			return std::tuple(a.getX(), a.getY(), a.getAbsX(), a.getAbsY()) ==
@@ -103,7 +117,7 @@ TclObject toTclList(const Event& event)
 			//
 			// Within these constraints it's fine to ignore 'scanCode' in this
 			// method.
-			auto result = makeTclList("keyb", Keys::getName(e.getKeyCode()));
+			auto result = makeTclList("keyb", e.getKey().toString());
 			if (e.getUnicode() != 0) {
 				result.addListElement(tmpStrCat("unicode", e.getUnicode()));
 			}

src/events/Event.hh
@@ -2,7 +2,7 @@
 #define EVENT_HH
 
 #include "ObjectPool.hh"
-#include "Keys.hh"
+#include "SDLKey.hh"
 #include "static_vector.hh"
 #include "StringStorage.hh"
 #include "stl.hh"
@@ -38,58 +38,66 @@ protected:
 	SDL_Event evt;
 };
 
-class TimedEvent : public EventBase
+
+class KeyEvent : public SdlEvent
 {
 public:
-	TimedEvent(uint32_t timestamp_) : timestamp(timestamp_) {}
-	[[nodiscard]] uint32_t getTimestamp() const { return timestamp; }
-
-private:
-	uint32_t timestamp;
-};
-
-
-class KeyEvent : public TimedEvent
-{
-public:
-	[[nodiscard]] Keys::KeyCode getKeyCode() const { return keyCode; }
-	[[nodiscard]] Keys::KeyCode getScanCode() const { return scanCode; }
-	[[nodiscard]] uint32_t getUnicode() const { return unicode; }
+	[[nodiscard]] SDL_Keycode  getKeyCode()   const { return evt.key.keysym.sym; }
+	[[nodiscard]] SDL_Scancode getScanCode()  const { return evt.key.keysym.scancode; }
+	[[nodiscard]] uint16_t     getModifiers() const { return evt.key.keysym.mod; }
+	[[nodiscard]] uint32_t     getUnicode()   const {
+		// HACK: repurposed 'unused' field as 'unicode' field
+		return evt.key.keysym.unused;
+	}
+	[[nodiscard]] SDLKey getKey() const { return SDLKey{evt.key.keysym, evt.type == SDL_KEYDOWN}; }
 
 protected:
-	KeyEvent(uint32_t timestamp_, Keys::KeyCode keyCode_, Keys::KeyCode scanCode_, uint32_t unicode_)
-		: TimedEvent(timestamp_), keyCode(keyCode_), scanCode(scanCode_), unicode(unicode_) {}
-
-private:
-	Keys::KeyCode keyCode;
-	Keys::KeyCode scanCode; // 2nd class support, see comments in toTclList()
-	uint32_t unicode;
+	KeyEvent(const SDL_Event& e) : SdlEvent(e) {}
 };
 
 class KeyUpEvent final : public KeyEvent
 {
 public:
-	explicit KeyUpEvent(uint32_t timestamp_, Keys::KeyCode keyCode_)
-		: KeyUpEvent(timestamp_, keyCode_, keyCode_) {}
+	explicit KeyUpEvent(const SDL_Event& e) : KeyEvent(e) {}
 
-	explicit KeyUpEvent(uint32_t timestamp_, Keys::KeyCode keyCode_, Keys::KeyCode scanCode_)
-		: KeyEvent(timestamp_, keyCode_, scanCode_, 0) {}
+	[[nodiscard]] static KeyUpEvent create(SDL_Keycode code, SDL_Keymod mod = KMOD_NONE) {
+		SDL_Event evt = {};
+		SDL_KeyboardEvent& e = evt.key;
+		e.type = SDL_KEYUP;
+		e.timestamp = SDL_GetTicks();
+		e.state = SDL_RELEASED;
+		e.keysym.sym = code;
+		e.keysym.mod = mod;
+		return KeyUpEvent(evt);
+	}
 };
 
 class KeyDownEvent final : public KeyEvent
 {
 public:
-	explicit KeyDownEvent(uint32_t timestamp_, Keys::KeyCode keyCode_)
-		: KeyDownEvent(timestamp_, keyCode_, keyCode_, 0) {}
+	explicit KeyDownEvent(const SDL_Event& e) : KeyEvent(e) {}
 
-	explicit KeyDownEvent(uint32_t timestamp_, Keys::KeyCode keyCode_, Keys::KeyCode scanCode_)
-		: KeyDownEvent(timestamp_, keyCode_, scanCode_, 0) {}
-
-	explicit KeyDownEvent(uint32_t timestamp_, Keys::KeyCode keyCode_, uint32_t unicode_)
-		: KeyDownEvent(timestamp_, keyCode_, keyCode_, unicode_) {}
-
-	explicit KeyDownEvent(uint32_t timestamp_, Keys::KeyCode keyCode_, Keys::KeyCode scanCode_, uint32_t unicode_)
-		: KeyEvent(timestamp_, keyCode_, scanCode_, unicode_) {}
+	[[nodiscard]] static KeyDownEvent create(SDL_Keycode code, SDL_Keymod mod = KMOD_NONE) {
+		SDL_Event evt = {};
+		SDL_KeyboardEvent& e = evt.key;
+		e.type = SDL_KEYDOWN;
+		e.timestamp = SDL_GetTicks();
+		e.state = SDL_PRESSED;
+		e.keysym.sym = code;
+		e.keysym.mod = mod;
+		return KeyDownEvent(evt);
+	}
+	[[nodiscard]] static KeyDownEvent create(uint32_t timestamp, unsigned unicode) {
+		SDL_Event evt = {};
+		SDL_KeyboardEvent& e = evt.key;
+		e.type = SDL_KEYDOWN;
+		e.timestamp = timestamp;
+		e.state = SDL_PRESSED;
+		e.keysym.sym = SDLK_UNKNOWN;
+		e.keysym.mod = KMOD_NONE;
+		e.keysym.unused = unicode;
+		return KeyDownEvent(evt);
+	}
 };
 
 
@@ -503,6 +511,8 @@ struct GetIfEventHelper<SdlEvent> { // extension for base-classes
 	const SdlEvent* operator()(const Event& event) {
 		const auto& var = event;
 		switch (EventType(var.index())) {
+		case EventType::KEY_UP:              return &std::get<KeyUpEvent>(var);
+		case EventType::KEY_DOWN:            return &std::get<KeyDownEvent>(var);
 		case EventType::MOUSE_BUTTON_UP:     return &std::get<MouseButtonUpEvent>(var);
 		case EventType::MOUSE_BUTTON_DOWN:   return &std::get<MouseButtonDownEvent>(var);
 		case EventType::MOUSE_WHEEL:         return &std::get<MouseWheelEvent>(var);
@@ -512,17 +522,6 @@ struct GetIfEventHelper<SdlEvent> { // extension for base-classes
 		case EventType::JOY_AXIS_MOTION:     return &std::get<JoystickAxisMotionEvent>(var);
 		case EventType::JOY_HAT:             return &std::get<JoystickHatEvent>(var);
 		case EventType::WINDOW:              return &std::get<WindowEvent>(var);
-		default: return nullptr;
-		}
-	}
-};
-template<>
-struct GetIfEventHelper<TimedEvent> {
-	const TimedEvent* operator()(const Event& event) {
-		const auto& var = event;
-		switch (EventType(var.index())) {
-		case EventType::KEY_UP:              return &std::get<KeyUpEvent>(var);
-		case EventType::KEY_DOWN:            return &std::get<KeyDownEvent>(var);
 		default: return nullptr;
 		}
 	}

commit e0cdb773f90c8c01b69b3769adf42ab6266b31d1

Fix kbd_numkeypad_enter_key setting
Fix KEYPAD_COMMA input bug introduced in d24d37d and add POSITIONAL mode support

Use a dummy value for SCANCODE as it is not used in unit tests.

src/events/SDLKey.hh
@@ -19,17 +19,17 @@ struct SDLKey {
 	SDL_Keysym sym;
 	bool down; // down=press / up=release
 
-	[[nodiscard]] static SDLKey create(SDL_Keycode code, bool down, uint16_t mod = 0) {
+	[[nodiscard]] static SDLKey create(SDL_Keycode code, SDL_Scancode scancode, bool down, uint16_t mod = 0) {
 		SDL_Keysym sym;
-		sym.scancode = SDL_SCANCODE_UNKNOWN;
+		sym.scancode = scancode; // scancode needs for POSITIONAL mapping mode
 		sym.sym = code;
 		sym.mod = mod;
 		sym.unused = 0; // unicode
 		return {.sym = sym, .down = down};
 	}
 
-	[[nodiscard]] static SDLKey createDown(SDL_Keycode code) {
-		return create(code, true);
+	[[nodiscard]] static SDLKey createDown(SDL_Keycode code, SDL_Scancode scancode) {
+		return create(code, scancode, true);
 	}
 
 	[[nodiscard]] static std::optional<SDLKey> fromString(std::string_view name);

src/input/Keyboard.cc
@@ -253,7 +253,8 @@ static constexpr auto getMSXMapping()
 		M{P{0x74}, K{SDLK_F8},          S{SDL_SCANCODE_F8}}, // STOP
 		M{P{0x75}, K{SDLK_BACKSPACE},   S{SDL_SCANCODE_BACKSPACE}},
 		M{P{0x76}, K{SDLK_F7},          S{SDL_SCANCODE_F7}}, // SELECT
-		M{P{0x77}, K{SDLK_RETURN},      S{SDL_SCANCODE_RETURN}},
+		M{P{0x77}, K{SDLK_RETURN,         SDLK_KP_ENTER},
+				   S{SDL_SCANCODE_RETURN, SDL_SCANCODE_KP_ENTER}},
 
 		M{P{0x80}, K{SDLK_SPACE},       S{SDL_SCANCODE_SPACE}},
 		M{P{0x81}, K{SDLK_HOME},        S{SDL_SCANCODE_HOME}},
@@ -371,7 +372,8 @@ static constexpr auto getSVIMapping()
 		M{P{0x63}, K{SDLK_RALT},        S{SDL_SCANCODE_RALT}},
 		M{P{0x64}, K{SDLK_ESCAPE},      S{SDL_SCANCODE_ESCAPE}},
 		M{P{0x65}, K{SDLK_F8},          S{SDL_SCANCODE_F8}}, // STOP
-		M{P{0x66}, K{SDLK_RETURN},      S{SDL_SCANCODE_RETURN}},
+		M{P{0x66}, K{SDLK_RETURN,         SDLK_KP_ENTER},
+				   S{SDL_SCANCODE_RETURN, SDL_SCANCODE_KP_ENTER}},
 		M{P{0x67}, K{SDLK_LEFT},        S{SDL_SCANCODE_LEFT}},
 
 		M{P{0x70}, K{SDLK_F1},          S{SDL_SCANCODE_F1}},
@@ -993,7 +995,8 @@ bool Keyboard::processQueuedEvent(const Event& event, EmuTime time)
 	} else if (key.sym.sym == SDLK_LALT) {
 		processGraphChange(time, down);
 		return false;
-	} else if (key.sym.sym == SDLK_KP_ENTER) {
+	// KEYPAD_RETURN and KEYPAD_COMMA exist at the same physical position on the host side.
+	} else if ((key.sym.sym == SDLK_KP_ENTER) || (key.sym.sym == SDLK_KP_COMMA)) {
 		processKeypadEnterKey(time, down);
 		return false;
 	} else {
@@ -1067,9 +1070,9 @@ void Keyboard::processKeypadEnterKey(EmuTime time, bool down)
 		return;
 	}
 	processSdlKey(time,
-	              SDLKey::create(keyboardSettings.getKpEnterMode() == KeyboardSettings::KpEnterMode::MSX_KP_COMMA
-	                                 ? SDLK_KP_ENTER : SDLK_RETURN,
-	                             down));
+		keyboardSettings.getKpEnterMode() == KeyboardSettings::KpEnterMode::MSX_KP_COMMA ?
+		SDLKey::create(SDLK_KP_COMMA, SDL_SCANCODE_KP_COMMA, down) :
+		SDLKey::create(SDLK_KP_ENTER, SDL_SCANCODE_KP_ENTER, down));
 }
 
 /*

src/unittest/Keys_test.cc
@@ -34,36 +34,36 @@ static void test(const std::string& name1, SDLKey code1, bool canonical)
 
 TEST_CASE("Keys")
 {
-	test("P", SDLKey::createDown(SDLK_p), true);
-	test("p", SDLKey::createDown(SDLK_p), false);
-	test("1", SDLKey::createDown(SDLK_1), true);
-	test("z", SDLKey::createDown(SDLK_z), false);
-	test("AT", SDLKey::createDown(SDLK_AT), false);
-	test("@", SDLKey::createDown(SDLK_AT), true);
-	test("Hash", SDLKey::createDown(SDLK_HASH), false);
-	test("slash", SDLKey::createDown(SDLK_SLASH), false);
-	test("EXCLAIM", SDLKey::createDown(SDLK_EXCLAIM), false);
-	test("!", SDLKey::createDown(SDLK_EXCLAIM), true);
-	test("PeRiOd", SDLKey::createDown(SDLK_PERIOD), false);
-	test("CARET", SDLKey::createDown(SDLK_CARET), false);
-	test("delete", SDLKey::createDown(SDLK_DELETE), false);
-	test("Keypad 7", SDLKey::createDown(SDLK_KP_7), true);
-	test("KP7", SDLKey::createDown(SDLK_KP_7), false);
-	test("KP_ENTER", SDLKey::createDown(SDLK_KP_ENTER), false);
-	test("up", SDLKey::createDown(SDLK_UP), false);
-	test("End", SDLKey::createDown(SDLK_END), false);
-	test("pageup", SDLKey::createDown(SDLK_PAGEUP), false);
-	test("PageDown", SDLKey::createDown(SDLK_PAGEDOWN), true);
-	test("PAGEDOWN", SDLKey::createDown(SDLK_PAGEDOWN), false);
-	test("F8", SDLKey::createDown(SDLK_F8), true);
-	test("RCTRL", SDLKey::createDown(SDLK_RCTRL), false);
-	test("Right Ctrl", SDLKey::createDown(SDLK_RCTRL), true);
+	test("P",          SDLKey::createDown(SDLK_p,        SDL_SCANCODE_UNKNOWN), true);
+	test("p",          SDLKey::createDown(SDLK_p,        SDL_SCANCODE_UNKNOWN), false);
+	test("1",          SDLKey::createDown(SDLK_1,        SDL_SCANCODE_UNKNOWN), true);
+	test("z",          SDLKey::createDown(SDLK_z,        SDL_SCANCODE_UNKNOWN), false);
+	test("AT",         SDLKey::createDown(SDLK_AT,       SDL_SCANCODE_UNKNOWN), false);
+	test("@",          SDLKey::createDown(SDLK_AT,       SDL_SCANCODE_UNKNOWN), true);
+	test("Hash",       SDLKey::createDown(SDLK_HASH,     SDL_SCANCODE_UNKNOWN), false);
+	test("slash",      SDLKey::createDown(SDLK_SLASH,    SDL_SCANCODE_UNKNOWN), false);
+	test("EXCLAIM",    SDLKey::createDown(SDLK_EXCLAIM,  SDL_SCANCODE_UNKNOWN), false);
+	test("!",          SDLKey::createDown(SDLK_EXCLAIM,  SDL_SCANCODE_UNKNOWN), true);
+	test("PeRiOd",     SDLKey::createDown(SDLK_PERIOD,   SDL_SCANCODE_UNKNOWN), false);
+	test("CARET",      SDLKey::createDown(SDLK_CARET,    SDL_SCANCODE_UNKNOWN), false);
+	test("delete",     SDLKey::createDown(SDLK_DELETE,   SDL_SCANCODE_UNKNOWN), false);
+	test("Keypad 7",   SDLKey::createDown(SDLK_KP_7,     SDL_SCANCODE_UNKNOWN), true);
+	test("KP7",        SDLKey::createDown(SDLK_KP_7,     SDL_SCANCODE_UNKNOWN), false);
+	test("KP_ENTER",   SDLKey::createDown(SDLK_KP_ENTER, SDL_SCANCODE_UNKNOWN), false);
+	test("up",         SDLKey::createDown(SDLK_UP,       SDL_SCANCODE_UNKNOWN), false);
+	test("End",        SDLKey::createDown(SDLK_END,      SDL_SCANCODE_UNKNOWN), false);
+	test("pageup",     SDLKey::createDown(SDLK_PAGEUP,   SDL_SCANCODE_UNKNOWN), false);
+	test("PageDown",   SDLKey::createDown(SDLK_PAGEDOWN, SDL_SCANCODE_UNKNOWN), true);
+	test("PAGEDOWN",   SDLKey::createDown(SDLK_PAGEDOWN, SDL_SCANCODE_UNKNOWN), false);
+	test("F8",         SDLKey::createDown(SDLK_F8,       SDL_SCANCODE_UNKNOWN), true);
+	test("RCTRL",      SDLKey::createDown(SDLK_RCTRL,    SDL_SCANCODE_UNKNOWN), false);
+	test("Right Ctrl", SDLKey::createDown(SDLK_RCTRL,    SDL_SCANCODE_UNKNOWN), true);
 
-	test("P+SHIFT", SDLKey::create(SDLK_p, true, KMOD_SHIFT), true);
-	test("q,shift", SDLKey::create(SDLK_q, true, KMOD_SHIFT), false);
-	test("R/shift", SDLKey::create(SDLK_r, true, KMOD_SHIFT), false);
-	test("ctrl+c", SDLKey::create(SDLK_c, true, KMOD_CTRL), false);
-	test("ALT/E", SDLKey::create(SDLK_e, true, KMOD_ALT), false);
-	test("E+ALT", SDLKey::create(SDLK_e, true, KMOD_ALT), true);
-	test("5,release", SDLKey::create(SDLK_5, false), false);
+	test("P+SHIFT",   SDLKey::create(SDLK_p, SDL_SCANCODE_UNKNOWN, true, KMOD_SHIFT), true);
+	test("q,shift",   SDLKey::create(SDLK_q, SDL_SCANCODE_UNKNOWN, true, KMOD_SHIFT), false);
+	test("R/shift",   SDLKey::create(SDLK_r, SDL_SCANCODE_UNKNOWN, true, KMOD_SHIFT), false);
+	test("ctrl+c",    SDLKey::create(SDLK_c, SDL_SCANCODE_UNKNOWN, true, KMOD_CTRL ), false);
+	test("ALT/E",     SDLKey::create(SDLK_e, SDL_SCANCODE_UNKNOWN, true, KMOD_ALT  ), false);
+	test("E+ALT",     SDLKey::create(SDLK_e, SDL_SCANCODE_UNKNOWN, true, KMOD_ALT  ), true);
+	test("5,release", SDLKey::create(SDLK_5, SDL_SCANCODE_UNKNOWN, false           ), false);
 }